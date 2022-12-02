a83755eb2685c7da3079353b33d4fb85fc4e2c44
Damien Garros <dgarros@gmail.com>
2022-12-02 11:50:12 +0100

    Add unit tests for is_valid_uuid

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,4 +1,5 @@
-from infrahub.utils import duplicates
+import uuid
+from infrahub.utils import duplicates, is_valid_uuid
 
 
 def test_duplicates():
@@ -6,3 +7,16 @@ def test_duplicates():
     assert duplicates([2, 4, 6, 8, 4, 6, 12]) == [4, 6]
     assert duplicates(["first", "second", "first", "third", "first", "last"]) == ["first"]
     assert duplicates([2, 8, 4, 6, 12]) == []
+
+
+def test_is_valid_uuid():
+
+    assert is_valid_uuid(uuid.uuid4()) is True
+    assert is_valid_uuid(uuid.UUID("ba0aecd9-546a-4d77-9187-23e17a20633e")) is True
+    assert is_valid_uuid("ba0aecd9-546a-4d77-9187-23e17a20633e") is True
+
+    assert is_valid_uuid("xxx-546a-4d77-9187-23e17a20633e") is False
+    assert is_valid_uuid(222) is False
+    assert is_valid_uuid(False) is False
+    assert is_valid_uuid("Not a valid UUID") is False
+    assert is_valid_uuid(uuid.UUID) is False
