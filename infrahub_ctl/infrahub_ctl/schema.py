d39489c2040ee944ba238f2c3755d57255a28ff9
Damien Garros <dgarros@gmail.com>
2023-03-09 15:48:34 +0100

    Add command to load schema from the cli

--- infrahub_ctl/infrahub_ctl/schema.py
+++ infrahub_ctl/infrahub_ctl/schema.py
@@ -1,11 +1,90 @@
+import logging
+from asyncio import run as aiorun
+from pathlib import Path
+
 import typer
+import ujson
+from pydantic import ValidationError
+from rich.console import Console
+from rich.logging import RichHandler
+from ujson import JSONDecodeError
+
+import infrahub_ctl.config as config
+from infrahub_client import InfrahubClient
 
 app = typer.Typer()
 
 
+async def _load(schema: Path, log: logging.Logger):
+    console = Console()
+
+    try:
+        schema_data = ujson.loads(schema.read_text())
+    except JSONDecodeError as exc:
+        console.print("[red]Invalid JSON file")
+        raise typer.Exit(2) from exc
+
+    client = await InfrahubClient.init(address=config.SETTINGS.server_address, insert_tracker=True)
+
+    try:
+        client.schema.validate(schema_data)
+    except ValidationError as exc:
+        console.print(f"[red]Schema not valid, found '{len(exc.errors())}' error(s)")
+        for error in exc.errors():
+            loc_str = [str(item) for item in error["loc"]]
+            console.print(f"  '{'/'.join(loc_str)}' | {error['msg']} ({error['type']})")
+        raise typer.Exit(2)
+
+    current_nodes = {node.kind.value: node for node in await client.all(kind="NodeSchema")}
+
+    for node_kind, node in current_nodes.items():
+        print(f"{node.id}: {node_kind}")
+
+    for node in schema_data.get("nodes"):
+        if node["kind"] in current_nodes:
+            # Ignoring the existing nodes for now, will need to revisit
+            pass
+
+        node_data = {key: value for key, value in node.items() if key not in ["relationships", "attributes"]}
+        log.info(f"Creating Node {node_data['kind']}")
+        new_node = await client.create(kind="NodeSchema", data=node_data)
+        await new_node.save()
+
+        log.info(f"Created Node {new_node.id}")
+
+        for attribute in node.get("attributes", []):
+            attribute["node"] = str(new_node.id)
+            new_attribute = await client.create(kind="AttributeSchema", data=attribute)
+            await new_attribute.save()
+            log.info(f"  - Attribute {attribute['name']} {new_attribute.id}")
+
+            # new_node.attributes.add(new_attribute)
+
+        for rel in node.get("relationships", []):
+            rel["node"] = str(new_node.id)
+            new_rel = await client.create(kind="RelationshipSchema", data=rel)
+            await new_rel.save()
+            log.info(f"  - Relationship {rel['name']} {new_rel.id}")
+            # new_node.relationships.add(new_rel)
+
+
 @app.command()
-def load():
-    pass
+def load(
+    schema: Path,
+    debug: bool = False,
+    config_file: str = typer.Option("infrahubctl.toml", envvar="INFRAHUBCTL_CONFIG"),
+):
+    """Execute a script."""
+    config.load_and_exit(config_file=config_file)
+
+    logging.getLogger("infrahub_client").setLevel(logging.CRITICAL)
+
+    log_level = "DEBUG" if debug else "INFO"
+    FORMAT = "%(message)s"
+    logging.basicConfig(level=log_level, format=FORMAT, datefmt="[%X]", handlers=[RichHandler()])
+    log = logging.getLogger("infrahubctl")
+
+    aiorun(_load(schema=schema, log=log))
 
 
 @app.command()
