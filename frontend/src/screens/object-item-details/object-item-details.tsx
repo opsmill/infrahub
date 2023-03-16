f18eda7c55714656de8f5a160244e5996a0279cc
pa-lem <pa-lem@hotmail.fr>
2023-03-16 15:43:29 +0100

    revert id column

--- frontend/src/screens/object-item-details/object-item-details.tsx
+++ frontend/src/screens/object-item-details/object-item-details.tsx
@@ -174,9 +174,9 @@ export default function ObjectItemDetails() {
         <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
           <dl className="sm:divide-y sm:divide-gray-200">
             <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
-              <dt className="text-sm font-medium text-gray-500">Display label</dt>
+              <dt className="text-sm font-medium text-gray-500">ID</dt>
               <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                {row.display_label}
+                {row.id}
               </dd>
             </div>
             {schema.attributes?.map((attribute) => {
