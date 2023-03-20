efd9df5098f3a380278684f48c232d6b87cd0216
Gaurav Saluja <gauravsaluja2006@gmail.com>
2023-03-20 15:01:19 +0530

    Refactor App.tsx to get schema details

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import { useAtom } from "jotai";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import { Branch } from "./generated/graphql";
 import { components } from "./infraops";
 import DeviceList from "./screens/device-list/device-list";
 import Layout from "./screens/layout/layout";
@@ -12,6 +13,7 @@ import OpsObjects from "./screens/ops-objects/ops-objects";
 import { branchState } from "./state/atoms/branch.atom";
 import { schemaState } from "./state/atoms/schema.atom";
 import { schemaKindNameState } from "./state/atoms/schemaKindName.atom";
+import * as R from "ramda";
 
 type APIResponse = components["schemas"]["SchemaAPI"];
 
@@ -48,45 +50,49 @@ export function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
+const getSchemaEndpoint = (branch: Branch | null) => {
+  return `http://localhost:8000/schema/?branch=${
+    branch ? branch.name : "main"
+  }`;
+};
+
 function App() {
   const [, setSchema] = useAtom(schemaState);
   const [, setSchemaKindNameState] = useAtom(schemaKindNameState);
   const [branch] = useAtom(branchState);
 
-  useEffect(() => {
-    fetch(
-      `http://localhost:8000/schema/?branch=${branch ? branch.name : "main"}`
-    )
-    .then((res) => res.json())
-    .then((data: APIResponse) => {
-      if (data.nodes.length) {
-        setSchema(
-          data.nodes.sort((a, b) => {
-            if (a.name && b.name) {
-              return a.name.localeCompare(b.name);
-            }
-            return -1;
-          })
-        );
-        setSchemaKindNameState(
-          data.nodes
-          .map((node) => ({
-            name: node.name,
-            kind: node.kind,
-          }))
-          .reduce((acc, value) => {
-            return {
-              ...acc,
-              [value.kind]: value.name,
-            };
-          }, {})
-        );
-      }
-    })
-    .catch(() => {
+  /**
+   * Fetch schema from the backend, sort, and return them
+   */
+  const fetchSchema = useCallback(async () => {
+    const schemaEndpoint = getSchemaEndpoint(branch);
+    const sortByName = R.sortBy(R.compose(R.toLower, R.prop("name")));
+    try {
+      const rawResponse = await fetch(schemaEndpoint);
+      const data = await rawResponse.json();
+      return sortByName(data.nodes || []);
+    } catch(err) {
       console.error("Something went wrong when fetching the schema details");
-    });
-  }, [setSchema, branch, setSchemaKindNameState]);
+      return [];
+    }
+  }, [branch])
+
+  /**
+   * Set schema in state atom
+   */
+  const setSchemaInState = useCallback(async () => {
+    const schema: APIResponse["nodes"] = await fetchSchema();
+    setSchema(schema);
+    const schemaNames = R.map(R.prop("name"), schema);
+    const schemaKinds = R.map(R.prop("kind"), schema);
+    const schemaKindNameTuples = R.zip(schemaKinds, schemaNames);
+    const schemaKindNameMap = R.fromPairs(schemaKindNameTuples);
+    setSchemaKindNameState(schemaKindNameMap);
+  }, [fetchSchema, setSchema, setSchemaKindNameState]);
+
+  useEffect(() => {
+    setSchemaInState();
+  }, [setSchemaInState, branch]);
 
   return <RouterProvider router={router} />;
 }
