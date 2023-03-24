e7d5f7e30b723af245fa3b381c14e4559a27eca4
pa-lem <pa-lem@hotmail.fr>
2023-03-24 12:33:35 +0100

    add popover as compoent

---
+++ frontend/src/components/popover.tsx
@@ -0,0 +1,28 @@
+// type PopOver = {}
+
+import { Popover, Transition } from "@headlessui/react"
+import { Fragment } from "react"
+
+export const PopOver = ({label, children}: any) => {
+  return (
+    <Popover className="relative">
+      <Popover.Button>
+        {label}
+      </Popover.Button>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-200"
+        enterFrom="opacity-0 translate-y-1"
+        enterTo="opacity-100 translate-y-0"
+        leave="transition ease-in duration-150"
+        leaveFrom="opacity-100 translate-y-0"
+        leaveTo="opacity-0 translate-y-1"
+      >
+        <Popover.Panel className="absolute rounded-md right-0 mt-3 p-2 bg-white">
+          {children}
+        </Popover.Panel>
+      </Transition>
+    </Popover>
+  )
+}
