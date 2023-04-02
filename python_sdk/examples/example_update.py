139e5fdaa2d9d0d7b8f91cd156a19c01b280ce99
Damien Garros <dgarros@gmail.com>
2023-04-02 11:00:16 +0200

    Add example scripts for python sdk

---
+++ python_sdk/examples/example_update.py
@@ -0,0 +1,13 @@
+from asyncio import run as aiorun
+
+from infrahub_client import InfrahubClient
+
+
+async def main():
+    client = await InfrahubClient.init(address="http://localhost:8000")
+    obj = await client.get(kind="Account", name__value="admin")
+    obj.label.value = "Administrator"
+    await obj.save()
+
+if __name__ == "__main__":
+    aiorun(main())
