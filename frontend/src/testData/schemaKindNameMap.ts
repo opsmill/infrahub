2bcafeba03d422123c7d1ca3c5b6c258473b8e59
Gaurav Saluja <gauravsaluja2006@gmail.com>
2023-04-06 14:11:04 +0530

    2 step dropdown/select component

---
+++ frontend/src/testData/schemaKindNameMap.ts
@@ -0,0 +1,10 @@
+import { iSchemaKindNameMap } from "../state/atoms/schemaKindName.atom";
+
+export const C_SchemaKindNameMap: iSchemaKindNameMap = {
+  Account: "account",
+  account: "Account",
+  Group: "group",
+  group: "Group",
+  Repository: "repository",
+  repository: "Repository",
+};
