98d9199465d633de72f36d4e172322b64dfbf776
Gaurav Saluja <gauravsaluja2006@gmail.com>
2023-04-07 16:19:48 +0530

    Generic schema map for dropdown options

--- frontend/src/state/atoms/schema.atom.ts
+++ frontend/src/state/atoms/schema.atom.ts
@@ -3,3 +3,8 @@ import { components } from "../../infraops";
 export type iNodeSchema = components["schemas"]["NodeSchema"];
 
 export const schemaState = atom<iNodeSchema[]>([]);
+
+export interface iGenericSchemaMapping {
+    [node: string]: string[];
+}
+export const genericSchemaState = atom<iGenericSchemaMapping>({});
