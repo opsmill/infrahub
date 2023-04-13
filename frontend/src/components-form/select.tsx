c428e20fdcc969c01ec5c041499b1e79b909659c
Gaurav Saluja <gauravsaluja2006@gmail.com>
2023-04-13 18:39:29 +0530

    Fix for not existing attributes

--- frontend/src/components-form/select.tsx
+++ frontend/src/components-form/select.tsx
@@ -36,12 +36,11 @@ export const OpsSelect = (props: Props) => {
       <Combobox.Label className="block text-sm font-medium leading-6 text-gray-900">{label}</Combobox.Label>
       <div className="relative">
         <Combobox.Input
-          disabled={disabled}
           className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           onChange={(event) => setQuery(event.target.value)}
           displayValue={(selected: HasNameAndID | undefined) => selected?.name ?? ""}
         />
-        <Combobox.Button disabled={disabled} className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+        <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
           <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
         </Combobox.Button>
 
