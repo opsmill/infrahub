0f72e72bf2de7bc98b0bf92dd57d9a0d62d7fb6e
Damien Garros <dgarros@gmail.com>
2023-04-14 15:37:51 +0200

    comment out unused tests

--- backend/tests/unit/core/test_manager_schema.py
+++ backend/tests/unit/core/test_manager_schema.py
@@ -158,7 +158,6 @@ from infrahub.core.schema import (
 # SchemaRegistryBranch
 # -----------------------------------------------------------------
 
-
 async def test_schema_branch_set():
     SCHEMA = {
         "name": "criticality",
@@ -475,103 +474,103 @@ async def test_schema_manager_get(default_branch):
 # -----------------------------------------------------------------
 
 
-async def test_load_node_to_db_node_schema(session, default_branch):
-    schema = SchemaRoot(**internal_schema)
-    await SchemaManager.register_schema_to_registry(schema=schema)
+# async def test_load_node_to_db_node_schema(session, default_branch):
+#     schema = SchemaRoot(**internal_schema)
+#     await SchemaManager.register_schema_to_registry(schema=schema)
 
-    SCHEMA = {
-        "name": "criticality",
-        "kind": "Criticality",
-        "default_filter": "name__value",
-        "attributes": [
-            {"name": "name", "kind": "Text", "unique": True},
-            {"name": "level", "kind": "Number"},
-            {"name": "color", "kind": "Text", "default_value": "#444444"},
-            {"name": "description", "kind": "Text", "optional": True},
-        ],
-    }
-    node = NodeSchema(**SCHEMA)
-    await SchemaManager.load_node_to_db(node=node, session=session)
+#     SCHEMA = {
+#         "name": "criticality",
+#         "kind": "Criticality",
+#         "default_filter": "name__value",
+#         "attributes": [
+#             {"name": "name", "kind": "Text", "unique": True},
+#             {"name": "level", "kind": "Number"},
+#             {"name": "color", "kind": "Text", "default_value": "#444444"},
+#             {"name": "description", "kind": "Text", "optional": True},
+#         ],
+#     }
+#     node = NodeSchema(**SCHEMA)
+#     await SchemaManager.load_node_to_db(node=node, session=session)
 
-    assert True
-
-
-async def test_load_node_to_db_generic_schema(session, default_branch):
-    schema = SchemaRoot(**internal_schema)
-    await SchemaManager.register_schema_to_registry(schema=schema)
-
-    SCHEMA = {
-        "name": "generic_interface",
-        "kind": "GenericInterface",
-        "attributes": [
-            {"name": "my_generic_name", "kind": "Text"},
-        ],
-    }
-    node = GenericSchema(**SCHEMA)
-    await SchemaManager.load_node_to_db(node=node, session=session)
-
-    assert True
+#     assert True
 
 
-async def test_load_node_to_db_group_schema(session, default_branch):
-    schema = SchemaRoot(**internal_schema)
-    await SchemaManager.register_schema_to_registry(schema=schema)
+# async def test_load_node_to_db_generic_schema(session, default_branch):
+#     schema = SchemaRoot(**internal_schema)
+#     await SchemaManager.register_schema_to_registry(schema=schema)
 
-    SCHEMA = {
-        "name": "generic_group",
-        "kind": "GenericGroup",
-    }
+#     SCHEMA = {
+#         "name": "generic_interface",
+#         "kind": "GenericInterface",
+#         "attributes": [
+#             {"name": "my_generic_name", "kind": "Text"},
+#         ],
+#     }
+#     node = GenericSchema(**SCHEMA)
+#     await SchemaManager.load_node_to_db(node=node, session=session)
 
-    node = GroupSchema(**SCHEMA)
-    await SchemaManager.load_node_to_db(node=node, session=session)
-
-    assert True
-
-
-async def test_load_schema_to_db_internal_models(session, default_branch):
-    schema = SchemaRoot(**internal_schema)
-    await SchemaManager.register_schema_to_registry(schema=schema)
-
-    await SchemaManager.load_schema_to_db(schema=schema, session=session)
-
-    node_schema = registry.get_schema(name="NodeSchema")
-    results = await SchemaManager.query(schema=node_schema, session=session)
-    assert len(results) > 1
+#     assert True
 
 
-async def test_load_schema_to_db_core_models(session, default_branch, register_internal_models_schema):
-    schema = SchemaRoot(**core_models)
-    await SchemaManager.register_schema_to_registry(schema=schema)
+# async def test_load_node_to_db_group_schema(session, default_branch):
+#     schema = SchemaRoot(**internal_schema)
+#     await SchemaManager.register_schema_to_registry(schema=schema)
 
-    await SchemaManager.load_schema_to_db(schema=schema, session=session)
+#     SCHEMA = {
+#         "name": "generic_group",
+#         "kind": "GenericGroup",
+#     }
 
-    node_schema = registry.get_schema(name="GenericSchema")
-    results = await SchemaManager.query(schema=node_schema, session=session)
-    assert len(results) > 1
+#     node = GroupSchema(**SCHEMA)
+#     await SchemaManager.load_node_to_db(node=node, session=session)
 
-
-async def test_load_schema_to_db_simple_01(
-    session, default_branch, register_core_models_schema, schema_file_infra_simple_01
-):
-    schema = SchemaRoot(**schema_file_infra_simple_01)
-
-    schema.extend_nodes_with_interfaces()
-    await SchemaManager.register_schema_to_registry(schema)
-    await SchemaManager.load_schema_to_db(schema=schema, session=session)
-
-    assert True
+#     assert True
 
 
-async def test_load_schema_to_db_w_generics_01(
-    session, default_branch, register_core_models_schema, schema_file_infra_w_generics_01
-):
-    schema = SchemaRoot(**schema_file_infra_w_generics_01)
+# async def test_load_schema_to_db_internal_models(session, default_branch):
+#     schema = SchemaRoot(**internal_schema)
+#     await SchemaManager.register_schema_to_registry(schema=schema)
 
-    schema.extend_nodes_with_interfaces()
-    await SchemaManager.register_schema_to_registry(schema)
-    await SchemaManager.load_schema_to_db(schema=schema, session=session)
+#     await SchemaManager.load_schema_to_db(schema=schema, session=session)
 
-    assert True
+#     node_schema = registry.get_schema(name="NodeSchema")
+#     results = await SchemaManager.query(schema=node_schema, session=session)
+#     assert len(results) > 1
+
+
+# async def test_load_schema_to_db_core_models(session, default_branch, register_internal_models_schema):
+#     schema = SchemaRoot(**core_models)
+#     await SchemaManager.register_schema_to_registry(schema=schema)
+
+#     await SchemaManager.load_schema_to_db(schema=schema, session=session)
+
+#     node_schema = registry.get_schema(name="GenericSchema")
+#     results = await SchemaManager.query(schema=node_schema, session=session)
+#     assert len(results) > 1
+
+
+# async def test_load_schema_to_db_simple_01(
+#     session, default_branch, register_core_models_schema, schema_file_infra_simple_01
+# ):
+#     schema = SchemaRoot(**schema_file_infra_simple_01)
+
+#     schema.extend_nodes_with_interfaces()
+#     await SchemaManager.register_schema_to_registry(schema)
+#     await SchemaManager.load_schema_to_db(schema=schema, session=session)
+
+#     assert True
+
+
+# async def test_load_schema_to_db_w_generics_01(
+#     session, default_branch, register_core_models_schema, schema_file_infra_w_generics_01
+# ):
+#     schema = SchemaRoot(**schema_file_infra_w_generics_01)
+
+#     schema.extend_nodes_with_interfaces()
+#     await SchemaManager.register_schema_to_registry(schema)
+#     await SchemaManager.load_schema_to_db(schema=schema, session=session)
+
+#     assert True
 
 
 async def test_load_schema_from_db(session, reset_registry, default_branch, register_internal_models_schema):
@@ -630,13 +629,11 @@ async def test_load_schema_from_db(session, reset_registry, default_branch, regi
     }
     schema1 = SchemaRoot(**FULL_SCHEMA)
     registry.schema.load_schema_to_db(schema=schema1, session=session)
-    # await SchemaManager.load_schema_to_db
-
     schema2 = await registry.schema.load_schema_from_db(session=session)
 
     assert len(schema2.nodes) == 2
     assert len(schema2.generics) == 1
-    # assert len(schema2.groups) == 1
+    assert len(schema2.groups) == 1
 
     schema_criticality = [node for node in schema2.nodes if node.kind == "Criticality"][0]
 
