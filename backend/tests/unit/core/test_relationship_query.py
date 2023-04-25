ea98ed61321eee60ce1528f9844310523f3cc552
Damien Garros <dgarros@gmail.com>
2023-04-25 06:23:59 +0200

    Refactor test_relationship_query to use more fixtures and run on both main and branch2

--- backend/tests/unit/core/test_relationship_query.py
+++ backend/tests/unit/core/test_relationship_query.py
@@ -2,6 +2,7 @@ import pytest
 from neo4j import AsyncSession
 
 from infrahub.core import registry
+from infrahub.core.branch import Branch
 from infrahub.core.node import Node
 from infrahub.core.query.relationship import (
     RelationshipCreateQuery,
@@ -19,139 +20,126 @@ class DummyRelationshipQuery(RelationshipQuery):
         pass
 
 
-async def test_RelationshipQuery_init(session, default_branch, person_tag_schema):
+async def test_RelationshipQuery_init(
+    session: AsyncSession, tag_blue_main: Node, person_jack_main: Node, branch: Branch
+):
     person_schema = registry.get_schema(name="Person")
     rel_schema = person_schema.get_relationship("tags")
-
-    t1 = await Node.init(session=session, schema="Tag")
-    await t1.new(session=session, name="blue")
-    await t1.save(session=session)
-    p1 = await Node.init(session=session, schema=person_schema)
-    await p1.new(session=session, firstname="John", lastname="Doe")
-    await p1.save(session=session)
 
     with pytest.raises(ValueError) as exc:
         rq = DummyRelationshipQuery()
     assert "Either source or source_id must be provided." in str(exc.value)
 
     with pytest.raises(ValueError) as exc:
-        rq = DummyRelationshipQuery(source=p1)
+        rq = DummyRelationshipQuery(source=person_jack_main)
     assert "Either rel or rel_type must be provided." in str(exc.value)
 
     with pytest.raises(ValueError) as exc:
-        rq = DummyRelationshipQuery(source=p1, rel=Relationship)
+        rq = DummyRelationshipQuery(source=person_jack_main, rel=Relationship)
     assert "Either an instance of Relationship or a valid schema must be provided." in str(exc.value)
 
     with pytest.raises(ValueError) as exc:
-        rq = DummyRelationshipQuery(source=p1, rel=Relationship, schema=rel_schema)
+        rq = DummyRelationshipQuery(source=person_jack_main, rel=Relationship, schema=rel_schema)
     assert "Either an instance of Relationship or a valid branch must be provided." in str(exc.value)
 
     # Initialization with the Relationship class
-    rq = DummyRelationshipQuery(source=p1, rel=Relationship, schema=rel_schema, branch=default_branch)
+    rq = DummyRelationshipQuery(source=person_jack_main, rel=Relationship, schema=rel_schema, branch=branch)
     assert rq.schema == rel_schema
-    assert rq.branch == default_branch
-    assert rq.source_id == p1.id
-    assert rq.source == p1
+    assert rq.branch == branch
+    assert rq.source_id == person_jack_main.id
+    assert rq.source == person_jack_main
 
-    rq = DummyRelationshipQuery(source_id=p1.id, rel=Relationship, schema=rel_schema, branch=default_branch)
+    rq = DummyRelationshipQuery(source_id=person_jack_main.id, rel=Relationship, schema=rel_schema, branch=branch)
     assert rq.schema == rel_schema
-    assert rq.branch == default_branch
-    assert rq.source_id == p1.id
+    assert rq.branch == branch
+    assert rq.source_id == person_jack_main.id
     assert rq.source is None
 
     # Initialization with an instance of Relationship
-    rel = Relationship(schema=rel_schema, branch=default_branch, node=p1)
-    rq = DummyRelationshipQuery(source=p1, rel=rel)
+    rel = Relationship(schema=rel_schema, branch=branch, node=person_jack_main)
+    rq = DummyRelationshipQuery(source=person_jack_main, rel=rel)
     assert rq.schema == rel_schema
-    assert rq.branch == default_branch
+    assert rq.branch == branch
 
 
-async def test_query_RelationshipCreateQuery(session, default_branch, person_tag_schema):
+async def test_query_RelationshipCreateQuery(
+    session: AsyncSession, tag_blue_main: Node, person_jack_main: Node, branch: Branch
+):
     person_schema = registry.get_schema(name="Person")
     rel_schema = person_schema.get_relationship("tags")
 
-    t1 = await Node.init(session=session, schema="Tag")
-    await t1.new(session=session, name="blue")
-    await t1.save(session=session)
-    t2 = await Node.init(session=session, schema="Tag")
-    await t2.new(session=session, name="red")
-    await t2.save(session=session)
-    p1 = await Node.init(session=session, schema=person_schema)
-    await p1.new(session=session, firstname="John", lastname="Doe")
-    await p1.save(session=session)
-
     query = await RelationshipCreateQuery.init(
         session=session,
-        source=p1,
-        destination=t1,
+        source=person_jack_main,
+        destination=tag_blue_main,
         schema=rel_schema,
         rel=Relationship,
-        branch=default_branch,
+        branch=branch,
         at=Timestamp(),
     )
     await query.execute(session=session)
 
     # We should have 2 paths between t1 and p1
     # First for the relationship, Second via the branch
-    paths = await get_paths_between_nodes(session=session, source_id=t1.db_id, destination_id=p1.db_id, max_length=2)
+    paths = await get_paths_between_nodes(
+        session=session, source_id=tag_blue_main.db_id, destination_id=person_jack_main.db_id, max_length=2
+    )
     assert len(paths) == 2
 
 
-async def test_query_RelationshipCreateQuery_w_node_property(session, default_branch, person_tag_schema, first_account):
+async def test_query_RelationshipCreateQuery_w_node_property(
+    session: AsyncSession, tag_blue_main: Node, person_jack_main: Node, first_account: Node, branch: Branch
+):
     person_schema = registry.get_schema(name="Person")
     rel_schema = person_schema.get_relationship("tags")
 
-    t1 = await Node.init(session=session, schema="Tag")
-    await t1.new(session=session, name="blue")
-    await t1.save(session=session)
-    t2 = await Node.init(session=session, schema="Tag")
-    await t2.new(session=session, name="red")
-    await t2.save(session=session)
-    p1 = await Node.init(session=session, schema=person_schema)
-    await p1.new(session=session, firstname="John", lastname="Doe")
-    await p1.save(session=session)
-
     paths = await get_paths_between_nodes(
-        session=session, source_id=t1.db_id, destination_id=p1.db_id, relationships=["IS_RELATED"], max_length=2
+        session=session,
+        source_id=tag_blue_main.db_id,
+        destination_id=person_jack_main.db_id,
+        relationships=["IS_RELATED"],
+        max_length=2,
     )
     assert len(paths) == 0
 
-    rel = Relationship(schema=rel_schema, branch=default_branch, node=p1, source=first_account, owner=first_account)
-    query = await RelationshipCreateQuery.init(session=session, source=p1, destination=t1, rel=rel)
+    rel = Relationship(
+        schema=rel_schema, branch=branch, node=person_jack_main, source=first_account, owner=first_account
+    )
+    query = await RelationshipCreateQuery.init(
+        session=session, branch=branch, source=person_jack_main, destination=tag_blue_main, rel=rel
+    )
     await query.execute(session=session)
 
     paths = await get_paths_between_nodes(
-        session=session, source_id=t1.db_id, destination_id=p1.db_id, relationships=["IS_RELATED"], max_length=2
+        session=session,
+        source_id=tag_blue_main.db_id,
+        destination_id=person_jack_main.db_id,
+        relationships=["IS_RELATED"],
+        max_length=2,
     )
     assert len(paths) == 1
 
 
-async def test_query_RelationshipDeleteQuery(session, default_branch, person_tag_schema):
+async def test_query_RelationshipDeleteQuery(
+    session: AsyncSession, tag_blue_main: Node, person_jack_tags_main: Node, branch: Branch
+):
     person_schema = registry.get_schema(name="Person")
     rel_schema = person_schema.get_relationship("tags")
 
-    t1 = await Node.init(session=session, schema="Tag")
-    await t1.new(session=session, name="blue")
-    await t1.save(session=session)
-    t2 = await Node.init(session=session, schema="Tag")
-    await t2.new(session=session, name="red")
-    await t2.save(session=session)
-    p1 = await Node.init(session=session, schema=person_schema)
-    await p1.new(session=session, firstname="John", lastname="Doe", tags=[t1, t2])
-    await p1.save(session=session)
-
     # We should have 2 paths between t1 and p1
     # First for the relationship, Second via the branch
-    paths = await get_paths_between_nodes(session=session, source_id=t1.db_id, destination_id=p1.db_id, max_length=2)
+    paths = await get_paths_between_nodes(
+        session=session, source_id=tag_blue_main.db_id, destination_id=person_jack_tags_main.db_id, max_length=2
+    )
     assert len(paths) == 2
 
     query = await RelationshipDeleteQuery.init(
         session=session,
-        source=p1,
-        destination=t1,
+        source=person_jack_tags_main,
+        destination=tag_blue_main,
         schema=rel_schema,
         rel=Relationship,
-        branch=default_branch,
+        branch=branch,
         at=Timestamp(),
     )
     await query.execute(session=session)
@@ -159,25 +147,25 @@ async def test_query_RelationshipDeleteQuery(session, default_branch, person_tag
     # We should have 5 paths between t1 and p1
     # Because we have 3 "real" paths between the nodes
     # but if we calculate all the permutations it will equal to 5 paths.
-    paths = await get_paths_between_nodes(session=session, source_id=t1.db_id, destination_id=p1.db_id, max_length=2)
+    paths = await get_paths_between_nodes(
+        session=session, source_id=tag_blue_main.db_id, destination_id=person_jack_tags_main.db_id, max_length=2
+    )
     assert len(paths) == 5
 
 
-async def test_query_RelationshipGetPeerQuery(session, default_branch, person_tag_schema):
+async def test_query_RelationshipGetPeerQuery(
+    session: AsyncSession, tag_blue_main: Node, person_jack_tags_main: Node, branch: Branch
+):
     person_schema = registry.get_schema(name="Person")
     rel_schema = person_schema.get_relationship("tags")
-    t1 = await Node.init(session=session, schema="Tag")
-    await t1.new(session=session, name="blue")
-    await t1.save(session=session)
-    t2 = await Node.init(session=session, schema="Tag")
-    await t2.new(session=session, name="red")
-    await t2.save(session=session)
-    p1 = await Node.init(session=session, schema=person_schema)
-    await p1.new(session=session, firstname="John", lastname="Doe", tags=[t1, t2])
-    await p1.save(session=session)
 
     query = await RelationshipGetPeerQuery.init(
-        session=session, source_id=p1.id, schema=rel_schema, rel=Relationship, branch=default_branch, at=Timestamp()
+        session=session,
+        source_id=person_jack_tags_main.id,
+        schema=rel_schema,
+        rel=Relationship,
+        branch=branch,
+        at=Timestamp(),
     )
     await query.execute(session=session)
 
@@ -195,29 +183,22 @@ async def test_query_RelationshipGetPeerQuery(session, default_branch, person_ta
     assert isinstance(peers[0].properties["is_protected"].prop_db_id, str)
 
 
-async def test_query_RelationshipGetPeerQuery_with_filter(session, default_branch, person_tag_schema):
+async def test_query_RelationshipGetPeerQuery_with_filter(
+    session: AsyncSession, person_jack_tags_main: Node, branch: Branch
+):
     person_schema = registry.get_schema(name="Person")
     rel_schema = person_schema.get_relationship("tags")
 
-    t1 = await Node.init(session=session, schema="Tag")
-    await t1.new(session=session, name="blue")
-    await t1.save(session=session)
-    t2 = await Node.init(session=session, schema="Tag")
-    await t2.new(session=session, name="red")
-    await t2.save(session=session)
-    p1 = await Node.init(session=session, schema=person_schema)
-    await p1.new(session=session, firstname="John", lastname="Doe", tags=[t1, t2])
-    await p1.save(session=session)
-
     query = await RelationshipGetPeerQuery.init(
         session=session,
-        source_id=p1.id,
+        source_id=person_jack_tags_main.id,
         schema=rel_schema,
-        filters={"tags__name__value": "blue"},
+        filters={"tags__name__value": "Blue"},
         rel=Relationship,
-        branch=default_branch,
+        branch=branch,
         at=Timestamp(),
     )
+
     await query.execute(session=session)
 
     assert len(query.get_peer_ids()) == 1
