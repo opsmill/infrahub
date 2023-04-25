7dcdf452e85c11c3150eac50a3fcd2d27055a6b4
Patrick Ogenstad <patrick@ogenstad.com>
2023-04-25 07:43:42 +0200

    Catch errors from  InfrahubRepository.new()

--- backend/infrahub/git/actions.py
+++ backend/infrahub/git/actions.py
@@ -1,8 +1,12 @@
+import logging
+
 from infrahub.exceptions import RepositoryError
 from infrahub.lock import registry as lock_registry
 from infrahub_client import InfrahubClient
 
 from .repository import InfrahubRepository
+
+LOGGER = logging.getLogger("infrahub.git")
 
 
 async def sync_remote_repositories(client: InfrahubClient) -> None:
@@ -11,14 +15,23 @@ async def sync_remote_repositories(client: InfrahubClient) -> None:
 
     for repo_name, repository in repositories.items():
         async with lock_registry.get(repo_name):
+            init_failed = False
             try:
                 repo = await InfrahubRepository.init(
                     id=repository.id, name=repository.name, location=repository.location, client=client
                 )
-            except RepositoryError:
-                repo = await InfrahubRepository.new(
-                    id=repository.id, name=repository.name, location=repository.location, client=client
-                )
-                await repo.import_objects_from_files(branch_name=repo.default_branch_name)
+            except RepositoryError as excinfo:
+                LOGGER.error(excinfo)
+                init_failed = True
+
+            if init_failed:
+                try:
+                    repo = await InfrahubRepository.new(
+                        id=repository.id, name=repository.name, location=repository.location, client=client
+                    )
+                    await repo.import_objects_from_files(branch_name=repo.default_branch_name)
+                except RepositoryError as excinfo:
+                    LOGGER.error(excinfo)
+                    continue
 
             await repo.sync()
