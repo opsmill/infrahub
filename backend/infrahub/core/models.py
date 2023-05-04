37a43d4690ca3802c3c21229957bdebe11e814a4
Damien Garros <dgarros@gmail.com>
2023-05-04 06:33:14 +0200

    Remove comments

--- backend/infrahub/core/models.py
+++ backend/infrahub/core/models.py
@@ -9,12 +9,3 @@ class NodeSchema(Node):
 
 class RelationshipSchema(Node):
     pass
-    # async def process_identifier(self, session: AsyncSession) -> None:
-    #     if self.identifier.value:
-    #         return None
-
-    #     if hasattr(self, "node") and getattr(self, "node"):
-    #         node = await self.node.get_peer(session=session)
-    #         if node.kind.value and self.peer.value:
-    #             identifier = "__".join(sorted([node.kind.value, self.peer.value]))
-    #             self.identifier.value = identifier.lower()
