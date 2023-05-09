94e131e19aa3f281e996a59d97d901ff8f24e5d4
Gaurav Saluja <gauravsaluja2006@gmail.com>
2023-05-09 14:12:50 +0530

    Add React version auto0detect in ESLint. Fix lint error.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,11 @@
+import * as TaildinwFormsPlugin from "@tailwindcss/forms";
+
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [TaildinwFormsPlugin],
 };
