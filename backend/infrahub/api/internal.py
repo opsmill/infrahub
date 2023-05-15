23edcfbe540fff7af1c4be6d14deda7ab4ef3de5
Damien Garros <dgarros@gmail.com>
2023-05-15 12:10:56 +0200

    Rename features to experimental_features, simplify env default settings

--- backend/infrahub/api/internal.py
+++ backend/infrahub/api/internal.py
@@ -3,7 +3,12 @@ from pydantic import BaseModel
 
 import infrahub.config as config
 from infrahub import __version__
-from infrahub.config import AnalyticsSettings, LoggingSettings, MainSettings, FeaturesSettings
+from infrahub.config import (
+    AnalyticsSettings,
+    ExperimentalFeaturesSettings,
+    LoggingSettings,
+    MainSettings,
+)
 from infrahub.core import registry
 
 router = APIRouter()
@@ -13,7 +18,7 @@ class ConfigAPI(BaseModel):
     main: MainSettings
     logging: LoggingSettings
     analytics: AnalyticsSettings
-    features: FeaturesSettings
+    features: ExperimentalFeaturesSettings
 
 
 class InfoAPI(BaseModel):
@@ -23,7 +28,12 @@ class InfoAPI(BaseModel):
 
 @router.get("/config")
 async def get_config() -> ConfigAPI:
-    return ConfigAPI(main=config.SETTINGS.main, logging=config.SETTINGS.logging, analytics=config.SETTINGS.analytics, features=config.SETTINGS.features)
+    return ConfigAPI(
+        main=config.SETTINGS.main,
+        logging=config.SETTINGS.logging,
+        analytics=config.SETTINGS.analytics,
+        experimental_features=config.SETTINGS.experimental_features,
+    )
 
 
 @router.get("/info")
