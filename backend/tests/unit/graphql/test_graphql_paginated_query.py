6065b2fe0005d5dfee7e5792df1fadaa4c09f4fc
Patrick Ogenstad <patrick@ogenstad.com>
2023-05-23 17:01:17 +0200

    Add a unittest for the paginated schema generator

--- backend/tests/unit/graphql/test_graphql_paginated_query.py
+++ backend/tests/unit/graphql/test_graphql_paginated_query.py
@@ -1,7 +1,9 @@
 from graphql import graphql
 
+from infrahub.core import registry
 from infrahub.core.branch import Branch
 from infrahub.core.node import Node
+from infrahub.core.schema import NodeSchema
 from infrahub.graphql import generate_graphql_paginated_schema
 
 
@@ -77,3 +79,49 @@ async def test_simple_query_with_offset_and_limit(db, session, default_branch: B
     assert result.errors is None
     assert result.data["criticality"]["count"] == 2
     assert len(result.data["criticality"]["edges"]) == 1
+
+
+async def test_display_label_one_item(db, session, default_branch: Branch, data_schema):
+    SCHEMA = {
+        "name": "criticality",
+        "kind": "Criticality",
+        "display_labels": ["label__value"],
+        "branch": True,
+        "attributes": [
+            {"name": "name", "kind": "Text", "unique": True},
+            {"name": "label", "kind": "Text", "optional": True},
+        ],
+    }
+
+    schema = NodeSchema(**SCHEMA)
+    registry.set_schema(name=schema.kind, schema=schema)
+
+    obj1 = await Node.init(session=session, schema=schema)
+    await obj1.new(session=session, name="low")
+    await obj1.save(session=session)
+
+    query = """
+    query {
+        criticality {
+            edges {
+                node {
+                    id
+                    display_label
+                }
+            }
+        }
+    }
+    """
+    result = await graphql(
+        await generate_graphql_paginated_schema(
+            branch=default_branch, session=session, include_mutation=False, include_subscription=False
+        ),
+        source=query,
+        context_value={"infrahub_session": session, "infrahub_database": db, "infrahub_branch": default_branch},
+        root_value=None,
+        variable_values={},
+    )
+
+    assert result.errors is None
+    assert len(result.data["criticality"]["edges"]) == 1
+    assert result.data["criticality"]["edges"][0]["node"]["display_label"] == "Low"
