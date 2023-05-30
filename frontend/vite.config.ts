46e7f52a1316ea5601c45938d1a8509680b5d73d
pa-lem <pa-lem@hotmail.fr>
2023-05-30 15:32:59 +0200

    update docker to build and serve the frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,11 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    port: 8080,
+  },
+  preview: {
     port: 3000,
+    host: "0.0.0.0",
   },
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   test: {
