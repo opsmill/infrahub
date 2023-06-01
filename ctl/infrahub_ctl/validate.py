93cf3b35fb0d41912818b544954be025ac0e6405
Damien Garros <dgarros@gmail.com>
2023-06-01 18:00:42 +0200

    ADD validate graphql command

--- ctl/infrahub_ctl/validate.py
+++ ctl/infrahub_ctl/validate.py
@@ -1,5 +1,7 @@
+import sys
 from asyncio import run as aiorun
 from pathlib import Path
+from typing import List, Optional
 
 import typer
 import yaml
@@ -8,12 +10,12 @@ from rich.console import Console
 from ujson import JSONDecodeError
 
 import infrahub_ctl.config as config
-from infrahub_client import InfrahubClient
+from infrahub_client import InfrahubClient, InfrahubClientSync
+from infrahub_client.exceptions import GraphQLError
+from infrahub_ctl.exceptions import QueryNotFoundError
+from infrahub_ctl.utils import find_graphql_query, get_branch
 
 app = typer.Typer()
-
-DEFAULT_CONFIG_FILE = "infrahubctl.toml"
-ENVVAR_CONFIG_FILE = "INFRAHUBCTL_CONFIG"
 
 
 @app.callback()
@@ -48,8 +50,58 @@ async def _schema(schema: Path) -> None:
 
 @app.command(name="schema")
 def validate_schema(
-    schema: Path, config_file: Path = typer.Option(DEFAULT_CONFIG_FILE, envvar=ENVVAR_CONFIG_FILE)
+    schema: Path, config_file: Path = typer.Option(config.DEFAULT_CONFIG_FILE, envvar=config.ENVVAR_CONFIG_FILE)
 ) -> None:
     """Validate the format of a schema file either in JSON or YAML"""
     config.load_and_exit(config_file=config_file)
     aiorun(_schema(schema=schema))
+
+
+@app.command(name="graphql-query")
+def validate_graphql(
+    query: str,
+    vars: Optional[List[str]] = typer.Argument(
+        None, help="Variables to pass along with the query. Format key=value key=value."
+    ),
+    debug: bool = typer.Option(False, help="Display more troubleshooting information."),
+    branch: str = typer.Option(None, help="Branch on which to validate the GraphQL Query."),
+    config_file: Path = typer.Option(config.DEFAULT_CONFIG_FILE, envvar=config.ENVVAR_CONFIG_FILE),
+) -> None:
+    """Validate the format of a GraphQL Query stored locally by executing it on a remote GraphQL endpoint"""
+    "DEBUG" if debug else "INFO"
+
+    config.load_and_exit(config_file=config_file)
+
+    console = Console()
+
+    branch = get_branch(branch)
+
+    try:
+        query_str = find_graphql_query(query)
+    except QueryNotFoundError:
+        console.print(f"[red]Unable to find the GraphQL Query : {query}")
+        sys.exit(1)
+
+    console.print(f"[purple]Query '{query}' will be validated on branch '{branch}'.")
+
+    variables = {}
+    if vars:
+        variables = {var.split("=")[0]: var.split("=")[1] for var in vars if "=" in var}
+
+    client = InfrahubClientSync.init(address=config.SETTINGS.server_address, insert_tracker=True)
+    try:
+        response = client.execute_graphql(
+            query=query_str, branch_name=branch, variables=variables, raise_for_error=False
+        )
+    except GraphQLError as exc:
+        console.print(f"[red]{len(exc.errors)} error(s) occured while executing the query")
+        for error in exc.errors:
+            if isinstance(error, dict) and "message" in error and "locations" in error:
+                console.print(f"[yellow] - Message: {error['message']}")
+                console.print(f"[yellow]   Location: {error['locations']}")
+            elif isinstance(error, str) and "Branch:" in error:
+                console.print(f"[yellow] - {error}")
+                console.print(f"[yellow]   you can specify a different branch with --branch")
+        sys.exit(1)
+
+    console.print("[green]Query executed successfully.")
