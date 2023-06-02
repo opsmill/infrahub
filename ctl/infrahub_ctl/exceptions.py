d0d47bbbb320d946cc0ce88b3587277865a16590
Damien Garros <dgarros@gmail.com>
2023-06-02 08:50:10 +0200

    Refactor infrahubctl render

--- ctl/infrahub_ctl/exceptions.py
+++ ctl/infrahub_ctl/exceptions.py
@@ -6,3 +6,15 @@ class QueryNotFoundError(Error):
     def __init__(self, name: str, message: str = ""):
         self.message = message or f"The requested query '{name}' was not found."
         super().__init__(self.message)
+
+
+class FileNotFoundError(Error):
+    def __init__(self, name: str, message: str = ""):
+        self.message = message or f"Unable to locate the file '{name}'."
+        super().__init__(self.message)
+
+
+class FileNotValidError(Error):
+    def __init__(self, name: str, message: str = ""):
+        self.message = message or f"Unable to access the file '{name}'."
+        super().__init__(self.message)
