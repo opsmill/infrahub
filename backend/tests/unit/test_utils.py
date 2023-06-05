91bbf0eb089c0f99010f0665fb6045b9f51ca7b0
Damien Garros <dgarros@gmail.com>
2023-06-05 14:38:05 +0200

    Move is_uuid_valid function in the SDK

--- backend/tests/unit/test_utils.py
+++ backend/tests/unit/test_utils.py
@@ -1,33 +1,14 @@
 import os
-import uuid
 
 import pytest
 
-from infrahub.utils import (
-    deep_merge_dict,
-    duplicates,
-    get_fixtures_dir,
-    is_valid_uuid,
-    str_to_bool,
-)
+from infrahub.utils import deep_merge_dict, duplicates, get_fixtures_dir, str_to_bool
 
 
 def test_duplicates():
     assert duplicates([2, 4, 6, 8, 4, 6, 12]) == [4, 6]
     assert duplicates(["first", "second", "first", "third", "first", "last"]) == ["first"]
     assert duplicates([2, 8, 4, 6, 12]) == []
-
-
-def test_is_valid_uuid():
-    assert is_valid_uuid(uuid.uuid4()) is True
-    assert is_valid_uuid(uuid.UUID("ba0aecd9-546a-4d77-9187-23e17a20633e")) is True
-    assert is_valid_uuid("ba0aecd9-546a-4d77-9187-23e17a20633e") is True
-
-    assert is_valid_uuid("xxx-546a-4d77-9187-23e17a20633e") is False
-    assert is_valid_uuid(222) is False
-    assert is_valid_uuid(False) is False
-    assert is_valid_uuid("Not a valid UUID") is False
-    assert is_valid_uuid(uuid.UUID) is False
 
 
 def test_get_fixtures_dir():
--- python_sdk/infrahub_client/utils.py
+++ python_sdk/infrahub_client/utils.py
@@ -1,5 +1,7 @@
 import os
 from pathlib import Path
+from typing import Any
+from uuid import UUID
 
 
 def get_fixtures_dir() -> Path:
@@ -8,3 +10,12 @@ def get_fixtures_dir() -> Path:
     fixtures_dir = os.path.join(here, "..", "tests", "fixtures")
 
     return Path(os.path.abspath(fixtures_dir))
+
+
+def is_valid_uuid(value: Any) -> bool:
+    """Check if the input is a valid UUID."""
+    try:
+        UUID(str(value))
+        return True
+    except ValueError:
+        return False
