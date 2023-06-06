86695193a84b78a87652d334c0fba28d1c1df38e
pa-lem <pa-lem@hotmail.fr>
2023-06-06 13:10:33 +0200

    switch from spinning bubble to bars

--- frontend/src/screens/loading-screen/loading-screen.tsx
+++ frontend/src/screens/loading-screen/loading-screen.tsx
@@ -16,12 +16,7 @@ export default function LoadingScreen(props: Props) {
   return (
     <div
       className={classNames("flex-1 flex flex-col items-center justify-center", className ?? "")}>
-      <ReactLoading
-        className={color}
-        type={"spinningBubbles"}
-        height={size ?? 70}
-        width={size ?? 70}
-      />
+      <ReactLoading className={color} type={"bars"} height={size ?? 70} width={size ?? 70} />
       {!hideText && <div className="text-xl mt-2">Just a moment</div>}
     </div>
   );
