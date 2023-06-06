36ca00615d25ee315cdd434d71562385843fdd43
pa-lem <pa-lem@hotmail.fr>
2023-06-06 15:52:10 +0200

    fix metadata update

--- frontend/src/screens/object-item-meta-edit/object-item-meta-edit.tsx
+++ frontend/src/screens/object-item-meta-edit/object-item-meta-edit.tsx
@@ -1,55 +1,15 @@
+import { gql, useReactiveVar } from "@apollo/client";
+import { useState } from "react";
 import { toast } from "react-toastify";
 import { ALERT_TYPES, Alert } from "../../components/alert";
+import graphqlClient from "../../graphql/graphqlClientApollo";
 import { updateObjectWithId } from "../../graphql/mutations/objects/updateObjectWithId";
+import { branchVar } from "../../graphql/variables/branchVar";
+import { dateVar } from "../../graphql/variables/dateVar";
 import { iNodeSchema } from "../../state/atoms/schema.atom";
 import { getFormStructureForMetaEdit } from "../../utils/formStructureForCreateEdit";
 import { stringifyWithoutQuotes } from "../../utils/string";
 import EditFormHookComponent from "../edit-form-hook/edit-form-hook-component";
-
-// const optionsLeft: SelectOption[] = [
-//   {
-//     label: "Account",
-//     value: "account"
-//   },
-//   {
-//     label: "Group",
-//     value: "group"
-//   },
-//   {
-//     label: "Repository",
-//     value: "repository"
-//   }
-// ];
-
-// function Toggle(props: {label: string}) {
-//   const { label } = props;
-//   const [enabled, setEnabled] = useState(false);
-
-//   return (
-//     <Switch.Group as="div" className="flex flex-col justify-center">
-//       <Switch.Label as="span" className="text-sm">
-//         <span className="font-medium text-gray-900">{label}</span>
-//       </Switch.Label>
-//       <Switch
-//         checked={enabled}
-//         onChange={setEnabled}
-//         className={classNames(
-//           enabled ? "bg-indigo-600" : "bg-gray-200",
-//           "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 mt-2"
-//         )}
-//       >
-//         <span
-//           aria-hidden="true"
-//           className={classNames(
-//             enabled ? "translate-x-5" : "translate-x-0",
-//             "pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
-//           )}
-//         />
-//       </Switch>
-//     </Switch.Group>
-//   );
-// }
-
 interface Props {
   row: any;
   schema: iNodeSchema;
@@ -69,21 +29,32 @@ export default function ObjectItemMetaEdit(props: Props) {
     schema,
     schemaList,
     attributeOrRelationshipToEdit,
+    onUpdateComplete,
   } = props;
+
+  const branch = useReactiveVar(branchVar);
+  const date = useReactiveVar(dateVar);
+  const [isLoading, setIsLoading] = useState(false);
+
   const formStructure = getFormStructureForMetaEdit(
     attributeOrRelationshipToEdit,
     type,
     attributeOrRelationshipName,
     schemaList
   );
+
   async function onSubmit(data: any) {
+    setIsLoading(true);
+
     let updatedObject: any = {
       id: props.row.id,
     };
+
     if (type === "relationship") {
       const relationshipSchema = schema.relationships?.find(
         (s) => s.name === attributeOrRelationshipName
       );
+
       if (relationshipSchema?.cardinality === "many") {
         const newRelationshipList = row[attributeOrRelationshipName].map((item: any) => {
           if (item.id === props.attributeOrRelationshipToEdit.id) {
@@ -97,6 +68,7 @@ export default function ObjectItemMetaEdit(props: Props) {
             };
           }
         });
+
         updatedObject[attributeOrRelationshipName] = newRelationshipList;
       } else {
         updatedObject[attributeOrRelationshipName] = {
@@ -110,19 +82,47 @@ export default function ObjectItemMetaEdit(props: Props) {
       };
     }
 
-    try {
-      await updateObjectWithId({
-        name: schema.name,
-        data: stringifyWithoutQuotes({
-          id: row.id,
-          ...updatedObject,
-        }),
-      });
-      toast(<Alert type={ALERT_TYPES.SUCCESS} message={"Metadata updated"} />);
-    } catch {
-      console.error("Something went wrong while updating the object");
+    if (Object.keys(updatedObject).length) {
+      try {
+        const mutationString = updateObjectWithId({
+          name: schema.name,
+          data: stringifyWithoutQuotes({
+            id: row.id,
+            ...updatedObject,
+          }),
+        });
+
+        const mutation = gql`
+          ${mutationString}
+        `;
+
+        await graphqlClient.mutate({
+          mutation,
+          context: { branch: branch?.name, date },
+        });
+
+        toast(<Alert type={ALERT_TYPES.SUCCESS} message={"Metadata updated"} />);
+
+        onUpdateComplete();
+
+        setIsLoading(false);
+
+        return;
+      } catch (e) {
+        setIsLoading(false);
+
+        toast(
+          <Alert
+            message="Something went wrong while updating the meetadata"
+            type={ALERT_TYPES.ERROR}
+          />
+        );
+
+        console.error("Something went wrong while updating the meetadata", e);
+
+        return;
+      }
     }
-    props.onUpdateComplete();
   }
 
   return (
@@ -131,50 +131,8 @@ export default function ObjectItemMetaEdit(props: Props) {
         onCancel={props.closeDrawer}
         onSubmit={onSubmit}
         fields={formStructure}
+        isLoading={isLoading}
       />
     </div>
   );
-  // return (
-  //   <>
-  //     <div className="mt-2 bg-white px-4 flex-1">
-  //       <div>
-  //         <Select2Step
-  //           defaultValue={{ parent: "group", child: "0831f2dd-4138-4b00-9fdc-c366abee2bd0" }}
-  //           label="Source"
-  //           optionsLeft={optionsLeft}
-  //           onChange={(value) => {
-  //             // console.log(value);
-  //           }}
-  //         />
-  //       </div>
-  //       <div>
-  //         <Select2Step
-  //           defaultValue={{ parent: "group", child: "0831f2dd-4138-4b00-9fdc-c366abee2bd0" }}
-  //           label="Owner"
-  //           optionsLeft={optionsLeft}
-  //           onChange={(value) => {
-  //             // console.log(value);
-  //           }}
-  //         />
-  //       </div>
-  //       <div className="mt-4">
-  //         <Toggle label="Is Visible" />
-  //       </div>
-  //       <div className="mt-4">
-  //         <Toggle label="Is Protected" />
-  //       </div>
-  //     </div>
-  //     <div className="mt-6 flex items-center justify-end gap-x-6 py-3 max-w-lg pr-3 border-t">
-  //       <button type="button" className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-  //         Cancel
-  //       </button>
-  //       <button
-  //         type="submit"
-  //         className="rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-  //       >
-  //         Save
-  //       </button>
-  //     </div>
-  //   </>
-  // );
 }
