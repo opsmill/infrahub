926dd2cb5784e1e975fc96ffc600917e6792ce78
pa-lem <pa-lem@hotmail.fr>
2023-06-08 17:02:56 +0200

    add first e2e test with localhost

---
+++ frontend/tests/e2e/app-init.cy.ts
@@ -0,0 +1,11 @@
+describe("Main application", () => {
+  it("should load", () => {
+    cy.visit("/");
+
+    // The first item should be Account
+    cy.get("[href='/objects/account'] > .group").should("have.text", "Account");
+
+    // The branch selector should display the main branch
+    cy.get(".ml-2\\.5").should("have.text", "main");
+  });
+});
