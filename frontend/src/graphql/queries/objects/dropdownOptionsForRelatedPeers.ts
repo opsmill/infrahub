52bf6579ebdaa81192b1f66228b6ab770b2698ab
pa-lem <pa-lem@hotmail.fr>
2023-06-09 09:38:44 +0200

    remov legacy templates

--- frontend/src/graphql/queries/objects/dropdownOptionsForRelatedPeers.ts
+++ frontend/src/graphql/queries/objects/dropdownOptionsForRelatedPeers.ts
@@ -9,39 +9,6 @@ export interface iPeerDropdownOptions {
   [peer: string]: iPeerDropdownOption[];
 }
 
-/**
- * Get form options, for each relationshp get all the dropdown options by creating a dynamic query and fetching id and displaylabel
- * Peers -> [status, tag, role, location]
- *
- * Sample output below
- * {
- *    "status": [
- *      { "id": "1", "display_label": "Provisionning" },
- *      { "id": "2", "display_label": "Active" }
- *    ],
- *    "tag": [
- *      { "id": "1", "display_label": "red" },
- *      { "id": "2", "display_label": "green" }
- *    ],
- *    "role": [
- *      { "id": "1", "display_label": "Management" },
- *      { "id": "2", "display_label": "Loopback" }
- *    ],
- *    "location": [
- *      { "id": "1", "display_label": "jfk1" },
- *      { "id": "2", "display_label": "ord1" }
- *    ]
- *  }
- */
-export const getDropdownOptionsForRelatedPeers = Handlebars.compile(`query DropdownFormOptions {
-    {{#each peers}}
-    {{this}} {
-      id
-      display_label
-    }
-    {{/each}}
-}`);
-
 export const getDropdownOptionsForRelatedPeersPaginated =
   Handlebars.compile(`query DropdownFormOptions {
   {{#each peers}}
