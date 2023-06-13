35c9a608bacfff4ad9f8752acb6da19aa65fd2eb
Damien Garros <dgarros@gmail.com>
2023-06-13 17:02:22 +0200

    MMove some logic into generator directory

--- sync/infrahub-sync/infrahub_sync/cli.py
+++ sync/infrahub-sync/infrahub_sync/cli.py
@@ -14,14 +14,7 @@ from rich.console import Console
 from pydantic import BaseModel
 
 from infrahub_sync import SyncConfig, SyncInstance, SchemaMappingModel, SyncAdapter
-from infrahub_sync.jinja_filters import (
-    filter_get_identifier,
-    filter_get_attributes,
-    filter_list_to_set,
-    filter_list_to_str,
-    filter_has_node,
-    filter_has_field,
-)
+from infrahub_sync.generator import render_template
 from infrahub_client import InfrahubClientSync
 
 from potenda import Potenda
@@ -31,16 +24,13 @@ app = typer.Typer()
 
 logging.basicConfig(level=logging.INFO)
 
-# INFRAHUB_URL = "http://localhost:8000"
-
-# NETBOX_URL = "https://demo.netbox.dev"
-# NETBOX_TOKEN = "110ed3a44c36ea5edf2ed30bb941fe0187f466bf"
 
 def import_adapter(adapter: SyncAdapter, directory: str):
     here = os.path.abspath(os.path.dirname(__file__))
     relative_directory = directory.replace(here, "")[1:]
     module = importlib.import_module(f"infrahub_sync.{relative_directory.replace('/', '.')}.{adapter.name}.adapter")
     return getattr(module, f"{adapter.name.title()}Sync")
+
 
 def get_all_sync() -> List[SyncInstance]:
     results = []
@@ -69,9 +59,6 @@ def list():
         print(f"{item.name} | {item.source.name} >> {item.destination.name} | {item.directory}")
 
 
-TOP_LEVEL = ["tag", "location"]  # "role"]
-
-
 @app.command()
 def diff(
     name: str = typer.Argument(..., help="Name of the sync to use"),
@@ -87,7 +74,7 @@ def diff(
     src = source(config=sync_instance, target="source", adapter=sync_instance.source)
     dst = destination(config=sync_instance, target="destination", adapter=sync_instance.destination)
 
-    ptd = Potenda(destination=dst, source=src, config=sync_instance, top_level=TOP_LEVEL)
+    ptd = Potenda(destination=dst, source=src, config=sync_instance, top_level=sync_instance.order)
     ptd.load()
 
     mydiff = ptd.diff()
@@ -110,7 +97,7 @@ def sync(
     src = source(config=sync_instance, target="source", adapter=sync_instance.source)
     dst = destination(config=sync_instance, target="destination", adapter=sync_instance.destination)
 
-    ptd = Potenda(destination=dst, source=src, config=sync_instance, top_level=TOP_LEVEL)
+    ptd = Potenda(destination=dst, source=src, config=sync_instance, top_level=sync_instance.order)
     ptd.load()
 
     mydiff = ptd.diff()
@@ -118,7 +105,6 @@ def sync(
     print(mydiff.str())
 
     ptd.sync(diff=mydiff)
-
 
 
 @app.command()
@@ -143,8 +129,10 @@ def generate(
     here = os.path.abspath(os.path.dirname(__file__))
     current_dir = str(os.getcwd())
 
-    template_dir_absolute = str(os.path.join(here, "templates"))
+    template_dir_absolute = str(os.path.join(here, "generator/templates"))
     template_dir_relative = template_dir_absolute.replace(current_dir, "")
+
+    schema = client.schema.all()
 
     for adapter in [sync_instance.source, sync_instance.destination]:
         output_dir_absolute = str(os.path.join(sync_instance.directory, adapter.name))
@@ -153,44 +141,52 @@ def generate(
             Path(output_dir_absolute).mkdir(exist_ok=True)
 
         for item in files_to_render:
-            template_path = os.path.join(template_dir_relative, item[0])
-            output_file = Path(os.path.join(output_dir_absolute, item[1]))
+            render_template(
+                template_dir=template_dir_relative,
+                template_file=item[0],
+                output_dir=output_dir_absolute,
+                output_file=item[1],
+                context={"schema": schema, "adapter": adapter, "config": sync_instance},
+            )
 
-            templateLoader = jinja2.FileSystemLoader(searchpath=".")
-            templateEnv = jinja2.Environment(loader=templateLoader, trim_blocks=True, lstrip_blocks=True)
-            templateEnv.filters["get_identifier"] = filter_get_identifier
-            templateEnv.filters["get_attributes"] = filter_get_attributes
-            templateEnv.filters["list_to_set"] = filter_list_to_set
-            templateEnv.filters["list_to_str"] = filter_list_to_str
-            templateEnv.filters["has_node"] = filter_has_node
-            templateEnv.filters["has_field"] = filter_has_field
+            #     template_path = os.path.join(template_dir_relative, item[0])
+            #     output_file = Path(os.path.join(output_dir_absolute, item[1]))
 
-            template = templateEnv.get_template(str(template_path))
+            #     templateLoader = jinja2.FileSystemLoader(searchpath=".")
+            #     templateEnv = jinja2.Environment(loader=templateLoader, trim_blocks=True, lstrip_blocks=True)
+            #     templateEnv.filters["get_identifier"] = filter_get_identifier
+            #     templateEnv.filters["get_attributes"] = filter_get_attributes
+            #     templateEnv.filters["list_to_set"] = filter_list_to_set
+            #     templateEnv.filters["list_to_str"] = filter_list_to_str
+            #     templateEnv.filters["has_node"] = filter_has_node
+            #     templateEnv.filters["has_field"] = filter_has_field
 
-            dict_schema = dict(client.schema.all())
+            #     template = templateEnv.get_template(str(template_path))
 
-            node_list = []
-            for _, node in dict_schema.items():
-                has_unique = False
-                for attr in node.attributes:
-                    if attr.kind in ["Text", "String", "TextArea", "DateTime", "HashedPassword"]:
-                        attr.kind = "str"
-                    elif attr.kind in ["Number", "Integer"]:
-                        attr.kind = "int"
-                    elif attr.kind in ["Boolean"]:
-                        attr.kind = "bool"
+            #     dict_schema = dict(client.schema.all())
 
-                    if attr.unique:
-                        has_unique = True
+            #     node_list = []
+            #     for _, node in dict_schema.items():
+            #         has_unique = False
+            #         for attr in node.attributes:
+            #             if attr.kind in ["Text", "String", "TextArea", "DateTime", "HashedPassword"]:
+            #                 attr.kind = "str"
+            #             elif attr.kind in ["Number", "Integer"]:
+            #                 attr.kind = "int"
+            #             elif attr.kind in ["Boolean"]:
+            #                 attr.kind = "bool"
 
-                for attr in node.attributes:
-                    if attr.optional or attr.default_value is not None:
-                        attr.kind = f"Optional[{attr.kind}]"
+            #             if attr.unique:
+            #                 has_unique = True
 
-                if has_unique:
-                    node_list.append(node.kind)
+            #         for attr in node.attributes:
+            #             if attr.optional or attr.default_value is not None:
+            #                 attr.kind = f"Optional[{attr.kind}]"
 
-            rendered_tpl = template.render(adapter=adapter, config=sync_instance, schema=dict_schema, node_list=node_list)  # type: ignore[arg-type]
+            #         if has_unique:
+            #             node_list.append(node.kind)
+
+            #     rendered_tpl = template.render(adapter=adapter, config=sync_instance, schema=dict_schema, node_list=node_list)  # type: ignore[arg-type]
 
             console.print(f"Saved {item[0]} in {item[1]}")
-            output_file.write_text(rendered_tpl)
+            # output_file.write_text(rendered_tpl)
---
+++ sync/infrahub-sync/infrahub_sync/generator/__init__.py
@@ -0,0 +1,139 @@
+import logging
+from typing import List, Optional, Any, Union
+from asyncio import run as aiorun
+import os
+import typer
+import jinja2
+import glob
+import yaml
+
+import importlib
+
+from pathlib import Path
+from rich.console import Console
+from pydantic import BaseModel
+
+from infrahub_sync import SyncConfig, SyncInstance, SchemaMappingModel, SyncAdapter
+from infrahub_sync.generator.utils import (
+    list_to_set,
+    list_to_str,
+)
+from infrahub_client import InfrahubClientSync, NodeSchema, AttributeSchema, RelationshipSchema
+
+
+ATTRIBUTE_KIND_MAP = {
+    "Text": "str",
+    "String": "str",
+    "TextArea": "str",
+    "DateTime": "str",
+    "HashedPassword": "str",
+    "Number": "int",
+    "Integer": "int",
+    "Boolean": "bool",
+}
+
+
+def has_node(config: SyncConfig, name: str) -> bool:
+    for item in config.schema_mapping:
+        if item.name == name:
+            return True
+    return False
+
+
+def has_field(config: SyncConfig, name: str, field: str) -> bool:
+    for item in config.schema_mapping:
+        if item.name == name:
+            for subitem in item.fields:
+                if subitem.name == field:
+                    return True
+    return False
+
+
+def get_identifiers(node: NodeSchema, config: SyncConfig) -> List[str]:
+    """Return the identifiers that should be used by DiffSync."""
+    identifiers = [
+        attr.name for attr in node.attributes if attr.unique and has_field(config, name=node.name, field=attr.name)
+    ]
+
+    if not identifiers:
+        return None
+    return identifiers
+
+
+def get_attributes(node: NodeSchema, config: SyncConfig) -> List[str]:
+    """Return the attributes that should be used by DiffSync."""
+    attrs_attributes = [
+        attr.name for attr in node.attributes if not attr.unique and has_field(config, name=node.name, field=attr.name)
+    ]
+    rels_identifiers = [
+        rel.name
+        for rel in node.relationships
+        if rel.cardinality == "one" and has_field(config, name=node.name, field=rel.name)
+    ]
+
+    attributes = rels_identifiers + attrs_attributes
+
+    if not attributes:
+        return None
+
+    return attributes
+
+
+def get_children(node: NodeSchema, config: SyncConfig) -> str:
+    # rel.peer.lower() might now work in all cases we should have a better function to convert that
+    children = {
+        rel.peer.lower(): rel.name
+        for rel in node.relationships
+        if rel.cardinality == "many" and has_field(config, name=node.name, field=rel.name)
+    }
+
+    if not children:
+        return None
+
+    children_list = [f'"{key}": "{value}"' for key, value in children.items()]
+    return "{ " + ", ".join(children_list) + " }"
+
+
+def get_kind(item: Union[RelationshipSchema, AttributeSchema]) -> str:
+    kind = "str"
+    if isinstance(item, AttributeSchema):
+        kind = ATTRIBUTE_KIND_MAP.get(item.kind, "str")
+        if item.optional or item.default_value is not None:
+            kind = f"Optional[{kind}]"
+
+    elif isinstance(item, RelationshipSchema) and item.cardinality == "one":
+        if item.optional:
+            kind = f"Optional[{kind}]"
+
+    elif isinstance(item, RelationshipSchema) and item.cardinality == "many":
+        kind = "List[str] = []"
+
+    return kind
+
+
+def has_children(node: NodeSchema, config: SyncConfig) -> bool:
+    if get_children(config=config, node=node):
+        return True
+    return False
+
+
+def render_template(template_dir: str, template_file: str, output_dir: str, output_file: str, context):
+    template_path = os.path.join(template_dir, template_file)
+    output_file = Path(os.path.join(output_dir, output_file))
+
+    templateLoader = jinja2.FileSystemLoader(searchpath=".")
+    templateEnv = jinja2.Environment(loader=templateLoader, trim_blocks=True, lstrip_blocks=True)
+    templateEnv.filters["get_identifiers"] = get_identifiers
+    templateEnv.filters["get_attributes"] = get_attributes
+    templateEnv.filters["get_children"] = get_children
+    templateEnv.filters["list_to_set"] = list_to_set
+    templateEnv.filters["list_to_str"] = list_to_str
+    templateEnv.filters["has_node"] = has_node
+    templateEnv.filters["has_field"] = has_field
+    templateEnv.filters["has_children"] = has_children
+    templateEnv.filters["get_kind"] = get_kind
+
+    template = templateEnv.get_template(str(template_path))
+
+    rendered_tpl = template.render(**context)  # type: ignore[arg-type]
+    output_file.write_text(rendered_tpl)
