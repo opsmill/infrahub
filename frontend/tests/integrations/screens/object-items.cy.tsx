5b1b5cdc65e93aa8d1502d713b73b182bc8bdbe3
pa-lem <pa-lem@hotmail.fr>
2023-06-14 08:26:50 +0200

    add test for last page in pagination

--- frontend/tests/integrations/screens/object-items.cy.tsx
+++ frontend/tests/integrations/screens/object-items.cy.tsx
@@ -6,7 +6,11 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import ObjectItems from "../../../src/screens/object-items/object-items-paginated";
 import { schemaState } from "../../../src/state/atoms/schema.atom";
-import { graphqlQueriesMocksData, graphqlQueriesMocksQuery } from "../../mocks/data/graphqlQueries";
+import {
+  graphqlQueriesMocksData,
+  graphqlQueriesMocksQuery,
+  graphqlQueriesMocksQueryWithLimit,
+} from "../../mocks/data/graphqlQueries";
 import { schemaMocks } from "../../mocks/data/schema";
 import { TestProvider } from "../../mocks/jotai/atom";
 
@@ -18,10 +22,22 @@ const graphqlQueryItemsPath = "/objects/:objectname";
 
 // Mock the apollo query and data
 const mocks: any[] = [
+  // Initial query
   {
     request: {
       query: gql`
         ${graphqlQueriesMocksQuery}
+      `,
+    },
+    result: {
+      data: graphqlQueriesMocksData,
+    },
+  },
+  // After limit update
+  {
+    request: {
+      query: gql`
+        ${graphqlQueriesMocksQueryWithLimit}
       `,
     },
     result: {
@@ -68,6 +84,16 @@ describe("List screen", () => {
     cy.get(":nth-child(4) > :nth-child(5) > div.flex > :nth-child(3)").should(
       "have.text",
       "maroon"
+    );
+
+    // Select the limit 50
+    cy.get("[id^=headlessui-combobox-button-]").click();
+    cy.contains("50").click();
+
+    // The last page should be the number 20
+    cy.get(":nth-child(7) > .cursor-pointer").should(
+      "have.text",
+      Math.ceil(graphqlQueriesMocksData.graphql_query.count / 50)
     );
   });
 });
