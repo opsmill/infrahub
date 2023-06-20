65f54a51ca12c2ae9d3d9182290fe66e0b5b6a13
pa-lem <pa-lem@hotmail.fr>
2023-06-20 08:56:10 +0200

    add profile edit

--- frontend/src/screens/user-profile/user-profile.tsx
+++ frontend/src/screens/user-profile/user-profile.tsx
@@ -1,18 +1,23 @@
-import { gql } from "@apollo/client";
-import { Cog6ToothIcon } from "@heroicons/react/24/outline";
+import { gql, useReactiveVar } from "@apollo/client";
+import { Cog6ToothIcon, PencilIcon, Square3Stack3DIcon } from "@heroicons/react/24/outline";
 import { useAtom } from "jotai";
+import { useState } from "react";
 import { StringParam, useQueryParam } from "use-query-params";
 import { Avatar } from "../../components/avatar";
+import { Button } from "../../components/button";
+import SlideOver from "../../components/slide-over";
 import { Tabs } from "../../components/tabs";
-import { ACCESS_TOKEN_KEY } from "../../config/constants";
+import { ACCESS_TOKEN_KEY, DEFAULT_BRANCH_NAME } from "../../config/constants";
 import { QSP } from "../../config/qsp";
 import { getProfileDetails } from "../../graphql/queries/profile/getProfileDetails";
+import { branchVar } from "../../graphql/variables/branchVar";
 import useQuery from "../../hooks/useQuery";
 import { configState } from "../../state/atoms/config.atom";
 import { schemaState } from "../../state/atoms/schema.atom";
 import { parseJwt } from "../../utils/common";
 import { getSchemaRelationshipColumns } from "../../utils/getSchemaObjectColumns";
 import LoadingScreen from "../loading-screen/loading-screen";
+import ObjectItemEditComponent from "../object-item-edit/object-item-edit-paginated";
 import TabPassword from "./tab-account";
 import TabPreferences from "./tab-preferences";
 import TabProfile from "./tab-profile";
@@ -63,6 +68,8 @@ export default function UserProfile() {
   const [qspTab] = useQueryParam(QSP.TAB, StringParam);
   const [config] = useAtom(configState);
   const [schemaList] = useAtom(schemaState);
+  const [showEditDrawer, setShowEditDrawer] = useState(false);
+  const branch = useReactiveVar(branchVar);
 
   const schema = schemaList.find((s) => s.name === ACCOUNT_OBJECT);
 
@@ -72,11 +79,13 @@ export default function UserProfile() {
 
   const tokenData = parseJwt(localToken);
 
+  const accountId = tokenData?.sub;
+
   const queryString = schema
     ? getProfileDetails({
         ...schema,
         relationships,
-        objectid: tokenData?.sub,
+        objectid: accountId,
       })
     : // Empty query to make the gql parsing work
       // TODO: Find another solution for queries while loading schema
@@ -87,7 +96,7 @@ export default function UserProfile() {
   `;
 
   // TODO: Find a way to avoid querying object details if we are on a tab
-  const { loading, data } = useQuery(query, { skip: !schema });
+  const { loading, data, refetch } = useQuery(query, { skip: !schema });
 
   if (loading || !schema) {
     return <LoadingScreen />;
@@ -131,10 +140,59 @@ export default function UserProfile() {
       </div>
 
       <div className="sticky top-0 z-10 shadow-sm">
-        <Tabs tabs={tabs} />
+        <Tabs
+          tabs={tabs}
+          rightItems={
+            <Button
+              disabled={config?.main?.allow_anonymous_access}
+              onClick={() => setShowEditDrawer(true)}
+              className="mr-4">
+              Edit
+              <PencilIcon className="-mr-0.5 h-4 w-4" aria-hidden="true" />
+            </Button>
+          }
+        />
       </div>
 
       {renderContent(qspTab, objectDetailsData)}
+
+      <SlideOver
+        title={
+          <div className="space-y-2">
+            <div className="flex items-center w-full">
+              <span className="text-lg font-semibold mr-3">{objectDetailsData.display_label}</span>
+              <div className="flex-1"></div>
+              <div className="flex items-center">
+                <Square3Stack3DIcon className="w-5 h-5" />
+                <div className="ml-1.5 pb-1">{branch?.name ?? DEFAULT_BRANCH_NAME}</div>
+              </div>
+            </div>
+            <span className="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
+              <svg
+                className="h-1.5 w-1.5 mr-1 fill-yellow-500"
+                viewBox="0 0 6 6"
+                aria-hidden="true">
+                <circle cx={3} cy={3} r={3} />
+              </svg>
+              {schema.kind}
+            </span>
+            <div className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10 ml-3">
+              <svg className="h-1.5 w-1.5 mr-1 fill-blue-500" viewBox="0 0 6 6" aria-hidden="true">
+                <circle cx={3} cy={3} r={3} />
+              </svg>
+              ID: {objectDetailsData.id}
+            </div>
+          </div>
+        }
+        open={showEditDrawer}
+        setOpen={setShowEditDrawer}>
+        <ObjectItemEditComponent
+          closeDrawer={() => setShowEditDrawer(false)}
+          onUpdateComplete={() => refetch()}
+          objectid={accountId}
+          objectname={ACCOUNT_OBJECT!}
+        />
+      </SlideOver>
     </div>
   );
 }
