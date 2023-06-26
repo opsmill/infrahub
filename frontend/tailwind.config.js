491c8a89ee339db89d9e4d33e00035744c3d47f5
pa-lem <pa-lem@hotmail.fr>
2023-06-26 17:13:42 +0200

    add custom colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,19 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-blue": "#0B97BB",
+        "custom-blue-green": "#0B6581",
+        "custom-dark-gray": "#0D3F54",
+        "custom-gray": "#0B1829",
+        "custom-black": "#000000",
+        "custom-white": "#FFFFFF",
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
