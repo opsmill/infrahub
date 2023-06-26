dc3605743fb7ccff890536ae489a5cbdfa3c48c4
pa-lem <pa-lem@hotmail.fr>
2023-06-26 17:21:22 +0200

    fix blue-gray color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       colors: {
         "custom-blue": "#0B97BB",
         "custom-blue-green": "#0B6581",
-        "custom-dark-gray": "#0D3F54",
+        "custom-blue-gray": "#0D3F54",
         "custom-gray": "#0B1829",
         "custom-black": "#000000",
         "custom-white": "#FFFFFF",
