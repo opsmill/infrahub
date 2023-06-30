1eac2dafaae19919242b085d109f95ce7bad09c7
Damien Garros <dgarros@gmail.com>
2023-06-30 14:11:15 +0200

    Pass Config object to InfrahubClient

--- ctl/infrahub_ctl/client.py
+++ ctl/infrahub_ctl/client.py
@@ -2,6 +2,7 @@ from typing import Any
 
 import infrahub_ctl.config as config
 from infrahub_client import InfrahubClient, InfrahubClientSync
+from infrahub_client.config import Config
 
 
 async def initialize_client(**kwargs: Any) -> InfrahubClient:
@@ -11,7 +12,7 @@ async def initialize_client(**kwargs: Any) -> InfrahubClient:
         client_config["api_token"] = config.SETTINGS.api_token
 
     client = await InfrahubClient.init(
-        address=config.SETTINGS.server_address, config=client_config or None, insert_tracker=True, **kwargs
+        address=config.SETTINGS.server_address, config=Config(**client_config), insert_tracker=True, **kwargs
     )
 
     return client
@@ -24,7 +25,7 @@ def initialize_client_sync(**kwargs: Any) -> InfrahubClientSync:
         client_config["api_token"] = config.SETTINGS.api_token
 
     client = InfrahubClientSync.init(
-        address=config.SETTINGS.server_address, config=client_config or None, insert_tracker=True, **kwargs
+        address=config.SETTINGS.server_address, config=Config(**client_config), insert_tracker=True, **kwargs
     )
 
     return client
