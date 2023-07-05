61740c0bcafdb682f8fa566e061a527141296a83
pa-lem <pa-lem@hotmail.fr>
2023-07-05 15:05:13 +0200

    add test to verify user profile

--- frontend/tests/e2e/auth.cy.ts
+++ frontend/tests/e2e/auth.cy.ts
@@ -1,35 +1,35 @@
 /// <reference types="cypress" />
 
-import { ADMIN_CREDENTIALS } from "../utils";
+import { ADMIN_CREDENTIALS, READ_ONLY_CREDENTIALS, READ_WRITE_CREDENTIALS } from "../utils";
 
 describe("Authentication", () => {
   beforeEach(() => {
     cy.visit("/");
   });
 
-  // it("should login as read-write role and have write access", () => {
-  //   cy.login(ADMIN_CREDENTIALS.username, ADMIN_CREDENTIALS.password);
+  it("should login as read-write role and have write access", () => {
+    cy.login(READ_WRITE_CREDENTIALS.username, READ_WRITE_CREDENTIALS.password);
 
-  //   cy.visit("/");
+    cy.visit("/");
 
-  //   // The branch creation button should not be disabled
-  //   cy.get("#headlessui-popover-button-\\:r8\\: > .py-1\\.5").should("not.be.disabled");
+    // The branch creation button should not be disabled
+    cy.get("#headlessui-popover-button-\\:r8\\: > .py-1\\.5").should("not.be.disabled");
 
-  //   // The avatar should have the intials
-  //   cy.get(".h-12").should("have.text", "CO");
-  // });
+    // The avatar should have the intials
+    cy.get(".h-12").should("have.text", "CO");
+  });
 
-  // it("should login as read-only role and have read-only access", () => {
-  //   cy.login(READ_ONLY_CREDENTIALS.username, READ_ONLY_CREDENTIALS.password);
+  it("should login as read-only role and have read-only access", () => {
+    cy.login(READ_ONLY_CREDENTIALS.username, READ_ONLY_CREDENTIALS.password);
 
-  //   cy.visit("/");
+    cy.visit("/");
 
-  //   // The branch creation button should be disabled
-  //   cy.get("#headlessui-popover-button-\\:r8\\: > .py-1\\.5").should("be.disabled");
+    // The branch creation button should be disabled
+    cy.get("#headlessui-popover-button-\\:r8\\: > .py-1\\.5").should("be.disabled");
 
-  //   // The avatar should have the intials
-  //   cy.get(".h-12").should("have.text", "JB");
-  // });
+    // The avatar should have the intials
+    cy.get(".h-12").should("have.text", "JB");
+  });
 
   it("should access the profile page", () => {
     cy.login(ADMIN_CREDENTIALS.username, ADMIN_CREDENTIALS.password);
@@ -52,5 +52,6 @@ describe("Authentication", () => {
     cy.get(".-mb-px > .border-transparent").click();
 
     // Check if a token exists
+    cy.get(".hover\\:bg-gray-50 > :nth-child(2)").should("exist");
   });
 });
