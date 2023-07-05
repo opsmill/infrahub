391cc41aec03886a32d9cd94438d2258ec72616a
Damien Garros <dgarros@gmail.com>
2023-07-05 18:35:00 +0200

    Update repo name after changing the package

--- backend/tests/unit/git/conftest.py
+++ backend/tests/unit/git/conftest.py
@@ -92,7 +92,7 @@ def git_upstream_repo_03(git_upstream_repo_01) -> Dict[str, str]:
 def git_upstream_repo_10(helper, git_sources_dir) -> Dict[str, str]:
     """Git Repository used as part of the  demo-edge tutorial."""
 
-    name = "infrahub-demo-edge-develop"
+    name = "infrahub-demo-edge"
     fixtures_dir = helper.get_fixtures_dir()
     fixture_repo = os.path.join(fixtures_dir, "infrahub-demo-edge-8d18455.tar.gz")
 
