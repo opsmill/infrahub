05fd3ca56a43a1ed731947861faf1bc42fc2703e
pa-lem <pa-lem@hotmail.fr>
2023-07-05 10:48:22 +0200

    add test for write access and avatar initials

--- frontend/tests/utils.ts
+++ frontend/tests/utils.ts
@@ -14,3 +14,8 @@ export const ADMIN_CREDENTIALS = {
   username: "Chloe O'Brian",
   password: "Password123",
 };
+
+export const READ_ONLY_CREDENTIALS = {
+  username: "Jack Bauer",
+  password: "Password123",
+};
