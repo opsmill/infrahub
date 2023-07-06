51a70f54180933f13eaaee98d3195a274f313619
Damien Garros <dgarros@gmail.com>
2023-07-06 08:09:31 +0200

    Fix encoding

--- backend/infrahub/cli/doc.py
+++ backend/infrahub/cli/doc.py
@@ -25,13 +25,13 @@ def generate_schema():
         if not os.path.exists(template_file):
             raise typer.Exit(f"Unable to find the template file at {template_file}")
 
-        template_text = Path(template_file).read_text()
+        template_text = Path(template_file).read_text(encoding="utf-8")
 
         environment = jinja2.Environment()
         template = environment.from_string(template_text)
         rendered_file = template.render(schema=internal_schema)
 
-        with open(output_file, "w") as f:
+        with open(output_file, "w", encoding="utf-8") as f:
             f.write(rendered_file)
 
         print(f"Schema generated for {schema_name}")
