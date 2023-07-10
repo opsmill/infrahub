aa8ce52a7f59a82fa69693bda89ee184d13db014
pa-lem <pa-lem@hotmail.fr>
2023-07-10 09:59:30 +0200

    reset tuto 1 test

--- frontend/tests/e2e/tutorial/tutorial-1-branch-and-version-control.cy.ts
+++ frontend/tests/e2e/tutorial/tutorial-1-branch-and-version-control.cy.ts
@@ -35,18 +35,12 @@ describe("Tutorial - Part 1", () => {
     // Submit the form
     cy.get(".justify-center > .rounded-md").click();
 
-    // The popin should be closed
-    cy.get("Create a new branch").should("not.exist");
-
     // Verify if the new branch is selected
     cy.get(":nth-child(1) > :nth-child(1) > .border").should("have.text", NEW_BRANCH_NAME);
   });
 
   it("should update the Admin Account", function () {
     cy.visit(`/?branch=${NEW_BRANCH_NAME}`);
-
-    // Verify if the new branch is selected
-    cy.get(":nth-child(1) > :nth-child(1) > .border").should("have.text", NEW_BRANCH_NAME);
 
     // Select the Admin object in the menu
     cy.get(`[href='/objects/Account?branch=${NEW_BRANCH_NAME}'] > .group`).click();
@@ -58,10 +52,7 @@ describe("Tutorial - Part 1", () => {
       cy.screenshot("tutorial_1_accounts", screenshotConfig);
     }
 
-    // Select the admin account
     cy.contains(ADMIN_ACCOUNT_NAME).click();
-
-    // Verify that the view is correctly filled
     cy.get(".sm\\:divide-y > :nth-child(2) > div.flex > .mt-1").should(
       "have.text",
       ADMIN_ACCOUNT_NAME
@@ -70,9 +61,6 @@ describe("Tutorial - Part 1", () => {
     if (this.screenshots) {
       cy.screenshot("tutorial_1_account_details", screenshotConfig);
     }
-
-    // Verify if the new branch is selected
-    cy.get(":nth-child(1) > :nth-child(1) > .border").should("have.text", NEW_BRANCH_NAME);
 
     // Open the edit panel
     cy.get(".md\\:pl-64 > .flex-col > .bg-custom-white > :nth-child(2) > :nth-child(1)").click();
@@ -92,11 +80,8 @@ describe("Tutorial - Part 1", () => {
     // Submit the form
     cy.contains("Save").click();
 
-    // Loading button should not exist
-    cy.get(".mt-6 > .bg-custom-blue-700").should("not.exist");
-
     // The new label should be saved
-    cy.get(":nth-child(3) > div.items-center > .mt-1").should("have.text", NEW_ADMIN_ACCOUNT_LABEL);
+    cy.get(":nth-child(3) > .relative > .block").should("have.value", NEW_ADMIN_ACCOUNT_LABEL);
   });
 
   it("should access the Admin Account diff", function () {
