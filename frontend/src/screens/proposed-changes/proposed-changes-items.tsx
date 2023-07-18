06aefcf2343237f338f821a4d84c961c2676bd5d
pa-lem <pa-lem@hotmail.fr>
2023-07-18 21:25:35 +0200

    add create proposed changes

--- frontend/src/screens/proposed-changes/proposed-changes-items.tsx
+++ frontend/src/screens/proposed-changes/proposed-changes-items.tsx
@@ -1,19 +1,25 @@
-import { gql } from "@apollo/client";
-import { ChevronLeftIcon } from "@heroicons/react/24/outline";
+import { gql, useReactiveVar } from "@apollo/client";
+import { ChevronLeftIcon, PlusIcon, Square3Stack3DIcon } from "@heroicons/react/24/outline";
 import { useAtom } from "jotai";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AVATAR_SIZE, Avatar } from "../../components/avatar";
 import { Badge } from "../../components/badge";
 import { DateDisplay } from "../../components/date-display";
+import { RoundedButton } from "../../components/rounded-button";
+import SlideOver from "../../components/slide-over";
 import { Tooltip } from "../../components/tooltip";
-import { PROPOSED_CHANGES_OBJECT } from "../../config/constants";
+import { DEFAULT_BRANCH_NAME, PROPOSED_CHANGES_OBJECT } from "../../config/constants";
+import { AuthContext } from "../../decorators/withAuth";
 import { getProposedChanges } from "../../graphql/queries/proposed-changes/getProposedChanges";
+import { branchVar } from "../../graphql/variables/branchVar";
 import useQuery from "../../hooks/useQuery";
 import { schemaState } from "../../state/atoms/schema.atom";
 import { constructPath } from "../../utils/fetch";
 import { getSchemaRelationshipColumns } from "../../utils/getSchemaObjectColumns";
 import ErrorScreen from "../error-screen/error-screen";
 import LoadingScreen from "../loading-screen/loading-screen";
+import ObjectItemCreate from "../object-item-create/object-item-create-paginated";
 
 const ProposedChange = (props: any) => {
   const { row } = props;
@@ -87,6 +93,12 @@ const ProposedChange = (props: any) => {
 export const ProposedChanges = () => {
   const [schemaList] = useAtom(schemaState);
 
+  const auth = useContext(AuthContext);
+
+  const branch = useReactiveVar(branchVar);
+
+  const [showCreateDrawer, setShowCreateDrawer] = useState(false);
+
   const schemaData = schemaList.filter((s) => s.name === PROPOSED_CHANGES_OBJECT)[0];
 
   const queryString = schemaData
@@ -103,7 +115,7 @@ export const ProposedChanges = () => {
     ${queryString}
   `;
 
-  const { loading, error, data = {} } = useQuery(query, { skip: !schemaData });
+  const { loading, error, data = {}, refetch } = useQuery(query, { skip: !schemaData });
 
   const result = data ? data[schemaData?.kind] ?? {} : {};
 
@@ -132,6 +144,12 @@ export const ProposedChanges = () => {
             </p>
           </div>
         )}
+
+        <RoundedButton
+          disabled={!auth?.permissions?.write}
+          onClick={() => setShowCreateDrawer(true)}>
+          <PlusIcon className="h-5 w-5" aria-hidden="true" />
+        </RoundedButton>
       </div>
 
       <ul className="grid gap-6 grid-cols-1 p-6">
@@ -139,6 +157,40 @@ export const ProposedChanges = () => {
           <ProposedChange key={index} row={row} />
         ))}
       </ul>
+
+      <SlideOver
+        title={
+          <div className="space-y-2">
+            <div className="flex items-center w-full">
+              <span className="text-lg font-semibold mr-3">Create {PROPOSED_CHANGES_OBJECT}</span>
+              <div className="flex-1"></div>
+              <div className="flex items-center">
+                <Square3Stack3DIcon className="w-5 h-5" />
+                <div className="ml-1.5 pb-1">{branch?.name ?? DEFAULT_BRANCH_NAME}</div>
+              </div>
+            </div>
+            <span className="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
+              <svg
+                className="h-1.5 w-1.5 mr-1 fill-yellow-500"
+                viewBox="0 0 6 6"
+                aria-hidden="true">
+                <circle cx={3} cy={3} r={3} />
+              </svg>
+              {schemaData?.kind}
+            </span>
+          </div>
+        }
+        open={showCreateDrawer}
+        setOpen={setShowCreateDrawer}
+        // title={`Create ${objectname}`}
+      >
+        <ObjectItemCreate
+          onCreate={() => setShowCreateDrawer(false)}
+          onCancel={() => setShowCreateDrawer(false)}
+          objectname={PROPOSED_CHANGES_OBJECT!}
+          refetch={refetch}
+        />
+      </SlideOver>
     </div>
   );
 };
