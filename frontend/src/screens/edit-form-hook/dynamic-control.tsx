64648be897aeae694e1c853cea7c51a55f8b88df
pa-lem <pa-lem@hotmail.fr>
2023-07-31 12:34:16 +0200

    add code editor

--- frontend/src/screens/edit-form-hook/dynamic-control.tsx
+++ frontend/src/screens/edit-form-hook/dynamic-control.tsx
@@ -1,5 +1,6 @@
 import { useFormContext } from "react-hook-form";
 import { OpsCheckboxRegister } from "../../components-form/checkbox.register";
+import { CodeEditorRegister } from "../../components-form/code-editor.register";
 import { OpsDatePickerRegister } from "../../components-form/date-picker.register";
 import { OpsInputRegister } from "../../components-form/input.register";
 import { OpsMultiSelectRegister } from "../../components-form/multi-select.register";
@@ -7,7 +8,7 @@ import { OpsSelect2StepRegister } from "../../components-form/select-2-step.regi
 import { OpsSelectRegister } from "../../components-form/select.register";
 import { OpsSwitchRegister } from "../../components-form/switch.register";
 import { OpsTextareaRegister } from "../../components-form/textarea.register";
-import { ControlType, DynamicFieldData } from "./dynamic-control-types";
+import { DynamicFieldData } from "./dynamic-control-types";
 
 export const DynamicControl = (props: DynamicFieldData) => {
   const {
@@ -20,112 +21,116 @@ export const DynamicControl = (props: DynamicFieldData) => {
     },
     // config = {},
   } = props;
+
   const { register, setValue, getValues } = useFormContext();
 
-  const getFormFieldByInputType = (inputType: ControlType) => {
-    const existingValue = getValues(name);
+  const existingValue = getValues(name);
 
-    switch (inputType) {
-      case "password":
-      case "text":
-      case "number":
-        return (
-          <OpsInputRegister
-            {...props}
-            inputType={inputType}
-            register={register}
-            setValue={setValue}
-            value={existingValue ?? value}
-          />
-        );
-      case "textarea":
-        return (
-          <OpsTextareaRegister
-            {...props}
-            register={register}
-            setValue={setValue}
-            value={existingValue ?? value}
-          />
-        );
-      case "switch":
-        return (
-          <OpsSwitchRegister
-            {...props}
-            register={register}
-            setValue={setValue}
-            value={existingValue ?? value}
-          />
-        );
-      case "checkbox":
-        return (
-          <OpsCheckboxRegister
-            {...props}
-            register={register}
-            setValue={setValue}
-            value={existingValue ?? value}
-          />
-        );
-      case "select": {
-        // if (kind && ["Integer", "Number", "Bandwidth"].indexOf(kind) > -1) {
-        //   config.valueAsNumber = true;
-        // }
-        return (
-          <OpsSelectRegister
-            {...props}
-            options={options.values}
-            register={register}
-            setValue={setValue}
-            value={existingValue ?? value}
-          />
-        );
-      }
-      case "select2step": {
-        const selectOptions = options.values.map((o) => ({
-          name: o.name,
-          id: o.id,
-        }));
-        const regex = /^Related/; // starts with Related
-
-        const selectValue = {
-          parent: value?.__typename?.replace(regex, ""),
-          child: value?.id,
-        };
-
-        return (
-          <OpsSelect2StepRegister
-            {...props}
-            options={selectOptions}
-            value={selectValue}
-            register={register}
-            setValue={setValue}
-          />
-        );
-      }
-      case "multiselect": {
-        const currentValue = options.values.filter(
-          (option) => (value || []).indexOf(option.id) > -1
-        );
-
-        return (
-          <OpsMultiSelectRegister
-            {...props}
-            options={options.values}
-            value={existingValue ?? currentValue}
-            register={register}
-            setValue={setValue}
-          />
-        );
-        // If user does not change anything in the multi-select dropdown, the field value in the form data remains undefined
-        // Not a problem currently, as if nothing get's changed, we are not sending out that field in the update mutation
-        // but something that we can look into later on
-      }
-      case "datepicker": {
-        return <OpsDatePickerRegister {...props} register={register} setValue={setValue} />;
-      }
-      default:
-        return null;
+  switch (type) {
+    case "password":
+    case "text":
+    case "number":
+      return (
+        <OpsInputRegister
+          {...props}
+          inputType={type}
+          register={register}
+          setValue={setValue}
+          value={existingValue ?? value}
+        />
+      );
+    case "textarea":
+      return (
+        <OpsTextareaRegister
+          {...props}
+          register={register}
+          setValue={setValue}
+          value={existingValue ?? value}
+        />
+      );
+    case "switch":
+      return (
+        <OpsSwitchRegister
+          {...props}
+          register={register}
+          setValue={setValue}
+          value={existingValue ?? value}
+        />
+      );
+    case "checkbox":
+      return (
+        <OpsCheckboxRegister
+          {...props}
+          register={register}
+          setValue={setValue}
+          value={existingValue ?? value}
+        />
+      );
+    case "select": {
+      // if (kind && ["Integer", "Number", "Bandwidth"].indexOf(kind) > -1) {
+      //   config.valueAsNumber = true;
+      // }
+      return (
+        <OpsSelectRegister
+          {...props}
+          options={options.values}
+          register={register}
+          setValue={setValue}
+          value={existingValue ?? value}
+        />
+      );
     }
-  };
+    case "select2step": {
+      const selectOptions = options.values.map((o) => ({
+        name: o.name,
+        id: o.id,
+      }));
+      const regex = /^Related/; // starts with Related
 
-  return getFormFieldByInputType(type);
+      const selectValue = {
+        parent: value?.__typename?.replace(regex, ""),
+        child: value?.id,
+      };
+
+      return (
+        <OpsSelect2StepRegister
+          {...props}
+          options={selectOptions}
+          value={selectValue}
+          register={register}
+          setValue={setValue}
+        />
+      );
+    }
+    case "multiselect": {
+      const currentValue = options.values.filter((option) => (value || []).indexOf(option.id) > -1);
+
+      return (
+        <OpsMultiSelectRegister
+          {...props}
+          options={options.values}
+          value={existingValue ?? currentValue}
+          register={register}
+          setValue={setValue}
+        />
+      );
+      // If user does not change anything in the multi-select dropdown, the field value in the form data remains undefined
+      // Not a problem currently, as if nothing get's changed, we are not sending out that field in the update mutation
+      // but something that we can look into later on
+    }
+    case "datepicker": {
+      return <OpsDatePickerRegister {...props} register={register} setValue={setValue} />;
+    }
+    case "json": {
+      const objectValue = existingValue ?? value;
+
+      const jsonValue = typeof objectValue === "string" ? objectValue : JSON.stringify(objectValue);
+
+      return (
+        <CodeEditorRegister {...props} value={jsonValue} register={register} setValue={setValue} />
+      );
+    }
+    default:
+      return null;
+  }
 };
