0dd1c34297579e1bdda3bf640be4ad402fe2779c
Damien Garros <dgarros@gmail.com>
2023-08-22 14:25:54 +0200

    Refactor UUIDT to use base16 and be UUID compliant

--- python_sdk/infrahub_client/uuidt.py
+++ python_sdk/infrahub_client/uuidt.py
@@ -4,42 +4,56 @@ import os
 import random
 import socket
 import time
-from typing import TYPE_CHECKING, Optional
+from typing import Optional
+from uuid import UUID
 
-from infrahub_client.utils import base36encode
+from infrahub_client.utils import base16encode
 
-if TYPE_CHECKING:
-    from typing_extensions import Self
-
-BASE = 36
+BASE = 16
 DIVISOR = BASE - 1
 CHARACTERS = list("0123456789abcdefghijklmnopqrstuvwxyz")[:BASE]
 
 # Code inspired from https://github.com/isaacharrisholt/uuidt
 
 
+def encode_number(number: int, min_length: int) -> str:
+    """Encore a number into a base16 string and ensure the result has a minimum size.
+    If the initial response produced doesn't match the min requirement,
+    random number will be used to fill the ga
+    """
+    response = base16encode(number=number).lower()
+    if len(response) >= min_length:
+        return response
+    return response + "".join(random.choices(CHARACTERS, k=min_length - len(response)))
+
+
 class UUIDT:
-    def __init__(self, namespace: str, timestamp: int, hostname: str, random_chars: str):
-        self.namespace = namespace
-        self.timestamp = timestamp
-        self.hostname = hostname
-        self.random_chars = random_chars
+    def __init__(
+        self,
+        namespace: Optional[str] = None,
+        timestamp: Optional[int] = None,
+        hostname: Optional[str] = None,
+        random_chars: Optional[str] = None,
+    ):
+        self.namespace = namespace or os.path.abspath(os.path.dirname(__file__))
+        self.timestamp = timestamp or time.time_ns()
+        self.hostname = hostname or socket.gethostname()
+        self.random_chars = random_chars or "".join(random.choices(CHARACTERS, k=8))
 
     def __str__(self) -> str:
         hostname_enc = sum(self.hostname.encode("utf-8"))
         namespace_enc = sum(self.namespace.encode("utf-8"))
 
-        timestamp_str = base36encode(number=self.timestamp).lower()
-        hostname_str = base36encode(number=hostname_enc).lower()
-        namespace_str = base36encode(number=namespace_enc).lower()
+        timestamp_str = encode_number(number=self.timestamp, min_length=16)
+        hostname_str = encode_number(number=hostname_enc, min_length=4)
+        namespace_str = encode_number(number=namespace_enc, min_length=4)
 
-        return f"{timestamp_str[:8]}-{timestamp_str[8:]}-{hostname_str:0>4}-" f"{namespace_str:0>4}-{self.random_chars}"
+        return f"{timestamp_str[:8]}-{timestamp_str[8:12]}-{timestamp_str[-4:]}-{hostname_str[:4]}-{namespace_str[:4]}{self.random_chars[:8]}"
+
+    def short(self) -> str:
+        """Return the last 8 digit of the UUID (the most random part)"""
+        return self.__str__()[-8:]
 
     @classmethod
-    def new(cls, namespace: Optional[str] = None) -> Self:
-        namespace = namespace or os.path.abspath(os.path.dirname(__file__))
-        timestamp = time.time_ns()
-        hostname = socket.gethostname()
-        random_chars = "".join(random.choices(CHARACTERS, k=4))
-
-        return cls(namespace, timestamp, hostname, random_chars)
+    def new(cls, namespace: Optional[str] = None) -> UUID:
+        return UUID(str(cls(namespace=namespace)))
