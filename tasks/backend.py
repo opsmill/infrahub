e17650707f09fe5610b4136b6022d42bc2f11e0c
Damien Garros <dgarros@gmail.com>
2023-08-23 08:51:10 +0200

    Use execute_command in demo.test-unit

--- tasks/backend.py
+++ tasks/backend.py
@@ -189,8 +189,7 @@ def test_unit(context: Context):
         base_cmd = f"{get_env_vars(context)} docker compose {compose_files_cmd} -p {BUILD_NAME} run infrahub-test"
         exec_cmd = f"pytest -n {NBR_WORKERS} -v --cov=infrahub {MAIN_DIRECTORY}/tests/unit"
         print(exec_cmd)
-
-        return context.run(f"{base_cmd} {exec_cmd}")
+        return execute_command(context=context, command=f"{base_cmd} {exec_cmd}")
 
 
 @task(optional=["database"])
@@ -201,7 +200,6 @@ def test_core(context: Context, database: str = "memgraph"):
         exec_cmd = f"pytest -n {NBR_WORKERS} -v {MAIN_DIRECTORY}/tests/unit/core"
         if database == "neo4j":
             exec_cmd += " --neo4j"
-
         print(exec_cmd)
         return execute_command(context=context, command=f"{base_cmd} {exec_cmd}")
 
@@ -214,7 +212,6 @@ def test_integration(context: Context, database: str = "memgraph"):
         exec_cmd = f"infrahub-test pytest -n {NBR_WORKERS} -v --cov=infrahub {MAIN_DIRECTORY}/tests/integration"
         if database == "neo4j":
             exec_cmd += " --neo4j"
-
         return execute_command(context=context, command=f"{base_cmd} {exec_cmd}")
 
 
