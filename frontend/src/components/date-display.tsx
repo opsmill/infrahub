5453ba20c37b055d84e81fe1504675c10bc00957
pa-lem <pa-lem@hotmail.fr>
2023-08-28 17:17:33 +0200

    :wrench:

--- frontend/src/components/date-display.tsx
+++ frontend/src/components/date-display.tsx
@@ -17,7 +17,7 @@ export const DateDisplay = (props: DateDisplayProps) => {
   }
 
   return (
-    <span className="flex items-center">
+    <span className="flex items-center flex-wrap">
       <Pill className="text-sm font-normal">{getDateDisplay(date)}</Pill>
 
       <i className="text-xs font-normal ml-2">
