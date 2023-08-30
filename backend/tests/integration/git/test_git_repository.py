d32fa6a99f67425ed78c745bbcbf463652a0f827
Damien Garros <dgarros@gmail.com>
2023-08-30 13:40:36 +0200

    Update git integration tests to load infra schema

--- backend/tests/integration/git/test_git_repository.py
+++ backend/tests/integration/git/test_git_repository.py
@@ -1,13 +1,38 @@
+import os
+from pathlib import Path
+
 import pytest
+import yaml
 from fastapi.testclient import TestClient
 
-from infrahub.core.initialization import first_time_initialization
+import infrahub.config as config
+from infrahub.core import registry
+from infrahub.core.initialization import first_time_initialization, initialization
 from infrahub.core.node import Node
+from infrahub.core.schema import SchemaRoot
 from infrahub.core.utils import count_relationships, delete_all_nodes
 from infrahub.git import InfrahubRepository
+from infrahub.utils import get_models_dir
 from infrahub_client import Config, InfrahubClient, NodeNotFound
 
 # pylint: disable=unused-argument
+
+
+async def load_infrastructure_schema(session):
+    models_dir = get_models_dir()
+
+    schema_txt = Path(os.path.join(models_dir, "infrastructure_base.yml")).read_text()
+    infra_schema = yaml.safe_load(schema_txt)
+
+    default_branch_name = config.SETTINGS.main.default_branch
+    branch_schema = registry.schema.get_schema_branch(name=default_branch_name)
+    tmp_schema = branch_schema.duplicate()
+    tmp_schema.load_schema(schema=SchemaRoot(**infra_schema))
+    tmp_schema.process()
+
+    await registry.schema.update_schema_branch(
+        schema=tmp_schema, session=session, branch=default_branch_name, update_db=True
+    )
 
 
 class TestInfrahubClient:
@@ -15,6 +40,8 @@ class TestInfrahubClient:
     async def base_dataset(self, session):
         await delete_all_nodes(session=session)
         await first_time_initialization(session=session)
+        await load_infrastructure_schema(session=session)
+        await initialization(session=session)
 
     @pytest.fixture(scope="class")
     async def test_client(
