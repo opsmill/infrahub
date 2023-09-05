5b307d63b09df29e31cb536d86df987d80126615
Patrick Ogenstad <patrick@ogenstad.com>
2023-09-05 10:21:15 +0200

    Fix functions for sync examples
    Noticed that some of the functions in the sync examples were incorrectly
    declared as async functions.

--- python_sdk/examples/branch_rebase_sync.py
+++ python_sdk/examples/branch_rebase_sync.py
@@ -1,9 +1,10 @@
 from infrahub_client import InfrahubClientSync
 
 
-async def main():
+def main():
     client = InfrahubClientSync.init(address="http://localhost:8000")
     client.branch.rebase(branch_name="new-branch")
+
 
 if __name__ == "__main__":
     main()
