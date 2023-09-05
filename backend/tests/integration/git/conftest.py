7e1967933b69502f84ed25a0e551c4ca8cefa8f7
Damien Garros <dgarros@gmail.com>
2023-09-05 15:53:23 +0200

    Update infrahub-demo-edge archive

--- backend/tests/integration/git/conftest.py
+++ backend/tests/integration/git/conftest.py
@@ -33,7 +33,7 @@ def git_upstream_repo_10(helper, git_sources_dir) -> Dict[str, str]:
 
     name = "infrahub-demo-edge"
     fixtures_dir = helper.get_fixtures_dir()
-    fixture_repo = os.path.join(fixtures_dir, "infrahub-demo-edge-ef0bb75.tar.gz")
+    fixture_repo = os.path.join(fixtures_dir, "infrahub-demo-edge-b635811.tar.gz")
 
     # Extract the fixture package in the source directory
     file = tarfile.open(fixture_repo)
