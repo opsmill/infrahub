59dd833ef48bc4070fa74a917b1896392848420d
pa-lem <pa-lem@hotmail.fr>
2023-09-07 14:12:10 +0200

    fix tooltip transition

--- frontend/src/components/tooltip.tsx
+++ frontend/src/components/tooltip.tsx
@@ -8,12 +8,12 @@ export const Tooltip = (props: TooltipProps) => {
     <div className={"group/tooltip relative flex cursor-pointer"}>
       {props.children}
       <span
-        className={`absolute hidden z-10 top-8 left-1/2 transform -translate-x-1/2
-          transition delay-1000 duration-300
+        className={`absolute z-10 top-8 left-1/2 transform -translate-x-1/2
+          transition-all delay-1000 duration-300
           px-3 py-2
           text-sm font-medium text-white text-center
           bg-gray-600 rounded-lg shadow-sm opacity-0
-          group-hover/tooltip:opacity-100 group-hover/tooltip:inline-block`}>
+          group-hover/tooltip:opacity-100`}>
         {props.message}
       </span>
     </div>
