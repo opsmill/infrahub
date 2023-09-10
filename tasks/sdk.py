c89ceb9676037b35977afe3497286488147d06dd
Damien Garros <dgarros@gmail.com>
2023-09-10 19:09:23 +0200

    Disable xdist temporarily for SDK for testing

--- tasks/sdk.py
+++ tasks/sdk.py
@@ -168,7 +168,8 @@ def test_unit(context: Context):
     with context.cd(REPO_BASE):
         compose_files_cmd = build_test_compose_files_cmd(database=False)
         base_cmd = f"{get_env_vars(context)} docker compose {compose_files_cmd} -p {BUILD_NAME} run {build_test_envs()} infrahub-test"
-        exec_cmd = f"pytest -n {NBR_WORKERS} -v --cov=infrahub_client {MAIN_DIRECTORY}/tests/unit"
+        # exec_cmd = f"pytest -n {NBR_WORKERS} -v --cov=infrahub_client {MAIN_DIRECTORY}/tests/unit"
+        exec_cmd = f"pytest -v --cov=infrahub_client {MAIN_DIRECTORY}/tests/unit"
         print(f"{base_cmd} {exec_cmd}")
         return execute_command(context=context, command=f"{base_cmd} {exec_cmd}")
 
@@ -178,7 +179,8 @@ def test_integration(context: Context, database: str = "memgraph"):
     with context.cd(REPO_BASE):
         compose_files_cmd = build_test_compose_files_cmd(database=database)
         base_cmd = f"{get_env_vars(context)} docker compose {compose_files_cmd} -p {BUILD_NAME} run {build_test_envs()} infrahub-test"
-        exec_cmd = f"pytest -n {NBR_WORKERS} -v --cov=infrahub_client {MAIN_DIRECTORY}/tests/integration"
+        # exec_cmd = f"pytest -n {NBR_WORKERS} -v --cov=infrahub_client {MAIN_DIRECTORY}/tests/integration"
+        exec_cmd = f"pytest -v --cov=infrahub_client {MAIN_DIRECTORY}/tests/integration"
         print(f"{base_cmd} {exec_cmd}")
         return execute_command(context=context, command=f"{base_cmd} {exec_cmd}")
 
