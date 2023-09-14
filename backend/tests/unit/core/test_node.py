3e92d5b547c9f191194bd0267751857b7ddaa3a2
Benoit Kohler <bkohler@bearchitek.com>
2023-09-14 17:47:38 +0200

    other typo + add ipv6 test for ip_interface

--- backend/tests/unit/core/test_node.py
+++ backend/tests/unit/core/test_node.py
@@ -1085,15 +1085,22 @@ async def test_node_serialize_prefix(session, default_branch: Branch, prefix_sch
     assert retrieve_p2.prefix.value == "192.0.2.1/32"
 
     p3 = await Node.init(session=session, schema=prefix)
-    await p3.new(session=session, prefix="2001:db8::/32", name="prefix3")
+    await p3.new(session=session, prefix="2001:db8::/128", name="prefix3")
     await p3.save(session=session)
 
     retrieve_p3 = await NodeManager.get_one(id=p3.id, session=session)
-    assert retrieve_p3.prefix.value == "2001:db8::/32"
+    assert retrieve_p3.prefix.value == "2001:db8::/128"
 
     i1 = await Node.init(session=session, schema=ip)
-    await i1.new(session=session, prefix="192.0.2.1", name="ip1")
+    await i1.new(session=session, address="192.0.2.1", name="ip1")
     await i1.save(session=session)
 
     retrieve_i1 = await NodeManager.get_one(id=i1.id, session=session)
     assert retrieve_i1.prefix.value == "192.0.2.1/32"
+
+    i2 = await Node.init(session=session, schema=ip)
+    await i2.new(session=session, address="2001:db8::", name="ip1")
+    await i2.save(session=session)
+
+    retrieve_i2 = await NodeManager.get_one(id=i1.id, session=session)
+    assert retrieve_i2.prefix.value == "2001:db8::/128"
