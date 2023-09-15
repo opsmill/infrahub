c92811afd08fedcb69b3843dcbca89bb120be2d1
John Partee <morganpartee@gmail.com>
2023-09-15 11:10:30 -0500

    remove extra tests

--- backend/tests/unit/api/test_auth_cookies.py
+++ backend/tests/unit/api/test_auth_cookies.py
@@ -2,8 +2,6 @@ import jwt
 
 from infrahub import config
 
-from .test_auth import EXPIRED_ACCESS_TOKEN, EXPIRED_REFRESH_TOKEN
-
 
 async def test_password_based_login(session, default_branch, client, first_account):
     with client:
@@ -18,29 +16,6 @@ async def test_password_based_login(session, default_branch, client, first_accou
     access_token = response.cookies["access_token"]
     decoded = jwt.decode(access_token, key=config.SETTINGS.security.secret_key, algorithms=["HS256"])
     assert first_account.id == decoded["sub"]
-
-
-async def test_refresh_with_invalidated_token(session, default_branch, client, first_account):
-    with client:
-        response = client.post("/api/auth/login", json={"username": "First Account", "password": "FirstPassword123"})
-
-    assert response.status_code == 200
-
-    with client:
-        logout_response = client.post("/api/auth/logout", cookies=response.cookies)
-
-    assert logout_response.status_code == 200
-
-    with client:
-        refresh_response = client.post("/api/auth/refresh", cookies=response.cookies)
-
-    assert refresh_response.status_code == 401
-    assert refresh_response.json() == {
-        "data": None,
-        "errors": [
-            {"message": "The provided refresh token has been invalidated in the database", "extensions": {"code": 401}}
-        ],
-    }
 
 
 async def test_refresh_access_token(session, default_branch, client, first_account):
@@ -65,46 +40,6 @@ async def test_refresh_access_token(session, default_branch, client, first_accou
     assert first_account.id == decoded_refresh["sub"]
     assert decoded_access["session_id"]
     assert decoded_access["session_id"] == decoded_refresh["session_id"]
-
-
-async def test_fail_to_refresh_access_token_with_access_token(session, default_branch, client, first_account):
-    """Validate that it's not possible to refresh an access token using an access token"""
-    with client:
-        login_response = client.post(
-            "/api/auth/login", json={"username": "First Account", "password": "FirstPassword123"}
-        )
-
-    assert login_response.status_code == 200
-
-    client.cookies["refresh_token"] = client.cookies["access_token"]
-
-    with client:
-        refresh_response = client.post("/api/auth/refresh")
-
-    assert refresh_response.status_code == 401
-    assert refresh_response.json() == {
-        "data": None,
-        "errors": [{"message": "Invalid token, current token is not a refresh token", "extensions": {"code": 401}}],
-    }
-
-
-async def test_use_expired_token(session, default_branch, client):
-    client.cookies["access_token"] = EXPIRED_ACCESS_TOKEN
-
-    with client:
-        response = client.get("/api/rfile/testing")
-
-    assert response.status_code == 401
-    assert response.json() == {"data": None, "errors": [{"message": "Expired Signature", "extensions": {"code": 401}}]}
-
-
-async def test_refresh_access_token_with_expired_refresh_token(session, default_branch, client):
-    """Validate that the correct error is returned for an expired refresh token"""
-    with client:
-        response = client.post("/api/auth/refresh", headers={"Authorization": f"Bearer {EXPIRED_REFRESH_TOKEN}"})
-
-    assert response.status_code == 401
-    assert response.json() == {"data": None, "errors": [{"message": "Expired Signature", "extensions": {"code": 401}}]}
 
 
 async def test_access_resource_using_refresh_token(session, default_branch, client, first_account):
@@ -159,16 +94,4 @@ async def test_generate_api_token(session, default_branch, client, create_test_a
         )
 
     assert jwt_response.status_code == 200
-    api_token = jwt_response.json()["data"]["CoreAccountTokenCreate"]["object"]["token"]["value"]
-
-    # Validate that the generated API token can't be used to generate another API token
-    with client:
-        api_response = client.post(
-            "/graphql",
-            json={"query": token_mutation},
-            headers={"X-INFRAHUB-KEY": api_token},
-        )
-
-    assert api_response.status_code == 200
-    assert not api_response.json()["data"]["CoreAccountTokenCreate"]
-    assert api_response.json()["errors"][0]["message"] == api_response.json()["errors"][0]["message"]
+    assert jwt_response.json()["data"]["CoreAccountTokenCreate"]["object"]["token"]["value"]
