05ebc405a4df027912d819d50a8029958e0119c1
wvandeun <7521270+wvandeun@users.noreply.github.com>
2023-09-20 00:17:23 +0200

    adds comments to nornir_inventory example

--- nornir_plugin/examples/nornir_inventory.py
+++ nornir_plugin/examples/nornir_inventory.py
@@ -7,9 +7,13 @@ def main():
             "options": {
                 "address": "http://localhost:8000",
                 "token": "06438eb2-8019-4776-878c-0941b1f1d1ec",
+                # defines the Infrahub Node kind that will mapped to Nornir Hosts
                 "host_node": {
                     "kind": "InfraDevice"
                 },
+                # maps a Nornir Host property to an InfraNode attributes or relation
+                # name is the name of the Nornir Host property we want to map
+                # mapping is the attribute or relation of the Node from which we have to extract the value
                 "schema_mappings": [
                     {
                         "name": "hostname",
@@ -20,12 +24,17 @@ def main():
                         "mapping": "platform.nornir_platform",
                     },
                 ],
+                # create Nornir groups from InfraNode attributesor relations
+                # groups is created as attribute__value `site__jfk1`
                 "group_mappings": [
                     "site.name"
-                ]
+                ],
+                "group_file": "dummy.yml"
             },
         }
     )
+    print(nr.inventory.hosts.keys())
+    print(nr.inventory.groups.keys())
     return 0
 
 
