03f2bd6091fe03151e1041b2d9e49e3b988a2604
pa-lem <pa-lem@hotmail.fr>
2023-09-20 09:11:18 +0200

    fix proposed changes creation

--- frontend/src/screens/object-item-create/object-item-create-paginated.tsx
+++ frontend/src/screens/object-item-create/object-item-create-paginated.tsx
@@ -1,5 +1,6 @@
 import { gql, useReactiveVar } from "@apollo/client";
 import { useAtom } from "jotai";
+import * as R from "ramda";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import { ALERT_TYPES, Alert } from "../../components/alert";
@@ -30,7 +31,6 @@ interface iProps {
 }
 
 export default function ObjectItemCreate(props: iProps) {
-  console.log("RENDER CREATE");
   const { objectname, onCreate, onCancel, refetch, formStructure, customObject = {} } = props;
 
   const [schemaList] = useAtom(schemaState);
@@ -42,7 +42,7 @@ export default function ObjectItemCreate(props: iProps) {
 
   const schema = schemaList.filter((s) => s.name === objectname)[0];
 
-  const peers = (schema.relationships || []).map((r) => r.peer).filter(Boolean);
+  const peers = R.uniq((schema.relationships || []).map((r) => r.peer).filter(Boolean));
 
   const queryString = peers.length
     ? getDropdownOptionsForRelatedPeersPaginated({
@@ -94,14 +94,13 @@ export default function ObjectItemCreate(props: iProps) {
 
   async function onSubmit(data: any) {
     setIsLoading(true);
-
-    const newObject = getMutationDetailsFromFormData(schema, data, "create");
-
-    if (!Object.keys(newObject).length) {
-      return;
-    }
-
     try {
+      const newObject = getMutationDetailsFromFormData(schema, data, "create");
+
+      if (!Object.keys(newObject).length) {
+        return;
+      }
+
       const mutationString = createObject({
         kind: schema.kind,
         data: stringifyWithoutQuotes({ ...newObject, ...customObject }),
@@ -127,12 +126,7 @@ export default function ObjectItemCreate(props: iProps) {
         onCreate(result?.data?.[`${schema.kind}Create`]);
       }
 
-      console.log("DONE");
-
-      // setTimeout(() => {
-      console.log("REFETCH");
       if (refetch) refetch();
-      // }, 3000);
 
       setIsLoading(false);
     } catch (error: any) {
@@ -145,6 +139,7 @@ export default function ObjectItemCreate(props: iProps) {
           details={error.message}
         />
       );
+
       setIsLoading(false);
     }
   }
