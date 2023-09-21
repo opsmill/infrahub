a31653993fee548be49507ed2b90524f6d9d1433
Patrick Ogenstad <patrick@ogenstad.com>
2023-09-21 12:22:35 +0200

    Address feedback

--- backend/infrahub/message_bus/messages/git_file_get.py
+++ backend/infrahub/message_bus/messages/git_file_get.py
@@ -6,7 +6,7 @@ from infrahub.message_bus import InfrahubBaseMessage
 class GitFileGet(InfrahubBaseMessage):
     """Read a file from a Git repository."""
 
-    commit: str = Field(..., description="The commit id to use when rendering the template")
+    commit: str = Field(..., description="The commit id to use to access the file")
     file: str = Field(..., description="The path and filename within the repository")
     repository_id: str = Field(..., description="The unique ID of the Repository")
     repository_name: str = Field(..., description="The name of the repository")
