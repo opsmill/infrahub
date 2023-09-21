1d22e11e36263a7357d0f6ab1b7e63a185501c5a
pa-lem <pa-lem@hotmail.fr>
2023-09-21 13:10:20 +0200

    add account constants

---
+++ frontend/tests/mocks/e2e/accounts.ts
@@ -0,0 +1,9 @@
+export const MAIN_BRANCH_NAME = "main";
+export const NEW_BRANCH_NAME = "cr1234";
+export const ADMIN_ACCOUNT_NAME = "admin";
+export const ADMIN_ACCOUNT_LABEL = "Admin";
+export const NEW_ADMIN_ACCOUNT_LABEL = "Administrator";
+export const NEW_ACCOUNT = {
+  name: "New Account",
+  password: "test",
+};
