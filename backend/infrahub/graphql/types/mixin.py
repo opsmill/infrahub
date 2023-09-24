a3695e55d0ec47b768000eeb01ce95d29e15d894
Damien Garros <dgarros@gmail.com>
2023-09-24 18:27:25 +0200

    Add missing context manager for mixin

--- backend/infrahub/graphql/types/mixin.py
+++ backend/infrahub/graphql/types/mixin.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
-from typing import Any, Dict
+from typing import TYPE_CHECKING, Any, Dict
 
-import infrahub.config as config
 from infrahub.core.manager import NodeManager
+
+if TYPE_CHECKING:
+    from infrahub.core.branch import Branch
+    from infrahub.database import InfrahubDatabase
 
 
 class GetListMixin:
@@ -12,13 +15,11 @@ class GetListMixin:
     @classmethod
     async def get_list(cls, fields: dict, context: dict, **kwargs):
         at = context.get("infrahub_at")
-        branch = context.get("infrahub_branch")
         account = context.get("infrahub_account", None)
-        db = context.get("infrahub_database")
+        branch: Branch = context.get("infrahub_branch")
+        db: InfrahubDatabase = context.get("infrahub_database")
 
-        async with db.session(database=config.SETTINGS.database.database) as session:
-            context["infrahub_session"] = session
-
+        async with db.start_session() as db:
             filters = {key: value for key, value in kwargs.items() if ("__" in key and value) or key == "ids"}
 
             objs = await NodeManager.query(
@@ -43,39 +44,40 @@ class GetListMixin:
         at = context.get("infrahub_at")
         branch = context.get("infrahub_branch")
         account = context.get("infrahub_account", None)
-        db = context.get("infrahub_database")
+        db: InfrahubDatabase = context.get("infrahub_database")
 
-        response: Dict[str, Any] = {"edges": []}
-        offset = kwargs.pop("offset", None)
-        limit = kwargs.pop("limit", None)
-        filters = {key: value for key, value in kwargs.items() if ("__" in key and value) or key == "ids"}
-        if "count" in fields:
-            response["count"] = await NodeManager.count(
+        async with db.start_session() as db:
+            response: Dict[str, Any] = {"edges": []}
+            offset = kwargs.pop("offset", None)
+            limit = kwargs.pop("limit", None)
+            filters = {key: value for key, value in kwargs.items() if ("__" in key and value) or key == "ids"}
+            if "count" in fields:
+                response["count"] = await NodeManager.count(
+                    db=db,
+                    schema=cls._meta.schema,
+                    filters=filters,
+                    at=at,
+                    branch=branch,
+                )
+            edges = fields.get("edges", {})
+            node_fields = edges.get("node", {})
+
+            objs = await NodeManager.query(
                 db=db,
                 schema=cls._meta.schema,
-                filters=filters,
+                filters=filters or None,
+                fields=node_fields,
                 at=at,
                 branch=branch,
+                limit=limit,
+                offset=offset,
+                account=account,
+                include_source=True,
+                include_owner=True,
             )
-        edges = fields.get("edges", {})
-        node_fields = edges.get("node", {})
 
-        objs = await NodeManager.query(
-            db=db,
-            schema=cls._meta.schema,
-            filters=filters or None,
-            fields=node_fields,
-            at=at,
-            branch=branch,
-            limit=limit,
-            offset=offset,
-            account=account,
-            include_source=True,
-            include_owner=True,
-        )
+            if objs:
+                objects = [{"node": await obj.to_graphql(db=db, fields=node_fields)} for obj in objs]
+                response["edges"] = objects
 
-        if objs:
-            objects = [{"node": await obj.to_graphql(db=db, fields=node_fields)} for obj in objs]
-            response["edges"] = objects
-
-        return response
+            return response
