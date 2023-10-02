74419830db880bca55d0a9d2a764b0f25204f720
pa-lem <pa-lem@hotmail.fr>
2023-10-02 15:25:08 +0200

    disable retry for unauthenticated users

--- frontend/src/components/retry.tsx
+++ frontend/src/components/retry.tsx
@@ -1,4 +1,6 @@
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
+import { useContext } from "react";
+import { AuthContext } from "../decorators/withAuth";
 import { classNames } from "../utils/common";
 
 type tRetryProps = {
@@ -9,7 +11,13 @@ type tRetryProps = {
 export const Retry = (props: tRetryProps) => {
   const { isInProgress, onClick } = props;
 
+  const auth = useContext(AuthContext);
+
   const handleClick = (event: any) => {
+    if (!auth?.permissions?.write) {
+      return;
+    }
+
     if (isInProgress) {
       return;
     }
@@ -23,7 +31,8 @@ export const Retry = (props: tRetryProps) => {
     <div
       className={classNames(
         "ml-2 p-1 rounded-full",
-        isInProgress ? "cursor-not-allowed " : " cursor-pointer hover:bg-gray-200"
+        isInProgress ? "cursor-not-allowed " : "",
+        auth?.permissions?.write ? "cursor-pointer hover:bg-gray-200" : "cursor-not-allowed"
       )}
       onClick={handleClick}>
       <ArrowPathIcon className={classNames("h-4 w-4", isInProgress ? "text-gray-400" : "")} />
