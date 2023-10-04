14ce7e0174495fe3fb9c1520917f41942f484610
pa-lem <pa-lem@hotmail.fr>
2023-10-04 15:03:25 +0200

    fix test url and component route

--- frontend/tests/e2e/tutorial/tutorial-6-git-integration.cy.ts
+++ frontend/tests/e2e/tutorial/tutorial-6-git-integration.cy.ts
@@ -42,7 +42,7 @@ describe("Tutorial - Part 6", () => {
 
   it("should update the device and an interface", function () {
     // Access the devices
-    cy.get("[href^='/objects/Device'] > .group", { timeout: 10000 }).click();
+    cy.get("[href^='/objects/InfraDevice'] > .group", { timeout: 10000 }).click();
 
     // Access the device
     cy.contains(DEVICE).click();
