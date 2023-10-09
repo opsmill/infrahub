2c6eac868c0a4b3cb9cb3bd48dec856378433007
Benoit Kohler <BeArchiTek@users.noreply.github.com>
2023-10-09 15:12:28 +0000

    playing around

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-cd /workspace/toto
-pyenv install 3.11 --skip-existing
-pyenv local 3.11
-pip install toml invoke
-curl -sSL https://install.python-poetry.org | python3 -
-poetry config virtualenvs.create false
+cd /workspace/infrahub
+
+# Dynamic or project-specific setup
 poetry install
 poetry run invoke demo.build
