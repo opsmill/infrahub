71c28254e81cc5f38edeb9b575ead25d3ede66c6
Benoit Kohler <BeArchiTek@users.noreply.github.com>
2023-10-10 14:55:33 +0200

    Update postAttachCommand.sh

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-gh codespace ports visibility 8000:org > /dev/null 2>&1
+# Inspired by https://github.com/orgs/community/discussions/15351
+echo "gh codespace ports visibility 8000:org -c $CODESPACE_NAME" >> ~/.bashrc
