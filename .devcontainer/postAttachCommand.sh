2259a3c2d968de29d53a1aae613ae0062b2bad0b
Benoit Kohler <BeArchiTek@users.noreply.github.com>
2023-10-11 07:23:04 +0000

    change postAttachCommand a bit

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Inspired by https://github.com/orgs/community/discussions/15351
-echo "gh codespace ports visibility 8000:org -c $CODESPACE_NAME" >> ~/.bashrc
+gh codespace ports visibility 8000:org -c $CODESPACE_NAME
+echo "gh codespace ports -c $CODESPACE_NAME" >> ~/.bashrc
