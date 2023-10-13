d63c7206b0172d934ce578c76e418d41050db8f9
Patrick Ogenstad <patrick@ogenstad.com>
2023-10-13 13:54:17 +0200

    Work on cli type hints

--- backend/infrahub/cli/generate_schema.py
+++ backend/infrahub/cli/generate_schema.py
@@ -7,7 +7,7 @@ app = typer.Typer()
 
 
 @app.callback()
-def callback():
+def callback() -> None:
     """
     Generate some Schema files used by Infrahub
     """
@@ -16,7 +16,7 @@ def callback():
 @app.command(name="schema")
 def generate_schema(
     output_file: Path = typer.Argument("infrahub_schema.schema.json"),
-):
+) -> None:
     """Generate a the schema expected by infrahub for the schema `infrahubctl schema load`."""
 
     from infrahub.api.schema import (  # pylint: disable=import-outside-toplevel
