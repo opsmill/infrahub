bbb8291561d9bae028ea924794ef2a821705e22f
pa-lem <pa-lem@hotmail.fr>
2023-10-23 12:45:05 +0200

    add unit test for updated object and mutation

---
+++ frontend/tests/unit/utils/getMutationMetaDetailsFromFormData.test.ts
@@ -0,0 +1,37 @@
+import { describe, expect, it } from "vitest";
+import { updateObjectWithId } from "../../../src/graphql/mutations/objects/updateObjectWithId";
+import getMutationMetaDetailsFromFormData from "../../../src/utils/getMutationMetaDetailsFromFormData";
+import { stringifyWithoutQuotes } from "../../../src/utils/string";
+import {
+  deviceDetailsMocksData,
+  deviceDetailsMocksSchema,
+  mutationStringForMetaEdit,
+  newDataForMetaEdit,
+  updatedObjectForMetaEdit,
+} from "../../mocks/data/devices";
+
+const updatedObject = getMutationMetaDetailsFromFormData(
+  deviceDetailsMocksSchema[0],
+  newDataForMetaEdit,
+  deviceDetailsMocksData.InfraDevice.edges[0].node,
+  "relationship",
+  "site",
+  deviceDetailsMocksData.InfraDevice.edges[0].node.site.properties
+);
+
+describe("Mutation details from object data", () => {
+  it("should return a correct updated object structure", () => {
+    expect(JSON.stringify(updatedObject)).toStrictEqual(JSON.stringify(updatedObjectForMetaEdit));
+  });
+
+  it("should return a correct mutation from the updated object", () => {
+    const mutationString = updateObjectWithId({
+      kind: deviceDetailsMocksSchema[0].kind,
+      data: stringifyWithoutQuotes(updatedObject),
+    });
+
+    console.log("mutationString: ", mutationString);
+    console.log("mutationStringForMetaEdit: ", mutationStringForMetaEdit);
+    expect(mutationString).toEqual(mutationStringForMetaEdit);
+  });
+});
