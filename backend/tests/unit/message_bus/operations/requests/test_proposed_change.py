600cf7bccd60b9095ef5184ac35291736e662ad0
Patrick Ogenstad <patrick@ogenstad.com>
2023-10-24 10:24:01 +0200

    Update testdata

--- backend/tests/unit/message_bus/operations/requests/test_proposed_change.py
+++ backend/tests/unit/message_bus/operations/requests/test_proposed_change.py
@@ -18,18 +18,21 @@ async def test_repository_checks(helper):
     playback = JSONPlayback(directory=f"{TEST_DATA}/repository_checks_01")
     config = Config(address="http://infrahub-testing:8000", requester=playback.async_request)
     client = InfrahubClient(config=config)
+    proposed_change_id = "1790fa8f-dd4d-ed00-58dd-18835e51189a"
+    repository1_id = "1790fa5c-68b5-c478-58da-18835608efda"
+    repository2_id = "1790fa6d-1654-9068-58df-1883e684d3fd"
 
     bus_recorder = helper.get_message_bus_recorder()
     service = InfrahubServices(client=client, message_bus=bus_recorder)
-    message = messages.RequestProposedChangeRepositoryChecks(proposed_change="13a49493-b186-4f7e-a1bb-cd015ed0bdb0")
+    message = messages.RequestProposedChangeRepositoryChecks(proposed_change=proposed_change_id)
     await repository_checks(message=message, service=service)
     assert len(bus_recorder.messages) == 2
     assert ["request.repository.checks"] == bus_recorder.seen_routing_keys
     assert (
         messages.RequestRepositoryChecks(
             meta=Meta(request_id=""),
-            proposed_change="13a49493-b186-4f7e-a1bb-cd015ed0bdb0",
-            repository="b002c0e6-78e6-4a8b-812f-8aa41d89d386",
+            proposed_change=proposed_change_id,
+            repository=repository1_id,
             source_branch="test-pc-1",
             target_branch="main",
         )
@@ -38,8 +41,8 @@ async def test_repository_checks(helper):
     assert (
         messages.RequestRepositoryChecks(
             meta=Meta(request_id=""),
-            proposed_change="13a49493-b186-4f7e-a1bb-cd015ed0bdb0",
-            repository="0af9707a-9c53-450d-96db-d721bfd0350b",
+            proposed_change=proposed_change_id,
+            repository=repository2_id,
             source_branch="test-pc-1",
             target_branch="main",
         )
