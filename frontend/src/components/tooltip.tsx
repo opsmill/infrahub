88f4c1e31eaf439ea12883f065b75948b3eec7c7
pa-lem <pa-lem@hotmail.fr>
2023-10-26 14:54:04 +0200

    fix tooltip for diff view

--- frontend/src/components/tooltip.tsx
+++ frontend/src/components/tooltip.tsx
@@ -29,12 +29,13 @@ export const Tooltip = (props: TooltipProps) => {
       <span
         className={classNames(
           position ? getPositionClassName[position] : getPositionClassName[TooltipPosition.BOTTOM],
-          `whitespace-pre absolute z-10
-          transition-all delay-500 duration-100
+          `whitespace-pre absolute -z-10
           px-3 py-2 min-w-min
           text-sm font-medium text-white text-center
           bg-gray-600 rounded-lg shadow-sm opacity-0
-          group-hover/tooltip:opacity-100`
+          transition-opacity delay-500 duration-100
+          group-hover/tooltip:opacity-100
+          group-hover/tooltip:z-10`
         )}>
         {message}
       </span>
