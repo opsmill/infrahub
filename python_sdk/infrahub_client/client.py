9b21d1f247ade1b2b9a79be108f9981bfbf18ed3
Benoit Kohler <benoit@opsmill.com>
2023-10-26 12:30:41 +0200

    linter

--- python_sdk/infrahub_client/client.py
+++ python_sdk/infrahub_client/client.py
@@ -10,7 +10,11 @@ from typing import Any, Dict, List, MutableMapping, Optional, Union
 import httpx
 
 from infrahub_client.batch import InfrahubBatch
-from infrahub_client.branch import BranchData, InfrahubBranchManager, InfrahubBranchManagerSync
+from infrahub_client.branch import (
+    BranchData,
+    InfrahubBranchManager,
+    InfrahubBranchManagerSync,
+)
 from infrahub_client.config import Config
 from infrahub_client.data import RepositoryData
 from infrahub_client.exceptions import (
--- python_sdk/infrahub_client/data.py
+++ python_sdk/infrahub_client/data.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional
+from typing import Dict
 
 from pydantic import BaseModel
 
