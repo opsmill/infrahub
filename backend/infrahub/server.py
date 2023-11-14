aac546f4c2b3db4de0f8cc2fb84d59ef3ac3d183
Benoit Kohler <benoit@opsmill.com>
2023-11-14 13:34:20 +0100

    correct previous issue

--- backend/infrahub/server.py
+++ backend/infrahub/server.py
@@ -163,6 +163,7 @@ if os.path.exists(FRONTEND_ASSET_DIRECTORY) and os.path.isdir(FRONTEND_ASSET_DIR
     app.mount("/assets", StaticFiles(directory=FRONTEND_ASSET_DIRECTORY), "assets")
     app.mount("/favicons", StaticFiles(directory=FRONTEND_ASSET_DIRECTORY), "favicons")
 
+
 @app.get("/{rest_of_path:path}", include_in_schema=False)
 async def react_app(req: Request, rest_of_path: str):  # pylint: disable=unused-argument
     return templates.TemplateResponse("index.html", {"request": req})
