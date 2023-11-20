7bfd72c803ce30811e9426d22809eb90d4259f38
pa-lem <pa-lem@hotmail.fr>
2023-11-20 08:31:18 +0100

    remove videos for integration tests

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     },
     specPattern: "tests/integrations/**/*.cy.{js,jsx,ts,tsx}",
     reporter: "spec",
-    video: true,
+    video: false,
     viewportHeight: 720,
     viewportWidth: 1280,
   },
