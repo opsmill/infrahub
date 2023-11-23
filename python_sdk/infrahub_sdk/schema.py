68950fb65283f273aff22bedd0f73380fa69a5f5
Damien Garros <dgarros@gmail.com>
2023-11-23 09:13:07 +0100

    Increase schema loading timeout to 120sec

--- python_sdk/infrahub_sdk/schema.py
+++ python_sdk/infrahub_sdk/schema.py
@@ -377,7 +377,7 @@ class InfrahubSchema(InfrahubSchemaBase):
     async def load(self, schemas: List[dict], branch: Optional[str] = None) -> Tuple[bool, Optional[dict]]:
         branch = branch or self.client.default_branch
         url = f"{self.client.address}/api/schema/load?branch={branch}"
-        response = await self.client._post(url=url, timeout=60, payload={"schemas": schemas})
+        response = await self.client._post(url=url, timeout=90, payload={"schemas": schemas})
 
         if response.status_code == 202:
             return True, None
