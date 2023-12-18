4e78128b495f951330aef3200072c782c99c7c74
Bilal <bilal@opsmill.com>
2023-12-18 10:48:34 +0100

    Updated schema type to match open api

--- frontend/src/state/atoms/schema.atom.ts
+++ frontend/src/state/atoms/schema.atom.ts
@@ -1,10 +1,10 @@
 import { atom } from "jotai";
 import { components } from "../../infraops";
 
-export type iNodeSchema = components["schemas"]["NodeSchema"];
+export type iNodeSchema = components["schemas"]["APINodeSchema"];
 export const schemaState = atom<iNodeSchema[]>([]);
 
-export type iGenericSchema = components["schemas"]["GenericSchema"];
+export type iGenericSchema = components["schemas"]["APIGenericSchema"];
 export const genericsState = atom<iGenericSchema[]>([]);
 
 export const currentSchemaHashAtom = atom<string | null>(null);
