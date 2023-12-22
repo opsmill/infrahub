b395dca76b7573b30ddfbff325289000b8c0c2f0
Patrick Ogenstad <patrick@ogenstad.com>
2023-12-22 16:36:40 +0100

    Temporary disable dropdown idempotency test

--- backend/tests/unit/api/test_40_schema_api.py
+++ backend/tests/unit/api/test_40_schema_api.py
@@ -229,7 +229,7 @@ async def test_schema_load_endpoint_idempotent_simple(
         assert attributes["description"] == 900
         assert attributes["type"] == 3000
         assert relationships["interfaces"] == 450
-        assert relationships["tags"] == 7000
+        assert relationships["tags"] == 5000
 
         creation = client.post(
             "/api/schema/load", headers=admin_headers, json={"schemas": [helper.schema_file("infra_simple_01.json")]}
