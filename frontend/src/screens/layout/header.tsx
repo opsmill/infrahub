4c95ea3cf5faf7df2c6d32e183197fcb7adcca03
Bilal <bilal@opsmill.com>
2023-12-27 11:11:28 +0100

    removed none-working searchbar

--- frontend/src/screens/layout/header.tsx
+++ frontend/src/screens/layout/header.tsx
@@ -1,4 +1,3 @@
-import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 import { Bars3BottomLeftIcon } from "@heroicons/react/24/outline";
 import { formatISO, isEqual, isValid } from "date-fns";
 import { useAtom } from "jotai";
@@ -54,48 +53,21 @@ export default function Header(props: Props) {
   };
 
   return (
-    <div className="z-10 flex h-16 flex-shrink-0 bg-custom-white shadow">
+    <div className="flex justify-between gap-4 flex-shrink-0 h-16 bg-custom-white shadow pr-4">
       <button
         type="button"
-        className="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-custom-blue-500 md:hidden"
+        className="border-r border-gray-200 p-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-custom-blue-500 md:hidden"
         onClick={() => setSidebarOpen(true)}>
         <span className="sr-only">Open sidebar</span>
         <Bars3BottomLeftIcon className="h-6 w-6" aria-hidden="true" />
       </button>
 
-      <div className="flex flex-1 justify-between px-4">
-        <div className="flex flex-1 opacity-30">
-          <form className="flex w-full md:ml-0" action="#" method="GET">
-            <label htmlFor="search-field" className="sr-only">
-              Search
-            </label>
-            <div className="relative w-full text-gray-400 focus-within:text-gray-600">
-              <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center">
-                <MagnifyingGlassIcon className="w-4 h-4" aria-hidden="true" />
-              </div>
-              <input
-                onChange={() => {}}
-                id="search-field"
-                className="block h-full w-full border-transparent py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:text-sm cursor-not-allowed"
-                placeholder="Search"
-                type="search"
-                name="search"
-              />
-            </div>
-          </form>
-        </div>
+      <div className="flex flex-1 items-center justify-end gap-4">
+        <DatePicker date={date} onChange={debouncedHandleDateChange} onClickNow={handleClickNow} />
 
-        <div className="flex items-center gap-4 ml-4 md:ml-6">
-          <DatePicker
-            date={date}
-            onChange={debouncedHandleDateChange}
-            onClickNow={handleClickNow}
-          />
+        <BranchSelector />
 
-          <BranchSelector />
-
-          <AccountMenu />
-        </div>
+        <AccountMenu />
       </div>
     </div>
   );
