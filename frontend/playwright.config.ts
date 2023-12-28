f3d62a629658358fa5bf1517b48193c1b775a5ea
Bilal <bilal@opsmill.com>
2023-12-28 21:25:37 +0100

    increase timeout in CI

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -17,6 +17,9 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  expect: {
+    timeout: process.env.CI ? 10000 : 5000,
+  },
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
