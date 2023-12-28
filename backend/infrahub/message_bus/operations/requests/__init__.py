297fdcad47817ee5ce4eda42cdfe90bfd748fc30
Damien Garros <dgarros@gmail.com>
2023-12-28 21:27:09 +0100

    Fix format

--- backend/infrahub/message_bus/operations/requests/__init__.py
+++ backend/infrahub/message_bus/operations/requests/__init__.py
@@ -1,3 +1,3 @@
-from . import artifact, artifact_definition, git, proposed_change, repository, graphql_query_group
+from . import artifact, artifact_definition, git, graphql_query_group, proposed_change, repository
 
 __all__ = ["artifact", "artifact_definition", "git", "graphql_query_group", "proposed_change", "repository"]
