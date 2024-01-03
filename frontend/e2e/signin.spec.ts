b5be1de5915060e0e253002e631af75daa55122d
Bilal <bilal@opsmill.com>
2024-01-03 15:33:03 +0100

    Added e2e test when login fails

--- frontend/e2e/signin.spec.ts
+++ frontend/e2e/signin.spec.ts
@@ -2,16 +2,30 @@ import { expect, test } from "@playwright/test";
 import { ACCOUNT_STATE_PATH, ADMIN_CREDENTIALS } from "../tests/utils";
 
 test.describe("/signin", () => {
-  test("should log in the user", async ({ page }) => {
-    await page.goto("/");
+  test.describe("When is not logged in", () => {
+    test("should log in the user", async ({ page }) => {
+      await page.goto("/");
 
-    await page.getByRole("link", { name: "Sign in" }).click();
-    await expect(page.getByText("Sign in to your account")).toBeVisible();
-    await page.getByLabel("Username").fill(ADMIN_CREDENTIALS.username);
-    await page.getByLabel("Password").fill(ADMIN_CREDENTIALS.password);
-    await page.getByRole("button", { name: "Sign in" }).click();
+      await page.getByRole("link", { name: "Sign in" }).click();
+      await expect(page.getByText("Sign in to your account")).toBeVisible();
+      await page.getByLabel("Username").fill(ADMIN_CREDENTIALS.username);
+      await page.getByLabel("Password").fill(ADMIN_CREDENTIALS.password);
+      await page.getByRole("button", { name: "Sign in" }).click();
 
-    await expect(page.getByTestId("current-user-avatar-button")).toBeVisible();
+      await expect(page.getByTestId("current-user-avatar-button")).toBeVisible();
+    });
+
+    test("should display an error message when authentication fails", async ({ page }) => {
+      await page.goto("/");
+
+      await page.getByRole("link", { name: "Sign in" }).click();
+      await expect(page.getByText("Sign in to your account")).toBeVisible();
+      await page.getByLabel("Username").fill("wrong username");
+      await page.getByLabel("Password").fill("wrong password");
+      await page.getByRole("button", { name: "Sign in" }).click();
+
+      await expect(page.getByRole("alert")).toContainText("Invalid username and password");
+    });
   });
 
   test.describe("When logged in", () => {
