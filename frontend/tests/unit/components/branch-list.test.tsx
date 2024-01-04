94c6ce5331300758a61b09c56d03e0a6d489bcf5
pa-lem <pa-lem@hotmail.fr>
2024-01-04 11:12:44 +0100

    move unit tests for components

--- frontend/tests/unit/components/branch-list.test.tsx
+++ frontend/tests/unit/components/branch-list.test.tsx
@@ -1,8 +1,8 @@
 import { MockedProvider } from "@apollo/client/testing";
 import { cleanup, render, screen } from "@testing-library/react";
 import { afterAll, describe, expect, it } from "vitest";
-import Apollo, { QUERY } from "../../../src/components/tests/branch-list-test";
 import { branchesMocks } from "../../../tests/mocks/data/branches";
+import Apollo, { QUERY } from "./branch-list";
 
 afterAll(cleanup);
 
