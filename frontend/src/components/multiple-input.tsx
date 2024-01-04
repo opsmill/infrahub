e4168237305ce5c428c5a882c798ca44978e7fea
pa-lem <pa-lem@hotmail.fr>
2024-01-04 11:02:51 +0100

    update multiple input height

--- frontend/src/components/multiple-input.tsx
+++ frontend/src/components/multiple-input.tsx
@@ -33,7 +33,7 @@ export const MultipleInput = React.forwardRef((props: MultipleInputProps, ref: a
   return (
     <div
       className={classNames(
-        `flex flex-wrap w-full rounded-md border-0 p-2 pt-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 min-h-[40px]
+        `flex flex-wrap w-full rounded-md border-0 p-2 pt-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 min-h-[44px]
           border-gray-300 bg-custom-white
           sm:text-sm sm:leading-6 px-2
           focus:ring-2 focus:ring-inset focus:ring-custom-blue-600 focus:border-custom-blue-600 focus:outline-none
