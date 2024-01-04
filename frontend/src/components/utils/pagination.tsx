6ad59c685d332b09e0e5ed322b49995784579807
Bilal <bilal@opsmill.com>
2024-01-04 16:53:25 +0100

    cleaned css

--- frontend/src/components/utils/pagination.tsx
+++ frontend/src/components/utils/pagination.tsx
@@ -82,7 +82,7 @@ export const Pagination = (props: tPaginationType) => {
     );
 
   return (
-    <div className="sticky bottom-0 flex items-center justify-between border-t border-gray-200 bg-custom-white p-2">
+    <div className="sticky bottom-0 flex items-center justify-between bg-custom-white p-2">
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
         <div className="flex items-center">
           <div className="text-sm text-gray-700">{paginationText}</div>
