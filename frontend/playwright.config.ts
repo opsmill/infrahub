4b4a1d90ad7bce735dd51e1225748d95f561be0e
pa-lem <pa-lem@hotmail.fr>
2024-01-05 09:43:22 +0100

    update config to remove warnings in CI and get videos only on failure

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -25,19 +25,19 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [[process.env.CI ? "github" : "list"], ["html", { open: "never" }]],
+  reporter: [["list"], ["html", { open: "never" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.CI ? "http://localhost:8000" : "http://localhost:8080",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "retain-on-failure",
     screenshot: {
       mode: "only-on-failure",
     },
     video: {
-      mode: "on-first-retry",
+      mode: "retain-on-failure",
     },
   },
 
