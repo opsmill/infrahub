d6c9d7e1c59b58d40241878bbc8b8f85f4ae4a2d
pa-lem <pa-lem@hotmail.fr>
2024-01-08 14:57:26 +0100

    rename contribute button

--- frontend/src/screens/branches/branch-details.tsx
+++ frontend/src/screens/branches/branch-details.tsx
@@ -219,7 +219,7 @@ export const BranchDetails = () => {
                   disabled={!auth?.permissions?.write || branch.is_default}
                   className="mr-0 md:mr-3"
                   onClick={() => setShowCreateDrawer(true)}>
-                  Contribute
+                  Propose change
                   <PlusIcon className="-mr-0.5 h-4 w-4" aria-hidden="true" />
                 </Button>
 
