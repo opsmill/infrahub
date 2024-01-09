673e0d894d9054f990ba0542d393d3e234f488fc
Patrick Ogenstad <patrick@ogenstad.com>
2024-01-09 08:23:09 +0100

    Remove AbstractExchange from InfrahubRpcClient

--- backend/infrahub/message_bus/rpc.py
+++ backend/infrahub/message_bus/rpc.py
@@ -1,25 +1,14 @@
 from __future__ import annotations
 
-import asyncio
-from typing import TYPE_CHECKING, Any, List, MutableMapping
+from typing import TYPE_CHECKING, List
 
 from infrahub.services import services
 
 if TYPE_CHECKING:
-    from aio_pika.abc import (
-        AbstractExchange,
-    )
-
     from . import InfrahubMessage
 
 
 class InfrahubRpcClientBase:
-    exchange: AbstractExchange
-
-    def __init__(self) -> None:
-        self.futures: MutableMapping[str, asyncio.Future] = {}
-        self.loop = asyncio.get_running_loop()
-
     async def send(self, message: InfrahubMessage) -> None:
         await services.send(message=message)
 
@@ -31,9 +20,7 @@ class InfrahubRpcClient(InfrahubRpcClientBase):
 class InfrahubRpcClientTesting(InfrahubRpcClientBase):
     """InfrahubRPCClient instrumented for testing and mocking."""
 
-    def __init__(self, *args: Any, **kwargs: Any):
-        super().__init__(*args, **kwargs)
-
+    def __init__(self) -> None:
         self.sent: List[InfrahubMessage] = []
 
     async def send(self, message: InfrahubMessage) -> None:
