ed007e101c3961f68b50494413f17b459905b38f
Patrick Ogenstad <patrick@ogenstad.com>
2024-01-10 14:18:40 +0100

    Add unittest for openapi spec generation
    Fixes #1843

---
+++ backend/tests/unit/api/test_openapi.py
@@ -0,0 +1,15 @@
+from infrahub.core.branch import Branch
+
+
+async def test_openapi(
+    client,
+    default_branch: Branch,
+):
+    """Validate that the OpenAPI specs can be generated."""
+    with client:
+        response = client.get(
+            "/api/openapi.json",
+        )
+
+    assert response.status_code == 200
+    assert response.json() is not None
