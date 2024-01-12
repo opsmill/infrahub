7b8a630711dba6050d2ed4240cf26b2348a7b8d4
Guillaume Mazoyer <guillaume@opsmill.com>
2024-01-12 14:12:43 +0100

    Add tests for RFile tests

--- python_sdk/tests/unit/pytest_plugin/test_plugin.py
+++ python_sdk/tests/unit/pytest_plugin/test_plugin.py
@@ -1,6 +1,6 @@
-def test_help_message(testdir):
+def test_help_message(pytester):
     """Make sure that the plugin is loaded by capturing an option it adds in the help message."""
-    result = testdir.runpytest("--help")
+    result = pytester.runpytest("--help")
     result.stdout.fnmatch_lines(["*Infrahub configuration file for the repository*"])
 
 
@@ -23,3 +23,208 @@ def test_emptyconfig(pytester):
 
     result = pytester.runpytest()
     result.assert_outcomes()
+
+
+def test_rfile_config_missing_directory(pytester):
+    """Make sure that the plugin load the test file properly."""
+    pytester.makefile(
+        ".yml",
+        test_rfile="""
+        ---
+        version: "1.0"
+        infrahub_tests:
+          - resource: "RFile"
+            resource_name: "bgp_config"
+            tests:
+              - name: "base"
+                expect: PASS
+                spec:
+                  kind: "rfile-unit-render"
+                  directory: bgp_config/base
+    """,
+    )
+    pytester.makefile(
+        ".yml",
+        infrahub_config="""
+        ---
+        schemas:
+          - schemas/demo_edge_fabric.yml
+
+        rfiles:
+          - name: bgp_config
+            description: "Template for BGP config base"
+            query: "bgp_sessions"
+            template_path: "templates/bgp_config.j2"
+
+    """,
+    )
+
+    result = pytester.runpytest("--infrahub-repo-config=infrahub_config.yml")
+    result.assert_outcomes(errors=1)
+
+
+def test_rfile_config_missing_input(pytester):
+    """Make sure that the plugin load the test file properly."""
+    pytester.makefile(
+        ".yml",
+        test_rfile="""
+        ---
+        version: "1.0"
+        infrahub_tests:
+          - resource: "RFile"
+            resource_name: "bgp_config"
+            tests:
+              - name: "base"
+                expect: PASS
+                spec:
+                  kind: "rfile-unit-render"
+                  directory: bgp_config/base
+    """,
+    )
+    pytester.makefile(
+        ".yml",
+        infrahub_config="""
+        ---
+        schemas:
+          - schemas/demo_edge_fabric.yml
+
+        rfiles:
+          - name: bgp_config
+            description: "Template for BGP config base"
+            query: "bgp_sessions"
+            template_path: "templates/bgp_config.j2"
+
+    """,
+    )
+
+    pytester.mkdir("bgp_config")
+    pytester.mkdir("bgp_config/base")
+
+    result = pytester.runpytest("--infrahub-repo-config=infrahub_config.yml")
+    result.assert_outcomes(errors=1)
+
+
+def test_rfile_no_expected_output(pytester):
+    """Make sure that the plugin load the test file properly."""
+    pytester.makefile(
+        ".yml",
+        test_rfile="""
+        ---
+        version: "1.0"
+        infrahub_tests:
+          - resource: "RFile"
+            resource_name: "bgp_config"
+            tests:
+              - name: "base"
+                expect: PASS
+                spec:
+                  kind: "rfile-unit-render"
+                  directory: bgp_config/base
+    """,
+    )
+    pytester.makefile(
+        ".yml",
+        infrahub_config="""
+        ---
+        schemas:
+          - schemas/demo_edge_fabric.yml
+
+        rfiles:
+          - name: bgp_config
+            description: "Template for BGP config base"
+            query: "bgp_sessions"
+            template_path: "templates/bgp_config.j2"
+
+    """,
+    )
+
+    pytester.mkdir("bgp_config")
+    pytester.mkdir("bgp_config/base")
+    pytester.makefile(".json", input='{"data": {}}')
+
+    template_dir = pytester.mkdir("templates")
+    template = pytester.makefile(
+        ".j2",
+        bgp_config="""
+    protocols {
+        bgp {
+            log-up-down;
+            bgp-error-tolerance;
+        }
+    }
+    """,
+    )
+    pytester.run("mv", template, template_dir)
+
+    result = pytester.runpytest("--infrahub-repo-config=infrahub_config.yml")
+    result.assert_outcomes(passed=1)
+
+
+def test_rfile_unexpected_output(pytester):
+    """Make sure that the plugin load the test file properly."""
+    pytester.makefile(
+        ".yml",
+        test_rfile="""
+        ---
+        version: "1.0"
+        infrahub_tests:
+          - resource: "RFile"
+            resource_name: "bgp_config"
+            tests:
+              - name: "base"
+                expect: PASS
+                spec:
+                  kind: "rfile-unit-render"
+                  directory: bgp_config/base
+    """,
+    )
+    pytester.makefile(
+        ".yml",
+        infrahub_config="""
+        ---
+        schemas:
+          - schemas/demo_edge_fabric.yml
+
+        rfiles:
+          - name: bgp_config
+            description: "Template for BGP config base"
+            query: "bgp_sessions"
+            template_path: "templates/bgp_config.j2"
+
+    """,
+    )
+
+    pytester.mkdir("bgp_config")
+    pytester.mkdir("bgp_config/base")
+    pytester.makefile(".json", input='{"data": {}}')
+    pytester.makefile(
+        ".txt",
+        output="""
+    protocols {
+        bgp {
+            group ipv4-ibgp {
+                peer-as 64545;
+            }
+            log-up-down;
+            bgp-error-tolerance;
+        }
+    }
+    """,
+    )
+
+    template_dir = pytester.mkdir("templates")
+    template = pytester.makefile(
+        ".j2",
+        bgp_config="""
+    protocols {
+        bgp {
+            log-up-down;
+            bgp-error-tolerance;
+        }
+    }
+    """,
+    )
+    pytester.run("mv", template, template_dir)
+
+    result = pytester.runpytest("--infrahub-repo-config=infrahub_config.yml")
+    result.assert_outcomes(failed=1)
