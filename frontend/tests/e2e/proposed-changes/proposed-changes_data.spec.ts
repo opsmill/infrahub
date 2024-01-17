e257ce0e181e2c3ec9c286f2d14c96e940617ed8
Bilal <bilal@opsmill.com>
2024-01-17 11:51:29 +0100

    migrate proposed changes e2e test from cypress to playwright

--- frontend/tests/e2e/proposed-changes/proposed-changes_data.spec.ts
+++ frontend/tests/e2e/proposed-changes/proposed-changes_data.spec.ts
@@ -7,61 +7,74 @@ test.describe("/proposed-changes diff data", () => {
 
   test("should create a new proposed changes", async ({ page }) => {
     await page.goto("/proposed-changes");
-    await page.getByTestId("add-proposed-changes-button").click();
 
-    // Form
-    await expect(page.getByText("Create Proposed Changes")).toBeVisible();
-    await page.getByLabel("Name *").fill("pc-data-diff");
-    await page.locator("div:below(#Name)").first().getByTestId("select-open-option-button").click();
-    await page.getByRole("option", { name: "atl1-delete-transit" }).click();
-    await page.getByRole("button", { name: "Create" }).click();
+    await test.step("create a new proposed change", async () => {
+      await page.getByTestId("add-proposed-changes-button").click();
+      await expect(page.getByText("Create Proposed Change")).toBeVisible();
+      await page.getByLabel("Name *").fill("pc-data-diff");
+      await page
+        .locator("div:below(#Name)")
+        .first()
+        .getByTestId("select-open-option-button")
+        .click();
+      await page.getByRole("option", { name: "atl1-delete-transit" }).click();
+      await page.getByRole("button", { name: "Create" }).click();
+      await expect(page.getByText("ProposedChange created")).toBeVisible();
+    });
 
-    // After submit
-    await expect(page.getByText("ProposedChange created")).toBeVisible();
+    await test.step("display created proposed change details", async () => {
+      await expect(page.getByText("Namepc-data-diff")).toBeVisible();
+      await expect(page.getByText("Source branchatl1-delete-transit")).toBeVisible();
+      await expect(page.getByText("Stateopen")).toBeVisible();
+    });
 
-    await expect(page.getByText("Namepc-data-diff")).toBeVisible();
-    await expect(page.getByText("Source branchatl1-delete-transit")).toBeVisible();
-    await expect(page.getByText("Stateopen")).toBeVisible();
+    await test.step("go to Data tab and open comment form", async () => {
+      await page.getByLabel("Tabs").getByText("Data").click();
+      await page.getByText("InfraCircuit").first().hover();
+      await page.getByTestId("data-diff-add-comment").first().click();
+      await expect(page.getByText("Conversation")).toBeVisible();
+    });
 
-    await page.getByLabel("Tabs").getByText("Data").click();
-    await page.getByText("InfraCircuit").first().hover();
-    await page.getByTestId("data-diff-add-comment").first().click();
+    await test.step("add first comment", async () => {
+      await page.getByTestId("codemirror-editor").getByRole("textbox").fill("first is comment");
+      await page.getByRole("button", { name: "Comment" }).click();
+      await expect(page.getByTestId("thread").getByTestId("comment")).toContainText(
+        "first is comment"
+      );
+      await expect(page.getByTestId("comments-count")).toContainText("1");
+    });
 
-    // form
-    await expect(page.getByText("Conversation")).toBeVisible();
+    const thread = page.getByTestId("thread");
 
-    // first comment
-    await page.getByTestId("codemirror-editor").getByRole("textbox").fill("first is comment");
-    await page.getByRole("button", { name: "Comment" }).click();
-    const thread = await page.getByTestId("thread");
-    await expect(thread.getByTestId("comment")).toContainText("first is comment");
-    await expect(page.getByTestId("comments-count")).toContainText("1");
+    await test.step("reply to first comment", async () => {
+      await thread.getByRole("button", { name: "Reply" }).click();
+      await thread.getByTestId("codemirror-editor").getByRole("textbox").fill("second is reply");
+      await page.getByRole("button", { name: "Comment" }).click();
+      await expect(thread.getByTestId("comment").nth(1)).toContainText("second is reply");
+      await expect(page.getByTestId("comments-count")).toContainText("2");
+    });
 
-    // reply
-    await thread.getByRole("button", { name: "Reply" }).click();
-    await thread.getByTestId("codemirror-editor").getByRole("textbox").fill("second is reply");
-    await page.getByRole("button", { name: "Comment" }).click();
-    await expect(thread.getByTestId("comment").nth(1)).toContainText("second is reply");
-    await expect(page.getByTestId("comments-count")).toContainText("2");
+    await test.step("start then cancel new comment", async () => {
+      await page.getByRole("button", { name: "Reply" }).click();
+      await expect(thread.getByTestId("codemirror-editor")).toBeVisible();
+      await page.getByRole("button", { name: "Cancel" }).click();
+      await expect(thread.getByTestId("codemirror-editor")).toBeHidden();
+    });
 
-    // cancel reply
-    await page.getByRole("button", { name: "Reply" }).click();
-    await expect(thread.getByTestId("codemirror-editor")).toBeVisible();
-    await page.getByRole("button", { name: "Cancel" }).click();
-    await expect(thread.getByTestId("codemirror-editor")).toBeHidden();
+    await test.step("Resolve thread", async () => {
+      await thread.getByLabel("Resolve thread").click();
+      await expect(page.getByRole("heading", { name: "Confirm" })).toBeVisible();
+      await page.getByRole("button", { name: "Confirm" }).click();
+      await expect(thread.getByText("Resolved")).toBeVisible();
+    });
 
-    // Resolve
-    await thread.getByLabel("Resolve thread").click();
-    await expect(page.getByRole("heading", { name: "Confirm" })).toBeVisible();
-    await page.getByRole("button", { name: "Confirm" }).click();
-    await expect(thread.getByText("Resolved")).toBeVisible();
-
-    // third comment
-    await thread.getByRole("button", { name: "Reply" }).click();
-    await thread.getByTestId("codemirror-editor").getByRole("textbox").fill("third resolved");
-    await thread.getByRole("button", { name: "Comment" }).click();
-    await expect(thread.getByTestId("comment").nth(2)).toContainText("third resolved");
-    await expect(page.getByTestId("comments-count")).toContainText("3");
+    await test.step("add comment when thread is resolved", async () => {
+      await thread.getByRole("button", { name: "Reply" }).click();
+      await thread.getByTestId("codemirror-editor").getByRole("textbox").fill("third resolved");
+      await thread.getByRole("button", { name: "Comment" }).click();
+      await expect(thread.getByTestId("comment").nth(2)).toContainText("third resolved");
+      await expect(page.getByTestId("comments-count")).toContainText("3");
+    });
   });
 
   test("should delete proposed changes", async ({ page }) => {
