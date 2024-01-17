22d69a3e44b5a62131ee579ddefc36c4ab2ce886
Guillaume Mazoyer <guillaume@opsmill.com>
2024-01-17 14:07:09 +0100

    Add expiration time to `InfrahubMessage` meta (#1916)
    Add git sync interval setting
    
    Introduce a new git sync interval setting. This new setting value is also used as
    expiration time for the resync repositories schedule task.

--- backend/infrahub/config.py
+++ backend/infrahub/config.py
@@ -176,6 +176,9 @@ class ApiSettings(BaseSettings):
 
 class GitSettings(BaseSettings):
     repositories_directory: str = "repositories"
+    sync_interval: int = Field(
+        default=10, ge=0, description="Time (in seconds) between git repositories synchronizations"
+    )
 
 
 class MiscellaneousSettings(BaseSettings):
