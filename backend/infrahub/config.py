00051a74fe9c75528ee1ed07d5e52a6ed3ed1e03
Guillaume Mazoyer <guillaume@opsmill.com>
2024-01-20 00:27:23 +0100

    Rename neo4j specific config options (#1963)

--- backend/infrahub/config.py
+++ backend/infrahub/config.py
@@ -53,7 +53,7 @@ class MainSettings(BaseSettings):
 
 
 class FileSystemStorageSettings(BaseSettings):
-    # Make variable lookup case-sensitive to avoid fetch $PATH value
+    # Make variable lookup case-sensitive to avoid fetching $PATH value
     model_config = SettingsConfigDict(case_sensitive=True)
     path_: str = Field(
         default="/opt/infrahub/storage",
@@ -103,32 +103,26 @@ class StorageSettings(BaseSettings):
 
 
 class DatabaseSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="INFRAHUB_DB_")
     db_type: DatabaseType = Field(
-        default=DatabaseType.MEMGRAPH, validation_alias=AliasChoices("db_type", "INFRAHUB_DB_TYPE")
+        default=DatabaseType.MEMGRAPH, validation_alias=AliasChoices("INFRAHUB_DB_TYPE", "db_type")
     )
-    protocol: str = Field(default="bolt", validation_alias=AliasChoices("protocol", "NEO4J_PROTOCOL"))
-    username: str = Field(default="neo4j", validation_alias=AliasChoices("username", "NEO4J_USERNAME"))
-    password: str = Field(default="admin", validation_alias=AliasChoices("password", "NEO4J_PASSWORD"))
-    address: str = Field(default="localhost", validation_alias=AliasChoices("address", "NEO4J_ADDRESS"))
-    port: int = Field(default=7687, validation_alias=AliasChoices("port", "NEO4J_PORT"))
-    database: Optional[str] = Field(
-        default=None,
-        pattern=VALID_DATABASE_NAME_REGEX,
-        description="Name of the database",
-        validation_alias=AliasChoices("database", "NEO4J_DATABASE"),
-    )
+    protocol: str = "bolt"
+    username: str = "neo4j"
+    password: str = "admin"
+    address: str = "localhost"
+    port: int = 7687
+    database: Optional[str] = Field(default=None, pattern=VALID_DATABASE_NAME_REGEX, description="Name of the database")
     query_size_limit: int = Field(
         default=5000,
         ge=1,
         le=20000,
         description="The max number of records to fetch in a single query before performing internal pagination.",
-        validation_alias=AliasChoices("query_size_limit", "INFRAHUB_DB_QUERY_SIZE_LIMIT"),
     )
     max_depth_search_hierarchy: int = Field(
         default=5,
         le=20,
         description="Maximum number of level to search in a hierarchy.",
-        validation_alias=AliasChoices("max_depth_search_hierarchy", "INFRAHUB_DB_MAX_DEPTH_SEARCH_HIERARCHY"),
     )
 
     @property
