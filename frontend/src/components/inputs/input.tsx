3a0572bdf2ce0aa614c57264e6a7208cf3e8a125
pa-lem <pa-lem@hotmail.fr>
2024-01-24 08:28:06 +0100

    add empty button in input

--- frontend/src/components/inputs/input.tsx
+++ frontend/src/components/inputs/input.tsx
@@ -16,9 +16,15 @@ import { BUTTON_TYPES, Button } from "../buttons/button";
 // Forward ref used for Combobox.Input in Select
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
 export const Input = forwardRef((props: any, ref: any) => {
-  const { className, onChange, error, type, ...propsToPass } = props;
+  const { className, onChange, error, type, hideEmpty, ...propsToPass } = props;
 
   const [display, setDisplay] = useState(false);
+
+  const handleInputChange = (event: any) => {
+    const value = type === "number" ? event.target.valueAsNumber : event.target.value;
+
+    onChange(value, event);
+  };
 
   const displayButton = (
     <Button buttonType={BUTTON_TYPES.INVISIBLE} onClick={() => setDisplay(!display)}>
@@ -26,14 +32,18 @@ export const Input = forwardRef((props: any, ref: any) => {
     </Button>
   );
 
+  const removeButton = (
+    <Button
+      buttonType={BUTTON_TYPES.INVISIBLE}
+      onClick={(event) => onChange(type === "number" ? 0 : "", event)}>
+      <Icon icon="mdi:close" className="text-gray-400" />
+    </Button>
+  );
+
   return (
     <div className="relative flex items-center h-full w-full">
       <input
-        onChange={(event) => {
-          const value = type === "number" ? event.target.valueAsNumber : event.target.value;
-
-          onChange(value, event);
-        }}
+        onChange={handleInputChange}
         className={classNames(
           `block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
           border-gray-300 bg-custom-white
@@ -47,6 +57,11 @@ export const Input = forwardRef((props: any, ref: any) => {
         type={type === "password" && display ? "text" : type}
         {...propsToPass}
       />
+
+      {propsToPass.value && !hideEmpty && (
+        <div className="absolute right-2 top-0 bottom-0 flex items-center">{removeButton}</div>
+      )}
+
       {error?.message && (
         <div
           className="absolute text-sm text-red-500 bg-custom-white -bottom-2 ml-2 px-2"
@@ -54,8 +69,9 @@ export const Input = forwardRef((props: any, ref: any) => {
           {error?.message}
         </div>
       )}
+
       {type === "password" && (
-        <div className="absolute right-2 top-0 bottom-0 flex items-center">{displayButton}</div>
+        <div className="absolute right-4 top-0 bottom-0 flex items-center">{displayButton}</div>
       )}
     </div>
   );
