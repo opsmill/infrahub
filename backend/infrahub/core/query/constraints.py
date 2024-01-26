542003d806a903f018fb92c88eceb23b353b8d34
Aaron McCarty <aaron@opsmill.com>
2024-01-25 22:22:54 -0800

    aaron's query

--- backend/infrahub/core/query/constraints.py
+++ backend/infrahub/core/query/constraints.py
@@ -24,6 +24,45 @@ class NodeConstraintsUniquenessQuery(Query):
         # Add the Branch filters
         branch_filter, branch_params = self.branch.get_query_filter_path(at=self.at.to_string())
         self.params.update(branch_params)
+        self.params.update({"node_kind": self.schema.kind, "attr_names": [attr_schema.name for attr_schema in self.schema.unique_attributes]})
+        from_times = db.render_list_comprehension(items="relationships(sub_path)", item_name="from")
+
+        query = """
+        MATCH sub_path = (sub_n:Node)-[:HAS_ATTRIBUTE]-(sub_attr:Attribute)-[:HAS_VALUE]-(sub_attr_value:AttributeValue)
+        WHERE $node_kind IN LABELS(sub_n) AND (sub_attr.name IN $attr_names)
+        CALL {
+        WITH sub_n, sub_path, sub_attr, sub_attr_value
+        WHERE all(
+            r IN relationships(sub_path) WHERE (
+                (
+                    (%(branch_filter)s)
+                        // (r.branch IN ['main', '-global-'] AND r.from <= "2024-01-26T02:10:19.000000Z" AND r.to IS NULL)
+                        // OR (r.branch IN ['main', '-global-'] AND r.from <= "2024-01-26T02:10:19.000000Z" AND r.to >= "2024-01-26T02:10:19.000000Z")
+                    AND r.status = "active"
+                )
+            )
+        )
+        WITH
+            sub_attr,
+            sub_attr_value,
+            sub_path,
+            reduce(br_lvl = 0, r in relationships(sub_path) | br_lvl + r.branch_level) AS branch_level
+            , %(from_times)s AS from_times
+        RETURN sub_n AS n, sub_attr AS attr, sub_attr_value AS attr_value
+        ORDER BY
+            branch_level DESC
+            , from_times[1] DESC
+            , from_times[2] DESC
+        LIMIT 1
+        }
+        WITH n.kind as kind, collect(n.uuid) as node_ids, count(*) as cnt, attr.name as attr_name, attr_value.value as attr_value
+        WHERE cnt > 1
+        """ % {"branch_filter": branch_filter, "from_times": from_times}
+
+
+        self.add_to_query(query)
+
+        self.return_labels = ["kind", "node_ids", "cnt", "attr_name", "attr_value"]
 
         #         """
         #         CALL {
@@ -35,36 +74,41 @@ class NodeConstraintsUniquenessQuery(Query):
         # WITH cnt1, value1, value2
         # WHERE cnt1 > 1
         # RETURN cnt1, value1, value2
-        #         """
-        froms_var = db.render_list_comprehension(items="relationships(path1)", item_name="from")
-        with_clause = (
-            "av1, av2, path1, path2,"
-            " reduce(br_lvl = 0, r in relationships(path1) | br_lvl + r.branch_level) AS branch_level,"
-            f" {froms_var} AS froms"
-        )
-        query = """
-        MATCH (n:Node)
-        WHERE $node_kind IN LABELS(n)
-        CALL {
-            WITH n
-            MATCH path1 = (n)-[:HAS_ATTRIBUTE]-(:Attribute {name: "nbr_seats"})-[:HAS_VALUE]-(av1:AttributeValue)
-            MATCH path2 = (n)-[:HAS_ATTRIBUTE]-(:Attribute {name: "is_electric"})-[:HAS_VALUE]-(av2:AttributeValue)
-            WHERE all(r IN relationships(path1) WHERE (%(branch_filter)s)) AND all(r IN relationships(path2) WHERE (%(branch_filter)s))
-            WITH %(with_clause)s
-            RETURN n as n1, av1 as av11, av2 as av22, path1 as path11, path2 as path22
-            ORDER BY branch_level DESC, froms[-1] DESC, froms[-2] DESC
-            LIMIT 1
-            // RETURN n.uuid as node, r1.status as status1, rav1.value as value1
-        }
-        WITH n1, av11, av22, path11, path22
-        WHERE all(r IN relationships(path11) WHERE (r.status = "active")) AND all(r IN relationships(path22) WHERE (r.status = "active"))
-        CALL {
-            WITH n1, av11, av22
-            RETURN count(n1) as cnt1, av11.value as value1, av22.value as value2
-        }
-        WITH cnt1, value1, value2
-        RETURN cnt1, value1, value2
-        """ % {"branch_filter": branch_filter, "with_clause": with_clause}
+        #     
+        #     """
+
+        ###############################
+        # DAMIEN'S QUERY
+        ###############################
+        # froms_var = db.render_list_comprehension(items="relationships(path1)", item_name="from")
+        # with_clause = (
+        #     "av1, av2, path1, path2,"
+        #     " reduce(br_lvl = 0, r in relationships(path1) | br_lvl + r.branch_level) AS branch_level,"
+        #     f" {froms_var} AS froms"
+        # )
+        # query = """
+        # MATCH (n:Node)
+        # WHERE $node_kind IN LABELS(n)
+        # CALL {
+        #     WITH n
+        #     MATCH path1 = (n)-[:HAS_ATTRIBUTE]-(:Attribute {name: "nbr_seats"})-[:HAS_VALUE]-(av1:AttributeValue)
+        #     MATCH path2 = (n)-[:HAS_ATTRIBUTE]-(:Attribute {name: "is_electric"})-[:HAS_VALUE]-(av2:AttributeValue)
+        #     WHERE all(r IN relationships(path1) WHERE (%(branch_filter)s)) AND all(r IN relationships(path2) WHERE (%(branch_filter)s))
+        #     WITH %(with_clause)s
+        #     RETURN n as n1, av1 as av11, av2 as av22, path1 as path11, path2 as path22
+        #     ORDER BY branch_level DESC, froms[-1] DESC, froms[-2] DESC
+        #     LIMIT 1
+        #     // RETURN n.uuid as node, r1.status as status1, rav1.value as value1
+        # }
+        # WITH n1, av11, av22, path11, path22
+        # WHERE all(r IN relationships(path11) WHERE (r.status = "active")) AND all(r IN relationships(path22) WHERE (r.status = "active"))
+        # CALL {
+        #     WITH n1, av11, av22
+        #     RETURN count(n1) as cnt1, av11.value as value1, av22.value as value2
+        # }
+        # WITH cnt1, value1, value2
+        # RETURN cnt1, value1, value2
+        # """ % {"branch_filter": branch_filter, "with_clause": with_clause}
 
         # CALL {
         # MATCH (n:Node)
@@ -92,6 +136,6 @@ class NodeConstraintsUniquenessQuery(Query):
         # WHERE cnt1 > 1
         # RETURN cnt1, value1, value2
 
-        self.add_to_query(query)
+        # self.add_to_query(query)
 
-        self.return_labels = ["cnt1", "value1"]
+        # self.return_labels = ["cnt1", "value1"]
--- backend/tests/unit/core/test_query_constraints.py
+++ backend/tests/unit/core/test_query_constraints.py
@@ -13,12 +13,23 @@ async def test_query_uniqueness(
     car_volt_main,
     car_yaris_main,
     car_yaris2_main,
+    car_prius_main,
     branch: Branch,
 ):
     node_to_delete = await NodeManager.get_one(id=car_camry_main.id, db=db, branch=branch)
     await node_to_delete.delete(db=db)
 
+    car_volt = await NodeManager.get_one(id=car_volt_main.id, db=db, branch=branch)
+    car_volt.nbr_seats.value = 3
+    await car_volt.save(db=db)
+
+    car_volt = await NodeManager.get_one(id=car_volt_main.id, db=db, branch=branch)
+    car_volt.nbr_seats.value = 2
+    await car_volt.save(db=db)
+
     schema = registry.schema.get(name="TestCar", branch=branch)
+
+    breakpoint()
 
     query = await NodeConstraintsUniquenessQuery.init(
         db=db, branch=branch, schema=schema, constraints={"is_electric__value": False}
