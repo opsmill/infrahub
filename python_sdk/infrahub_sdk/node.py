40e8ec1dba3827405b91b006f30b4b97811342d0
Benoit Kohler <benoit@opsmill.com>
2024-01-29 13:41:48 +0100

    move setting the id at the end of the function, as it was rewrite in between causing issue

--- python_sdk/infrahub_sdk/node.py
+++ python_sdk/infrahub_sdk/node.py
@@ -715,9 +715,6 @@ class InfrahubNodeBase:
         data = {}
         variables = {}
 
-        if self.id is not None:
-            data["id"] = self.id
-
         for item_name in self._attributes:
             attr: Attribute = getattr(self, item_name)
             if attr._schema.read_only:
@@ -775,6 +772,9 @@ class InfrahubNodeBase:
             data, variables = self._strip_unmodified(data=data, variables=variables)
 
         mutation_variables = {key: type(value) for key, value in variables.items()}
+
+        if self.id is not None:
+            data["id"] = self.id
 
         return {
             "data": {"data": data},
@@ -1220,6 +1220,7 @@ class InfrahubNode(InfrahubNodeBase):
 
     async def update(self, at: Timestamp, do_full_update: bool = False) -> None:
         input_data = self._generate_input_data(exclude_unmodified=not do_full_update)
+        # input_data["data"]["data"]["id"] = self.id
         mutation_query = {"ok": None, "object": {"id": None}}
         query = Mutation(
             mutation=f"{self._schema.kind}Update",
