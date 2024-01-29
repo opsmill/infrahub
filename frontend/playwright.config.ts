4c7b2053919163560c30ad707c75de78f5e027f9
pa-lem <pa-lem@hotmail.fr>
2024-01-29 11:02:51 +0100

    update base url

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -33,8 +33,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: process.env.CI ? process.env.INFRAHUB_ADDRESS : "http://localhost:8080",
-    baseURL: process.env.INFRAHUB_ADDRESS,
+    baseURL: process.env.CI ? process.env.INFRAHUB_ADDRESS : "http://localhost:8000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: process.env.CI ? "retain-on-failure" : "on",
