9d8b9749a4e7e85fbf923cbd6f0d5ca8337b9aee
Bilal <bilal@opsmill.com>
2024-01-30 16:24:19 +0100

    remove absolute from avatar

--- frontend/src/screens/layout/header.tsx
+++ frontend/src/screens/layout/header.tsx
@@ -4,7 +4,7 @@ import { SearchBar } from "../../components/search/search-bar";
 export default function Header() {
   // Search bar after buttons to fix a z-index issue
   return (
-    <header className="relative z-10 flex items-center justify-between h-16 bg-custom-white border-b">
+    <header className="px-4 relative z-10 flex items-center justify-between h-16 bg-custom-white border-b">
       <SearchBar />
 
       <AccountMenu />
