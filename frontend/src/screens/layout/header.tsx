8c327d05dd4872e2b5e7d1560177386b9d7a3c97
pa-lem <pa-lem@hotmail.fr>
2024-01-31 13:07:17 +0100

    add subscription

--- frontend/src/screens/layout/header.tsx
+++ frontend/src/screens/layout/header.tsx
@@ -7,6 +7,10 @@ export default function Header() {
     <div className="relative z-10 flex items-center justify-between h-16 bg-custom-white shadow">
       <SearchBar />
 
+      {/* <Notifications query="oc_bgp_neighbors" />
+      <Notifications query="topology_info" />
+      <Notifications query="oc_interfaces" /> */}
+
       <AccountMenu />
     </div>
   );
