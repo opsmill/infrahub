9c4c558f16697f0a8312cb149a36842a3f53f528
Benoit Kohler <BeArchiTek@users.noreply.github.com>
2024-01-31 11:00:12 +0100

    cleanup cli.py

--- python_sdk/infrahub_sdk/ctl/cli.py
+++ python_sdk/infrahub_sdk/ctl/cli.py
@@ -77,36 +77,6 @@ async def _run(
     await func(client=client, log=log, branch=branch, **variables)
 
 
-def identify_faulty_jinja_code(traceback: Traceback, nbr_context_lines: int = 3) -> List[Tuple[Frame, Syntax]]:
-    response = []
-
-    # The Traceback from rich is very helpful to parse the entire stack trace
-    # to will generate a Frame object for each exception in the trace
-
-    # Extract only the Jinja related exceptioin from the stack
-    frames = [frame for frame in traceback.trace.stacks[0].frames if frame.filename.endswith(".j2")]
-
-    for frame in frames:
-        code = "".join(linecache.getlines(frame.filename))
-        lexer_name = Traceback._guess_lexer(frame.filename, code)
-        syntax = Syntax(
-            code,
-            lexer_name,
-            line_numbers=True,
-            line_range=(
-                frame.lineno - nbr_context_lines,
-                frame.lineno + nbr_context_lines,
-            ),
-            highlight_lines={frame.lineno},
-            code_width=88,
-            theme=traceback.theme,
-            dedent=False,
-        )
-        response.append((frame, syntax))
-
-    return response
-
-
 def render_jinja2_template(template_path: Path, variables: Dict[str, str], data: str) -> str:
     if not template_path.is_file():
         console.print(f"[red]Unable to locate the template at {template_path}")
