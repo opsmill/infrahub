b6db1094ab7f074a5892b35a2c5f5c4d11b10dd6
Damien Garros <dgarros@gmail.com>
2024-02-05 13:30:52 +0100

    Remove deprecated checks and transforms

--- backend/infrahub/checks.py
+++
@@ -1,9 +0,0 @@
-from warnings import warn
-
-from infrahub_sdk.checks import INFRAHUB_CHECK_VARIABLE_TO_IMPORT, InfrahubCheck
-
-warn(
-    f"The module {__name__} is deprecated. Update to use infrahub_sdk.checks instead.", DeprecationWarning, stacklevel=2
-)
-
-__all__ = ["INFRAHUB_CHECK_VARIABLE_TO_IMPORT", "InfrahubCheck"]
