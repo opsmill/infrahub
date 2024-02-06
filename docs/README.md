9cc3e7c477c95f5f38a1766e8411403b044f6cbb
Bilal <bilal@opsmill.com>
2024-02-06 16:47:52 +0100

    formatting

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Website
+# Infrahub Documentation Website
 
 This website is built using [Docusaurus](https://docusaurus.io/), a modern static website generator.
 
@@ -7,13 +7,13 @@ This website is built using [Docusaurus](https://docusaurus.io/), a modern stati
 ### Installation
 
 ```shell
-$ npm install
+npm install
 ```
 
 ### Local Development
 
 ```shell
-$ npm start
+npm start
 ```
 
 This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
@@ -21,7 +21,7 @@ This command starts a local development server and opens up a browser window. Mo
 ### Build
 
 ```shell
-$ npm run build
+npm run build
 ```
 
 This command generates static content into the `build` directory and can be served using any static contents hosting service.
--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,61 +1,66 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'Infrahub Documentation',
-  tagline: 'Explore our guides and examples to use Infrahub.',
-  favicon: 'img/favicon.ico',
+  title: "Infrahub Documentation",
+  tagline: "Explore our guides and examples to use Infrahub.",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'http://localhost:8000',
+  url: "http://localhost:8000",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'opsmill', // Usually your GitHub org/user name.
-  projectName: 'infrahub', // Usually your repo name.
+  organizationName: "opsmill", // Usually your GitHub org/user name.
+  projectName: "infrahub", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenAnchors: 'throw',
-  onBrokenMarkdownLinks: 'throw',
-  onDuplicateRoutes: 'throw',
+  onBrokenLinks: "throw",
+  onBrokenAnchors: "throw",
+  onBrokenMarkdownLinks: "throw",
+  onDuplicateRoutes: "throw",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/opsmill/infrahub/tree/bab-docusaurus/docs-docusaurus/',
-          routeBasePath: '/',
+          editUrl: "https://github.com/opsmill/infrahub/tree/bab-docusaurus/docs-docusaurus/",
+          routeBasePath: "/",
           sidebarCollapsed: true,
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
         },
         blog: false,
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
   ],
-  themes: [["@easyops-cn/docusaurus-search-local", {
-    indexBlog: false,
-    indexDocs: true,
-    docsRouteBasePath: '/', // this needs to be the same as routeBasePath
-    hashed: true
-  }]],
+  themes: [
+    [
+      "@easyops-cn/docusaurus-search-local",
+      {
+        indexBlog: false,
+        indexDocs: true,
+        docsRouteBasePath: "/", // this needs to be the same as routeBasePath
+        hashed: true,
+      }
+    ],
+  ],
   themeConfig: {
     // announcementBar: {
     //   content: 'Welcome to our brand new docs!',
@@ -63,20 +68,20 @@ const config: Config = {
     // },
     navbar: {
       logo: {
-        alt: 'Infrahub',
-        src: 'img/infrahub-hori.svg',
-        srcDark: 'img/infrahub-hori-dark.svg',
+        alt: "Infrahub",
+        src: "img/infrahub-hori.svg",
+        srcDark: "img/infrahub-hori-dark.svg",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'docsSidebar',
-          position: 'left',
-          label: 'Documentation',
+          type: "docSidebar",
+          sidebarId: "docsSidebar",
+          position: "left",
+          label: "Documentation",
         },
         {
-          type: 'search',
-          position: 'right',
+          type: "search",
+          position: "right",
         },
         {
           href: "https://github.com/opsmill/infrahub",
@@ -91,7 +96,7 @@ const config: Config = {
     },
     prism: {
       theme: prismThemes.oneDark,
-      additionalLanguages: ["bash", "python", "markup-templating", "django", "json", "toml", "yaml"]
+      additionalLanguages: ["bash", "python", "markup-templating", "django", "json", "toml", "yaml"],
     },
   } satisfies Preset.ThemeConfig,
 };
