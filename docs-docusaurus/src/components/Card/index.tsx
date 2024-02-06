a58a54c500597d213a232ac4589083df832ec437
Bilal <bilal@opsmill.com>
2024-02-06 14:58:18 +0100

    delete old doc-docusaurus folder

--- docs-docusaurus/src/components/Card/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function ReferenceLink({ title, url, openInNewTab }) {
-  return (
-    <a href={url} {...openInNewTab && { target: "_blank" }} className="flex justify-between pagination-nav__link margin-bottom--md">
-      <span>{title}</span>
-      <span>{url}</span>
-    </a>
-  )
-};
