3f111445d3641192e59dfd26c520899e2513f87e
pa-lem <pa-lem@hotmail.fr>
2024-02-09 12:11:39 +0100

    fix empty display for table

--- frontend/src/components/table/table.tsx
+++ frontend/src/components/table/table.tsx
@@ -29,63 +29,65 @@ export const Table = (props: tTableProps) => {
   const auth = useContext(AuthContext);
 
   return (
-    <table className="table-auto border-spacing-0 w-full border border-gray-300 rounded-md">
-      <thead className="bg-gray-50 text-left border-b border-gray-300 rounded-md">
-        <tr>
-          {columns?.map((column) => (
-            <th key={column.name} scope="col" className="p-2 text-xs font-semibold text-gray-900">
-              {column.label}
-            </th>
-          ))}
-          {onDelete && <th scope="col"></th>}
-        </tr>
-      </thead>
-
-      <tbody className="bg-custom-white text-left">
-        {!rows?.length && <NoDataFound message="No items" className="m-auto w-full" />}
-
-        {rows?.map((row: any, index: number) => (
-          <tr
-            key={index}
-            className={classNames(
-              "border-b border-gray-200",
-              row.link ? "hover:bg-gray-50 cursor-pointer" : ""
-            )}
-            data-cy="object-table-row">
-            {columns?.map((column, index) => (
-              <td key={index} className="p-0">
-                {row.link && (
-                  <Link
-                    className="whitespace-wrap px-2 py-1 text-xs text-gray-900 min-h-7 flex items-center"
-                    to={row.link}>
-                    <div>{row.values[column.name]}</div>
-                  </Link>
-                )}
-
-                {!row.link && (
-                  <div className="whitespace-wrap px-2 py-1 text-xs text-gray-900 min-h-7 flex items-center whitespace-pre">
-                    {row.values[column.name]}
-                  </div>
-                )}
-              </td>
+    <>
+      <table className="table-auto border-spacing-0 w-full border border-gray-300 rounded-md">
+        <thead className="bg-gray-50 text-left border-b border-gray-300 rounded-md">
+          <tr>
+            {columns?.map((column) => (
+              <th key={column.name} scope="col" className="p-2 text-xs font-semibold text-gray-900">
+                {column.label}
+              </th>
             ))}
-
-            {onDelete && (
-              <td className="text-right w-8">
-                <Button
-                  data-cy="delete"
-                  disabled={!auth?.permissions?.write}
-                  buttonType={BUTTON_TYPES.INVISIBLE}
-                  onClick={() => {
-                    onDelete(row);
-                  }}>
-                  <Icon icon="mdi:trash" className="text-red-500" />
-                </Button>
-              </td>
-            )}
+            {onDelete && <th scope="col"></th>}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody className="bg-custom-white text-left">
+          {rows?.map((row: any, index: number) => (
+            <tr
+              key={index}
+              className={classNames(
+                "border-b border-gray-200",
+                row.link ? "hover:bg-gray-50 cursor-pointer" : ""
+              )}
+              data-cy="object-table-row">
+              {columns?.map((column, index) => (
+                <td key={index} className="p-0">
+                  {row.link && (
+                    <Link
+                      className="whitespace-wrap px-2 py-1 text-xs text-gray-900 min-h-7 flex items-center"
+                      to={row.link}>
+                      <div>{row.values[column.name]}</div>
+                    </Link>
+                  )}
+
+                  {!row.link && (
+                    <div className="whitespace-wrap px-2 py-1 text-xs text-gray-900 min-h-7 flex items-center whitespace-pre">
+                      {row.values[column.name]}
+                    </div>
+                  )}
+                </td>
+              ))}
+
+              {onDelete && (
+                <td className="text-right w-8">
+                  <Button
+                    data-cy="delete"
+                    disabled={!auth?.permissions?.write}
+                    buttonType={BUTTON_TYPES.INVISIBLE}
+                    onClick={() => {
+                      onDelete(row);
+                    }}>
+                    <Icon icon="mdi:trash" className="text-red-500" />
+                  </Button>
+                </td>
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {!rows?.length && <NoDataFound message="No items" className="m-auto w-full" />}
+    </>
   );
 };
--- frontend/src/screens/no-data-found/no-data-found.tsx
+++ frontend/src/screens/no-data-found/no-data-found.tsx
@@ -11,7 +11,7 @@ type tNoData = {
 const DEFAULT_MESSAGE = "Sorry, no data found.";
 
 export default function NoDataFound(props: tNoData) {
-  const { className, message, icon } = props;
+  const { className = "", message, icon } = props;
 
   return (
     <div
@@ -22,7 +22,7 @@ export default function NoDataFound(props: tNoData) {
       <div className="bg-custom-white rounded-full p-4 text-custom-blue-green">
         {icon || <DocumentTextIcon className="h-8 w-8" />}
       </div>
-      <div className="mt-4">{message ?? DEFAULT_MESSAGE}</div>
+      <div>{message ?? DEFAULT_MESSAGE}</div>
     </div>
   );
 }
