d152a43e25013069905610676f388df38cd26fd1
pa-lem <pa-lem@hotmail.fr>
2024-02-12 12:53:29 +0100

    add retry in list view

--- frontend/src/screens/object-items/object-items-paginated.tsx
+++ frontend/src/screens/object-items/object-items-paginated.tsx
@@ -7,6 +7,7 @@ import { useContext, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import { BUTTON_TYPES, Button } from "../../components/buttons/button";
+import { Retry } from "../../components/buttons/retry";
 import { RoundedButton } from "../../components/buttons/rounded-button";
 import SlideOver from "../../components/display/slide-over";
 import ModalDelete from "../../components/modals/modal-delete";
@@ -118,7 +119,12 @@ export default function ObjectItems(props: any) {
     ${queryString}
   `;
 
-  const { loading, error, data = {}, refetch } = useQuery(query, { skip: !schemaData });
+  const {
+    loading,
+    error,
+    data = {},
+    refetch,
+  } = useQuery(query, { skip: !schemaData, notifyOnNetworkStatusChange: true });
 
   const result = data && schemaData?.kind ? data[schemaData?.kind] ?? {} : {};
 
@@ -171,6 +177,8 @@ export default function ObjectItems(props: any) {
     setIsLoading(false);
   };
 
+  const handleRefetch = () => refetch();
+
   if (error) {
     return <ErrorScreen message="Something went wrong when fetching list." />;
   }
@@ -185,6 +193,10 @@ export default function ObjectItems(props: any) {
             </h1>
 
             <div className="text-sm">{schemaData?.description}</div>
+
+            <div className="ml-2">
+              <Retry isLoading={loading} onClick={handleRefetch} />
+            </div>
           </div>
         )}
 
