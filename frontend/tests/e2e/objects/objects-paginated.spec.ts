bc2ac0b6af665b5d7abeded9b71413f0ab1e7942
pa-lem <pa-lem@hotmail.fr>
2024-02-14 14:17:16 +0100

    rename test folder

--- frontend/tests/e2e/objects/objects-paginated.spec.ts
+++ frontend/tests/e2e/objects/objects-paginated.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "@playwright/test";
-import { ACCOUNT_STATE_PATH } from "../../../constants";
+import { ACCOUNT_STATE_PATH } from "../../constants";
 
 test.describe("/objects/:objectname", () => {
   test.describe("when not logged in", () => {
