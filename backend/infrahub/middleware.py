5eacaf9250661cf4be9cc1b47b6cea000df5e009
Patrick Ogenstad <patrick@ogenstad.com>
2024-02-14 22:24:55 +0100

    Rework settings to avoid various type-hint issues

--- backend/infrahub/middleware.py
+++ backend/infrahub/middleware.py
@@ -1,21 +1,14 @@
-import os
 from typing import Any
 
-from fastapi.logger import logger
 from starlette.middleware.cors import CORSMiddleware
 from starlette.types import ASGIApp
 
-import infrahub.config as config
+from infrahub import config
 
 
 class InfrahubCORSMiddleware(CORSMiddleware):
     def __init__(self, app: ASGIApp, *args: Any, **kwargs: Any):
-        if not config.SETTINGS:
-            config_file_name = os.environ.get("INFRAHUB_CONFIG", "infrahub.toml")
-            config_file_path = os.path.abspath(config_file_name)
-            logger.info(f"Loading the configuration from {config_file_path}")
-            config.load_and_exit(config_file_path)
-
+        config.SETTINGS.initialize()
         kwargs["allow_origins"] = config.SETTINGS.api.cors_allow_origins
         kwargs["allow_credentials"] = config.SETTINGS.api.cors_allow_credentials
         kwargs["allow_methods"] = config.SETTINGS.api.cors_allow_methods
