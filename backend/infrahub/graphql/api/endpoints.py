96f964bd2490a278e91879b2c4824c95513aa906
Guillaume Mazoyer <guillaume@opsmill.com>
2024-02-14 11:29:48 +0100

    Add `/schema.graphql` to download GraphQL schema

--- backend/infrahub/graphql/api/endpoints.py
+++ backend/infrahub/graphql/api/endpoints.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from graphql import print_schema
+
+from infrahub.api.dependencies import get_branch_dep
+from infrahub.core import registry
+from infrahub.core.branch import Branch
 
 from .dependencies import build_graphql_app
 
@@ -10,3 +15,11 @@ router.add_route(path="/graphql", endpoint=graphql_app, methods=["GET", "POST", 
 router.add_route(path="/graphql/{branch_name:path}", endpoint=graphql_app, methods=["GET", "POST", "OPTIONS"])
 router.add_websocket_route(path="/graphql", endpoint=graphql_app)
 router.add_websocket_route(path="/graphql/{branch_name:str}", endpoint=graphql_app)
+
+
+@router.get("/schema.graphql")
+async def get_graphql_schema(branch: Branch = Depends(get_branch_dep)) -> str:
+    schema = registry.schema.get_schema_branch(name=branch.name)
+    gql_schema = schema.get_graphql_schema()
+
+    return print_schema(gql_schema)
