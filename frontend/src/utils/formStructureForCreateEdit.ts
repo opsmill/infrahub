533714ab02e37d73dac1594df1e159692ebd7aea
pa-lem <pa-lem@hotmail.fr>
2024-02-15 10:43:45 +0100

    fix parent pre fill for select 2 step

--- frontend/src/utils/formStructureForCreateEdit.ts
+++ frontend/src/utils/formStructureForCreateEdit.ts
@@ -14,6 +14,7 @@ import {
   getOptionsFromAttribute,
   getRelationshipOptions,
   getRelationshipValue,
+  getSelectParent,
 } from "./getSchemaObjectColumns";
 
 const getIsDisabled = ({ owner, user, isProtected, isReadOnly }: any) => {
@@ -110,6 +111,7 @@ const getFormStructureForCreateEdit = (
             isReadOnly: field.read_only,
           }),
           isInherited,
+          parent: isInherited && getSelectParent(row, field), // Used by select 2 steps
           field,
           schema,
         };
