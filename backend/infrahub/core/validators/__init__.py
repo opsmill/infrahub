5c1e4b3fbad76520bc26f34c3f6f1bb002fa60b9
Aaron McCarty <aaron@opsmill.com>
2024-02-26 16:49:39 -0800

    add AttributeUniquenessChecker to map too

--- backend/infrahub/core/validators/__init__.py
+++ backend/infrahub/core/validators/__init__.py
@@ -3,6 +3,7 @@ from typing import Dict, Optional, Type
 from .attribute.choices import AttributeChoicesChecker
 from .attribute.optional import AttributeOptionalChecker
 from .attribute.regex import AttributeRegexChecker
+from .attribute.unique import AttributeUniquenessChecker
 from .interface import ConstraintCheckerInterface
 from .relationship.optional import RelationshipOptionalChecker
 from .uniqueness.checker import UniquenessChecker
@@ -12,7 +13,7 @@ CONSTRAINT_VALIDATOR_MAP: Dict[str, Optional[Type[ConstraintCheckerInterface]]] 
     "attribute.enum.update": None,
     "attribute.min_length.update": None,
     "attribute.max_length.update": None,
-    "attribute.unique.update": None,
+    "attribute.unique.update": AttributeUniquenessChecker,
     "attribute.optional.update": AttributeOptionalChecker,
     "attribute.choices.update": AttributeChoicesChecker,
     "relationship.peer.update": None,
