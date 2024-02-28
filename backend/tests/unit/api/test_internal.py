9f67235b8a0f0a692dda755f5c84099a00f2af79
Bilal <bilal@opsmill.com>
2024-02-28 19:17:54 +0100

    added api test on search docs

---
+++ backend/tests/unit/api/test_internal.py
@@ -0,0 +1,44 @@
+import os
+
+import pytest
+
+from infrahub import config
+from tests.helpers.fixtures import get_fixtures_dir
+
+
+@pytest.fixture
+def override_search_index_path():
+    old_search_index_path = config.SETTINGS.main.docs_index_path
+    config.SETTINGS.main.docs_index_path = os.path.join(get_fixtures_dir(), "/docs/search-index.json")
+    yield
+    config.SETTINGS.main.docs_index_path = old_search_index_path
+
+
+@pytest.fixture
+def no_search_index_path():
+    old_search_index_path = config.SETTINGS.main.docs_index_path
+    config.SETTINGS.main.docs_index_path = os.path.join(get_fixtures_dir(), "/docs/no-index.json")
+    yield
+    config.SETTINGS.main.docs_index_path = old_search_index_path
+
+
+async def test_search_docs(client, override_search_index_path):
+    response = client.get("/api/search/docs?query=guid")
+
+    assert response.status_code == 200
+    assert response.json() is not None
+    response_json = response.json()
+    assert isinstance(response_json, list)
+    assert response_json[0]["title"] == "Guides"
+
+
+async def _test_no_search_docs(client, no_search_index_path):
+    response = client.get("/api/search/docs?query=guid")
+
+    assert response.status_code == 404
+    assert response.json() is not None
+    response_json = response.json()
+    assert response_json == {
+        "data": None,
+        "errors": [{"message": "documentation index not found", "extensions": {"code": 404}}],
+    }
