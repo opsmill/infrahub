db330be6c5a4ef27117f41e8a56aaeea93746476
Bilal <bilal@opsmill.com>
2024-03-06 11:01:25 +0100

    Improved classname merge utility function

--- frontend/src/utils/common.ts
+++ frontend/src/utils/common.ts
@@ -1,13 +1,10 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 import * as R from "ramda";
 import { IModelSchema } from "../state/atoms/schema.atom";
 
-export const classNames = (...classes: string[]) => {
-  // Replcaa tabs and spaces to have multiline in code, but one ligne in output
-  return classes
-    .filter(Boolean)
-    .join(" ")
-    .replaceAll(/(\t|\s)+/g, " ")
-    .trim();
+export const classNames = (...classes: ClassValue[]) => {
+  return twMerge(clsx(classes));
 };
 
 export const objectToString = (object: any) =>
