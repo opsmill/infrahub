26b9e6adf731fe1d8b0cac72336829937678dcdd
Bilal <bilal@opsmill.com>
2024-03-08 15:19:19 +0100

    Redesigned time selector to indicate current view time

--- frontend/src/components/TimeFrameSelector.tsx
+++ frontend/src/components/TimeFrameSelector.tsx
@@ -1,48 +1,69 @@
-import { DatePicker } from "./inputs/date-picker";
 import { useAtom } from "jotai/index";
 import { datetimeAtom } from "../state/atoms/time.atom";
-import { useEffect } from "react";
-import { formatISO, isEqual, isValid } from "date-fns";
-import { debounce } from "../utils/common";
-import { StringParam, useQueryParam } from "use-query-params";
+import { DateTimeParam, useQueryParam } from "use-query-params";
 import { QSP } from "../config/qsp";
+import { Icon } from "@iconify-icon/react";
+import DateTimePicker from "react-datepicker";
+import { classNames } from "../utils/common";
+import { format, setHours, setMinutes } from "date-fns";
+import { useEffect } from "react";
+import { Transition } from "@headlessui/react";
 
 export const TimeFrameSelector = () => {
   const [date, setDate] = useAtom(datetimeAtom);
-  const [qspDate, setQspDate] = useQueryParam(QSP.DATETIME, StringParam);
+  const [qspDate, setQspDate] = useQueryParam(QSP.DATETIME, DateTimeParam);
 
   useEffect(() => {
-    // Remove the date from the state
-    if (!qspDate || (qspDate && !isValid(new Date(qspDate)))) {
-      setDate(null);
-    }
+    if (date) return;
+    if (!qspDate) return;
 
-    if (qspDate) {
-      const newQspDate = new Date(qspDate);
+    setDate(qspDate);
+  }, []);
 
-      // Store the new QSP date only if it's not defined OR if it's different
-      if (!date || (date && !isEqual(newQspDate, date))) {
-        setDate(newQspDate);
-      }
-    }
-  }, [date, qspDate]);
-
-  const handleDateChange = (newDate: any) => {
-    if (newDate) {
-      setQspDate(formatISO(newDate));
-    } else {
-      // Undefined is needed to remove a parameter from the QSP
-      setQspDate(undefined);
-    }
+  const onChange = (newDate: Date) => {
+    setDate(newDate);
+    setQspDate(newDate);
   };
-  const debouncedHandleDateChange = debounce(handleDateChange);
 
-  const handleClickNow = () => {
-    // Undefined is needed to remove a parameter from the QSP
+  const reset = () => {
+    setDate(null);
     setQspDate(undefined);
   };
 
   return (
-    <DatePicker date={date} onChange={debouncedHandleDateChange} onClickNow={handleClickNow} />
+    <div
+      className={classNames(
+        "p-3 rounded-full inline-flex items-center bg-gray-800 text-white",
+        date && "bg-orange-600 py-1.5 shadow-md "
+      )}>
+      <Transition
+        show={!!date}
+        enter="ease-out duration-300"
+        enterFrom="w-0"
+        enterTo="w-40"
+        leave="ease-out duration-300"
+        leaveFrom="w-40"
+        leaveTo="w-0"
+        className="flex items-center truncate">
+        <Icon icon="mdi:close" className="cursor-pointer" onClick={reset} />
+        <div className="flex flex-col mx-3">
+          <span className="font-medium text-xs">Current view time:</span>
+          {date && <span className="text-sm">{format(date, "PP | H:mm")}</span>}
+        </div>
+      </Transition>
+
+      <DateTimePicker
+        customInput={<Icon icon="mdi:calendar-clock" className="text-2xl cursor-pointer" />}
+        wrapperClassName="w-6 h-6"
+        selected={date}
+        onChange={onChange}
+        showTimeSelect
+        timeIntervals={10}
+        calendarStartDay={1}
+        maxDate={new Date()}
+        minTime={setHours(setMinutes(new Date(), 0), 0)}
+        maxTime={new Date()}
+      />
+    </div>
   );
 };
