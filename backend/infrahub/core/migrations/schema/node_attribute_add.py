0b4af34c585e6257fb42b615c7680410909b5d42
Damien Garros <dgarros@gmail.com>
2024-03-08 09:47:56 +0100

    Add back args for query_init

--- backend/infrahub/core/migrations/schema/node_attribute_add.py
+++ backend/infrahub/core/migrations/schema/node_attribute_add.py
@@ -24,7 +24,7 @@ class NodeAttributeAddMigrationQuery01(Query):
 
         super().__init__(**kwargs)
 
-    async def query_init(self, db: InfrahubDatabase, **kwargs: Dict[str, Any]) -> None:
+    async def query_init(self, db: InfrahubDatabase, *args: Any, **kwargs: Dict[str, Any]) -> None:
         branch_filter, branch_params = self.branch.get_query_filter_path(at=self.at.to_string())
         self.params.update(branch_params)
 
