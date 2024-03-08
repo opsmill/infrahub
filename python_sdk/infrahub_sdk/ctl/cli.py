70f55f3563c276a8365a4a873d10f0649bc80bc8
Guillaume Mazoyer <guillaume@opsmill.com>
2024-03-08 11:51:38 +0100

    Export app from cli

--- python_sdk/infrahub_sdk/ctl/cli.py
+++ python_sdk/infrahub_sdk/ctl/cli.py
@@ -1,9 +1,11 @@
 import sys
 
 try:
-    from .cli_commands import app  # noqa: F401
+    from .cli_commands import app
 except ImportError as exc:
     sys.exit(
         f"Module {exc.name} is not available, install the 'ctl' extra of the infrahub-sdk package, `pip install 'infrahub-sdk[ctl]'` or enable the "
         "Poetry shell and run `poetry install --extras ctl`."
     )
+
+__all__ = ["app"]
