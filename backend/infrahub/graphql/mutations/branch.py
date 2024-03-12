15f5930bbfa1e6284786e2ae9e0bdf4b3fe7032d
Damien Garros <dgarros@gmail.com>
2024-03-12 08:55:47 +0100

    Update unit tests for branch rebase

--- backend/infrahub/graphql/mutations/branch.py
+++ backend/infrahub/graphql/mutations/branch.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, Optional
+from typing_extensions import Self
 
 import pydantic
 from graphene import Boolean, Field, InputObjectType, List, Mutation, String
