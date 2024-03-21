15ac3ad5d531575c8305a6c08dc0e7d7f868d26d
pa-lem <pa-lem@hotmail.fr>
2024-03-21 08:22:43 +0100

    remove unused files

--- frontend/src/graphql/variables/filtersVar.ts
+++
@@ -1,9 +0,0 @@
-import { makeVar } from "@apollo/client";
-
-export interface iComboBoxFilter {
-  name: string;
-  value: string;
-  display_label: string;
-}
-
-export const comboxBoxFilterVar = makeVar<iComboBoxFilter[]>([]);
