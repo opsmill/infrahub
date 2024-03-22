21a247fd5e2ec429d80b5436a29075827c8f90f1
Damien Garros <dgarros@gmail.com>
2024-03-22 17:39:14 +0100

    Allow to define profile and runtime when executing Cypher query

--- backend/infrahub/database/constants.py
+++ backend/infrahub/database/constants.py
@@ -4,3 +4,10 @@ from enum import Enum
 class DatabaseType(str, Enum):
     NEO4J = "neo4j"
     MEMGRAPH = "memgraph"
+
+
+class Neo4jRuntime(str, Enum):
+    DEFAULT = "default"
+    INTERPRETED = "interpreted"
+    SLOTTED = "slotted"
+    PIPELINED = "pipelined"
