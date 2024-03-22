332b18733e5899c412d48de3e73c8c254ad1a5c2
pa-lem <pa-lem@hotmail.fr>
2024-03-22 15:53:23 +0100

    update test

--- frontend/tests/e2e/objects/object-list-search.spec.ts
+++ frontend/tests/e2e/objects/object-list-search.spec.ts
@@ -2,8 +2,6 @@ import { expect, test } from "@playwright/test";
 
 const OBJECT_NAME = "atl1-core1";
 const SEARCH = "atl";
-const INITIAL_AMOUNT = "20";
-const SEARCHED_AMOUNT = "4";
 
 test.describe("Object list search", async () => {
   test("verify the search", async ({ page }) => {
@@ -11,15 +9,13 @@ test.describe("Object list search", async () => {
 
     await test.step("should access object list and verify the total amount of results", async () => {
       await expect(page.locator("tbody")).toContainText(OBJECT_NAME);
-      await page.getByText(INITIAL_AMOUNT, { exact: true }).click();
-      await expect(page.getByRole("main")).toContainText(INITIAL_AMOUNT);
+      await expect(page.getByRole("main")).toContainText("Showing 1 to 10 of 20 results");
     });
 
     await test.step("should search an object and verify the total amount of results", async () => {
-      await page.getByTestId("object-list-search-bar").click();
       await page.getByTestId("object-list-search-bar").fill(SEARCH);
       await expect(page.locator("tbody")).toContainText(OBJECT_NAME);
-      await expect(page.getByRole("main")).toContainText(SEARCHED_AMOUNT);
+      await expect(page.getByRole("main")).toContainText("Showing 1 to 4 of 4 results");
     });
   });
 });
