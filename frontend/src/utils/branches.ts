f509da97859f0e8090d65ac0d6f277a438f2f893
Bilal <bilal@opsmill.com>
2024-03-27 11:00:33 +0100

    export "branches to select options" code to reuse it

--- frontend/src/utils/branches.ts
+++ frontend/src/utils/branches.ts
@@ -5,3 +5,34 @@ export const findSelectedBranch = (branches: Branch[], branchName?: string | nul
 
   return branches.find(filter) ?? null;
 };
+
+export const branchesToSelectOptions = (branches: Branch[]) =>
+  branches
+    .map((branch) => ({
+      id: branch.id,
+      name: branch.name,
+      sync_with_git: branch.sync_with_git,
+      is_default: branch.is_default,
+      is_isolated: branch.is_isolated,
+      has_schema_changes: branch.has_schema_changes,
+      created_at: branch.created_at,
+    }))
+    .sort((branch1, branch2) => {
+      if (branch1.name === "main") {
+        return -1;
+      }
+
+      if (branch2.name === "main") {
+        return 1;
+      }
+
+      if (branch2.name === "main") {
+        return -1;
+      }
+
+      if (branch1.name > branch2.name) {
+        return 1;
+      }
+
+      return -1;
+    });
