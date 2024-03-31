664240e6ac2b927527dbcd4db96d20c9a2378de0
Damien Garros <dgarros@gmail.com>
2024-03-31 18:33:47 +0200

    Add unit test for generate_python_enum

--- backend/infrahub/core/enums.py
+++ backend/infrahub/core/enums.py
@@ -5,9 +5,9 @@ from typing import Any, List
 ENUM_NAME_REGEX = re.compile("[_a-zA-Z0-9]+")
 
 
-def generate_python_enum(name: str, options: List[Any]) -> enum.Enum:
+def generate_python_enum(name: str, options: List[Any]) -> enum.EnumType:
     main_attrs = {}
     for option in options:
         enum_name = "_".join(re.findall(ENUM_NAME_REGEX, option)).upper()
         main_attrs[enum_name] = option
-    return enum.Enum(name, main_attrs)
+    return enum.Enum(name, main_attrs)  # type: ignore[return-value]
