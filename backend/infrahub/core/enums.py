a82f92d74e6b6f14109c2173c8e95a2bcd410ebd
Damien Garros <dgarros@gmail.com>
2024-04-03 06:08:56 +0200

    Change return type for generate_python_enum

--- backend/infrahub/core/enums.py
+++ backend/infrahub/core/enums.py
@@ -1,11 +1,11 @@
 import enum
 import re
-from typing import Any, List
+from typing import Any, List, Type
 
 ENUM_NAME_REGEX = re.compile("[_a-zA-Z0-9]+")
 
 
-def generate_python_enum(name: str, options: List[Any]) -> enum.EnumType:
+def generate_python_enum(name: str, options: List[Any]) -> Type[enum.Enum]:
     main_attrs = {}
     for option in options:
         enum_name = "_".join(re.findall(ENUM_NAME_REGEX, option)).upper()
