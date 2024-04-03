5827cb57aafd3a1f46c2634fb57ca795144779e8
Damien Garros <dgarros@gmail.com>
2024-04-03 06:16:20 +0200

    Leverage TestInfrahubApp in TestObjectStore

--- python_sdk/tests/integration/test_object_store.py
+++ python_sdk/tests/integration/test_object_store.py
@@ -1,9 +1,7 @@
-import pytest
-from infrahub.server import app
-
 from infrahub_sdk import Config, InfrahubClient
 
-from .conftest import InfrahubTestClient
+from tests.helpers.test_app import TestInfrahubApp
+
 
 FILE_CONTENT_01 = """
     any content
@@ -11,16 +9,7 @@ FILE_CONTENT_01 = """
     """
 
 
-class TestObjectStore:
-    @pytest.fixture(scope="class")
-    async def test_client(self):
-        return InfrahubTestClient(app)
-
-    @pytest.fixture
-    async def client(self, test_client):
-        config = Config(username="admin", password="infrahub", requester=test_client.async_request)
-        return await InfrahubClient.init(config=config)
-
+class TestObjectStore(TestInfrahubApp):
     async def test_upload_and_get(self, client: InfrahubClient):
         response = await client.object_store.upload(content=FILE_CONTENT_01)
 
