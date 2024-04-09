6a0ffb0ada426e86f5feb8d3e6075536622c247f
Bilal <bilal@opsmill.com>
2024-04-09 15:16:20 +0200

    Add button to copy object ID to clipboard

--- frontend/src/screens/object-item-details/object-attribute-row.tsx
+++ frontend/src/screens/object-item-details/object-attribute-row.tsx
@@ -1,14 +1,23 @@
 import { ReactElement } from "react";
+import { CopyToClipboard } from "../../components/buttons/copy-to-clipboard";
 
 type ObjectAttributeRowProps = {
   name: string;
   value: string | ReactElement;
+  enableCopyToClipboard?: boolean;
 };
-export const ObjectAttributeRow = ({ name, value }: ObjectAttributeRowProps) => {
+export const ObjectAttributeRow = ({
+  name,
+  value,
+  enableCopyToClipboard,
+}: ObjectAttributeRowProps) => {
   return (
     <div className="p-2 grid grid-cols-3 gap-4 text-xs">
       <dt className="font-medium text-gray-500 flex items-center h-8">{name}</dt>
-      <dd className="flex items-center gap-2">{value}</dd>
+      <dd className="flex items-center gap-2">
+        {value}
+        {enableCopyToClipboard && <CopyToClipboard text={value.toString()} />}
+      </dd>
     </div>
   );
 };
