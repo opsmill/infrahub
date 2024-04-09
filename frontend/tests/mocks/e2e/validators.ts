1dcdd821a96347be63e714e0e2b9dd690a393cea
Benoit Kohler <BeArchiTek@users.noreply.github.com>
2024-04-09 16:29:05 +0200

    Removing CoreOrganization references (#2611)
    * updating demo base schema and edge script
    
    * Remove some comments
    
    * Python formatting
    
    * Fix test circuit link
    
    * Change transit ref to upstream in e2e tests
    
    * Fix arelion policy check
    
    * fix issue with the BGPSession
    
    * add real connection between edge on the same site, not just decription
    
    * updating core organization related tests
    
    * replace Organization by Tenant
    
    * checking only the row and not the whole tbody
    
    * Label is not present in the new organization model so removing it
    
    ---------
    
    Co-authored-by: Guillaume Mazoyer <guillaume@opsmill.com>

--- frontend/tests/mocks/e2e/validators.ts
+++ frontend/tests/mocks/e2e/validators.ts
@@ -2,6 +2,6 @@ export const PROPOSED_CHANGES_NAME_FAIL = "New changes with failed validators";
 
 export const PROPOSED_CHANGES_NAME_WARNING = "New changes with warning";
 
-export const PROPOSED_CHANGES_BRANCH_CLEAN = "ord1-add-transit";
+export const PROPOSED_CHANGES_BRANCH_CLEAN = "ord1-add-upstream";
 
 export const PROPOSED_CHANGES_BRANCH_CONFLICT = "platform-conflict";
