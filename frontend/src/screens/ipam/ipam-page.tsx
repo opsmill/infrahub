6a2e3d2fb37fff4aa3405c5e3e78f58bc538e8ee
pa-lem <pa-lem@hotmail.fr>
2024-04-11 13:38:18 +0200

    use cards

--- frontend/src/screens/ipam/ipam-page.tsx
+++ frontend/src/screens/ipam/ipam-page.tsx
@@ -1,24 +1,18 @@
+import { Card } from "../../components/ui/card";
 import Content from "../layout/content";
 import IpamTree from "./ipam-tree";
 
 export default function IpamPage() {
   return (
     <>
-      <Content.Title
-        title={
-          <div className="flex items-center">
-            <h1 className="mr-2 truncate">IP Address Manager</h1>
-          </div>
-        }
-      />
-
+      <Content.Title title="IP Address Manager" />
       <Content>
-        <div className="flex items-stretch min-h-full">
-          <div className="m-2 p-2 bg-custom-white rounded-md">
+        <div className="p-2 flex flex-wrap gap-2 items-stretch min-h-full">
+          <Card>
             <IpamTree />
-          </div>
+          </Card>
 
-          <div className="flex-1 m-2 p-2 bg-custom-white rounded-md">Table</div>
+          <Card className="flex-grow">Table</Card>
         </div>
       </Content>
     </>
