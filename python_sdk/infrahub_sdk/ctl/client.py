ca765edf0783d0448b30b2b1af0b64e96ea293dd
Patrick Ogenstad <patrick@ogenstad.com>
2024-04-11 13:58:32 +0200

    Slignt cleanup of client config assignment in CTL

--- python_sdk/infrahub_sdk/ctl/client.py
+++ python_sdk/infrahub_sdk/ctl/client.py
@@ -12,7 +12,43 @@ async def initialize_client(
     max_concurrent_execution: Optional[int] = None,
     retry_on_failure: Optional[bool] = None,
 ) -> InfrahubClient:
-    client_config = {"insert_tracker": True, "identifier": identifier}
+    return InfrahubClient(
+        config=_define_config(
+            branch=branch,
+            identifier=identifier,
+            timeout=timeout,
+            max_concurrent_execution=max_concurrent_execution,
+            retry_on_failure=retry_on_failure,
+        )
+    )
+
+
+def initialize_client_sync(
+    branch: Optional[str] = None,
+    identifier: Optional[str] = None,
+    timeout: Optional[int] = None,
+    max_concurrent_execution: Optional[int] = None,
+    retry_on_failure: Optional[bool] = None,
+) -> InfrahubClientSync:
+    return InfrahubClientSync(
+        config=_define_config(
+            branch=branch,
+            identifier=identifier,
+            timeout=timeout,
+            max_concurrent_execution=max_concurrent_execution,
+            retry_on_failure=retry_on_failure,
+        )
+    )
+
+
+def _define_config(
+    branch: Optional[str] = None,
+    identifier: Optional[str] = None,
+    timeout: Optional[int] = None,
+    max_concurrent_execution: Optional[int] = None,
+    retry_on_failure: Optional[bool] = None,
+) -> Config:
+    client_config = {"address": config.SETTINGS.active.server_address, "insert_tracker": True, "identifier": identifier}
 
     if config.SETTINGS.active.api_token:
         client_config["api_token"] = config.SETTINGS.active.api_token
@@ -29,31 +65,4 @@ async def initialize_client(
     if branch:
         client_config["default_branch"] = branch
 
-    client = await InfrahubClient.init(
-        address=config.SETTINGS.active.server_address,
-        config=Config(**client_config),
-    )
-
-    return client
-
-
-def initialize_client_sync(
-    branch: Optional[str] = None, identifier: Optional[str] = None, timeout: Optional[int] = None
-) -> InfrahubClientSync:
-    client_config = {"insert_tracker": True, "identifier": identifier}
-
-    if config.SETTINGS.active.api_token:
-        client_config["api_token"] = config.SETTINGS.active.api_token
-
-    if timeout:
-        client_config["timeout"] = timeout
-
-    if branch:
-        client_config["default_branch"] = branch
-
-    client = InfrahubClientSync.init(
-        address=config.SETTINGS.active.server_address,
-        config=Config(**client_config),
-    )
-
-    return client
+    return Config(**client_config)
