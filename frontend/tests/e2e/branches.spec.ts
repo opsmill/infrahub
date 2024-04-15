ae88f932390d64340e0c882a0640650c12bdfd10
Fatih Acar <fatih@opsmill.com>
2024-04-15 09:24:15 +0200

    fix(tests, e2e): wait for page load
    Signed-off-by: Fatih Acar <fatih@opsmill.com>

--- frontend/tests/e2e/branches.spec.ts
+++ frontend/tests/e2e/branches.spec.ts
@@ -53,8 +53,9 @@ test.describe("Branches creation and deletion", () => {
     test("should delete a non-selected branch and remain on the current branch", async ({
       page,
     }) => {
-      await page.goto("/");
+      await page.goto("/", { waitUntil: "networkidle" });
       await createBranch(page, "test456");
+      await page.waitForURL("/?branch=test456"); // createBranch redirects to this URL, we must wait for it to avoid ERR_ABORTED errors in the next goto
       await page.goto("/branches/test456?branch=test123");
 
       await page.getByRole("button", { name: "Delete" }).click();
