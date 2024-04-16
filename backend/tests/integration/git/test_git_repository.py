115e032c50c400fa2b349956b3efbcfe784b3cb4
Benoit Kohler <benoit@opsmill.com>
2024-04-16 14:02:54 +0200

    update integration tests

--- backend/tests/integration/git/test_git_repository.py
+++ backend/tests/integration/git/test_git_repository.py
@@ -25,19 +25,17 @@ from tests.helpers.test_client import InfrahubTestClient
 async def load_infrastructure_schema(db: InfrahubDatabase):
     base_dir = get_models_dir() + "/base"
 
-    infra_schema = {}
+    default_branch_name = registry.default_branch
+    branch_schema = registry.schema.get_schema_branch(name=default_branch_name)
+    tmp_schema = branch_schema.duplicate()
+
     for file_name in os.listdir(base_dir):
         file_path = os.path.join(base_dir, file_name)
 
         if file_path.endswith((".yml", ".yaml")):
             schema_txt = Path(file_path).read_text()
             loaded_schema = yaml.safe_load(schema_txt)
-            infra_schema.update(loaded_schema)
-
-    default_branch_name = registry.default_branch
-    branch_schema = registry.schema.get_schema_branch(name=default_branch_name)
-    tmp_schema = branch_schema.duplicate()
-    tmp_schema.load_schema(schema=SchemaRoot(**infra_schema))
+            tmp_schema.load_schema(schema=SchemaRoot(**loaded_schema))
     tmp_schema.process()
 
     await registry.schema.update_schema_branch(schema=tmp_schema, db=db, branch=default_branch_name, update_db=True)
