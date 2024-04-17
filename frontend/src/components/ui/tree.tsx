59e875dcf873e0224758995e59344f098903ba40
Bilal <bilal@opsmill.com>
2024-04-17 10:46:12 +0200

    improvement tree default style

--- frontend/src/components/ui/tree.tsx
+++ frontend/src/components/ui/tree.tsx
@@ -1,30 +1,58 @@
 import React from "react";
 import TreeViewPrimitive, { ITreeViewProps, INodeRendererProps } from "react-accessible-treeview";
 import { Icon } from "@iconify-icon/react";
+import { classNames } from "../../utils/common";
 
 export interface TreeProps extends Omit<ITreeViewProps, "nodeRenderer"> {
   nodeRenderer?: (props: INodeRendererProps) => React.ReactNode;
 }
 
 export const Tree = (props: TreeProps) => {
-  return <TreeViewPrimitive {...props} nodeRenderer={props.nodeRenderer ?? TreeItem} />;
+  return (
+    <TreeViewPrimitive
+      {...props}
+      nodeRenderer={props.nodeRenderer ?? TreeItem}
+      className={classNames(
+        "border rounded-md p-2",
+        "[&_li:focus]:rounded-md [&_li:focus]:outline-none [&_li:focus]:ring-2 [&_li:focus]:ring-custom-blue-500 [&_li:focus]:ring-offset-2"
+      )}
+    />
+  );
 };
 
 export const TreeItem = ({
   element,
   getNodeProps,
   handleExpand,
+  handleSelect,
   isBranch,
+  isSelected,
   isExpanded,
   level,
 }: INodeRendererProps) => {
   return (
     <div
-      {...getNodeProps({ onClick: handleExpand })}
-      style={{ marginLeft: (level - 1) * 24 }}
-      className="flex items-center gap-2 hover:bg-gray-100 cursor-pointer px-2 py-1.5 rounded-md text-gray-600">
-      {isBranch && <Icon icon={isExpanded ? "mdi:chevron-down" : "mdi:chevron-right"} />}
-      {element.name}
+      {...getNodeProps({
+        onClick: handleSelect,
+      })}
+      style={{ marginLeft: (level - 1) * 20 }}
+      className={classNames(
+        "flex items-center",
+        "text-gray-600 mix-blend-multiply",
+        "h-8 rounded-md cursor-pointer",
+        "focus-visible:outline-none focus:ring-2 focus:ring-custom-blue-500 focus:ring-offset-2",
+        isSelected ? "bg-custom-blue-500 text-custom-white" : "hover:bg-custom-blue-400"
+      )}>
+      {isBranch ? (
+        <Icon
+          icon={isExpanded ? "mdi:chevron-down" : "mdi:chevron-right"}
+          onClick={handleExpand}
+          className="px-1.5"
+        />
+      ) : (
+        <div className="px-1.5" />
+      )}
+      <span>{element.name}</span>
     </div>
   );
 };
