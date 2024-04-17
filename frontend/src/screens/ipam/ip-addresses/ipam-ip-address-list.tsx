a32a3c723256257078aea1a44c7737e038f3d2a7
Paul Leménager <pa-lem@hotmail.fr>
2024-04-17 13:34:52 +0200

    Updates routes and links (#2947)
    * add routes for ipam components
    
    * rename with prefix
    
    * add graphql queries for namespaces, prefixes and ip address
    
    * update routes
    
    * add prefiexes navigation
    
    * update components, views and routes
    
    * fix routes and tabs
    
    * rename ip address main component

--- frontend/src/screens/ipam/ip-addresses/ipam-ip-address-list.tsx
+++ frontend/src/screens/ipam/ip-addresses/ipam-ip-address-list.tsx
@@ -1,41 +1,38 @@
 import { useParams } from "react-router-dom";
 import { StringParam, useQueryParam } from "use-query-params";
 import { Table } from "../../../components/table/table";
-import { IPAM_PREFIX_OBJECT } from "../../../config/constants";
-import { GET_PREFIXES } from "../../../graphql/queries/ipam/prefixes";
+import { IPAM_IP_ADDRESS_OBJECT } from "../../../config/constants";
+import { GET_IP_ADDRESSES } from "../../../graphql/queries/ipam/ip-address";
 import useQuery from "../../../hooks/useQuery";
 import { constructPath } from "../../../utils/fetch";
 import ErrorScreen from "../../error-screen/error-screen";
 import LoadingScreen from "../../loading-screen/loading-screen";
 import { IPAM_QSP } from "../constants";
 
-export default function IpamIPPrefixesSummaryList() {
+export default function IpamIPAddressesList() {
   const { prefix } = useParams();
   const [qspTab] = useQueryParam(IPAM_QSP, StringParam);
 
   const constructLink = (data) => {
-    switch (data.__typename) {
-      case IPAM_PREFIX_OBJECT: {
-        return constructPath(`/ipam/prefixes/${encodeURIComponent(data?.prefix?.value)}`, [
-          { name: IPAM_QSP, value: qspTab },
-        ]);
-      }
-      default: {
-        return constructPath(`/ipam/ip_address/${encodeURIComponent(data?.prefix?.value)}`, [
-          { name: IPAM_QSP, value: qspTab },
-        ]);
-      }
+    if (prefix) {
+      return constructPath(
+        `/ipam/prefixes/${encodeURIComponent(prefix)}/${encodeURIComponent(data?.address?.value)}`,
+        [{ name: IPAM_QSP, value: qspTab }]
+      );
     }
+
+    return constructPath(`/ipam/ip-addresses/${encodeURIComponent(data?.address?.value)}`, [
+      { name: IPAM_QSP, value: qspTab },
+    ]);
   };
 
-  const { loading, error, data } = useQuery(GET_PREFIXES, { variables: { prefix: prefix } });
+  const { loading, error, data } = useQuery(GET_IP_ADDRESSES, { variables: { prefix: prefix } });
 
   const rows =
     data &&
-    data[IPAM_PREFIX_OBJECT]?.edges.map((edge) => ({
+    data[IPAM_IP_ADDRESS_OBJECT]?.edges.map((edge) => ({
       values: {
         ...edge?.node,
-        children_count: edge?.node?.children?.edges?.length,
       },
       link: constructLink(edge?.node),
     }));
@@ -45,10 +42,6 @@ export default function IpamIPPrefixesSummaryList() {
       name: "display_label",
       label: "Name",
     },
-    {
-      name: "children_count",
-      label: "Children",
-    },
   ];
 
   if (error) {
@@ -57,7 +50,7 @@ export default function IpamIPPrefixesSummaryList() {
 
   return (
     <div>
-      <div>Prefixes</div>
+      <div>IP Addresses</div>
 
       {loading && <LoadingScreen hideText />}
 
