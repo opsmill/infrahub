4fcac1e9de2eb678e414b9c7b4219f4c49af3028
Bilal <bilal@opsmill.com>
2024-04-19 14:13:55 +0200

    style and formatting on Pie chart usage

--- frontend/src/screens/ipam/styled.tsx
+++ frontend/src/screens/ipam/styled.tsx
@@ -7,21 +7,25 @@ type PrefixUsageChartProps = {
 
 export function PrefixUsageChart({ usagePercentage }: PrefixUsageChartProps) {
   const data = [
-    { name: "Used", usagePercentage: usagePercentage, fill: "#E5484D" },
-    { name: "Available", usagePercentage: 100 - usagePercentage, fill: "#5BB98C" },
+    { name: "available", usagePercentage: 100 - usagePercentage, fill: "#5BB98C" },
+    { name: "used", usagePercentage: usagePercentage, fill: "#E5484D" },
   ];
 
   return (
     <CardWithBorder className="max-w-xs">
       <header className="bg-neutral-100 p-2 font-semibold text-sm">IP Availability</header>
-      <ResponsiveContainer height={207}>
+      <ResponsiveContainer height={208}>
         <PieChart title="usage">
           <Pie data={data} nameKey="name" dataKey="usagePercentage" />
-          <ChartTooltip />
+          <ChartTooltip formatter={(value) => <span className="text-sm">{value}%</span>} />
           <Legend
             iconType="circle"
             iconSize={8}
-            formatter={(value) => <span className="text-gray-600 text-sm">{value}</span>}
+            formatter={(value, { payload }) => (
+              <span className="text-gray-600 text-sm">
+                {payload && `${payload.value}%`} {value}
+              </span>
+            )}
           />
         </PieChart>
       </ResponsiveContainer>
