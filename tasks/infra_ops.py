cfd21669973c77ad277bdc568d16ff2e423fd3c3
Damien Garros <dgarros@gmail.com>
2024-04-19 05:29:22 +0200

    Fix load-infra-schema for demo environment

--- tasks/infra_ops.py
+++ tasks/infra_ops.py
@@ -17,11 +17,13 @@ def load_infrastructure_data(context: Context, database: str, namespace: str) ->
         execute_command(context=context, command=command)
 
 
-def load_infrastructure_schema(context: Context, database: str, namespace: str, add_wait: bool = True) -> None:
+def load_infrastructure_schema(
+    context: Context, database: str, namespace: str, add_wait: bool = True, target: str = "models/base"
+) -> None:
     with context.cd(ESCAPED_REPO_PATH):
         compose_files_cmd = build_compose_files_cmd(database=database)
         base_cmd = f"{get_env_vars(context, namespace=namespace)} docker compose {compose_files_cmd} -p {BUILD_NAME}"
-        command = f"{base_cmd} run infrahub-git infrahubctl schema load models/base"
+        command = f"{base_cmd} run infrahub-git infrahubctl schema load {target}"
         if add_wait:
             command += " --wait 30"
         execute_command(context=context, command=command)
