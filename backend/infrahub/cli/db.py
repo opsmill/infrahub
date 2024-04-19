a37bbd898738d10c9c702bf731158ecdc9e713e3
Damien Garros <dgarros@gmail.com>
2024-04-19 09:16:53 +0200

    Add command infrahub db update-core-schema

--- backend/infrahub/cli/db.py
+++ backend/infrahub/cli/db.py
@@ -5,20 +5,28 @@ from typing import Optional
 
 import typer
 from infrahub_sdk.async_typer import AsyncTyper
+from rich import print as rprint
 from rich.console import Console
 from rich.logging import RichHandler
 from rich.table import Table
 
 from infrahub import config
+from infrahub.core import registry
 from infrahub.core.graph import GRAPH_VERSION
 from infrahub.core.graph.constraints import ConstraintManagerBase, ConstraintManagerMemgraph, ConstraintManagerNeo4j
 from infrahub.core.graph.index import node_indexes, rel_indexes
 from infrahub.core.graph.schema import GRAPH_SCHEMA
-from infrahub.core.initialization import first_time_initialization, get_root_node, initialization
+from infrahub.core.initialization import first_time_initialization, get_root_node, initialization, initialize_registry
 from infrahub.core.migrations.graph import get_graph_migrations
+from infrahub.core.migrations.schema.runner import schema_migrations_runner
+from infrahub.core.schema import SchemaRoot, core_models, internal_schema
+from infrahub.core.schema_manager import SchemaManager
 from infrahub.core.utils import delete_all_nodes
+from infrahub.core.validators.checker import schema_validators_checker
 from infrahub.database import DatabaseType, InfrahubDatabase, get_db
 from infrahub.log import get_logger
+from infrahub.services import InfrahubServices
+from infrahub.services.adapters.message_bus.local import BusSimulator
 
 app = AsyncTyper()
 
@@ -112,15 +120,16 @@ async def migrate(
 
     dbdriver = InfrahubDatabase(driver=await get_db(retry=1))
     async with dbdriver.start_session() as db:
-        log.info("Checking current state of the Database")
+        rprint("Checking current state of the Database")
 
+        await initialize_registry(db=db)
         root_node = await get_root_node(db=db)
         migrations = await get_graph_migrations(root=root_node)
 
         if not migrations:
-            log.info(f"Database up-to-date (v{root_node.graph_version}), no migration to execute.")
+            rprint(f"Database up-to-date (v{root_node.graph_version}), no migration to execute.")
         else:
-            log.info(
+            rprint(
                 f"Database needs to be updated (v{root_node.graph_version} -> v{GRAPH_VERSION}), {len(migrations)} migrations pending"
             )
 
@@ -133,18 +142,128 @@ async def migrate(
                 if execution_result.success:
                     validation_result = await migration.validate_migration(db=db)
                     if validation_result.success:
-                        log.info(f"Migration: {migration.name} SUCCESS")
+                        rprint(f"Migration: {migration.name} [green]SUCCESS[/green]")
+                        root_node.graph_version = migration.minimum_version + 1
+                        await root_node.save(db=db)
 
                 if not execution_result.success or validation_result and not validation_result.success:
-                    log.info(f"Migration: {migration.name} FAILED")
+                    rprint(f"Migration: {migration.name} [bold red]FAILED[/bold red]")
                     for error in execution_result.errors:
-                        log.warning(f"  {error}")
+                        rprint(f"  {error}")
                     if validation_result and not validation_result.success:
                         for error in validation_result.errors:
-                            log.warning(f"  {error}")
+                            rprint(f"  {error}")
                     break
 
     await dbdriver.close()
+
+
+@app.command()
+async def update_core_schema(  # pylint: disable=too-many-statements
+    debug: bool = typer.Option(False, help="Enable advanced logging and troubleshooting"),
+    config_file: str = typer.Argument("infrahub.toml", envvar="INFRAHUB_CONFIG"),
+) -> None:
+    """Check the current format of the internal graph and apply the necessary migrations"""
+    logging.getLogger("infrahub").setLevel(logging.WARNING)
+    logging.getLogger("neo4j").setLevel(logging.ERROR)
+    config.load_and_exit(config_file_name=config_file)
+
+    dbdriver = InfrahubDatabase(driver=await get_db(retry=1))
+
+    error_badge = "[bold red]ERROR[/bold red]"
+
+    async with dbdriver.start_session() as db:
+        # ----------------------------------------------------------
+        # Initialize Schema and Registry
+        # ----------------------------------------------------------
+        service = InfrahubServices(database=db, message_bus=BusSimulator(database=db))
+        await initialize_registry(db=db)
+
+        default_branch = registry.get_branch_from_registry(branch=registry.default_branch)
+
+        registry.schema = SchemaManager()
+        schema = SchemaRoot(**internal_schema)
+        registry.schema.register_schema(schema=schema)
+
+        # ----------------------------------------------------------
+        # Load Current Schema from the database
+        # ----------------------------------------------------------
+        schema_default_branch = await registry.schema.load_schema_from_db(db=db, branch=default_branch)
+        registry.schema.set_schema_branch(name=default_branch.name, schema=schema_default_branch)
+        branch_schema = registry.schema.get_schema_branch(name=registry.default_branch)
+
+        candidate_schema = branch_schema.duplicate()
+        candidate_schema.load_schema(schema=SchemaRoot(**internal_schema))
+        candidate_schema.load_schema(schema=SchemaRoot(**core_models))
+        candidate_schema.process()
+
+        result = branch_schema.validate_update(other=candidate_schema)
+        if result.errors:
+            rprint(f"{error_badge} | Unable to update the schema, due to failed validations")
+            for error in result.errors:
+                rprint(error.to_string())
+            raise typer.Exit(1)
+
+        if not result.diff.all:
+            rprint("Core Schema Up to date, nothing to update")
+            raise typer.Exit(0)
+
+        rprint("Core Schema has diff, will need to be updated")
+        if debug:
+            result.diff.print()
+
+        # ----------------------------------------------------------
+        # Validate if the new schema is valid with the content of the database
+        # ----------------------------------------------------------
+        error_messages, _ = await schema_validators_checker(
+            branch=default_branch, schema=candidate_schema, constraints=result.constraints, service=service
+        )
+        if error_messages:
+            rprint(f"{error_badge} | Unable to update the schema, due to failed validations")
+            for message in error_messages:
+                rprint(message)
+            raise typer.Exit(1)
+
+        # ----------------------------------------------------------
+        # Update the schema
+        # ----------------------------------------------------------
+        origin_schema = branch_schema.duplicate()
+
+        # Update the internal schema
+        schema_default_branch.load_schema(schema=SchemaRoot(**internal_schema))
+        schema_default_branch.process()
+        registry.schema.set_schema_branch(name=default_branch.name, schema=schema_default_branch)
+
+        async with db.start_transaction() as dbt:
+            await registry.schema.update_schema_branch(
+                schema=candidate_schema,
+                db=dbt,
+                branch=default_branch.name,
+                diff=result.diff,
+                limit=result.diff.all,
+                update_db=True,
+            )
+            default_branch.update_schema_hash()
+            rprint("The Core Schema has been updated")
+            if debug:
+                rprint(f"New schema hash: {default_branch.active_schema_hash.main}")
+            await default_branch.save(db=dbt)
+
+        # ----------------------------------------------------------
+        # Run the migrations
+        # ----------------------------------------------------------
+        error_messages = await schema_migrations_runner(
+            branch=default_branch,
+            new_schema=candidate_schema,
+            previous_schema=origin_schema,
+            migrations=result.migrations,
+            service=service,
+        )
+        if error_messages:
+            rprint(f"{error_badge} | Some error(s) happened while running the schema migrations")
+            for message in error_messages:
+                rprint(message)
+            raise typer.Exit(1)
 
 
 @app.command()
