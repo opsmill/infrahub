b9a91ccc4195a9a818b846585a9c46d26fa9a22a
Damien Garros <dgarros@gmail.com>
2024-04-19 09:50:53 +0200

    Remove commented code

--- backend/tests/unit/core/migrations/graph/test_003.py
+++ backend/tests/unit/core/migrations/graph/test_003.py
@@ -62,28 +62,3 @@ async def test_migration_003(
 
     nbr_rels_after = await count_relationships(db=db)
     assert nbr_rels_after == nbr_rels_before + 1
-
-
-# async def test_migration_001_validate(db: InfrahubDatabase, reset_registry, delete_all_nodes_in_db):
-#     root_id = str(UUIDT().new())
-
-#     query_init_root = """
-#     CREATE (root:Root { uuid: "%(uuid)s", graph_version: 0, default_branch: "main" })
-#     RETURN root
-#     """ % {"uuid": root_id}
-#     await db.execute_query(query=query_init_root)
-
-#     migration = Migration001()
-#     execution_result = await migration.execute(db=db)
-#     assert not execution_result.errors
-
-#     query_init_root = """
-#     MATCH (root:Root { uuid: "%(uuid)s" })
-#     SET root.graph_version = 0
-#     RETURN root
-#     """ % {"uuid": root_id}
-#     await db.execute_query(query=query_init_root)
-
-#     validation_result = await migration.validate_migration(db=db)
-#     assert not validation_result.success
-#     assert "The version of the graph should be 1, found 0 instead." in validation_result.errors
