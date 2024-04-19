355a7a71233f2de3ed6443ac5db76e4ea6d0a999
Bilal <bilal@opsmill.com>
2024-04-19 15:59:39 +0200

    add component PieChart

--- frontend/src/screens/ipam/styled.tsx
+++ frontend/src/screens/ipam/styled.tsx
@@ -1,5 +1,5 @@
 import { CardWithBorder } from "../../components/ui/card";
-import { Legend, Pie, PieChart, ResponsiveContainer, Tooltip as ChartTooltip } from "recharts";
+import { PieChart } from "../../components/stats/pie-chart";
 
 type PrefixUsageChartProps = {
   usagePercentage: number;
@@ -7,29 +7,19 @@ type PrefixUsageChartProps = {
 
 export function PrefixUsageChart({ usagePercentage }: PrefixUsageChartProps) {
   const data = [
-    { name: "available", usagePercentage: 100 - usagePercentage, fill: "#5BB98C" },
-    { name: "used", usagePercentage: usagePercentage, fill: "#E5484D" },
+    { name: "available", value: 100 - usagePercentage, fill: "#5BB98C" },
+    { name: "used", value: usagePercentage, fill: "#E5484D" },
   ];
 
   return (
     <CardWithBorder className="max-w-xs">
       <CardWithBorder.Title>IP Availability</CardWithBorder.Title>
 
-      <ResponsiveContainer height={208}>
-        <PieChart title="usage">
-          <Pie data={data} nameKey="name" dataKey="usagePercentage" />
-          <ChartTooltip formatter={(value) => <span className="text-sm">{value}%</span>} />
-          <Legend
-            iconType="circle"
-            iconSize={8}
-            formatter={(value, { payload }) => (
-              <span className="text-gray-600 text-sm">
-                {payload && `${payload.value}%`} {value}
-              </span>
-            )}
-          />
-        </PieChart>
-      </ResponsiveContainer>
+      <PieChart
+        data={data}
+        tooltipFormatter={(value) => <span className="text-sm">{value}%</span>}
+        legendFormatter={(name, value) => `${value}% ${name}`}
+      />
     </CardWithBorder>
   );
 }
