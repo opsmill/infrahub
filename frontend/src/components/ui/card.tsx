047126b3a5b3f182c54d2b841e44d8b43a219c28
Bilal <bilal@opsmill.com>
2024-04-19 13:55:12 +0200

    add CardWithBorder component

--- frontend/src/components/ui/card.tsx
+++ frontend/src/components/ui/card.tsx
@@ -5,8 +5,18 @@ export const Card = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => (
     <div
       ref={ref}
-      className={classNames("bg-custom-white rounded-lg border border-gray-300 p-4", className)}
+      className={classNames("bg-custom-white rounded-lg border border-gray-300 p-3", className)}
       {...props}
     />
   )
+);
+
+export const CardWithBorder = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+  ({ children, className }) => {
+    return (
+      <div className={classNames("border rounded-lg", className)}>
+        <div className="border rounded-md m-3 overflow-hidden">{children}</div>
+      </div>
+    );
+  }
 );
