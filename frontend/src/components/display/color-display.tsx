97fe0f969f8e54f773c00716d3de997ac3a02765
Bilal <bilal@opsmill.com>
2024-04-20 13:50:00 +0200

    bg color width matches content size on color display

--- frontend/src/components/display/color-display.tsx
+++ frontend/src/components/display/color-display.tsx
@@ -10,7 +10,7 @@ export const ColorDisplay = (props: tColorDisplay) => {
 
   return (
     <div
-      className="px-2 py-1 rounded-md flex flex-col min-w-[24px] min-h-[24px]"
+      className="px-2 py-1 rounded-md inline-flex flex-col min-w-[24px] min-h-[24px]"
       style={{
         backgroundColor: color || "",
         color: color ? getTextColor(color) : "",
