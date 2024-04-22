768ba6910f60e8f43bd6500eab71c13d4527fe51
Paul Leménager <pa-lem@hotmail.fr>
2024-04-22 19:02:10 +0200

    IPAM CRUD for prefixes and addresses (#2998)
    * add ipam generics
    
    * add select for kinds used by generic in create form
    
    * enable create form for generics
    
    * add rceation for ipam
    
    * rename generic kind selection
    
    * add update action in table
    
    * add update + delete in lists
    
    * remove log
    
    * update test for generic button to create objects
    
    * fix merge
    
    * fix link for prefix summary
    
    * remove logs
    
    * adds ref for refetch
    
    * :wrench:

--- frontend/src/screens/ipam/prefixes/ipam-prefix-details.tsx
+++ frontend/src/screens/ipam/prefixes/ipam-prefix-details.tsx
@@ -1,18 +1,36 @@
+import { gql } from "@apollo/client";
 import { Icon } from "@iconify-icon/react";
+import { useAtomValue } from "jotai";
+import { forwardRef, useImperativeHandle, useState } from "react";
 import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import SlideOver from "../../../components/display/slide-over";
+import ModalDelete from "../../../components/modals/modal-delete";
 import ProgressBar from "../../../components/stats/progress-bar";
 import { Table } from "../../../components/table/table";
+import { ALERT_TYPES, Alert } from "../../../components/utils/alert";
 import { Link } from "../../../components/utils/link";
 import { Pagination } from "../../../components/utils/pagination";
-import { IPAM_PREFIX_OBJECT } from "../../../config/constants";
+import { DEFAULT_BRANCH_NAME, IPAM_PREFIX_OBJECT } from "../../../config/constants";
+import graphqlClient from "../../../graphql/graphqlClientApollo";
+import { deleteObject } from "../../../graphql/mutations/objects/deleteObject";
 import { GET_PREFIX } from "../../../graphql/queries/ipam/prefixes";
 import useQuery from "../../../hooks/useQuery";
+import { currentBranchAtom } from "../../../state/atoms/branches.atom";
+import { datetimeAtom } from "../../../state/atoms/time.atom";
+import { stringifyWithoutQuotes } from "../../../utils/string";
 import ErrorScreen from "../../error-screen/error-screen";
 import LoadingScreen from "../../loading-screen/loading-screen";
+import ObjectItemEditComponent from "../../object-item-edit/object-item-edit-paginated";
 import { constructPathForIpam } from "../common/utils";
 
-export default function IpamIPPrefixDetails() {
+const IpamIPPrefixDetails = forwardRef((props, ref) => {
   const { prefix } = useParams();
+  const branch = useAtomValue(currentBranchAtom);
+  const date = useAtomValue(datetimeAtom);
+  const [relatedRowToDelete, setRelatedRowToDelete] = useState();
+  const [relatedObjectToEdit, setRelatedObjectToEdit] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
   if (!prefix) {
     return <div>Select a prefix</div>;
@@ -29,11 +47,13 @@ export default function IpamIPPrefixDetails() {
     }
   };
 
-  const { loading, error, data } = useQuery(GET_PREFIX, { variables: { prefix: prefix } });
+  const { loading, error, data, refetch } = useQuery(GET_PREFIX, {
+    variables: { prefix: prefix },
+  });
+
+  useImperativeHandle(ref, () => ({ refetch }));
 
   const parent = data && data[IPAM_PREFIX_OBJECT]?.edges[0]?.node?.parent?.node;
-  console.log("data: ", data);
-  console.log("parent: ", parent);
 
   const children = data && data[IPAM_PREFIX_OBJECT]?.edges[0]?.node?.children;
 
@@ -52,6 +72,8 @@ export default function IpamIPPrefixDetails() {
   ];
 
   const rows = children?.edges?.map((child) => ({
+    id: child?.node?.id,
+    __typename: child?.node?.__typename,
     values: {
       prefix: child?.node?.prefix?.value,
       description: child?.node?.description?.value,
@@ -72,6 +94,55 @@ export default function IpamIPPrefixDetails() {
     link: constructLink(child?.node),
   }));
 
+  const handleUpdate = (data) => {
+    setRelatedObjectToEdit(data);
+  };
+
+  const handleDelete = (data) => {
+    setRelatedRowToDelete(data);
+  };
+
+  const handleDeleteObject = async () => {
+    if (!relatedRowToDelete?.id) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    try {
+      const mutationString = deleteObject({
+        kind: relatedRowToDelete?.__typename,
+        data: stringifyWithoutQuotes({
+          id: relatedRowToDelete?.id,
+        }),
+      });
+
+      const mutation = gql`
+        ${mutationString}
+      `;
+
+      await graphqlClient.mutate({
+        mutation,
+        context: { branch: branch?.name, date },
+      });
+
+      refetch();
+
+      setRelatedRowToDelete(undefined);
+
+      toast(
+        <Alert
+          type={ALERT_TYPES.SUCCESS}
+          message={`Prefix ${relatedRowToDelete?.values?.prefix} deleted`}
+        />
+      );
+    } catch (error) {
+      console.error("Error while deleting address: ", error);
+    }
+
+    setIsLoading(false);
+  };
+
   if (error) {
     return <ErrorScreen message="An error occured while retrieving prefixes" />;
   }
@@ -90,9 +161,77 @@ export default function IpamIPPrefixDetails() {
 
       {loading && <LoadingScreen hideText />}
 
-      {data && <Table rows={rows} columns={columns} />}
+      {data && (
+        <Table rows={rows} columns={columns} onDelete={handleDelete} onUpdate={handleUpdate} />
+      )}
+
+      {relatedRowToDelete && (
+        <ModalDelete
+          title="Delete"
+          description={
+            <>
+              Are you sure you want to delete the Prefix:{" "}
+              <b>{relatedRowToDelete?.values?.prefix}</b>
+            </>
+          }
+          onCancel={() => setRelatedRowToDelete(undefined)}
+          onDelete={handleDeleteObject}
+          open={!!relatedRowToDelete}
+          setOpen={() => setRelatedRowToDelete(undefined)}
+          isLoading={isLoading}
+        />
+      )}
+
+      {relatedObjectToEdit && (
+        <SlideOver
+          title={
+            <>
+              {
+                <div className="space-y-2">
+                  <div className="flex items-center w-full">
+                    <span className="text-lg font-semibold mr-3">
+                      {relatedObjectToEdit?.values?.prefix}
+                    </span>
+                    <div className="flex-1"></div>
+                    <div className="flex items-center">
+                      <Icon icon={"mdi:layers-triple"} />
+                      <div className="ml-1.5 pb-1">{branch?.name ?? DEFAULT_BRANCH_NAME}</div>
+                    </div>
+                  </div>
+                  <span className="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20 mr-2">
+                    <svg
+                      className="h-1.5 w-1.5 mr-1 fill-yellow-500"
+                      viewBox="0 0 6 6"
+                      aria-hidden="true">
+                      <circle cx={3} cy={3} r={3} />
+                    </svg>
+                    {relatedObjectToEdit?.__typename}
+                  </span>
+                </div>
+              }
+            </>
+          }
+          open={!!relatedObjectToEdit}
+          setOpen={() => setRelatedObjectToEdit(undefined)}>
+          <ObjectItemEditComponent
+            closeDrawer={() => {
+              setRelatedObjectToEdit(undefined);
+            }}
+            onUpdateComplete={async () => {
+              setRelatedObjectToEdit(undefined);
+              if (refetch) {
+                refetch();
+              }
+            }}
+            objectid={relatedObjectToEdit.id}
+            objectname={relatedObjectToEdit.__typename}
+          />
+        </SlideOver>
+      )}
 
       <Pagination count={children?.count} />
     </div>
   );
-}
+});
+
+export default IpamIPPrefixDetails;
--- frontend/src/screens/ipam/prefixes/ipam-prefixes-summary-list.tsx
+++ frontend/src/screens/ipam/prefixes/ipam-prefixes-summary-list.tsx
@@ -1,17 +1,35 @@
+import { gql } from "@apollo/client";
 import { Icon } from "@iconify-icon/react";
+import { useAtomValue } from "jotai";
+import { forwardRef, useImperativeHandle, useState } from "react";
 import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import SlideOver from "../../../components/display/slide-over";
+import ModalDelete from "../../../components/modals/modal-delete";
 import ProgressBar from "../../../components/stats/progress-bar";
 import { Table } from "../../../components/table/table";
+import { ALERT_TYPES, Alert } from "../../../components/utils/alert";
 import { Pagination } from "../../../components/utils/pagination";
-import { IPAM_PREFIX_OBJECT } from "../../../config/constants";
+import { DEFAULT_BRANCH_NAME, IPAM_PREFIX_OBJECT } from "../../../config/constants";
+import graphqlClient from "../../../graphql/graphqlClientApollo";
+import { deleteObject } from "../../../graphql/mutations/objects/deleteObject";
 import { GET_PREFIXES } from "../../../graphql/queries/ipam/prefixes";
 import useQuery from "../../../hooks/useQuery";
+import { currentBranchAtom } from "../../../state/atoms/branches.atom";
+import { datetimeAtom } from "../../../state/atoms/time.atom";
+import { stringifyWithoutQuotes } from "../../../utils/string";
 import ErrorScreen from "../../error-screen/error-screen";
 import LoadingScreen from "../../loading-screen/loading-screen";
+import ObjectItemEditComponent from "../../object-item-edit/object-item-edit-paginated";
 import { constructPathForIpam } from "../common/utils";
 
-export default function IpamIPPrefixesSummaryList() {
+const IpamIPPrefixesSummaryList = forwardRef((props, ref) => {
   const { prefix } = useParams();
+  const branch = useAtomValue(currentBranchAtom);
+  const date = useAtomValue(datetimeAtom);
+  const [relatedRowToDelete, setRelatedRowToDelete] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [relatedObjectToEdit, setRelatedObjectToEdit] = useState();
 
   const constructLink = (data) => {
     switch (data.__typename) {
@@ -24,7 +42,11 @@ export default function IpamIPPrefixesSummaryList() {
     }
   };
 
-  const { loading, error, data } = useQuery(GET_PREFIXES, { variables: { prefix: prefix } });
+  const { loading, error, data, refetch } = useQuery(GET_PREFIXES, {
+    variables: { prefix: prefix },
+  });
+
+  useImperativeHandle(ref, () => ({ refetch }));
 
   const columns = [
     { name: "prefix", label: "Prefix" },
@@ -40,6 +62,8 @@ export default function IpamIPPrefixesSummaryList() {
   const rows =
     data &&
     data[IPAM_PREFIX_OBJECT]?.edges.map((edge) => ({
+      id: edge?.node?.id,
+      __typename: edge?.node?.__typename,
       values: {
         prefix: edge?.node?.prefix?.value,
         description: edge?.node?.description?.value,
@@ -61,6 +85,55 @@ export default function IpamIPPrefixesSummaryList() {
       link: constructLink(edge?.node),
     }));
 
+  const handleUpdate = (data) => {
+    setRelatedObjectToEdit(data);
+  };
+
+  const handleDelete = (data) => {
+    setRelatedRowToDelete(data);
+  };
+
+  const handleDeleteObject = async () => {
+    if (!relatedRowToDelete?.id) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    try {
+      const mutationString = deleteObject({
+        kind: relatedRowToDelete?.__typename,
+        data: stringifyWithoutQuotes({
+          id: relatedRowToDelete?.id,
+        }),
+      });
+
+      const mutation = gql`
+        ${mutationString}
+      `;
+
+      await graphqlClient.mutate({
+        mutation,
+        context: { branch: branch?.name, date },
+      });
+
+      refetch();
+
+      setRelatedRowToDelete(undefined);
+
+      toast(
+        <Alert
+          type={ALERT_TYPES.SUCCESS}
+          message={`Prefix ${relatedRowToDelete?.values?.prefix} deleted`}
+        />
+      );
+    } catch (error) {
+      console.error("Error while deleting address: ", error);
+    }
+
+    setIsLoading(false);
+  };
+
   if (error) {
     return <ErrorScreen message="An error occured while retrieving prefixes" />;
   }
@@ -68,8 +141,78 @@ export default function IpamIPPrefixesSummaryList() {
   return (
     <div>
       {loading && <LoadingScreen hideText />}
-      {data && <Table rows={rows} columns={columns} />}
+
+      {data && (
+        <Table rows={rows} columns={columns} onDelete={handleDelete} onUpdate={handleUpdate} />
+      )}
+
+      {relatedRowToDelete && (
+        <ModalDelete
+          title="Delete"
+          description={
+            <>
+              Are you sure you want to delete the Prefix:{" "}
+              <b>{relatedRowToDelete?.values?.prefix}</b>
+            </>
+          }
+          onCancel={() => setRelatedRowToDelete(undefined)}
+          onDelete={handleDeleteObject}
+          open={!!relatedRowToDelete}
+          setOpen={() => setRelatedRowToDelete(undefined)}
+          isLoading={isLoading}
+        />
+      )}
+
+      {relatedObjectToEdit && (
+        <SlideOver
+          title={
+            <>
+              {
+                <div className="space-y-2">
+                  <div className="flex items-center w-full">
+                    <span className="text-lg font-semibold mr-3">
+                      {relatedObjectToEdit?.values?.prefix}
+                    </span>
+                    <div className="flex-1"></div>
+                    <div className="flex items-center">
+                      <Icon icon={"mdi:layers-triple"} />
+                      <div className="ml-1.5 pb-1">{branch?.name ?? DEFAULT_BRANCH_NAME}</div>
+                    </div>
+                  </div>
+                  <span className="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20 mr-2">
+                    <svg
+                      className="h-1.5 w-1.5 mr-1 fill-yellow-500"
+                      viewBox="0 0 6 6"
+                      aria-hidden="true">
+                      <circle cx={3} cy={3} r={3} />
+                    </svg>
+                    {relatedObjectToEdit?.__typename}
+                  </span>
+                </div>
+              }
+            </>
+          }
+          open={!!relatedObjectToEdit}
+          setOpen={() => setRelatedObjectToEdit(undefined)}>
+          <ObjectItemEditComponent
+            closeDrawer={() => {
+              setRelatedObjectToEdit(undefined);
+            }}
+            onUpdateComplete={async () => {
+              setRelatedObjectToEdit(undefined);
+              if (refetch) {
+                refetch();
+              }
+            }}
+            objectid={relatedObjectToEdit.id}
+            objectname={relatedObjectToEdit.__typename}
+          />
+        </SlideOver>
+      )}
+
       <Pagination count={data && data[IPAM_PREFIX_OBJECT]?.count} />
     </div>
   );
-}
+});
+
+export default IpamIPPrefixesSummaryList;
