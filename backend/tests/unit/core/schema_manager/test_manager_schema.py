eb50fbf6582b530a76f5a9bb5b9d834c69ed5ea2
Aaron McCarty <ajtmccarty@pm.me>
2024-04-23 12:47:10 -0700

    move profile attributes to generic, set CoreProfile.used_by (#3038)
    * move profile attributes to generic, set CoreProfile.used_by
    
    * keep CoreProfile out of the database

--- backend/tests/unit/core/schema_manager/test_manager_schema.py
+++ backend/tests/unit/core/schema_manager/test_manager_schema.py
@@ -281,6 +281,28 @@ async def test_schema_branch_generate_weight(schema_all_in_one):
     assert len(in_second) == 1 and in_second[0].startswith(new_attr2_partial_id)
 
 
+async def test_schema_branch_add_profile_schema(schema_all_in_one):
+    core_profile_schema = _get_schema_by_kind(core_models, kind="CoreProfile")
+    schema_all_in_one["generics"].append(core_profile_schema)
+
+    schema = SchemaBranch(cache={}, name="test")
+    schema.load_schema(schema=SchemaRoot(**schema_all_in_one))
+    schema.add_profile_schemas()
+
+    profile = schema.get(name="ProfileBuiltinCriticality")
+    assert profile.get_attribute("profile_name").branch == BranchSupportType.AGNOSTIC.value
+    assert profile.get_attribute("profile_priority").branch == BranchSupportType.AGNOSTIC.value
+    assert set(profile.attribute_names) == {"profile_name", "profile_priority", "name", "level", "color", "description"}
+    core_profile_schema = schema.get("CoreProfile")
+    assert set(core_profile_schema.used_by) == {
+        "ProfileBuiltinCriticality",
+        "ProfileBuiltinTag",
+        "ProfileBuiltinStatus",
+        "ProfileBuiltinBadge",
+        "ProfileCoreStandardGroup",
+    }
+
+
 async def test_schema_branch_generate_identifiers(schema_all_in_one):
     schema = SchemaBranch(cache={}, name="test")
     schema.load_schema(schema=SchemaRoot(**schema_all_in_one))
@@ -2169,7 +2191,7 @@ async def test_load_schema_from_db(
     schema2 = await registry.schema.load_schema_from_db(db=db, branch=default_branch.name)
 
     assert len(schema2.nodes) == 6
-    assert len(schema2.generics) == 1
+    assert set(schema2.generics.keys()) == {"CoreProfile", "TestGenericInterface"}
     assert set(schema2.profiles.keys()) == {"ProfileBuiltinTag", "ProfileTestCriticality"}
 
     assert schema11.get(name="TestCriticality").get_hash() == schema2.get(name="TestCriticality").get_hash()
@@ -2243,7 +2265,7 @@ async def test_load_schema(
     schema2 = await registry.schema.load_schema(db=db, branch=default_branch.name)
 
     assert len(schema2.nodes) == 6
-    assert len(schema2.generics) == 1
+    assert set(schema2.generics.keys()) == {"CoreProfile", "TestGenericInterface"}
     assert set(schema2.profiles.keys()) == {"ProfileBuiltinTag", "ProfileTestCriticality"}
 
     assert schema11.get(name="TestCriticality").get_hash() == schema2.get(name="TestCriticality").get_hash()
