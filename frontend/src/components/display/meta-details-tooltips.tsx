c7071cdc83c9857ac925a2c12fd0dcf62e07d099
Bilal <bilal@opsmill.com>
2024-04-24 12:36:31 +0200

    Use new Popover in MetaDetailsToolips

--- frontend/src/components/display/meta-details-tooltips.tsx
+++ frontend/src/components/display/meta-details-tooltips.tsx
@@ -1,10 +1,8 @@
-import { Popover } from "@headlessui/react";
-import React, { useState } from "react";
-import { usePopper } from "react-popper";
+import React from "react";
 import { Property, PropertyList } from "../table/property-list";
 import { Icon } from "@iconify-icon/react";
-import { Card } from "../ui/card";
 import { Button } from "../buttons/button-primitive";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 interface MetaDetailsTooltipProps {
   items: Property[];
@@ -12,42 +10,24 @@ interface MetaDetailsTooltipProps {
 }
 
 export default function MetaDetailsTooltip({ header, items }: MetaDetailsTooltipProps) {
-  let [referenceElement, setReferenceElement] = useState<HTMLButtonElement | null>();
-  let [popperElement, setPopperElement] = useState<HTMLDivElement | null>();
-  let { styles, attributes } = usePopper(referenceElement, popperElement, {
-    modifiers: [
-      {
-        name: "flip",
-      },
-    ],
-  });
-
-  // TODO: use the popover component
   return (
     <Popover>
-      <Popover.Button
-        data-testid="view-metadata-button"
-        ref={setReferenceElement}
-        as={Button}
-        size="icon"
-        variant="ghost"
-        className="text-gray-500 focus-visible:ring-0"
-        data-cy="metadata-button">
-        <Icon icon="mdi:information-slab-circle-outline" />
-      </Popover.Button>
+      <PopoverTrigger asChild>
+        <Button
+          size="icon"
+          variant="ghost"
+          className="text-gray-500 focus-visible:ring-0"
+          data-cy="metadata-button"
+          data-testid="view-metadata-button">
+          <Icon icon="mdi:information-slab-circle-outline" />
+        </Button>
+      </PopoverTrigger>
 
-      <Popover.Panel
-        ref={setPopperElement}
-        style={styles.popper}
-        {...attributes.popper}
-        data-testid="metadata-tooltip"
-        data-cy="metadata-tooltip">
-        <Card className="text-sm divide-y shadow-xl">
-          {!!header && header}
+      <PopoverContent data-testid="metadata-tooltip" data-cy="metadata-tooltip">
+        {!!header && header}
 
-          <PropertyList properties={items} valueClassName="text-right" />
-        </Card>
-      </Popover.Panel>
+        <PropertyList properties={items} valueClassName="text-right" />
+      </PopoverContent>
     </Popover>
   );
 }
