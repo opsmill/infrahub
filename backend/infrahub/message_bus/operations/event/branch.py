55e356d4170118ff6b3138d22f74934ed8816a1a
Aaron McCarty <aaron@opsmill.com>
2024-04-29 06:49:59 -0700

    add IpamReconcile to merge mutation

--- backend/infrahub/message_bus/operations/event/branch.py
+++ backend/infrahub/message_bus/operations/event/branch.py
@@ -39,6 +39,7 @@ async def merge(message: messages.EventBranchMerge, service: InfrahubServices) -
         messages.RefreshRegistryBranches(),
         messages.TriggerArtifactDefinitionGenerate(branch=message.target_branch),
         messages.TriggerGeneratorDefinitionRun(branch=message.target_branch),
+        messages.TriggerIpamReconciliation(branch=message.target_branch, ipam_node_details=message.ipam_node_details),
     ]
 
     for event in events:
