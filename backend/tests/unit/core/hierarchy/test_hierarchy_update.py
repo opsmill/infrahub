a7febf77bfcd437c746451b659f74f2be0516640
Aaron McCarty <aaron@opsmill.com>
2024-04-29 15:11:26 -0700

    one more test

--- backend/tests/unit/core/hierarchy/test_hierarchy_update.py
+++ backend/tests/unit/core/hierarchy/test_hierarchy_update.py
@@ -28,3 +28,5 @@ async def test_update_node_with_hierarchy(db: InfrahubDatabase, hierarchical_loc
     results = await db.execute_query(query=CHECK_HIERARCHY_QUERY, params={"node_uuid": updated_node.id})
     for result in results:
         assert result.get("rel").get("hierarchy") == site_schema.hierarchy
+    nodes = await NodeManager.query(db=db, schema=site_schema, filters={"parent__name__value": "europe"})
+    assert {node.name.value for node in nodes} == {"paris", "london", "seattle"}
