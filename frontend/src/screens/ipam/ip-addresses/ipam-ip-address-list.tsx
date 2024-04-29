a10cd1761afecad9bf0af370de2412468dd65bae
Paul Leménager <pa-lem@hotmail.fr>
2024-04-29 15:47:22 +0200

    Columns update for IP details list view (#3134)
    * display prefix column only in global list view + remove interface column
    
    * remove log

--- frontend/src/screens/ipam/ip-addresses/ipam-ip-address-list.tsx
+++ frontend/src/screens/ipam/ip-addresses/ipam-ip-address-list.tsx
@@ -22,7 +22,7 @@ import ErrorScreen from "../../error-screen/error-screen";
 import LoadingScreen from "../../loading-screen/loading-screen";
 import ObjectItemEditComponent from "../../object-item-edit/object-item-edit-paginated";
 import { constructPathForIpam } from "../common/utils";
-import { IP_ADDRESS_GENERIC, IPAM_QSP, IPAM_ROUTE, IPAM_TABS } from "../constants";
+import { IPAM_QSP, IPAM_ROUTE, IPAM_TABS, IP_ADDRESS_GENERIC } from "../constants";
 
 const IpamIPAddressesList = forwardRef((props, ref) => {
   const { prefix } = useParams();
@@ -56,10 +56,10 @@ const IpamIPAddressesList = forwardRef((props, ref) => {
   const columns = [
     { name: "address", label: "Address" },
     { name: "description", label: "Description" },
-    { name: "interface", label: "Interface" },
     { name: "ip_namespace", label: "Ip Namespace" },
-    { name: "ip_prefix", label: "Ip Prefix" },
-  ];
+    // Display prefix column only in main list
+    !prefix && { name: "ip_prefix", label: "Ip Prefix" },
+  ].filter(Boolean);
 
   const rows =
     data &&
@@ -69,7 +69,6 @@ const IpamIPAddressesList = forwardRef((props, ref) => {
       values: {
         address: edge?.node?.address?.value,
         description: edge?.node?.description?.value,
-        interface: edge?.node?.interface?.node?.display_label,
         ip_namespace: edge?.node?.ip_namespace?.node?.display_label,
         ip_prefix: edge?.node?.ip_prefix?.node?.display_label,
       },
