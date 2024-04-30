0b6ab5d3aefab8b4265ff28befc5556dd7f1baea
Bilal <bilal@opsmill.com>
2024-04-30 10:50:32 +0200

    ProgressBar adheres to the  progressbar role requirements + redesign

--- frontend/src/components/stats/progress-bar.tsx
+++ frontend/src/components/stats/progress-bar.tsx
@@ -1,9 +1,20 @@
+import { HTMLAttributes } from "react";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
 import { classNames } from "../../utils/common";
 
-type tProgressBar = {
-  value: number;
-  displayValue?: boolean;
-};
+export const Progress = ({ className, value, ...props }: ProgressPrimitive.ProgressProps) => (
+  <ProgressPrimitive.Root
+    className={classNames(
+      "relative h-2 w-full overflow-hidden rounded-full bg-custom-blue-600/20",
+      className
+    )}
+    {...props}>
+    <ProgressPrimitive.Indicator
+      className="h-full w-full flex-1 bg-custom-blue-600 transition-all"
+      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+    />
+  </ProgressPrimitive.Root>
+);
 
 const getCleanedValue = (value: number) => {
   if (isNaN(value)) return 0;
@@ -13,18 +24,15 @@ const getCleanedValue = (value: number) => {
   return value;
 };
 
-export default function ProgressBar(props: tProgressBar) {
-  const { value, displayValue } = props;
+interface ProgressBarProps extends HTMLAttributes<HTMLDivElement> {
+  value: number;
+}
 
-  const cleanedValue = getCleanedValue(value);
-
+export default function ProgressBar({ value, className, ...props }: ProgressBarProps) {
   return (
-    <div className={classNames("w-full bg-gray-300 rounded-full", displayValue ? "h-4" : "h-2")}>
-      <div
-        style={{ width: `${cleanedValue}%` }}
-        className={"h-full bg-custom-blue-500 rounded-full flex items-center justify-center"}>
-        {displayValue && <span className="text-xs text-custom-white">{cleanedValue}%</span>}
-      </div>
+    <div className={classNames("w-full flex items-center gap-2", className)} {...props}>
+      <Progress value={getCleanedValue(getCleanedValue(value))} className="flex-grow h-2" />
+      <span className="text-custom-blue-700 font-medium">{value}%</span>
     </div>
   );
 }
