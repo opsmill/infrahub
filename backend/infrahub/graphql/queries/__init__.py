0a74bfb77d5a662b7e7d62b27263dcbc0da1d83d
Damien Garros <dgarros@gmail.com>
2024-05-01 12:14:41 +0200

    Add IPPrefixGetNextAvailable

--- backend/infrahub/graphql/queries/__init__.py
+++ backend/infrahub/graphql/queries/__init__.py
@@ -2,8 +2,18 @@ from .branch import BranchQueryList
 from .diff.diff import DiffSummary
 from .diff.old import DiffSummaryOld
 from .internal import InfrahubInfo
+from .ipam import InfrahubIPPrefixGetNextAvailable
 from .relationship import Relationship
 from .status import InfrahubStatus
 from .task import Task
 
-__all__ = ["BranchQueryList", "DiffSummary", "DiffSummaryOld", "InfrahubInfo", "InfrahubStatus", "Relationship", "Task"]
+__all__ = [
+    "BranchQueryList",
+    "DiffSummary",
+    "DiffSummaryOld",
+    "InfrahubInfo",
+    "InfrahubStatus",
+    "InfrahubIPPrefixGetNextAvailable",
+    "Relationship",
+    "Task",
+]
