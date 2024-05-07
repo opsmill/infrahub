a53e6721107ed7c2f1c949873760c8f9f3428392
pa-lem <pa-lem@hotmail.fr>
2024-05-07 16:19:07 +0200

    moves namespace selector

--- frontend/src/screens/ipam/ipam-page.tsx
+++ frontend/src/screens/ipam/ipam-page.tsx
@@ -3,11 +3,18 @@ import { Outlet } from "react-router-dom";
 import { Card } from "../../components/ui/card";
 import Content from "../layout/content";
 import IpamTree from "./ipam-tree/ipam-tree";
+import NamespaceSelector from "./namespace-selector";
 
 export default function IpamPage() {
   return (
     <>
-      <Content.Title title="IP Address Manager" />
+      <Content.Title>
+        <div className="flex items-center">
+          <h3 className="font-semibold text-sm mr-3">IP Adress Manager</h3>
+
+          <NamespaceSelector />
+        </div>
+      </Content.Title>
 
       <Content className="flex p-2 gap-2">
         <Card className="overflow-auto">
