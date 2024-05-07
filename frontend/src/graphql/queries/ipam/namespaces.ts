175e80f56ab309c012a6b985628c40020672b972
pa-lem <pa-lem@hotmail.fr>
2024-05-07 16:18:48 +0200

    get default attribute for namespace query

--- frontend/src/graphql/queries/ipam/namespaces.ts
+++ frontend/src/graphql/queries/ipam/namespaces.ts
@@ -19,6 +19,11 @@ export const GET_NAMESPACES = gql`
               value
             }
           }
+          ... on IpamNamespace {
+            default {
+              value
+            }
+          }
         }
       }
     }
