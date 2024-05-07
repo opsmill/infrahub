a1348f23e2a78634a3b8d4fb675e394c483711c7
pa-lem <pa-lem@hotmail.fr>
2024-05-07 18:36:12 +0200

    fix namespace query after cherry pick

--- frontend/src/graphql/queries/ipam/namespaces.ts
+++ frontend/src/graphql/queries/ipam/namespaces.ts
@@ -1,23 +1,17 @@
 import { gql } from "@apollo/client";
 
 export const GET_NAMESPACES = gql`
-  query IpamIPNamespace {
-      IpamIPNamespace {
-        edges {
-          node {
-            display_label
+  query BuiltinIPNamespace {
+    BuiltinIPNamespace {
+      edges {
+        node {
+          display_label
+          id
+          name {
             id
-            name {
-              id
-            }
-            description {
-              id
-            }
           }
-          ... on IpamNamespace {
-            default {
-              value
-            }
+          description {
+            id
           }
           ... on IpamNamespace {
             default {
