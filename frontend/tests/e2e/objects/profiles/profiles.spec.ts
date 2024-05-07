3184e240b3db59a700f447b3f9df8c7735055b4d
Paul Leménager <pa-lem@hotmail.fr>
2024-05-07 09:29:41 +0200

    Adds advanced test for profile for interface l2 (#3239)
    * add advanced test for profile for interface l2
    
    * add steps
    
    * add describe in previous e2e test for interface l2
    
    * update steps

--- frontend/tests/e2e/objects/profiles/profiles.spec.ts
+++ frontend/tests/e2e/objects/profiles/profiles.spec.ts
@@ -1,6 +1,8 @@
 import { expect, test } from "@playwright/test";
 import { ACCOUNT_STATE_PATH } from "../../../constants";
 
+const PROFILE_NAME = "Interface L2 profile test";
+
 test.describe("/objects/CoreProfile - Profiles page", () => {
   test.describe.configure({ mode: "serial" });
   test.use({ storageState: ACCOUNT_STATE_PATH.ADMIN });
@@ -152,5 +154,77 @@ test.describe("/objects/CoreProfile - Profiles page", () => {
       await page.getByText("Description-").getByTestId("view-metadata-button").click();
       await expect(page.getByTestId("metadata-tooltip").getByText("Source-")).toBeVisible();
     });
+  });
+});
+
+test.describe("/objects/CoreProfile - Profile for Interface L2 and fields verification", () => {
+  test.describe.configure({ mode: "serial" });
+  test.use({ storageState: ACCOUNT_STATE_PATH.ADMIN });
+
+  test("should verify the form fields for a new profile for interface L2", async ({ page }) => {
+    await test.step("access Interface L2 form", async () => {
+      await page.goto("/objects/CoreProfile");
+      await page.getByTestId("create-object-button").click();
+      await page.getByTestId("side-panel-container").getByTestId("select-input").fill("l2");
+      await page.getByText("ProfileInfraInterfaceL2").click();
+    });
+
+    await test.step("verify Interface L2 optional attributes are all visible", async () => {
+      await expect(page.getByText("Profile Name *")).toBeVisible();
+      await expect(page.getByText("Description")).toBeVisible();
+      await expect(page.getByText("MTU")).toBeVisible();
+      await expect(page.getByText("Enabled")).toBeVisible();
+      await expect(page.getByText("Status")).toBeVisible();
+      await expect(page.getByText("Role")).toBeVisible();
+    });
+
+    await test.step("verify Interface L2 mandatory attributes and relationships are not visible", async () => {
+      await expect(page.getByText("Layer2 Mode *")).not.toBeVisible();
+      await expect(page.getByText("Speed *")).not.toBeVisible();
+      await expect(page.getByText("Untagged VLAN")).not.toBeVisible();
+      await expect(
+        page.getByTestId("side-panel-container").getByText("Tagged VLANs")
+      ).not.toBeVisible();
+      await expect(page.getByText("Device *")).not.toBeVisible();
+    });
+  });
+
+  test("should create a new profile successfully for interface L2", async ({ page }) => {
+    await test.step("access Interface L2 form", async () => {
+      await page.goto("/objects/CoreProfile");
+      await page.getByTestId("create-object-button").click();
+      await page.getByTestId("side-panel-container").getByTestId("select-input").fill("l2");
+      await page.getByText("ProfileInfraInterfaceL2").click();
+    });
+
+    await test.step("fill and submit form", async () => {
+      await page.getByLabel("Profile Name *").fill(PROFILE_NAME);
+      await page.getByLabel("Profile Priority").fill("2000");
+      await page.getByLabel("MTU").fill("256");
+      await page.getByLabel("Enabled").check();
+      await page
+        .locator("div:below(:text('Status'))")
+        .first()
+        .getByTestId("select-open-option-button")
+        .click();
+      await page.getByText("Provisioning").click();
+      await page.getByRole("button", { name: "Create" }).click();
+    });
+  });
+
+  test("should verify profile values after creation", async ({ page }) => {
+    await page.goto("/objects/CoreProfile");
+    await page.getByRole("link", { name: PROFILE_NAME }).click();
+    await expect(page.locator("dl").getByText(PROFILE_NAME)).toBeVisible();
+    await expect(page.getByText("2000")).toBeVisible();
+    await expect(page.getByText("256")).toBeVisible();
+    await expect(
+      page
+        .locator("div")
+        .filter({ hasText: /^Enabled$/ })
+        .locator("svg")
+        .first()
+    ).toBeVisible();
+    await expect(page.getByText("Provisioning")).toBeVisible();
   });
 });
