859f8102d9ca8847f1652daba0f01e0cdc85d1ac
pa-lem <pa-lem@hotmail.fr>
2024-05-07 16:34:03 +0200

    redirects to main lists on name

--- frontend/src/screens/ipam/namespace-selector.tsx
+++ frontend/src/screens/ipam/namespace-selector.tsx
@@ -1,15 +1,20 @@
 import { useAtom } from "jotai";
 import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { StringParam, useQueryParam } from "use-query-params";
 import { Select } from "../../components/inputs/select";
 import { Skeleton } from "../../components/skeleton";
 import { GET_NAMESPACES } from "../../graphql/queries/ipam/namespaces";
 import useQuery from "../../hooks/useQuery";
 import { defaultNamespaceAtom } from "../../state/atoms/namespace.atom";
-import { IPAM_QSP, NAMESPACE_GENERIC } from "./constants";
+import { constructPath } from "../../utils/fetch";
+import { IPAM_QSP, IPAM_ROUTE, IPAM_TABS, NAMESPACE_GENERIC } from "./constants";
 
 export default function NamespaceSelector() {
+  const navigate = useNavigate();
+  const { prefix, ip_address } = useParams();
   const [namespace, setNamespace] = useQueryParam(IPAM_QSP.NAMESPACE, StringParam);
+  const [ipamTab] = useQueryParam(IPAM_QSP.TAB, StringParam);
   const [defaultNamespaceState, setDefaultNamespaceState] = useAtom(defaultNamespaceAtom);
   const [value, setValue] = useState(namespace ?? "");
 
@@ -20,10 +25,27 @@ export default function NamespaceSelector() {
 
     if (!newValue || newValue === defaultNamespaceState) {
       // Removes QSP for default namespace
-      return setNamespace(undefined);
+      setNamespace(undefined);
+    } else {
+      setNamespace(newValue);
     }
 
-    return setNamespace(newValue);
+    if (prefix || ip_address) {
+      // Redirects to main lists on namespace switch
+      if (ipamTab === IPAM_TABS.IP_DETAILS) {
+        // Redirects to main IP Adresses view
+        navigate(
+          constructPath(
+            `${IPAM_ROUTE.ADDRESSES}/`,
+            [{ name: IPAM_QSP.TAB, value: IPAM_TABS.IP_DETAILS }],
+            [IPAM_QSP.NAMESPACE]
+          )
+        );
+      } else {
+        // Redirects to main Prefixes view
+        navigate(constructPath(`${IPAM_ROUTE.PREFIXES}/`, [], [IPAM_QSP.NAMESPACE]));
+      }
+    }
   };
 
   const namespaces = (data && data[NAMESPACE_GENERIC]?.edges.map((edge) => edge.node)) ?? [];
