cd46f30bcd82bec0d156946018d1f997d400ac9d
Mikhail Yohman <mikhail.yohman@gmail.com>
2024-05-11 21:15:04 -0600

    Set integer enum values to strings, otherwise keep the previous logic of _.join

--- backend/infrahub/core/enums.py
+++ backend/infrahub/core/enums.py
@@ -8,6 +8,10 @@ ENUM_NAME_REGEX = re.compile("[_a-zA-Z0-9]+")
 def generate_python_enum(name: str, options: List[Any]) -> Type[enum.Enum]:
     main_attrs = {}
     for option in options:
-        enum_name = "_".join(re.findall(ENUM_NAME_REGEX, option)).upper()
+        if isinstance(option, int):
+            enum_name = str(option)
+        else:
+            enum_name = "_".join(re.findall(ENUM_NAME_REGEX, option)).upper()
+
         main_attrs[enum_name] = option
     return enum.Enum(name, main_attrs)  # type: ignore[return-value]
