c80693a822e96fddd9194a84ef041e4167ba338e
Bilal <bilal@opsmill.com>
2024-05-13 12:27:06 +0200

    ipam tree reload logic is in a atom to be called anywhere

--- frontend/src/screens/ipam/ipam-tree/ipam-tree.tsx
+++ frontend/src/screens/ipam/ipam-tree/ipam-tree.tsx
@@ -1,124 +1,41 @@
-import { Icon } from "@iconify-icon/react";
-import * as R from "ramda";
-import { useEffect, useState } from "react";
+import { useAtom, useAtomValue, useSetAtom } from "jotai";
+import React, { useEffect, useState } from "react";
 import { ITreeViewOnLoadDataProps, NodeId } from "react-accessible-treeview";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { Tree, TreeItemProps } from "../../../components/ui/tree";
+import { Icon } from "@iconify-icon/react";
+import { TreeItemProps, Tree } from "../../../components/ui/tree";
 import { useLazyQuery } from "../../../hooks/useQuery";
 
-import {
-  GET_PREFIX_ANCESTORS,
-  GET_PREFIXES_ONLY,
-  GET_TOP_LEVEL_PREFIXES,
-} from "../../../graphql/queries/ipam/prefixes";
+import { GET_PREFIXES_ONLY } from "../../../graphql/queries/ipam/prefixes";
 import { genericsState, schemaState } from "../../../state/atoms/schema.atom";
 import { constructPathForIpam } from "../common/utils";
-import { IP_PREFIX_GENERIC, IPAM_ROUTE, IPAM_TREE_ROOT_ID } from "../constants";
 import { IpamTreeSkeleton } from "./ipam-tree-skeleton";
+import { IPAM_ROUTE } from "../constants";
 import {
-  AncestorsData,
-  EMPTY_IPAM_TREE,
   formatIPPrefixResponseForTreeView,
+  getTreeItemAncestors,
   PrefixData,
   updateTreeData,
 } from "./utils";
-import { useAtom, useAtomValue } from "jotai";
-import { ipamTreeAtom } from "./ipam-tree.state";
+import { ipamTreeAtom, reloadIpamTreeAtom } from "./ipam-tree.state";
 
 export default function IpamTree() {
   const { prefix } = useParams();
   const [expandedIds, setExpandedIds] = useState<NodeId[]>([]);
   const [isLoading, setLoading] = useState(true);
   const [treeData, setTreeData] = useAtom(ipamTreeAtom);
-  const [fetchTopLevelIpPrefixes] = useLazyQuery<PrefixData>(GET_TOP_LEVEL_PREFIXES);
-  const [fetchPrefixAncestors] = useLazyQuery<AncestorsData>(GET_PREFIX_ANCESTORS);
+  const reloadIpamTree = useSetAtom(reloadIpamTreeAtom);
   const [fetchPrefixes] = useLazyQuery<PrefixData, { parentIds: string[] }>(GET_PREFIXES_ONLY);
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetchTopLevelIpPrefixes()
-      .then(({ data }) => {
-        if (!data) return;
-
-        const topLevelTreeItems = formatIPPrefixResponseForTreeView(data);
-
-        // assign all prefixes and IP addresses without parent to the root node
-        return updateTreeData(EMPTY_IPAM_TREE, IPAM_TREE_ROOT_ID, topLevelTreeItems);
-      })
-      .then((tree) => {
-        if (!tree) return;
-
-        if (!prefix) {
-          setTreeData(tree);
-          setLoading(false);
-          return;
-        }
-
-        fetchPrefixAncestors({ variables: { ids: [prefix] } })
-          .then(({ data }) => {
-            if (!data) return;
-
-            const prefixAncestorsData = data[IP_PREFIX_GENERIC].edges[0];
-
-            if (!prefixAncestorsData) {
-              setTreeData(tree);
-              setLoading(false);
-              return;
-            }
-
-            const ancestors = prefixAncestorsData.node.ancestors.edges.map(({ node }) => ({
-              id: node.id,
-              name: node.display_label,
-              parentId: node.parent.node?.id ?? IPAM_TREE_ROOT_ID,
-            }));
-
-            const parentToChildMap: Record<string, string> = {};
-
-            ancestors.forEach(({ id, parentId }) => {
-              parentToChildMap[parentId] = id;
-            });
-
-            const orderedAncestorIds: string[] = [];
-
-            const traverseHierarchy = (map: Record<string, string>, parentId: string) => {
-              const childId = map[parentId];
-              if (!childId) return;
-
-              orderedAncestorIds.push(childId);
-              traverseHierarchy(map, childId);
-            };
-
-            traverseHierarchy(parentToChildMap, IPAM_TREE_ROOT_ID);
-
-            return orderedAncestorIds;
-          })
-          .then((orderedAncestorIds) => {
-            if (!orderedAncestorIds) return;
-
-            fetchPrefixes({ variables: { parentIds: orderedAncestorIds } }).then(({ data }) => {
-              if (!data) return;
-
-              const treeItems = formatIPPrefixResponseForTreeView(data);
-              const groupedByParent = R.groupBy(
-                (node) => node.parent?.toString() ?? IPAM_TREE_ROOT_ID,
-                treeItems
-              );
-
-              const newTree = orderedAncestorIds.reduce((acc, currentAncestorId) => {
-                const children = groupedByParent[currentAncestorId];
-                if (!children) return acc;
-                return updateTreeData(acc, currentAncestorId, children);
-              }, tree);
-
-              const currentPrefix = newTree.find(({ id }) => id === prefix);
-              setTreeData(newTree);
-              setExpandedIds(
-                currentPrefix ? [...orderedAncestorIds, currentPrefix.id] : orderedAncestorIds
-              );
-              setLoading(false);
-            });
-          });
-      });
+    reloadIpamTree(prefix).then((newTree) => {
+      if (prefix) {
+        const ancestorIds = getTreeItemAncestors(newTree, prefix).map(({ id }) => id);
+        setExpandedIds(ancestorIds);
+      }
+      setLoading(false);
+    });
   }, []);
 
   const onLoadData = async ({ element }: ITreeViewOnLoadDataProps) => {
