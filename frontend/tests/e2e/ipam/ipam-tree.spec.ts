893c62777829e1ce0b0629274ee3db57157185e7
Bilal <bilal@opsmill.com>
2024-05-13 13:07:35 +0200

    Fix flaky e2e test on ipam tree + add step

--- frontend/tests/e2e/ipam/ipam-tree.spec.ts
+++ frontend/tests/e2e/ipam/ipam-tree.spec.ts
@@ -3,30 +3,41 @@ import { expect, test } from "@playwright/test";
 test.describe("/ipam - Ipam Tree", () => {
   test("load child tree item when clicking on parent tree item", async ({ page }) => {
     await page.goto("/ipam");
-
     await expect(page.getByRole("heading", { name: "Navigation" })).toBeVisible();
     await expect(page.getByTestId("ipam-tree")).toBeVisible();
-    await expect(await page.getByTestId("ipam-tree-item").count()).toEqual(4);
 
-    await page
-      .getByRole("treeitem", { name: "10.0.0.0/8" })
-      .getByTestId("tree-item-toggle")
-      .click();
-    await expect(page.getByRole("treeitem", { name: "10.1.0.0/16" })).toBeVisible();
-    await expect(await page.getByTestId("ipam-tree-item").count()).toEqual(7);
+    await test.step("all top level prefix are collapsed", async () => {
+      await expect(page.getByRole("treeitem", { name: "10.0.0.0/8" })).toBeVisible();
+      await expect(page.getByRole("treeitem", { name: "10.1.0.0/16" })).toBeHidden();
+    });
 
-    await page
-      .getByRole("treeitem", { name: "10.1.0.0/16" })
-      .getByTestId("tree-item-toggle")
-      .click();
-    await expect(page.getByRole("treeitem", { name: "10.1.0.12/31" })).toBeVisible();
-    await expect(await page.getByTestId("ipam-tree-item").count()).toEqual(17);
+    await test.step("view direct children of a top level prefix", async () => {
+      await page
+        .getByRole("treeitem", { name: "10.0.0.0/8" })
+        .getByTestId("tree-item-toggle")
+        .click();
+      await expect(page.getByRole("treeitem", { name: "10.0.0.0/8" })).toBeVisible();
+      await expect(page.getByRole("treeitem", { name: "10.1.0.0/16" })).toBeVisible();
+      await expect(page.getByRole("treeitem", { name: "10.1.0.12/31" })).toBeHidden();
+    });
+
+    await test.step("view children of a children prefix", async () => {
+      await page
+        .getByRole("treeitem", { name: "10.1.0.0/16" })
+        .getByTestId("tree-item-toggle")
+        .click();
+      await expect(page.getByRole("treeitem", { name: "10.0.0.0/8" })).toBeVisible();
+      await expect(page.getByRole("treeitem", { name: "10.1.0.0/16" })).toBeVisible();
+      await expect(page.getByRole("treeitem", { name: "10.1.0.12/31" })).toBeVisible();
+    });
 
     await test.step("On first load, it expands IPAM tree to the selected prefix position", async () => {
       await page.getByRole("treeitem", { name: "10.1.0.12/31" }).click();
       await page.reload();
       await expect(page.getByTestId("ipam-tree")).toBeVisible();
-      await expect(await page.getByTestId("ipam-tree-item").count()).toEqual(17);
+      await expect(page.getByRole("treeitem", { name: "10.0.0.0/8" })).toBeVisible();
+      await expect(page.getByRole("treeitem", { name: "10.1.0.0/16" })).toBeVisible();
+      await expect(page.getByRole("treeitem", { name: "10.1.0.12/31" })).toBeVisible();
       await expect(page.locator("[aria-selected=true]")).toContainText("10.1.0.12/31");
     });
   });
