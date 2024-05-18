d2ebdf51c64fcc0018c47950eda90805d28bf649
Damien Garros <dgarros@gmail.com>
2024-05-18 19:16:54 +0200

    Roll back find_node_schema to see if it improves the performance

--- backend/infrahub/core/query/utils.py
+++ backend/infrahub/core/query/utils.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Optional, Union
 
+from infrahub.core import registry
 from infrahub.core.schema import NodeSchema, ProfileSchema
 
 if TYPE_CHECKING:
@@ -12,12 +13,24 @@ if TYPE_CHECKING:
 
 
 def find_node_schema(
-    db: InfrahubDatabase, node: Neo4jNode, branch: Union[Branch, str], duplicate: bool = False
+    db: InfrahubDatabase,  # pylint: disable=unused-argument
+    node: Neo4jNode,
+    branch: Union[Branch, str],
+    duplicate: bool,
 ) -> Optional[Union[NodeSchema, ProfileSchema]]:
     for label in node.labels:
-        if db.schema.has(name=label, branch=branch):
-            schema = db.schema.get(name=label, branch=branch, duplicate=duplicate)
+        if registry.schema.has(name=label, branch=branch):
+            schema = registry.schema.get(name=label, branch=branch, duplicate=duplicate)
             if isinstance(schema, (NodeSchema, ProfileSchema)):
                 return schema
+
+    # def find_node_schema(
+    #     db: InfrahubDatabase, node: Neo4jNode, branch: Union[Branch, str], duplicate: bool = False
+    # ) -> Optional[Union[NodeSchema, ProfileSchema]]:
+    #     for label in node.labels:
+    #         if db.schema.has(name=label, branch=branch):
+    #             schema = db.schema.get(name=label, branch=branch, duplicate=duplicate)
+    #             if isinstance(schema, (NodeSchema, ProfileSchema)):
+    #                 return schema
 
     return None
