a15f3452d2620c9b3c7907ece0e29b002091ff17
pa-lem <pa-lem@hotmail.fr>
2024-05-20 10:29:17 +0200

    fix name selector

--- frontend/tests/e2e/form/multi-select.spec.ts
+++ frontend/tests/e2e/form/multi-select.spec.ts
@@ -50,7 +50,7 @@ test.describe("Verify multi select behaviour", () => {
 
     await test.step("Create a new tag directly on multi select", async () => {
       await page.getByRole("option", { name: "Add Tag" }).click();
-      await page.getByLabel("Create Tag").locator("#Name").fill("new tag");
+      await page.getByLabel("Create TagmainStandard Tag").locator("#Name").fill("new tag");
       await page.getByRole("button", { name: "Create" }).click();
       await expect(page.locator("form").first()).toContainText("bluenew tag");
     });
