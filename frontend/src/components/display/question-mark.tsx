16461d19ef085b211251e75a2e9420f2039fcfc6
pa-lem <pa-lem@hotmail.fr>
2024-05-21 14:32:59 +0200

    fix message prop

--- frontend/src/components/display/question-mark.tsx
+++ frontend/src/components/display/question-mark.tsx
@@ -9,7 +9,7 @@ export const QuestionMark = ({ message }: tQuestionMark) => {
   if (!message) return null;
 
   return (
-    <Tooltip content={"message"} enabled>
+    <Tooltip content={message} enabled>
       <span className="text-custom-blue-50 border border-sm rounded-full w-4 h-4 flex items-center justify-center cursor-pointer">
         <Icon icon={"mdi:question-mark"} className="text-xxs" />
       </span>
