73862f086d052e19f4e65946c36080cb1bb6e0d9
pa-lem <pa-lem@hotmail.fr>
2024-05-21 14:35:49 +0200

    adds ip adddress pool queries

--- frontend/src/screens/ipam/constants.ts
+++ frontend/src/screens/ipam/constants.ts
@@ -3,6 +3,7 @@ export const IP_ADDRESS_GENERIC = "BuiltinIPAddress";
 export const IP_PREFIX_GENERIC = "BuiltinIPPrefix";
 export const POOLS_PEER = [IP_ADDRESS_GENERIC, IP_PREFIX_GENERIC];
 export const POOLS_DICTIONNARY = {
+  IpamIPAddress: "CoreIPAddressPool",
   IpamIPPrefix: "CorePrefixPool",
 };
 
