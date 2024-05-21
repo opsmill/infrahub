109481854513b850fec1ee1e5f887bbde1db874a
Bilal <bilal@opsmill.com>
2024-05-21 23:01:43 +0200

    better handle formatted long address on resources

--- frontend/src/screens/resource-manager/resource-selector.tsx
+++ frontend/src/screens/resource-manager/resource-selector.tsx
@@ -1,10 +1,11 @@
 import { CardWithBorder } from "../../components/ui/card";
 import { Icon } from "@iconify-icon/react";
 import { Badge } from "../../components/ui/badge";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { getObjectDetailsUrl2 } from "../../utils/objects";
-import { classNames } from "../../utils/common";
 import ResourcePoolUtilization from "./common/ResourcePoolUtilization";
+import { PropertyList } from "../../components/table/property-list";
+import { constructPath } from "../../utils/fetch";
 
 export type ResourceProps = {
   id: string;
@@ -25,45 +26,34 @@ const ResourceSelector = ({ resources }: ResourcePoolSelectorProps) => {
         Resources <Badge>{resources.length}</Badge>
       </CardWithBorder.Title>
 
-      <div className="divide-y">
-        {resources.map((resource) => (
-          <ResourceOption key={resource.id} {...resource} />
-        ))}
-      </div>
-    </CardWithBorder>
-  );
-};
+      <PropertyList
+        valueClassName="w-full"
+        properties={resources.map((resource) => ({
+          name: (
+            <Link
+              to={getObjectDetailsUrl2(resource.kind, resource.id)}
+              className="font-semibold underline">
+              {resource.display_label}
+            </Link>
+          ),
+          value: (
+            <div className="flex gap-2">
+              <ResourcePoolUtilization
+                utilizationOverall={resource.utilization}
+                utilizationOtherBranches={resource.utilization_branches}
+                utilizationDefaultBranch={resource.utilization_default_branch}
+              />
 
-const ResourceOption = ({
-  display_label,
-  id,
-  kind,
-  utilization,
-  utilization_branches,
-  utilization_default_branch,
-}: ResourceProps) => {
-  const { resourceId } = useParams();
-
-  return (
-    <div
-      className={classNames(
-        "p-2 flex items-center gap-4 text-sm text-gray-600",
-        resourceId === id && "bg-sky-50"
-      )}>
-      <Link to={getObjectDetailsUrl2(kind, id)} className="font-semibold underline">
-        {display_label}
-      </Link>
-
-      <ResourcePoolUtilization
-        utilizationOverall={utilization}
-        utilizationOtherBranches={utilization_branches}
-        utilizationDefaultBranch={utilization_default_branch}
+              <Link
+                to={constructPath(`resources/${resource.id}`)}
+                className="flex items-center gap-1 text-nowrap hover:underline">
+                View <Icon icon="mdi:eye-outline" />
+              </Link>
+            </div>
+          ),
+        }))}
       />
-
-      <Link to={"resources/" + id} className="flex items-center gap-1 text-nowrap hover:underline">
-        View <Icon icon="mdi:eye-outline" />
-      </Link>
-    </div>
+    </CardWithBorder>
   );
 };
 
