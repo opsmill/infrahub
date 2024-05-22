27652b12bfbc16633045d8e0b0942ac030bec7e0
Bilal <bilal@opsmill.com>
2024-05-22 14:54:38 +0200

    feedback

--- frontend/src/screens/errors/error-fallback.tsx
+++ frontend/src/screens/errors/error-fallback.tsx
@@ -14,14 +14,18 @@ function ErrorFallback({ error }: ErrorFallbackProps) {
   const [bugPosition, setBugPosition] = useState({ top: 0, left: 0 });
 
   useEffect(() => {
-    const onPressEnterKey = (event: KeyboardEvent) => {
+    const onKeydown = (event: KeyboardEvent) => {
       if (event.key.toLowerCase() === "enter") {
         resetBoundary();
       }
+      console.error(event.key);
+      if (event.key.toLowerCase() === "backspace") {
+        window.location.href = window.location.origin;
+      }
     };
 
-    document.addEventListener("keydown", onPressEnterKey);
-    return () => document.removeEventListener("keydown", onPressEnterKey);
+    document.addEventListener("keydown", onKeydown);
+    return () => document.removeEventListener("keydown", onKeydown);
   }, []);
 
   const handleMoveOnBug = () => {
@@ -37,28 +41,49 @@ function ErrorFallback({ error }: ErrorFallbackProps) {
   return (
     <div className="bg-gray-100 flex flex-col items-center justify-center h-full">
       <Card className="flex flex-col items-center mb-4 p-4">
-        <h1 className="flex items-center gap-2 font-semibold text-lg">
-          <Icon
-            icon="mdi:bug"
-            className="relative text-custom-blue-600 transition-all text-xl cursor-pointer"
-            style={bugPosition}
-            onMouseEnter={handleMoveOnBug}
-            onClick={handleMoveOnBug}
-          />
-          Uh-oh, something went wrong :(
-        </h1>
+        <h1 className="font-semibold text-lg">Uh-oh, something went wrong :(</h1>
 
-        <p>Looks like you found a bug...</p>
+        <div>
+          <p className="flex items-center gap-1">
+            You might have encounter a{" "}
+            <Icon
+              icon="mdi:bug"
+              className="relative text-custom-blue-600 transition-all text-xl cursor-pointer"
+              style={bugPosition}
+              onMouseEnter={handleMoveOnBug}
+              onClick={handleMoveOnBug}
+            />
+            ...
+          </p>
 
-        <Button className="my-4" onClick={resetBoundary}>
-          Refresh
-        </Button>
+          <div>
+            <Button className="mr-2" onClick={resetBoundary}>
+              Refresh
+            </Button>
+            <a href={window.location.origin}>
+              <Button variant="outline" className="my-4" onClick={resetBoundary}>
+                Homepage
+              </Button>
+            </a>
+          </div>
 
-        <p className="font-semibold text-xs mb-1">
-          Press <Kbd>Enter</Kbd> to refresh
-        </p>
+          <p className="font-medium text-xs mb-1">
+            Press{" "}
+            <Kbd keys={["enter"]} keyClassName="relative top-px mr-1">
+              enter
+            </Kbd>{" "}
+            to try again
+          </p>
+          <p className="font-medium text-xs mb-4">
+            Press{" "}
+            <Kbd keys={["delete"]} keyClassName="mr-1">
+              backspace
+            </Kbd>{" "}
+            to go back to Homepage
+          </p>
+        </div>
         <p className="text-xs text-gray-600">
-          or reach out to us on{" "}
+          If this was unexpected, please reach out to us on{" "}
           <a
             className="underline"
             href="https://discord.com/channels/1212332642801025064/1212669187198025759"
@@ -74,7 +99,6 @@ function ErrorFallback({ error }: ErrorFallbackProps) {
             rel="noreferrer">
             GitHub
           </a>
-          , we will reply super fast.
         </p>
       </Card>
 
