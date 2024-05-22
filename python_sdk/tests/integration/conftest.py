d711790c3fa2d613177c3013876647ad6b8977dd
Guillaume Mazoyer <guillaume@opsmill.com>
2024-05-22 15:41:21 +0200

    Add HFID to tests

--- python_sdk/tests/integration/conftest.py
+++ python_sdk/tests/integration/conftest.py
@@ -496,7 +496,7 @@ async def ipam_schema() -> SchemaRoot:
                 "name": "Device",
                 "namespace": "Infra",
                 "label": "Device",
-                "default_filter": "name__value",
+                "human_friendly_id": ["name__value"],
                 "order_by": ["name__value"],
                 "display_labels": ["name__value"],
                 "attributes": [{"name": "name", "kind": "Text", "unique": True}],
