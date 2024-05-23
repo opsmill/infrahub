8a7449495d820c4c6b439c10844f6fc80642a01b
pa-lem <pa-lem@hotmail.fr>
2024-05-23 16:47:50 +0200

    prevent menu from disappearing

--- frontend/src/screens/layout/sidebar/index.tsx
+++ frontend/src/screens/layout/sidebar/index.tsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 import BranchSelector from "../../../components/branch-selector";
+import { ReactComponent as InfrahubLogo } from "../../../images/Infrahub-SVG-hori.svg";
 import { Footer } from "../footer";
 import { DesktopMenu } from "./desktop-menu";
-import { ReactComponent as InfrahubLogo } from "../../../images/Infrahub-SVG-hori.svg";
 
 export function Sidebar() {
   return (
-    <div className="z-100 hidden w-64 md:flex flex-col border-r">
+    <div className="z-100 w-64 flex-col border-r">
       <div className="flex flex-grow flex-col overflow-y-auto min-h-0">
         <Link to="/" className="h-16 flex-shrink-0 px-5 flex items-center">
           <InfrahubLogo />
