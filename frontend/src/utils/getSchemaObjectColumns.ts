b7c15137d37acf00e9489a0efaa25b379222564a
pa-lem <pa-lem@hotmail.fr>
2024-05-24 16:16:14 +0200

    fix relationships in create form

--- frontend/src/utils/getSchemaObjectColumns.ts
+++ frontend/src/utils/getSchemaObjectColumns.ts
@@ -178,10 +178,11 @@ export const getObjectRelationshipsForForm = (
     // Filter allowed fields
     .filter((relationship) => peersKindForForm.includes(relationship?.kind ?? ""))
     // Create form includes cardinality many but only if required, edit form doesn't include it at all
-    .filter((relationship) =>
-      isUpdate
-        ? relationship.cardinality === "one" || peersKindForForm.includes(relationship?.kind ?? "")
-        : relationship.cardinality === "one" || !relationship.optional
+    .filter(
+      (relationship) =>
+        relationship.cardinality === "one" ||
+        peersKindForForm.includes(relationship?.kind ?? "") ||
+        (isUpdate && !relationship.optional)
     )
     .filter(Boolean);
 
