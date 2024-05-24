66f913e339243075a72fe9b1e1433c5cf8073094
Paul Leménager <pa-lem@hotmail.fr>
2024-05-24 10:11:03 +0200

    fix menu overflow (#3446)

--- frontend/src/screens/layout/sidebar/index.tsx
+++ frontend/src/screens/layout/sidebar/index.tsx
@@ -6,7 +6,7 @@ import { DesktopMenu } from "./desktop-menu";
 
 export function Sidebar() {
   return (
-    <div className="z-100 w-64 flex-col border-r">
+    <div className="z-100 w-64 flex flex-col border-r">
       <div className="flex flex-grow flex-col overflow-y-auto min-h-0">
         <Link to="/" className="h-16 flex-shrink-0 px-5 flex items-center">
           <InfrahubLogo />
