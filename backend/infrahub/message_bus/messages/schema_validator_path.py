c66f8933d370db201ed95a9f9bd747e201abfea4
Damien Garros <dgarros@gmail.com>
2024-05-25 19:20:33 +0200

    Update SchemaValidatorPath to support profile

--- backend/infrahub/message_bus/messages/schema_validator_path.py
+++ backend/infrahub/message_bus/messages/schema_validator_path.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
-from typing import List, Optional, Union
+from typing import List, Optional
 
 from pydantic import Field
 
 from infrahub.core.branch import Branch  # noqa: TCH001
 from infrahub.core.path import SchemaPath  # noqa: TCH001
-from infrahub.core.schema import GenericSchema, NodeSchema  # noqa: TCH001
+from infrahub.core.schema import MainSchemaTypes  # noqa: TCH001
 from infrahub.core.validators.model import SchemaViolation  # noqa: TCH001
 from infrahub.message_bus import InfrahubMessage, InfrahubResponse, InfrahubResponseData
 
@@ -16,7 +16,7 @@ ROUTING_KEY = "schema.validator.path"
 class SchemaValidatorPath(InfrahubMessage):
     branch: Branch = Field(..., description="The name of the branch to target")
     constraint_name: str = Field(..., description="The name of the constraint to validate")
-    node_schema: Union[NodeSchema, GenericSchema] = Field(..., description="Schema of Node or Generic to validate")
+    node_schema: MainSchemaTypes = Field(..., description="Schema of Node or Generic to validate")
     schema_path: SchemaPath = Field(..., description="SchemaPath to the element of the schema to validate")
 
 
