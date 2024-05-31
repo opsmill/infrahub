8eb0922765b6dac6eba8ebe03c2f214f7591ac63
Bilal <bilal@opsmill.com>
2024-05-31 11:20:42 +0200

    MarkdownEditor supports focus on label click and form error

--- frontend/src/components/editor/index.tsx
+++ frontend/src/components/editor/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useRef, useState } from "react";
+import { FC, forwardRef, useRef, useState } from "react";
 import { useCodeMirror } from "../../hooks/useCodeMirror";
 import { classNames } from "../../utils/common";
 import { MarkdownEditorHeader } from "./markdown-editor-header";
@@ -8,24 +8,22 @@ type MarkdownEditorProps = {
   className?: string;
   defaultValue?: string;
   disabled?: boolean;
-  onChange: (value: string) => void;
+  onChange?: (value: string) => void;
   placeholder?: string;
+  id?: string;
 };
 
-export const MarkdownEditor: FC<MarkdownEditorProps> = ({
-  className = "",
-  defaultValue = "",
-  disabled = false,
-  onChange,
-  placeholder,
-}) => {
+export const MarkdownEditor: FC<MarkdownEditorProps> = forwardRef<
+  HTMLButtonElement,
+  MarkdownEditorProps
+>(({ id, className = "", defaultValue = "", disabled = false, onChange, placeholder }, ref) => {
   const [isPreviewActive, setPreviewActive] = useState<boolean>(false);
   const [editorText, setEditorText] = useState<string>(defaultValue);
   const codeMirrorRef = useRef<HTMLDivElement>(null);
 
   const handleTextChange = (value: string) => {
     setEditorText(value);
-    onChange(value);
+    if (onChange) onChange(value);
   };
 
   const codeMirror = useCodeMirror(codeMirrorRef.current, {
@@ -44,25 +42,37 @@ export const MarkdownEditor: FC<MarkdownEditorProps> = ({
   }
 
   return (
-    <div
-      className={classNames(
-        `
+    <>
+      {id && (
+        <button
+          id={id}
+          ref={ref}
+          type="button"
+          onClick={() => codeMirror.view?.focus()}
+          onFocus={() => codeMirror.view?.focus()}
+          className="w-0 h-0"
+        />
+      )}
+      <div
+        className={classNames(
+          `
         bg-white rounded-lg border border-gray-300 shadow-sm
         focus-within:outline focus-within:outline-custom-blue-600 focus-within:border-custom-blue-600
         `,
-        className
-      )}>
-      <MarkdownEditorHeader
-        codeMirror={codeMirror}
-        previewMode={isPreviewActive}
-        onPreviewToggle={() => setPreviewActive((prev) => !prev)}
-      />
+          className
+        )}>
+        <MarkdownEditorHeader
+          codeMirror={codeMirror}
+          previewMode={isPreviewActive}
+          onPreviewToggle={() => setPreviewActive((prev) => !prev)}
+        />
 
-      {isPreviewActive ? (
-        <MarkdownViewer markdownText={editorText} className="p-2" />
-      ) : (
-        <div ref={codeMirrorRef} data-cy="codemirror-editor" data-testid="codemirror-editor" />
-      )}
-    </div>
+        {isPreviewActive ? (
+          <MarkdownViewer markdownText={editorText} className="p-2" />
+        ) : (
+          <div ref={codeMirrorRef} data-cy="codemirror-editor" data-testid="codemirror-editor" />
+        )}
+      </div>
+    </>
   );
-};
+});
