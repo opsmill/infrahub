75ff279563797ac749f4b8c615cfd47853bbb5a9
Bilal ABBAD <bilal@opsmill.com>
2024-06-05 11:16:32 +0200

    Added new Form component + Combobox + use them on proposed change creation form (#3552)
    * Added new Form component to improve flexibility and composability
    
    * MarkdownEditor supports focus on label click and form error
    
    * Input has to same default height than other form fields
    
    * Designer feedback on form UI
    
    * added combobox component
    
    * Enable select to be associated to a label
    
    * Use new Form in proposed change creation form
    
    * fix test
    
    * added test to create and delete a proposed change
    
    * fix flaky

--- frontend/src/components/ui/input.tsx
+++ frontend/src/components/ui/input.tsx
@@ -9,7 +9,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={classNames(
-          "flex w-full rounded-md border border-gray-300 bg-custom-white p-2 text-sm placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-inset focus-visible:ring-custom-blue-600 focus-visible:border-custom-blue-600 disabled:cursor-not-allowed disabled:bg-gray-100",
+          "h-10 flex w-full rounded-md border border-gray-300 bg-custom-white p-2 text-sm placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-inset focus-visible:ring-custom-blue-600 focus-visible:border-custom-blue-600 disabled:cursor-not-allowed disabled:bg-gray-100",
           className
         )}
         ref={ref}
