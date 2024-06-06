969929b91344794425b57ab668ca3f19c1c617cb
Mikhail Yohman <mikhail.yohman@gmail.com>
2024-06-06 06:54:09 -0600

    Change DateTime.graphql and DateTime.graphql_filter to graphene.DateTime

--- backend/infrahub/types.py
+++ backend/infrahub/types.py
@@ -133,11 +133,11 @@ class TextArea(Text):
 
 class DateTime(InfrahubDataType):
     label: str = "DateTime"
-    graphql = graphene.String
+    graphql = graphene.DateTime
     graphql_query = "TextAttributeType"
     graphql_create = "TextAttributeCreate"
     graphql_update = "TextAttributeUpdate"
-    graphql_filter = graphene.String
+    graphql_filter = graphene.DateTime
     infrahub = "DateTime"
 
 
