55e0ed9e87cc5e8e3dd1f14a6da2bb9517944fd2
Bilal <bilal@opsmill.com>
2024-06-10 10:26:05 +0200

    update new password form with form v2

--- frontend/src/screens/user-profile/tab-preferences.tsx
+++ frontend/src/screens/user-profile/tab-preferences.tsx
@@ -1,47 +1,43 @@
 import { gql } from "@apollo/client";
-import { useState } from "react";
 import { toast } from "react-toastify";
 import { ALERT_TYPES, Alert } from "../../components/utils/alert";
 import { ACCOUNT_SELF_UPDATE_OBJECT } from "../../config/constants";
 import graphqlClient from "../../graphql/graphqlClientApollo";
 import { updateObjectWithId } from "../../graphql/mutations/objects/updateObjectWithId";
 import { stringifyWithoutQuotes } from "../../utils/string";
-import { DynamicFieldData } from "../edit-form-hook/dynamic-control-types";
-import { Form } from "../edit-form-hook/form";
 import Content from "../layout/content";
 import { Card } from "../../components/ui/card";
+import DynamicForm from "../../components/form/dynamic-form";
+import { DynamicFieldProps } from "../../components/form/fields/common";
 
-const fields: DynamicFieldData[] = [
+const fields: Array<DynamicFieldProps> = [
   {
     name: "newPassword",
     label: "New password",
-    type: "password",
-    value: "",
-    config: {
-      required: "Required",
+    type: "Password",
+    rules: {
+      required: true,
     },
   },
   {
     name: "confirmPassword",
     label: "Confirm password",
-    type: "password",
-    value: "",
-    config: {
-      required: "Required",
+    type: "Password",
+    rules: {
+      required: true,
+      validate: (value, fieldValues) => {
+        return value === fieldValues.newPassword || "Passwords don't match.";
+      },
     },
   },
 ];
 
 export default function TabPreferences() {
-  const [isLoading, setIsLoading] = useState(false);
-
   const onSubmit = async ({ newPassword, confirmPassword }: any) => {
     if (newPassword !== confirmPassword) {
       toast(() => <Alert type={ALERT_TYPES.WARNING} message="Passwords must be the same" />);
       return;
     }
-
-    setIsLoading(true);
 
     try {
       const mutationString = updateObjectWithId({
@@ -61,21 +57,14 @@ export default function TabPreferences() {
     } catch (error) {
       console.log("Error while updating the password: ", error);
     }
-
-    setIsLoading(false);
   };
 
   return (
     <Content className="p-2">
       <Card className="m-auto w-full max-w-md">
-        <h3 className="leading-6 font-semibold">Update your password</h3>
+        <h3 className="leading-6 font-semibold mb-4">Update your password</h3>
 
-        <Form
-          onSubmit={onSubmit}
-          fields={fields}
-          submitLabel={"Update password"}
-          isLoading={isLoading}
-        />
+        <DynamicForm fields={fields} onSubmit={onSubmit} submitLabel="Update password" />
       </Card>
     </Content>
   );
