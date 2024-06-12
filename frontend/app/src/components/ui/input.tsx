a82faeae7ba2de9762a819dda1b860713725d126
Bilal <bilal@opsmill.com>
2024-06-12 17:28:05 +0200

    Simplified Input component

--- frontend/app/src/components/ui/input.tsx
+++ frontend/app/src/components/ui/input.tsx
@@ -3,18 +3,15 @@ import { forwardRef, InputHTMLAttributes } from "react";
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={classNames(
-          "h-10 flex w-full rounded-md border border-gray-300 bg-custom-white p-2 text-sm placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-inset focus-visible:ring-custom-blue-600 focus-visible:border-custom-blue-600 disabled:cursor-not-allowed disabled:bg-gray-100",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    );
-  }
-);
+export const Input = forwardRef<HTMLInputElement, InputProps>(({ className, ...props }, ref) => {
+  return (
+    <input
+      ref={ref}
+      className={classNames(
+        "h-10 flex w-full rounded-md border border-gray-300 bg-custom-white p-2 text-sm placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-inset focus-visible:ring-custom-blue-600 focus-visible:border-custom-blue-600 disabled:cursor-not-allowed disabled:bg-gray-100",
+        className
+      )}
+      {...props}
+    />
+  );
+});
