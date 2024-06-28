dfbf290d569383503588afb05ab9044357ce839e
Bilal <bilal@opsmill.com>
2024-06-28 18:20:31 +0200

    fix test

--- frontend/app/src/screens/objects/object-header.tsx
+++ frontend/app/src/screens/objects/object-header.tsx
@@ -18,11 +18,18 @@ type ObjectHeaderProps = {
 };
 
 const ObjectHeader = ({ schema, objectId }: ObjectHeaderProps) => {
+  return objectId ? (
+    <ObjectDetailsHeader schema={schema} objectId={objectId} />
+  ) : (
+    <ObjectItemsHeader schema={schema} />
+  );
+};
+
+const ObjectItemsHeader = ({ schema }: ObjectHeaderProps) => {
   const [filters] = useFilters();
+  const { data, loading, error } = useObjectItems(schema, filters);
 
   const schemaKind = schema.kind as string;
-  const { data: objectItemsData, loading: isCountLoading, error } = useObjectItems(schema, filters);
-
   const isProfile = schema.namespace === "Profile" || schemaKind === PROFILE_KIND;
   const breadcrumbModelLabel = isProfile ? "All Profiles" : schema.label || schema.name;
 
@@ -36,15 +43,14 @@ const ObjectHeader = ({ schema, objectId }: ObjectHeaderProps) => {
             <h1 className="font-semibold text-gray-900 mr-2 hover:underline">
               {breadcrumbModelLabel}
             </h1>
-            <Badge>{isCountLoading && !error ? "..." : objectItemsData?.[schemaKind]?.count}</Badge>
+            <Badge>{loading && !error ? "..." : data?.[schemaKind]?.count}</Badge>
           </Link>
-
-          {objectId && <ObjectDetailBreadcrumb objectId={objectId} schema={schema} />}
         </div>
       }
       description={schema.description}
-      isReloadLoading={isCountLoading}
-      reload={() => graphqlClient.refetchQueries({ include: [schema.kind!] })}>
+      isReloadLoading={loading}
+      reload={() => graphqlClient.refetchQueries({ include: [schema.kind!] })}
+      data-testid="object-header">
       <ObjectHelpButton
         kind={schema.kind}
         documentationUrl={schema.documentation}
@@ -53,30 +59,52 @@ const ObjectHeader = ({ schema, objectId }: ObjectHeaderProps) => {
     </Content.Title>
   );
 };
+const ObjectDetailsHeader = ({ schema, objectId }: ObjectHeaderProps & { objectId: string }) => {
+  const { data, loading, error } = useObjectDetails(schema, objectId);
 
-const ObjectDetailBreadcrumb = ({ schema, objectId }: ObjectHeaderProps & { objectId: string }) => {
-  const { data, loading, error: objectDetailsError } = useObjectDetails(schema, objectId);
+  if (error) return null;
 
-  if (objectDetailsError) return null;
+  const schemaKind = schema.kind as string;
+  const isProfile = schema.namespace === "Profile" || schemaKind === PROFILE_KIND;
+  const breadcrumbModelLabel = isProfile ? "All Profiles" : schema.label || schema.name;
 
-  if (loading) {
-    return (
-      <>
-        <Icon icon="mdi:chevron-right" />
-
-        <Skeleton className="h-6 w-60" />
-      </>
-    );
-  }
-
-  const objectDetailsData = data[schema.kind!]?.edges[0]?.node;
+  const objectDetailsData = data?.[schema.kind!]?.edges[0]?.node;
 
   return (
-    <>
-      <Icon icon="mdi:chevron-right" />
+    <Content.Title
+      title={
+        <div className="text-md flex gap-2 items-center">
+          <Link
+            to={constructPath(`/objects/${isProfile ? PROFILE_KIND : schemaKind}`)}
+            className="flex items-center cursor-pointer">
+            <h1 className="font-semibold text-gray-900 mr-2 hover:underline">
+              {breadcrumbModelLabel}
+            </h1>
+          </Link>
 
-      <p>{objectDetailsData.display_label}</p>
-    </>
+          {loading ? (
+            <>
+              <Icon icon="mdi:chevron-right" />
+              <Skeleton className="h-6 w-60" />
+            </>
+          ) : (
+            <>
+              <Icon icon="mdi:chevron-right" />
+              <p>{objectDetailsData.display_label}</p>
+            </>
+          )}
+        </div>
+      }
+      description={schema.description}
+      isReloadLoading={loading}
+      reload={() => graphqlClient.refetchQueries({ include: [schema.kind!] })}
+      data-testid="object-header">
+      <ObjectHelpButton
+        kind={schema.kind}
+        documentationUrl={schema.documentation}
+        className="ml-auto"
+      />
+    </Content.Title>
   );
 };
 
