8209b44052d78d7a76ec099ee90e3ff97cf3d0ce
Bilal <bilal@opsmill.com>
2024-06-28 10:03:21 +0200

    fix conflict

--- frontend/app/src/hooks/useObjectItems.ts
+++ frontend/app/src/hooks/useObjectItems.ts
@@ -3,12 +3,10 @@ import { IModelSchema } from "@/state/atoms/schema.atom";
 import { gql } from "@apollo/client";
 import { getObjectItemsPaginated } from "@/graphql/queries/objects/getObjectItems";
 import { getObjectAttributes, getObjectRelationships } from "@/utils/getSchemaObjectColumns";
-import usePagination from "@/hooks/usePagination";
 import useFilters from "@/hooks/useFilters";
 
 export const useObjectItems = (schema?: IModelSchema) => {
   const [filters] = useFilters();
-  const [pagination] = usePagination();
 
   // All the filter values are being sent out as strings inside quotes.
   // This will not work if the type of filter value is not string.
@@ -27,11 +25,6 @@ export const useObjectItems = (schema?: IModelSchema) => {
 
         return `${row.name}: ${row.value}`;
       }),
-    // Add pagination filters
-    ...[
-      { name: "offset", value: pagination.offset },
-      { name: "limit", value: pagination.limit },
-    ].map((row) => `${row.name}: ${row.value}`),
   ].join(",");
 
   const attributes = getObjectAttributes({ schema, forListView: true });
