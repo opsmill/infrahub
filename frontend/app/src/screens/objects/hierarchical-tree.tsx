6b2eb9befc1974b3b22abe7a66bf8cd9370ce6d0
pa-lem <pa-lem@hotmail.fr>
2024-07-01 15:48:25 +0200

    cleanup and update hierarchical tree fetch

--- frontend/app/src/screens/objects/hierarchical-tree.tsx
+++ frontend/app/src/screens/objects/hierarchical-tree.tsx
@@ -29,51 +29,51 @@ export const HierarchicalTree = ({ schema, currentNodeId, className }: Hierarchi
   const [treeData, setTreeData] = useState<TreeProps["data"]>(EMPTY_IPAM_TREE);
   const [expandedIds, setExpandedIds] = useState<NodeId[]>([]);
 
-  const [isLoading, setLoading] = useState(true);
-
   const query = gql(objectTreeQuery({ kind: schema.kind, id: currentNodeId }));
-  const [getObjectTree] = useLazyQuery(query);
+  const [getObjectTree, { loading }] = useLazyQuery(query);
+
+  const fetchTree = async () => {
+    const { data } = await getObjectTree();
+
+    let rootNodeIds: string[] = [];
+    const objectTreeData = data[schema.kind as string].edges;
+    const tree = objectTreeData.map(({ node }: { node: any }) => {
+      const nodeId = node?.id;
+      const parentId = node.parent?.node?.id ?? IPAM_TREE_ROOT_ID;
+
+      if (parentId === IPAM_TREE_ROOT_ID) rootNodeIds.push(nodeId);
+
+      return {
+        id: node.id,
+        name: node.display_label,
+        children: objectTreeData
+          .filter((object: any) => object.node?.parent?.node?.id === nodeId)
+          .map((object: any) => object.node?.id),
+        parent: parentId,
+        isBranch: node.children.count > 0,
+        metadata: {
+          kind: node.__typename,
+        },
+      };
+    });
+
+    const newTree = [{ ...ROOT_TREE_ITEM, children: rootNodeIds }, ...tree];
+
+    if (currentNodeId) {
+      const ancestorIds = getTreeItemAncestors(newTree, currentNodeId).map(({ id }) => id);
+      setExpandedIds(ancestorIds);
+    }
+    setTreeData(newTree);
+  };
 
   useEffect(() => {
-    getObjectTree().then(({ data }) => {
-      let rootNodeIds: string[] = [];
-      const objectTreeData = data[schema.kind as string].edges;
-      const tree = objectTreeData.map(({ node }: { node: any }) => {
-        const nodeId = node?.id;
-        const parentId = node.parent?.node?.id ?? IPAM_TREE_ROOT_ID;
-
-        if (parentId === IPAM_TREE_ROOT_ID) rootNodeIds.push(nodeId);
-
-        return {
-          id: node.id,
-          name: node.display_label,
-          children: objectTreeData
-            .filter((object: any) => object.node?.parent?.node?.id === nodeId)
-            .map((object: any) => object.node?.id),
-          parent: parentId,
-          isBranch: node.children.count > 0,
-          metadata: {
-            kind: node.__typename,
-          },
-        };
-      });
-
-      const newTree = [{ ...ROOT_TREE_ITEM, children: rootNodeIds }, ...tree];
-
-      if (currentNodeId) {
-        const ancestorIds = getTreeItemAncestors(newTree, currentNodeId).map(({ id }) => id);
-        setExpandedIds(ancestorIds);
-      }
-      setTreeData(newTree);
-      setLoading(false);
-    });
+    fetchTree();
   }, [schema.kind, currentNodeId]);
-
-  if (isLoading && treeData.length === 1) return null;
 
   return (
     <Card className={className}>
       <Tree
+        loading={loading}
         data={treeData}
         itemContent={ObjectTreeItem}
         defaultExpandedIds={expandedIds}
