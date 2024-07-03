0debcc62602b62aa938ae6890f70b2cbebe47028
Fatih Acar <fatih@opsmill.com>
2024-07-03 11:32:10 +0200

    feat(ci): use demo data for benchmarks
    Also use a better query for stress testing.
    
    Signed-off-by: Fatih Acar <fatih@opsmill.com>

--- backend/tests/benchmark/conftest.py
+++ backend/tests/benchmark/conftest.py
@@ -1,6 +1,25 @@
 import asyncio
+import os
 
 import pytest
+
+from infrahub.core import registry
+from infrahub.core.branch import Branch
+from infrahub.core.initialization import (
+    create_default_branch,
+    create_global_branch,
+    create_root_node,
+)
+from infrahub.core.schema import (
+    SchemaRoot,
+    core_models,
+    internal_schema,
+)
+from infrahub.core.schema_manager import SchemaBranch, SchemaManager
+from infrahub.core.utils import delete_all_nodes
+from infrahub.database import InfrahubDatabase
+
+USE_DEMO_DATA = bool(os.getenv("INFRAHUB_BENCHMARK_USE_DEMO", False))
 
 
 @pytest.fixture
@@ -28,3 +47,45 @@ async def aio_benchmark(benchmark, event_loop):
             return benchmark(func, *args, **kwargs)
 
     return _wrapper
+
+
+@pytest.fixture
+async def register_core_models_schema(default_branch: Branch, register_internal_models_schema) -> SchemaBranch:
+    if not USE_DEMO_DATA:
+        schema = SchemaRoot(**core_models)
+        schema_branch = registry.schema.register_schema(schema=schema, branch=default_branch.name)
+        default_branch.update_schema_hash()
+        return schema_branch
+
+
+@pytest.fixture
+async def reset_environment(db: InfrahubDatabase) -> None:
+    if not USE_DEMO_DATA:
+        registry.delete_all()
+        await delete_all_nodes(db=db)
+        await create_root_node(db=db)
+
+
+@pytest.fixture
+async def default_branch(reset_environment, db: InfrahubDatabase) -> Branch:
+    branch: Branch
+
+    if not USE_DEMO_DATA:
+        branch = await create_default_branch(db=db)
+        await create_global_branch(db=db)
+        registry.schema = SchemaManager()
+    else:
+        branch = registry.default_branch
+    return branch
+
+
+@pytest.fixture
+async def register_default_schema(db: InfrahubDatabase, default_branch: Branch) -> SchemaBranch:
+    if not USE_DEMO_DATA:
+        schema_branch = registry.schema.get_schema_branch(name=default_branch.name)
+        schema_branch.load_schema(schema=SchemaRoot(**internal_schema))
+        schema_branch.load_schema(schema=SchemaRoot(**core_models))
+        schema_branch.process()
+        default_branch.update_schema_hash()
+        await default_branch.save(db=db)
+        return schema_branch
--- backend/tests/benchmark/test_graphql_query.py
+++ backend/tests/benchmark/test_graphql_query.py
@@ -3,51 +3,12 @@ import os
 import pytest
 from graphql import graphql
 
-from infrahub.core import registry
 from infrahub.core.branch import Branch
-from infrahub.core.initialization import (
-    create_default_branch,
-    create_global_branch,
-    create_root_node,
-)
-from infrahub.core.schema import (
-    SchemaRoot,
-    core_models,
-    internal_schema,
-)
-from infrahub.core.schema_manager import SchemaBranch, SchemaManager
-from infrahub.core.utils import delete_all_nodes
 from infrahub.database import InfrahubDatabase
 from infrahub.graphql import prepare_graphql_params
 from infrahub.test_data.dataset04 import load_data
 
 NBR_WARMUP = int(os.getenv("INFRAHUB_BENCHMARK_NBR_WARMUP", 5))
-
-
-@pytest.fixture(scope="module")
-async def reset_environment(db: InfrahubDatabase) -> None:
-    registry.delete_all()
-    await delete_all_nodes(db=db)
-    await create_root_node(db=db)
-
-
-@pytest.fixture(scope="module")
-async def default_branch(reset_environment, db: InfrahubDatabase) -> Branch:
-    branch = await create_default_branch(db=db)
-    await create_global_branch(db=db)
-    registry.schema = SchemaManager()
-    return branch
-
-
-@pytest.fixture(scope="module")
-async def register_default_schema(db: InfrahubDatabase, default_branch: Branch) -> SchemaBranch:
-    schema_branch = registry.schema.get_schema_branch(name=default_branch.name)
-    schema_branch.load_schema(schema=SchemaRoot(**internal_schema))
-    schema_branch.load_schema(schema=SchemaRoot(**core_models))
-    schema_branch.process()
-    default_branch.update_schema_hash()
-    await default_branch.save(db=db)
-    return schema_branch
 
 
 @pytest.fixture(scope="module")
@@ -100,20 +61,18 @@ def test_query_one_model(exec_async, aio_benchmark, db: InfrahubDatabase, defaul
 def test_query_rel_many(exec_async, aio_benchmark, db: InfrahubDatabase, default_branch: Branch, dataset04):
     query = """
     query {
-        CoreGraphQLQuery {
-            count
+        InfraDevice {
             edges {
                 node {
-                    id
-                    display_label
                     name {
                         value
                     }
-                    tags {
+                    interfaces {
                         edges {
                             node {
-                                id
-                                display_label
+                                name {
+                                    value
+                                }
                             }
                         }
                     }
