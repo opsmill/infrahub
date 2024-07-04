dc5d887cfbb087ee22d71613faac7212c4926b59
Aaron McCarty <aaron@opsmill.com>
2024-07-04 12:35:46 -0700

    unit tests for new constraint validator

--- backend/infrahub/core/relationship/constraints/profiles_kind.py
+++ backend/infrahub/core/relationship/constraints/profiles_kind.py
@@ -4,13 +4,15 @@ from typing import TYPE_CHECKING, Optional
 
 from infrahub.core import registry
 from infrahub.core.schema import NodeSchema
+from infrahub.core.schema.generic_schema import GenericSchema
 from infrahub.exceptions import ValidationError
 
 from .interface import RelationshipManagerConstraintInterface
 
 if TYPE_CHECKING:
     from infrahub.core.branch import Branch
-    from infrahub.core.relationship.model import Relationship, RelationshipManager
+    from infrahub.core.node import Node
+    from infrahub.core.relationship.model import RelationshipManager
     from infrahub.core.schema import MainSchemaTypes
     from infrahub.database import InfrahubDatabase
 
@@ -29,26 +31,25 @@ class RelationshipProfilesKindConstraint(RelationshipManagerConstraintInterface)
         if len(profile_relationships) == 0:
             return
 
+        if not node_schema.generate_profile:
+            raise ValidationError({"profiles": f"{node_schema.kind} does not allow profiles"})
+
         allowed_profile_kinds = {f"Profile{node_schema.kind}"}
 
         for generic_schema_name in node_schema.inherit_from:
             generic_schema = self.schema_branch.get(name=generic_schema_name, duplicate=False)
-            try:
-                generic_profiles_schema = generic_schema.get_relationship(name="profiles")
-            except ValueError:
-                continue
-            allowed_profile_kinds.add(generic_profiles_schema.peer)
+            if isinstance(generic_schema, GenericSchema) and generic_schema.generate_profile:
+                allowed_profile_kinds.add(f"Profile{generic_schema.kind}")
 
-        illegal_profile_relationships: list[Relationship] = []
+        illegal_profile_nodes: list[Node] = []
         for profile_relationship in profile_relationships:
-            if profile_relationship.get_kind() not in allowed_profile_kinds:
-                illegal_profile_relationships.append(profile_relationship)
-        if not illegal_profile_relationships:
+            peer_node = await profile_relationship.get_peer(db=self.db)
+            if peer_node.get_kind() not in allowed_profile_kinds:
+                illegal_profile_nodes.append(peer_node)
+        if not illegal_profile_nodes:
             return
 
-        error_str_parts = [
-            f"peer {p_rel.get_peer_id()} is of kind {p_rel.get_kind()}" for p_rel in illegal_profile_relationships
-        ]
+        error_str_parts = [f"peer {p_rel.get_id()} is of kind {p_rel.get_kind()}" for p_rel in illegal_profile_nodes]
         error_str = ", ".join(error_str_parts)
-        error_str += f". only {allowed_profile_kinds} are allowed"
+        error_str += f". only {sorted(allowed_profile_kinds)} are allowed"
         raise ValidationError({"profiles": error_str})
