4c0d5d6d1da36b112d3c4d1c1e207d5227736ebf
pa-lem <pa-lem@hotmail.fr>
2024-07-04 11:32:44 +0200

    adds no data found for object list

--- frontend/app/src/pages/objects/layout.tsx
+++ frontend/app/src/pages/objects/layout.tsx
@@ -3,6 +3,7 @@ import { Outlet, useParams } from "react-router-dom";
 import ObjectHeader from "@/screens/objects/object-header";
 import { genericsState, profilesAtom, schemaState } from "@/state/atoms/schema.atom";
 import { HierarchicalTree } from "@/screens/objects/hierarchical-tree";
+import NoDataFound from "@/screens/errors/no-data-found";
 
 const ObjectPageLayout = () => {
   const { objectKind, objectid } = useParams();
@@ -13,7 +14,7 @@ const ObjectPageLayout = () => {
 
   const schema = [...nodes, ...generics, ...profiles].find(({ kind }) => kind === objectKind);
 
-  if (!schema) return null;
+  if (!schema) return <NoDataFound message="No schema found for this kind." />;
 
   const isHierarchicalModel = "hierarchical" in schema && schema.hierarchical;
   const inheritFormHierarchicalModel = "hierarchy" in schema && schema.hierarchy;
