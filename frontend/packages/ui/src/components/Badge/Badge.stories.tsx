3ba4a6fbf88e9bf140e8d3407038a3f3dc29cd4e
Bilal ABBAD <bilal@opsmill.com>
2024-07-10 18:31:17 +0200

    Update Storybook to 8.2.1 + fix app building (#3810)

--- frontend/packages/ui/src/components/Badge/Badge.stories.tsx
+++ frontend/packages/ui/src/components/Badge/Badge.stories.tsx
@@ -7,7 +7,6 @@ const meta: Meta<typeof Badge> = {
   parameters: {
     layout: "centered",
   },
-  tags: ["autodocs"],
 };
 
 export default meta;
--- frontend/packages/ui/vite.config.ts
+++ frontend/packages/ui/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
 });
