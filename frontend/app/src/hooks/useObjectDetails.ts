fbd0aab6f2b28797d1738c739caa2ac1382f6dd8
pa-lem <pa-lem@hotmail.fr>
2024-07-18 16:04:01 +0200

    lint

--- frontend/app/src/hooks/useObjectDetails.ts
+++ frontend/app/src/hooks/useObjectDetails.ts
@@ -3,7 +3,7 @@ import { useAtomValue } from "jotai";
 import useQuery from "@/hooks/useQuery";
 import { genericsState, IModelSchema } from "@/state/atoms/schema.atom";
 import { getObjectDetailsPaginated } from "@/graphql/queries/objects/getObjectDetails";
-import { ACCOUNT_OBJECT, PROFILE_KIND, TASK_OBJECT } from "@/config/constants";
+import { PROFILE_KIND, TASK_OBJECT } from "@/config/constants";
 import { getSchemaObjectColumns, getTabs } from "@/utils/getSchemaObjectColumns";
 import { isGeneric } from "@/utils/common";
 
@@ -24,10 +24,10 @@ export const useObjectDetails = (schema: IModelSchema, objectId: string) => {
           objectid: objectId,
           // Do not query profiles on profiles objects
           queryProfiles:
-          !profileGenericSchema?.used_by?.includes(schema?.kind!) &&
-          schema?.kind !== PROFILE_KIND &&
-          !isGeneric(schema) &&
-          schema?.generate_profile
+            !profileGenericSchema?.used_by?.includes(schema?.kind!) &&
+            schema?.kind !== PROFILE_KIND &&
+            !isGeneric(schema) &&
+            schema?.generate_profile,
         })
       : // Empty query to make the gql parsing work
         // TODO: Find another solution for queries while loading schema
