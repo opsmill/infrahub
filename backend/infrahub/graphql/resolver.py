ed66956b22d2e21e70b74bb5e834e005ee082847
Damien Garros <dgarros@gmail.com>
2024-07-19 15:09:57 +0200

    Add option to fetch peers nodes in query_peers

--- backend/infrahub/graphql/resolver.py
+++ backend/infrahub/graphql/resolver.py
@@ -79,6 +79,7 @@ async def default_resolver(*args, **kwargs):
             at=context.at,
             branch=context.branch,
             branch_agnostic=node_rel.branch is BranchSupportType.AGNOSTIC,
+            fetch_peers=True,
         )
 
         if node_rel.cardinality == "many":
@@ -134,6 +135,7 @@ async def single_relationship_resolver(parent: dict, info: GraphQLResolveInfo, *
             at=context.at,
             branch=context.branch,
             branch_agnostic=node_rel.branch is BranchSupportType.AGNOSTIC,
+            fetch_peers=True,
         )
 
         if not objs:
@@ -231,6 +233,7 @@ async def many_relationship_resolver(
             at=context.at,
             branch=context.branch,
             branch_agnostic=node_rel.branch is BranchSupportType.AGNOSTIC,
+            fetch_peers=True,
         )
 
         if not objs:
