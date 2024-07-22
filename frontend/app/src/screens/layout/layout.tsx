6668e1cb12477fe11838649d469b38f3a45b4460
pa-lem <pa-lem@hotmail.fr>
2024-07-22 09:59:09 +0200

    update message

--- frontend/app/src/screens/layout/layout.tsx
+++ frontend/app/src/screens/layout/layout.tsx
@@ -68,7 +68,7 @@ function Layout() {
   if (isLoadingBranches) {
     return (
       <div className="w-screen h-screen flex items-center justify-center">
-        <LoadingScreen />;
+        <LoadingScreen message="Loading branches..." />;
       </div>
     );
   }
