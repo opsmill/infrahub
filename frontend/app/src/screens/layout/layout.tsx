8666023a893cb0d2a83114594d11b8a3f8a4e048
pa-lem <pa-lem@hotmail.fr>
2024-07-22 09:57:38 +0200

    center branch loader

--- frontend/app/src/screens/layout/layout.tsx
+++ frontend/app/src/screens/layout/layout.tsx
@@ -67,7 +67,7 @@ function Layout() {
 
   if (isLoadingBranches) {
     return (
-      <div className="w-screen h-screen flex">
+      <div className="w-screen h-screen flex items-center justify-center">
         <LoadingScreen />;
       </div>
     );
