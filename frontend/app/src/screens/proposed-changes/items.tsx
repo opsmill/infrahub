35a64601ad8bfcd76f4ea4604cc0e41ab9e5b56a
bilalabbad <bilal@opsmill.com>
2024-08-01 17:32:03 +0200

    added toast when delete proposed change returns error

--- frontend/app/src/screens/proposed-changes/items.tsx
+++ frontend/app/src/screens/proposed-changes/items.tsx
@@ -86,6 +86,12 @@ export const ProposedChangesPage = () => {
       );
     } catch (error) {
       console.error("Error while deleting proposed change:", error);
+      toast(
+        <Alert
+          type={ALERT_TYPES.ERROR}
+          message="An error occurred while deleting the proposed changes"
+        />
+      );
     }
   };
 
