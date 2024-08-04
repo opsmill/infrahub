dbcd949d28486a5dc8135b3cd883f107abffddcf
Damien Garros <dgarros@gmail.com>
2024-08-04 12:23:53 +0200

    Reapply InfrahubKind.THREAD

--- backend/infrahub/core/schema/definitions/core.py
+++ backend/infrahub/core/schema/definitions/core.py
@@ -1045,7 +1045,7 @@ core_models: dict[str, Any] = {
                 },
                 {
                     "name": "threads",
-                    "peer": "CoreThread",
+                    "peer": InfrahubKind.THREAD,
                     "identifier": "proposedchange__thread",
                     "kind": "Component",
                     "optional": True,
@@ -1070,7 +1070,7 @@ core_models: dict[str, Any] = {
             "include_in_menu": False,
             "label": "Change Thread",
             "branch": BranchSupportType.AGNOSTIC.value,
-            "inherit_from": ["CoreThread"],
+            "inherit_from": [InfrahubKind.THREAD],
             "generate_profile": False,
             "attributes": [],
             "relationships": [],
@@ -1082,7 +1082,7 @@ core_models: dict[str, Any] = {
             "include_in_menu": False,
             "label": "Thread - File",
             "branch": BranchSupportType.AGNOSTIC.value,
-            "inherit_from": ["CoreThread"],
+            "inherit_from": [InfrahubKind.THREAD],
             "generate_profile": False,
             "attributes": [
                 {"name": "file", "kind": "Text", "optional": True},
