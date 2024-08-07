7427aba4743a9c02fe6dc3fa44c01cf6f6593f94
Bearchitek <benoit@opsmill.com>
2024-08-07 17:39:55 +0200

    Fix issue when you are trying to sync only one attribute, use as identifier

--- sync/infrahub-sync/infrahub_sync/generator/utils.py
+++ sync/infrahub-sync/infrahub_sync/generator/utils.py
@@ -4,7 +4,7 @@ from typing import List
 def list_to_set(items: List[str]) -> str:
     """Convert a list in a string representation of a Set."""
     if not items:
-        return "None"
+        return "()"
 
     response = '"' + '", "'.join(items) + '"'
     if len(items) == 1:
