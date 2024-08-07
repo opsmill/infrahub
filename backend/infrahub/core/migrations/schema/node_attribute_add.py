aa2604c5f3972ca505f6a3163b4e3f957ff06caf
Damien Garros <dgarros@gmail.com>
2024-08-07 19:03:14 +0200

    Add db migration for CorePasswordCredential (#4015)
    * Add db migration for GitPasswordCredential
    
    * Set identifier for relationship between gitrepo and credentials
    
    * Fix issue related to memgraph
    
    * Add support for repository without creds
    
    * Extract attribute_add query from node_attribute_add schema migration
    
    * Fix issue with root in Migration013ConvertCoreRepositoryWithCred query
    Add specific DeleteUsernamePassword query per kind
    Add migration to add admin_status to all existing node
    
    * Fix issues with DeleteElementInSchemaQuery
    extend to work with Generic and add DISTINCT

--- backend/infrahub/core/migrations/schema/node_attribute_add.py
+++ backend/infrahub/core/migrations/schema/node_attribute_add.py
@@ -1,70 +1,29 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Sequence
-
-from infrahub.core.constants import NULL_VALUE, RelationshipStatus
+from typing import Any, Sequence
 
 from ..query import AttributeMigrationQuery
+from ..query.attribute_add import AttributeAddQuery
 from ..shared import AttributeSchemaMigration
 
-if TYPE_CHECKING:
-    from infrahub.database import InfrahubDatabase
 
-
-class NodeAttributeAddMigrationQuery01(AttributeMigrationQuery):
+class NodeAttributeAddMigrationQuery01(AttributeMigrationQuery, AttributeAddQuery):
     name = "migration_node_attribute_add_01"
 
-    async def query_init(self, db: InfrahubDatabase, **kwargs: dict[str, Any]) -> None:
-        branch_filter, branch_params = self.branch.get_query_filter_path(at=self.at.to_string())
-        self.params.update(branch_params)
-
-        self.params["node_kind"] = self.migration.new_schema.kind
-        self.params["attr_name"] = self.migration.new_attribute_schema.name
-        self.params["attr_type"] = self.migration.new_attribute_schema.kind
-        self.params["branch_support"] = self.migration.new_attribute_schema.get_branch().value
-
-        if self.migration.new_attribute_schema.default_value:
-            self.params["attr_value"] = self.migration.new_attribute_schema.default_value
-        else:
-            self.params["attr_value"] = NULL_VALUE
-
-        self.params["rel_props"] = {
-            "branch": self.branch.name,
-            "branch_level": self.branch.hierarchy_level,
-            "status": RelationshipStatus.ACTIVE.value,
-            "from": self.at.to_string(),
-        }
-
-        self.params["is_protected_default"] = False
-        self.params["is_visible_default"] = True
-
-        query = """
-        MATCH p = (n:Node)
-        WHERE $node_kind IN LABELS(n) AND NOT exists((n)-[:HAS_ATTRIBUTE]-(:Attribute {name: $attr_name}))
-        CALL {
-            WITH n
-            MATCH (root:Root)<-[r:IS_PART_OF]-(n)
-            WHERE %(branch_filter)s
-            RETURN n as n1, r as r1
-            ORDER BY r.branch_level DESC, r.from DESC
-            LIMIT 1
-        }
-        WITH n1 as n, r1 as rb
-        WHERE rb.status = "active"
-        MERGE (av:AttributeValue { value: $attr_value, is_default: true })
-        MERGE (is_protected_value:Boolean { value: $is_protected_default })
-        MERGE (is_visible_value:Boolean { value: $is_visible_default })
-        WITH n, av, is_protected_value, is_visible_value
-        CREATE (a:Attribute { name: $attr_name, branch_support: $branch_support })
-        CREATE (n)-[:HAS_ATTRIBUTE $rel_props ]->(a)
-        CREATE (a)-[:HAS_VALUE $rel_props ]->(av)
-        CREATE (a)-[:IS_PROTECTED $rel_props]->(is_protected_value)
-        CREATE (a)-[:IS_VISIBLE $rel_props]->(is_visible_value)
-        """ % {"branch_filter": branch_filter}
-        self.add_to_query(query)
-        self.return_labels = ["n.uuid", "a.uuid"]
-
-        self.add_to_query(db.render_uuid_generation(node_label="a", node_attr="uuid"))
+    def __init__(
+        self,
+        migration: AttributeSchemaMigration,
+        **kwargs: Any,
+    ):
+        super().__init__(
+            migration=migration,
+            node_kind=migration.new_schema.kind,
+            attribute_name=migration.new_attribute_schema.name,
+            attribute_kind=migration.new_attribute_schema.kind,
+            branch_support=migration.new_attribute_schema.get_branch().value,
+            default_value=migration.new_attribute_schema.default_value,
+            **kwargs,
+        )
 
 
 class NodeAttributeAddMigration(AttributeSchemaMigration):
