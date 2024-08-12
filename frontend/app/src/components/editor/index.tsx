ac19ed8aacb6e67dc0c438e3f7c7d166045c0ad9
Bilal ABBAD <bilal@opsmill.com>
2024-08-12 19:22:03 +0200

    [FIX] Added value props to markdown editor + reset comment after submit (#4079)

--- frontend/app/src/components/editor/index.tsx
+++ frontend/app/src/components/editor/index.tsx
@@ -7,6 +7,7 @@ import { MarkdownViewer } from "./markdown-viewer";
 type MarkdownEditorProps = {
   className?: string;
   defaultValue?: string;
+  value?: string;
   disabled?: boolean;
   onChange?: (value: string) => void;
   placeholder?: string;
@@ -16,63 +17,67 @@ type MarkdownEditorProps = {
 export const MarkdownEditor: FC<MarkdownEditorProps> = forwardRef<
   HTMLButtonElement,
   MarkdownEditorProps
->(({ id, className = "", defaultValue = "", disabled = false, onChange, placeholder }, ref) => {
-  const [isPreviewActive, setPreviewActive] = useState<boolean>(false);
-  const [editorText, setEditorText] = useState<string>(defaultValue);
-  const codeMirrorRef = useRef<HTMLDivElement>(null);
+>(
+  (
+    { value, id, className = "", defaultValue = "", disabled = false, onChange, placeholder },
+    ref
+  ) => {
+    const [isPreviewActive, setPreviewActive] = useState<boolean>(false);
+    const codeMirrorRef = useRef<HTMLDivElement>(null);
 
-  const handleTextChange = (value: string) => {
-    setEditorText(value);
-    if (onChange) onChange(value);
-  };
+    const handleTextChange = (value: string) => {
+      if (onChange) onChange(value);
+    };
 
-  const codeMirror = useCodeMirror(codeMirrorRef.current, {
-    placeholder,
-    defaultValue,
-    onChange: handleTextChange,
-  });
+    const codeMirror = useCodeMirror(codeMirrorRef.current, {
+      placeholder,
+      defaultValue,
+      value,
+      onChange: handleTextChange,
+    });
 
-  if (disabled) {
-    return (
-      <MarkdownViewer
-        markdownText={editorText}
-        className="w-full bg-gray-100 min-h-10 rounded-md p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 cursor-not-allowed"
-      />
-    );
-  }
-
-  return (
-    <>
-      {id && (
-        <button
-          id={id}
-          ref={ref}
-          type="button"
-          onClick={() => codeMirror.view?.focus()} // for E2E
-          onFocus={() => codeMirror.view?.focus()}
-          className="w-0 h-0"
+    if (disabled) {
+      return (
+        <MarkdownViewer
+          markdownText={codeMirror.view?.state?.doc.toString()}
+          className="w-full bg-gray-100 min-h-10 rounded-md p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 cursor-not-allowed"
         />
-      )}
-      <div
-        className={classNames(
-          `
+      );
+    }
+
+    return (
+      <>
+        {id && (
+          <button
+            id={id}
+            ref={ref}
+            type="button"
+            onClick={() => codeMirror.view?.focus()} // for E2E
+            onFocus={() => codeMirror.view?.focus()}
+            className="w-0 h-0"
+          />
+        )}
+        <div
+          className={classNames(
+            `
         bg-white rounded-md border border-gray-300 shadow-sm
         focus-within:outline focus-within:outline-custom-blue-600 focus-within:border-custom-blue-600
         `,
-          className
-        )}>
-        <MarkdownEditorHeader
-          codeMirror={codeMirror}
-          previewMode={isPreviewActive}
-          onPreviewToggle={() => setPreviewActive((prev) => !prev)}
-        />
+            className
+          )}>
+          <MarkdownEditorHeader
+            codeMirror={codeMirror}
+            previewMode={isPreviewActive}
+            onPreviewToggle={() => setPreviewActive((prev) => !prev)}
+          />
 
-        {isPreviewActive ? (
-          <MarkdownViewer markdownText={editorText} className="p-2" />
-        ) : (
-          <div ref={codeMirrorRef} data-cy="codemirror-editor" data-testid="codemirror-editor" />
-        )}
-      </div>
-    </>
-  );
-});
+          {isPreviewActive ? (
+            <MarkdownViewer markdownText={codeMirror.view?.state?.doc.toString()} className="p-2" />
+          ) : (
+            <div ref={codeMirrorRef} data-cy="codemirror-editor" data-testid="codemirror-editor" />
+          )}
+        </div>
+      </>
+    );
+  }
+);
