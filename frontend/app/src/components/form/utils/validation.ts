7d076c8d6d473a2afaf66d0da7d827db09c25b89
bilalabbad <bilal@opsmill.com>
2024-08-13 17:44:59 +0200

    fix required on branch form

--- frontend/app/src/components/form/utils/validation.ts
+++ frontend/app/src/components/form/utils/validation.ts
@@ -3,3 +3,12 @@ import { FormFieldValue } from "@/components/form/type";
 export const isRequired = ({ value }: FormFieldValue) => {
   return (value !== null && value !== undefined && value !== "") || "Required";
 };
+
+export const isMinLength =
+  (minLength: number) =>
+  ({ value }: FormFieldValue) => {
+    if (!value) return "Required";
+    if (typeof value !== "string") return true;
+
+    return value.length >= minLength || `Name must be at least ${minLength} characters long`;
+  };
