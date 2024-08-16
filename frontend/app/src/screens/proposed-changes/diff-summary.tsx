568422f41bfcf6862bd2d278866e44ab8ac538c0
bilalabbad <bilal@opsmill.com>
2024-08-16 09:58:21 +0200

    [FIX] Update query name after backend update

--- frontend/app/src/screens/proposed-changes/diff-summary.tsx
+++ frontend/app/src/screens/proposed-changes/diff-summary.tsx
@@ -1,21 +1,20 @@
 import { Badge } from "@/components/ui/badge";
-import { getProposedChangesDiffSummary } from "@/graphql/queries/proposed-changes/getProposedChangesDiffSummary";
 import useQuery from "@/hooks/useQuery";
-import { gql } from "@apollo/client";
 import { Icon } from "@iconify-icon/react";
 import ErrorScreen from "../errors/error-screen";
 import LoadingScreen from "../loading-screen/loading-screen";
+import { getProposedChangesDiffSummary } from "@/graphql/queries/proposed-changes/getProposedChangesDiffSummary";
 
 type tProposedChangesDiffSummary = {
   branch: string;
 };
 
 export const ProposedChangesDiffSummary = ({ branch }: tProposedChangesDiffSummary) => {
-  const query = gql`
-    ${getProposedChangesDiffSummary}
-  `;
-
-  const { loading, error, data = {} } = useQuery(query, { variables: { branch } });
+  const {
+    loading,
+    error,
+    data = {},
+  } = useQuery(getProposedChangesDiffSummary, { variables: { branch } });
 
   if (error) {
     return <ErrorScreen message="An error occured while fetching diff summary." />;
@@ -25,22 +24,22 @@ export const ProposedChangesDiffSummary = ({ branch }: tProposedChangesDiffSumma
     <div className="flex gap-2">
       <Badge className="rounded-full" variant="green">
         <Icon icon="mdi:plus-circle-outline" className="text-xs mr-1" />
-        {loading ? <LoadingScreen size={8} hideText /> : data.DiffTree.num_added}
+        {loading ? <LoadingScreen size={8} hideText /> : data.DiffTreeSummary.num_added}
       </Badge>
 
       <Badge className="rounded-full" variant="red">
         <Icon icon="mdi:minus-circle-outline" className="text-xs mr-1" />
-        {loading ? <LoadingScreen size={8} hideText /> : data.DiffTree.num_removed}
+        {loading ? <LoadingScreen size={8} hideText /> : data.DiffTreeSummary.num_removed}
       </Badge>
 
       <Badge className="rounded-full" variant="blue">
         <Icon icon="mdi:circle-arrows" className="text-xs mr-1" />
-        {loading ? <LoadingScreen size={8} hideText /> : data.DiffTree.num_updated}
+        {loading ? <LoadingScreen size={8} hideText /> : data.DiffTreeSummary.num_updated}
       </Badge>
 
       {/* <Badge className="rounded-full" variant="yellow">
         <Icon icon="mdi:warning-outline" className="text-xs mr-1" />
-        {loading ? <LoadingScreen size={8} hideText /> : data.DiffTree.num_conflicts}
+        {loading ? <LoadingScreen size={8} hideText /> : data.DiffTreeSummary.num_conflicts}
       </Badge> */}
     </div>
   );
