7d6cfc6433b4f05f6ace1f9ab73c058237a8551a
Fatih Acar <fatih@opsmill.com>
2024-08-19 19:09:20 +0200

    fix(codespaces): use two worker when prebuilding
    Prebuild jobs run with 2 vCPUs (show cpuinfo at startup).
    Sleep for 2 minutes because starting infrahub takes at least 90s.
    Schema loading also takes 90s.
    Set username of rabbitmq docker compose to prevent EACCES error on
    codespaces startup.
    
    Signed-off-by: Fatih Acar <fatih@opsmill.com>

--- .devcontainer/updateContentCommand.sh
+++ .devcontainer/updateContentCommand.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+export WEB_CONCURRENCY=2
 invoke demo.build demo.start
-sleep 30
-invoke demo.load-infra-schema demo.load-infra-data
+sleep 120
+docker logs infrahub-infrahub-server-1
+invoke demo.load-infra-schema
+docker logs infrahub-infrahub-server-1
+sleep 90
+docker logs infrahub-infrahub-server-1
+invoke demo.load-infra-data
 invoke demo.stop
