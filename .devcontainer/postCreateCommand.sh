03527065e0e7aaa58334a568a5712029b9d66a86
Fatih Acar <fatih@opsmill.com>
2024-08-19 19:07:54 +0200

    Revert "fix(codespaces): set env var before demo.start"
    This reverts commit 52794b048ae4b6629e3454c8b2144d9f008b47bb.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 git pull
-export INFRAHUB_IMAGE_VER=${REPO_URL##*/}
 invoke demo.start
--- .devcontainer/updateContentCommand.sh
+++ .devcontainer/updateContentCommand.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export INFRAHUB_IMAGE_VER=${REPO_URL##*/}
 invoke demo.start
 sleep 60
 invoke demo.load-infra-schema demo.load-infra-data
