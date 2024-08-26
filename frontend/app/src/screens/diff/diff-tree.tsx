daf07df2662b74829d1b18431e974e3f0a88e540
bilalabbad <bilal@opsmill.com>
2024-08-26 19:22:21 +0200

    Added filter input on diff tree

--- frontend/app/src/screens/diff/diff-tree.tsx
+++ frontend/app/src/screens/diff/diff-tree.tsx
@@ -7,6 +7,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { TREE_ROOT_ID } from "@/screens/ipam/constants";
 import { useSchema } from "@/hooks/useSchema";
 import { Icon } from "@iconify-icon/react";
+import { SearchInput } from "@/components/ui/search-input";
 
 interface DiffTreeProps extends Omit<TreeProps, "data"> {
   nodes: Array<DiffNode>;
@@ -14,68 +15,71 @@ interface DiffTreeProps extends Omit<TreeProps, "data"> {
 
 export default function DiffTree({ nodes, loading, ...props }: DiffTreeProps) {
   const [treeData, setTreeData] = useState<TreeProps["data"]>(EMPTY_TREE);
+  const [query, setQuery] = useState("");
   const location = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
     let kindToUUIDsMap: Record<string, Array<string>> = {};
 
-    const formattedNodes: TreeProps["data"] = nodes.flatMap((node: DiffNode) => {
-      const currentUUIDs = kindToUUIDsMap[node.kind];
-      kindToUUIDsMap[node.kind] = currentUUIDs ? [...currentUUIDs, node.uuid] : [node.uuid];
+    const formattedNodes: TreeProps["data"] = nodes
+      .filter((node) => node.label.includes(query))
+      .flatMap((node: DiffNode) => {
+        const currentUUIDs = kindToUUIDsMap[node.kind];
+        kindToUUIDsMap[node.kind] = currentUUIDs ? [...currentUUIDs, node.uuid] : [node.uuid];
 
-      const newNode = {
-        id: node.uuid,
-        name: node.label,
-        parent: node.kind,
-        children: [] as string[],
-        isBranch: node.relationships.length > 0,
-        metadata: {
-          uuid: node.uuid,
-          status: node.status,
-          containsConflicts: node.contains_conflict,
-        },
-      };
-
-      const nodesFromRelationships = node.relationships.flatMap((relationship) => {
-        const relationshipNode = {
-          id: relationship?.name + newNode.id,
-          name: relationship?.label,
-          parent: newNode.id,
-          isBranch: !!relationship?.elements?.length,
+        const newNode = {
+          id: node.uuid,
+          name: node.label,
+          parent: node.kind,
           children: [] as string[],
+          isBranch: node.relationships.length > 0,
           metadata: {
             uuid: node.uuid,
-            containsConflicts: relationship.contains_conflict,
+            status: node.status,
+            containsConflicts: node.contains_conflict,
           },
         };
 
-        newNode.children.push(relationshipNode.id);
+        const nodesFromRelationships = node.relationships.flatMap((relationship) => {
+          const relationshipNode = {
+            id: relationship?.name + newNode.id,
+            name: relationship?.label,
+            parent: newNode.id,
+            isBranch: !!relationship?.elements?.length,
+            children: [] as string[],
+            metadata: {
+              uuid: node.uuid,
+              containsConflicts: relationship.contains_conflict,
+            },
+          };
 
-        const relationshipChildNodes =
-          relationship?.elements?.map((element) => {
-            const child = {
-              id: newNode.id + element.peer_label + element?.peer_id,
-              name: element.peer_label,
-              parent: relationshipNode.id,
-              isBranch: false,
-              children: [],
-              metadata: {
-                uuid: element?.peer_id,
-                status: nodes.find(({ uuid }) => uuid === element.peer_id)?.status,
-                containsConflicts: element.contains_conflict,
-              },
-            };
-            relationshipNode.children.push(child.id);
+          newNode.children.push(relationshipNode.id);
 
-            return child;
-          }) ?? [];
+          const relationshipChildNodes =
+            relationship?.elements?.map((element) => {
+              const child = {
+                id: newNode.id + element.peer_label + element?.peer_id,
+                name: element.peer_label,
+                parent: relationshipNode.id,
+                isBranch: false,
+                children: [],
+                metadata: {
+                  uuid: element?.peer_id,
+                  status: nodes.find(({ uuid }) => uuid === element.peer_id)?.status,
+                  containsConflicts: element.contains_conflict,
+                },
+              };
+              relationshipNode.children.push(child.id);
 
-        return [relationshipNode, ...relationshipChildNodes];
+              return child;
+            }) ?? [];
+
+          return [relationshipNode, ...relationshipChildNodes];
+        });
+
+        return [newNode, ...nodesFromRelationships];
       });
-
-      return [newNode, ...nodesFromRelationships];
-    });
 
     const parents = Object.entries(kindToUUIDsMap).map(([kind, uuids]) => {
       return {
@@ -90,27 +94,35 @@ export default function DiffTree({ nodes, loading, ...props }: DiffTreeProps) {
       };
     });
     setTreeData(addItemsToTree(EMPTY_TREE, [...parents, ...formattedNodes]));
-  }, [nodes.length]);
-
-  if (treeData.length <= 1) return null;
+  }, [nodes.length, query]);
 
   return (
-    <Tree
-      itemContent={DiffTreeItem}
-      propagateSelectUpwards={false}
-      defaultExpandedIds={treeData
-        .filter(({ parent }) => parent === TREE_ROOT_ID)
-        .map(({ id }) => id)}
-      loading={loading}
-      data={treeData}
-      onNodeSelect={({ element }) => {
-        navigate({
-          ...location,
-          hash: element?.metadata?.uuid as string | undefined,
-        });
-      }}
-      {...props}
-    />
+    <>
+      <SearchInput
+        placeholder="Filter..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {treeData.length > 1 && (
+        <Tree
+          itemContent={DiffTreeItem}
+          propagateSelectUpwards={false}
+          defaultSelectedIds={location.hash ? [location.hash.slice(1)] : []}
+          defaultExpandedIds={treeData
+            .filter(({ parent }) => parent === TREE_ROOT_ID)
+            .map(({ id }) => id)}
+          loading={loading}
+          data={treeData}
+          onNodeSelect={({ element }) => {
+            navigate({
+              ...location,
+              hash: element?.metadata?.uuid as string | undefined,
+            });
+          }}
+          {...props}
+        />
+      )}
+    </>
   );
 }
 
