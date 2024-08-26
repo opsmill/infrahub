f579ea4d47450aad7de6c8dc5e428c367afe4a26
bilalabbad <bilal@opsmill.com>
2024-08-26 16:34:12 +0200

    Update colors on diff card

--- frontend/app/src/screens/diff/node-diff/node-attribute.tsx
+++ frontend/app/src/screens/diff/node-diff/node-attribute.tsx
@@ -1,15 +1,15 @@
 import { useParams } from "react-router-dom";
-import { Badge } from "@/components/ui/badge";
 import { DiffRow, formatValue } from "@/screens/diff/node-diff/utils";
-import { DiffAttribute } from "@/screens/diff/node-diff/types";
+import { DiffAttribute, DiffStatus } from "@/screens/diff/node-diff/types";
 import { DiffThread } from "@/screens/diff/node-diff/thread";
 import { DiffNodeProperty } from "@/screens/diff/node-diff/node-property";
 
 type DiffNodeAttributeProps = {
   attribute: DiffAttribute;
+  status: DiffStatus;
 };
 
-export const DiffNodeAttribute = ({ attribute }: DiffNodeAttributeProps) => {
+export const DiffNodeAttribute = ({ attribute, status }: DiffNodeAttributeProps) => {
   const { "*": branchName } = useParams();
 
   const valueProperty = attribute.properties.find(
@@ -18,6 +18,7 @@ export const DiffNodeAttribute = ({ attribute }: DiffNodeAttributeProps) => {
 
   return (
     <DiffRow
+      status={status}
       hasConflicts={attribute.contains_conflict}
       title={
         <div className="flex justify-between items-center pr-2">
@@ -28,21 +29,11 @@ export const DiffNodeAttribute = ({ attribute }: DiffNodeAttributeProps) => {
           )}
         </div>
       }
-      left={
-        valueProperty?.previous_value && (
-          <Badge variant="green" className="font-medium">
-            {formatValue(valueProperty?.previous_value)}
-          </Badge>
-        )
-      }
-      right={
-        <Badge variant="blue" className="font-medium">
-          {formatValue(valueProperty?.new_value)}
-        </Badge>
-      }>
+      left={valueProperty?.previous_value && formatValue(valueProperty?.previous_value)}
+      right={status !== "REMOVED" && formatValue(valueProperty?.new_value)}>
       <div className="divide-y border-t">
         {attribute.properties.map((property, index: number) => (
-          <DiffNodeProperty key={index} property={property} />
+          <DiffNodeProperty key={index} property={property} status={status} />
         ))}
       </div>
     </DiffRow>
