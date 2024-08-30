d4b492c767ef65648c81ccbba0088b18f4ba1f1c
bilalabbad <bilal@opsmill.com>
2024-08-30 15:04:07 +0200

    more precise comment

--- frontend/app/src/components/form/utils/getRelationshipsForForm.ts
+++ frontend/app/src/components/form/utils/getRelationshipsForForm.ts
@@ -5,7 +5,9 @@ export const getRelationshipsForForm = (
   relationships: components["schemas"]["RelationshipSchema-Output"][],
   isUpdate?: boolean
 ) => {
-  // Create form includes cardinality many but only if required, edit form doesn't include it at all
+  // Filter relationships based on cardinality and kind for form inclusion
+  // For create forms, include relationships with cardinality 'one', eligible kinds, or mandatory cardinality 'many'
+  // For update forms, only include relationships with cardinality 'one' or those with eligible kinds (Attribute or Parent). Other should be display as tabs on details view
   return relationships.filter((relationship) => {
     if (relationship.cardinality === "one") return true;
 
