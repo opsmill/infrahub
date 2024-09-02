229a4c594cea0437823b815a74edbd7f09b93b4c
Damien Garros <dgarros@gmail.com>
2024-09-02 06:14:42 +0200

    Remove test_rfile_endpoint test

--- backend/tests/unit/api/test_10_transformation_api.py
+++ backend/tests/unit/api/test_10_transformation_api.py
@@ -4,7 +4,6 @@ from infrahub.core.constants import InfrahubKind
 from infrahub.core.manager import NodeManager
 from infrahub.core.node import Node
 from infrahub.database import InfrahubDatabase
-from infrahub.message_bus.messages.transform_jinja_template import TransformJinjaTemplateResponse
 from infrahub.message_bus.messages.transform_python_data import TransformPythonDataResponse
 
 
@@ -46,39 +45,3 @@ async def test_transform_endpoint(
     result = response.json()
 
     assert result == {"KEY1": "value1", "KEY2": "value2"}
-
-
-async def test_rfile_endpoint(
-    db: InfrahubDatabase, client_headers, default_branch, rpc_bus, register_core_models_schema, car_person_data
-):
-    from infrahub.server import app
-
-    client = TestClient(app)
-
-    repositories = await NodeManager.query(db=db, schema=InfrahubKind.REPOSITORY)
-    queries = await NodeManager.query(db=db, schema=InfrahubKind.GRAPHQLQUERY)
-
-    t1 = await Node.init(db=db, schema=InfrahubKind.TRANSFORMJINJA2)
-    await t1.new(
-        db=db,
-        name="test-rfile",
-        query=str(queries[0].id),
-        repository=str(repositories[0].id),
-        template_path="templates/device_startup_config.tpl.j2",
-    )
-    await t1.save(db=db)
-
-    # Must execute in a with block to execute the startup/shutdown events
-    with client:
-        mock_response = TransformJinjaTemplateResponse(
-            data={"rendered_template": "Rendered by a mocked agent"},
-        )
-        rpc_bus.add_mock_reply(response=mock_response)
-
-        response = client.get(
-            "/api/transform/jinja2/test-rfile",
-            headers=client_headers,
-        )
-
-    assert response.status_code == 200
-    assert response.text == "Rendered by a mocked agent"
