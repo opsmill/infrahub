4671c2eaca4b7e7e72da2a5bdea81a5a0184fc59
bilalabbad <bilal@opsmill.com>
2024-09-05 12:21:32 +0200

    update e2e test of groups

--- frontend/app/tests/e2e/groups/groups.spec.ts
+++ frontend/app/tests/e2e/groups/groups.spec.ts
@@ -3,6 +3,7 @@ import { ACCOUNT_STATE_PATH } from "../../constants";
 import { saveScreenshotForDocs } from "../../utils";
 
 test.describe("/objects/CoreGroup - Generic Group Object.", () => {
+  test.describe.configure({ mode: "serial" });
   test.use({ storageState: ACCOUNT_STATE_PATH.ADMIN });
 
   test.beforeEach(async function ({ page }) {
@@ -12,14 +13,17 @@ test.describe("/objects/CoreGroup - Generic Group Object.", () => {
       }
     });
   });
+
   test("1. Create a new Standard Group", async ({ page }) => {
-    await page.goto("/");
-    await page.getByTestId("sidebar-menu").getByRole("link", { name: "All Groups" }).click();
+    await page.goto("/objects/CoreGroup");
+    await expect(
+      page.getByTestId("object-items").getByRole("link", { name: "arista_devices" })
+    ).toBeVisible();
 
     await test.step("fill and submit form for new group", async () => {
       await page.getByTestId("create-object-button").click();
       await page.getByLabel("Select an object type").click();
-      await page.getByRole("option", { name: "Standard Group Core" }).click();
+      await page.getByRole("option", { name: "Standard Group" }).click();
 
       await page.getByLabel("Name *").fill("TagConfigGroup");
       await saveScreenshotForDocs(page, "group_tagconfig_grp_new_grp");
@@ -29,24 +33,22 @@ test.describe("/objects/CoreGroup - Generic Group Object.", () => {
   });
 
   test("2. Add members to Standard Group", async ({ page }) => {
-    await page.goto("/");
+    await page.goto("/objects/CoreGroup");
+
     await test.step("add members to Standard Group", async () => {
-      await page.goto("/");
-      await page.getByTestId("sidebar-menu").getByRole("link", { name: "All Groups" }).click();
       await page
         .getByTestId("hierarchical-tree")
         .getByRole("link", { name: "TagConfigGroup", exact: true })
         .click();
       await page.getByText("Members0").click();
       await page.getByTestId("open-relationship-form-button").click();
-      await page.getByTestId("side-panel-container").getByTestId("select-input").click();
       await page
         .getByTestId("side-panel-container")
         .getByTestId("select-open-option-button")
         .click();
-      await page.getByTestId("side-panel-container").getByText("Tag", { exact: true }).click();
+      await page.getByRole("option", { name: "Tag Builtin" }).click();
       await page.getByLabel("Tag").click();
-      await page.getByText("blue").click();
+      await page.getByRole("option", { name: "blue" }).click();
 
       await saveScreenshotForDocs(page, "group_tagconfig_grp_adding_members");
 
@@ -54,12 +56,11 @@ test.describe("/objects/CoreGroup - Generic Group Object.", () => {
       await page.getByTestId("close-alert").click();
 
       await page.getByTestId("open-relationship-form-button").click();
-      await page.getByTestId("side-panel-container").getByTestId("select-input").click();
       await page
         .getByTestId("side-panel-container")
         .getByTestId("select-open-option-button")
         .click();
-      await page.getByTestId("side-panel-container").getByText("Tag", { exact: true }).click();
+      await page.getByRole("option", { name: "Tag Builtin" }).click();
       await page.getByLabel("Tag").click();
       await page.getByRole("option", { name: "red" }).click();
       await page.getByRole("button", { name: "Save" }).click();
