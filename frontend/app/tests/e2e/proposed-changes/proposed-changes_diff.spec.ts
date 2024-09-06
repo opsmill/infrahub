9cdb2b6127b4653f72caf0d591903106dfb93170
pa-lem <pa-lem@hotmail.fr>
2024-09-06 10:01:44 +0200

    uncomment refetch

--- frontend/app/tests/e2e/proposed-changes/proposed-changes_diff.spec.ts
+++ frontend/app/tests/e2e/proposed-changes/proposed-changes_diff.spec.ts
@@ -28,11 +28,11 @@ test.describe("/proposed-changes diff data", () => {
       await page.getByText("Data").click();
     });
 
-    // await test.step("trigger the diff update", async () => {
-    //   await expect(page.getByText("We are computing the diff")).toBeVisible();
-    //   await page.getByRole("button", { name: "Refresh" }).click();
-    //   await expect(page.getByText("Diff updated!")).toBeVisible({ timeout: 5 * 60 * 1000 });
-    // });
+    await test.step("trigger the diff update", async () => {
+      await expect(page.getByText("We are computing the diff")).toBeVisible();
+      await page.getByRole("button", { name: "Refresh" }).click();
+      await expect(page.getByText("Diff updated!")).toBeVisible({ timeout: 5 * 60 * 1000 });
+    });
 
     await test.step("check diff data", async () => {
       await expect(page.getByText("UpdatedDeviceden1-edge1")).toBeVisible();
