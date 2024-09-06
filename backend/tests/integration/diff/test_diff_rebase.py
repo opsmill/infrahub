bdbd1a2c246f4d034b6dc8c58dbe21f05426b339
Aaron McCarty <aaron@opsmill.com>
2024-09-06 16:08:23 -0700

    add integration test for diff update/recalculation on rebase

--- backend/tests/integration/diff/test_diff_rebase.py
+++ backend/tests/integration/diff/test_diff_rebase.py
@@ -1,18 +1,13 @@
-
 from __future__ import annotations
 
-from dataclasses import dataclass
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
+
+import pytest
 
 from infrahub import config, lock
-from infrahub.core.diff.coordinator import DiffCoordinator
-import pytest
-from infrahub_sdk.exceptions import GraphQLError
-
-from infrahub.core import registry
-from infrahub.core.constants import BranchConflictKeep, DiffAction, InfrahubKind, ProposedChangeState
+from infrahub.core.constants import DiffAction, InfrahubKind
 from infrahub.core.constants.database import DatabaseEdgeType
-from infrahub.core.diff.model.path import BranchTrackingId, ConflictSelection, EnrichedDiffProperty, EnrichedDiffRoot
+from infrahub.core.diff.model.path import BranchTrackingId, ConflictSelection
 from infrahub.core.diff.repository.repository import DiffRepository
 from infrahub.core.initialization import create_branch
 from infrahub.core.manager import NodeManager
@@ -48,6 +43,21 @@ mutation($branch: String!) {
 }
 """
 
+CONFLICT_SELECTION_QUERY = """
+mutation ResolveDiffConflict($conflict_id: String!, $selected_branch: ConflictSelection!) {
+    ResolveDiffConflict(data: { conflict_id: $conflict_id, selected_branch: $selected_branch }) {
+        ok
+    }
+}
+"""
+
+BRANCH_REBASE = """
+mutation($branch: String!) {
+    BranchRebase(data: { name: $branch }) {
+        ok
+    }
+}
+"""
 
 
 class TestDiffRebase(TestInfrahubApp):
@@ -146,35 +156,56 @@ class TestDiffRebase(TestInfrahubApp):
         return await create_branch(branch_name="branch_2", db=db)
 
     @pytest.fixture(scope="class")
-    def diff_coordinator(self, db: InfrahubDatabase) -> DiffCoordinator:
-        ...
-
-    @pytest.fixture(scope="class")
     async def diff_repository(self, db: InfrahubDatabase, default_branch: Branch) -> DiffRepository:
         component_registry = get_component_registry()
         return await component_registry.get_component(DiffRepository, db=db, branch=default_branch)
 
     @pytest.fixture(scope="class")
-    async def add_branches_and_diffs(self, db: InfrahubDatabase, client: InfrahubClient, default_branch: Branch, initial_dataset, branch_1: Branch, branch_2: Branch):
+    async def add_branch_1_changes(
+        self, db: InfrahubDatabase, client: InfrahubClient, default_branch: Branch, initial_dataset, branch_1: Branch
+    ):
         kara_id = initial_dataset["kara"].id
         kara_branch_1 = await NodeManager.get_one(db=db, id=kara_id, branch=branch_1)
         kara_branch_1.description.value = "branch-1-description"
         await kara_branch_1.save(db=db)
+
+        result = await client.execute_graphql(query=DIFF_UPDATE_QUERY, variables={"branch_name": branch_1.name})
+        assert result["DiffUpdate"]["ok"]
+
+    @pytest.fixture(scope="class")
+    async def add_branch_2_changes(
+        self, db: InfrahubDatabase, client: InfrahubClient, default_branch: Branch, initial_dataset, branch_2: Branch
+    ):
+        kara_id = initial_dataset["kara"].id
         kara_branch_2 = await NodeManager.get_one(db=db, id=kara_id, branch=branch_2)
         kara_branch_2.description.value = "branch-2-description"
         await kara_branch_2.save(db=db)
 
-        result = await client.execute_graphql(query=DIFF_UPDATE_QUERY, variables={"branch_name": branch_1.name})
-        assert result["DiffUpdate"]["ok"]
         result = await client.execute_graphql(query=DIFF_UPDATE_QUERY, variables={"branch_name": branch_2.name})
         assert result["DiffUpdate"]["ok"]
 
-    async def test_no_conflicts_before_merge(self, db: InfrahubDatabase, initial_dataset, add_branches_and_diffs, branch_1, branch_2, diff_repository: DiffRepository):
+    async def test_no_conflicts_before_merge(
+        self,
+        db: InfrahubDatabase,
+        initial_dataset,
+        add_branch_1_changes,
+        add_branch_2_changes,
+        branch_1: Branch,
+        branch_2: Branch,
+        diff_repository: DiffRepository,
+    ):
         kara_id = initial_dataset["kara"].id
-        
-        branch_1_diff = await diff_repository.get_one(diff_branch_name=branch_1.name, tracking_id=BranchTrackingId(name=branch_1.name))
-        branch_2_diff = await diff_repository.get_one(diff_branch_name=branch_2.name, tracking_id=BranchTrackingId(name=branch_2.name))
-        for (new_value, branch_diff) in [("branch-1-description", branch_1_diff), ("branch-2-description", branch_2_diff)]:
+
+        branch_1_diff = await diff_repository.get_one(
+            diff_branch_name=branch_1.name, tracking_id=BranchTrackingId(name=branch_1.name)
+        )
+        branch_2_diff = await diff_repository.get_one(
+            diff_branch_name=branch_2.name, tracking_id=BranchTrackingId(name=branch_2.name)
+        )
+        for new_value, branch_diff in [
+            ("branch-1-description", branch_1_diff),
+            ("branch-2-description", branch_2_diff),
+        ]:
             assert len(branch_diff.nodes) == 1
             kara_node = branch_diff.nodes.pop()
             assert kara_node.uuid == kara_id
@@ -188,13 +219,24 @@ class TestDiffRebase(TestInfrahubApp):
             assert value_prop.new_value == new_value
             assert value_prop.conflict is None
 
-    async def test_merge_causes_diff_update(self,db: InfrahubDatabase, client: InfrahubClient, initial_dataset, add_branches_and_diffs, branch_1, branch_2, diff_repository):
+    async def test_merge_causes_diff_update(
+        self,
+        db: InfrahubDatabase,
+        client: InfrahubClient,
+        initial_dataset,
+        add_branch_1_changes,
+        branch_1: Branch,
+        branch_2: Branch,
+        diff_repository,
+    ):
         kara_id = initial_dataset["kara"].id
         before_merge = Timestamp()
         result = await client.execute_graphql(query=BRANCH_MERGE, variables={"branch": branch_1.name})
         assert result["BranchMerge"]["ok"]
 
-        branch_2_diff = await diff_repository.get_one(diff_branch_name=branch_2.name, tracking_id=BranchTrackingId(name=branch_2.name))
+        branch_2_diff = await diff_repository.get_one(
+            diff_branch_name=branch_2.name, tracking_id=BranchTrackingId(name=branch_2.name)
+        )
 
         assert len(branch_2_diff.nodes) == 1
         assert branch_2_diff.to_time > before_merge
@@ -213,3 +255,57 @@ class TestDiffRebase(TestInfrahubApp):
         assert value_prop.conflict.base_branch_value == "branch-1-description"
         assert value_prop.conflict.diff_branch_action is DiffAction.UPDATED
         assert value_prop.conflict.diff_branch_value == "branch-2-description"
+
+    async def test_resolve_conflict(self, client: InfrahubClient, branch_2: Branch, diff_repository: DiffRepository):
+        branch_2_diff = await diff_repository.get_one(
+            diff_branch_name=branch_2.name, tracking_id=BranchTrackingId(name=branch_2.name)
+        )
+        conflict = branch_2_diff.get_all_conflicts()[0]
+
+        result = await client.execute_graphql(
+            query=CONFLICT_SELECTION_QUERY,
+            variables={"conflict_id": conflict.uuid, "selected_branch": ConflictSelection.DIFF_BRANCH.name},
+        )
+        assert result["ResolveDiffConflict"]["ok"]
+
+        branch_2_diff = await diff_repository.get_one(
+            diff_branch_name=branch_2.name, tracking_id=BranchTrackingId(name=branch_2.name)
+        )
+        conflict = branch_2_diff.get_all_conflicts()[0]
+        assert conflict.selected_branch is ConflictSelection.DIFF_BRANCH
+
+    async def test_rebase_causes_diff_recalculation(
+        self,
+        db: InfrahubDatabase,
+        client: InfrahubClient,
+        initial_dataset,
+        branch_2: Branch,
+        diff_repository: DiffRepository,
+    ):
+        kara_id = initial_dataset["kara"].id
+        before_rebase = Timestamp()
+        result = await client.execute_graphql(query=BRANCH_REBASE, variables={"branch": branch_2.name})
+        assert result["BranchRebase"]["ok"]
+
+        branch_2_diff = await diff_repository.get_one(
+            diff_branch_name=branch_2.name, tracking_id=BranchTrackingId(name=branch_2.name)
+        )
+
+        assert len(branch_2_diff.nodes) == 1
+        assert branch_2_diff.to_time > before_rebase
+        kara_node = branch_2_diff.nodes.pop()
+        assert kara_node.uuid == kara_id
+        assert len(kara_node.attributes) == 1
+        description_attr = kara_node.attributes.pop()
+        assert description_attr.name == "description"
+        assert len(description_attr.properties) == 1
+        value_prop = description_attr.properties.pop()
+        assert value_prop.property_type is DatabaseEdgeType.HAS_VALUE
+        assert value_prop.previous_value == "Starbuck"
+        assert value_prop.new_value == "branch-2-description"
+        assert value_prop.conflict
+        assert value_prop.conflict.base_branch_action is DiffAction.UPDATED
+        assert value_prop.conflict.base_branch_value == "branch-1-description"
+        assert value_prop.conflict.diff_branch_action is DiffAction.UPDATED
+        assert value_prop.conflict.diff_branch_value == "branch-2-description"
+        assert value_prop.conflict.selected_branch is ConflictSelection.DIFF_BRANCH
