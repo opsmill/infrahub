0c691558d69427f0906db20b9bd5806961199b0e
Aaron McCarty <aaron@opsmill.com>
2024-09-06 16:07:38 -0700

    update merge and rebase event handling, unit tests

--- backend/infrahub/message_bus/messages/request_diff_refresh.py
+++ backend/infrahub/message_bus/messages/request_diff_refresh.py
@@ -7,4 +7,4 @@ class RequestDiffRefresh(InfrahubMessage):
     """Request diff be recalculated from scratch."""
 
     branch_name: str = Field(..., description="The branch associated with the diff")
-    tracking_id_str: str = Field(..., description="The serialized tracking_id for this diff")
+    diff_id: str = Field(..., description="The id for this diff")
--- backend/infrahub/message_bus/operations/event/branch.py
+++ backend/infrahub/message_bus/operations/event/branch.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from infrahub.core import registry
-from infrahub.core.diff.model.path import EnrichedDiffRoot, NameTrackingId
+from infrahub.core.diff.model.path import BranchTrackingId
 from infrahub.core.diff.repository.repository import DiffRepository
 from infrahub.dependencies.registry import get_component_registry
 from infrahub.log import get_logger
@@ -45,14 +45,19 @@ async def merge(message: messages.EventBranchMerge, service: InfrahubServices) -
         messages.TriggerArtifactDefinitionGenerate(branch=message.target_branch),
         messages.TriggerGeneratorDefinitionRun(branch=message.target_branch),
     ]
-    open_branch_diffs = await get_diff_roots_for_open_branches(service=service)
-    for diff in open_branch_diffs:
-        if diff.tracking_id:
-            if isinstance(diff.tracking_id, NameTrackingId):
-                name = diff.tracking_id.name
-            else:
-                name = None
-            events.append(messages.RequestDiffUpdate(branch_name=diff.diff_branch_name, name=name))
+    component_registry = get_component_registry()
+    default_branch = registry.get_branch_from_registry()
+    diff_repository = await component_registry.get_component(DiffRepository, db=service.database, branch=default_branch)
+    # send diff update requests for every branch-tracking diff
+    branch_diff_roots = await diff_repository.get_empty_roots(base_branch_names=[message.target_branch])
+
+    for diff_root in branch_diff_roots:
+        if (
+            diff_root.base_branch_name != diff_root.diff_branch_name
+            and diff_root.tracking_id
+            and isinstance(diff_root.tracking_id, BranchTrackingId)
+        ):
+            events.append(messages.RequestDiffUpdate(branch_name=diff_root.diff_branch_name))
 
     for event in events:
         event.assign_meta(parent=message)
@@ -70,28 +75,16 @@ async def rebased(message: messages.EventBranchRebased, service: InfrahubService
             messages.TriggerIpamReconciliation(branch=message.branch, ipam_node_details=message.ipam_node_details),
         )
 
-    open_branch_diffs = await get_diff_roots_for_open_branches(service=service)
-    for diff in open_branch_diffs:
-        if diff.tracking_id:
-            events.append(
-                messages.RequestDiffRefresh(
-                    branch_name=diff.diff_branch_name, tracking_id_str=diff.tracking_id.serialize()
-                )
-            )
+    # for every diff that touches the rebased branch, recalculate it
+    component_registry = get_component_registry()
+    default_branch = registry.get_branch_from_registry()
+    diff_repository = await component_registry.get_component(DiffRepository, db=service.database, branch=default_branch)
+    diff_roots_to_refresh = await diff_repository.get_empty_roots(diff_branch_names=[message.branch])
+
+    for diff_root in diff_roots_to_refresh:
+        if diff_root.base_branch_name != diff_root.diff_branch_name:
+            events.append(messages.RequestDiffRefresh(branch_name=diff_root.diff_branch_name, diff_id=diff_root.uuid))
 
     for event in events:
         event.assign_meta(parent=message)
         await service.send(message=event)
-
-
-async def get_diff_roots_for_open_branches(service: InfrahubServices) -> list[EnrichedDiffRoot]:
-    component_registry = get_component_registry()
-    default_branch = registry.get_branch_from_registry()
-    diff_repository = await component_registry.get_component(DiffRepository, db=service.database, branch=default_branch)
-    all_diff_roots = await diff_repository.get_empty_roots()
-    open_branch_names = set()
-    for branch_name in registry.branch:
-        branch = registry.get_branch_from_registry(branch=branch_name)
-        if branch.status == "OPEN":
-            open_branch_names.add(branch.name)
-    return [dr for dr in all_diff_roots if dr.diff_branch_name in open_branch_names]
