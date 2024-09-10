80000ade11c80fe037227831c3a63280e73fb1da
Damien Garros <dgarros@gmail.com>
2024-09-10 08:52:27 +0200

    Move tasks initialization to workflow service

--- backend/infrahub/services/adapters/workflow/worker.py
+++ backend/infrahub/services/adapters/workflow/worker.py
@@ -1,20 +1,49 @@
+from __future__ import annotations
+
 import base64
 import json
 from pathlib import Path
 from typing import TYPE_CHECKING, Any, Awaitable, Callable
 
 import cloudpickle
+from prefect.client.orchestration import get_client
+from prefect.client.schemas.actions import WorkPoolCreate
 from prefect.deployments import run_deployment
+from prefect.exceptions import ObjectAlreadyExists
 
-from infrahub.workflows.models import WorkflowDefinition
+from infrahub.workflows.catalogue import worker_pools, workflows
 
 from . import InfrahubWorkflow, Return
 
 if TYPE_CHECKING:
     from prefect.client.schemas.objects import FlowRun
 
+    from infrahub.services import InfrahubServices
+    from infrahub.workflows.models import WorkflowDefinition
+
 
 class WorkflowWorkerExecution(InfrahubWorkflow):
+    async def initialize(self, service: InfrahubServices) -> None:
+        """Initialize the Workflow engine"""
+
+        async with get_client(sync_client=False) as client:
+            for worker in worker_pools:
+                wp = WorkPoolCreate(
+                    name=worker.name,
+                    type=worker.worker_type,
+                    description=worker.description,
+                )
+                try:
+                    await client.create_work_pool(work_pool=wp)
+                    service.log.info(f"work pool {worker} created successfully ... ")
+                except ObjectAlreadyExists:
+                    service.log.info(f"work pool {worker} already present ")
+
+            # Create deployment
+            for workflow in workflows:
+                flow_id = await client.create_flow_from_name(workflow.name)
+                await client.create_deployment(flow_id=flow_id, **workflow.to_deployment())
+
     async def execute(
         self,
         workflow: WorkflowDefinition | None = None,
