a03762c72b9f63e8f0bc980960e786b0ffe11c85
Aaron McCarty <aaron@opsmill.com>
2024-09-13 13:59:00 -0700

    update core schemas to sync unique attrs and uniqueness_constraints

--- backend/infrahub/core/schema/definitions/core.py
+++ backend/infrahub/core/schema/definitions/core.py
@@ -34,6 +34,7 @@ core_profile_schema_definition: dict[str, Any] = {
     "label": "Profile",
     "display_labels": ["profile_name__value"],
     "default_filter": "profile_name__value",
+    "uniqueness_constraints": [["profile_name__value"]],
     "attributes": [
         {
             "name": "profile_name",
@@ -156,6 +157,7 @@ core_models: dict[str, Any] = {
             "icon": "mdi:group",
             "hierarchical": True,
             "branch": BranchSupportType.AWARE.value,
+            "uniqueness_constraints": [["name__value"]],
             "attributes": [
                 {"name": "name", "kind": "Text", "unique": True},
                 {"name": "label", "kind": "Text", "optional": True},
@@ -291,6 +293,7 @@ core_models: dict[str, Any] = {
             "display_labels": ["label__value"],
             "branch": BranchSupportType.AWARE.value,
             "documentation": "/topics/proposed-change",
+            "uniqueness_constraints": [["name__value"]],
             "attributes": [
                 {"name": "name", "kind": "Text", "unique": True},
                 {"name": "label", "kind": "Text", "optional": True},
@@ -357,6 +360,7 @@ core_models: dict[str, Any] = {
             "display_labels": ["name__value"],
             "include_in_menu": False,
             "branch": BranchSupportType.AGNOSTIC.value,
+            "uniqueness_constraints": [["name__value"]],
             "attributes": [
                 {"name": "name", "kind": "Text", "unique": True, "order_weight": 1000},
                 {"name": "description", "kind": "Text", "optional": True, "order_weight": 2000},
@@ -381,6 +385,7 @@ core_models: dict[str, Any] = {
             "display_labels": ["name__value"],
             "icon": "mdi:source-repository",
             "branch": BranchSupportType.AGNOSTIC.value,
+            "uniqueness_constraints": [["name__value"], ["location__value"]],
             "documentation": "/topics/repository",
             "attributes": [
                 {
@@ -575,6 +580,7 @@ core_models: dict[str, Any] = {
             "display_labels": ["name__value"],
             "icon": "mdi:format-list-group",
             "branch": BranchSupportType.AWARE.value,
+            "uniqueness_constraints": [["name__value"]],
             "generate_profile": False,
             "attributes": [
                 {
@@ -802,6 +808,7 @@ core_models: dict[str, Any] = {
             "human_friendly_id": ["name__value"],
             "icon": "mdi:view-grid-outline",
             "branch": BranchSupportType.AGNOSTIC.value,
+            "uniqueness_constraints": [["name__value"]],
             "generate_profile": False,
             "attributes": [
                 {
@@ -831,6 +838,7 @@ core_models: dict[str, Any] = {
             "human_friendly_id": ["name__value"],
             "branch": BranchSupportType.AGNOSTIC.value,
             "documentation": "/topics/auth",
+            "uniqueness_constraints": [["name__value"]],
             "attributes": [
                 {"name": "name", "kind": "Text", "unique": True},
                 {"name": "password", "kind": "HashedPassword", "unique": False},
@@ -884,6 +892,7 @@ core_models: dict[str, Any] = {
             "icon": "mdi:key-variant",
             "human_friendly_id": ["name__value"],
             "branch": BranchSupportType.AGNOSTIC.value,
+            "uniqueness_constraints": [["name__value"]],
             "documentation": "/topics/auth",
             "attributes": [
                 {"name": "name", "kind": "Text", "unique": True, "order_weight": 1000},
@@ -958,6 +967,7 @@ core_models: dict[str, Any] = {
             "order_by": ["name__value"],
             "display_labels": ["name__value"],
             "branch": BranchSupportType.AWARE.value,
+            "uniqueness_constraints": [["name__value"]],
             "attributes": [
                 {"name": "name", "kind": "Text", "unique": True},
                 {"name": "description", "kind": "Text", "optional": True},
@@ -987,6 +997,7 @@ core_models: dict[str, Any] = {
             "display_labels": ["token__value"],
             "generate_profile": False,
             "branch": BranchSupportType.AGNOSTIC.value,
+            "uniqueness_constraints": [["token__value"]],
             "documentation": "/topics/auth",
             "attributes": [
                 {"name": "name", "kind": "Text", "optional": True},
@@ -1541,6 +1552,7 @@ core_models: dict[str, Any] = {
             "order_by": ["name__value"],
             "display_labels": ["name__value"],
             "branch": BranchSupportType.AWARE.value,
+            "uniqueness_constraints": [["name__value"]],
             "generate_profile": False,
             "inherit_from": [InfrahubKind.TASKTARGET],
             "attributes": [
@@ -1615,6 +1627,7 @@ core_models: dict[str, Any] = {
             "display_labels": ["name__value"],
             "generate_profile": False,
             "branch": BranchSupportType.AWARE.value,
+            "uniqueness_constraints": [["name__value"]],
             "documentation": "/topics/graphql",
             "attributes": [
                 {"name": "name", "kind": "Text", "unique": True},
@@ -1742,6 +1755,7 @@ core_models: dict[str, Any] = {
             "display_labels": ["name__value"],
             "branch": BranchSupportType.AWARE.value,
             "generate_profile": False,
+            "uniqueness_constraints": [["name__value"]],
             "inherit_from": [InfrahubKind.TASKTARGET],
             "documentation": "/topics/artifact",
             "attributes": [
@@ -1784,6 +1798,7 @@ core_models: dict[str, Any] = {
             "order_by": ["name__value"],
             "display_labels": ["name__value"],
             "branch": BranchSupportType.AWARE.value,
+            "uniqueness_constraints": [["name__value"]],
             "generate_profile": False,
             "inherit_from": [InfrahubKind.TASKTARGET],
             "attributes": [
