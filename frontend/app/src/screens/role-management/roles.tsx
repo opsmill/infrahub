2b16c8edcdbd88f39d7e3815b7bd586b9c51507d
pa-lem <pa-lem@hotmail.fr>
2024-09-26 14:07:34 +0200

    delete roles

--- frontend/app/src/screens/role-management/roles.tsx
+++ frontend/app/src/screens/role-management/roles.tsx
@@ -6,9 +6,15 @@ import ErrorScreen from "../errors/error-screen";
 import { Pagination } from "@/components/ui/pagination";
 import { GET_ROLE_MANAGEMENT_ROLES } from "@/graphql/queries/role-management/getRoles";
 import { Pill } from "@/components/display/pill";
+import ModalDeleteObject from "@/components/modals/modal-delete-object";
+import { useAtomValue } from "jotai";
+import { schemaKindNameState } from "@/state/atoms/schemaKindName.atom";
+import { useState } from "react";
 
 function Roles() {
-  const { loading, data, error } = useQuery(GET_ROLE_MANAGEMENT_ROLES);
+  const { loading, data, error, refetch } = useQuery(GET_ROLE_MANAGEMENT_ROLES);
+  const schemaKindName = useAtomValue(schemaKindNameState);
+  const [rowToDelete, setRowToDelete] = useState(null);
 
   const columns = [
     {
@@ -46,11 +52,26 @@ function Roles() {
   if (loading) return <LoadingScreen message="Retrieving accounts..." />;
 
   return (
-    <div>
-      <Table columns={columns} rows={rows ?? []} className="border-0" />
+    <>
+      <div>
+        <Table
+          columns={columns}
+          rows={rows ?? []}
+          className="border-0"
+          onDelete={(data) => setRowToDelete(data.values)}
+        />
 
-      <Pagination count={data && data[ACCOUNT_OBJECT]?.count} />
-    </div>
+        <Pagination count={data && data[ACCOUNT_OBJECT]?.count} />
+      </div>
+
+      <ModalDeleteObject
+        label={schemaKindName[ACCOUNT_OBJECT]}
+        rowToDelete={rowToDelete}
+        open={!!rowToDelete}
+        close={() => setRowToDelete(null)}
+        onDelete={refetch}
+      />
+    </>
   );
 }
 
