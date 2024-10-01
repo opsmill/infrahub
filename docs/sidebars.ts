b9fbde25862033dba4d5b58008f61a2acb0cebb2
Brett Lykins <lykinsbd@gmail.com>
2024-10-01 18:38:57 -0400

    release v0.16.2 prep

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -308,6 +308,7 @@ const sidebars: SidebarsConfig = {
           },
           items: [
             // 'release-notes/infrahub/release-1_0-DRAFT',
+            'release-notes/infrahub/release-0_16_2',
             'release-notes/infrahub/release-0_16_1',
             'release-notes/infrahub/release-0_16',
             'release-notes/infrahub/release-0_15_3',
