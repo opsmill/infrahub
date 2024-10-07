a1ced64040ffa4fc280cc62b904a31459897c74f
Guillaume Mazoyer <guillaume@opsmill.com>
2024-10-07 18:28:44 +0200

    Protect schema load API endpoint with a permission

--- backend/infrahub/core/constants/__init__.py
+++ backend/infrahub/core/constants/__init__.py
@@ -55,6 +55,7 @@ class GlobalPermissions(InfrahubStringEnum):
     SUPER_ADMIN = "super_admin"
     MERGE_BRANCH = "merge_branch"
     MERGE_PROPOSED_CHANGE = "merge_proposed_change"
+    MANAGE_SCHEMA = "manage_schema"
     MANAGE_ACCOUNTS = "manage_accounts"
     MANAGE_PERMISSIONS = "manage_permissions"
     MANAGE_REPOSITORIES = "manage_repositories"
