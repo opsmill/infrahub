f848543461d675bad9191c0595b3e3cad1c7bd1d
Guillaume Mazoyer <guillaume@opsmill.com>
2024-10-08 10:12:15 +0200

    Raise early

--- backend/infrahub/graphql/auth/query_permission_checker/object_permission_checker.py
+++ backend/infrahub/graphql/auth/query_permission_checker/object_permission_checker.py
@@ -196,14 +196,10 @@ class RepositoryManagerPermissionChecker(GraphQLQueryPermissionCheckerInterface)
         if not is_repository_operation or not analyzed_query.contains_mutation:
             return CheckerResolution.NEXT_CHECKER
 
-        has_permission = False
         for permission_backend in registry.permission_backends:
-            if has_permission := await permission_backend.has_permission(
+            if not await permission_backend.has_permission(
                 db=db, account_id=account_session.account_id, permission=self.permission_required, branch=branch
             ):
-                break
-
-        if not has_permission:
-            raise PermissionDeniedError("You do not have the permission to manage repositories")
+                raise PermissionDeniedError("You do not have the permission to manage repositories")
 
         return CheckerResolution.NEXT_CHECKER
