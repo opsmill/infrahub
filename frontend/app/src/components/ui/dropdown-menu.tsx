21f47e7ec049632613df55da56446bb44538842d
bilalabbad <bilal@opsmill.com>
2024-10-11 17:08:00 +0200

    animate dropdown menu

--- frontend/app/src/components/ui/dropdown-menu.tsx
+++ frontend/app/src/components/ui/dropdown-menu.tsx
@@ -26,7 +26,9 @@ export const DropdownMenuContent = forwardRef<
         ref={ref}
         className={classNames(
           "p-2 bg-white rounded-xl shadow-lg min-w-32 overflow-hidden space-y-1",
-          "data-[state=open]:animate-in data-[state=closed]:animate-out transition-all",
+          "data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95",
+          "data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95",
+          "data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
           className
         )}
         {...props}
@@ -91,6 +93,9 @@ export const DropdownMenuSubContent = forwardRef<
     ref={ref}
     className={classNames(
       "p-2 bg-white rounded-xl shadow-lg min-w-32 overflow-hidden space-y-1",
+      "data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95",
+      "data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95",
+      "data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
       className
     )}
     {...props}
