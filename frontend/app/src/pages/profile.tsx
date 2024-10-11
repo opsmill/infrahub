9667b4b394f8d9acd4c38c55b397e66a4a3eddbd
bilalabbad <bilal@opsmill.com>
2024-10-11 18:50:05 +0200

    On account settings, renamed tab "Preferences" into "Password"

--- frontend/app/src/pages/profile.tsx
+++ frontend/app/src/pages/profile.tsx
@@ -1,12 +1,13 @@
 import { useAuth } from "@/hooks/useAuth";
 import { UserProfilePage } from "@/screens/user-profile/user-profile";
+import { constructPath } from "@/utils/fetch";
 import { Navigate } from "react-router-dom";
 
 export function Component() {
   const auth = useAuth();
 
   if (!auth.isAuthenticated) {
-    return <Navigate to={"/"} />;
+    return <Navigate to={constructPath("/")} />;
   }
 
   return <UserProfilePage />;
