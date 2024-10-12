0e3dde1e8afbfb4fe02a017e1372a23bfd166097
pa-lem <pa-lem@hotmail.fr>
2024-10-12 16:58:46 +0200

    add useLocalStorage to store menu state

--- frontend/app/src/screens/layout/sidebar.tsx
+++ frontend/app/src/screens/layout/sidebar.tsx
@@ -1,23 +1,26 @@
 import { AccountMenu } from "@/components/account-menu";
 import { Button } from "@/components/buttons/button-primitive";
 import { SearchAnywhere } from "@/components/search/search-anywhere";
+import { SIDEBAR_COLLAPSED_KEY } from "@/config/localStorage";
+import { useLocalStorage } from "@/hooks/useLocalStorage";
 import MenuNavigation from "@/screens/layout/menu-navigation/menu-navigation";
 import { classNames } from "@/utils/common";
 import { Icon } from "@iconify-icon/react";
-import { useState } from "react";
 
 export default function Sidebar() {
-  const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useLocalStorage(SIDEBAR_COLLAPSED_KEY);
+
+  const booleanCollapsed = JSON.parse(collapsed);
 
   return (
     <nav
-      data-collapsed={collapsed}
+      data-collapsed={booleanCollapsed}
       className={classNames(
         "flex flex-col gap-3 shrink-0",
         "relative -top-px",
         "w-[256px] border rounded-lg py-5 px-4 bg-white",
         "group/sidebar transition-all",
-        collapsed && "w-[72px] px-2 items-center"
+        booleanCollapsed && "w-[72px] px-2 items-center"
       )}
     >
       <Button
@@ -25,16 +28,16 @@ export default function Sidebar() {
         size="icon"
         className={classNames(
           "transition-all absolute -right-3.5 top-6",
-          collapsed ? "rotate-180" : "hidden group-hover/sidebar:inline-flex"
+          booleanCollapsed ? "rotate-180" : "hidden group-hover/sidebar:inline-flex"
         )}
-        onClick={() => setCollapsed(!collapsed)}
+        onClick={() => setCollapsed(JSON.stringify(!booleanCollapsed))}
       >
         <Icon icon="mdi:chevron-left" className="text-xl text-neutral-600" />
       </Button>
 
-      <SearchAnywhere isCollapsed={collapsed} />
+      <SearchAnywhere isCollapsed={booleanCollapsed} />
 
-      <MenuNavigation isCollapsed={collapsed} />
+      <MenuNavigation isCollapsed={booleanCollapsed} />
 
       <AccountMenu />
     </nav>
