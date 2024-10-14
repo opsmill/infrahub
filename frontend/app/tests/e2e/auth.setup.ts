db50f23bce99365cc5db965d8513c895020b8912
bilalabbad <bilal@opsmill.com>
2024-10-14 10:09:29 +0200

    Enable E2E auth setup to work when SSO is enabled

--- frontend/app/tests/e2e/auth.setup.ts
+++ frontend/app/tests/e2e/auth.setup.ts
@@ -9,9 +9,15 @@ import {
 setup("authenticate admin", async ({ page }) => {
   await page.goto("/login");
   await expect(page.getByText("Log in to your account")).toBeVisible();
+  const loginButton = page.getByRole("button", {
+    name: "Log in with your credentials",
+  });
+  if (await loginButton.isVisible()) {
+    await loginButton.click();
+  }
   await page.getByLabel("Username").fill(ADMIN_CREDENTIALS.username);
   await page.getByLabel("Password").fill(ADMIN_CREDENTIALS.password);
-  await page.getByRole("button", { name: "Log in" }).click();
+  await page.getByRole("button", { name: "Log in", exact: true }).click();
 
   await expect(page.getByTestId("authenticated-menu-trigger")).toBeVisible();
   await page.context().storageState({ path: ACCOUNT_STATE_PATH.ADMIN });
@@ -20,9 +26,15 @@ setup("authenticate admin", async ({ page }) => {
 setup("authenticate read-write", async ({ page }) => {
   await page.goto("/login");
   await expect(page.getByText("Log in to your account")).toBeVisible();
+  const loginButton = page.getByRole("button", {
+    name: "Log in with your credentials",
+  });
+  if (await loginButton.isVisible()) {
+    await loginButton.click();
+  }
   await page.getByLabel("Username").fill(READ_WRITE_CREDENTIALS.username);
   await page.getByLabel("Password").fill(READ_WRITE_CREDENTIALS.password);
-  await page.getByRole("button", { name: "Log in" }).click();
+  await page.getByRole("button", { name: "Log in", exact: true }).click();
 
   await expect(page.getByTestId("authenticated-menu-trigger")).toBeVisible();
   await page.context().storageState({ path: ACCOUNT_STATE_PATH.READ_WRITE });
@@ -31,9 +43,15 @@ setup("authenticate read-write", async ({ page }) => {
 setup("authenticate read-only", async ({ page }) => {
   await page.goto("/login");
   await expect(page.getByText("Log in to your account")).toBeVisible();
+  const loginButton = page.getByRole("button", {
+    name: "Log in with your credentials",
+  });
+  if (await loginButton.isVisible()) {
+    await loginButton.click();
+  }
   await page.getByLabel("Username").fill(READ_ONLY_CREDENTIALS.username);
   await page.getByLabel("Password").fill(READ_ONLY_CREDENTIALS.password);
-  await page.getByRole("button", { name: "Log in" }).click();
+  await page.getByRole("button", { name: "Log in", exact: true }).click();
 
   await expect(page.getByTestId("authenticated-menu-trigger")).toBeVisible();
   await page.context().storageState({ path: ACCOUNT_STATE_PATH.READ_ONLY });
