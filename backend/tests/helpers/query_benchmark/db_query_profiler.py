7ff2cc9bfec0e991695efc676f1787129c1f9846
Damien Garros <dgarros@gmail.com>
2024-10-18 06:05:24 +0200

    Add support for additional context to the metrics for query execution time

--- backend/tests/helpers/query_benchmark/db_query_profiler.py
+++ backend/tests/helpers/query_benchmark/db_query_profiler.py
@@ -179,7 +179,11 @@ class InfrahubDatabaseProfiler(InfrahubDatabase):
         return ctx
 
     async def execute_query_with_metadata(
-        self, query: str, params: dict[str, Any] | None = None, name: str | None = "undefined"
+        self,
+        query: str,
+        params: dict[str, Any] | None = None,
+        name: str | None = "undefined",
+        context: dict[str, str] | None = None,
     ) -> tuple[list[Record], dict[str, Any]]:
         if not self.query_analyzer.profile_duration:
             # Profiling might be disabled to avoid capturing queries while loading data
