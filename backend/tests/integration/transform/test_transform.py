e5656ff949d4010adedcaccbf6f2a78df3d038dc
Damien Garros <dgarros@gmail.com>
2024-10-20 14:29:57 +0200

    Convert Transform tasks to prefect

--- backend/tests/integration/transform/test_transform.py
+++ backend/tests/integration/transform/test_transform.py
@@ -13,6 +13,7 @@ from infrahub.core.utils import delete_all_nodes
 from infrahub.database import InfrahubDatabase
 from infrahub.git import InfrahubRepository
 from infrahub.server import app, app_initialization
+from infrahub.services import InfrahubServices, services
 from tests.adapters.log import FakeTaskReportLogger
 from tests.constants import TestKind
 from tests.helpers.schema import CAR_SCHEMA, load_schema
@@ -24,7 +25,16 @@ if TYPE_CHECKING:
     from tests.helpers.file_repo import FileRepo
 
 
-class TestCreateRepository(TestInfrahubApp):
+@pytest.fixture
+def init_service():
+    original = services.service
+    service = InfrahubServices(client=InfrahubClient())
+    services.service = service
+    yield service
+    services.service = original
+
+
+class TestTransforms(TestInfrahubApp):
     @pytest.fixture(scope="class")
     async def base_dataset(self, db: InfrahubDatabase):
         await delete_all_nodes(db=db)
@@ -81,14 +91,14 @@ class TestCreateRepository(TestInfrahubApp):
         await app_initialization(app)
         return InfrahubTestClient(app=app)
 
-    @pytest.fixture
+    @pytest.fixture(scope="class")
     async def client(self, test_client: InfrahubTestClient, integration_helper):  # type: ignore[override]
         admin_token = await integration_helper.create_token()
         config = Config(api_token=admin_token, requester=test_client.async_request)
 
         return InfrahubClient(config=config)
 
-    @pytest.fixture
+    @pytest.fixture(scope="class")
     async def repo(self, test_client, client, db: InfrahubDatabase, git_repo_car_dealership: FileRepo, git_repos_dir):
         # Create the repository in the Graph
         obj = await Node.init(schema=InfrahubKind.REPOSITORY, db=db)
@@ -126,5 +136,24 @@ class TestCreateRepository(TestInfrahubApp):
         await t1.save(db=db)
 
         response = await client._get(url=f"{client.address}/api/transform/jinja2/test-rfile?name=John")
-
         assert response.text == "Name: John"
+
+    async def test_transform_python(
+        self, db: InfrahubDatabase, client: InfrahubClient, init_service, repo: InfrahubRepository
+    ):
+        repositories = await NodeManager.query(db=db, schema=InfrahubKind.REPOSITORY)
+        queries = await NodeManager.query(db=db, schema=InfrahubKind.GRAPHQLQUERY)
+
+        t2 = await Node.init(db=db, schema=InfrahubKind.TRANSFORMPYTHON)
+        await t2.new(
+            db=db,
+            name="test-python-transform",
+            query=str(queries[0].id),
+            repository=str(repositories[0].id),
+            class_name="PersonWithCarsTransform",
+            file_path="transforms/person_with_cars_transform.py",
+        )
+        await t2.save(db=db)
+
+        response = await client._get(url=f"{client.address}/api/transform/python/test-python-transform?name=John")
+        assert response.json() == {"name": "John"}
--- backend/tests/unit/git/test_git_transform.py
+++ backend/tests/unit/git/test_git_transform.py
@@ -1,14 +1,27 @@
+import pytest
+from infrahub_sdk import InfrahubClient
 from infrahub_sdk.uuidt import UUIDT
 
 from infrahub.core.constants import InfrahubKind
+from infrahub.exceptions import RepositoryFileNotFoundError, TransformError
 from infrahub.git import InfrahubRepository
-from infrahub.message_bus import messages
-from infrahub.services import InfrahubServices
+from infrahub.services import InfrahubServices, services
+from infrahub.transformations.models import TransformJinjaTemplateData, TransformPythonData
+from infrahub.transformations.tasks import transform_python, transform_render_jinja2_template
 
 
-async def test_git_transform_jinja2_success(git_repo_jinja: InfrahubRepository, helper):
+@pytest.fixture
+def init_service():
+    original = services.service
+    service = InfrahubServices(client=InfrahubClient())
+    services.service = service
+    yield service
+    services.service = original
+
+
+async def test_git_transform_jinja2_success(git_repo_jinja: InfrahubRepository, prefect_test_fixture, helper):
     commit = git_repo_jinja.get_commit_value(branch_name="main")
-    message = messages.TransformJinjaTemplate(
+    message = TransformJinjaTemplateData(
         repository_id=str(UUIDT()),
         repository_name=git_repo_jinja.name,
         repository_kind=InfrahubKind.REPOSITORY,
@@ -23,20 +36,14 @@ potum
 album
 magnum
 """
-
-    bus_simulator = helper.get_message_bus_simulator()
-    service = InfrahubServices(message_bus=bus_simulator)
-    bus_simulator.service = service
-
-    reply = await service.message_bus.rpc(message=message, response_class=messages.TransformJinjaTemplateResponse)
-    assert reply.passed
-    assert reply.data.rendered_template == expected_response
+    response = await transform_render_jinja2_template(message=message)
+    assert response == expected_response
 
 
-async def test_git_transform_jinja2_missing(git_repo_jinja: InfrahubRepository, helper):
+async def test_git_transform_jinja2_missing(git_repo_jinja: InfrahubRepository, prefect_test_fixture, helper):
     commit = git_repo_jinja.get_commit_value(branch_name="main")
 
-    message = messages.TransformJinjaTemplate(
+    message = TransformJinjaTemplateData(
         repository_id=str(UUIDT()),
         repository_name=git_repo_jinja.name,
         repository_kind=InfrahubKind.REPOSITORY,
@@ -46,19 +53,16 @@ async def test_git_transform_jinja2_missing(git_repo_jinja: InfrahubRepository, 
         data={"data": {"items": ["consilium", "potum", "album", "magnum"]}},
     )
 
-    bus_simulator = helper.get_message_bus_simulator()
-    service = InfrahubServices(message_bus=bus_simulator)
-    bus_simulator.service = service
+    with pytest.raises(RepositoryFileNotFoundError) as exc:
+        await transform_render_jinja2_template(message=message)
 
-    reply = await service.message_bus.rpc(message=message, response_class=messages.TransformJinjaTemplateResponse)
-    assert not reply.passed
-    assert "Unable to find the file" in reply.errors[0]
+    assert "Unable to find the file" in exc.value.message
 
 
-async def test_git_transform_jinja2_invalid(git_repo_jinja: InfrahubRepository, helper, caplog):
+async def test_git_transform_jinja2_invalid(git_repo_jinja: InfrahubRepository, prefect_test_fixture, helper, caplog):
     commit = git_repo_jinja.get_commit_value(branch_name="main")
 
-    message = messages.TransformJinjaTemplate(
+    message = TransformJinjaTemplateData(
         repository_id=str(UUIDT()),
         repository_name=git_repo_jinja.name,
         repository_kind=InfrahubKind.REPOSITORY,
@@ -68,10 +72,26 @@ async def test_git_transform_jinja2_invalid(git_repo_jinja: InfrahubRepository, 
         data={"data": {"items": ["consilium", "potum", "album", "magnum"]}},
     )
 
-    bus_simulator = helper.get_message_bus_simulator()
-    service = InfrahubServices(message_bus=bus_simulator)
-    bus_simulator.service = service
+    with pytest.raises(TransformError) as exc:
+        await transform_render_jinja2_template(message=message)
 
-    reply = await service.message_bus.rpc(message=message, response_class=messages.TransformJinjaTemplateResponse)
-    assert not reply.passed
-    assert "Encountered unknown tag 'end'." in reply.errors[0]
+    assert "Encountered unknown tag" in exc.value.message
+
+
+async def test_transform_python_success(
+    git_fixture_repo: InfrahubRepository, init_service, prefect_test_fixture, helper
+):
+    commit = git_fixture_repo.get_commit_value(branch_name="main")
+
+    message = TransformPythonData(
+        repository_id=str(git_fixture_repo.id),
+        repository_name=git_fixture_repo.name,
+        repository_kind=InfrahubKind.REPOSITORY,
+        commit=commit,
+        branch="main",
+        transform_location="unit/transforms/multiplier.py::Multiplier",
+        data={"multiplier": 2, "key": "abc", "answer": 21},
+    )
+
+    response = await transform_python(message=message)
+    assert response == {"key": "abcabc", "answer": 42}
