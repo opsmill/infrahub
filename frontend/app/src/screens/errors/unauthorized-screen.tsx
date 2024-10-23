d2d6ea6e092f840390e01e75275c055b43e4729d
pa-lem <pa-lem@hotmail.fr>
2024-10-23 10:56:49 +0200

    cleanup unauth screen

--- frontend/app/src/screens/errors/unauthorized-screen.tsx
+++ frontend/app/src/screens/errors/unauthorized-screen.tsx
@@ -7,19 +7,20 @@ type tUnauthorized = {
   className?: string;
   message?: string;
   icon?: ReactElement;
-  hideIcon?: boolean;
 };
 
 const DEFAULT_MESSAGE = "Sorry, you are not authorized to access this view.";
 
-export default function UnauthorizedScreen({ className, message, icon, hideIcon }: tUnauthorized) {
+export default function UnauthorizedScreen({ className, message, icon }: tUnauthorized) {
   return (
     <div className={classNames("flex flex-col flex-1 items-center justify-center p-8", className)}>
-      {!hideIcon && (
-        <div className="bg-custom-white rounded-full text-red-300">
-          {icon || <Icon icon={"mdi:warning-circle-outline"} className="text-3xl" />}
-        </div>
+      {icon || (
+        <Icon
+          icon={"mdi:warning-circle-outline"}
+          className="bg-custom-white rounded-full text-red-300 text-3xl"
+        />
       )}
+
       <Accordion
         title={"You can't access this view"}
         className="flex flex-col items-center w-full text-center"
