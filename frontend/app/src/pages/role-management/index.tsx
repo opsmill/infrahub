148dddd4eb0fb43cb79630229b612d3055bf853d
pa-lem <pa-lem@hotmail.fr>
2024-10-29 12:41:48 +0100

    check permissions on role management access + rename titles

--- frontend/app/src/pages/role-management/index.tsx
+++ frontend/app/src/pages/role-management/index.tsx
@@ -1,17 +1,35 @@
-import { useAuth } from "@/hooks/useAuth";
+import { GLOBAL_PERMISSION_OBJECT } from "@/config/constants";
+import useQuery from "@/hooks/useQuery";
+import ErrorScreen from "@/screens/errors/error-screen";
+import UnauthorizedScreen from "@/screens/errors/unauthorized-screen";
 import Content from "@/screens/layout/content";
+import LoadingScreen from "@/screens/loading-screen/loading-screen";
+import { getObjectPermissionsQuery } from "@/screens/permission/queries/getObjectPermissions";
 import { RoleManagementNavigation } from "@/screens/role-management";
-import { Navigate, Outlet } from "react-router-dom";
+import { gql } from "@apollo/client";
+import { Outlet } from "react-router-dom";
 
 function RoleManagement() {
-  const { accessToken } = useAuth();
+  const { loading, error } = useQuery(gql(getObjectPermissionsQuery(GLOBAL_PERMISSION_OBJECT)));
 
-  if (!accessToken) return <Navigate to={"/"} />;
+  if (loading) {
+    return <LoadingScreen message="Loading permissions..." />;
+  }
+
+  if (error) {
+    if (error.networkError?.statusCode === 403) {
+      const { message } = error.networkError?.result?.errors?.[0] ?? {};
+
+      return <UnauthorizedScreen message={message} />;
+    }
+
+    return <ErrorScreen message="Something went wrong when fetching the permissions." />;
+  }
 
   return (
     <Content.Card>
       <Content.CardTitle
-        title="Role Management"
+        title="Users & Permissions"
         description="Accounts, groups, roles and permissions management"
         className="border-none"
       />
