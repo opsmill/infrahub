ae9eda388dde5983c2f7f5ae46b4cac3108e9e40
pa-lem <pa-lem@hotmail.fr>
2024-11-06 08:27:39 +0100

    update test

--- frontend/app/tests/e2e/tasks/tasks-view.spec.ts
+++ frontend/app/tests/e2e/tasks/tasks-view.spec.ts
@@ -4,11 +4,7 @@ test.describe("Tasks - READ", () => {
   test("should correctly access to the tasks list and details", async ({ page }) => {
     await page.goto("/tasks");
     await expect(page.getByRole("heading", { name: "Task Overview" })).toBeVisible();
-    await page
-      .getByRole("row", { name: "didactic-skylark SCHEDULED -" })
-      .getByRole("link")
-      .nth(1)
-      .click();
+    await page.getByRole("row", { name: "SCHEDULED" }).getByRole("link").nth(1).click();
     await expect(page.getByRole("link", { name: "All tasks" })).toBeVisible();
     await expect(page.getByText("StateSCHEDULED")).toBeVisible();
     await expect(page.getByRole("heading", { name: "Task Logs (0)" })).toBeVisible();
