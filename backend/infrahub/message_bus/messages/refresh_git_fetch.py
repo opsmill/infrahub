5826d907bd9b21fce35b98bea2c945f89851ff99
Guillaume Mazoyer <guillaume@opsmill.com>
2024-11-07 21:55:02 +0100

    Fix mypy error

--- backend/infrahub/message_bus/messages/refresh_git_fetch.py
+++ backend/infrahub/message_bus/messages/refresh_git_fetch.py
@@ -9,5 +9,5 @@ class RefreshGitFetch(InfrahubMessage):
     repository_id: str = Field(..., description="The unique ID of the repository")
     repository_name: str = Field(..., description="The name of the repository")
     repository_kind: str = Field(..., description="The type of repository")
-    commit: str = Field(..., description="Specific commit to pull")
+    commit: str | None = Field(None, description="Specific commit to pull")
     infrahub_branch_name: str = Field(..., description="Infrahub branch on which to sync the remote repository")
