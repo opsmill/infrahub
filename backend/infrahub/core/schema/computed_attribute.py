93334f183b21171482341632c5961ebb6efb5e25
Patrick Ogenstad <patrick@ogenstad.com>
2024-11-10 17:54:38 +0100

    Allow computed_attribute object to be populated by a value key

--- backend/infrahub/core/schema/computed_attribute.py
+++ backend/infrahub/core/schema/computed_attribute.py
@@ -1,6 +1,6 @@
 from typing import Any, Optional
 
-from pydantic import ConfigDict, Field, model_serializer
+from pydantic import ConfigDict, Field, model_serializer, model_validator
 
 from infrahub.core.constants import ComputedAttributeKind
 from infrahub.core.models import HashableModel
@@ -14,12 +14,30 @@ class ComputedAttribute(HashableModel):
     transform: Optional[str] = Field(
         default=None, description="The Python Transform name or ID, required when assignment_type=transform"
     )
+    value: Optional[dict[str, Any]] = Field(
+        default=None,
+        description="Only used for internal serialization",
+    )
 
     @model_serializer
     def ser_model(self) -> dict[str, Any]:
         return {
             "value": {"kind": self.kind.value, "jinja2_template": self.jinja2_template, "transform": self.transform}
         }
+
+    @model_validator(mode="before")
+    @classmethod
+    def validate_time_from_if_required(cls, values: dict[str, Any]) -> dict[str, Any]:
+        value: dict | None = values.get("value")
+        if value:
+            if "kind" not in values:
+                values["kind"] = value.get("kind")
+            if "jinja2_template" not in values:
+                values["jinja2_template"] = value.get("jinja2_template")
+            if "transform" not in values:
+                values["transform"] = value.get("transform")
+
+        return values
 
     model_config = ConfigDict(
         extra="forbid",
