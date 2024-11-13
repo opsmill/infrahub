2700b6a50603c558ee68fd7558feeeb3b53541f4
pa-lem <pa-lem@hotmail.fr>
2024-11-13 09:58:18 +0100

    update test

--- frontend/app/tests/e2e/tasks/tasks-view.spec.ts
+++ frontend/app/tests/e2e/tasks/tasks-view.spec.ts
@@ -4,7 +4,6 @@ test.describe("Tasks - READ", () => {
   test("should correctly access to the tasks list and details", async ({ page }) => {
     await page.goto("/tasks");
     await expect(page.getByRole("heading", { name: "Task Overview" })).toBeVisible();
-    await expect(page.getByRole("link", { name: "All tasks" })).toBeVisible();
     await expect(page.getByText("StateSCHEDULED")).toBeVisible();
   });
 });
