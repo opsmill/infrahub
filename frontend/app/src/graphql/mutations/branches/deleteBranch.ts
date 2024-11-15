17ab4b68885412b44c5e66e48c2c6088870edafe
pa-lem <pa-lem@hotmail.fr>
2024-11-15 15:12:51 +0100

    remove wait for completion in mutation

--- frontend/app/src/graphql/mutations/branches/deleteBranch.ts
+++ frontend/app/src/graphql/mutations/branches/deleteBranch.ts
@@ -3,7 +3,6 @@ import { gql } from "@apollo/client";
 export const BRANCH_DELETE = gql`
 mutation BRANCH_DELETE($name: String) {
   BranchDelete (
-    wait_until_completion: false
     data: {
       name: $name
     }
