975466773e802f08fff95c09119927c8aa2990fd
Guillaume Mazoyer <guillaume@opsmill.com>
2024-11-21 10:19:06 +0100

    Remove FIXME for storage REST endpoints

--- backend/infrahub/api/storage.py
+++ backend/infrahub/api/storage.py
@@ -35,7 +35,6 @@ def upload_content(
     item: UploadContentPayload,
     _: str = Depends(get_current_user),
 ) -> UploadResponse:
-    # FIXME: Lock with permission?
     # TODO need to optimized how we read the content of the file, especially if the file is really large
     # Check this discussion for more details
     # https://stackoverflow.com/questions/63048825/how-to-upload-file-using-fastapi
@@ -53,7 +52,6 @@ def upload_file(
     file: UploadFile = File(...),
     _: str = Depends(get_current_user),
 ) -> UploadResponse:
-    # FIXME: Lock with permission?
     # TODO need to optimized how we read the content of the file, especially if the file is really large
     # Check this discussion for more details
     # https://stackoverflow.com/questions/63048825/how-to-upload-file-using-fastapi
