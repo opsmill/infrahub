ba2a2276e84d742a133b950ccceae91e73bc4374
Patrick Ogenstad <patrick@ogenstad.com>
2024-11-28 14:04:43 +0100

    Remove flows for webhooks and node mutated event

--- backend/infrahub/message_bus/operations/event/node.py
+++ backend/infrahub/message_bus/operations/event/node.py
@@ -1,7 +1,5 @@
 from typing import List
 
-from prefect import flow
-
 from infrahub.core.constants import InfrahubKind
 from infrahub.log import get_logger
 from infrahub.message_bus import InfrahubMessage, messages
@@ -10,7 +8,6 @@ from infrahub.services import InfrahubServices
 log = get_logger()
 
 
-@flow(name="event-node-mutated")
 async def mutated(
     message: messages.EventNodeMutated,
     service: InfrahubServices,
