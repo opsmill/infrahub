830d177b5a9c04e5525b7269558d1e100d3e5628
Aaron McCarty <aaron@opsmill.com>
2024-12-05 11:29:07 -0800

    support memgraph

--- backend/infrahub/core/query/delete.py
+++ backend/infrahub/core/query/delete.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+from infrahub import config
 from infrahub.core.query import Query, QueryType
 from infrahub.core.timestamp import Timestamp
 from infrahub.database import InfrahubDatabase
@@ -16,47 +17,42 @@ class DeleteAfterTimeQuery(Query):
 
     async def query_init(self, db: InfrahubDatabase, **kwargs: Any) -> None:
         self.params = {"timestamp": self.timestamp.to_string()}
-        query = """
-// ---------------------
-// Reset edges with to time after timestamp
-// ---------------------
-CALL {
-    OPTIONAL MATCH (p)-[r]-(q)
-    WHERE r.to > $timestamp
-    SET r.to = NULL
-}
-// ---------------------
-// Delete edges with from time after timestamp timestamp
-// ---------------------
-CALL {
-    OPTIONAL MATCH (p)-[r]->(q)
-    WHERE r.from > $timestamp
-    DELETE r
-    WITH p, q
-    UNWIND [p, q] AS maybe_orphan
-    WITH maybe_orphan
-    WHERE NOT exists((maybe_orphan)--())
-    DELETE maybe_orphan
-}
-"""
-        self.add_to_query(query)
-
-        # if config.SETTINGS.database.db_type == config.DatabaseType.MEMGRAPH:
-        #     query = """
-        #     MATCH p = (s)-[r]-(d)
-        #     WHERE r.branch = $branch_name
-        #     DELETE r
-        #     """
-        # else:
-        #     query = """
-        #     MATCH p = (s)-[r]-(d)
-        #     WHERE r.branch = $branch_name
-        #     DELETE r
-        #     WITH *
-        #     UNWIND nodes(p) AS n
-        #     MATCH (n)
-        #     WHERE NOT exists((n)--())
-        #     DELETE n
-        #     """
-        # self.params["branch_name"] = self.branch_name
-        # self.add_to_query(query)
+        query_1 = """
+        // ---------------------
+        // Reset edges with to time after timestamp
+        // ---------------------
+        CALL {
+            OPTIONAL MATCH (p)-[r]-(q)
+            WHERE r.to > $timestamp
+            SET r.to = NULL
+        }
+        """
+        self.add_to_query(query_1)
+        if config.SETTINGS.database.db_type is config.DatabaseType.NEO4J:
+            query_2 = """
+            // ---------------------
+            // Delete edges with from time after timestamp timestamp
+            // ---------------------
+            CALL {
+                OPTIONAL MATCH (p)-[r]->(q)
+                WHERE r.from > $timestamp
+                DELETE r
+                WITH p, q
+                UNWIND [p, q] AS maybe_orphan
+                WITH maybe_orphan
+                WHERE NOT exists((maybe_orphan)--())
+                DELETE maybe_orphan
+            }
+            """
+        else:
+            query_2 = """
+            // ---------------------
+            // Delete edges with from time after timestamp timestamp
+            // ---------------------
+            CALL {
+                OPTIONAL MATCH (p)-[r]->(q)
+                WHERE r.from > $timestamp
+                DELETE r
+            }
+            """
+        self.add_to_query(query_2)
