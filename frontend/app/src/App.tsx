846a42810161abed381c62fa8b356708e223b5d6
Bilal ABBAD <bilal@opsmill.com>
2024-12-10 11:29:11 +0100

    refactor Search Anywhere component (#5175)
    - Used cmdk everywhere
    - split initial big component into smaller well-scoped components
    - introduce @tanstack/query for fetching
    - improved search anywhere context and how to consume it
    - better navigation
    - some fixes

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -11,9 +11,12 @@ import { store } from "@/state";
 import { ApolloProvider } from "@apollo/client";
 import { addCollection } from "@iconify-icon/react";
 import mdiIcons from "@iconify-json/mdi/icons.json";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 import "./styles/index.css";
 import "react-toastify/dist/ReactToastify.css";
+import { queryClient } from "@/api/client";
+import { TanStackQueryDevtools } from "@/devtools";
 
 addCollection(mdiIcons);
 
@@ -22,17 +25,20 @@ export function App() {
     <ErrorBoundary FallbackComponent={ErrorFallback}>
       <Provider store={store}>
         <AuthProvider>
-          <ApolloProvider client={graphqlClient}>
-            <ToastContainer
-              hideProgressBar={true}
-              transition={Slide}
-              autoClose={5000}
-              closeOnClick={false}
-              newestOnTop
-              position="bottom-right"
-            />
-            <RouterProvider router={router} />
-          </ApolloProvider>
+          <QueryClientProvider client={queryClient}>
+            <ApolloProvider client={graphqlClient}>
+              <ToastContainer
+                hideProgressBar={true}
+                transition={Slide}
+                autoClose={5000}
+                closeOnClick={false}
+                newestOnTop
+                position="bottom-right"
+              />
+              <RouterProvider router={router} />
+            </ApolloProvider>
+            <TanStackQueryDevtools />
+          </QueryClientProvider>
         </AuthProvider>
       </Provider>
     </ErrorBoundary>
