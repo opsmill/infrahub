c3aa86a7a8698274b890cf682f47e4185d7083e7
Bilal ABBAD <bilal@opsmill.com>
2024-12-10 13:28:38 +0100

    Refresh menu after access token has expired (#5180)
    Issue: https://github.com/opsmill/infrahub/issues/5099

--- frontend/app/src/api/client.ts
+++ frontend/app/src/api/client.ts
@@ -1,4 +1,11 @@
 import { QueryClient } from "@tanstack/react-query";
+import createClient, { Middleware } from "openapi-fetch";
+
+import { INFRAHUB_API_SERVER_URL } from "@/config/config";
+
+import { ACCESS_TOKEN_KEY } from "@/config/localStorage";
+import { getNewToken } from "@/hooks/useAuth";
+import type { paths } from "@/infraops";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -7,3 +14,34 @@ export const queryClient = new QueryClient({
     },
   },
 });
+
+export const apiClient = createClient<paths>({ baseUrl: INFRAHUB_API_SERVER_URL });
+
+const authMiddleware: Middleware = {
+  async onRequest({ request }) {
+    const accessToken = localStorage.getItem(ACCESS_TOKEN_KEY);
+
+    if (!accessToken) return request;
+
+    request.headers.set("Authorization", `Bearer ${accessToken}`);
+    return request;
+  },
+  async onResponse({ request, response }) {
+    if (response.status === 401) {
+      try {
+        const newToken = await getNewToken();
+
+        if (!newToken?.access_token) {
+          return response;
+        }
+
+        request.headers.set("Authorization", `Bearer ${newToken.access_token}`);
+        return fetch(request);
+      } catch (e) {
+        return response;
+      }
+    }
+  },
+};
+
+apiClient.use(authMiddleware);
