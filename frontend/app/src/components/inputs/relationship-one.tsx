90c67bfc97d5ac4c5a9f9809f0d2779987ab74bc
Bilal ABBAD <bilal@opsmill.com>
2024-12-12 14:09:29 +0100

    chore: removed duplicated component AddRelationshipAction (#5199)

--- frontend/app/src/components/inputs/relationship-one.tsx
+++ frontend/app/src/components/inputs/relationship-one.tsx
@@ -1,6 +1,4 @@
 import { Button } from "@/components/buttons/button-primitive";
-import SlideOver, { SlideOverTitle } from "@/components/display/slide-over";
-import ObjectForm from "@/components/form/object-form";
 import { PoolValue } from "@/components/form/pool-selector";
 import {
   Combobox,
@@ -18,6 +16,7 @@ import { useDebounce } from "@/hooks/useDebounce";
 import { useLazyQuery } from "@/hooks/useQuery";
 import { useSchema } from "@/hooks/useSchema";
 import { POOLS_DICTIONNARY, POOLS_PEER } from "@/screens/ipam/constants";
+import { AddRelationshipAction } from "@/screens/objects/relationships/components/add-relationship-action";
 import { classNames } from "@/utils/common";
 import { Node, RelationshipManyType } from "@/utils/getObjectItemDisplayValue";
 import { gql } from "@apollo/client";
@@ -265,57 +264,3 @@ export const RelationshipInput = React.forwardRef<
     </Combobox>
   );
 });
-
-export interface AddRelationshipActionProps {
-  peer: string;
-  onSuccess?: (newObject: Node) => void;
-}
-
-const AddRelationshipAction: React.FC<AddRelationshipActionProps> = ({ peer, onSuccess }) => {
-  const { schema } = useSchema(peer);
-  const [open, setOpen] = useState(false);
-
-  if (!schema) return null;
-
-  return (
-    <div className="p-2 pt-0">
-      <Button
-        className="w-full bg-custom-blue-700/10 border border-custom-blue-700/20 text-custom-blue-700 enabled:hover:bg-custom-blue-700/20"
-        onClick={() => setOpen(!open)}
-      >
-        + Add new {schema.label}
-      </Button>
-
-      <SlideOver
-        title={
-          <SlideOverTitle
-            schema={schema}
-            currentObjectLabel="New"
-            title={`Create ${schema.label}`}
-            subtitle={schema.description}
-          />
-        }
-        offset={1}
-        open={open}
-        setOpen={setOpen}
-      >
-        <ObjectForm
-          kind={peer}
-          onSuccess={({ object }) => {
-            setOpen(false);
-            if (!onSuccess) return;
-
-            const newNode: Node = {
-              id: object.id,
-              display_label: object.display_label,
-              __typename: peer,
-            };
-            onSuccess(newNode);
-          }}
-          onCancel={() => setOpen(false)}
-          data-testid="new-object-form"
-        />
-      </SlideOver>
-    </div>
-  );
-};
