06ccd698e070f140a441d40758aa5353b91b67cd
Paul Leménager <pa-lem@hotmail.fr>
2024-12-18 16:26:51 +0100

    Display tasks in the proposed changes view only if needed - IFC-1010 (#5248)
    * add query
    
    * update style
    
    * check count and hide tasks accordion in proposed changes view
    
    * update filters
    
    * disable merge button
    
    * fix data access
    
    * add fragment
    
    * update fragment
    
    * update pollinterval
    
    * add pollintervak on proposed changes data to update informations
    
    * update fragment
    
    * update fragment
    
    * fix typo

--- frontend/app/src/screens/proposed-changes/proposed-change-details.tsx
+++ frontend/app/src/screens/proposed-changes/proposed-change-details.tsx
@@ -6,7 +6,8 @@ import { Property, PropertyList } from "@/components/table/property-list";
 import { Badge } from "@/components/ui/badge";
 import { Card, CardWithBorder } from "@/components/ui/card";
 import { Tooltip } from "@/components/ui/tooltip";
-import { PROPOSED_CHANGES_OBJECT } from "@/config/constants";
+import { PROPOSED_CHANGES_OBJECT, TASK_OBJECT } from "@/config/constants";
+import { TASK_DETAILS_CHECK } from "@/graphql/queries/tasks/checkTasksItemDetails";
 import useQuery from "@/hooks/useQuery";
 import { PcApproveButton } from "@/screens/proposed-changes/action-button/pc-approve-button";
 import { PcCloseButton } from "@/screens/proposed-changes/action-button/pc-close-button";
@@ -23,13 +24,10 @@ import { useAtom } from "jotai";
 import { HTMLAttributes } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { TaskDisplay } from "../branches/task-display";
+import LoadingScreen from "../loading-screen/loading-screen";
 import { getObjectPermissionsQuery } from "../permission/queries/getObjectPermissions";
 import { getPermission } from "../permission/utils";
-import {
-  BRANCH_MERGE_WORKFLOW,
-  BRANCH_REBASE_WORKFLOW,
-  BRANCH_VALIDATE_WORKFLOW,
-} from "../tasks/constants";
+import { PROPOSED_CHANGE_MERGE_WORKFLOW } from "../tasks/constants";
 
 export const ProposedChangeDetails = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => {
   const { proposedChangeId } = useParams();
@@ -37,9 +35,17 @@ export const ProposedChangeDetails = ({ className, ...props }: HTMLAttributes<HT
 
   const navigate = useNavigate();
 
-  const { loading, data, error } = useQuery(
-    gql(getObjectPermissionsQuery(PROPOSED_CHANGES_OBJECT))
-  );
+  const { data } = useQuery(gql(getObjectPermissionsQuery(PROPOSED_CHANGES_OBJECT)), {
+    pollInterval: 2000,
+  });
+
+  const { loading: loadingCheck, data: checkData } = useQuery(TASK_DETAILS_CHECK, {
+    variables: {
+      workflow: [PROPOSED_CHANGE_MERGE_WORKFLOW],
+      relatedNodes: proposedChangeId ? [proposedChangeId] : undefined,
+    },
+    pollInterval: 2000,
+  });
 
   const permission = getPermission(data?.[PROPOSED_CHANGES_OBJECT]?.permissions?.edges);
 
@@ -122,7 +128,7 @@ export const ProposedChangeDetails = ({ className, ...props }: HTMLAttributes<HT
             proposedChangeId={proposedChangeId!}
             state={state}
             sourceBranch={proposedChangesDetails?.source_branch?.value}
-            disabled={!permission.update.isAllowed}
+            disabled={!permission.update.isAllowed || (checkData && checkData[TASK_OBJECT].count)}
           />
           <PcCloseButton
             proposedChangeId={proposedChangeId!}
@@ -136,16 +142,20 @@ export const ProposedChangeDetails = ({ className, ...props }: HTMLAttributes<HT
 
   return (
     <div className="bg-stone-50 p-2.5 flex flex-col flex-grow gap-2.5">
-      <Card>
-        <Accordion title={<div className="font-normal text-xs">Tasks</div>}>
-          <div className="mt-2">
-            <TaskDisplay
-              relatedNode={proposedChangeId}
-              workflow={[BRANCH_VALIDATE_WORKFLOW, BRANCH_MERGE_WORKFLOW, BRANCH_REBASE_WORKFLOW]}
-            />
-          </div>
-        </Accordion>
-      </Card>
+      {loadingCheck && <LoadingScreen hideText />}
+
+      {!loadingCheck && checkData && !!checkData[TASK_OBJECT].count && (
+        <Card>
+          <Accordion title={<div className="font-normal text-xs">Actions in progress</div>}>
+            <div className="mt-2">
+              <TaskDisplay
+                relatedNode={proposedChangeId}
+                workflow={[PROPOSED_CHANGE_MERGE_WORKFLOW]}
+              />
+            </div>
+          </Accordion>
+        </Card>
+      )}
 
       <div className={classNames("grid grid-cols-3 gap-2", className)} {...props}>
         <div className="col-start-1 col-end-3 space-y-4">
