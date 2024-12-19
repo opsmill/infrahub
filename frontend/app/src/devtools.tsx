961040bd27356f56f9b9becd77cd3d7c06b38cc3
bilalabbad <bilal@opsmill.com>
2024-12-19 12:07:56 +0100

    Added new "dev" command that enable devtools on UI

--- frontend/app/src/devtools.tsx
+++ frontend/app/src/devtools.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 
-export const TanStackQueryDevtools = import.meta.env.PROD
-  ? () => null
-  : React.lazy(() =>
+export const TanStackQueryDevtools = import.meta.env.VITE_DEVTOOLS
+  ? React.lazy(() =>
       import("@tanstack/react-query-devtools").then((module) => ({
         default: module.ReactQueryDevtools,
       }))
-    );
+    )
+  : () => null;
