aecc5d238523753fe10231029b20c0a994f6704a
Brett Lykins <brett@opsmill.com>
2025-01-05 19:25:32 -0500

    add release note file to docs

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -315,6 +315,7 @@ const sidebars: SidebarsConfig = {
             slug: 'release-notes/infrahub',
           },
           items: [
+            'release-notes/infrahub/release-1_1_1',
             'release-notes/infrahub/release-1_1_0',
             'release-notes/infrahub/release-1_0_10',
             'release-notes/infrahub/release-1_0_9',
