32a947ac1a599dc3a869945bca4278929c639173
Guillaume Mazoyer <guillaume@opsmill.com>
2025-01-08 10:50:23 +0100

    Remove test left over

--- backend/tests/unit/api/test_70_openapi.py
+++ backend/tests/unit/api/test_70_openapi.py
@@ -1,26 +1,10 @@
-import pytest
 from fastapi.testclient import TestClient
 
-from infrahub import config
 from infrahub.core.branch import Branch
 
 
 async def test_openapi(client: TestClient, default_branch: Branch, register_core_models_schema: None) -> None:
     """Validate that the OpenAPI specs can be generated."""
-    with client:
-        response = client.get("/api/openapi.json")
-
-    assert response.status_code == 200
-    assert response.json() is not None
-
-
-@pytest.mark.parametrize("allow_anonymous_access", [False, True])
-async def test_openapi_anonymous_account(
-    client: TestClient, default_branch: Branch, register_core_models_schema: None, allow_anonymous_access: bool
-) -> None:
-    """Validate that the OpenAPI specs can be generated."""
-    config.SETTINGS.main.allow_anonymous_access = allow_anonymous_access
-
     with client:
         response = client.get("/api/openapi.json")
 
