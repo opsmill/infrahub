f35a3ea3394ad710cd7bdb3e23e9ad965a75affd
Brett Lykins <brett@opsmill.com>
2025-01-08 16:01:51 -0500

    prepare v1.1.2

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -315,6 +315,7 @@ const sidebars: SidebarsConfig = {
             slug: 'release-notes/infrahub',
           },
           items: [
+            'release-notes/infrahub/release-1_1_2',
             'release-notes/infrahub/release-1_1_1',
             'release-notes/infrahub/release-1_1_0',
             'release-notes/infrahub/release-1_0_10',
