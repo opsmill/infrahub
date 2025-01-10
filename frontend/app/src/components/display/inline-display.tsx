ae216e595c02735a65f83bad0aada7c749513eaa
pa-lem <pa-lem@hotmail.fr>
2025-01-10 15:14:02 +0100

    use hover for inline display button

--- frontend/app/src/components/display/inline-display.tsx
+++ frontend/app/src/components/display/inline-display.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/buttons/button-primitive";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { ReactElement } from "react";
+import { debounce } from "@/utils/common";
+import { ReactElement, useState } from "react";
 
 type Item = string | Record<string, string>;
 
@@ -25,8 +26,18 @@ const handleRender = (item: Item, render: Render) => {
 };
 
 export function InlineDisplay({ items, render, maxDisplay = 3 }: InlineDisplayProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
   const trimedItems = items.slice(0, maxDisplay);
   const remainingItems = items.slice(maxDisplay);
+
+  const handleMouseEnter = () => {
+    setIsOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsOpen(false);
+  };
 
   return (
     <div className="flex items-center gap-4">
@@ -35,12 +46,23 @@ export function InlineDisplay({ items, render, maxDisplay = 3 }: InlineDisplayPr
       </div>
 
       {!!remainingItems?.length && (
-        <Popover>
-          <PopoverTrigger asChild>
+        <Popover open={isOpen}>
+          <PopoverTrigger
+            asChild
+            onMouseEnter={debounce(handleMouseEnter, 200)}
+            onMouseLeave={debounce(handleMouseLeave, 200)}
+          >
             <Button variant="outline" size={"icon"}>{`+${remainingItems?.length}`}</Button>
           </PopoverTrigger>
 
-          <PopoverContent align="start">
+          <PopoverContent
+            align="start"
+            onMouseEnter={debounce(handleMouseEnter, 200)}
+            onMouseLeave={debounce(handleMouseLeave, 200)}
+            onClick={(event) => {
+              event.preventDefault();
+            }}
+          >
             <div className="flex flex-col gap-2">
               {remainingItems.map((item) => handleRender(item, render))}
             </div>
