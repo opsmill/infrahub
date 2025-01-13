36d6c3a35aa03820e4a7c40b11e8abfe58ddfa39
pa-lem <pa-lem@hotmail.fr>
2025-01-13 19:05:16 +0100

    remove old files

--- frontend/app/src/screens/branches/branch-actions/graphql/getBranchActionState.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const GET_BRANCH_ACTION_STATE = gql`
-  query GET_BRANCH_ACTION_STATE($branch: String!, $workflow: [String], $state: [StateType]) {
-    InfrahubTask(branch: $branch, workflow: $workflow, state: $state) {
-      count
-    }
-  }
-`;
