ec78c6b7b583566d33edb57fdf7b0229d6a8c5dd
Aaron McCarty <aaron@opsmill.com>
2025-01-15 13:19:10 -0800

    separate node specifiers

--- backend/infrahub/core/query/diff.py
+++ backend/infrahub/core/query/diff.py
@@ -568,17 +568,26 @@ WITH CASE
     ELSE [[$new_node_field_specifiers, $branch_from_time], [$current_node_field_specifiers, $from_time]]
 END AS diff_filter_params_list
 UNWIND diff_filter_params_list AS diff_filter_params
+WITH diff_filter_params[0] AS node_field_specifiers_list, diff_filter_params[1] AS from_time
 CALL {
-    WITH diff_filter_params
-    WITH diff_filter_params[0] AS node_field_specifiers_list, diff_filter_params[1] AS from_time
+    // -------------------------------------
+    // These lists contain duplicate data, but vastly improve querying speed below
+    // -------------------------------------
+    WITH node_field_specifiers_list
+    UNWIND node_field_specifiers_list AS nfs
+    WITH nfs[0] AS uuid, nfs[1] AS field_name
+    WITH collect(DISTINCT uuid) as uuids, collect(DISTINCT field_name) AS field_names
+    RETURN uuids AS node_ids_list, field_names AS field_names_list
+}
+CALL {
+    WITH node_field_specifiers_list, node_ids_list, from_time
     CALL {
-        WITH node_field_specifiers_list, from_time
-        WITH reduce(node_ids = [], nfs IN node_field_specifiers_list | node_ids + [nfs[0]]) AS node_ids_list, from_time
+        WITH node_field_specifiers_list, from_time, node_ids_list
         // -------------------------------------
         // Identify nodes added/removed on branch
         // -------------------------------------
         MATCH (q:Root)<-[diff_rel:IS_PART_OF {branch: $branch_name}]-(p:Node)
-        WHERE (node_ids_list IS NULL OR p.uuid IN node_ids_list)
+        WHERE (size(node_ids_list) = 0 OR p.uuid IN node_ids_list)
         AND (from_time <= diff_rel.from < $to_time)
         AND (diff_rel.to IS NULL OR (from_time <= diff_rel.to < $to_time))
         AND p.branch_support = $branch_aware
@@ -647,18 +656,19 @@ CALL {
     }
     RETURN diff_path
     UNION
-    WITH diff_filter_params
-    WITH diff_filter_params[0] AS node_field_specifiers_list, diff_filter_params[1] AS from_time
+    WITH node_field_specifiers_list, node_ids_list, field_names_list, from_time
     CALL {
-        WITH node_field_specifiers_list, from_time
+        WITH node_field_specifiers_list, node_ids_list, field_names_list, from_time
         // -------------------------------------
         // Identify attributes/relationships added/removed on branch
         // -------------------------------------
         CALL {
-            WITH node_field_specifiers_list, from_time
+            WITH node_field_specifiers_list, node_ids_list, field_names_list, from_time
             MATCH (root:Root)<-[r_root:IS_PART_OF]-(p:Node)-[diff_rel:HAS_ATTRIBUTE {branch: $branch_name}]->(q:Attribute)
             // exclude attributes and relationships under added/removed nodes b/c they are covered above
-            WHERE (node_field_specifiers_list IS NULL OR [p.uuid, q.name] IN node_field_specifiers_list)
+            WHERE (size(node_ids_list) = 0 OR p.uuid IN node_ids_list)
+            AND (size(field_names_list) = 0 OR q.name IN field_names_list)
+            AND (node_field_specifiers_list IS NULL OR [p.uuid, q.name] IN node_field_specifiers_list)
             AND r_root.branch IN [$branch_name, $base_branch_name, $global_branch_name]
             AND q.branch_support = $branch_aware
             // if p has a different type of branch support and was addded within our timeframe
@@ -671,10 +681,12 @@ CALL {
             AND (r_root.to IS NULL OR diff_rel.branch <> r_root.branch OR r_root.to >= diff_rel.from)
             RETURN root, r_root, p, diff_rel, q
             UNION ALL
-            WITH node_field_specifiers_list, from_time
+            WITH node_field_specifiers_list, node_ids_list, field_names_list, from_time
             MATCH (root:Root)<-[r_root:IS_PART_OF]-(p:Node)-[diff_rel:IS_RELATED {branch: $branch_name}]-(q:Relationship)
             // exclude attributes and relationships under added/removed nodes b/c they are covered above
-            WHERE (node_field_specifiers_list IS NULL OR [p.uuid, q.name] IN node_field_specifiers_list)
+            WHERE (size(node_ids_list) = 0 OR p.uuid IN node_ids_list)
+            AND (size(field_names_list) = 0 OR q.name IN field_names_list)
+            AND (node_field_specifiers_list IS NULL OR [p.uuid, q.name] IN node_field_specifiers_list)
             AND r_root.branch IN [$branch_name, $base_branch_name, $global_branch_name]
             AND q.branch_support = $branch_aware
             // if p has a different type of branch support and was addded within our timeframe
@@ -762,15 +774,16 @@ CALL {
     }
     RETURN mid_diff_path AS diff_path
     UNION
-    WITH diff_filter_params
-    WITH diff_filter_params[0] AS node_field_specifiers_list, diff_filter_params[1] AS from_time
+    WITH node_field_specifiers_list, node_ids_list, field_names_list, from_time
     CALL {
-        WITH node_field_specifiers_list, from_time
+        WITH node_field_specifiers_list, node_ids_list, field_names_list, from_time
         // -------------------------------------
         // Identify properties added/removed on branch
         // -------------------------------------
         MATCH diff_rel_path = (root:Root)<-[r_root:IS_PART_OF]-(n:Node)-[r_node]-(p)-[diff_rel {branch: $branch_name}]->(q)
-        WHERE (node_field_specifiers_list IS NULL OR [n.uuid, p.name] IN node_field_specifiers_list)
+        WHERE (size(node_ids_list) = 0 OR n.uuid IN node_ids_list)
+        AND (size(field_names_list) = 0 OR p.name IN field_names_list)
+        AND (node_field_specifiers_list IS NULL OR [n.uuid, p.name] IN node_field_specifiers_list)
         AND (
             (from_time <= diff_rel.from < $to_time)
             OR (from_time <= diff_rel.to < $to_time)
