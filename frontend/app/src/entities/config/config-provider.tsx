e2038695c49a085a41f226218cbc4fcb0837034b
bilalabbad <bilal@opsmill.com>
2025-01-15 10:54:34 +0100

    added component infrahub loading

--- frontend/app/src/entities/config/config-provider.tsx
+++ frontend/app/src/entities/config/config-provider.tsx
@@ -1,5 +1,5 @@
-import infrahubLogo from "@/assets/infrahub-logo.svg";
 import { getConfigQueryOptions } from "@/entities/config/get-config.query";
+import { InfrahubLoading } from "@/shared/components/loading/infrahub-loading";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
@@ -7,12 +7,7 @@ export const ConfigProvider = ({ children }: { children: React.ReactNode }) => {
   const { isPending, error } = useQuery(getConfigQueryOptions());
 
   if (isPending) {
-    return (
-      <div className="h-screen w-screen bg-stone-100 flex items-center justify-center flex-col">
-        <img src={infrahubLogo} alt="Infrahub logo" className="h-14 animate-bounce" />
-        <span className="text-neutral-900 font-medium">Loading config...</span>
-      </div>
-    );
+    return <InfrahubLoading>Loading config...</InfrahubLoading>;
   }
 
   if (error) {
