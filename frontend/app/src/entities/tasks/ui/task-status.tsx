6256c6254a60ff9c8fa78cc320e797df4bea5239
bilalabbad <bilal@opsmill.com>
2025-01-17 17:01:52 +0100

    cleaning

--- frontend/app/src/entities/tasks/ui/task-status.tsx
+++ frontend/app/src/entities/tasks/ui/task-status.tsx
@@ -3,7 +3,7 @@ import { QSP } from "@/config/qsp";
 import { useCurrentBranch } from "@/entities/branches/ui/hooks/use-current-branch";
 import { isTaskRunningOnBranchQueryOptions } from "@/entities/tasks/domain/is-task-running-on-branch.query";
 import { constructPath } from "@/shared/api/rest/fetch";
-import { LinkButton } from "@/shared/components/buttons/button-primitive";
+import { LinkButton, LinkButtonProps } from "@/shared/components/buttons/button-primitive";
 import { Pulse } from "@/shared/components/ui/pulse";
 import { Spinner } from "@/shared/components/ui/spinner";
 import { Tooltip } from "@/shared/components/ui/tooltip";
@@ -28,17 +28,18 @@ export function TaskStatus() {
     value: branch?.name,
   };
 
+  const commonButtonProps: LinkButtonProps = {
+    size: "square",
+    variant: "ghost",
+    className: "h-8 w-8 bg-neutral-50 border border-neutral-200 rounded-lg relative",
+    to: constructPath("/tasks", [{ name: QSP.FILTER, value: JSON.stringify([filter]) }]),
+  };
+
   if (error) {
     const tooltipContent = "Error checking task status";
     return (
       <Tooltip enabled content={tooltipContent}>
-        <LinkButton
-          size="square"
-          variant="ghost"
-          className="h-8 w-8 bg-neutral-50 border border-neutral-200 rounded-lg relative"
-          to={constructPath("/tasks", [{ name: QSP.FILTER, value: JSON.stringify([filter]) }])}
-          aria-label={tooltipContent}
-        >
+        <LinkButton {...commonButtonProps} aria-label={tooltipContent}>
           <Icon icon="mdi:error-outline" className="text-red-500" />
         </LinkButton>
       </Tooltip>
@@ -51,15 +52,8 @@ export function TaskStatus() {
 
   return (
     <Tooltip enabled content={tooltipContent}>
-      <LinkButton
-        size="square"
-        variant="ghost"
-        className="h-8 w-8 bg-neutral-50 border border-neutral-200 rounded-lg relative"
-        to={constructPath("/tasks", [{ name: QSP.FILTER, value: JSON.stringify([filter]) }])}
-        aria-label={tooltipContent}
-      >
+      <LinkButton {...commonButtonProps} aria-label={tooltipContent}>
         {isPending ? <Spinner /> : <TasksStatusIcon />}
-
         {isTaskRunningOnBranch && (
           <Pulse className="right-[6.5px] bottom-[6.5px]" data-testid="pulse" />
         )}
