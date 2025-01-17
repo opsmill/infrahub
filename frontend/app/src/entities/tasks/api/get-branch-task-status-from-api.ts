a5bcd11d83e9e9c9d3d396419fc5c8364f6d4253
bilalabbad <bilal@opsmill.com>
2025-01-17 10:56:06 +0100

    fix

--- frontend/app/src/entities/tasks/api/get-branch-task-status-from-api.ts
+++ frontend/app/src/entities/tasks/api/get-branch-task-status-from-api.ts
@@ -1,16 +1,17 @@
-import { gql } from "@/shared/api/graphql/generated";
+import { Tasks_Branch_Status_CountQuery } from "@/shared/api/graphql/generated/graphql";
 import graphqlClient from "@/shared/api/graphql/graphqlClientApollo";
+import { gql } from "@apollo/client";
 
-export const TASKS_BRANCH_STATUS_COUNT = gql(/* GraphQL */ `
+export const TASKS_BRANCH_STATUS_COUNT = gql`
   query TASKS_BRANCH_STATUS_COUNT($branch: String!) {
     InfrahubTaskBranchStatus(branch: $branch){
       count
     }
   }
-`);
+`;
 
 export const getBranchTaskStatusFromApi = (branch: string) => {
-  return graphqlClient.query({
+  return graphqlClient.query<Tasks_Branch_Status_CountQuery>({
     query: TASKS_BRANCH_STATUS_COUNT,
     variables: { branch },
   });
