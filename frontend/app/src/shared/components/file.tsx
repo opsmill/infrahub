ce78de993d3a11a90535cadaab6cf46c9e57e6c1
pa-lem <pa-lem@hotmail.fr>
2025-01-22 14:00:25 +0100

    update file component to use different editors

--- frontend/app/src/shared/components/file.tsx
+++ frontend/app/src/shared/components/file.tsx
@@ -4,16 +4,19 @@ import LoadingScreen from "@/shared/components/loading-screen";
 import { ALERT_TYPES, Alert } from "@/shared/components/ui/alert";
 import { useCallback, useEffect, useState } from "react";
 import { toast } from "react-toastify";
-import { CodeEditor } from "./editor/code-editor";
+import { Download } from "./buttons/download";
+import { Svg } from "./display/svg";
+import { JsonEditor } from "./editor/json/json-editor";
+import { MarkdownEditor } from "./editor/markdown";
+import { TextEditor } from "./editor/text/text-editor";
+import { YamlEditor } from "./editor/yaml/yaml-editor";
 
 type tFile = {
   url: string;
-  enableCopy?: boolean;
+  contentType?: string;
 };
 
-export const File = (props: tFile) => {
-  const { url, enableCopy } = props;
-
+export const File = ({ url, contentType }: tFile) => {
   const [isLoading, setIsLoading] = useState(false);
   const [fileContent, setFileContent] = useState("");
 
@@ -46,9 +49,76 @@ export const File = (props: tFile) => {
     return <NoDataFound message="No file found." />;
   }
 
+  if (contentType === "application/json") {
+    return (
+      <div className="p-4">
+        <JsonEditor value={fileContent} disabled onChange={function (): void {}} />
+
+        <Download
+          value={JSON.parse(fileContent)}
+          download={"file.json"}
+          className="absolute right-6 top-6"
+          variant={"outline"}
+        />
+      </div>
+    );
+  }
+
+  if (contentType === "application/markdown") {
+    return (
+      <div className="p-4">
+        <MarkdownEditor value={fileContent} />
+
+        <Download
+          value={fileContent}
+          download={"markdown.md"}
+          className="absolute right-6 top-6"
+          variant={"outline"}
+        />
+      </div>
+    );
+  }
+
+  if (contentType === "application/yaml") {
+    return (
+      <div className="p-4 relative">
+        <YamlEditor value={fileContent} disabled onChange={function (): void {}} />
+
+        <Download
+          value={fileContent}
+          download={"file.yaml"}
+          className="absolute right-6 top-6"
+          variant={"outline"}
+        />
+      </div>
+    );
+  }
+
+  if (contentType === "image/svg+xml") {
+    return (
+      <div className="p-4 relative">
+        <Svg value={fileContent} className="border rounded-md p-2" />
+
+        <Download
+          value={fileContent}
+          download={"file.svg"}
+          className="absolute right-6 top-6"
+          variant={"outline"}
+        />
+      </div>
+    );
+  }
+
   return (
-    <div className="p-4">
-      <CodeEditor value={fileContent} disabled enableCopy={enableCopy} />
+    <div className="p-4 relative">
+      <TextEditor value={fileContent} disabled onChange={function (): void {}} />
+
+      <Download
+        value={fileContent}
+        download={"file.txt"}
+        className="absolute right-6 top-6"
+        variant={"outline"}
+      />
     </div>
   );
 };
