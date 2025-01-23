19be9dab85bb529f5a159fb22a94d9b286cc4c96
pa-lem <pa-lem@hotmail.fr>
2025-01-23 12:40:56 +0100

    update artifact details view

--- frontend/app/src/entities/artifacts/ui/artifact-details.tsx
+++ frontend/app/src/entities/artifacts/ui/artifact-details.tsx
@@ -1,6 +1,5 @@
 import { CONFIG } from "@/config/config";
 import { ARTIFACT_OBJECT, MENU_EXCLUDELIST } from "@/config/constants";
-import { Generate } from "@/entities/artifacts/ui/generate";
 import { getObjectDetailsPaginated } from "@/entities/nodes/api/getObjectDetails";
 import {
   getSchemaObjectColumns,
@@ -11,23 +10,23 @@ import { getPermission } from "@/entities/permission/utils";
 import { genericsState, schemaState } from "@/entities/schema/stores/schema.atom";
 import useQuery from "@/shared/api/graphql/useQuery";
 import { constructPath } from "@/shared/api/rest/fetch";
-import { CopyToClipboard } from "@/shared/components/buttons/copy-to-clipboard";
 import ErrorScreen from "@/shared/components/errors/error-screen";
 import NoDataFound from "@/shared/components/errors/no-data-found";
 import UnauthorizedScreen from "@/shared/components/errors/unauthorized-screen";
 import { File } from "@/shared/components/file";
+import Content from "@/shared/components/layout/content";
 import LoadingScreen from "@/shared/components/loading-screen";
 import { PropertyList } from "@/shared/components/table/property-list";
-import { Badge } from "@/shared/components/ui/badge";
 import { Link } from "@/shared/components/ui/link";
 import { useTitle } from "@/shared/hooks/useTitle";
 import { gql } from "@apollo/client";
 import { useAtom } from "jotai";
-import { Navigate } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import { ARTIFACT_RELATIONSHIPS_BLACKLIST } from "../constants";
+import ArtifactHeader from "./artifact-header";
 
-export default function ArtifactsDetails({ artifactId }: { artifactId: string }) {
-  const objectid = artifactId;
+function ArtifactsDetails() {
+  const { objectid } = useParams();
 
   const [schemaList] = useAtom(schemaState);
   const [genericList] = useAtom(genericsState);
@@ -97,66 +96,46 @@ export default function ArtifactsDetails({ artifactId }: { artifactId: string })
   const fileUrl = CONFIG.ARTIFACTS_CONTENT_URL(objectDetailsData?.storage_id?.value);
   const contentType = objectDetailsData?.content_type?.value;
 
-  const properies = [
-    {
-      name: "Status",
-      value: <Badge>{objectDetailsData?.status?.value}</Badge>,
-    },
-    ...(schema?.relationships ?? [])
-      .filter(({ name }) => !ARTIFACT_RELATIONSHIPS_BLACKLIST.includes(name))
-      .map((schemaRelationship) => {
-        const relationshipData = objectDetailsData[schemaRelationship.name]?.node;
+  const properies = (schema?.relationships ?? [])
+    .filter(({ name }) => !ARTIFACT_RELATIONSHIPS_BLACKLIST.includes(name))
+    .map((schemaRelationship) => {
+      const relationshipData = objectDetailsData[schemaRelationship.name]?.node;
 
-        return {
-          name: schemaRelationship.label || schemaRelationship.name,
-          value: relationshipData && (
-            <Link
-              to={constructPath(
-                getObjectDetailsUrl(relationshipData.id, relationshipData.__typename)
-              )}
-            >
-              {relationshipData?.display_label}
-            </Link>
-          ),
-        };
-      }),
-  ];
+      return {
+        name: schemaRelationship.label || schemaRelationship.name,
+        value: relationshipData && (
+          <Link
+            to={constructPath(
+              getObjectDetailsUrl(relationshipData.id, relationshipData.__typename)
+            )}
+          >
+            {relationshipData?.display_label}
+          </Link>
+        ),
+      };
+    });
 
   return (
-    <div className="flex flex-col gap-4 p-4">
+    <Content.Card className="p-4">
+      <ArtifactHeader
+        name={objectDetailsData?.display_label}
+        status={objectDetailsData?.status?.value}
+        id={objectid}
+        hfid={objectDetailsData?.hfid && JSON.stringify(objectDetailsData?.hfid)}
+        checksum={objectDetailsData?.checksum?.value}
+        storageId={objectDetailsData?.storage_id?.value}
+        definitionId={objectDetailsData?.definition?.node?.id}
+      />
+
       <div className="flex flex-col gap-4">
-        <div className="flex items-center gap-4">
-          <Generate
-            label="Re-generate"
-            artifactid={objectid}
-            definitionid={objectDetailsData?.definition?.node?.id}
-          />
-
-          {objectDetailsData?.checksum?.value && (
-            <CopyToClipboard
-              text={objectDetailsData?.checksum?.value}
-              size={"default"}
-              variant={"outline"}
-            >
-              Checksum
-            </CopyToClipboard>
-          )}
-
-          {objectDetailsData?.storage_id?.value && (
-            <CopyToClipboard
-              text={objectDetailsData?.storage_id?.value}
-              size={"default"}
-              variant={"outline"}
-            >
-              Storage ID
-            </CopyToClipboard>
-          )}
-        </div>
-
         <PropertyList properties={properies} />
-      </div>
 
-      <File url={fileUrl} contentType={contentType} />
-    </div>
+        <File url={fileUrl} contentType={contentType} />
+      </div>
+    </Content.Card>
   );
 }
+
+export const Component = () => {
+  return <ArtifactsDetails />;
+};
