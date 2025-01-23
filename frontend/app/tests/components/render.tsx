3fd9ed7caebceb8898035b029a47412622921bf4
pa-lem <pa-lem@hotmail.fr>
2025-01-23 18:18:34 +0100

    fix query param provider

--- frontend/app/tests/components/render.tsx
+++ frontend/app/tests/components/render.tsx
@@ -4,10 +4,8 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Slide, ToastContainer } from "react-toastify";
 import { render as renderFromVitest } from "vitest-browser-react";
-
 import { queryClient } from "../../src/shared/api/rest/client";
 import { store } from "../../src/shared/stores";
-
 import "/src/app/styles/index.css";
 import "react-toastify/dist/ReactToastify.css";
 import queryString from "query-string";
@@ -19,23 +17,25 @@ export const render = (component: React.ReactElement, options = {}) =>
     wrapper: ({ children }) => (
       <Provider store={store}>
         <QueryClientProvider client={queryClient}>
-          <QueryParamProvider
-            adapter={ReactRouter6Adapter}
-            options={{
-              searchStringToObject: queryString.parse,
-              objectToSearchString: queryString.stringify,
-            }}
-          >
-            <ToastContainer
-              hideProgressBar={true}
-              transition={Slide}
-              autoClose={5000}
-              closeOnClick={false}
-              newestOnTop
-              position="bottom-right"
-            />
-            <BrowserRouter> {children}</BrowserRouter>
-          </QueryParamProvider>
+          <ToastContainer
+            hideProgressBar={true}
+            transition={Slide}
+            autoClose={5000}
+            closeOnClick={false}
+            newestOnTop
+            position="bottom-right"
+          />
+          <BrowserRouter>
+            <QueryParamProvider
+              adapter={ReactRouter6Adapter}
+              options={{
+                searchStringToObject: queryString.parse,
+                objectToSearchString: queryString.stringify,
+              }}
+            >
+              {children}
+            </QueryParamProvider>
+          </BrowserRouter>
         </QueryClientProvider>
       </Provider>
     ),
