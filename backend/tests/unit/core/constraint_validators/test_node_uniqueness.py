1c03e52496de11390f1471908f76e85d128650b1
Guillaume Mazoyer <guillaume@opsmill.com>
2025-01-24 16:42:40 +0100

    Use existing fixtures for tests

--- backend/tests/unit/core/constraint_validators/test_node_uniqueness.py
+++ backend/tests/unit/core/constraint_validators/test_node_uniqueness.py
@@ -34,85 +34,40 @@ async def test_node_validate_constraint_node_uniqueness_success(
     await constraint.check(alfred)
 
 
-async def test_hierarchical_uniqueness_constraint(db, default_branch):
-    schema = {
-        "generics": [
-            {
-                "attributes": [{"kind": "Text", "name": "name"}],
-                "description": "Generic hierarchical location",
-                "hierarchical": True,
-                "human_friendly_id": ["name__value"],
-                "label": "Location",
-                "name": "Generic",
-                "namespace": "Location",
-            }
-        ],
-        "nodes": [
-            {
-                "children": "LocationCountry",
-                "description": "A continent on planet earth",
-                "display_labels": ["name__value"],
-                "generate_profile": False,
-                "human_friendly_id": ["name__value"],
-                "inherit_from": ["LocationGeneric"],
-                "name": "Continent",
-                "namespace": "Location",
-                "order_by": ["name__value"],
-                "parent": "",
-                "uniqueness_constraints": [["name__value"]],
-            },
-            {
-                "children": "LocationSite",
-                "description": "A country within a continent",
-                "display_labels": ["name__value"],
-                "generate_profile": False,
-                "human_friendly_id": ["parent__name__value", "name__value"],
-                "inherit_from": ["LocationGeneric"],
-                "name": "Country",
-                "namespace": "Location",
-                "order_by": ["name__value"],
-                "parent": "LocationContinent",
-                "uniqueness_constraints": [["parent", "name__value"]],
-            },
-            {
-                "attributes": [{"kind": "Text", "name": "city", "optional": True}],
-                "children": "",
-                "description": "A site within a country",
-                "display_labels": ["name__value"],
-                "human_friendly_id": ["parent__name__value", "name__value"],
-                "inherit_from": ["LocationGeneric"],
-                "name": "Site",
-                "namespace": "Location",
-                "order_by": ["name__value"],
-                "parent": "LocationCountry",
-                "uniqueness_constraints": [["parent", "name__value"]],
-            },
-        ],
-    }
+async def test_hierarchical_uniqueness_constraint(
+    db: InfrahubDatabase, default_branch: Branch, hierarchical_location_schema_simple_unregistered: SchemaRoot
+):
+    site_schema = hierarchical_location_schema_simple_unregistered.get(name="LocationSite")
+    site_schema.human_friendly_id = ["parent__name__value", "name__value"]
+    site_schema.uniqueness_constraints = [["parent", "name__value"]]
 
-    registry.schema.register_schema(schema=SchemaRoot(**schema), branch=default_branch.name)
+    rack_schema = hierarchical_location_schema_simple_unregistered.get(name="LocationRack")
+    rack_schema.human_friendly_id = ["parent__name__value", "name__value"]
+    rack_schema.uniqueness_constraints = [["parent", "name__value"]]
+
+    registry.schema.register_schema(schema=hierarchical_location_schema_simple_unregistered, branch=default_branch.name)
     constraint = NodeGroupedUniquenessConstraint(db=db, branch=default_branch)
 
-    eu = await Node.init(db=db, schema="LocationContinent", branch=default_branch)
+    eu = await Node.init(db=db, schema="LocationRegion", branch=default_branch)
     await eu.new(db=db, name="Europe")
     await eu.save(db=db)
-    fr = await Node.init(db=db, schema="LocationCountry", branch=default_branch)
+    fr = await Node.init(db=db, schema="LocationSite", branch=default_branch)
     await fr.new(db=db, name="France", parent=eu)
     await fr.save(db=db)
-    uk = await Node.init(db=db, schema="LocationCountry", branch=default_branch)
+    uk = await Node.init(db=db, schema="LocationSite", branch=default_branch)
     await uk.new(db=db, name="United Kingdom", parent=eu)
     await fr.save(db=db)
 
-    th2 = await Node.init(db=db, schema="LocationSite", branch=default_branch)
-    await th2.new(db=db, name="Telehouse 2", city="Paris", parent=fr)
+    th2 = await Node.init(db=db, schema="LocationRack", branch=default_branch)
+    await th2.new(db=db, name="th2-par", parent=fr)
     await th2.save(db=db)
 
-    ld6 = await Node.init(db=db, schema="LocationSite", branch=default_branch)
-    await ld6.new(db=db, name="Equinix LD6", city="London", parent=uk)
+    ld6 = await Node.init(db=db, schema="LocationRack", branch=default_branch)
+    await ld6.new(db=db, name="ld6-ldn", parent=uk)
     await ld6.save(db=db)
     await constraint.check(ld6)
 
-    ld6 = await Node.init(db=db, schema="LocationSite", branch=default_branch)
-    await ld6.new(db=db, name="Equinix LD6", city="London", parent=uk)
+    ld6 = await Node.init(db=db, schema="LocationRack", branch=default_branch)
+    await ld6.new(db=db, name="ld6-ldn", parent=uk)
     with pytest.raises(ValidationError, match=r"Violates uniqueness constraint 'parent-name' at parent"):
         await constraint.check(ld6)
