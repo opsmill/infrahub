7b3a77f4d587aa720dbc8059474cbdf70a1a5506
pa-lem <pa-lem@hotmail.fr>
2025-01-24 15:42:19 +0100

    update themes

--- frontend/app/src/shared/components/editor/code/code-viewer.tsx
+++ frontend/app/src/shared/components/editor/code/code-viewer.tsx
@@ -1,10 +1,13 @@
 import { classNames } from "@/shared/utils/common";
-import SyntaxHighlighter, { SyntaxHighlighterProps } from "react-syntax-highlighter";
+import { Light as SyntaxHighlighter, SyntaxHighlighterProps } from "react-syntax-highlighter";
+import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 export const CodeViewer = ({ className, children, ...props }: SyntaxHighlighterProps) => {
   return (
-    <div className={classNames("rounded-md", className)}>
-      <SyntaxHighlighter {...props}>{children}</SyntaxHighlighter>
+    <div className={classNames("rounded-md overflow-hidden text-sm", className)}>
+      <SyntaxHighlighter {...props} showLineNumbers style={atomOneDark}>
+        {children}
+      </SyntaxHighlighter>
     </div>
   );
 };
