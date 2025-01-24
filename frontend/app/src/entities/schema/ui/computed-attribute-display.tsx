678c49e483432e0028f9e7ba62c80b8c48a8748b
Paul Leménager <pa-lem@hotmail.fr>
2025-01-24 15:15:27 +0100

    Display the computed attribute in the schema visualizer (#5534)
    * update modal
    
    * add badge
    
    * add variant
    
    * add component for computed attribute
    
    * update schema visualizer
    
    * handle transform python
    
    * add fragment
    
    * add test
    
    * add test id
    
    * update props
    
    * update icon
    
    * use model display for transforms
    
    * fix scroll
    
    * update test
    
    * update render hook with query param provider
    
    * fix query param provider

---
+++ frontend/app/src/entities/schema/ui/computed-attribute-display.tsx
@@ -0,0 +1,75 @@
+import { components } from "@/shared/api/rest/types.generated";
+import { Button, LinkButton } from "@/shared/components/buttons/button-primitive";
+import { CodeEditor } from "@/shared/components/editor/code-editor";
+import Modal, { ModalTitle } from "@/shared/components/modals/modal";
+import { Badge } from "@/shared/components/ui/badge";
+import { Tooltip } from "@/shared/components/ui/tooltip";
+import { Icon } from "@iconify-icon/react";
+import { useState } from "react";
+import { ModelDisplay } from "./styled";
+
+export const ComputedAttributeDisplay = ({
+  computedAttribute,
+}: {
+  computedAttribute?: components["schemas"]["ComputedAttribute-Output"] | null;
+}) => {
+  const [isOpen, setOpen] = useState(false);
+
+  if (!computedAttribute) {
+    return "-";
+  }
+
+  if (computedAttribute.kind === "Jinja2") {
+    const fileData = JSON.stringify(computedAttribute.jinja2_template);
+    const blob = new Blob([fileData], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+
+    return (
+      <div className="flex items-center gap-2">
+        <ModelDisplay kinds={["CoreTransformJinja2"]} />
+
+        <Button
+          variant={"active-outline"}
+          size={"icon"}
+          onClick={() => setOpen(true)}
+          data-testid="jinja2-transform-button"
+        >
+          <Icon icon={"mdi:eye-outline"} />
+        </Button>
+
+        <Modal setOpen={setOpen} open={isOpen}>
+          <div className="flex items-center gap-2 mb-2">
+            <ModalTitle>Jinja2 Template</ModalTitle>
+
+            <Tooltip enabled content="Download template">
+              <LinkButton
+                variant={"ghost"}
+                size={"icon"}
+                to={url}
+                target="_blank"
+                rel="noopener noreferrer"
+                download={"jinja2-template.txt"}
+              >
+                <Icon icon={"mdi:download-outline"} />
+              </LinkButton>
+            </Tooltip>
+          </div>
+
+          <CodeEditor value={computedAttribute.jinja2_template} disabled />
+        </Modal>
+      </div>
+    );
+  }
+
+  if (computedAttribute.kind === "TransformPython") {
+    return (
+      <div className="flex items-center gap-2">
+        <ModelDisplay kinds={["CoreTransformPython"]} />
+
+        <Badge variant={"gray-outline"}>{computedAttribute.transform}</Badge>
+      </div>
+    );
+  }
+
+  return null;
+};
--- frontend/app/tests/components/render.tsx
+++ frontend/app/tests/components/render.tsx
@@ -4,12 +4,13 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Slide, ToastContainer } from "react-toastify";
 import { render as renderFromVitest } from "vitest-browser-react";
-
 import { queryClient } from "../../src/shared/api/rest/client";
 import { store } from "../../src/shared/stores";
-
 import "/src/app/styles/index.css";
 import "react-toastify/dist/ReactToastify.css";
+import queryString from "query-string";
+import { QueryParamProvider } from "use-query-params";
+import { ReactRouter6Adapter } from "use-query-params/adapters/react-router-6";
 
 export const render = (component: React.ReactElement, options = {}) =>
   renderFromVitest(component, {
@@ -24,7 +25,17 @@ export const render = (component: React.ReactElement, options = {}) =>
             newestOnTop
             position="bottom-right"
           />
-          <BrowserRouter> {children}</BrowserRouter>
+          <BrowserRouter>
+            <QueryParamProvider
+              adapter={ReactRouter6Adapter}
+              options={{
+                searchStringToObject: queryString.parse,
+                objectToSearchString: queryString.stringify,
+              }}
+            >
+              {children}
+            </QueryParamProvider>
+          </BrowserRouter>
         </QueryClientProvider>
       </Provider>
     ),
