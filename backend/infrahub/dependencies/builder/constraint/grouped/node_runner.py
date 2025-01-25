738a42945bd51e884972901715c73898b100731c
Fatih Acar <fatih@opsmill.com>
2025-01-26 00:52:46 +0100

    fix(backend): do not use old uniqueness constraint filter
    It adds a significant overhead since it uses NodeGetListQuery.
    
    Signed-off-by: Fatih Acar <fatih@opsmill.com>

--- backend/infrahub/dependencies/builder/constraint/grouped/node_runner.py
+++ backend/infrahub/dependencies/builder/constraint/grouped/node_runner.py
@@ -2,7 +2,6 @@ from infrahub.core.constraint.node.runner import NodeConstraintRunner
 from infrahub.dependencies.interface import DependencyBuilder, DependencyBuilderContext
 
 from ..node.grouped_uniqueness import NodeGroupedUniquenessConstraintDependency
-from ..node.uniqueness import NodeAttributeUniquenessConstraintDependency
 from ..relationship_manager.count import RelationshipCountConstraintDependency
 from ..relationship_manager.peer_kind import RelationshipPeerKindConstraintDependency
 from ..relationship_manager.profiles_kind import RelationshipProfilesKindConstraintDependency
@@ -15,7 +14,6 @@ class NodeConstraintRunnerDependency(DependencyBuilder[NodeConstraintRunner]):
             db=context.db,
             branch=context.branch,
             node_constraints=[
-                NodeAttributeUniquenessConstraintDependency.build(context=context),
                 NodeGroupedUniquenessConstraintDependency.build(context=context),
             ],
             relationship_manager_constraints=[
