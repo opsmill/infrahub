0c3d8a90b84c4fb87ca0404bf5533e631b65be91
bilalabbad <bilal@opsmill.com>
2025-01-30 15:03:18 +0100

    remove old combobox from number pool

--- frontend/app/src/shared/components/form/pool-selector.tsx
+++ frontend/app/src/shared/components/form/pool-selector.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/shared/components/buttons/button-primitive";
 import { FormFieldValue, NumberPoolData } from "@/shared/components/form/type";
-import { ComboboxList, tComboboxItem } from "@/shared/components/ui/combobox-legacy";
+import { ComboboxContent, ComboboxItem, ComboboxList } from "@/shared/components/ui/combobox";
 import { Popover, PopoverAnchor, PopoverTrigger } from "@/shared/components/ui/popover";
 import { Tooltip } from "@/shared/components/ui/tooltip";
 import { Combobox as ComboboxPrimitive } from "@headlessui/react";
@@ -19,16 +19,15 @@ export type PoolValue = {
 type PoolSelectorProps = {
   children: React.ReactNode;
   onChange: (value: PoolValue) => void;
-  onReset: () => void;
   pools: Array<NumberPoolData>;
   value: FormFieldValue;
 };
 
 export const PoolSelector = forwardRef<HTMLElement, PoolSelectorProps>(
-  ({ children, onChange, onReset, value, pools }, ref) => {
+  ({ children, onChange, value, pools }, ref) => {
     const [override, setOverride] = React.useState(false);
 
-    const items: Array<tComboboxItem> = pools.map((pool) => ({
+    const items = pools.map((pool) => ({
       label: pool.label,
       value: {
         from_pool: {
@@ -75,9 +74,22 @@ export const PoolSelector = forwardRef<HTMLElement, PoolSelectorProps>(
           </Tooltip>
         </div>
 
-        <ComboboxPrimitive onChange={onChange}>
-          <ComboboxList items={items} onReset={onReset} />
-        </ComboboxPrimitive>
+        <ComboboxContent>
+          <ComboboxPrimitive onChange={onChange}>
+            <ComboboxList>
+              {items.map((item) => (
+                <ComboboxItem
+                  key={item.value.from_pool.id}
+                  value={item.label}
+                  onSelect={() => onChange(item.value)}
+                  selectedValue={value?.from_pool?.id}
+                >
+                  {item.label}
+                </ComboboxItem>
+              ))}
+            </ComboboxList>
+          </ComboboxPrimitive>
+        </ComboboxContent>
       </Popover>
     );
   }
