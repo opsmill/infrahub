fb0bdc2e11a6801441bf1d76508918b5b489d988
Aaron McCarty <ajtmccarty@pm.me>
2025-02-04 07:47:14 -0800

    add test and changes for a relationship identifier migration (#5652)
    * add test and changes for a relationship identifier migration
    
    * mypy fix
    
    * fix handling to be unique on name AND identifier
    
    * look before you leap
    
    * use PathType.from_relationship
    
    * reset python_sdk commit

--- backend/infrahub/core/diff/enricher/path_identifier.py
+++ backend/infrahub/core/diff/enricher/path_identifier.py
@@ -1,4 +1,4 @@
-from infrahub.core.constants import PathType, RelationshipCardinality
+from infrahub.core.constants import PathType
 from infrahub.core.path import DataPath
 from infrahub.database import InfrahubDatabase
 
@@ -44,14 +44,8 @@ class DiffPathIdentifierEnricher(DiffEnricherInterface):
                     attribute_property.path_identifier = property_path.get_path()
             if not node.relationships:
                 continue
-            node_schema = self.db.schema.get(name=node.kind, branch=self.diff_branch_name, duplicate=False)
             for relationship in node.relationships:
-                relationship_schema = node_schema.get_relationship(name=relationship.name)
-                path_type = (
-                    PathType.RELATIONSHIP_ONE
-                    if relationship_schema.cardinality is RelationshipCardinality.ONE
-                    else PathType.RELATIONSHIP_MANY
-                )
+                path_type = PathType.from_relationship(relationship.cardinality)
                 relationship_path = DataPath(
                     branch=enriched_diff_root.diff_branch_name,
                     path_type=path_type,
