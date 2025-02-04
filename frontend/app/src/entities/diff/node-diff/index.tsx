e086d3471f2d59b9ad4e7bd1d09e9508970793ce
bilalabbad <bilal@opsmill.com>
2025-02-04 16:35:56 +0100

    rebase query

--- frontend/app/src/entities/diff/node-diff/index.tsx
+++ frontend/app/src/entities/diff/node-diff/index.tsx
@@ -1,28 +1,22 @@
 import { PROPOSED_CHANGES_OBJECT_THREAD_OBJECT } from "@/config/constants";
 import { QSP } from "@/config/qsp";
-import { BRANCH_REBASE } from "@/entities/branches/api/rebaseBranch";
-import { useUpdateDiffMutation } from "@/entities/diff/domain/update-diff.mutation";
 import { DIFF_STATUS, DiffNode as DiffNodeType } from "@/entities/diff/node-diff/types";
 import { DiffComputing } from "@/entities/diff/ui/diff-computing";
 import { DiffEmpty } from "@/entities/diff/ui/diff-empty";
 import { DiffNoFound } from "@/entities/diff/ui/diff-no-found";
+import { DiffRebaseButton } from "@/entities/diff/ui/diff-rebase-button";
 import { DiffRefreshButton } from "@/entities/diff/ui/diff-refresh-button";
 import DiffTree from "@/entities/diff/ui/diff-tree";
 import { getProposedChangesDiffTree } from "@/entities/proposed-changes/api/getProposedChangesDiffTree";
 import { proposedChangedState } from "@/entities/proposed-changes/stores/proposedChanges.atom";
 import { schemaState } from "@/entities/schema/stores/schema.atom";
-import graphqlClient from "@/shared/api/graphql/graphqlClientApollo";
 import useQuery from "@/shared/api/graphql/useQuery";
-import { Button } from "@/shared/components/buttons/button-primitive";
 import { DateDisplay } from "@/shared/components/display/date-display";
 import ErrorScreen from "@/shared/components/errors/error-screen";
 import LoadingScreen from "@/shared/components/loading-screen";
-import { ALERT_TYPES, Alert } from "@/shared/components/ui/alert";
-import { datetimeAtom } from "@/shared/stores/time.atom";
 import { NetworkStatus } from "@apollo/client";
 import { useAtomValue } from "jotai";
-import { createContext, useState } from "react";
-import { toast } from "react-toastify";
+import { createContext } from "react";
 import { StringParam, useQueryParam } from "use-query-params";
 import { DiffFilter, ProposedChangeDiffFilter } from "../../proposed-changes/ui/diff-filter";
 import { DiffNode } from "./node";
@@ -52,14 +46,10 @@ const buildFilters = (filters: DiffFilter, qsp?: string | null) => {
 
 export const NodeDiff = ({ branchName, filters }: NodeDiffProps) => {
   const [qspStatus] = useQueryParam(QSP.STATUS, StringParam);
-  const date = useAtomValue(datetimeAtom);
   const proposedChangesDetails = useAtomValue(proposedChangedState);
   const nodeSchemas = useAtomValue(schemaState);
-  const [isLoadingUpdate, setIsLoadingUpdate] = useState(false);
 
   const branch = proposedChangesDetails?.source_branch?.value || branchName; // Used in proposed changes view and branch view
-
-  const updateDiffMutation = useUpdateDiffMutation();
 
   const schemaData = nodeSchemas.find((s) => s.kind === PROPOSED_CHANGES_OBJECT_THREAD_OBJECT);
 
@@ -77,47 +67,6 @@ export const NodeDiff = ({ branchName, filters }: NodeDiffProps) => {
   if (error) {
     return <ErrorScreen message={error?.message} className="max-w-lg m-auto" />;
   }
-
-  const handleRefresh = async () => {
-    setIsLoadingUpdate(true);
-    try {
-      await updateDiffMutation.mutateAsync(branch);
-
-      await graphqlClient.refetchQueries({
-        include: ["GET_PROPOSED_CHANGES_DIFF_TREE", "GET_PROPOSED_CHANGES_DIFF_SUMMARY"],
-      });
-
-      toast(<Alert type={ALERT_TYPES.SUCCESS} message="Diff updated!" />);
-    } catch (error: any) {
-      toast(<Alert type={ALERT_TYPES.SUCCESS} message={error?.message} />);
-    }
-    setIsLoadingUpdate(false);
-  };
-
-  const handleRebase = async () => {
-    setIsLoadingUpdate(true);
-
-    try {
-      await graphqlClient.mutate({
-        mutation: BRANCH_REBASE,
-        variables: {
-          name: branch,
-        },
-        context: {
-          branch: branchName,
-          date,
-        },
-      });
-
-      toast(<Alert type={ALERT_TYPES.SUCCESS} message="Branch rebased!" />);
-
-      await handleRefresh();
-    } catch (error: any) {
-      toast(<Alert type={ALERT_TYPES.ERROR} message={error?.message} />);
-    }
-
-    setIsLoadingUpdate(false);
-  };
 
   const diffTreeData = (data || previousData)?.DiffTree;
 
@@ -146,38 +95,21 @@ export const NodeDiff = ({ branchName, filters }: NodeDiffProps) => {
 
   return (
     <div className="h-full overflow-hidden flex flex-col">
-      <div className="flex items-center p-2 bg-custom-white border-b">
+      <header className="flex items-center px-4 py-2 border-b gap-2">
         <ProposedChangeDiffFilter branch={branch} filters={filters} />
-
-        <div className="flex flex-1 items-center gap-2 justify-end pr-2">
-          {isLoadingUpdate && <LoadingScreen size={22} hideText />}
-
-          <div className="flex items-center">
-            <div className="flex items-center text-xs mr-2">
-              <span className="mr-1">Updated</span>
-              <DateDisplay date={diffTreeData?.to_time} />
-            </div>
-
-            <DiffRefreshButton size="sm" variant="primary" branchName={branch} />
-          </div>
-
-          <Button
-            size="sm"
-            variant="primary-outline"
-            onClick={handleRebase}
-            disabled={isLoadingUpdate}
-          >
-            Rebase
-          </Button>
-        </div>
-      </div>
+        <span className="text-xs inline-flex gap-1 ml-auto">
+          Updated <DateDisplay date={diffTreeData?.to_time} />
+        </span>
+        <DiffRefreshButton size="sm" variant="primary" branchName={branch} />
+        <DiffRebaseButton branchName={branch} />
+      </header>
 
       <div className="flex-grow grid grid-cols-4 overflow-hidden">
-        <div className="p-4 col-span-1 overflow-auto border-r">
+        <nav className="p-4 col-span-1 overflow-auto border-r">
           <DiffTree nodes={nodes} className="w-full" />
-        </div>
+        </nav>
 
-        <div className="space-y-4 p-4 col-start-2 col-end-5 overflow-auto bg-stone-100">
+        <main className="space-y-4 p-4 col-start-2 col-end-5 overflow-auto bg-stone-100">
           {nodes.length ? (
             nodes
               .filter(({ status }) => status !== "UNCHANGED")
@@ -192,7 +124,7 @@ export const NodeDiff = ({ branchName, filters }: NodeDiffProps) => {
           ) : (
             <DiffNoFound diffStatus={qspStatus as string} />
           )}
-        </div>
+        </main>
       </div>
     </div>
   );
--- frontend/app/src/entities/diff/ui/diff-refresh-button.tsx
+++ frontend/app/src/entities/diff/ui/diff-refresh-button.tsx
@@ -1,7 +1,11 @@
-import { useUpdateDiffMutation } from "@/entities/diff/domain/update-diff.mutation";
+import {
+  UPDATE_DIFF_KEY,
+  useUpdateDiffMutation,
+} from "@/entities/diff/domain/update-diff.mutation";
 import { Button, ButtonProps } from "@/shared/components/buttons/button-primitive";
 import { classNames } from "@/shared/utils/common";
 import { Icon } from "@iconify-icon/react";
+import { useMutationState } from "@tanstack/react-query";
 
 export interface DiffRefreshButtonProps extends Omit<ButtonProps, "onClick"> {
   branchName: string;
@@ -9,6 +13,12 @@ export interface DiffRefreshButtonProps extends Omit<ButtonProps, "onClick"> {
 
 export function DiffRefreshButton({ branchName, ...props }: DiffRefreshButtonProps) {
   const updateDiffMutation = useUpdateDiffMutation();
+  const allUpdatingDiffs = useMutationState({
+    filters: { status: "pending", mutationKey: [UPDATE_DIFF_KEY] },
+    select: (mutation) => mutation.state.variables,
+  });
+
+  const isLoading = allUpdatingDiffs.includes(branchName);
 
   return (
     <Button
@@ -16,11 +26,8 @@ export function DiffRefreshButton({ branchName, ...props }: DiffRefreshButtonPro
       onClick={() => updateDiffMutation.mutate(branchName)}
       {...props}
     >
-      <Icon
-        icon="mdi:reload"
-        className={classNames("mr-1", updateDiffMutation.isPending && "animate-spin")}
-      />
-      {updateDiffMutation.isPending ? "Refreshing diff..." : "Refresh diff"}
+      <Icon icon="mdi:reload" className={classNames("mr-1", isLoading && "animate-spin")} />
+      {isLoading ? "Refreshing diff..." : "Refresh diff"}
     </Button>
   );
 }
