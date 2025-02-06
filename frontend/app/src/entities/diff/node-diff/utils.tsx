7fc5cd6093b64ec365213a55a321e56ac39ae4d8
pa-lem <pa-lem@hotmail.fr>
2025-02-06 11:06:20 +0100

    lint

--- frontend/app/src/entities/diff/node-diff/utils.tsx
+++ frontend/app/src/entities/diff/node-diff/utils.tsx
@@ -54,48 +54,53 @@ type DiffRowProps = {
   iconClassName?: string;
   status?: DiffStatus;
 };
-export const DiffRow = ({
-  className,
-  children,
+
+const DiffDisplay = ({
   hasConflicts,
   title,
+  status,
+  className,
   left,
   leftClassName,
   right,
   rightClassName,
-  iconClassName,
-  status,
 }: DiffRowProps) => {
-  if (!children) {
-    return (
-      <div className={classNames("min-h-9 relative", hasConflicts && "bg-yellow-50")}>
-        {hasConflicts && <div className="absolute top-0 bottom-0 left-0 w-0.5 bg-yellow-400" />}
+  return (
+    <div className={classNames("min-h-9 relative", hasConflicts && "bg-yellow-50")}>
+      {hasConflicts && <div className="absolute top-0 bottom-0 left-0 w-0.5 bg-yellow-400" />}
 
-        <div className={classNames("grid grid-cols-3 text-xs font-normal group pl-8", className)}>
-          {title}
+      <div className={classNames("grid grid-cols-3 text-xs font-normal group pl-8", className)}>
+        {title}
 
-          <div className="bg-custom-white">
-            <div className={classNames("bg-gray-50 p-2 flex items-center h-full", leftClassName)}>
-              {left}
-            </div>
+        <div className="bg-custom-white">
+          <div className={classNames("bg-gray-50 p-2 flex items-center h-full", leftClassName)}>
+            {left}
           </div>
+        </div>
 
-          <div className="bg-custom-white">
-            <div
-              className={classNames(
-                "p-2 flex items-center h-full font-medium",
-                status === "ADDED" && "bg-green-100 text-green-900",
-                status === "REMOVED" && "bg-red-100 text-red-900",
-                status === "UPDATED" && "bg-blue-100 text-blue-900",
-                rightClassName
-              )}
-            >
-              {right}
-            </div>
+        <div className="bg-custom-white">
+          <div
+            className={classNames(
+              "p-2 flex items-center h-full font-medium",
+              status === "ADDED" && "bg-green-100 text-green-900",
+              status === "REMOVED" && "bg-red-100 text-red-900",
+              status === "UPDATED" && "bg-blue-100 text-blue-900",
+              rightClassName
+            )}
+          >
+            {right}
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
+};
+
+export const DiffRow = ({ children, iconClassName, ...props }: DiffRowProps) => {
+  const { hasConflicts } = props;
+
+  if (!children) {
+    return <DiffDisplay {...props} />;
   }
 
   return (
@@ -106,31 +111,7 @@ export const DiffRow = ({
         defaultOpen={hasConflicts}
         iconClassName={classNames("absolute", iconClassName)}
         hideChevron={!children}
-        title={
-          <div className={classNames("grid grid-cols-3 text-xs font-normal group pl-8", className)}>
-            {title}
-
-            <div className="bg-custom-white">
-              <div className={classNames("bg-gray-50 p-2 flex items-center h-full", leftClassName)}>
-                {left}
-              </div>
-            </div>
-
-            <div className="bg-custom-white">
-              <div
-                className={classNames(
-                  "p-2 flex items-center h-full font-medium",
-                  status === "ADDED" && "bg-green-100 text-green-900",
-                  status === "REMOVED" && "bg-red-100 text-red-900",
-                  status === "UPDATED" && "bg-blue-100 text-blue-900",
-                  rightClassName
-                )}
-              >
-                {right}
-              </div>
-            </div>
-          </div>
-        }
+        title={<DiffDisplay {...props} />}
       >
         {children}
       </Accordion>
