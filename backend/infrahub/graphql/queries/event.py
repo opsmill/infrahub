1c472f882c0a039df655888ffefad059985f4d51
Patrick Ogenstad <patrick@ogenstad.com>
2025-02-07 09:30:55 +0100

    Add pagination support for InfrahubEvent through InfrahubEventNext

--- backend/infrahub/graphql/queries/event.py
+++ backend/infrahub/graphql/queries/event.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING, Any
 from graphene import Field, Int, List, NonNull, ObjectType, String
 from infrahub_sdk.utils import extract_fields_first_node
 
+from infrahub.exceptions import ValidationError
 from infrahub.graphql.types.event import EventNodes
 from infrahub.task_manager.event import PrefectEvent
 
@@ -15,13 +16,13 @@ if TYPE_CHECKING:
 class Events(ObjectType):
     edges = List(NonNull(EventNodes), required=True)
     count = Int(required=True)
+    next_token = String(required=False)
 
     @staticmethod
     async def resolve(
         root: dict,  # noqa: ARG004
         info: GraphQLResolveInfo,
         limit: int = 10,
-        offset: int = 0,
         account: str | None = None,
         ids: list[str] | None = None,
         branch: str | None = None,
@@ -29,16 +30,33 @@ class Events(ObjectType):
         related_node__ids: list[str] | None = None,
     ) -> dict[str, Any]:
         ids = ids or []
+        if limit > 50:
+            # Prefect restricts this to 50
+            raise ValidationError(input_value="The parameter 'limit' can't be above 50")
         return await Events.query(
             info=info,
             branch=branch,
             account=account,
             limit=limit,
-            offset=offset,
             related_node__ids=related_node__ids,
             q=q,
             ids=ids,
         )
+
+    @staticmethod
+    async def resolve_next(
+        root: dict,  # noqa: ARG004
+        info: GraphQLResolveInfo,
+        next_token: str,
+    ) -> dict[str, Any]:
+        fields = await extract_fields_first_node(info)
+
+        prefect_tasks = await PrefectEvent.query_next(fields=fields, next_token=next_token)
+        return {
+            "count": prefect_tasks.get("count", 0),
+            "next_token": prefect_tasks.get("next_token"),
+            "edges": prefect_tasks.get("edges", []),
+        }
 
     @classmethod
     async def query(
@@ -50,7 +68,6 @@ class Events(ObjectType):
         branch: str | None = None,
         account: str | None = None,
         limit: int | None = None,
-        offset: int | None = None,
     ) -> dict[str, Any]:
         fields = await extract_fields_first_node(info)
 
@@ -62,10 +79,10 @@ class Events(ObjectType):
             branch=branch,
             account=account,
             limit=limit,
-            offset=offset,
         )
         return {
             "count": prefect_tasks.get("count", 0),
+            "next_token": prefect_tasks.get("next_token"),
             "edges": prefect_tasks.get("edges", []),
         }
 
@@ -73,12 +90,18 @@ class Events(ObjectType):
 Event = Field(
     Events,
     limit=Int(required=False),
-    offset=Int(required=False),
     related_node__ids=List(String),
     branch=String(required=False),
     account=String(required=False),
     ids=List(String),
     q=String(required=False),
     resolver=Events.resolve,
+    required=True,
+)
+
+EventNext = Field(
+    Events,
+    next_token=String(required=True),
+    resolver=Events.resolve_next,
     required=True,
 )
