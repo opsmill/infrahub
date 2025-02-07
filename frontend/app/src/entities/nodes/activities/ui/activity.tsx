24f4af08c39cdd9b508fc3b5801efef6103aca20
pa-lem <pa-lem@hotmail.fr>
2025-02-07 18:25:50 +0100

    start add more details

--- frontend/app/src/entities/nodes/activities/ui/activity.tsx
+++ frontend/app/src/entities/nodes/activities/ui/activity.tsx
@@ -3,6 +3,9 @@ import { DateDisplay } from "@/shared/components/display/date-display";
 import { Badge } from "@/shared/components/ui/badge";
 
 import { DisplayLabel } from "@/entities/nodes/object/ui/display-label";
+import { PropertyRow } from "@/entities/schema/ui/styled";
+import { CopyToClipboard } from "@/shared/components/buttons/copy-to-clipboard";
+import { Popover, PopoverContent, PopoverTrigger } from "@/shared/components/ui/popover";
 import { Icon } from "@iconify-icon/react";
 
 export type ActivityType = EventNodeInterface | NodeMutatedEvent;
@@ -12,7 +15,7 @@ const AcitivityAttributes = ({ attributes }: Pick<NodeMutatedEvent, "attributes"
     <div className="pl-8 text-sm">
       {attributes.map(({ action, name, value, value_previous }) => {
         return (
-          <div className="grid grid-cols-2" key={`${action}_${name}`}>
+          <div className="grid grid-cols-2 gap-2" key={`${action}_${name}`}>
             <div>{name}</div>
 
             <div className="flex items-center gap-4">
@@ -29,14 +32,33 @@ const AcitivityAttributes = ({ attributes }: Pick<NodeMutatedEvent, "attributes"
   );
 };
 
-export const Activity = ({
-  id,
-  event,
-  occurred_at,
-  account_id,
-  branch,
-  ...props
-}: ActivityType) => {
+const ActivityDetails = ({ id, event, occurred_at, account_id, ...props }: ActivityType) => {
+  return (
+    <div className="divide-y">
+      <PropertyRow
+        title="ID"
+        value={
+          <div className="flex items-center gap-2">
+            {id} <CopyToClipboard text={id} />
+          </div>
+        }
+      />
+      <PropertyRow title="Event" value={event} />
+      <PropertyRow title="Occured at" value={<DateDisplay date={occurred_at} />} />
+      <PropertyRow title="Account" value={<DisplayLabel id={account_id} />} />
+      {"attributes" in props && (
+        <PropertyRow
+          title="Changes"
+          value={<AcitivityAttributes attributes={props.attributes} />}
+        />
+      )}
+    </div>
+  );
+};
+
+export const Activity = (props: ActivityType) => {
+  const { event, occurred_at, account_id } = props;
+
   return (
     <div className="flex gap-3 bg-slate-50/50 p-2 rounded-md shadow-sm">
       {/* <TimelineBorder /> */}
@@ -59,7 +81,18 @@ export const Activity = ({
 
         {"attributes" in props && <AcitivityAttributes attributes={props.attributes} />}
 
-        <p className="text-sm underline text-gray-600 dark:text-neutral-400 mb-1">View more.</p>
+        <div>
+          <Popover>
+            <PopoverTrigger>
+              <p className="text-sm underline text-gray-600 dark:text-neutral-400 mb-1">
+                View more.
+              </p>
+            </PopoverTrigger>
+            <PopoverContent className="w-full">
+              <ActivityDetails {...props} />
+            </PopoverContent>
+          </Popover>
+        </div>
       </div>
     </div>
   );
