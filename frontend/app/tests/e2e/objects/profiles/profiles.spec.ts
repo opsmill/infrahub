b669c5ee8e71176edcdfa5ef95f769de6e9d5ec7
bilalabbad <bilal@opsmill.com>
2025-02-07 15:28:10 +0100

    fix

--- frontend/app/tests/e2e/objects/profiles/profiles.spec.ts
+++ frontend/app/tests/e2e/objects/profiles/profiles.spec.ts
@@ -80,8 +80,7 @@ test.describe("/objects/CoreProfile - Profiles page", () => {
       await page.getByTestId("source-profile-badge").hover();
       await expect(page.getByTestId("source-profile-tooltip").first()).toBeVisible();
       await expect(page.getByRole("link", { name: "profile test tag" }).first()).toBeVisible();
-      await page.locator("body").press("Escape"); // hide tooltip
-
+      await page.getByLabel("Name *").click(); // hide tooltip
       await page.getByLabel("Name *").fill("tag with profile");
       await page.getByRole("button", { name: "Save" }).click();
     });
