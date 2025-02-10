b6841c9ae3a7acbc8ffb39971e4119f56e1ca9e7
bilalabbad <bilal@opsmill.com>
2025-02-11 00:56:31 +0100

    better colors

--- frontend/app/src/entities/nodes/object/ui/objects-table/cells/style.tsx
+++ frontend/app/src/entities/nodes/object/ui/objects-table/cells/style.tsx
@@ -1,5 +1,6 @@
 export const cellsStyle = "flex items-center gap-1.5 p-2 text-sm h-10";
 
-export const cellHeaderStyle = "z-[1] sticky top-0 border-r border-y bg-stone-50 hover:bg-gray-100";
+export const cellHeaderStyle =
+  "z-[1] sticky top-0 border-r border-y bg-white hover:bg-gray-100 font-medium";
 
-export const cellBodyStyle = "bg-white border-r border-b";
+export const cellBodyStyle = "bg-gray-50 border-r border-b";
