1c4b66af29a84d7a87c4e4b5b0b014d4328ba0ab
pa-lem <pa-lem@hotmail.fr>
2025-02-12 15:35:42 +0100

    fix test

--- frontend/app/tests/e2e/tutorial/tutorial-1_object-create-update-diff-and-merge.spec.ts
+++ frontend/app/tests/e2e/tutorial/tutorial-1_object-create-update-diff-and-merge.spec.ts
@@ -105,7 +105,7 @@ test.describe("Getting started with Infrahub - Object and branch creation, updat
       await page.getByRole("link", { name: "View all branches" }).click();
       await saveScreenshotForDocs(page, "tutorial_1_branch_list");
       await page.getByTestId("branches-items").getByText("cr1234").click();
-      await expect(page.locator("dd")).toContainText("cr1234");
+      await expect(page.getByRole("definition").filter({ hasText: "cr1234" })).toBeVisible();
     });
 
     await test.step("trigger the diff update", async () => {
