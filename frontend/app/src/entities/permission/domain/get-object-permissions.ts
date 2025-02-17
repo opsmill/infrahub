c873fd8ae8ace776e986884fe1710d321ddbcef0
bilalabbad <bilal@opsmill.com>
2025-02-17 17:20:07 +0100

    more

--- frontend/app/src/entities/permission/domain/get-object-permissions.ts
+++ frontend/app/src/entities/permission/domain/get-object-permissions.ts
@@ -3,11 +3,7 @@ import { Permission } from "@/entities/permission/types";
 import { getPermission } from "@/entities/permission/utils";
 import { ContextParams } from "@/shared/api/types";
 
-export type GetObjectPermissions = (
-  args: ContextParams & {
-    kind: string;
-  }
-) => Promise<Permission>;
+export type GetObjectPermissions = (args: ContextParams & { kind: string }) => Promise<Permission>;
 
 export const getObjectPermissions: GetObjectPermissions = async ({ kind, branchName, atDate }) => {
   const { data } = await getPermissionsFromApi({ kind, branchName, atDate });
