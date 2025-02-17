9f3b626d221fdd33d705f6090ac39d2f1d005cf9
pa-lem <pa-lem@hotmail.fr>
2025-02-17 17:51:40 +0100

    lint

--- frontend/app/src/entities/events/ui/global-branch-event.tsx
+++ frontend/app/src/entities/events/ui/global-branch-event.tsx
@@ -6,14 +6,12 @@ export const BranchEvent = (props: EventNodeInterface) => {
   const { event, branch } = props;
 
   return (
-    <>
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-2 text-sm text-gray-500">
-          <Icon icon="mdi:source-branch" className="text-gray-400" />
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-2 text-sm text-gray-500">
+        <Icon icon="mdi:source-branch" className="text-gray-400" />
 
-          {branch && BRANCH_EVENTS_MAPPING[event] && BRANCH_EVENTS_MAPPING[event](branch)}
-        </div>
+        {branch && BRANCH_EVENTS_MAPPING[event] && BRANCH_EVENTS_MAPPING[event](branch)}
       </div>
-    </>
+    </div>
   );
 };
