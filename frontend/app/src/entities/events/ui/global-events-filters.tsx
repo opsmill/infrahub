af7cc4a4b181299a9449d29fe4fa2d3dc155036e
pa-lem <pa-lem@hotmail.fr>
2025-02-19 09:38:31 +0100

    fix brancehs choices

--- frontend/app/src/entities/events/ui/global-events-filters.tsx
+++ frontend/app/src/entities/events/ui/global-events-filters.tsx
@@ -1,82 +1,83 @@
 import { branchesState } from "@/entities/branches/stores";
-import { branchesToSelectOptions } from "@/entities/branches/utils";
 import { ScrollArea } from "@/shared/components/ui/scroll-area";
-import { store } from "@/shared/stores";
+import { useAtomValue } from "jotai";
 import { TagGroup, TagList } from "react-aria-components";
 import { GlobalFilter } from "./global-filter";
 
-const FILTERS = [
-  {
-    name: "hasChildren",
-    label: "Has Children",
-    fieldSchema: {
-      kind: "Boolean",
-    },
-  },
-  {
-    name: "branches",
-    label: "Branch",
-    fieldSchema: {
-      kind: "Dropdown",
-      choices: branchesToSelectOptions(store.get(branchesState)).map((branch) => {
-        return {
-          label: branch.name,
-          name: branch.name,
-        };
-      }),
-    },
-  },
-  {
-    name: "eventType",
-    label: "Event Type",
-    fieldSchema: {
-      kind: "Dropdown",
-      choices: [
-        {
-          label: "Node update",
-          name: "NodeMutatedEvent",
-        },
-      ],
-    },
-  },
-  {
-    name: "primaryNodeIds",
-    label: "Primary Node",
-    fieldSchema: {
-      peer: "CoreNode",
-    },
-  },
-  {
-    name: "relatedNodeIds",
-    label: "Related Node",
-    fieldSchema: {
-      peer: "CoreNode",
-    },
-  },
-  {
-    name: "accountIds",
-    label: "Account",
-    fieldSchema: {
-      peer: "CoreAccount",
-    },
-  },
-  {
-    name: "since",
-    label: "Start Date",
-    fieldSchema: {
-      kind: "DateTime",
-    },
-  },
-  {
-    name: "until",
-    label: "End Date",
-    fieldSchema: {
-      kind: "DateTime",
-    },
-  },
-];
-
 export const GlobalEventsFilters = () => {
+  const branches = useAtomValue(branchesState);
+
+  const FILTERS = [
+    {
+      name: "hasChildren",
+      label: "Has Children",
+      fieldSchema: {
+        kind: "Boolean",
+      },
+    },
+    {
+      name: "branches",
+      label: "Branch",
+      fieldSchema: {
+        kind: "Dropdown",
+        choices: branches.map((branch) => {
+          return {
+            label: branch.name,
+            name: branch.name,
+          };
+        }),
+      },
+    },
+    {
+      name: "eventType",
+      label: "Event Type",
+      fieldSchema: {
+        kind: "Dropdown",
+        choices: [
+          {
+            label: "Node update",
+            name: "NodeMutatedEvent",
+          },
+        ],
+      },
+    },
+    {
+      name: "primaryNodeIds",
+      label: "Primary Node",
+      fieldSchema: {
+        peer: "CoreNode",
+      },
+    },
+    {
+      name: "relatedNodeIds",
+      label: "Related Node",
+      fieldSchema: {
+        peer: "CoreNode",
+      },
+    },
+    {
+      name: "accountIds",
+      label: "Account",
+      fieldSchema: {
+        peer: "CoreAccount",
+      },
+    },
+    {
+      name: "since",
+      label: "Start Date",
+      fieldSchema: {
+        kind: "DateTime",
+      },
+    },
+    {
+      name: "until",
+      label: "End Date",
+      fieldSchema: {
+        kind: "DateTime",
+      },
+    },
+  ];
+
   return (
     <ScrollArea scrollX>
       <TagGroup className="flex" selectionMode="single">
