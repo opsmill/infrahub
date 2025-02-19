0b96be17017a39d30363adadd9fc51b8fe5510ba
Damien Garros <dgarros@gmail.com>
2025-02-19 11:11:48 +0100

    Cleanup helpers file

--- python_testcontainers/infrahub_testcontainers/helpers.py
+++ python_testcontainers/infrahub_testcontainers/helpers.py
@@ -1,10 +1,8 @@
 import os
-import shutil
 import subprocess  # noqa: S404
 from pathlib import Path
 
 import pytest
-from testcontainers.core.exceptions import ContainerIsNotRunning
 
 from infrahub_testcontainers import __version__ as infrahub_version
 
@@ -93,87 +91,3 @@ class TestInfrahubDocker:
     @pytest.fixture(scope="class")
     def task_manager_port(self, infrahub_app: dict[str, int]) -> int:
         return infrahub_app["task-manager"]
-
-
-def backup_database(
-    request: pytest.FixtureRequest, backup_name: str = "neo4j_database.backup", dest_dir: Path | None = None
-) -> None:
-    assert "enterprise" in os.environ.get("NEO4J_DOCKER_IMAGE", "")
-
-    backup_dir: Path = request.getfixturevalue("remote_backups_dir")
-    infrahub_compose: InfrahubDockerCompose = request.getfixturevalue("infrahub_compose")
-
-    infrahub_compose.exec_in_container(
-        command=[
-            "neo4j-admin",
-            "database",
-            "backup",
-            "--compress=false",
-            "--to-path",
-            os.environ.get(
-                "INFRAHUB_TESTING_INTERNAL_DB_BACKUP_DIRECTORY",
-                PROJECT_ENV_VARIABLES["INFRAHUB_TESTING_INTERNAL_DB_BACKUP_DIRECTORY"],
-            ),
-        ],
-        service_name="database",
-    )
-
-    if dest_dir:
-        backup_files = list(backup_dir.glob("*.backup"))
-        if not backup_files:
-            raise FileNotFoundError(f"No .backup files found in {backup_dir}")
-
-        backup_file = backup_files[0]
-        shutil.copy(
-            backup_file,
-            dest_dir / backup_name,
-        )
-
-
-def restore_database(compose: InfrahubDockerCompose, backup_file: Path, remote_backup_dir: Path) -> None:
-    assert "enterprise" in os.environ.get("NEO4J_DOCKER_IMAGE", "")
-
-    shutil.copy(
-        str(backup_file),
-        str(remote_backup_dir / backup_file.name),
-    )
-    service_name = "database"
-
-    # Ensure the database container is running
-    try:
-        compose.get_container(service_name=service_name)
-    except ContainerIsNotRunning:
-        compose.start_container(service_name=service_name)
-
-    compose.exec_in_container(
-        command=["cypher-shell", "-u", "neo4j", "-p", "admin", "STOP DATABASE neo4j;"],
-        service_name=service_name,
-    )
-
-    compose.exec_in_container(
-        command=[
-            "neo4j-admin",
-            "database",
-            "restore",
-            "--overwrite-destination",
-            "--from-path",
-            str(
-                Path(
-                    os.environ.get(
-                        "INFRAHUB_TESTING_INTERNAL_DB_BACKUP_DIRECTORY",
-                        PROJECT_ENV_VARIABLES["INFRAHUB_TESTING_INTERNAL_DB_BACKUP_DIRECTORY"],
-                    )
-                )
-                / backup_file.name
-            ),
-        ],
-        service_name=service_name,
-    )
-
-    compose.exec_in_container(
-        command=["cypher-shell", "-d", "system", "-u", "neo4j", "-p", "admin", "START DATABASE neo4j;"],
-        service_name=service_name,
-    )
-
-    compose.stop(down=False)
-    compose.start()
