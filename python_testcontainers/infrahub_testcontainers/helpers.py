6b02ab06079fa05ac6a16585aa777aa1f83f9db8
Damien Garros <dgarros@gmail.com>
2025-02-19 09:36:12 +0100

    Add performance test plugin for pytest

--- python_testcontainers/infrahub_testcontainers/helpers.py
+++ python_testcontainers/infrahub_testcontainers/helpers.py
@@ -4,6 +4,7 @@ import subprocess  # noqa: S404
 from pathlib import Path
 
 import pytest
+from testcontainers.core.exceptions import ContainerIsNotRunning
 
 from infrahub_testcontainers import __version__ as infrahub_version
 
@@ -15,7 +16,8 @@ class TestInfrahubDocker:
     def infrahub_version(self) -> str:
         return infrahub_version
 
-    def execute_ctl_run(self, address: str, script: str) -> str:
+    @staticmethod
+    def execute_ctl_run(address: str, script: str) -> str:
         env = os.environ.copy()
         env["INFRAHUB_ADDRESS"] = address
         env["INFRAHUB_API_TOKEN"] = PROJECT_ENV_VARIABLES["INFRAHUB_TESTING_INITIAL_ADMIN_TOKEN"]
@@ -24,6 +26,17 @@ class TestInfrahubDocker:
             f"infrahubctl run {script}", shell=True, capture_output=True, text=True, env=env, check=False
         )
         return result.stdout
+
+    @staticmethod
+    def execute_command(command: str, address: str, concurrent_execution: int = 10) -> subprocess.CompletedProcess[str]:
+        env = os.environ.copy()
+        env["INFRAHUB_ADDRESS"] = address
+        env["INFRAHUB_API_TOKEN"] = PROJECT_ENV_VARIABLES["INFRAHUB_TESTING_INITIAL_ADMIN_TOKEN"]
+        env["INFRAHUB_MAX_CONCURRENT_EXECUTION"] = f"{concurrent_execution}"
+        result = subprocess.run(  # noqa: S602
+            command, shell=True, capture_output=True, text=True, env=env, check=False
+        )
+        return result
 
     @pytest.fixture(scope="class")
     def tmp_directory(self, tmpdir_factory: pytest.TempdirFactory) -> Path:
@@ -81,72 +94,86 @@ class TestInfrahubDocker:
     def task_manager_port(self, infrahub_app: dict[str, int]) -> int:
         return infrahub_app["task-manager"]
 
-    def backup_database(self, request: pytest.FixtureRequest, dest_dir: Path | None = None) -> None:
-        assert "enterprise" in os.environ.get("NEO4J_DOCKER_IMAGE", "")
 
-        backup_dir: Path = request.getfixturevalue("remote_backups_dir")
-        infrahub_compose: InfrahubDockerCompose = request.getfixturevalue("infrahub_compose")
+def backup_database(
+    request: pytest.FixtureRequest, backup_name: str = "neo4j_database.backup", dest_dir: Path | None = None
+) -> None:
+    assert "enterprise" in os.environ.get("NEO4J_DOCKER_IMAGE", "")
 
-        infrahub_compose.exec_in_container(
-            command=[
-                "neo4j-admin",
-                "database",
-                "backup",
-                "--to-path",
-                os.environ.get(
-                    "INFRAHUB_TESTING_INTERNAL_DB_BACKUP_DIRECTORY",
-                    PROJECT_ENV_VARIABLES["INFRAHUB_TESTING_INTERNAL_DB_BACKUP_DIRECTORY"],
-                ),
-            ],
-            service_name="database",
-        )
+    backup_dir: Path = request.getfixturevalue("remote_backups_dir")
+    infrahub_compose: InfrahubDockerCompose = request.getfixturevalue("infrahub_compose")
 
-        if dest_dir:
-            shutil.copytree(
-                str(backup_dir),
-                str(dest_dir),
-            )
+    infrahub_compose.exec_in_container(
+        command=[
+            "neo4j-admin",
+            "database",
+            "backup",
+            "--compress=false",
+            "--to-path",
+            os.environ.get(
+                "INFRAHUB_TESTING_INTERNAL_DB_BACKUP_DIRECTORY",
+                PROJECT_ENV_VARIABLES["INFRAHUB_TESTING_INTERNAL_DB_BACKUP_DIRECTORY"],
+            ),
+        ],
+        service_name="database",
+    )
 
-    def restore_database(self, request: pytest.FixtureRequest, backup_file: Path) -> None:
-        assert "enterprise" in os.environ.get("NEO4J_DOCKER_IMAGE", "")
+    if dest_dir:
+        backup_files = list(backup_dir.glob("*.backup"))
+        if not backup_files:
+            raise FileNotFoundError(f"No .backup files found in {backup_dir}")
 
-        backup_dir: Path = request.getfixturevalue("remote_backups_dir")
-        infrahub_compose: InfrahubDockerCompose = request.getfixturevalue("infrahub_compose")
-
+        backup_file = backup_files[0]
         shutil.copy(
-            str(backup_file),
-            str(backup_dir / backup_file.name),
+            backup_file,
+            dest_dir / backup_name,
         )
 
-        infrahub_compose.exec_in_container(
-            command=["cypher-shell", "-u", "neo4j", "-p", "admin", "STOP DATABASE neo4j;"],
-            service_name="database",
-        )
 
-        infrahub_compose.exec_in_container(
-            command=[
-                "neo4j-admin",
-                "database",
-                "restore",
-                "--overwrite-destination",
-                "--from-path",
-                str(
-                    Path(
-                        os.environ.get(
-                            "INFRAHUB_TESTING_INTERNAL_DB_BACKUP_DIRECTORY",
-                            PROJECT_ENV_VARIABLES["INFRAHUB_TESTING_INTERNAL_DB_BACKUP_DIRECTORY"],
-                        )
+def restore_database(compose: InfrahubDockerCompose, backup_file: Path, remote_backup_dir: Path) -> None:
+    assert "enterprise" in os.environ.get("NEO4J_DOCKER_IMAGE", "")
+
+    shutil.copy(
+        str(backup_file),
+        str(remote_backup_dir / backup_file.name),
+    )
+    service_name = "database"
+
+    # Ensure the database container is running
+    try:
+        compose.get_container(service_name=service_name)
+    except ContainerIsNotRunning:
+        compose.start_container(service_name=service_name)
+
+    compose.exec_in_container(
+        command=["cypher-shell", "-u", "neo4j", "-p", "admin", "STOP DATABASE neo4j;"],
+        service_name=service_name,
+    )
+
+    compose.exec_in_container(
+        command=[
+            "neo4j-admin",
+            "database",
+            "restore",
+            "--overwrite-destination",
+            "--from-path",
+            str(
+                Path(
+                    os.environ.get(
+                        "INFRAHUB_TESTING_INTERNAL_DB_BACKUP_DIRECTORY",
+                        PROJECT_ENV_VARIABLES["INFRAHUB_TESTING_INTERNAL_DB_BACKUP_DIRECTORY"],
                     )
-                    / backup_file.name
-                ),
-            ],
-            service_name="database",
-        )
+                )
+                / backup_file.name
+            ),
+        ],
+        service_name=service_name,
+    )
 
-        infrahub_compose.exec_in_container(
-            command=["cypher-shell", "-d", "system", "-u", "neo4j", "-p", "admin", "START DATABASE neo4j;"],
-            service_name="database",
-        )
+    compose.exec_in_container(
+        command=["cypher-shell", "-d", "system", "-u", "neo4j", "-p", "admin", "START DATABASE neo4j;"],
+        service_name=service_name,
+    )
 
-        infrahub_compose.stop(down=False)
-        infrahub_compose.start()
+    compose.stop(down=False)
+    compose.start()
---
+++ python_testcontainers/infrahub_testcontainers/plugin.py
@@ -0,0 +1,110 @@
+from __future__ import annotations
+
+import pytest
+
+from .performance_test import InfrahubPerformanceTest
+
+
+def pytest_addoption(parser: pytest.Parser) -> None:
+    group = parser.getgroup("infrahub-performance-test")
+
+    group.addoption(
+        "--performance-result-address",
+        action="store",
+        dest="infrahub_performance_test_result_address",
+        default="https://webhook.site/25802839-4b34-433e-9dc4-59623cd73c80",
+        metavar="INFRAHUB_PERFORMANCE_TEST_RESULT_ADDRESS",
+        help="Address to send the results of the performance test (default: %(default)s)",
+    )
+    group.addoption(
+        "--performance-backup",
+        action="store",
+        dest="infrahub_performance_backup",
+        default="neo4j_database.db",
+        metavar="INFRAHUB_PERFORMANCE_BACKUP",
+        help="Name of the backup to use (default: %(default)s)",
+    )
+    group.addoption(
+        "--performance-backup-location",
+        action="store",
+        dest="infrahub_performance_backup_location",
+        metavar="INFRAHUB_PERFORMANCE_BACKUP_LOCATION",
+        help="Location of the backup to use (default: %(default)s)",
+    )
+    group.addoption(
+        "--performance-create-backup",
+        action="store_true",
+        default=False,
+        dest="infrahub_performance_create_backup",
+        help="Generate a backup of the database (default: %(default)s)",
+    )
+    group.addoption(
+        "--performance-use-backup",
+        action="store_true",
+        default=False,
+        dest="infrahub_performance_use_backup",
+        help="Use a backup of the database and skip all tests that creates data (default: %(default)s)",
+    )
+    group.addoption(
+        "--performance-report",
+        action="store_true",
+        dest="infrahub_performance_report",
+        default=False,
+        help="Display performance report at the end of the test run",
+    )
+
+
+def pytest_collection_modifyitems(config: pytest.Config, items: list[pytest.Item]) -> None:
+    use_backup_skip = pytest.mark.skip(reason="load data from backup")
+    no_create_backup_skip = pytest.mark.skip(reason="no need to create a backup")
+    no_load_backup_skip = pytest.mark.skip(reason="no need to load a backup")
+
+    use_backup = config.getoption("infrahub_performance_use_backup")
+    create_backup = config.getoption("infrahub_performance_create_backup")
+
+    for item in items:
+        has_create_backup_marker = bool(
+            [True for marker in item.own_markers if marker.name == "performance_create_backup"]
+        )
+        has_load_backup_marker = bool([True for marker in item.own_markers if marker.name == "performance_load_backup"])
+        has_load_data_marker = bool([True for marker in item.own_markers if marker.name == "performance_load_data"])
+
+        if has_create_backup_marker and not create_backup:
+            item.add_marker(no_create_backup_skip)
+        if has_load_backup_marker and not use_backup:
+            item.add_marker(no_load_backup_skip)
+        if has_load_data_marker and use_backup:
+            item.add_marker(use_backup_skip)
+
+
+def pytest_sessionstart(session: pytest.Session) -> None:
+    session.infrahub_performance_test = InfrahubPerformanceTest(
+        results_url=session.config.getoption("infrahub_performance_test_result_address")
+    )
+
+
+def pytest_sessionfinish(session: pytest.Session, exitstatus: int) -> None:
+    """whole test run finishes."""
+    session.infrahub_performance_test.finalize(session=session)
+
+
+def pytest_configure(config: pytest.Config) -> None:
+    if config.getoption("infrahub_performance_use_backup") and config.getoption("infrahub_performance_create_backup"):
+        raise pytest.UsageError("--performance-use-backup and --performance-create-backup are mutually exclusive")
+
+    config.addinivalue_line("markers", "performance_create_backup: Create a backup of the database")
+    config.addinivalue_line("markers", "performance_load_backup: Load the backup of the database")
+    config.addinivalue_line("markers", "performance_load_data: Load initial data into the database")
+
+
+# def pytest_terminal_summary(session: pytest.Session, terminalreporter):
+
+#     performance_test = session.infrahub_performance_test
+
+#     report = [f"{measurement.name}: {measurement.value} {measurement.unit}" for measurement in performance_test.measurements]
+#     terminalreporter.write('\n' + "\n".join(report) + '\n')
+
+
+@pytest.fixture
+def perf_test(request: pytest.FixtureRequest) -> InfrahubPerformanceTest:
+    return request.session.infrahub_performance_test
