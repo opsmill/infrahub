990430fcd9f3449333716223a6dca1b35544f19a
bilalabbad <bilal@opsmill.com>
2025-02-20 17:41:43 +0100

    remove filter search input

--- frontend/app/src/entities/nodes/relationships/ui/object-relationships-manager.tsx
+++ frontend/app/src/entities/nodes/relationships/ui/object-relationships-manager.tsx
@@ -1,13 +1,8 @@
-import { ActiveFilterTags } from "@/entities/nodes/object/ui/filters/active-filter-tags";
-import { FilterResetButton } from "@/entities/nodes/object/ui/filters/filter-reset-button";
-import { FilterSearchInput } from "@/entities/nodes/object/ui/filters/filter-search-input";
 import { RelationshipTable } from "@/entities/nodes/relationships/ui/relationship-table/relationship-table";
 import { getObjectDetailsUrl2 } from "@/entities/nodes/utils";
 import { useSchema } from "@/entities/schema/hooks/useSchema";
 import { IModelSchema } from "@/entities/schema/stores/schema.atom";
 import { ALERT_TYPES, Alert } from "@/shared/components/ui/alert";
-import { ScrollArea } from "@/shared/components/ui/scroll-area";
-import useFilters from "@/shared/hooks/useFilters";
 import { Navigate } from "react-router";
 import { toast } from "react-toastify";
 
@@ -21,7 +16,6 @@ export function ObjectRelationshipsManager({
   parentNodeId,
   relationshipName,
 }: ObjectRelationshipsManagerProps) {
-  const [filters] = useFilters();
   const relationshipDefinition = parentNodeSchema.relationships?.find(
     (r) => r?.name === relationshipName
   );
@@ -43,26 +37,11 @@ export function ObjectRelationshipsManager({
   }
 
   return (
-    <>
-      <div className="flex items-center h-14 px-3 shrink-0">
-        <FilterSearchInput schema={relationshipSchema} />
-
-        {filters.length > 0 && (
-          <>
-            <ScrollArea scrollX>
-              <ActiveFilterTags schema={relationshipSchema} className="mx-2" />
-            </ScrollArea>
-            <FilterResetButton />
-          </>
-        )}
-      </div>
-
-      <RelationshipTable
-        parentKind={parentNodeSchema.kind as string}
-        parentId={parentNodeId}
-        relationshipName={relationshipName}
-        relationshipSchema={relationshipSchema}
-      />
-    </>
+    <RelationshipTable
+      parentKind={parentNodeSchema.kind as string}
+      parentId={parentNodeId}
+      relationshipName={relationshipName}
+      relationshipSchema={relationshipSchema}
+    />
   );
 }
