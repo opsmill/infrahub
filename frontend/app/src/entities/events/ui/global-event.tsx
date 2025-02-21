4a007860e7ad9fe7298f0cb2f64d0b56e97a459b
pa-lem <pa-lem@hotmail.fr>
2025-02-21 12:12:25 +0100

    update ui

--- frontend/app/src/entities/events/ui/global-event.tsx
+++ frontend/app/src/entities/events/ui/global-event.tsx
@@ -1,8 +1,7 @@
+import { Link } from "@/shared/components/ui/link";
 import { classNames } from "@/shared/utils/common";
 import { Icon } from "@iconify-icon/react";
 import { format } from "date-fns";
-import { useState } from "react";
-import { Link } from "react-router";
 import { BRANCH_EVENTS, STANDARD_EVENTS } from "../utils/constants";
 import { EventType } from "./event";
 import { BranchEvent } from "./global-branch-event";
@@ -10,19 +9,13 @@ import { NodeEvent } from "./global-node-event";
 import { StandardEvent } from "./global-standard-event";
 
 export const Event = ({ __typename, ...props }: EventType) => {
-  const [show, setShow] = useState(false);
-
   return (
-    <Link
-      to={`/activities/${props.id}`}
+    <div
       className={classNames(
         "flex w-full items-center flex-grow gap-3 p-2 rounded-md shadow-sm border transition-all",
-        "bg-gray-50 hover:bg-gray-100",
-        props.has_children && "bg-custom-blue-500/10 hover:bg-custom-blue-500/20 "
+        "bg-gray-50",
+        props.has_children && "bg-custom-blue-500/10"
       )}
-      onClick={() => {
-        setShow(!show);
-      }}
     >
       <div className="flex flex-col gap-2 grow">
         {"attributes" in props && <NodeEvent {...props} />}
@@ -32,14 +25,22 @@ export const Event = ({ __typename, ...props }: EventType) => {
         {STANDARD_EVENTS.includes(__typename) && <StandardEvent {...props} />}
       </div>
 
-      <div className="text-xs font-medium text-gray-500 flex items-center gap-1">
-        <Icon icon={"mdi:source-branch"} />
-        {props.branch}
-      </div>
+      <div className="grid grid-cols-3">
+        <Link to={`/activities/${props.id}`} className="text-xs text-gray-500">
+          View more
+        </Link>
 
-      <div className="text-xs font-medium text-gray-500">
-        {format(new Date(props.occurred_at), "yyyy-MM-dd HH:mm:ss (O)")}
+        <div className="text-xs font-medium text-gray-500 flex items-center gap-1">
+          <Icon icon={"mdi:source-branch"} />
+          {props.branch}
+        </div>
+
+        <div className="flex text-xs font-medium text-gray-500">
+          <span className="mr-2">
+            {props.occurred_at && format(new Date(props.occurred_at), "yyyy-MM-dd HH:mm:ss (O)")}
+          </span>
+        </div>
       </div>
-    </Link>
+    </div>
   );
 };
