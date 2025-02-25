76e9864af0b7652a0772d3cb18a8c2a06653bbbe
pa-lem <pa-lem@hotmail.fr>
2025-02-25 10:43:35 +0100

    update ui

--- frontend/app/src/entities/events/ui/global-event.tsx
+++ frontend/app/src/entities/events/ui/global-event.tsx
@@ -13,7 +13,7 @@ export const Event = ({ __typename, ...props }: EventType) => {
   return (
     <div
       className={classNames(
-        "grid grid-cols-3 relative gap-8 rounded-md shadow-sm transition-all",
+        "grid grid-cols-3 relative gap-8 rounded-md shadow-sm transition-all border",
         "bg-gray-50"
       )}
     >
@@ -25,13 +25,13 @@ export const Event = ({ __typename, ...props }: EventType) => {
         {STANDARD_EVENTS.includes(__typename) && <StandardEvent {...props} />}
       </div>
 
-      <div className="grid grid-cols-3 col-span-1 items-center text-right border-r border-b rounded-r-md border-custom-blue-500 p-2.5 relative">
+      <div className="grid grid-cols-3 col-span-1 items-center text-right p-2.5 relative">
         <div className="text-xs font-medium text-gray-500 flex items-center gap-1">
           {props.has_children && (
             <Tooltip enabled content="Contains sub activities">
               <Icon
                 icon={"mdi:subtasks"}
-                className="absolute -left-8 rounded-full text-white bg-custom-blue-500 p-1.5"
+                className="absolute -left-8 rounded-full text-custom-blue-500 bg-custom-blue-500/10 p-1.5"
               />
             </Tooltip>
           )}
@@ -41,7 +41,11 @@ export const Event = ({ __typename, ...props }: EventType) => {
         </div>
 
         <div className="flex text-xs font-medium text-gray-500 whitespace-nowrap">
-          {props.occurred_at && format(new Date(props.occurred_at), "yyyy-MM-dd HH:mm:ss (O)")}
+          {props.occurred_at && (
+            <Tooltip enabled content={props.occurred_at}>
+              <span>{format(new Date(props.occurred_at), "MMM dd - HH:mm:ss")}</span>
+            </Tooltip>
+          )}
         </div>
 
         <Link to={`/activities/${props.id}`} className="text-xs text-gray-500">
