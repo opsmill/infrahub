481657665772ccce0ff613d6e81e0628522a43d3
pa-lem <pa-lem@hotmail.fr>
2025-02-26 14:50:18 +0100

    set current branch in filter

--- frontend/app/src/entities/events/ui/global-events-filters.tsx
+++ frontend/app/src/entities/events/ui/global-events-filters.tsx
@@ -2,6 +2,7 @@ import { branchesState } from "@/entities/branches/stores";
 import { ScrollArea } from "@/shared/components/ui/scroll-area";
 import { useAtomValue } from "jotai";
 import { TagGroup, TagList } from "react-aria-components";
+import { GlobalBranchFilter } from "./global-branch-filter";
 import { GlobalFilter } from "./global-filter";
 
 export const GlobalEventsFilters = () => {
@@ -13,19 +14,6 @@ export const GlobalEventsFilters = () => {
       label: "Has Children",
       fieldSchema: {
         kind: "Boolean",
-      },
-    },
-    {
-      name: "branches",
-      label: "Branch",
-      fieldSchema: {
-        kind: "Dropdown",
-        choices: branches.map((branch) => {
-          return {
-            label: branch.name,
-            name: branch.name,
-          };
-        }),
       },
     },
     {
@@ -98,6 +86,20 @@ export const GlobalEventsFilters = () => {
     <ScrollArea scrollX>
       <TagGroup className="flex" selectionMode="single" aria-label="Filter group">
         <TagList className="flex items-center gap-2 py-3">
+          <GlobalBranchFilter
+            name="branches"
+            label="Branch"
+            fieldSchema={{
+              kind: "Dropdown",
+              choices: branches.map((branch) => {
+                return {
+                  label: branch.name,
+                  name: branch.name,
+                };
+              }),
+            }}
+          />
+
           {FILTERS.map((filter) => {
             return <GlobalFilter key={filter.name} {...filter} />;
           })}
