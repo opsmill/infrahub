06cc18969422c15b5907385df4d9a934178ed8bb
pa-lem <pa-lem@hotmail.fr>
2025-03-03 16:43:04 +0100

    handle commit events

--- frontend/app/src/entities/events/ui/global-standard-event.tsx
+++ frontend/app/src/entities/events/ui/global-standard-event.tsx
@@ -20,9 +20,13 @@ export const StandardEvent = (props: EventNodeInterface) => {
         <div className="flex items-center gap-2 text-sm text-gray-500">
           {getStandardEventIcon(event)}
 
-          <div className="text-black font-semibold">
-            <NodeLabel id={account_id} kind="CoreAccount" />
-          </div>
+          {account_id ? (
+            <div className="text-black font-semibold">
+              <NodeLabel id={account_id} kind="CoreAccount" />
+            </div>
+          ) : (
+            "-"
+          )}
 
           {(STANDARD_EVENTS_MAPPING[event] && STANDARD_EVENTS_MAPPING[event](props)) ?? event}
         </div>
--- frontend/app/src/entities/events/ui/standard-event.tsx
+++ frontend/app/src/entities/events/ui/standard-event.tsx
@@ -1,5 +1,7 @@
+import { QSP } from "@/config/qsp";
 import { NodeLabel } from "@/entities/nodes/object/ui/node-label";
 import { EventNodeInterface } from "@/shared/api/graphql/generated/graphql";
+import { constructPath } from "@/shared/api/rest/fetch";
 import { Link } from "@/shared/components/ui/link";
 import { ReactElement } from "react";
 
@@ -24,6 +26,23 @@ export const STANDARD_EVENTS_MAPPING: Record<string, (props: EventNodeInterface)
           merged the branch
           <Link to={`/branches/${props.payload?.context?.branch?.name}`} className="text-black">
             {props.payload?.context?.branch?.name}
+          </Link>
+        </div>
+      );
+    },
+    "infrahub.repository.update_commit": (props) => {
+      return (
+        <div className="flex items-center gap-2">
+          updated the commit
+          <span className="text-black">{props.payload?.commit}</span>
+          from repository
+          <Link
+            className="text-black"
+            to={constructPath(`/objects/CoreRepository/${props.payload?.repository_id}`, [
+              { name: QSP.BRANCH, value: props.payload?.context?.branch?.name },
+            ])}
+          >
+            {props.payload?.repository_name}
           </Link>
         </div>
       );
