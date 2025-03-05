9877800df490a8510c0e7ead5332a139656115a7
Bearchitek <benoit@opsmill.com>
2025-03-06 00:26:26 +0100

    linter

--- frontend/app/tests/e2e/activity-log/global-activity-log.spec.ts
+++ frontend/app/tests/e2e/activity-log/global-activity-log.spec.ts
@@ -54,7 +54,10 @@ test.describe("Global Activity Log - List view and filter usage", () => {
       await page.getByRole("button", { name: "Has Children" }).click();
       await page.getByText("True").click();
       await page.getByRole("button", { name: "Apply" }).click();
-      await saveScreenshotForDocs(page, "topics/activity-logs/activity_log_global_filters_children");
+      await saveScreenshotForDocs(
+        page,
+        "topics/activity-logs/activity_log_global_filters_children"
+      );
     });
 
     await test.step("View Event details with children", async () => {
@@ -63,7 +66,10 @@ test.describe("Global Activity Log - List view and filter usage", () => {
 
       // Check that at least one "View more." button is present in the details page
       await expect(page.locator("#root")).toContainText("View more.");
-      await saveScreenshotForDocs(page, "topics/activity-logs/activity_log_global_details_children");
+      await saveScreenshotForDocs(
+        page,
+        "topics/activity-logs/activity_log_global_details_children"
+      );
     });
   });
 });
--- frontend/app/tests/e2e/guide/resource_manager_guide.spec.ts
+++ frontend/app/tests/e2e/guide/resource_manager_guide.spec.ts
@@ -12,7 +12,10 @@ test.describe("Resource Managers guide", () => {
       await page.goto("/ipam");
       await page.getByTestId("create-object-button").click();
       await page.getByLabel("Prefix *").fill("10.100.0.0/24");
-      await saveScreenshotForDocs(page, "guides/resources-manager/resource_manager_rss_prefix_10_100_0");
+      await saveScreenshotForDocs(
+        page,
+        "guides/resources-manager/resource_manager_rss_prefix_10_100_0"
+      );
       await page.getByRole("button", { name: "Save" }).click();
       await page.getByText("IPPrefix created").waitFor({ state: "visible" });
     });
@@ -54,9 +57,15 @@ test.describe("Resource Managers guide", () => {
       await page.getByRole("textbox", { name: "Type *" }).fill("MX204");
       await page.getByTestId("select-open-pool-option-button").click();
       await page.getByRole("option", { name: "My IP address pool" }).waitFor({ state: "visible" });
-      await saveScreenshotForDocs(page, "guides/resources-manager/resource_manager_pool_device_before");
+      await saveScreenshotForDocs(
+        page,
+        "guides/resources-manager/resource_manager_pool_device_before"
+      );
       await page.getByRole("option", { name: "My IP address pool" }).click();
-      await saveScreenshotForDocs(page, "guides/resources-manager/resource_manager_pool_device_after");
+      await saveScreenshotForDocs(
+        page,
+        "guides/resources-manager/resource_manager_pool_device_after"
+      );
       await page.getByRole("button", { name: "Save" }).click();
     });
   });
@@ -71,7 +80,10 @@ test.describe("Resource Managers guide", () => {
         .locator("div")
         .filter({ hasText: /^Prefix$/ })
         .click();
-      await saveScreenshotForDocs(page, "guides/resources-manager/resource_manager_rss_prefix_10_100_1");
+      await saveScreenshotForDocs(
+        page,
+        "guides/resources-manager/resource_manager_rss_prefix_10_100_1"
+      );
       await page.getByRole("button", { name: "Save" }).click();
       await page.getByText("IPPrefix created").waitFor({ state: "visible" });
     });
@@ -134,9 +146,15 @@ test.describe("Resource Managers guide", () => {
       await page.getByRole("textbox", { name: "Name *" }).fill("My vlan");
       await page.getByTestId("number-pool-button").click();
       await page.getByRole("option", { name: "My VLAN ID Pool" }).waitFor({ state: "visible" });
-      await saveScreenshotForDocs(page, "guides/resources-manager/resource_manager_pool_vlan_before");
+      await saveScreenshotForDocs(
+        page,
+        "guides/resources-manager/resource_manager_pool_vlan_before"
+      );
       await page.getByRole("option", { name: "My VLAN ID Pool" }).click();
-      await saveScreenshotForDocs(page, "guides/resources-manager/resource_manager_pool_vlan_after");
+      await saveScreenshotForDocs(
+        page,
+        "guides/resources-manager/resource_manager_pool_vlan_after"
+      );
       await page.getByRole("button", { name: "Save" }).click();
     });
   });
