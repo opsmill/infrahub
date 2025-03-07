caffba17a661943fa42e05f23c205c5be94c7210
Brett Lykins <lykinsbd@gmail.com>
2025-03-07 16:17:47 -0500

    release v1.1.8 prep
    fix bad paste

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -208,6 +208,7 @@ const sidebars: SidebarsConfig = {
             slug: 'release-notes/infrahub',
           },
           items: [
+            'release-notes/infrahub/release-1_1_8',
             'release-notes/infrahub/release-1_1_7',
             'release-notes/infrahub/release-1_1_6',
             'release-notes/infrahub/release-1_1_5',
