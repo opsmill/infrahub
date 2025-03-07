c2ee92e62f97ed295273c7779bb0aeb3424a8d74
Bearchitek <benoit@opsmill.com>
2025-03-07 17:38:02 +0100

    add permissions screenshot and re-generate

--- frontend/app/tests/e2e/role-management/roles-crud.spec.ts
+++ frontend/app/tests/e2e/role-management/roles-crud.spec.ts
@@ -6,7 +6,7 @@ test.describe("Role management - Roles CRUD", () => {
   test.use({ storageState: ACCOUNT_STATE_PATH.ADMIN });
   test.describe.configure({ mode: "serial" });
 
-  test("should create a role ", async ({ page }) => {
+  test("Should create a role ", async ({ page }) => {
     await test.step("access main view", async () => {
       await page.goto("/role-management/roles");
     });
@@ -30,7 +30,7 @@ test.describe("Role management - Roles CRUD", () => {
         .getByText("global:manage_repositories:")
         .click();
       await page.getByTestId("side-panel-container").getByLabel("Permissions").click();
-      await saveScreenshotForDocs(page, "guides/permissions/permissions_roles");
+      await saveScreenshotForDocs(page, "guides/permissions/permissions_role");
       await page.getByRole("button", { name: "Save" }).click();
       await expect(page.getByText("Role created!")).toBeVisible();
     });
@@ -48,7 +48,7 @@ test.describe("Role management - Roles CRUD", () => {
     });
   });
 
-  test("should update a role ", async ({ page }) => {
+  test("Should update a role ", async ({ page }) => {
     await test.step("access main view", async () => {
       await page.goto("/role-management/roles");
     });
@@ -81,7 +81,7 @@ test.describe("Role management - Roles CRUD", () => {
     });
   });
 
-  test("should delete a role ", async ({ page }) => {
+  test("Should delete a role ", async ({ page }) => {
     await test.step("access main view", async () => {
       await page.goto("/role-management/roles");
     });
