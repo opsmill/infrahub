e7756a96cefcb61ffe142ca7b6fb601371cf4ef0
Aaron McCarty <aaron@opsmill.com>
2025-03-10 16:28:46 -0700

    remove whoopsidaisy

--- backend/infrahub/core/node/standard.py
+++ backend/infrahub/core/node/standard.py
@@ -8,7 +8,6 @@ import ujson
 from infrahub_sdk.uuidt import UUIDT
 from pydantic import BaseModel
 
-from infrahub import config
 from infrahub.core.constants import NULL_VALUE
 from infrahub.core.query.standard_node import (
     StandardNodeCreateQuery,
@@ -215,9 +214,8 @@ class StandardNode(BaseModel):
     async def get_list(
         cls, db: InfrahubDatabase, limit: int = 1000, ids: list[str] | None = None, name: str | None = None, **kwargs
     ) -> list[Self]:
-        use_profiles = not config.SETTINGS.experimental_features.no_profiles
         query: Query = await StandardNodeGetListQuery.init(
-            db=db, node_class=cls, ids=ids, node_name=name, limit=limit, use_profiles=use_profiles, **kwargs
+            db=db, node_class=cls, ids=ids, node_name=name, limit=limit, **kwargs
         )
         await query.execute(db=db)
 
