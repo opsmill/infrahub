af8413cb47b233e1477a66fcae8fe99825817ef5
bilalabbad <bilal@opsmill.com>
2025-03-11 14:37:52 +0100

    lint

--- frontend/app/src/entities/artifacts/ui/artifact-details.tsx
+++ frontend/app/src/entities/artifacts/ui/artifact-details.tsx
@@ -1,5 +1,6 @@
 import { CONFIG } from "@/config/config";
 import { ArtifactFile } from "@/entities/artifacts/ui/artifact-file";
+import { NodeEvents } from "@/entities/events/ui/node-details-events";
 import { useObjectDetails } from "@/entities/nodes/hooks/useObjectDetails";
 import { NodeDescription } from "@/entities/nodes/object/ui/node-description";
 import { ModelSchema } from "@/entities/schema/types";
@@ -7,10 +8,9 @@ import ErrorScreen from "@/shared/components/errors/error-screen";
 import NoDataFound from "@/shared/components/errors/no-data-found";
 import Content from "@/shared/components/layout/content";
 import { LoadingIndicator } from "@/shared/components/loading/loading-indicator";
+import { Card } from "@/shared/components/ui/card";
 import { Divider } from "@/shared/components/ui/divider";
 import ArtifactHeader from "./artifact-header";
-import { NodeEvents } from "@/entities/events/ui/node-details-events";
-import { Card } from "@/shared/components/ui/card";
 
 export interface ArtifactsDetailsProps {
   artifactSchema: ModelSchema;
