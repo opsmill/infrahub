5e41a252732b5ade4300969e8c2953bb45343616
bilalabbad <bilal@opsmill.com>
2025-03-15 15:57:04 +0100

    more cleanup on test

--- frontend/app/tests/e2e/activities/global-activities-filters.spec.ts
+++ frontend/app/tests/e2e/activities/global-activities-filters.spec.ts
@@ -2,17 +2,7 @@ import { expect, test } from "@playwright/test";
 import { ACCOUNT_STATE_PATH } from "../../constants";
 
 test.describe("Global Activities - List view and filter usage", () => {
-  test.describe.configure({ mode: "serial" });
   test.use({ storageState: ACCOUNT_STATE_PATH.ADMIN });
-  test.slow();
-
-  test.beforeEach(async function ({ page }) {
-    page.on("response", async (response) => {
-      if (response.status() === 500) {
-        await expect(response.url()).toBe("This URL responded with a 500 status");
-      }
-    });
-  });
 
   test("Filter by branch", async ({ page }) => {
     await page.goto("/activities");
