ba755b993c13ef91a50ff6f7eb4dc80e5abe81b7
Damien Garros <dgarros@gmail.com>
2025-03-17 19:35:43 +0100

    Convert core schema in dict format to use proper Model

--- backend/infrahub/core/schema/definitions/core/propose_change_comment.py
+++ backend/infrahub/core/schema/definitions/core/propose_change_comment.py
@@ -3,186 +3,191 @@ from infrahub.core.constants import (
     InfrahubKind,
     RelationshipDeleteBehavior,
 )
+from infrahub.core.constants import RelationshipCardinality as Cardinality
+from infrahub.core.constants import RelationshipKind as RelKind
 
-core_propose_change_comment = {
-    "name": "Comment",
-    "namespace": "Core",
-    "description": "A comment on a Proposed Change",
-    "label": "Comment",
-    "display_labels": ["text__value"],
-    "order_by": ["created_at__value"],
-    "include_in_menu": False,
-    "branch": BranchSupportType.AGNOSTIC.value,
-    "attributes": [
-        {"name": "text", "kind": "TextArea", "unique": False, "optional": False},
-        {"name": "created_at", "kind": "DateTime", "optional": True},
-    ],
-    "relationships": [
-        {
-            "name": "created_by",
-            "peer": InfrahubKind.GENERICACCOUNT,
-            "optional": True,
-            "branch": BranchSupportType.AGNOSTIC.value,
-            "cardinality": "one",
-            "identifier": "comment__account",
-        },
-    ],
-}
+from ...attribute_schema import AttributeSchema as Attr
+from ...generic_schema import GenericSchema
+from ...node_schema import NodeSchema
+from ...relationship_schema import (
+    RelationshipSchema as Rel,
+)
 
-core_thread = {
-    "name": "Thread",
-    "namespace": "Core",
-    "description": "A thread on a Proposed Change",
-    "label": "Thread",
-    "order_by": ["created_at__value"],
-    "branch": BranchSupportType.AGNOSTIC.value,
-    "include_in_menu": False,
-    "attributes": [
-        {"name": "label", "kind": "Text", "optional": True},
-        {"name": "resolved", "kind": "Boolean", "default_value": False},
-        {"name": "created_at", "kind": "DateTime", "optional": True},
+core_propose_change_comment = GenericSchema(
+    name="Comment",
+    namespace="Core",
+    description="A comment on a Proposed Change",
+    label="Comment",
+    display_labels=["text__value"],
+    order_by=["created_at__value"],
+    include_in_menu=False,
+    branch=BranchSupportType.AGNOSTIC,
+    attributes=[
+        Attr(name="text", kind="TextArea", unique=False, optional=False),
+        Attr(name="created_at", kind="DateTime", optional=True),
     ],
-    "relationships": [
-        {
-            "name": "change",
-            "peer": InfrahubKind.PROPOSEDCHANGE,
-            "identifier": "proposedchange__thread",
-            "kind": "Parent",
-            "optional": False,
-            "cardinality": "one",
-        },
-        {
-            "name": "comments",
-            "peer": InfrahubKind.THREADCOMMENT,
-            "identifier": "thread__threadcomment",
-            "kind": "Component",
-            "optional": True,
-            "cardinality": "many",
-            "on_delete": RelationshipDeleteBehavior.CASCADE,
-        },
-        {
-            "name": "created_by",
-            "peer": InfrahubKind.GENERICACCOUNT,
-            "identifier": "thread__account",
-            "optional": True,
-            "branch": BranchSupportType.AGNOSTIC.value,
-            "cardinality": "one",
-        },
+    relationships=[
+        Rel(
+            name="created_by",
+            peer=InfrahubKind.GENERICACCOUNT,
+            optional=True,
+            branch=BranchSupportType.AGNOSTIC,
+            cardinality=Cardinality.ONE,
+            identifier="comment__account",
+        ),
     ],
-}
+)
 
-core_change_thread = {
-    "name": "ChangeThread",
-    "namespace": "Core",
-    "description": "A thread on proposed change",
-    "include_in_menu": False,
-    "label": "Change Thread",
-    "branch": BranchSupportType.AGNOSTIC.value,
-    "inherit_from": [InfrahubKind.THREAD],
-    "generate_profile": False,
-    "attributes": [],
-    "relationships": [],
-}
+core_thread = GenericSchema(
+    name="Thread",
+    namespace="Core",
+    description="A thread on a Proposed Change",
+    label="Thread",
+    order_by=["created_at__value"],
+    branch=BranchSupportType.AGNOSTIC,
+    include_in_menu=False,
+    attributes=[
+        Attr(name="label", kind="Text", optional=True),
+        Attr(name="resolved", kind="Boolean", default_value=False),
+        Attr(name="created_at", kind="DateTime", optional=True),
+    ],
+    relationships=[
+        Rel(
+            name="change",
+            peer=InfrahubKind.PROPOSEDCHANGE,
+            identifier="proposedchange__thread",
+            kind=RelKind.PARENT,
+            optional=False,
+            cardinality=Cardinality.ONE,
+        ),
+        Rel(
+            name="comments",
+            peer=InfrahubKind.THREADCOMMENT,
+            identifier="thread__threadcomment",
+            kind=RelKind.COMPONENT,
+            optional=True,
+            cardinality=Cardinality.MANY,
+            on_delete=RelationshipDeleteBehavior.CASCADE,
+        ),
+        Rel(
+            name="created_by",
+            peer=InfrahubKind.GENERICACCOUNT,
+            identifier="thread__account",
+            optional=True,
+            branch=BranchSupportType.AGNOSTIC,
+            cardinality=Cardinality.ONE,
+        ),
+    ],
+)
 
-core_file_thread = {
-    "name": "FileThread",
-    "namespace": "Core",
-    "description": "A thread related to a file on a proposed change",
-    "include_in_menu": False,
-    "label": "Thread - File",
-    "branch": BranchSupportType.AGNOSTIC.value,
-    "inherit_from": [InfrahubKind.THREAD],
-    "generate_profile": False,
-    "attributes": [
-        {"name": "file", "kind": "Text", "optional": True},
-        {"name": "commit", "kind": "Text", "optional": True},
-        {"name": "line_number", "kind": "Number", "optional": True},
-    ],
-    "relationships": [
-        {
-            "name": "repository",
-            "peer": InfrahubKind.REPOSITORY,
-            "optional": False,
-            "cardinality": "one",
-            "branch": BranchSupportType.AGNOSTIC.value,
-        },
-    ],
-}
+core_change_thread = NodeSchema(
+    name="ChangeThread",
+    namespace="Core",
+    description="A thread on proposed change",
+    include_in_menu=False,
+    label="Change Thread",
+    branch=BranchSupportType.AGNOSTIC,
+    inherit_from=[InfrahubKind.THREAD],
+    generate_profile=False,
+)
 
-core_artifact_thread = {
-    "name": "ArtifactThread",
-    "namespace": "Core",
-    "description": "A thread related to an artifact on a proposed change",
-    "include_in_menu": False,
-    "label": "Thread - Artifact",
-    "branch": BranchSupportType.AGNOSTIC.value,
-    "inherit_from": [InfrahubKind.THREAD],
-    "generate_profile": False,
-    "attributes": [
-        {"name": "artifact_id", "kind": "Text", "optional": True},
-        {"name": "storage_id", "kind": "Text", "optional": True},
-        {"name": "line_number", "kind": "Number", "optional": True},
+core_file_thread = NodeSchema(
+    name="FileThread",
+    namespace="Core",
+    description="A thread related to a file on a proposed change",
+    include_in_menu=False,
+    label="Thread - File",
+    branch=BranchSupportType.AGNOSTIC,
+    inherit_from=[InfrahubKind.THREAD],
+    generate_profile=False,
+    attributes=[
+        Attr(name="file", kind="Text", optional=True),
+        Attr(name="commit", kind="Text", optional=True),
+        Attr(name="line_number", kind="Number", optional=True),
     ],
-    "relationships": [],
-}
+    relationships=[
+        Rel(
+            name="repository",
+            peer=InfrahubKind.REPOSITORY,
+            optional=False,
+            cardinality=Cardinality.ONE,
+            branch=BranchSupportType.AGNOSTIC,
+        ),
+    ],
+)
 
-core_object_thread = {
-    "name": "ObjectThread",
-    "namespace": "Core",
-    "description": "A thread related to an object on a proposed change",
-    "include_in_menu": False,
-    "label": "Thread - Object",
-    "branch": BranchSupportType.AGNOSTIC.value,
-    "inherit_from": [InfrahubKind.THREAD],
-    "generate_profile": False,
-    "attributes": [
-        {"name": "object_path", "kind": "Text", "optional": False},
+core_artifact_thread = NodeSchema(
+    name="ArtifactThread",
+    namespace="Core",
+    description="A thread related to an artifact on a proposed change",
+    include_in_menu=False,
+    label="Thread - Artifact",
+    branch=BranchSupportType.AGNOSTIC,
+    inherit_from=[InfrahubKind.THREAD],
+    generate_profile=False,
+    attributes=[
+        Attr(name="artifact_id", kind="Text", optional=True),
+        Attr(name="storage_id", kind="Text", optional=True),
+        Attr(name="line_number", kind="Number", optional=True),
     ],
-    "relationships": [],
-}
+)
 
-core_change_comment = {
-    "name": "ChangeComment",
-    "namespace": "Core",
-    "description": "A comment on proposed change",
-    "include_in_menu": False,
-    "label": "Change Comment",
-    "default_filter": "text__value",
-    "display_labels": ["text__value"],
-    "branch": BranchSupportType.AGNOSTIC.value,
-    "inherit_from": [InfrahubKind.COMMENT],
-    "generate_profile": False,
-    "relationships": [
-        {
-            "name": "change",
-            "kind": "Parent",
-            "peer": InfrahubKind.PROPOSEDCHANGE,
-            "cardinality": "one",
-            "optional": False,
-        },
+core_object_thread = NodeSchema(
+    name="ObjectThread",
+    namespace="Core",
+    description="A thread related to an object on a proposed change",
+    include_in_menu=False,
+    label="Thread - Object",
+    branch=BranchSupportType.AGNOSTIC,
+    inherit_from=[InfrahubKind.THREAD],
+    generate_profile=False,
+    attributes=[
+        Attr(name="object_path", kind="Text", optional=False),
     ],
-}
+)
 
-core_thread_comment = {
-    "name": "ThreadComment",
-    "namespace": "Core",
-    "description": "A comment on thread within a Proposed Change",
-    "include_in_menu": False,
-    "label": "Thread Comment",
-    "default_filter": "text__value",
-    "display_labels": ["text__value"],
-    "branch": BranchSupportType.AGNOSTIC.value,
-    "inherit_from": [InfrahubKind.COMMENT],
-    "generate_profile": False,
-    "attributes": [],
-    "relationships": [
-        {
-            "name": "thread",
-            "peer": InfrahubKind.THREAD,
-            "kind": "Parent",
-            "identifier": "thread__threadcomment",
-            "cardinality": "one",
-            "optional": False,
-        },
+core_change_comment = NodeSchema(
+    name="ChangeComment",
+    namespace="Core",
+    description="A comment on proposed change",
+    include_in_menu=False,
+    label="Change Comment",
+    default_filter="text__value",
+    display_labels=["text__value"],
+    branch=BranchSupportType.AGNOSTIC,
+    inherit_from=[InfrahubKind.COMMENT],
+    generate_profile=False,
+    relationships=[
+        Rel(
+            name="change",
+            kind=RelKind.PARENT,
+            peer=InfrahubKind.PROPOSEDCHANGE,
+            cardinality=Cardinality.ONE,
+            optional=False,
+        ),
     ],
-}
+)
+
+core_thread_comment = NodeSchema(
+    name="ThreadComment",
+    namespace="Core",
+    description="A comment on thread within a Proposed Change",
+    include_in_menu=False,
+    label="Thread Comment",
+    default_filter="text__value",
+    display_labels=["text__value"],
+    branch=BranchSupportType.AGNOSTIC,
+    inherit_from=[InfrahubKind.COMMENT],
+    generate_profile=False,
+    attributes=[],
+    relationships=[
+        Rel(
+            name="thread",
+            peer=InfrahubKind.THREAD,
+            kind=RelKind.PARENT,
+            identifier="thread__threadcomment",
+            cardinality=Cardinality.ONE,
+            optional=False,
+        ),
+    ],
+)
--- backend/infrahub/core/schema/definitions/core/resource_pool.py
+++ backend/infrahub/core/schema/definitions/core/resource_pool.py
@@ -2,179 +2,169 @@ from infrahub.core.constants import (
     BranchSupportType,
     InfrahubKind,
 )
+from infrahub.core.constants import RelationshipCardinality as Cardinality
+from infrahub.core.constants import RelationshipKind as RelKind
 
-core_resource_pool = {
-    "name": "ResourcePool",
-    "namespace": "Core",
-    "label": "Resource Pool",
-    "description": "The resource manager contains pools of resources to allow for automatic assignments.",
-    "include_in_menu": False,
-    "default_filter": "name__value",
-    "order_by": ["name__value"],
-    "display_labels": ["name__value"],
-    "human_friendly_id": ["name__value"],
-    "icon": "mdi:view-grid-outline",
-    "branch": BranchSupportType.AGNOSTIC.value,
-    "uniqueness_constraints": [["name__value"]],
-    "generate_profile": False,
-    "attributes": [
-        {
-            "name": "name",
-            "kind": "Text",
-            "order_weight": 1000,
-            "unique": True,
-        },
-        {
-            "name": "description",
-            "kind": "Text",
-            "optional": True,
-            "order_weight": 2000,
-        },
-    ],
-}
+from ...attribute_schema import AttributeSchema as Attr
+from ...generic_schema import GenericSchema
+from ...node_schema import NodeSchema
+from ...relationship_schema import (
+    RelationshipSchema as Rel,
+)
 
-core_ip_prefix_pool = {
-    "name": "IPPrefixPool",
-    "namespace": "Core",
-    "description": "A pool of IP prefix resources",
-    "label": "IP Prefix Pool",
-    "include_in_menu": False,
-    "branch": BranchSupportType.AGNOSTIC.value,
-    "generate_profile": False,
-    "inherit_from": [InfrahubKind.RESOURCEPOOL, InfrahubKind.LINEAGESOURCE],
-    "attributes": [
-        {
-            "name": "default_prefix_length",
-            "kind": "Number",
-            "description": "The default prefix length as an integer for prefixes allocated from this pool.",
-            "optional": True,
-            "order_weight": 5000,
-        },
-        {
-            "name": "default_member_type",
-            "kind": "Text",
-            "enum": ["prefix", "address"],
-            "default_value": "prefix",
-            "optional": True,
-            "order_weight": 3000,
-        },
-        {
-            "name": "default_prefix_type",
-            "kind": "Text",
-            "optional": True,
-            "order_weight": 4000,
-        },
+core_resource_pool = GenericSchema(
+    name="ResourcePool",
+    namespace="Core",
+    label="Resource Pool",
+    description="The resource manager contains pools of resources to allow for automatic assignments.",
+    include_in_menu=False,
+    default_filter="name__value",
+    order_by=["name__value"],
+    display_labels=["name__value"],
+    human_friendly_id=["name__value"],
+    icon="mdi:view-grid-outline",
+    branch=BranchSupportType.AGNOSTIC,
+    uniqueness_constraints=[["name__value"]],
+    generate_profile=False,
+    attributes=[
+        Attr(name="name", kind="Text", order_weight=1000, unique=True),
+        Attr(name="description", kind="Text", optional=True, order_weight=2000),
     ],
-    "relationships": [
-        {
-            "name": "resources",
-            "peer": "BuiltinIPPrefix",
-            "kind": "Attribute",
-            "identifier": "prefixpool__resource",
-            "cardinality": "many",
-            "branch": BranchSupportType.AGNOSTIC.value,
-            "optional": False,
-            "order_weight": 6000,
-        },
-        {
-            "name": "ip_namespace",
-            "peer": "BuiltinIPNamespace",
-            "kind": "Attribute",
-            "identifier": "prefixpool__ipnamespace",
-            "cardinality": "one",
-            "branch": BranchSupportType.AGNOSTIC.value,
-            "optional": False,
-            "order_weight": 7000,
-        },
-    ],
-}
+)
 
-core_ip_address_pool = {
-    "name": "IPAddressPool",
-    "namespace": "Core",
-    "description": "A pool of IP address resources",
-    "label": "IP Address Pool",
-    "include_in_menu": False,
-    "branch": BranchSupportType.AGNOSTIC.value,
-    "generate_profile": False,
-    "inherit_from": [InfrahubKind.RESOURCEPOOL, InfrahubKind.LINEAGESOURCE],
-    "attributes": [
-        {
-            "name": "default_address_type",
-            "kind": "Text",
-            "optional": False,
-            "description": "The object type to create when reserving a resource in the pool",
-            "order_weight": 3000,
-        },
-        {
-            "name": "default_prefix_length",
-            "kind": "Number",
-            "description": "The default prefix length as an integer for addresses allocated from this pool.",
-            "optional": True,
-            "order_weight": 4000,
-        },
+core_ip_prefix_pool = NodeSchema(
+    name="IPPrefixPool",
+    namespace="Core",
+    description="A pool of IP prefix resources",
+    label="IP Prefix Pool",
+    include_in_menu=False,
+    branch=BranchSupportType.AGNOSTIC,
+    generate_profile=False,
+    inherit_from=[InfrahubKind.RESOURCEPOOL, InfrahubKind.LINEAGESOURCE],
+    attributes=[
+        Attr(
+            name="default_prefix_length",
+            kind="Number",
+            description="The default prefix length as an integer for prefixes allocated from this pool.",
+            optional=True,
+            order_weight=5000,
+        ),
+        Attr(
+            name="default_member_type",
+            kind="Text",
+            enum=["prefix", "address"],
+            default_value="prefix",
+            optional=True,
+            order_weight=3000,
+        ),
+        Attr(name="default_prefix_type", kind="Text", optional=True, order_weight=4000),
     ],
-    "relationships": [
-        {
-            "name": "resources",
-            "peer": "BuiltinIPPrefix",
-            "kind": "Attribute",
-            "identifier": "ipaddresspool__resource",
-            "cardinality": "many",
-            "branch": BranchSupportType.AGNOSTIC.value,
-            "optional": False,
-            "order_weight": 5000,
-        },
-        {
-            "name": "ip_namespace",
-            "peer": "BuiltinIPNamespace",
-            "kind": "Attribute",
-            "identifier": "ipaddresspool__ipnamespace",
-            "cardinality": "one",
-            "branch": BranchSupportType.AGNOSTIC.value,
-            "optional": False,
-            "order_weight": 6000,
-        },
+    relationships=[
+        Rel(
+            name="resources",
+            peer=InfrahubKind.IPPREFIX,
+            kind=RelKind.ATTRIBUTE,
+            identifier="prefixpool__resource",
+            cardinality=Cardinality.MANY,
+            branch=BranchSupportType.AGNOSTIC,
+            optional=False,
+            order_weight=6000,
+        ),
+        Rel(
+            name="ip_namespace",
+            peer=InfrahubKind.IPNAMESPACE,
+            kind=RelKind.ATTRIBUTE,
+            identifier="prefixpool__ipnamespace",
+            cardinality=Cardinality.ONE,
+            branch=BranchSupportType.AGNOSTIC,
+            optional=False,
+            order_weight=7000,
+        ),
     ],
-}
+)
 
-core_number_pool = {
-    "name": "NumberPool",
-    "namespace": "Core",
-    "description": "A pool of number resources",
-    "label": "Number Pool",
-    "include_in_menu": False,
-    "branch": BranchSupportType.AGNOSTIC.value,
-    "generate_profile": False,
-    "inherit_from": [InfrahubKind.RESOURCEPOOL, InfrahubKind.LINEAGESOURCE],
-    "attributes": [
-        {
-            "name": "node",
-            "kind": "Text",
-            "optional": False,
-            "description": "The model of the object that requires integers to be allocated",
-            "order_weight": 3000,
-        },
-        {
-            "name": "node_attribute",
-            "kind": "Text",
-            "description": "The attribute of the selected model",
-            "optional": False,
-            "order_weight": 4000,
-        },
-        {
-            "name": "start_range",
-            "kind": "Number",
-            "optional": False,
-            "description": "The start range for the pool",
-            "order_weight": 5000,
-        },
-        {
-            "name": "end_range",
-            "kind": "Number",
-            "optional": False,
-            "description": "The end range for the pool",
-            "order_weight": 6000,
-        },
+core_ip_address_pool = NodeSchema(
+    name="IPAddressPool",
+    namespace="Core",
+    description="A pool of IP address resources",
+    label="IP Address Pool",
+    include_in_menu=False,
+    branch=BranchSupportType.AGNOSTIC,
+    generate_profile=False,
+    inherit_from=[InfrahubKind.RESOURCEPOOL, InfrahubKind.LINEAGESOURCE],
+    attributes=[
+        Attr(
+            name="default_address_type",
+            kind="Text",
+            optional=False,
+            description="The object type to create when reserving a resource in the pool",
+            order_weight=3000,
+        ),
+        Attr(
+            name="default_prefix_length",
+            kind="Number",
+            optional=True,
+            description="The default prefix length as an integer for addresses allocated from this pool.",
+            order_weight=4000,
+        ),
     ],
-}
+    relationships=[
+        Rel(
+            name="resources",
+            peer=InfrahubKind.IPPREFIX,
+            kind=RelKind.ATTRIBUTE,
+            identifier="ipaddresspool__resource",
+            cardinality=Cardinality.MANY,
+            branch=BranchSupportType.AGNOSTIC,
+            optional=False,
+            order_weight=5000,
+        ),
+        Rel(
+            name="ip_namespace",
+            peer=InfrahubKind.IPNAMESPACE,
+            kind=RelKind.ATTRIBUTE,
+            identifier="ipaddresspool__ipnamespace",
+            cardinality=Cardinality.ONE,
+            branch=BranchSupportType.AGNOSTIC,
+            optional=False,
+            order_weight=6000,
+        ),
+    ],
+)
+
+core_number_pool = NodeSchema(
+    name="NumberPool",
+    namespace="Core",
+    description="A pool of number resources",
+    label="Number Pool",
+    include_in_menu=False,
+    branch=BranchSupportType.AGNOSTIC,
+    generate_profile=False,
+    inherit_from=[InfrahubKind.RESOURCEPOOL, InfrahubKind.LINEAGESOURCE],
+    attributes=[
+        Attr(
+            name="node",
+            kind="Text",
+            optional=False,
+            description="The model of the object that requires integers to be allocated",
+            order_weight=3000,
+        ),
+        Attr(
+            name="node_attribute",
+            kind="Text",
+            description="The attribute of the selected model",
+            optional=False,
+            order_weight=4000,
+        ),
+        Attr(
+            name="start_range",
+            kind="Number",
+            optional=False,
+            description="The start range for the pool",
+            order_weight=5000,
+        ),
+        Attr(
+            name="end_range", kind="Number", optional=False, description="The end range for the pool", order_weight=6000
+        ),
+    ],
+)
