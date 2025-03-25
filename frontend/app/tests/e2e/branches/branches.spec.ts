ae026861cc2286ad739f42448d21de4adeef9d59
pa-lem <pa-lem@hotmail.fr>
2025-03-25 09:28:17 +0100

    move file

--- frontend/app/tests/e2e/branches/branches.spec.ts
+++ frontend/app/tests/e2e/branches/branches.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
-import { ACCOUNT_STATE_PATH } from "../constants";
-import { createBranch } from "../utils";
+import { ACCOUNT_STATE_PATH } from "../../constants";
+import { createBranch } from "../../utils";
 
 test.describe("Branches creation and deletion", () => {
   test.describe("when not logged in", () => {
