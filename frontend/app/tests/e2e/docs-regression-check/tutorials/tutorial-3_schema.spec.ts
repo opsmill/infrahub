2360648243017e6fd22ce9adaf530e11ca15a86d
bilalabbad <bilal@opsmill.com>
2025-03-26 13:29:17 +0100

    disable test

--- frontend/app/tests/e2e/docs-regression-check/tutorials/tutorial-3_schema.spec.ts
+++ frontend/app/tests/e2e/docs-regression-check/tutorials/tutorial-3_schema.spec.ts
@@ -2,16 +2,8 @@ import { expect, test } from "@playwright/test";
 import { ACCOUNT_STATE_PATH } from "../../../constants";
 import { saveScreenshotForDocs } from "../../../utils";
 
-test.describe.fixme("Getting started with Infrahub - Data lineage and metadata", () => {
+test.describe("Getting started with Infrahub - Data lineage and metadata", () => {
   test.use({ storageState: ACCOUNT_STATE_PATH.ADMIN });
-
-  test.beforeEach(async function ({ page }) {
-    page.on("response", async (response) => {
-      if (response.status() === 500) {
-        await expect(response.url()).toBe("This URL responded with a 500 status");
-      }
-    });
-  });
 
   test("1. Visualize the active schema", async ({ page }) => {
     await page.goto("/");
