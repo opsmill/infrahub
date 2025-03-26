8a4e531f4491da2a74fae9738486ca83f38f19bc
Lucas Guillermou <lucas@opsmill.com>
2025-03-26 15:54:20 +0100

    Disable duplicated addresses migration

--- backend/infrahub/core/migrations/graph/__init__.py
+++ backend/infrahub/core/migrations/graph/__init__.py
@@ -24,7 +24,6 @@ from .m019_restore_rels_to_time import Migration019
 from .m020_duplicate_edges import Migration020
 from .m021_missing_hierarchy_merge import Migration021
 from .m022_add_generate_template_attr import Migration022
-from .m023_deduplicate_cardinality_one_relationships import Migration023
 
 if TYPE_CHECKING:
     from infrahub.core.root import Root
@@ -54,7 +53,7 @@ MIGRATIONS: list[type[GraphMigration | InternalSchemaMigration | ArbitraryMigrat
     Migration020,
     Migration021,
     Migration022,
-    Migration023,
+    # Migration023,  Enable this migration once it has been tested on bigger databases
 ]
 
 
