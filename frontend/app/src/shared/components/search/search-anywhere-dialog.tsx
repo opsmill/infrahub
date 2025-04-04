e6cf81d1801109411ccb6eabfa20963f85121dca
Bilal ABBAD <bilal@opsmill.com>
2025-04-04 14:51:38 +0200

    Use react-aria dialog in search anywhere (#6236)

--- frontend/app/src/shared/components/search/search-anywhere-dialog.tsx
+++ frontend/app/src/shared/components/search/search-anywhere-dialog.tsx
@@ -1,35 +1,35 @@
 import { useSearchAnywhereContext } from "@/shared/components/search/search-anywhere-context";
 import { classNames } from "@/shared/utils/common";
-import { Command } from "cmdk";
-import React from "react";
+import { Dialog, DialogProps, Modal, ModalOverlay } from "react-aria-components";
 
-export function SearchAnywhereDialog({
-  children,
-  ...props
-}: React.ComponentProps<typeof Command.Dialog>) {
+export function SearchAnywhereDialog({ children, className, ...props }: DialogProps) {
   const { isOpen, setIsOpen } = useSearchAnywhereContext();
 
   return (
-    <Command.Dialog
-      {...props}
-      open={isOpen}
+    <ModalOverlay
+      isDismissable
+      isOpen={isOpen}
       onOpenChange={setIsOpen}
-      overlayClassName={classNames(
-        "fixed inset-0 z-50 bg-gray-600/25",
-        "data-[state=open]:animate-in data-[state=open]:fade-in-0",
-        "data-[state=closed]:animate-out data-[state=closed]:fade-out-0"
+      className={classNames(
+        "absolute overflow-auto inset-0 z-50 bg-gray-600/25",
+        "data-[entering]:animate-in data-[entering]:fade-in-0",
+        "data-[exiting]:duration-300 data-[exiting]:animate-out data-[exiting]:fade-out-0"
       )}
-      contentClassName={classNames(
-        "fixed top-1 left-[50%] translate-x-[-50%] z-50 grid w-full max-w-screen-md gap-4 border bg-stone-100 p-2 shadow-lg rounded-xl duration-200",
-        "data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
-        "data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%]"
-      )}
-      data-testid="search-anywhere"
-      className="overflow-hidden"
-      shouldFilter={false}
-      label="Search anywhere"
+      {...props}
     >
-      {children}
-    </Command.Dialog>
+      <Modal
+        className={classNames(
+          "fixed top-1 left-1/2 -translate-x-1/2 z-50 grid w-full max-w-screen-md gap-4 border bg-stone-100 p-2 shadow-lg rounded-xl duration-200",
+          "data-[entering]:animate-in data-[entering]:fade-in-0 data-[entering]:zoom-in-95 data-[entering]:slide-in-from-left-1/2",
+          "data-[exiting]:duration-300 data-[exiting]:animate-out data-[exiting]:fade-out-0 data-[exiting]:zoom-out-95 data-[exiting]:slide-out-to-left-1/2",
+          className
+        )}
+        {...props}
+      >
+        <Dialog aria-label="Search anywhere" data-testid="search-anywhere">
+          {children}
+        </Dialog>
+      </Modal>
+    </ModalOverlay>
   );
 }
