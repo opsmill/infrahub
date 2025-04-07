afddfc6fd1f024e8bc5eced2383eeb256f489624
Fatih Acar <fatih@opsmill.com>
2025-04-07 20:22:34 +0200

    fix(backend): reintroduce attribute value indexes
    This should significantly make node creation faster using this
    index because it solves the issue with the MERGE statement within the
    query node_create_all.
    
    Signed-off-by: Fatih Acar <fatih@opsmill.com>

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -23,6 +23,7 @@ from infrahub.config import load_and_exit
 from infrahub.core import registry
 from infrahub.core.branch import Branch
 from infrahub.core.constants import BranchSupportType, InfrahubKind, RelationshipCardinality, RelationshipDirection
+from infrahub.core.graph.index import node_indexes, rel_indexes
 from infrahub.core.initialization import (
     create_branch,
     create_default_branch,
@@ -117,6 +118,8 @@ async def db(
             config.SETTINGS.database.port = memgraph[PORT_MEMGRAPH]
 
     driver = InfrahubDatabase(driver=await get_db(retry=5))
+    driver.manager.index.init(nodes=node_indexes, rels=rel_indexes)
+    await driver.manager.index.add()
 
     yield driver
 
