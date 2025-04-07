fdce10e77532f04056386005cc3575cb4429f691
bilalabbad <bilal@opsmill.com>
2025-04-07 17:31:38 +0200

    use cva on label

--- frontend/app/src/shared/components/aria/label.tsx
+++ frontend/app/src/shared/components/aria/label.tsx
@@ -1,14 +1,12 @@
 import { classNames } from "@/shared/utils/common";
+import { cva } from "class-variance-authority";
 import { Label as AriaLabel, LabelProps as AriaLabelProps } from "react-aria-components";
 
+export const labelVariants = cva([
+  "text-sm font-medium leading-none text-gray-900 cursor-pointer",
+  "data-[disabled]:cursor-not-allowed data-[disabled]:opacity-70",
+]);
+
 export function Label({ className, ...props }: AriaLabelProps) {
-  return (
-    <AriaLabel
-      className={classNames(
-        "text-sm font-medium text-gray-900 cursor-pointer peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-        className
-      )}
-      {...props}
-    />
-  );
+  return <AriaLabel className={classNames(labelVariants(), className)} {...props} />;
 }
