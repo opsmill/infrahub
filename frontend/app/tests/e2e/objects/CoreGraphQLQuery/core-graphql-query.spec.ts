6c07b561b2332e1a3421b39f90d6e91e74324c33
pa-lem <pa-lem@hotmail.fr>
2025-04-10 09:41:34 +0200

    update check

--- frontend/app/tests/e2e/objects/CoreGraphQLQuery/core-graphql-query.spec.ts
+++ frontend/app/tests/e2e/objects/CoreGraphQLQuery/core-graphql-query.spec.ts
@@ -17,7 +17,7 @@ test.describe("/objects/CoreGraphQLQuery/:graphqlQueryId - GraphQL Query details
     await test.step("Navigate to CoreGraphQLQuery page", async () => {
       await page.goto("/objects/CoreGraphQLQuery");
       await expect(page.getByRole("heading", { name: "GraphQL Query" })).toBeVisible();
-      await expect(page.getByTestId("object-table-skeleton").first()).toBeHidden();
+      await expect(page.getByText("check_backbone_link_redundancy")).toBeVisible();
     });
 
     await test.step("Create a new graphql query", async () => {
