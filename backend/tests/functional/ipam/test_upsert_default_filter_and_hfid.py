c1cd1f849cc3a460d9935a0053ae7ee4c468d4f4
Lucas Guillermou <lucas@opsmill.com>
2025-04-14 10:54:25 +0200

    Fix upsert with both default_filter and hfid defined

---
+++ backend/tests/functional/ipam/test_upsert_default_filter_and_hfid.py
@@ -0,0 +1,37 @@
+import logging
+
+from infrahub_sdk import InfrahubClient
+
+from tests.helpers.test_app import TestInfrahubApp
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+schema = {
+    "version": "1.0",
+    "nodes": [
+        {
+            "name": "Person",
+            "namespace": "Test",
+            "default_filter": "firstname__value",
+            "human_friendly_id": ["firstname__value", "lastname__value"],
+            "attributes": [
+                {"name": "firstname", "kind": "Text"},
+                {"name": "lastname", "kind": "Text"},
+            ],
+        },
+    ],
+}
+
+
+class TestUpsertWithBothHfidAndDefaulFilter(TestInfrahubApp):
+    async def test_multiple_upsert(self, client: InfrahubClient):
+        res = await client.schema.load([schema])
+        assert len(res.errors) == 0, res.errors
+
+        lastnames = ["Martin", "Dupont", "Doe"]
+        for lastname in lastnames:
+            person = await client.create("TestPerson", firstname="Clement", lastname=lastname)
+            await person.save(allow_upsert=True)
+
+        persons = await client.all(kind="TestPerson")
+        assert len(persons) == 3
---
+++ changelog/+upsertdefaultfilterandhfid.md
@@ -0,0 +1,1 @@
+Fix node upsert incorrectly updating or creating when both `human_friendly_id` and `default_filter` are defined within corresponding schema
