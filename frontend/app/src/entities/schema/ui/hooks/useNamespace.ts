17821a9977d81a67be883254f683d7a7688c5951
pa-lem <pa-lem@hotmail.fr>
2025-04-18 15:24:13 +0200

    fix import

--- frontend/app/src/entities/schema/ui/hooks/useNamespace.ts
+++ frontend/app/src/entities/schema/ui/hooks/useNamespace.ts
@@ -1,6 +1,6 @@
+import { namespacesAtom } from "@/entities/schema/stores/schema.atom";
 import { Namespace } from "@/entities/schema/types";
 import { useAtomValue } from "jotai";
-import { namespacesAtom } from "../../stores/schema.atom";
 
 export const useNamespace = (namespace: string | null | undefined): Namespace | undefined => {
   const namespaces = useAtomValue(namespacesAtom);
