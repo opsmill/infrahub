8dd3c6337c4f2f58f6d17836b162a58a1dfac590
Guillaume Mazoyer <guillaume@opsmill.com>
2025-04-23 15:40:19 +0200

    Unneeded import

--- backend/tests/integration/services/adapters/workflow/test_setup.py
+++ backend/tests/integration/services/adapters/workflow/test_setup.py
@@ -2,7 +2,7 @@ from prefect.client.orchestration import PrefectClient
 
 from infrahub.workflows.initialization import setup_task_manager
 from infrahub.workflows.models import WorkerPoolDefinition
-from tests.helpers.test_worker import TestWorkerInfrahubAsync, TestWorkerProcess
+from tests.helpers.test_worker import TestWorkerInfrahubAsync
 
 # @pytest.fixture
 # async def prefect_server(redis, prefect):
@@ -21,3 +21,15 @@ class TestTaskManagerSetup(TestWorkerInfrahubAsync):
 
         response = await prefect_client.read_work_pool(infrahubasync_worker.name)
         assert response.type == "infrahubasync"
+
+
+class TestTaskManagerSetupProcess(TestWorkerProcess):
+    async def test_setup_task_manager(self, user_worker: WorkerPoolDefinition, prefect_client: PrefectClient):
+        response = await prefect_client.read_work_pool(user_worker.name)
+        assert response.type == "process"
+
+        # Setup the task manager a second time to validate that it's idempotent
+        await setup_task_manager()
+
+        response = await prefect_client.read_work_pool(user_worker.name)
+        assert response.type == "process"
