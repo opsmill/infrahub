d851c05ae424ab85ea38f870ccecb72613e25d2e
Fatih Acar <fatih@opsmill.com>
2025-04-24 16:21:54 +0200

    feat(taskmgr): instrument API calls
    Signed-off-by: Fatih Acar <fatih@opsmill.com>

--- backend/infrahub/prefect_server/app.py
+++ backend/infrahub/prefect_server/app.py
@@ -1,7 +1,14 @@
 from __future__ import annotations
 
+import importlib.metadata
+import os
+
 from fastapi import APIRouter, FastAPI
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from prefect.server.api.server import create_app
+
+from infrahub import config
+from infrahub.trace import configure_trace
 
 from . import events
 
@@ -11,8 +18,24 @@ router.include_router(events.router)
 
 
 def create_infrahub_prefect() -> FastAPI:
+    if not config.SETTINGS.settings:
+        config_file = os.environ.get("INFRAHUB_CONFIG", "infrahub.toml")
+        config.load_and_exit(config_file_name=config_file)
+
+    # Initialize trace
+    if config.SETTINGS.trace.enable:
+        configure_trace(
+            service="prefect-server",
+            version=importlib.metadata.version("prefect"),
+            exporter_type=config.SETTINGS.trace.exporter_type,
+            exporter_endpoint=config.SETTINGS.trace.exporter_endpoint,
+            exporter_protocol=config.SETTINGS.trace.exporter_protocol,
+        )
+
     app = create_app()
     api_app: FastAPI = app.__dict__["api_app"]
     api_app.include_router(router=router)
+
+    FastAPIInstrumentor.instrument_app(api_app)
 
     return app
