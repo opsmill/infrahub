85aec0df6cfaa05fe393f6a7049c03b2d3af4a75
pa-lem <pa-lem@hotmail.fr>
2025-04-28 13:55:51 +0200

    update merge test

--- frontend/app/tests/e2e/branches/merge-branch.spec.ts
+++ frontend/app/tests/e2e/branches/merge-branch.spec.ts
@@ -1,24 +1,29 @@
 import { expect, test } from "@playwright/test";
 import { ACCOUNT_STATE_PATH } from "../../constants";
+import { generateRandomBranchName } from "../../utils";
 
 test.describe("Verify branch merge button state", () => {
   test.use({ storageState: ACCOUNT_STATE_PATH.ADMIN });
 
   test("create a branch, merge it and verify button state", async ({ page }) => {
+    const branchName = generateRandomBranchName("merge-action-test");
+
     await test.step("Create and access a new branch", async () => {
       await page.goto("/branches");
       await page.getByTestId("branch-selector-trigger").click();
       await page.getByTestId("create-branch-button").click();
-      await page.getByRole("textbox", { name: "New branch name *" }).fill("merge-action-test");
+      await page.getByRole("textbox", { name: "New branch name *" }).fill(branchName);
       await page.getByRole("button", { name: "Create a new branch" }).click();
       await expect(page.getByText("New branch name *")).not.toBeVisible();
-      await expect(page.getByTestId("branches-items").getByText("merge-action-test")).toBeVisible();
-      await page.getByTestId("branches-items").getByText("merge-action-test").click();
-      await expect(page.getByText("Namemerge-action-test")).toBeVisible();
+      await expect(page.getByTestId("branches-items").getByText(branchName)).toBeVisible();
+      await page.getByTestId("branches-items").getByText(branchName).click();
+      await expect(page.getByText(`Name${branchName}`)).toBeVisible();
       await page.getByText("Tasks").click();
     });
 
     await test.step("Merge the branch and verify button state", async () => {
+      test.slow();
+
       await page.getByRole("button", { name: "Merge", exact: true }).click();
       await expect(page.getByText("Branch merge requested!")).toBeVisible();
       await expect(page.getByText("RUNNINGMerge branch graphQL")).toBeVisible();
