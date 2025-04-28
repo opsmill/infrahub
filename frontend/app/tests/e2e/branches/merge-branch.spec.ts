e9ddc811200b83c612b2e466585e3284336481d9
pa-lem <pa-lem@hotmail.fr>
2025-04-28 14:10:51 +0200

    delete branch after test

--- frontend/app/tests/e2e/branches/merge-branch.spec.ts
+++ frontend/app/tests/e2e/branches/merge-branch.spec.ts
@@ -1,23 +1,28 @@
 import { expect, test } from "@playwright/test";
 import { ACCOUNT_STATE_PATH } from "../../constants";
 import { generateRandomBranchName } from "../../utils";
+import { deleteBranchAPI } from "../utils/graphql";
 
 test.describe("Verify branch merge button state", () => {
   test.use({ storageState: ACCOUNT_STATE_PATH.ADMIN });
 
-  test("create a branch, merge it and verify button state", async ({ page }) => {
-    const branchName = generateRandomBranchName("merge-action-test");
+  const BRANCH_NAME = generateRandomBranchName("merge-action-test");
 
+  test.afterAll(async ({ request }) => {
+    await deleteBranchAPI(request, BRANCH_NAME);
+  });
+
+  test("create a branch, merge it and verify button state", async ({ page }) => {
     await test.step("Create and access a new branch", async () => {
       await page.goto("/branches");
       await page.getByTestId("branch-selector-trigger").click();
       await page.getByTestId("create-branch-button").click();
-      await page.getByRole("textbox", { name: "New branch name *" }).fill(branchName);
+      await page.getByRole("textbox", { name: "New branch name *" }).fill(BRANCH_NAME);
       await page.getByRole("button", { name: "Create a new branch" }).click();
       await expect(page.getByText("New branch name *")).not.toBeVisible();
-      await expect(page.getByTestId("branches-items").getByText(branchName)).toBeVisible();
-      await page.getByTestId("branches-items").getByText(branchName).click();
-      await expect(page.getByText(`Name${branchName}`)).toBeVisible();
+      await expect(page.getByTestId("branches-items").getByText(BRANCH_NAME)).toBeVisible();
+      await page.getByTestId("branches-items").getByText(BRANCH_NAME).click();
+      await expect(page.getByText(`Name${BRANCH_NAME}`)).toBeVisible();
       await page.getByText("Tasks").click();
     });
 
