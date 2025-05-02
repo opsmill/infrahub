483a55e251d9980ef62084777b821316b12bd929
pa-lem <pa-lem@hotmail.fr>
2025-05-02 16:08:22 +0200

    update check

--- frontend/app/src/entities/nodes/relationships/utils/index.ts
+++ frontend/app/src/entities/nodes/relationships/utils/index.ts
@@ -24,7 +24,10 @@ export function getDissociateAction({
   });
 
   // If bidirectional, check from the peer point of view
-  if (parentRelationship?.direction === "bidirectional" && peerRelationship) {
+  if (
+    (!parentRelationship?.direction || parentRelationship?.direction === "bidirectional") &&
+    peerRelationship
+  ) {
     return parentRelationship?.optional && peerRelationship?.optional;
   }
 
