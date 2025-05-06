45cfe9790c1ee53f4a264d76662f75a273ce361b
pa-lem <pa-lem@hotmail.fr>
2025-05-06 15:57:00 +0200

    use utils function

--- frontend/app/src/entities/nodes/relationships/utils/can-dissociate-relationship.test.ts
+++ frontend/app/src/entities/nodes/relationships/utils/can-dissociate-relationship.test.ts
@@ -1,70 +1,211 @@
-import {
-  deviceMockSchema,
-  deviceRelationshipMinCountMockSchema,
-  deviceRelationshipNotOptionalMockSchema,
-} from "../../../../../tests/mocks/data/devices";
-import {
-  interfaceL2MockSchema,
-  interfaceL2WithoutDeviceMockSchema,
-} from "../../../../../tests/mocks/data/interfaces";
+import { generateNodeSchema, generateRelationshipSchema } from "../../../../../tests/fake/schema";
 import { canDissociateRelationship } from "./can-dissociate-relationship";
 
 describe("Dissociate action", () => {
   it("should be enabled from relationship schema", () => {
+    const parentSchema = generateNodeSchema({
+      kind: "Test",
+      name: "Node",
+      relationships: [
+        generateRelationshipSchema({
+          cardinality: "many",
+          name: "relationshipName",
+          peer: "TestPeer",
+        }),
+      ],
+    });
+
+    const peerSchema = generateNodeSchema({
+      kind: "Test",
+      name: "Peer",
+      relationships: [
+        generateRelationshipSchema({
+          cardinality: "many",
+        }),
+      ],
+    });
+
     const isDissociateAllowed = canDissociateRelationship({
-      relationshipName: "interfaces",
-      parentSchema: deviceMockSchema,
-      peerSchema: interfaceL2WithoutDeviceMockSchema,
+      relationshipName: "relationshipName",
+      parentSchema,
+      peerSchema,
       relationshipsCount: 0,
     });
+
     expect(isDissociateAllowed).to.eq(true);
   });
 
   it("should be enabled from peers min count", () => {
+    const parentSchema = generateNodeSchema({
+      kind: "Test",
+      name: "Node",
+      relationships: [
+        generateRelationshipSchema({
+          cardinality: "many",
+          name: "relationshipName",
+          peer: "TestPeer",
+        }),
+      ],
+    });
+
+    const peerSchema = generateNodeSchema({
+      kind: "Test",
+      name: "Peer",
+      relationships: [
+        generateRelationshipSchema({
+          cardinality: "many",
+          optional: false,
+          peer: "TestNode",
+          min_count: 2,
+        }),
+      ],
+    });
+
     const isDissociateAllowed = canDissociateRelationship({
-      relationshipName: "interfaces",
-      parentSchema: deviceRelationshipMinCountMockSchema,
-      peerSchema: interfaceL2WithoutDeviceMockSchema,
+      relationshipName: "relationshipName",
+      parentSchema,
+      peerSchema,
       relationshipsCount: 3,
     });
+
     expect(isDissociateAllowed).to.eq(true);
   });
 
   it("should be enabled from peers global count", () => {
+    const parentSchema = generateNodeSchema({
+      kind: "Test",
+      name: "Node",
+      relationships: [
+        generateRelationshipSchema({
+          cardinality: "many",
+          name: "relationshipName",
+          peer: "TestPeer",
+        }),
+      ],
+    });
+
+    const peerSchema = generateNodeSchema({
+      kind: "Test",
+      name: "Peer",
+      relationships: [
+        generateRelationshipSchema({
+          cardinality: "many",
+          optional: false,
+          peer: "TestNode",
+        }),
+      ],
+    });
+
     const isDissociateAllowed = canDissociateRelationship({
-      relationshipName: "interfaces",
-      parentSchema: deviceRelationshipNotOptionalMockSchema,
-      peerSchema: interfaceL2WithoutDeviceMockSchema,
+      relationshipName: "relationshipName",
+      parentSchema,
+      peerSchema,
       relationshipsCount: 3,
     });
     expect(isDissociateAllowed).to.eq(true);
   });
 
   it("should be disabled from relationship schema", () => {
+    const parentSchema = generateNodeSchema({
+      kind: "Test",
+      name: "Node",
+      relationships: [
+        generateRelationshipSchema({
+          cardinality: "many",
+          name: "relationshipName",
+          peer: "TestPeer",
+          optional: false,
+        }),
+      ],
+    });
+
+    const peerSchema = generateNodeSchema({
+      kind: "Test",
+      name: "Peer",
+      relationships: [
+        generateRelationshipSchema({
+          cardinality: "many",
+          optional: false,
+          peer: "TestNode",
+        }),
+      ],
+    });
+
     const isDissociateAllowed = canDissociateRelationship({
-      relationshipName: "interfaces",
-      parentSchema: deviceMockSchema,
-      peerSchema: interfaceL2MockSchema,
+      relationshipName: "relationshipName",
+      parentSchema,
+      peerSchema,
       relationshipsCount: 0,
     });
     expect(isDissociateAllowed).to.eq(false);
   });
 
   it("should be disabled from peers min count", () => {
+    const parentSchema = generateNodeSchema({
+      kind: "Test",
+      name: "Node",
+      relationships: [
+        generateRelationshipSchema({
+          cardinality: "many",
+          name: "relationshipName",
+          peer: "TestPeer",
+          optional: false,
+          min_count: 2,
+        }),
+      ],
+    });
+
+    const peerSchema = generateNodeSchema({
+      kind: "Test",
+      name: "Peer",
+      relationships: [
+        generateRelationshipSchema({
+          cardinality: "many",
+          optional: false,
+          peer: "TestNode",
+        }),
+      ],
+    });
+
     const isDissociateAllowed = canDissociateRelationship({
-      relationshipName: "interfaces",
-      parentSchema: deviceRelationshipMinCountMockSchema,
-      peerSchema: interfaceL2MockSchema,
+      relationshipName: "relationshipName",
+      parentSchema,
+      peerSchema,
       relationshipsCount: 2,
     });
     expect(isDissociateAllowed).to.eq(false);
   });
 
   it("should be disabled from peers global count", () => {
+    const parentSchema = generateNodeSchema({
+      kind: "Test",
+      name: "Node",
+      relationships: [
+        generateRelationshipSchema({
+          cardinality: "many",
+          name: "relationshipName",
+          peer: "TestPeer",
+          optional: false,
+        }),
+      ],
+    });
+
+    const peerSchema = generateNodeSchema({
+      kind: "Test",
+      name: "Peer",
+      relationships: [
+        generateRelationshipSchema({
+          cardinality: "many",
+          optional: false,
+          peer: "TestNode",
+        }),
+      ],
+    });
+
     const isDissociateAllowed = canDissociateRelationship({
-      relationshipName: "interfaces",
-      parentSchema: deviceRelationshipNotOptionalMockSchema,
-      peerSchema: interfaceL2MockSchema,
+      relationshipName: "relationshipName",
+      parentSchema,
+      peerSchema,
       relationshipsCount: 0,
     });
     expect(isDissociateAllowed).to.eq(false);
