b61f55b5b5012d86b4e83a6c72f602a5196cd99a
bilalabbad <bilal@opsmill.com>
2025-05-13 13:16:19 +0200

    Migrate from useObjectDetails to useGetObject (part 2)

--- frontend/app/src/entities/artifacts/ui/artifact-details.tsx
+++ frontend/app/src/entities/artifacts/ui/artifact-details.tsx
@@ -1,11 +1,11 @@
 import { CONFIG } from "@/config/config";
 import { ArtifactFile } from "@/entities/artifacts/ui/artifact-file";
 import { NodeEvents } from "@/entities/events/ui/node-details-events";
-import { useObjectDetails } from "@/entities/nodes/hooks/useObjectDetails";
+import { useGetObject } from "@/entities/nodes/object/domain/get-object.query";
 import { NodeDescription } from "@/entities/nodes/object/ui/node-description";
 import { ModelSchema } from "@/entities/schema/types";
+import { CoreArtifact } from "@/shared/api/graphql/generated/graphql";
 import ErrorScreen from "@/shared/components/errors/error-screen";
-import NoDataFound from "@/shared/components/errors/no-data-found";
 import Content from "@/shared/components/layout/content";
 import { LoadingIndicator } from "@/shared/components/loading/loading-indicator";
 import { Card } from "@/shared/components/ui/card";
@@ -19,20 +19,20 @@ export interface ArtifactsDetailsProps {
 
 export function ArtifactsDetails({ artifactId, artifactSchema }: ArtifactsDetailsProps) {
   const artifactKind = artifactSchema.kind as string;
-  const { loading, error, data } = useObjectDetails(artifactSchema, artifactId);
+  const { isPending, error, data } = useGetObject({
+    objectSchema: artifactSchema,
+    objectId: artifactId,
+  });
 
-  if (loading) {
+  if (isPending) {
     return <LoadingIndicator className="h-full" />;
   }
 
   if (error) {
-    return <ErrorScreen message={`Something went wrong when fetching artifact ${artifactId}`} />;
+    return <ErrorScreen message={error.message} />;
   }
 
-  const objectDetailsData = data?.[artifactKind]?.edges?.[0]?.node;
-  if (!objectDetailsData) {
-    return <NoDataFound message={`No artifact found with id ${artifactId}`} />;
-  }
+  const objectDetailsData = data as unknown as CoreArtifact;
 
   const fileUrl: string = CONFIG.ARTIFACTS_CONTENT_URL(objectDetailsData?.storage_id?.value);
   const contentType = objectDetailsData.content_type?.value;
