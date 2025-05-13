d1873cab0a1a24cbf017a348be0c32ad707d54da
bilalabbad <bilal@opsmill.com>
2025-05-13 14:34:20 +0200

    Fix handlebars crashing causing by helpers not being initialized

--- frontend/app/src/entities/proposed-changes/api/getProposedChangesObjectThreads.ts
+++ frontend/app/src/entities/proposed-changes/api/getProposedChangesObjectThreads.ts
@@ -1,4 +1,4 @@
-import Handlebars from "handlebars";
+import Handlebars from "@/shared/libs/handlebars";
 
 export const getProposedChangesObjectThreads = Handlebars.compile(`
 query getProposedChangesThreadsFor{{kind}} {
