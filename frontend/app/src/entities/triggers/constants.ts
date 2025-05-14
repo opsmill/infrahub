9a23fae88ece80a1f6a1e87b0430896c8f0188e0
pa-lem <pa-lem@hotmail.fr>
2025-05-14 15:50:27 +0200

    add constants

---
+++ frontend/app/src/entities/triggers/constants.ts
@@ -0,0 +1,3 @@
+export const NODE_TRIGGER_RULE = "CoreNodeTriggerRule";
+export const NODE_TRIGGER_ATTRIBUTE = "CoreNodeTriggerAttributeMatch";
+export const NODE_TRIGGER_RELATIONSHIP = "CoreNodeTriggerRelationshipMatch";
