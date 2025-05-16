deaadb984e5dc82de1373de67d6482ad82840cc3
pa-lem <pa-lem@hotmail.fr>
2025-05-16 21:33:56 +0200

    remove const

--- frontend/app/src/entities/triggers/constants.ts
+++ frontend/app/src/entities/triggers/constants.ts
@@ -1,3 +1,2 @@
-export const NODE_TRIGGER_RULE = "CoreNodeTriggerRule";
 export const NODE_TRIGGER_ATTRIBUTE_MATCH = "CoreNodeTriggerAttributeMatch";
 export const NODE_TRIGGER_RELATIONSHIP = "CoreNodeTriggerRelationshipMatch";
