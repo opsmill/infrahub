6f8253caeee6a37806b74b6cb5c5ed9c34d8874b
Bilal ABBAD <bilal@opsmill.com>
2025-05-22 09:39:03 +0200

    Migrated menu to react-aria-components (#6504)
    * Migrated menu to react-aria-components

--- frontend/app/src/shared/components/menu/object-details-button.tsx
+++ frontend/app/src/shared/components/menu/object-details-button.tsx
@@ -1,16 +1,18 @@
 import TasksStatusIcon from "@/assets/icons/tasks-status.svg?react";
 import { QSP } from "@/config/qsp";
 import { constructPath } from "@/shared/api/rest/fetch";
-import { Button, ButtonProps } from "@/shared/components/buttons/button-primitive";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/shared/components/ui/dropdown-menu";
+  CopyToClipboardMenuItem,
+  Menu,
+  MenuHeader,
+  MenuItem,
+  MenuPopover,
+  MenuSection,
+  MenuTrigger,
+} from "@/shared/components/aria/menu";
+import { Button, ButtonProps } from "@/shared/components/buttons/button-primitive";
 import { Icon } from "@iconify-icon/react";
-import { Link } from "react-router";
-import { CopyToClipboard } from "../buttons/copy-to-clipboard";
+import { Pressable } from "react-aria-components";
 
 export interface ObjectDetailsButtonProps extends ButtonProps {
   id: string;
@@ -25,44 +27,42 @@ export const ObjectDetailsButton = ({ id, hfid, children, ...props }: ObjectDeta
   };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+    <MenuTrigger>
+      <Pressable>
         <Button variant="ghost" size={"sm"} {...props}>
           <Icon icon={"mdi:dots-vertical"} />
         </Button>
-      </DropdownMenuTrigger>
+      </Pressable>
 
-      <DropdownMenuContent align="end" className="z-10">
-        <DropdownMenuItem className="p-0">
-          <CopyToClipboard size={"default"} className="grow justify-start gap-2 p-2" text={id}>
-            Copy ID
-          </CopyToClipboard>
-        </DropdownMenuItem>
+      <MenuPopover placement="bottom end">
+        <Menu>
+          <MenuSection>
+            <MenuHeader>Actions</MenuHeader>
+            <CopyToClipboardMenuItem textToCopy={id}>Copy ID</CopyToClipboardMenuItem>
 
-        {hfid && hfid !== "null" && (
-          <DropdownMenuItem className="p-0">
-            <CopyToClipboard size={"default"} className="grow justify-start gap-2 p-2" text={hfid}>
-              Copy HFID
-            </CopyToClipboard>
-          </DropdownMenuItem>
-        )}
+            {hfid && hfid !== "null" && (
+              <CopyToClipboardMenuItem textToCopy={hfid}>Copy HFID</CopyToClipboardMenuItem>
+            )}
 
-        {children}
+            {children}
+          </MenuSection>
 
-        <DropdownMenuItem asChild>
-          <Link
-            to={constructPath("/tasks", [
-              { name: QSP.FILTER, value: JSON.stringify([taskFilter]) },
-            ])}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <TasksStatusIcon />
-            Tasks
-            <Icon icon="mdi:open-in-new" />
-          </Link>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <MenuSection>
+            <MenuHeader>Go to</MenuHeader>
+            <MenuItem
+              href={constructPath("/tasks", [
+                { name: QSP.FILTER, value: JSON.stringify([taskFilter]) },
+              ])}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <TasksStatusIcon width="12" height="12" />
+              Tasks
+              <Icon icon="mdi:open-in-new" />
+            </MenuItem>
+          </MenuSection>
+        </Menu>
+      </MenuPopover>
+    </MenuTrigger>
   );
 };
