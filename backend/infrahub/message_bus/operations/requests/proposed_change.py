5b1fa49551b7adbe5d8b74a62d1233ae9bc37e2c
Aaron McCarty <aaron@opsmill.com>
2025-06-02 11:44:35 -0700

    remove txn for diff calculation in pc pipeline

--- backend/infrahub/message_bus/operations/requests/proposed_change.py
+++ backend/infrahub/message_bus/operations/requests/proposed_change.py
@@ -105,11 +105,11 @@ async def pipeline(message: messages.RequestProposedChangePipeline, service: Inf
 
     await _gather_repository_repository_diffs(repositories=repositories, service=service)
 
-    async with service.database.start_transaction() as dbt:
-        destination_branch = await registry.get_branch(db=dbt, branch=message.destination_branch)
-        source_branch = await registry.get_branch(db=dbt, branch=message.source_branch)
+    async with service.database.start_session() as dbs:
+        destination_branch = await registry.get_branch(db=dbs, branch=message.destination_branch)
+        source_branch = await registry.get_branch(db=dbs, branch=message.source_branch)
         component_registry = get_component_registry()
-        diff_coordinator = await component_registry.get_component(DiffCoordinator, db=dbt, branch=source_branch)
+        diff_coordinator = await component_registry.get_component(DiffCoordinator, db=dbs, branch=source_branch)
         await diff_coordinator.update_branch_diff(base_branch=destination_branch, diff_branch=source_branch)
 
     diff_summary = await service.client.get_diff_summary(branch=message.source_branch)
