0c58c4c777443273ff5795a8161ee6ad4112acc3
pa-lem <pa-lem@hotmail.fr>
2025-06-05 15:32:39 +0200

    prevent dissociate in table toolabr if not possible

--- frontend/app/src/entities/nodes/object/ui/object-table/toolbar/object-table-toolbar.tsx
+++ frontend/app/src/entities/nodes/object/ui/object-table/toolbar/object-table-toolbar.tsx
@@ -43,7 +43,7 @@ export function ObjectTableToolbar({
 
       {renderMore && (
         <>
-          <ToolbarDivider />
+          {renderMore({ selectedRows }) && <ToolbarDivider />}
           {renderMore({ selectedRows })}
         </>
       )}
