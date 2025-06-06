33d142afce54cfb74c7974cf9568346198dfbee2
Guillaume Mazoyer <guillaume@opsmill.com>
2025-06-06 11:54:34 +0200

    Fix dependency for numberpool lifecyle test

--- backend/tests/functional/pools/test_numberpool_lifecycle.py
+++ backend/tests/functional/pools/test_numberpool_lifecycle.py
@@ -16,6 +16,7 @@ from infrahub.pools.registration import get_branches_with_schema_number_pool
 from infrahub.pools.tasks import validate_schema_number_pools
 from infrahub.services import InfrahubServices
 from infrahub.services.adapters.cache.redis import RedisCache
+from infrahub.workers.dependencies import build_cache
 from tests.helpers.test_app import TestInfrahubApp
 
 if TYPE_CHECKING:
@@ -53,8 +54,9 @@ class TestMutationGenerator(TestInfrahubApp):
         client: InfrahubClient,
         bus_simulator: BusSimulator,
         prefect_test_fixture,
+        dependency_provider,
     ) -> None:
-        bus_simulator.service._cache = RedisCache()
+        dependency_provider.override(build_cache, RedisCache)
 
         schema = {"version": "1.0", "nodes": [node_schema_definition]}
         schema_load_response = await client.schema.load(schemas=[schema], wait_until_converged=True)
