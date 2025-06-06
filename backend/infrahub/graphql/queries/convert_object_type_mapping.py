cbf6599ecf04e4b04263e20e82babe42e104864c
LucasG0 <44552904+LucasG0@users.noreply.github.com>
2025-06-06 10:48:18 +0200

    Remove branch parameter from ConvertObjectType mutation (#6616)

--- backend/infrahub/graphql/queries/convert_object_type_mapping.py
+++ backend/infrahub/graphql/queries/convert_object_type_mapping.py
@@ -12,13 +12,12 @@ class FieldsMapping(ObjectType):
 
 async def fields_mapping_type_conversion_resolver(
     root: dict,  # noqa: ARG001
-    info: GraphQLResolveInfo,  # noqa: ARG001
+    info: GraphQLResolveInfo,
     source_kind: str,
     target_kind: str,
-    branch: str,
 ) -> dict:
-    source_schema = registry.get_node_schema(name=source_kind, branch=branch)
-    target_schema = registry.get_node_schema(name=target_kind, branch=branch)
+    source_schema = registry.get_node_schema(name=source_kind, branch=info.context.branch)
+    target_schema = registry.get_node_schema(name=target_kind, branch=info.context.branch)
 
     mapping = get_schema_mapping(source_schema=source_schema, target_schema=target_schema)
     mapping_dict = {field_name: model.model_dump(mode="json") for field_name, model in mapping.items()}
@@ -29,7 +28,6 @@ FieldsMappingTypeConversion = Field(
     FieldsMapping,
     source_kind=String(),
     target_kind=String(),
-    branch=String(),
     description="Retrieve fields mapping for converting object type",
     resolver=fields_mapping_type_conversion_resolver,
     required=True,
