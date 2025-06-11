b8e3749983d1b260b2e85e711a70c1a1d0bda524
wvandeun <wim@opsmill.com>
2025-06-11 18:56:09 +0200

    add changelog and release notes

--- changelog/object-count.added.md
+++
@@ -1,1 +0,0 @@
-On object list views, The number of objects now changes when you apply filters in list views.
--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -187,6 +187,7 @@ const sidebars: SidebarsConfig = {
             slug: 'release-notes/infrahub',
           },
           items: [
+            'release-notes/infrahub/release-1_3_0',
             'release-notes/infrahub/release-1_2_12',
             'release-notes/infrahub/release-1_2_11',
             'release-notes/infrahub/release-1_2_10',
