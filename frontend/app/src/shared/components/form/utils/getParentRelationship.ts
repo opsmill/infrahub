1d6eefb4b565f52c8d7c46e65af05c7fd2ccf4e7
pa-lem <pa-lem@hotmail.fr>
2025-06-30 08:35:59 +0200

    update type

--- frontend/app/src/shared/components/form/utils/getParentRelationship.ts
+++ frontend/app/src/shared/components/form/utils/getParentRelationship.ts
@@ -3,7 +3,9 @@ import { useSchema } from "@/entities/schema/ui/hooks/useSchema";
 export const getParentRelationship = (peer?: string) => {
   const peerSchema = useSchema(peer);
 
-  const parentRelationship = peerSchema?.relationships?.find((rel) => rel.kind === "Parent");
+  const parentRelationship = peerSchema?.schema?.relationships?.find(
+    (rel) => rel.kind === "Parent"
+  );
 
   return parentRelationship;
 };
