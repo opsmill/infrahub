96d5957090603e009e8f74689f5a97bd7352b1c5
Bilal ABBAD <bilal@opsmill.com>
2025-06-30 16:44:56 +0200

    Fixed search in object template selector during creation form (#6756)

--- frontend/app/src/entities/nodes/object-template/object-template-autocomplete.tsx
+++ frontend/app/src/entities/nodes/object-template/object-template-autocomplete.tsx
@@ -2,6 +2,7 @@ import { useObjects } from "@/entities/nodes/object/domain/get-objects.query";
 import { getNodeLabel } from "@/entities/nodes/object/utils/get-node-label";
 import { NodeObject } from "@/entities/nodes/types";
 import { TemplateSchema } from "@/entities/schema/types";
+import ErrorScreen from "@/shared/components/errors/error-screen";
 import {
   ComboboxEmpty,
   ComboboxItem,
@@ -9,6 +10,8 @@ import {
   ComboboxListProps,
 } from "@/shared/components/ui/combobox";
 import { Spinner } from "@/shared/components/ui/spinner";
+import { debounce } from "@/shared/utils/common";
+import React from "react";
 
 export interface ObjectTemplateAutocompleteProps extends Omit<ComboboxListProps, "onSelect"> {
   templateSchema: TemplateSchema;
@@ -20,27 +23,52 @@ export function ObjectTemplateAutocomplete({
   onSelect,
   ...props
 }: ObjectTemplateAutocompleteProps) {
-  const { data, isPending, error } = useObjects({
+  const [search, setSearch] = React.useState("");
+  const { data, isPending, error, hasNextPage, fetchNextPage, isFetchingNextPage } = useObjects({
     schema: templateSchema,
+    filters: search ? [{ name: "any__value", value: search }] : undefined,
     getAttributesVisible: (attributes) => attributes,
     getRelationshipsVisible: (relationships) => relationships,
   });
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+  if (error) return <ErrorScreen message={error.message} />;
+
+  const setSearchDebounced = debounce(setSearch, 300);
 
   return (
-    <ComboboxList {...props}>
-      {isPending && <Spinner className="flex justify-center m-2" />}
+    <ComboboxList
+      onValueChange={(newValue) => setSearchDebounced(newValue)}
+      shouldFilter={false}
+      {...props}
+    >
+      {isPending ? (
+        <Spinner className="flex justify-center m-2" />
+      ) : (
+        <>
+          <ComboboxEmpty>No template found</ComboboxEmpty>
 
-      <ComboboxEmpty>No template found</ComboboxEmpty>
+          {data.pages.map((page) => {
+            return page.map((node) => {
+              return (
+                <ComboboxItem key={node.id} value={node.id} onSelect={() => onSelect(node)}>
+                  <span className="truncate">{getNodeLabel(node)}</span>
+                </ComboboxItem>
+              );
+            });
+          })}
+        </>
+      )}
 
-      {data?.pages.flat().map((node) => (
-        <ComboboxItem key={node.id} value={node.id} onSelect={() => onSelect(node)}>
-          <span className="truncate">{getNodeLabel(node)}</span>
+      {hasNextPage && (
+        <ComboboxItem
+          value="Load more"
+          onSelect={() => fetchNextPage()}
+          disabled={!hasNextPage || isFetchingNextPage}
+          className="justify-center text-custom-blue-700"
+        >
+          {isFetchingNextPage ? "Loading more..." : "Load more"}
         </ComboboxItem>
-      ))}
+      )}
     </ComboboxList>
   );
 }
