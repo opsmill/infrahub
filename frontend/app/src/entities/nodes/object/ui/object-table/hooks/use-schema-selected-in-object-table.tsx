0689f0a65ae3004e82cb9a138a097f2b4a5c6a15
bilalabbad <bilal@opsmill.com>
2025-07-04 19:46:25 +0200

    Show automatically the only inheriting schema of a generic on the generic list view

--- frontend/app/src/entities/nodes/object/ui/object-table/hooks/use-schema-selected-in-object-table.tsx
+++ frontend/app/src/entities/nodes/object/ui/object-table/hooks/use-schema-selected-in-object-table.tsx
@@ -11,6 +11,12 @@ export const useSchemaSelectedInObjectTable = (schema: ModelSchema) => {
   return React.useMemo<ModelSchema>(() => {
     if (!isGenericSchema(schema)) return schema;
 
+    if (schema.used_by?.length === 1) {
+      const singleInheritingKind = schema.used_by[0] as string;
+      const { schema: schemaOfSingleInheritingKind } = getSchema(singleInheritingKind);
+      if (schemaOfSingleInheritingKind) return schemaOfSingleInheritingKind;
+    }
+
     if (!kindFilter) return schema;
 
     const inheritingKindInFilter = schema.used_by?.find((kind) => kind === kindFilter.value);
