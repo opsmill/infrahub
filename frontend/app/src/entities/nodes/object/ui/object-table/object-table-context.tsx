e02f213284f864235c767ac0445c804a105752d4
Bilal ABBAD <bilal@opsmill.com>
2025-07-04 17:43:53 +0200

    Add a schema selector on generic list view to show columns based on selected schema (#6779)

---
+++ frontend/app/src/entities/nodes/object/ui/object-table/object-table-context.tsx
@@ -0,0 +1,54 @@
+import { useSchemaSelectedInObjectTable } from "@/entities/nodes/object/ui/object-table/hooks/use-schema-selected-in-object-table";
+import { Permission } from "@/entities/permission/types";
+import { RequireObjectPermissions } from "@/entities/permission/ui/require-object-permissions";
+import { ModelSchema } from "@/entities/schema/types";
+import useFilters, { Filter } from "@/shared/hooks/useFilters";
+import React from "react";
+
+export type ObjectTableContextProps = {
+  filters: Filter[];
+  setFilters: (filters: Filter[]) => void;
+  baseSchema: ModelSchema;
+  selectedSchema: ModelSchema;
+  permission: Permission;
+};
+
+export const ObjectTableContext = React.createContext<ObjectTableContextProps | null>(null);
+
+export const ObjectTableProvider = ({
+  children,
+  schema,
+}: { children?: React.ReactNode; schema: ModelSchema }) => {
+  const [filters, setFilters] = useFilters();
+  const selectedSchema = useSchemaSelectedInObjectTable(schema);
+
+  return (
+    <RequireObjectPermissions objectKind={schema.kind as string}>
+      {({ permission }) => {
+        return (
+          <ObjectTableContext
+            value={{
+              filters,
+              setFilters,
+              baseSchema: schema,
+              selectedSchema,
+              permission,
+            }}
+          >
+            {children}
+          </ObjectTableContext>
+        );
+      }}
+    </RequireObjectPermissions>
+  );
+};
+
+export function useObjectTableContext() {
+  const context = React.use(ObjectTableContext);
+
+  if (!context) {
+    throw new Error("useSidebar must be used within a SidebarProvider.");
+  }
+
+  return context;
+}
