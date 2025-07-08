96598a5115b4664cc96dea3b0a9bff2575a3690b
pa-lem <pa-lem@hotmail.fr>
2025-07-08 17:04:39 +0200

    fix context

--- frontend/app/src/entities/proposed-changes/ui/proposed-changes-manager.tsx
+++ frontend/app/src/entities/proposed-changes/ui/proposed-changes-manager.tsx
@@ -1,33 +1,25 @@
+import { ObjectTableProvider } from "@/entities/nodes/object/ui/object-table/object-table-context";
 import { RequireObjectPermissions } from "@/entities/permission/ui/require-object-permissions";
-import {
-  ProposedChangesTable,
-  ProposedChangesTableProps,
-} from "@/entities/proposed-changes/ui/proposed-changes-table";
+import { ProposedChangesTable } from "@/entities/proposed-changes/ui/proposed-changes-table";
 import { PROPOSED_CHANGE_OBJECT } from "@/entities/proposed-changes/utils/constant";
 import { ModelSchema } from "@/entities/schema/types";
 import { ProposedChangesManagerToolbar } from "./proposed-changes-manager-toolbar";
 
 export interface ProposedChangesManagerProps {
   schema: ModelSchema;
-  baseFilters?: ProposedChangesTableProps["baseFilters"];
 }
 
 export function ProposedChangesManager({
   schema: proposedChangeSchema,
-  baseFilters,
 }: ProposedChangesManagerProps) {
   return (
     <RequireObjectPermissions objectKind={PROPOSED_CHANGE_OBJECT}>
       {({ permission }) => {
         return (
-          <>
+          <ObjectTableProvider schema={proposedChangeSchema}>
             <ProposedChangesManagerToolbar permission={permission} schema={proposedChangeSchema} />
-            <ProposedChangesTable
-              permission={permission}
-              schema={proposedChangeSchema}
-              baseFilters={baseFilters}
-            />
-          </>
+            <ProposedChangesTable permission={permission} schema={proposedChangeSchema} />
+          </ObjectTableProvider>
         );
       }}
     </RequireObjectPermissions>
