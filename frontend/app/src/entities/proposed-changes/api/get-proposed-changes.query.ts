72df5e3255e090c1fabef4cf0c76417ed1aa1e4b
pa-lem <pa-lem@hotmail.fr>
2025-07-08 17:04:14 +0200

    add queries

---
+++ frontend/app/src/entities/proposed-changes/api/get-proposed-changes.query.ts
@@ -0,0 +1,56 @@
+import { useCurrentBranch } from "@/entities/branches/ui/branches-provider";
+import { ContextParams, PaginationParams } from "@/shared/api/types";
+import { datetimeAtom } from "@/shared/stores/time.atom";
+import { infiniteQueryOptions, useInfiniteQuery } from "@tanstack/react-query";
+import { useAtomValue } from "jotai";
+import {
+  GetProposedChangesParams,
+  OBJECTS_PER_PAGE,
+  getProposedChanges,
+} from "../domain/get-proposed-changes-from-api";
+
+type GetObjectsQueryParams = Omit<GetProposedChangesParams, keyof PaginationParams>;
+
+export function getProposedChangesInfiniteQueryOptions({
+  schema,
+  filters,
+  branchName,
+  atDate,
+  getAttributesVisible,
+  getRelationshipsVisible,
+}: GetObjectsQueryParams) {
+  return infiniteQueryOptions({
+    queryKey: [branchName, atDate, "objects", schema.kind, JSON.stringify(filters)],
+    queryFn: ({ pageParam }) => {
+      return getProposedChanges({
+        schema,
+        offset: pageParam,
+        branchName,
+        atDate,
+        filters,
+        getAttributesVisible,
+        getRelationshipsVisible,
+      });
+    },
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, _, lastPageParam) => {
+      if (lastPage.length < OBJECTS_PER_PAGE) {
+        return undefined;
+      }
+      return lastPageParam + OBJECTS_PER_PAGE;
+    },
+  });
+}
+
+export function useProposedChanges(params: Omit<GetObjectsQueryParams, keyof ContextParams>) {
+  const { currentBranch } = useCurrentBranch();
+  const timeMachineDate = useAtomValue(datetimeAtom);
+
+  return useInfiniteQuery(
+    getProposedChangesInfiniteQueryOptions({
+      ...params,
+      branchName: currentBranch.name,
+      atDate: timeMachineDate,
+    })
+  );
+}
---
+++ frontend/app/src/entities/proposed-changes/domain/get-proposed-changes-from-api.ts
@@ -0,0 +1,81 @@
+import { getAttributesVisibleInListView } from "@/entities/nodes/object/utils/get-attributes-visible-in-list-view";
+import { getRelationshipsVisibleInListView } from "@/entities/nodes/object/utils/get-relationships-visible-in-list-view";
+import { NodeObject } from "@/entities/nodes/types";
+import { PROPOSED_CHANGE_OBJECT } from "@/entities/proposed-changes/utils/constant";
+import { AttributeSchema, ModelSchema, RelationshipSchema } from "@/entities/schema/types";
+import graphqlClient from "@/shared/api/graphql/graphqlClientApollo";
+import {
+  addAttributesToRequest,
+  addFiltersToRequest,
+  addRelationshipsToRequest,
+} from "@/shared/api/graphql/utils";
+import { ContextParams, PaginationParams } from "@/shared/api/types";
+import { Filter } from "@/shared/hooks/useFilters";
+import { gql } from "@apollo/client";
+import { jsonToGraphQLQuery } from "json-to-graphql-query";
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+export const OBJECTS_PER_PAGE = 40;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+export type GetProposedChangesParams = ContextParams &
+  PaginationParams & {
+    schema: ModelSchema;
+    filters?: Array<Filter>;
+    getAttributesVisible?: (attributes: AttributeSchema[]) => AttributeSchema[];
+    getRelationshipsVisible?: (relationships: RelationshipSchema[]) => RelationshipSchema[];
+  };
+
+export type GetObjects = (args: GetProposedChangesParams) => Promise<Array<NodeObject>>;
+
+export const getProposedChanges: GetObjects = async ({
+  schema,
+  limit = OBJECTS_PER_PAGE,
+  offset,
+  branchName,
+  atDate,
+  filters,
+  getAttributesVisible = getAttributesVisibleInListView,
+  getRelationshipsVisible = getRelationshipsVisibleInListView,
+}) => {
+  const attributesVisible = getAttributesVisible(schema.attributes ?? []);
+  const relationshipsVisible = getRelationshipsVisible(schema.relationships ?? []);
+
+  const schemaKindToQuery = schema.kind as string;
+
+  const queryString = jsonToGraphQLQuery({
+    query: {
+      __name: `Get${PROPOSED_CHANGE_OBJECT}`,
+      [schemaKindToQuery]: {
+        __args: {
+          limit,
+          offset,
+          ...(filters ? addFiltersToRequest(filters) : {}),
+        },
+        edges: {
+          node: {
+            id: true,
+            display_label: true,
+            hfid: true,
+            _updated_at: true,
+            ...addAttributesToRequest(attributesVisible),
+            ...addRelationshipsToRequest(relationshipsVisible),
+          },
+        },
+      },
+    },
+  });
+
+  const query = gql(queryString);
+  const { data } = await graphqlClient.query({
+    query,
+    context: {
+      branch: branchName,
+      date: atDate,
+    },
+  });
+
+  return data[schemaKindToQuery]?.edges?.map((edge: any) => edge.node) ?? [];
+};
