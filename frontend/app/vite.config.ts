51a6bdb8e00437553dcd4685b03d1d8f63a51ce9
bilalabbad <bilal@opsmill.com>
2025-07-10 23:24:59 +0200

    lint

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -3,9 +3,9 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import monacoEditorPlugin from "vite-plugin-monaco-editor-esm";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
-import monacoEditorPlugin from "vite-plugin-monaco-editor-esm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
