89a7e2ec8cca779a637df73a8b5020de61c60e2f
bilalabbad <bilal@opsmill.com>
2025-07-15 17:17:01 +0200

    add branch in test

--- frontend/app/tests/e2e/resource-manager/number-pool.spec.ts
+++ frontend/app/tests/e2e/resource-manager/number-pool.spec.ts
@@ -1,13 +1,24 @@
 import { expect, test } from "@playwright/test";
 import { ACCOUNT_STATE_PATH } from "../../constants";
-import { saveScreenshotForDocs } from "../../utils";
+import { generateRandomBranchName, saveScreenshotForDocs } from "../../utils";
+import { createBranchAPI, deleteBranchAPI } from "../utils/graphql";
 
 test.describe("/resource-manager - Resource Manager", () => {
   test.describe.configure({ mode: "serial" });
   test.use({ storageState: ACCOUNT_STATE_PATH.ADMIN });
 
+  const BRANCH_NAME = generateRandomBranchName("number-pool");
+
+  test.beforeAll(async ({ request }) => {
+    await createBranchAPI(request, BRANCH_NAME);
+  });
+
+  test.afterAll(async ({ request }) => {
+    await deleteBranchAPI(request, BRANCH_NAME);
+  });
+
   test("should create a new pool", async ({ page }) => {
-    await page.goto("/resource-manager");
+    await page.goto(`/resource-manager?branch=${BRANCH_NAME}`);
     await page.getByTestId("create-object-button").click();
     await page.getByLabel("Select an object type").click();
     await page.getByRole("option", { name: "Number Pool Core" }).click();
@@ -28,7 +39,7 @@ test.describe("/resource-manager - Resource Manager", () => {
   });
 
   test("pool details should be correct", async ({ page }) => {
-    await page.goto("/resource-manager");
+    await page.goto(`/resource-manager?branch=${BRANCH_NAME}`);
     await page.getByTestId("object-items").getByRole("link", { name: "number pool test" }).click();
     await page.getByRole("cell", { name: "number pool test" }).first().click();
     await expect(page.getByRole("cell", { name: "speed" })).toBeVisible();
@@ -37,7 +48,7 @@ test.describe("/resource-manager - Resource Manager", () => {
   });
 
   test("update form should not include node and attribute selects", async ({ page }) => {
-    await page.goto("/resource-manager");
+    await page.goto(`/resource-manager?branch=${BRANCH_NAME}`);
     await page.getByTestId("object-items").getByRole("link", { name: "number pool test" }).click();
     await expect(page.getByRole("cell", { name: "number pool test" }).first()).toBeVisible();
     await expect(page.getByText("Node *")).not.toBeVisible();
@@ -45,7 +56,7 @@ test.describe("/resource-manager - Resource Manager", () => {
   });
 
   test("numberpool attribute kind resource manager", async ({ page }) => {
-    await page.goto("/resource-manager");
+    await page.goto(`/resource-manager?branch=${BRANCH_NAME}`);
     await expect(page.getByRole("link", { name: "InfraService." })).toBeVisible();
     await page.getByRole("link", { name: "InfraService." }).click();
     await page.getByRole("link", { name: "View", exact: true }).click();
