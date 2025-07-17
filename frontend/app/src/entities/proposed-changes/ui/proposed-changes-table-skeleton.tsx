92d58dbdbb326f14721f7e84264f2f539bd41ca9
pa-lem <pa-lem@hotmail.fr>
2025-07-17 21:59:50 +0200

    update skeleton

--- frontend/app/src/entities/proposed-changes/ui/proposed-changes-table-skeleton.tsx
+++ frontend/app/src/entities/proposed-changes/ui/proposed-changes-table-skeleton.tsx
@@ -6,12 +6,12 @@ export interface ProposedCHangesTableSkeletonProps {
 }
 
 export function ProposedCHangesTableSkeleton({ headerCount }: ProposedCHangesTableSkeletonProps) {
-  return [...Array(20)].map((_, rowIndex) => {
+  return [...Array(5)].map((_, rowIndex) => {
     return (
       <React.Fragment key={`skeleton-row-${rowIndex}`}>
         {[...Array(headerCount)].map((_, colIndex) => {
           return (
-            <div className="p-2 border border-gray-200" key={colIndex}>
+            <div className="p-2 border-t border-gray-200" key={colIndex}>
               <div className="flex flex-col gap-2">
                 <Skeleton className="h-4 w-40" />
                 <Skeleton className="h-4 w-2xl" />
