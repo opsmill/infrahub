6ae4bfff813f14f445db567930142b2ed26bb8f6
LucasG0 <44552904+LucasG0@users.noreply.github.com>
2025-07-18 13:46:43 +0200

    Add is_draft proposed change attribute (#6840)

--- backend/infrahub/core/protocols.py
+++ backend/infrahub/core/protocols.py
@@ -480,6 +480,7 @@ class CoreProposedChange(CoreTaskTarget):
     source_branch: String
     destination_branch: String
     state: Enum
+    is_draft: Boolean
     approved_by: RelationshipManager
     reviewers: RelationshipManager
     created_by: RelationshipManager
