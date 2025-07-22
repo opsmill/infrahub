5098b4dc7ac63b1697572e3947424ea519d246af
pa-lem <pa-lem@hotmail.fr>
2025-07-22 12:00:38 +0200

    update constants

--- frontend/app/src/entities/proposed-changes/constant.ts
+++ frontend/app/src/entities/proposed-changes/constant.ts
@@ -6,3 +6,11 @@ export const CANCEL_REJECT_DECISION = "CENCEL_REJECT";
 export const MERGE_STATE = "MERGE";
 export const OPEN_STATE = "OPEN";
 export const CLOSE_STATE = "CLOSE";
+export const PROPOSED_CHANGE_OBJECT = "CoreProposedChange";
+
+export const PROPOSED_CHANGE_STATES = {
+  opened: ["open", "merging"],
+  closed: ["closed", "merged", "canceled"],
+};
+
+export const QSP_STATE_CLOSE_VALUE = "closed";
