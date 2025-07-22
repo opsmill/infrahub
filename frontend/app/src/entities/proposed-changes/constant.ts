7693c9d7c3c63b5847ba620d592dd5099f6bc2d7
pa-lem <pa-lem@hotmail.fr>
2025-07-22 11:57:36 +0200

    update constants

--- frontend/app/src/entities/proposed-changes/constant.ts
+++ frontend/app/src/entities/proposed-changes/constant.ts
@@ -1,40 +1,8 @@
-export const PROPOSED_CHANGE_APPROVAL_ACTIONS = {
-  approve: {
-    decision: "APPROVE",
-    successMessage: "Proposed change approved!",
-    errorMessage: "An error occured while approving the proposed change",
-  },
-  "cancel-approve": {
-    decision: "CANCEL_APPROVE",
-    successMessage: "Proposed change approval canceled!",
-    errorMessage: "An error occured while canceling the proposed change approval",
-  },
-  reject: {
-    decision: "REJECT",
-    successMessage: "Proposed change rejected!",
-    errorMessage: "An error occured while rejecting the proposed change",
-  },
-  "cancel-reject": {
-    decision: "CANCEL_REJECT",
-    successMessage: "Proposed change reject canceled!",
-    errorMessage: "An error occured while canceling the proposed change reject",
-  },
-};
+export const APPROVE_DECISION = "APPROVE";
+export const CANCEL_APPROVE_DECISION = "CENCEL_APPROVE";
+export const REJECT_DECISION = "REJECT";
+export const CANCEL_REJECT_DECISION = "CENCEL_REJECT";
 
-export const PROPOSED_CHANGE_STATE_ACTIONS = {
-  merge: {
-    state: "merge",
-    successMessage: "Proposed change approved!",
-    errorMessage: "An error occured while merging the proposed change",
-  },
-  close: {
-    state: "close",
-    successMessage: "Proposed change rejected!",
-    errorMessage: "An error occured while closing the proposed change",
-  },
-  open: {
-    state: "open",
-    successMessage: "Proposed change opened!",
-    errorMessage: "An error occured while opening the proposed change",
-  },
-};
+export const MERGE_STATE = "MERGE";
+export const OPEN_STATE = "OPEN";
+export const CLOSE_STATE = "CLOSE";
