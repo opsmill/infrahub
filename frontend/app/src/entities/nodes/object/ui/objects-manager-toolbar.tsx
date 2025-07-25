6231868d9653b4c218c6361dc3a67d93e3fc8518
Bilal ABBAD <bilal@opsmill.com>
2025-07-25 18:37:11 +0200

    Fix: Always show suggested filters (#6896)

--- frontend/app/src/entities/nodes/object/ui/objects-manager-toolbar.tsx
+++ frontend/app/src/entities/nodes/object/ui/objects-manager-toolbar.tsx
@@ -1,6 +1,5 @@
 import { reloadIpamTreeAtom } from "@/entities/ipam/ipam-tree/ipam-tree.state";
-import { ActiveFilterTags } from "@/entities/nodes/object/ui/filters/active-filter-tags";
-import { FilterResetButton } from "@/entities/nodes/object/ui/filters/filter-reset-button";
+import { ActiveObjectFilterTags } from "@/entities/nodes/object/ui/filters/active-object-filter-tags";
 import { FilterSearchInput } from "@/entities/nodes/object/ui/filters/filter-search-input";
 import { useObjectTableContext } from "@/entities/nodes/object/ui/object-table/object-table-context";
 import { ObjectTableSchemaSelector } from "@/entities/nodes/object/ui/object-table/object-table-schema-selector";
@@ -8,7 +7,6 @@ import { isGenericSchema } from "@/entities/schema/utils/is-generic-schema";
 import { queryClient } from "@/shared/api/rest/client";
 import { ObjectCreateFormTrigger } from "@/shared/components/form/object-create-form-trigger";
 import {} from "@/shared/components/ui/combobox";
-import { ScrollArea } from "@/shared/components/ui/scroll-area";
 import { useSetAtom } from "jotai";
 import { useLocation, useParams } from "react-router";
 
@@ -17,7 +15,7 @@ export function ObjectsManagerToolbar() {
 
   const location = useLocation();
   const reloadIpamTree = useSetAtom(reloadIpamTreeAtom);
-  const { filters, selectedSchema: schema, baseSchema, permission } = useObjectTableContext();
+  const { selectedSchema, baseSchema, permission } = useObjectTableContext();
 
   return (
     <div className="flex items-center gap-2 h-14 px-3 shrink-0">
@@ -25,19 +23,12 @@ export function ObjectsManagerToolbar() {
         <ObjectTableSchemaSelector />
       )}
 
-      <FilterSearchInput schema={schema} />
+      <FilterSearchInput schema={selectedSchema} />
 
-      {filters.filter((f) => f.name !== "kind__value").length > 0 && (
-        <>
-          <ScrollArea scrollX>
-            <ActiveFilterTags schema={schema} />
-          </ScrollArea>
-          <FilterResetButton />
-        </>
-      )}
+      <ActiveObjectFilterTags schema={selectedSchema} />
 
       <ObjectCreateFormTrigger
-        schema={schema}
+        schema={selectedSchema}
         onSuccess={() => {
           queryClient.invalidateQueries({
             predicate: (query) => query.queryKey.includes("objects"),
