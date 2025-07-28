174cb5356a7383e0f287b98871ec756fb4973ec0
pa-lem <pa-lem@hotmail.fr>
2025-07-28 09:49:54 +0200

    updat const

--- frontend/app/src/entities/proposed-changes/constants.ts
+++ frontend/app/src/entities/proposed-changes/constants.ts
@@ -3,11 +3,14 @@ export const CANCEL_APPROVE_DECISION = "CANCEL_APPROVE";
 export const REJECT_DECISION = "REJECT";
 export const CANCEL_REJECT_DECISION = "CANCEL_REJECT";
 
+export const OPEN_STATE = "open";
 export const MERGE_STATE = "merged";
+export const MERGING_STATE = "merging";
 export const CLOSE_STATE = "closed";
+export const DRAFT_STATE = "draft";
 export const PROPOSED_CHANGE_OBJECT = "CoreProposedChange";
 
 export const PROPOSED_CHANGE_STATES = {
-  opened: ["open", "merging"],
+  opened: [OPEN_STATE, MERGING_STATE],
   closed: [CLOSE_STATE, MERGE_STATE, "canceled"],
 };
