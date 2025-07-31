5ba6992942cbbb784caf8f522048163fc6fb7b9a
Baptiste <baptiste@opsmill.com>
2025-07-31 13:45:30 +0200

    Update sidebar

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -248,7 +248,7 @@ const sidebars: SidebarsConfig = {
                 type: 'generated-index',
               },
               items: [
-                'topics/auth',
+                'topics/authentication',
                 'topics/permissions-roles',
               ],
             },
