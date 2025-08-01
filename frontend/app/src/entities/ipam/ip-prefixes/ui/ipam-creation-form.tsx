db9a189d7e1cd003dcfe42a7cbe11eb5a83e213d
Bilal ABBAD <bilal@opsmill.com>
2025-08-01 15:51:10 +0200

    Auto-suggest Ip address/prefix when opening a children creation form (#6939)

--- frontend/app/src/entities/ipam/ip-prefixes/ui/ipam-creation-form.tsx
+++ frontend/app/src/entities/ipam/ip-prefixes/ui/ipam-creation-form.tsx
@@ -1,8 +1,14 @@
+import { IP_ADDRESS_GENERIC, IP_PREFIX_GENERIC } from "@/entities/ipam/constants";
+import { useGetNextIpAddressAvailable } from "@/entities/ipam/ip-addresses/domain/get-next-ip-address-available.query";
+import { useGetNextIpPrefixAvailable } from "@/entities/ipam/ip-prefixes/domain/get-next-ip-prefix-available.query";
 import { useCreateObjectMutation } from "@/entities/nodes/object/domain/create-object.mutation";
 import { useAllocateResourceMutation } from "@/entities/resource-manager/domain/allocate-resource.mutation";
 import { getAllocateMutationNameFromSchema } from "@/entities/resource-manager/utils/get-allocate-mutation-name-from-schema";
+import { isOfKind } from "@/entities/schema/utils/is-of-kind";
 import { NodeForm, NodeFormProps } from "@/shared/components/form/node-form";
+import { useCurrentFormContext } from "@/shared/components/form/utils/form-context";
 import { getCreateMutationFromFormData } from "@/shared/components/form/utils/mutations/getCreateMutationFromFormData";
+import { LoadingIndicator } from "@/shared/components/loading/loading-indicator";
 import { ALERT_TYPES, Alert } from "@/shared/components/ui/alert";
 import { toast } from "react-toastify";
 
@@ -11,6 +17,35 @@ export interface IpamCreationFormProps extends NodeFormProps {}
 function IpamCreationForm(props: IpamCreationFormProps) {
   const allocateResource = useAllocateResourceMutation();
   const createObject = useCreateObjectMutation();
+  const { parentData } = useCurrentFormContext();
+
+  const isIpPrefixSchema = isOfKind(IP_PREFIX_GENERIC, props.schema);
+  const isIpAddressSchema = isOfKind(IP_ADDRESS_GENERIC, props.schema);
+
+  const ipFieldName = isIpPrefixSchema ? "prefix" : "address";
+
+  const { data: nextIpAddress, isLoading: isIpAddressLoading } = useGetNextIpAddressAvailable(
+    { parentPrefixId: parentData?.id },
+    // Enable fetching the next available IP address only if:
+    // - The current schema is for an IP address
+    // - The current object does NOT already have an address set
+    { enabled: isIpAddressSchema && !props.currentObject?.address }
+  );
+  const { data: nextIpPrefix, isLoading: isIpPrefixLoading } = useGetNextIpPrefixAvailable(
+    { parentPrefixId: parentData?.id },
+    {
+      // Enable fetching the next available IP prefix only if:
+      // - The current schema is for an IP prefix
+      // - The current object does NOT already have a prefix set
+      enabled: isIpPrefixSchema && !props.currentObject?.prefix,
+    }
+  );
+
+  if (parentData && (isIpAddressLoading || isIpPrefixLoading)) {
+    return <LoadingIndicator className="mt-4" />;
+  }
+
+  const nextIpValue = isIpPrefixSchema ? nextIpPrefix : nextIpAddress;
 
   const onSuccess: NodeFormProps["onSuccess"] = (newNode) => {
     toast(
@@ -24,7 +59,6 @@ function IpamCreationForm(props: IpamCreationFormProps) {
         toastId: `alert-success-${props.schema.name}-created`,
       }
     );
-
     props.onSuccess?.(newNode);
   };
 
@@ -35,15 +69,20 @@ function IpamCreationForm(props: IpamCreationFormProps) {
   return (
     <NodeForm
       {...props}
+      currentObject={
+        nextIpValue
+          ? {
+              ...props.currentObject,
+              [ipFieldName]: { value: nextIpValue },
+            }
+          : props.currentObject
+      }
       onSubmit={async ({ fields, formData }) => {
-        // prefix and address are fields that pool allocates
-        const formFieldsWithoutIpField = fields.filter(
-          ({ name }) => name !== "prefix" && name !== "address"
-        );
-
-        const fieldDataForIpField = formData.prefix ?? formData.address;
-
+        // Remove the IP field if it's being allocated from a pool
+        const formFieldsWithoutIpField = fields.filter(({ name }) => name !== ipFieldName);
+        const fieldDataForIpField = formData[ipFieldName];
         const allocateMutationName = getAllocateMutationNameFromSchema(props.schema);
+
         if (fieldDataForIpField?.source?.type === "pool" && allocateMutationName) {
           await allocateResource.mutateAsync(
             {
