26e8247eed2616eaa02fae12fc6b8c33be56963b
Bilal ABBAD <bilal@opsmill.com>
2025-08-04 22:45:05 +0200

    Added preview of current IP attributes/rels in IPAM details page header (#6960)

--- frontend/app/src/entities/ipam/ip-namespaces/ui/ip-namespace-tabs.tsx
+++ frontend/app/src/entities/ipam/ip-namespaces/ui/ip-namespace-tabs.tsx
@@ -1,3 +1,4 @@
+import { constructPathForIpam } from "@/entities/ipam/utils";
 import { ObjectDetailsTab } from "@/entities/nodes/object/ui/object-details/object-details-tab";
 import { ModelSchema } from "@/entities/schema/types";
 
@@ -21,7 +22,7 @@ export function IpNamespaceTabs({ objectId, schema }: IpNamespaceTabsProps) {
           parentKind={schema.kind!}
           parentId={objectId}
           relationship={relationshipSchemaWithIpPrefix}
-          href="/ipam"
+          href={constructPathForIpam("/ipam")}
         />
       )}
 
@@ -30,7 +31,7 @@ export function IpNamespaceTabs({ objectId, schema }: IpNamespaceTabsProps) {
           parentKind={schema.kind!}
           parentId={objectId}
           relationship={relationshipSchemaWithIpAddress}
-          href="/ipam/ip_addresses"
+          href={constructPathForIpam("/ipam/ip_addresses")}
         />
       )}
     </div>
