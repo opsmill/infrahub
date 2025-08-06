583183f1ecf08ce2c68068af52d40e801d102a83
Guillaume Mazoyer <guillaume@opsmill.com>
2025-08-06 23:03:07 +0200

    Fix typo

--- backend/infrahub/events/proposed_change_action.py
+++ backend/infrahub/events/proposed_change_action.py
@@ -114,7 +114,7 @@ class ProposedChangeRejectionRevokedEvent(ProposedChangeReviewRevokedEvent):
     event_name: ClassVar[str] = f"{EVENT_NAMESPACE}.proposed_change.rejection_revoked"
 
 
-class ProposedChangeThreadEevent(ProposedChangeEvent):
+class ProposedChangeThreadEvent(ProposedChangeEvent):
     thread_id: str = Field(..., description="The ID of the thread that was created or updated")
     thread_kind: str = Field(..., description="The name of the thread that was created or updated")
 
@@ -131,14 +131,14 @@ class ProposedChangeThreadEevent(ProposedChangeEvent):
         return related
 
 
-class ProposedChangeThreadCreatedEvent(ProposedChangeThreadEevent):
+class ProposedChangeThreadCreatedEvent(ProposedChangeThreadEvent):
     """Event generated when a thread has been created in a proposed change"""
 
     event_name: ClassVar[str] = f"{EVENT_NAMESPACE}.proposed_change_thread.created"
     action: MutationAction = MutationAction.CREATED
 
 
-class ProposedChangeThreadUpdatedEvent(ProposedChangeThreadEevent):
+class ProposedChangeThreadUpdatedEvent(ProposedChangeThreadEvent):
     """Event generated when a thread has been updated in a proposed change"""
 
     event_name: ClassVar[str] = f"{EVENT_NAMESPACE}.proposed_change_thread.updated"
