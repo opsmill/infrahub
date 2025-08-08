cf4a153ac0b196772e1ece07b2124a29ec90a9f8
pa-lem <pa-lem@hotmail.fr>
2025-08-08 14:25:10 +0200

    remove threshold

--- frontend/app/src/shared/components/utils/infinite-trigger.tsx
+++ frontend/app/src/shared/components/utils/infinite-trigger.tsx
@@ -4,7 +4,6 @@ export interface InfiniteTriggerProps extends React.HTMLAttributes<HTMLDivElemen
   hasNextPage: boolean;
   onLoadMore: () => void;
   isFetchingNextPage?: boolean;
-  threshold?: number;
 }
 
 export const InfiniteTrigger = ({
@@ -12,27 +11,19 @@ export const InfiniteTrigger = ({
   hasNextPage,
   onLoadMore,
   isFetchingNextPage,
-  threshold = 200,
   ...props
 }: InfiniteTriggerProps) => {
   const rootRef = React.useRef<HTMLDivElement>(null);
   const loadMoreRef = React.useRef<HTMLDivElement>(null);
 
   React.useEffect(() => {
-    const root = rootRef.current ?? undefined;
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting && hasNextPage && !isFetchingNextPage) {
-            onLoadMore();
-          }
-        });
-      },
-      {
-        root,
-        rootMargin: `0px 0px ${threshold}px 0px`,
-      }
-    );
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && hasNextPage && !isFetchingNextPage) {
+          onLoadMore();
+        }
+      });
+    });
 
     const currentContainer = loadMoreRef.current;
     if (currentContainer) {
