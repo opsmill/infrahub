2c6092a30b0cc9731853edd629fd07152d8146c4
Bilal ABBAD <bilal@opsmill.com>
2025-08-08 17:41:39 +0200

    Improve TS for object table and add function to reuse default object table column defs (#6992)

--- frontend/app/src/entities/nodes/object/ui/object-table/get-object-actions-column.tsx
+++ frontend/app/src/entities/nodes/object/ui/object-table/get-object-actions-column.tsx
@@ -1,12 +1,12 @@
 import { ActionsHeaderCell } from "@/entities/nodes/object/ui/object-table/cells/actions-header-cell";
 import { ObjectActionsCell } from "@/entities/nodes/object/ui/object-table/cells/object-actions-cell";
-import { NodeCore } from "@/entities/nodes/types";
+import { NodeObject } from "@/entities/nodes/types";
 import { Permission } from "@/entities/permission/types";
 import { ColumnDef, createColumnHelper } from "@tanstack/react-table";
 
-const columnHelper = createColumnHelper<NodeCore>();
+const columnHelper = createColumnHelper<NodeObject>();
 
-export function getObjectActionsColumn(permission: Permission): ColumnDef<NodeCore> {
+export function getObjectActionsColumn(permission: Permission): ColumnDef<NodeObject> {
   return columnHelper.display({
     id: "actions",
     header: () => <ActionsHeaderCell />,
