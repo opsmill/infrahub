e6d5f29501fcce8909b28a524ed8ba2fd45a48fa
pa-lem <pa-lem@hotmail.fr>
2025-08-08 16:40:49 +0200

    add threshold

--- frontend/app/src/shared/components/utils/infinite-trigger.tsx
+++ frontend/app/src/shared/components/utils/infinite-trigger.tsx
@@ -4,23 +4,29 @@ export interface InfiniteTriggerProps extends React.HTMLAttributes<HTMLDivElemen
   hasNextPage: boolean;
   onLoadMore: () => void;
   isFetchingNextPage?: boolean;
+  threshold?: number;
 }
 
 export const InfiniteTrigger = ({
   hasNextPage,
   onLoadMore,
   isFetchingNextPage,
+  threshold = 200,
+  ...props
 }: InfiniteTriggerProps) => {
   const loadMoreRef = React.useRef<HTMLDivElement>(null);
 
   React.useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting && hasNextPage && !isFetchingNextPage) {
-          onLoadMore();
-        }
-      });
-    });
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && hasNextPage && !isFetchingNextPage) {
+            onLoadMore();
+          }
+        });
+      },
+      { rootMargin: `${threshold}px 0px` }
+    );
 
     const currentContainer = loadMoreRef.current;
     if (currentContainer) {
@@ -32,5 +38,5 @@ export const InfiniteTrigger = ({
     };
   }, [onLoadMore, hasNextPage, isFetchingNextPage]);
 
-  return <div ref={loadMoreRef} className="h-px" aria-hidden="true" />;
+  return <div ref={loadMoreRef} className="h-px" aria-hidden="true" {...props} />;
 };
