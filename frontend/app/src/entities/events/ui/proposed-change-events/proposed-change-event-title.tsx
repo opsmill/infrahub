864b394d99d5b4b714bddbaa0303d158e9f366d4
pa-lem <pa-lem@hotmail.fr>
2025-08-08 08:40:37 +0200

    simplify types

--- frontend/app/src/entities/events/ui/proposed-change-events/proposed-change-event-title.tsx
+++ frontend/app/src/entities/events/ui/proposed-change-events/proposed-change-event-title.tsx
@@ -3,7 +3,7 @@ import { PROPOSED_CHANGE_THREAD } from "@/entities/proposed-changes/constants";
 import { ReactNode } from "react";
 import { ProposedChangeThreadEvent } from "./proposed-change-thread-event";
 
-export const PROPOSED_CHANGE_EVENTS_MAPPING: Record<string, (props: any) => ReactNode> = {
+export const PROPOSED_CHANGE_EVENTS_MAPPING: Record<string, () => ReactNode> = {
   "infrahub.proposed_change.merged": () => {
     return <div className="text-gray-600">merged the proposed change</div>;
   },
@@ -44,7 +44,7 @@ export const ProposedChangeEventTitle = (props: any) => {
     <div className="flex items-center flex-wrap gap-1 text-sm">
       <NodeLabel id={account_id} kind="CoreAccount" branch={branch} />
 
-      {PROPOSED_CHANGE_EVENTS_MAPPING[event] && PROPOSED_CHANGE_EVENTS_MAPPING[event](props)}
+      {PROPOSED_CHANGE_EVENTS_MAPPING[event] && PROPOSED_CHANGE_EVENTS_MAPPING[event]()}
     </div>
   );
 };
