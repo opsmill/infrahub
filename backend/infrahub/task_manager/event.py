8afb6fa30688ea386ffffd7553da4f76eeb8dd0b
Guillaume Mazoyer <guillaume@opsmill.com>
2025-08-14 16:08:29 +0200

    Lint

--- backend/infrahub/task_manager/event.py
+++ backend/infrahub/task_manager/event.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import asyncio
-from collections import defaultdict
 from typing import TYPE_CHECKING, Any
 
 from prefect.client.orchestration import PrefectClient, get_client
