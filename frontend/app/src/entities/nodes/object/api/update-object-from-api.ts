5a4194718067c491676568342aa68a9341e1e5c0
pa-lem <pa-lem@hotmail.fr>
2025-08-20 16:42:45 +0200

    fix mutation name

--- frontend/app/src/entities/nodes/object/api/update-object-from-api.ts
+++ frontend/app/src/entities/nodes/object/api/update-object-from-api.ts
@@ -89,7 +89,7 @@ export function updateObjectFromApi({
       ? getRelationshipMutation({
           id: objectData.id,
           data: relationshipRemoveData,
-          mutation: "RelationshipAdd",
+          mutation: "RelationshipRemove",
         })
       : {};
 
