5875c750606a30e1a28513344f90c6fe64d08bb1
pa-lem <pa-lem@hotmail.fr>
2025-08-21 12:14:10 +0200

    improve test

--- frontend/app/src/shared/components/form/utils/getFormFieldsFromSchema.test.ts
+++ frontend/app/src/shared/components/form/utils/getFormFieldsFromSchema.test.ts
@@ -9,6 +9,7 @@ import {
   generateAttributeSchema,
   generateRelationshipSchema,
 } from "../../../../../tests/fake/schema";
+import { RELATIONSHIP_BULK_ADD_PREFIX, RELATIONSHIP_BULK_REMOVE_PREFIX } from "../constants";
 
 describe("getFormFieldsFromSchema", () => {
   it("returns no fields if schema has no attributes nor relationships", () => {
@@ -738,8 +739,16 @@ describe("getFormFieldsFromSchema", () => {
   it("should display add and remvoe fields for relationship of cardinality many in bulk edit", () => {
     const schema = {
       relationships: [
-        generateRelationshipSchema({ name: "cardinality_many", cardinality: "many" }),
-        generateRelationshipSchema({ name: "cardinality_one", cardinality: "one" }),
+        generateRelationshipSchema({
+          name: "cardinality_many",
+          cardinality: "many",
+          order_weight: 1,
+        }),
+        generateRelationshipSchema({
+          name: "cardinality_one",
+          cardinality: "one",
+          order_weight: 2,
+        }),
       ],
     } as ModelSchema;
 
@@ -747,8 +756,10 @@ describe("getFormFieldsFromSchema", () => {
     const fields = getFormFieldsFromSchema({ schema, isBulkUpdate: true });
 
     // THEN
-    expect(fields[0]?.name).to.equal("__add_cardinality_many");
-    expect(fields[1]?.name).to.equal("__remove_cardinality_many");
+    expect(fields[0]?.name).to.equal(`${RELATIONSHIP_BULK_ADD_PREFIX}cardinality_many`);
+    expect(fields[0]?.type).to.equal("relationship-add");
+    expect(fields[1]?.name).to.equal(`${RELATIONSHIP_BULK_REMOVE_PREFIX}cardinality_many`);
+    expect(fields[1]?.type).to.equal("relationship-remove");
     expect(fields[2]?.name).to.equal("cardinality_one");
   });
 });
