e2e21c01bd52a268531f7f292c2dc675750048a5
pa-lem <pa-lem@hotmail.fr>
2025-09-10 08:59:28 +0200

    sort

--- frontend/app/src/entities/nodes/object-header.tsx
+++ frontend/app/src/entities/nodes/object-header.tsx
@@ -1,3 +1,6 @@
+import { useAtomValue } from "jotai";
+import { useLocation } from "react-router";
+
 import { queryClient } from "@/shared/api/rest/client";
 import { removeFiltersNotInSchema } from "@/shared/components/filters/utils/remove-filters-not-in-schema";
 import Content from "@/shared/components/layout/content";
@@ -5,15 +8,14 @@ import { ObjectDetailsButton } from "@/shared/components/menu/object-details-but
 import { ObjectHelpButton } from "@/shared/components/menu/object-help-button";
 import { Skeleton } from "@/shared/components/skeleton";
 import useFilters from "@/shared/hooks/useFilters";
-import { useAtomValue } from "jotai";
-import { useLocation } from "react-router";
-import { schemaKindLabelState } from "../schema/stores/schemaKindLabel.atom";
 
 import { useGetObject } from "@/entities/nodes/object/domain/get-object.query";
 import { useObjectsCount } from "@/entities/nodes/object/domain/get-objects-count.query";
 import { objectQueryKeys } from "@/entities/nodes/object/domain/object.query-keys";
 import { NodeAttribute } from "@/entities/nodes/types";
 import { ModelSchema } from "@/entities/schema/types";
+
+import { schemaKindLabelState } from "../schema/stores/schemaKindLabel.atom";
 
 type ObjectHeaderProps = {
   schema: ModelSchema;
