d3313a964f6be26807372abaae7b6785656f9512
pa-lem <pa-lem@hotmail.fr>
2025-09-12 10:27:07 +0200

    add initial test

---
+++ frontend/app/tests/e2e/objects/convert/object-convert.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from "@playwright/test";
+
+import { ACCOUNT_STATE_PATH } from "../../../constants";
+
+test.describe.fixme("Object details - convert", () => {
+  test.describe.configure({ mode: "serial" });
+  test.use({ storageState: ACCOUNT_STATE_PATH.ADMIN });
+  test.slow();
+
+  test.describe("when logged in", async () => {
+    test.use({ storageState: ACCOUNT_STATE_PATH.ADMIN });
+
+    test("should access the convert page", async ({ page }) => {
+      await page.goto("http://localhost:8080/objects/InfraInterface");
+      await page.getByRole("link", { name: "atl1-edge1, Ethernet1", exact: true }).click();
+      await page.getByTestId("object-details-action-button").click();
+      await page.getByRole("menuitem", { name: "Convert object type" }).click();
+      await expect(page.getByText("SOURCE")).toBeVisible();
+      await expect(page.getByText("NameEthernet1")).toBeVisible();
+    });
+  });
+});
