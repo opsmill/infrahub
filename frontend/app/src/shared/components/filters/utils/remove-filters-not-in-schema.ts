bc543a44a809cbbd49b937afd9bb33e312ecdc19
bilalabbad <bilal@opsmill.com>
2025-09-12 10:44:21 +0200

    Prevent available IPs filter when switching kind in IPAM list views

--- frontend/app/src/shared/components/filters/utils/remove-filters-not-in-schema.ts
+++ frontend/app/src/shared/components/filters/utils/remove-filters-not-in-schema.ts
@@ -1,6 +1,12 @@
 import { Filter } from "@/shared/hooks/useFilters";
 
+import {
+  AVAILABLE_IP_FILTER_NAME,
+  IP_ADDRESS_GENERIC,
+  IP_PREFIX_GENERIC,
+} from "@/entities/ipam/constants";
 import { ModelSchema } from "@/entities/schema/types";
+import { isOfKind } from "@/entities/schema/utils/is-of-kind";
 
 export const removeFiltersNotInSchema = (filters: Filter[], schema: ModelSchema | null) => {
   if (!schema) {
@@ -8,6 +14,13 @@ export const removeFiltersNotInSchema = (filters: Filter[], schema: ModelSchema 
   }
 
   return filters.filter((filter) => {
+    if (
+      (isOfKind(IP_PREFIX_GENERIC, schema) || isOfKind(IP_ADDRESS_GENERIC, schema)) &&
+      filter.name === AVAILABLE_IP_FILTER_NAME
+    ) {
+      return true;
+    }
+
     const [fieldName] = filter.name.split("__");
     return (
       schema.attributes?.some((attr) => attr.name === fieldName) ||
