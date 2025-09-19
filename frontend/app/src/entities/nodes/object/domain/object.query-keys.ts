1c137f455b647f463c58879693e17dcec7cbc9a8
Paul Leménager <pa-lem@hotmail.fr>
2025-09-19 16:42:44 +0200

    Add form from selected kind for object convert type (#7213)
    * add convert page with breadcrumb and kind select
    
    * :wrench:
    
    * :wrench:
    
    * sort
    
    * :wrench:
    
    * :wrench:
    
    * sort
    
    * remove unused
    
    * :wrench:
    
    * sort imports
    
    * add details
    
    * style
    
    * start
    
    * add initial test
    
    * cleanup test
    
    * :wrench:
    
    * :wrench:
    
    * :wrench:
    
    * fix test
    
    * start add form
    
    * :wrench:
    
    * get mappings and provide current object details data
    
    * fix name
    
    * fix open
    
    * :wrench:
    
    * use schema
    
    * update domain and api, rename files
    
    * use query factory
    
    * fix imports
    
    * rename
    
    * rename
    
    * fix imports
    
    * renamed with explicit context + fixed TS errors + better typing and interfaces
    
    ---------
    
    Co-authored-by: bilalabbad <bilal@opsmill.com>

--- frontend/app/src/entities/nodes/object/domain/object.query-keys.ts
+++ frontend/app/src/entities/nodes/object/domain/object.query-keys.ts
@@ -13,12 +13,23 @@ export interface ObjectDetailKeysParams extends ObjectKeysBaseParams {
   objectId: string;
 }
 
+export interface ObjectConvertFieldsMappingKeysParams extends ContextParams {
+  sourceKind: string;
+  targetKind: string;
+}
+
 export const objectQueryKeys = {
   all: ["objects"] as const,
   allWithContext: ({ branchName, atDate }: ContextParams) =>
     [...objectQueryKeys.all, branchName, atDate] as const,
   lists: (params: ObjectKeysBaseParams) =>
     [...objectQueryKeys.allWithContext(params), params.objectKind] as const,
+  convert: (params: ObjectConvertFieldsMappingKeysParams) =>
+    [
+      ...objectQueryKeys.lists({ ...params, objectKind: params.sourceKind }),
+      "fields-mapping-type-conversion",
+      params.targetKind,
+    ] as const,
   count: (params: ObjectKeysBaseParams) => [...objectQueryKeys.lists(params), "count"] as const,
   list: (params: ObjectListKeysParams) =>
     [...objectQueryKeys.lists(params), params.filters] as const,
