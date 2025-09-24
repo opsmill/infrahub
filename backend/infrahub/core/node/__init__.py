eacbe64921a9aec4379479f8f00a50bb74bbb156
Guillaume Mazoyer <guillaume@opsmill.com>
2025-09-24 11:30:21 +0200

    Add `DisplayLabel` class
    This class is used to keep track of the display label on a per node
    basis. It registers which fields are used to build the display label to
    be able to know if the display label needs to be updated when the node
    is.

--- backend/infrahub/core/node/__init__.py
+++ backend/infrahub/core/node/__init__.py
@@ -834,11 +834,16 @@ class Node(BaseNode, metaclass=BaseNodeMeta):
         update_at = Timestamp(at)
         node_changelog = NodeChangelog(node_id=self.get_id(), node_kind=self.get_kind(), display_label="")
 
-        if fields:
-            fields.append("_display_label")
+        # Update the display label if one of its variable is being updated
+        if self._display_label and self._display_label.needs_update(fields=fields):
+            await self._display_label.compute(db=db, node=self)
+            attr = await self._display_label.get_node_attribute(node=self, at=update_at)
+            updated_attribute = await attr.save(at=update_at, db=db)
+            if updated_attribute:
+                node_changelog.add_attribute(attribute=updated_attribute)
 
         # Go over the list of Attribute and update them one by one
-        for name in self._attributes + ["_display_label"]:
+        for name in self._attributes:
             if (fields and name in fields) or not fields:
                 attr: BaseAttribute = getattr(self, name)
                 updated_attribute = await attr.save(at=update_at, db=db)
