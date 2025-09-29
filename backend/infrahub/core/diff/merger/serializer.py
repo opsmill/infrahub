97e8dcf23b3c549584240bfd58b568c62153d736
Guillaume Mazoyer <guillaume@opsmill.com>
2025-09-29 13:22:12 +0200

    Fix merge of schema nodes

--- backend/infrahub/core/diff/merger/serializer.py
+++ backend/infrahub/core/diff/merger/serializer.py
@@ -157,7 +157,7 @@ class DiffMergeSerializer:
                 continue
             serial_attr_diffs = []
             for attr_diff in node.attributes:
-                if attr_diff.name in ATTRIBUTES_TO_IGNORE:
+                if attr_diff.name in ATTRIBUTES_TO_IGNORE and not node.kind.startswith("Schema"):
                     continue
                 serial_attr_diff, attribute_property_diff = self._serialize_attribute(
                     attribute_diff=attr_diff, node_uuid=node.uuid, node_kind=node.kind
