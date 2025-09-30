a8ed4790860478fb568516d8681a4cc61c4fd8af
Patrick Ogenstad <patrick@ogenstad.com>
2025-09-30 19:14:24 +0200

    Add mutation and events for HFID

--- backend/infrahub/graphql/schema.py
+++ backend/infrahub/graphql/schema.py
@@ -21,6 +21,7 @@ from .mutations.diff import DiffUpdateMutation
 from .mutations.diff_conflict import ResolveDiffConflict
 from .mutations.display_label import UpdateDisplayLabel
 from .mutations.generator import GeneratorDefinitionRequestRun
+from .mutations.hfid import UpdateHFID
 from .mutations.profile import InfrahubProfilesRefresh
 from .mutations.proposed_change import (
     ProposedChangeCheckForApprovalRevoke,
@@ -116,6 +117,7 @@ class InfrahubBaseMutation(ObjectType):
     InfrahubRepositoryConnectivity = ValidateRepositoryConnectivity.Field()
     InfrahubUpdateComputedAttribute = UpdateComputedAttribute.Field()
     InfrahubUpdateDisplayLabel = UpdateDisplayLabel.Field()
+    InfrahubUpdateHFID = UpdateHFID.Field()
     InfrahubRecomputeComputedAttribute = RecomputeComputedAttribute.Field()
 
     RelationshipAdd = RelationshipAdd.Field()
