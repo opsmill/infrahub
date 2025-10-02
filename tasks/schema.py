19e8ff9e160c83d61526a53c4d3d01bad70f2ed6
solababs <solababatunde12@gmail.com>
2025-10-02 13:46:35 +0100

    update logic

--- tasks/schema.py
+++ tasks/schema.py
@@ -1,13 +1,20 @@
+import asyncio
 import json
 from pathlib import Path
 
 from invoke.context import Context
 from invoke.tasks import task
 
-from .utils import REPO_BASE
+from .utils import ESCAPED_REPO_PATH, REPO_BASE
 
 SDK_DIRECTORY = REPO_BASE / "generated" / "python-sdk"
 INFRAHUB_DIRECTORY = REPO_BASE / "generated" / "infrahub"
+
+
+@task
+def generate_graphqlschema(context: Context) -> None:
+    """Generate GraphQL schema into ./schema"""
+    asyncio.run(generate_graphql_schema(context))
 
 
 @task
@@ -16,6 +23,17 @@ def generate_jsonschema(context: Context) -> None:  # noqa: ARG001
 
     generate_sdk_repository_config()
     generate_infrahub_node_schema()
+
+
+@task
+def validate_graphqlschema(context: Context) -> None:
+    """Validate that the generated GraphQL schema is up to date."""
+
+    asyncio.run(generate_graphql_schema(context))
+
+    exec_cmd = "git diff --exit-code schema"
+    with context.cd(ESCAPED_REPO_PATH):
+        context.run(exec_cmd)
 
 
 def generate_infrahub_node_schema() -> None:
@@ -46,3 +64,52 @@ def generate_sdk_repository_config() -> None:
 def write(file_path: Path, content: str) -> None:
     file_path.write_text(content, encoding="utf-8")
     print(f"Wrote to {file_path}")
+
+
+async def generate_graphql_schema(context: Context) -> None:
+    import neo4j.exceptions
+    from graphql import print_schema
+
+    from infrahub import config
+    from infrahub.core.initialization import initialization
+    from infrahub.core.registry import registry
+    from infrahub.database import InfrahubDatabase, get_db
+    from infrahub.graphql.manager import GraphQLSchemaManager
+    from infrahub.lock import initialize_lock
+    from infrahub.services import InfrahubServices
+
+    config.load_and_exit()
+    initialize_lock(local_only=True)
+
+    db_loading = True
+    attempt = 1
+    driver = None
+    while db_loading:
+        try:
+            driver = await get_db()
+            db_loading = False
+        except neo4j.exceptions.ServiceUnavailable:
+            if attempt > 9:
+                raise
+            await asyncio.sleep(delay=1)
+            attempt += 1
+    database = InfrahubDatabase(driver=driver)
+    service = await InfrahubServices.new(database=database)
+
+    async with service.database.start_session() as db:
+        await initialization(db=db)
+
+    schema_branch = registry.schema.get_schema_branch(name=registry.default_branch)
+
+    gqlm = GraphQLSchemaManager(schema=schema_branch)
+    gql_schema = gqlm.generate(
+        include_query=True,
+        include_mutation=True,
+        include_subscription=True,
+        include_types=True,
+    )
+
+    schema_file = Path(f"{REPO_BASE}/schema/schema.graphql")
+    write(file_path=schema_file, content=print_schema(schema=gql_schema))
+    with context.cd(ESCAPED_REPO_PATH):
+        context.run("npm run format-graphql -- ./schema/schema.graphql --write")
