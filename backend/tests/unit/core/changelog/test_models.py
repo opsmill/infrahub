927abd3038483f686c5d91d01afcab5ac1f4337c
Guillaume Mazoyer <guillaume@opsmill.com>
2025-10-03 10:22:53 +0200

    Put back display_label for node changelog

--- backend/tests/unit/core/changelog/test_models.py
+++ backend/tests/unit/core/changelog/test_models.py
@@ -28,6 +28,7 @@ async def test_node_changelog_creation(db: InfrahubDatabase, default_branch, ani
     assert person1.node_changelog == NodeChangelog(
         node_id=person1.id,
         node_kind="TestPerson",
+        display_label="Jack",
         attributes={
             "human_friendly_id": AttributeChangelog(
                 name="human_friendly_id",
@@ -102,6 +103,7 @@ async def test_node_changelog_creation(db: InfrahubDatabase, default_branch, ani
     assert dog1.node_changelog == NodeChangelog(
         node_id=dog1.id,
         node_kind="TestDog",
+        display_label="Rocky Labrador",
         attributes={
             "human_friendly_id": AttributeChangelog(
                 name="human_friendly_id",
@@ -206,6 +208,7 @@ async def test_node_changelog_update_with_cardinality_one_relationship(
     assert dog1_update.node_changelog == NodeChangelog(
         node_id=dog1.id,
         node_kind="TestDog",
+        display_label="Rocky Labrador",
         attributes={
             "color": AttributeChangelog(
                 name="color", value="Brown", value_previous="#444444", properties={}, kind="Color"
