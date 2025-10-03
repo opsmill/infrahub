d3f6430d3c49d5119e8d3b1dc8a5dbc324f4828b
Guillaume Mazoyer <guillaume@opsmill.com>
2025-10-03 10:22:53 +0200

    Put back display_label for node changelog

--- backend/infrahub/core/changelog/models.py
+++ backend/infrahub/core/changelog/models.py
@@ -227,6 +227,7 @@ class NodeChangelog(BaseModel):
 
     node_id: str
     node_kind: str
+    display_label: str
 
     attributes: dict[str, AttributeChangelog] = Field(default_factory=dict)
     relationships: dict[str, RelationshipCardinalityOneChangelog | RelationshipCardinalityManyChangelog] = Field(
