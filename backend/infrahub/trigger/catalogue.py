2a2c8a587e9986fef6eda128d4af13e49c709739
Patrick Ogenstad <patrick@ogenstad.com>
2025-10-03 09:05:25 +0200

    Finalize HFID events

--- backend/infrahub/trigger/catalogue.py
+++ backend/infrahub/trigger/catalogue.py
@@ -5,6 +5,7 @@ from infrahub.computed_attribute.triggers import (
     TRIGGER_COMPUTED_ATTRIBUTE_PYTHON_SETUP_COMMIT,
 )
 from infrahub.display_labels.triggers import TRIGGER_DISPLAY_LABELS_ALL_SCHEMA
+from infrahub.hfid.triggers import TRIGGER_HFID_ALL_SCHEMA
 from infrahub.schema.triggers import TRIGGER_SCHEMA_UPDATED
 from infrahub.trigger.models import TriggerDefinition
 from infrahub.webhook.triggers import TRIGGER_WEBHOOK_DELETE, TRIGGER_WEBHOOK_SETUP_UPDATE
@@ -15,6 +16,7 @@ builtin_triggers: list[TriggerDefinition] = [
     TRIGGER_COMPUTED_ATTRIBUTE_ALL_SCHEMA,
     TRIGGER_COMPUTED_ATTRIBUTE_PYTHON_SETUP_COMMIT,
     TRIGGER_DISPLAY_LABELS_ALL_SCHEMA,
+    TRIGGER_HFID_ALL_SCHEMA,
     TRIGGER_SCHEMA_UPDATED,
     TRIGGER_WEBHOOK_DELETE,
     TRIGGER_WEBHOOK_SETUP_UPDATE,
