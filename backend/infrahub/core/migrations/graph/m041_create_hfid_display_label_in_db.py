c9201017a6e6254cee9a51e45961942b26293f38
Guillaume Mazoyer <guillaume@opsmill.com>
2025-10-06 17:26:42 +0200

    Fix migration adding hfid/display_label to nodes
    Also add a test to validate the migrations 41+42 combo

--- backend/infrahub/core/migrations/graph/m041_create_hfid_display_label_in_db.py
+++ backend/infrahub/core/migrations/graph/m041_create_hfid_display_label_in_db.py
@@ -4,12 +4,14 @@ from typing import TYPE_CHECKING, Any
 
 from typing_extensions import Self
 
+from infrahub.core import registry
 from infrahub.core.constants import SchemaPathType
-from infrahub.core.migrations.shared import MigrationResult
+from infrahub.core.initialization import initialization
+from infrahub.core.migrations.schema.node_attribute_add import NodeAttributeAddMigration
+from infrahub.core.migrations.shared import InternalSchemaMigration, MigrationResult
 from infrahub.core.path import SchemaPath
-
-from ..schema.node_attribute_add import NodeAttributeAddMigration
-from ..shared import InternalSchemaMigration
+from infrahub.core.schema import NodeSchema
+from infrahub.lock import initialize_lock
 
 if TYPE_CHECKING:
     from infrahub.database import InfrahubDatabase
@@ -17,31 +19,73 @@ if TYPE_CHECKING:
 
 class Migration041(InternalSchemaMigration):
     name: str = "041_create_hfid_display_label_in_db"
-    minimum_version: int = 39  # Will switch to 40 later
+    minimum_version: int = 39  # FIXME: Will switch to 40 later
 
     @classmethod
     def init(cls, **kwargs: Any) -> Self:
         internal_schema = cls.get_internal_schema()
         schema_node = internal_schema.get_node(name="SchemaNode")
 
-        migrations = [
+        cls.migrations = [
+            # HFID is not needed, it was introduced at graph v8
             NodeAttributeAddMigration(
                 new_node_schema=schema_node,
                 previous_node_schema=schema_node,
                 schema_path=SchemaPath(
                     schema_kind="SchemaNode", path_type=SchemaPathType.ATTRIBUTE, field_name="display_label"
                 ),
-            ),
-            # NOTE: This already exists, introduced by Migration008
-            # NodeAttributeAddMigration(
-            #     new_node_schema=schema_node,
-            #     previous_node_schema=schema_node,
-            #     schema_path=SchemaPath(
-            #         schema_kind="SchemaNode", path_type=SchemaPathType.ATTRIBUTE, field_name="human_friendly_id"
-            #     ),
-            # ),
+            )
         ]
-        return cls(migrations=migrations, **kwargs)  # type: ignore[arg-type]
+        return cls(migrations=cls.migrations, **kwargs)  # type: ignore[arg-type]
+
+    async def execute(self, db: InfrahubDatabase) -> MigrationResult:
+        result = MigrationResult()
+
+        # load schemas from database into registry
+        initialize_lock()
+        await initialization(db=db)
+
+        default_branch = registry.get_branch_from_registry()
+        schema_branch = await registry.schema.load_schema_from_db(db=db, branch=default_branch)
+
+        migrations = list(self.migrations)
+
+        for node_schema_kind in schema_branch.node_names:
+            schema = schema_branch.get_node(name=node_schema_kind, duplicate=False)
+            if not isinstance(schema, NodeSchema):
+                continue
+
+            if schema.human_friendly_id:
+                migrations.append(
+                    NodeAttributeAddMigration(
+                        new_node_schema=schema,
+                        previous_node_schema=schema,
+                        schema_path=SchemaPath(
+                            schema_kind=schema.kind, path_type=SchemaPathType.ATTRIBUTE, field_name="human_friendly_id"
+                        ),
+                    )
+                )
+
+            if schema.display_label or schema.display_labels:
+                migrations.append(
+                    NodeAttributeAddMigration(
+                        new_node_schema=schema,
+                        previous_node_schema=schema,
+                        schema_path=SchemaPath(
+                            schema_kind=schema.kind, path_type=SchemaPathType.ATTRIBUTE, field_name="display_label"
+                        ),
+                    )
+                )
+
+        for migration in migrations:
+            try:
+                execution_result = await migration.execute(db=db, branch=default_branch)
+                result.errors.extend(execution_result.errors)
+            except Exception as exc:
+                result.errors.append(str(exc))
+                return result
+
+        return result
 
     async def validate_migration(self, db: InfrahubDatabase) -> MigrationResult:  # noqa: ARG002
         return MigrationResult()
