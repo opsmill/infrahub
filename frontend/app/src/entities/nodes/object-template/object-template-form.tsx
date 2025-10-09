ad9d86d8a1c0672ca743bf0bfe1f5055f8feae12
Alex Gittings <agitting96@gmail.com>
2025-10-09 10:32:46 +0100

    Refactor profile relationship settings and enhance profile handling in forms

--- frontend/app/src/entities/nodes/object-template/object-template-form.tsx
+++ frontend/app/src/entities/nodes/object-template/object-template-form.tsx
@@ -80,13 +80,7 @@ export default function ObjectTemplateForm({
   }
 
   if (selectedObjectTemplate !== undefined) {
-    return (
-      <ObjectForm
-        {...props}
-        currentProfiles={selectedObjectTemplate?.profiles?.edges?.map((edge: any) => edge?.node)}
-        objectTemplate={selectedObjectTemplate}
-      />
-    );
+    return <ObjectForm {...props} currentProfiles={selectedObjectTemplate?.profiles?.edges?.map((edge: any) => edge?.node) ?? []} objectTemplate={selectedObjectTemplate} />;
   }
 
   return (
