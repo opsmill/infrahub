c0b8cec51c97f7db39de84a50ee9ce6c8571763f
pa-lem <pa-lem@hotmail.fr>
2025-10-09 10:41:20 +0200

    lint

--- frontend/app/src/entities/nodes/convert/ui/convert-source-attribute-input.tsx
+++ frontend/app/src/entities/nodes/convert/ui/convert-source-attribute-input.tsx
@@ -49,12 +49,12 @@ export const ConvertSourceAttributeCombobox = ({
       }
 
       if (attribute.kind === "Dropdown") {
-        const sortedAttributeDropdownOptions = attribute?.choices?.sort(
-          (itemA, itemB) => itemA.name.localeCompare(itemB.name)
-        );
-        const sortedSourceDropdownOptions = sourceAttribute?.choices?.sort(
-          (itemA, itemB) => itemA.name.localeCompare(itemB.name)
-        );
+        const sortedAttributeDropdownOptions = attribute?.choices?.sort((itemA, itemB) => {
+          return itemA.name.localeCompare(itemB.name);
+        });
+        const sortedSourceDropdownOptions = sourceAttribute?.choices?.sort((itemA, itemB) => {
+          return itemA.name.localeCompare(itemB.name);
+        });
         const areEqual =
           JSON.stringify(sortedAttributeDropdownOptions) ===
           JSON.stringify(sortedSourceDropdownOptions);
