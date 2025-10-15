82bb8abc9e13812321f2cd6317d54eb9e6541ff8
pa-lem <pa-lem@hotmail.fr>
2025-10-15 16:35:44 +0200

    handle errors

--- frontend/app/src/entities/diff/domain/resolve-conflict.ts
+++ frontend/app/src/entities/diff/domain/resolve-conflict.ts
@@ -6,5 +6,11 @@ import {
 export type ResolveConflict = (params: ResolveConflictFromApiParams) => Promise<void>;
 
 export const resolveConflict: ResolveConflict = async (params) => {
-  await resolveConflictFromApi(params);
+  const { data, errors } = await resolveConflictFromApi(params);
+
+  if (errors?.[0]?.message) {
+    throw new Error(errors[0].message);
+  }
+
+  return data;
 };
