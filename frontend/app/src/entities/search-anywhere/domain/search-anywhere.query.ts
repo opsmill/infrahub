baa4f47397f44e7d48584dd643ca92c40603510c
bilalabbad <bilal@opsmill.com>
2025-10-15 17:09:07 +0200

    search anywhere convention

---
+++ frontend/app/src/entities/search-anywhere/domain/search-anywhere.query.ts
@@ -0,0 +1,41 @@
+import { queryOptions, useQuery } from "@tanstack/react-query";
+import { useAtomValue } from "jotai";
+
+import type { ContextParams, QueryConfig } from "@/shared/api/types";
+import { datetimeAtom } from "@/shared/stores/time.atom";
+
+import { useCurrentBranch } from "@/entities/branches/ui/branches-provider";
+import {
+  type SearchAnywhereParams,
+  searchAnywhere,
+} from "@/entities/search-anywhere/domain/search-anywhere";
+
+type SearchObjectsQueryOptionsParams = SearchAnywhereParams;
+
+export function searchAnywhereQueryOptions({
+  branchName,
+  search,
+  atDate,
+}: SearchObjectsQueryOptionsParams) {
+  return queryOptions({
+    queryKey: [branchName, atDate, "search-objects", search],
+    queryFn: async () => searchAnywhere({ branchName, search, atDate }),
+  });
+}
+
+export function useGetSearchAnywhere(
+  params: Omit<SearchObjectsQueryOptionsParams, keyof ContextParams>,
+  config?: QueryConfig<typeof searchAnywhereQueryOptions>
+) {
+  const { currentBranch } = useCurrentBranch();
+  const timeMachineDate = useAtomValue(datetimeAtom);
+
+  return useQuery({
+    ...searchAnywhereQueryOptions({
+      branchName: currentBranch.name,
+      atDate: timeMachineDate,
+      ...params,
+    }),
+    ...config,
+  });
+}
