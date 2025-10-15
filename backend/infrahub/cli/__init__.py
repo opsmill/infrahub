8327446eb09ccb312564021c4782241fc757c81d
Damien Garros <dgarros@gmail.com>
2025-10-15 06:37:11 +0200

    Add command to export infrahub graphql schema

--- backend/infrahub/cli/__init__.py
+++ backend/infrahub/cli/__init__.py
@@ -9,6 +9,7 @@ from .context import CliContext
 from .db import app as db_app
 from .events import app as events_app
 from .git_agent import app as git_app
+from .schema import app as schema_app
 from .server import app as server_app
 from .tasks import app as tasks_app
 from .upgrade import upgrade_cmd
@@ -27,6 +28,7 @@ app.add_typer(git_app, name="git-agent", hidden=True)
 app.add_typer(db_app, name="db")
 app.add_typer(events_app, name="events", help="Interact with the events system.", hidden=True)
 app.add_typer(tasks_app, name="tasks", hidden=True)
+app.add_typer(schema_app, name="schema")
 app.command(name="upgrade")(upgrade_cmd)
 
 
