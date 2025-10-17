5ea929dc56d48e68446ef06f11e219ff2f4c643b
Babatunde Olusola <solababatunde12@gmail.com>
2025-10-17 12:13:31 +0100

    Generate order_weight for generic templates (#7406)
    * Generate order_weight for generic templates
    
    * set template, update test
    
    * exclude template from generated weights
    
    * update test
    
    * update test
    
    * update test case

--- backend/tests/unit/core/schema_manager/test_manager_schema.py
+++ backend/tests/unit/core/schema_manager/test_manager_schema.py
@@ -605,6 +605,106 @@ async def test_schema_branch_generate_weight(schema_all_in_one):
     assert len(in_second) == 1 and in_second[0].startswith(new_attr2_partial_id)
 
 
+def test_schema_branch_processes_generic_template_schema_weight(register_core_models_schema):
+    schema = {
+        "generics": [
+            {
+                "name": "GenericDevice",
+                "namespace": "Dcim",
+                "description": "Generic Device object.",
+                "label": "Device",
+                "icon": "mdi:server",
+                "human_friendly_id": ["name__value"],
+                "order_by": ["name__value"],
+                "display_labels": ["name__value"],
+                "generate_template": True,
+                "attributes": [
+                    {"name": "name", "kind": "Text", "unique": True, "order_weight": 7000},
+                    {"name": "description", "kind": "Text", "optional": True, "order_weight": 8000},
+                    {"name": "os_version", "kind": "Text", "optional": True, "order_weight": 5200},
+                ],
+                "relationships": [
+                    {
+                        "name": "tags",
+                        "peer": "BuiltinTag",
+                        "optional": True,
+                        "cardinality": "many",
+                        "kind": "Attribute",
+                        "order_weight": 3000,
+                    },
+                ],
+            },
+            core_object_template,
+            core_object_component_template,
+        ],
+        "nodes": [
+            {
+                "name": "Tag",
+                "namespace": "Builtin",
+                "label": "Tag",
+                "default_filter": "name__value",
+                "attributes": [
+                    {"name": "name", "kind": "Text", "label": "Name", "unique": True},
+                    {
+                        "name": "description",
+                        "kind": "Text",
+                        "label": "Description",
+                        "optional": True,
+                        "branch": BranchSupportType.AGNOSTIC.value,
+                    },
+                ],
+            },
+        ],
+    }
+    schema_branch = SchemaBranch(cache={}, name="test")
+    schema_branch.load_schema(schema=SchemaRoot(**schema))
+    schema_branch.process()
+
+    template = schema_branch.get(name="TemplateDcimGenericDevice", duplicate=False)
+    dcim_generic_device = schema_branch.get(name="DcimGenericDevice", duplicate=False)
+
+    assert template.get_attribute(name="template_name").order_weight == 1000
+    assert (
+        template.get_attribute(name="description").order_weight
+        == dcim_generic_device.get_attribute(name="description").order_weight
+        == 8000
+    )
+    assert (
+        template.get_attribute(name="os_version").order_weight
+        == dcim_generic_device.get_attribute(name="os_version").order_weight
+        == 5200
+    )
+    assert (
+        template.get_relationship(name="tags").order_weight
+        == dcim_generic_device.get_relationship(name="tags").order_weight
+        == 3000
+    )
+
+    schema_2 = copy.deepcopy(schema)
+    schema_2["generics"][0]["attributes"] = [
+        {"name": "name", "kind": "Text", "unique": False},
+        {"name": "description", "kind": "Text", "optional": True},
+        {"name": "os_version", "kind": "Text", "optional": True},
+    ]
+    schema_branch.load_schema(schema=SchemaRoot(**schema_2))
+    schema_branch.process()
+
+    template = schema_branch.get(name="TemplateDcimGenericDevice", duplicate=False)
+    dcim_generic_device = schema_branch.get(name="DcimGenericDevice", duplicate=False)
+
+    assert (
+        template.get_attribute(name="name").order_weight == dcim_generic_device.get_attribute(name="name").order_weight
+    )
+    assert (
+        template.get_attribute(name="description").order_weight
+        == dcim_generic_device.get_attribute(name="description").order_weight
+    )
+    assert (
+        template.get_attribute(name="os_version").order_weight
+        == dcim_generic_device.get_attribute(name="os_version").order_weight
+    )
+
+
 async def test_schema_branch_add_profile_schema(schema_all_in_one):
     core_profile_schema = _get_schema_by_kind(core_models, kind=InfrahubKind.PROFILE)
     schema_all_in_one["generics"].append(core_profile_schema)
