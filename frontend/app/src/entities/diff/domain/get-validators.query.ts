a917c4332617c9a63975815f4b1d0afe9989e1ab
pa-lem <pa-lem@hotmail.fr>
2025-10-20 16:10:40 +0200

    update query

--- frontend/app/src/entities/diff/domain/get-validators.query.ts
+++ frontend/app/src/entities/diff/domain/get-validators.query.ts
@@ -1,12 +1,13 @@
 import { queryOptions, useQuery } from "@tanstack/react-query";
 
 import { GetValidatorsFromApiParams } from "../api/get-validators-from-api";
+import { proposedChangeValidatorsKeys } from "./diff.query-keys";
 import { getValidators } from "./get-validators";
 
 export const useGetValidatorsQuery = (params: GetValidatorsFromApiParams) => {
   return useQuery(
     queryOptions({
-      queryKey: ["proposed-change-validators", params.proposedChangeId],
+      queryKey: proposedChangeValidatorsKeys.allWithinProposedChange(params.proposedChangeId),
       queryFn: () => getValidators(params),
     })
   );
