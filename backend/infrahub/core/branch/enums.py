a692887db19e675da25ba9766a9b7d9f3141c50b
Guillaume Mazoyer <guillaume@opsmill.com>
2025-10-22 09:33:16 +0200

    Add need upgrade rebase branch status

--- backend/infrahub/core/branch/enums.py
+++ backend/infrahub/core/branch/enums.py
@@ -4,5 +4,6 @@ from infrahub.utils import InfrahubStringEnum
 class BranchStatus(InfrahubStringEnum):
     OPEN = "OPEN"
     NEED_REBASE = "NEED_REBASE"
+    NEED_UPGRADE_REBASE = "NEED_UPGRADE_REBASE"
     CLOSED = "CLOSED"
     DELETING = "DELETING"
