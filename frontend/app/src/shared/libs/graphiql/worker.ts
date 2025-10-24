b5b1be94feb0ae6d085bfa5ff880fd10acca85de
bilalabbad <bilal@opsmill.com>
2025-10-24 11:07:48 +0200

    attempt 2

--- frontend/app/src/shared/libs/graphiql/worker.ts
+++ frontend/app/src/shared/libs/graphiql/worker.ts
@@ -1,7 +1,7 @@
 // @ts-expect-error -- no types
-import { initialize } from "monaco-editor/esm/vs/editor/editor.worker?worker";
+import { initialize } from "monaco-editor/esm/vs/editor/editor.worker";
 // @ts-expect-error -- no types
-import { GraphQLWorker } from "monaco-graphql/esm/GraphQLWorker?worker";
+import { GraphQLWorker } from "monaco-graphql/esm/GraphQLWorker";
 
 globalThis.onmessage = () => {
   initialize((ctx: any, createData: any) => new GraphQLWorker(ctx, createData));
