d0b8988cd31e48b0496435230b70f4b9c9cf8bcb
Bilal ABBAD <bilal@opsmill.com>
2025-10-24 09:31:23 +0200

    Cleanup GraphiQL setup workers (#7469)

--- frontend/app/src/shared/libs/graphiql/worker.ts
+++ frontend/app/src/shared/libs/graphiql/worker.ts
@@ -1,8 +1,8 @@
+// @ts-expect-error -- no types
 import { initialize } from "monaco-editor/esm/vs/editor/editor.worker";
+// @ts-expect-error -- no types
 import { GraphQLWorker } from "monaco-graphql/esm/GraphQLWorker";
 
-// Minimal worker bootstrap without sourcemap footer
 globalThis.onmessage = () => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   initialize((ctx: any, createData: any) => new GraphQLWorker(ctx, createData));
 };
