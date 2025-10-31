c213557a2e71264c27f85d0220b2df958aa98740
Patrick Ogenstad <patrick@ogenstad.com>
2025-10-31 14:19:21 +0100

    Fix ruff warning

--- backend/infrahub/core/migrations/shared.py
+++ backend/infrahub/core/migrations/shared.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Sequence, TypeAlias
+from typing import TYPE_CHECKING, Any, Sequence
 
 from pydantic import BaseModel, ConfigDict, Field
 from typing_extensions import Self
@@ -247,4 +247,4 @@ class MigrationRequiringRebase(BaseModel):
         raise NotImplementedError()
 
 
-MigrationTypes: TypeAlias = GraphMigration | InternalSchemaMigration | ArbitraryMigration | MigrationRequiringRebase
+type MigrationTypes = GraphMigration | InternalSchemaMigration | ArbitraryMigration | MigrationRequiringRebase
