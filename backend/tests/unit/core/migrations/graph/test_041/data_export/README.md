33bbc9be3c8b22ec25e50a87e042d1cfd91308f3
Aaron McCarty <aaron@opsmill.com>
2025-11-04 08:48:15 -0800

    finish tests for Migration041

--- backend/tests/unit/core/migrations/graph/test_041/data_export/README.md
+++ backend/tests/unit/core/migrations/graph/test_041/data_export/README.md
@@ -1,3 +1,4 @@
 Small dataset from a unit test before the fix that includes
+
 - edges added to migrated kind nodes during a merge after the migrated kind node was deleted on main
 - duplicated edges on relationships touching migrated kind nodes added during a merge
--- backend/tests/unit/core/migrations/graph/test_041/test_041.py
+++ backend/tests/unit/core/migrations/graph/test_041/test_041.py
@@ -4,6 +4,7 @@ import pytest
 
 from infrahub.cli.db import load_export
 from infrahub.core.branch import Branch
+from infrahub.core.manager import NodeManager
 from infrahub.core.migrations.graph.m041_deleted_dup_edges import Migration041
 from infrahub.core.schema.schema_branch import SchemaBranch
 from infrahub.database import InfrahubDatabase
@@ -11,31 +12,30 @@ from tests.helpers.db_validation import verify_no_duplicate_paths
 
 
 class TestMigration041:
+    """Test Migration041 to delete duplicated edges after a merge that includes peers with updated kinds/inheritance
+    - load a known bad data set, then massage it into a state that can be used by the migration and NodeManager
+    - get the nodes before the migration
+    - run the migration
+    - verify the duplicate paths are all gone
+    - get the nodes after the migration
+    - verify the nodes attribute values and peers are the same
+    """
+
     @pytest.fixture
     async def load_bad_data(
         self,
         db: InfrahubDatabase,
         default_branch: Branch,
     ):
-        # await delete_all_nodes(db=db)
         export_dir = Path(__file__).parent / ("data_export")
         await load_export(db=db, export_dir=export_dir)
-        # set default branch
+        # set default branch on the import Root node
         query = """
 MATCH (r:Root)
 SET r.default_branch = "main"
         """
         await db.execute_query(query=query)
-        # # export does not include Branches and we need them for the migration
-        # branch = Branch(
-        #     name="main",
-        #     status=BranchStatus.OPEN,
-        #     hierarchy_level=2,
-        #     is_default=True,
-        #     sync_with_git=False,
-        # )
-        # await branch.save(db=db)
-
+        # make the branch_level properties integers
         query = """
 MATCH ()-[e]->()
 WHERE e.branch_level IS NOT NULL
@@ -80,7 +80,7 @@ CALL () {
 }
         """
         await db.execute_query(query=query)
-        # delete the other Root
+        # delete the other Root created in the fixture chain
         query = """
 MATCH (r:Root)
 WHERE NOT exists((:TestPerson)-[:IS_PART_OF]->(r))
@@ -105,15 +105,17 @@ DETACH DELETE r
             else:
                 db.schema.set(name=schema_name, schema=car_person_schema.get(name=schema_name, duplicate=False))
 
-        # before_nodes_map = await NodeManager.get_many(
-        #     db=db,
-        #     ids=[
-        #         "18749df3-97b6-870c-43e8-1677b956a31e",
-        #         "18749df4-3639-d70a-43ed-1677223041be",
-        #         "18749df4-092d-b550-43e0-1677bd841482",
-        #         "18749df4-38df-703f-43ed-1677fa762816",
-        #     ],
-        # )
+        expected_node_ids = [
+            "18749df3-97b6-870c-43e8-1677b956a31e",
+            "18749df4-3639-d70a-43ed-1677223041be",
+            "18749df4-092d-b550-43e0-1677bd841482",
+        ]
+        deleted_node_ids = [
+            "18749df4-38df-703f-43ed-1677fa762816",
+        ]
+        all_node_ids = expected_node_ids + deleted_node_ids
+        before_nodes_map = await NodeManager.get_many(db=db, ids=all_node_ids, prefetch_relationships=True)
+        assert set(before_nodes_map.keys()) == set(expected_node_ids)
 
         migration = Migration041()
         execution_result = await migration.execute(db=db)
@@ -121,4 +123,30 @@ DETACH DELETE r
 
         await verify_no_duplicate_paths(db=db)
 
-        # TODO: verify node values and relationships are still correct
+        after_nodes_map = await NodeManager.get_many(
+            db=db,
+            ids=all_node_ids,
+            prefetch_relationships=True,
+        )
+        assert set(after_nodes_map.keys()) == set(expected_node_ids)
+
+        for node_id in expected_node_ids:
+            before_node = before_nodes_map[node_id]
+            after_node = after_nodes_map[node_id]
+            # make sure we are getting the same Node from the database b/c the TestCar and Test2NewCar duplicates have the same UUID
+            assert before_node.db_id == after_node.db_id
+            for attr_name in before_node.get_schema().attribute_names:
+                before_attr = getattr(before_node, attr_name)
+                after_attr = getattr(after_node, attr_name)
+                assert before_attr.value == after_attr.value
+            for rel_name in before_node.get_schema().relationship_names:
+                before_relm = getattr(before_node, rel_name)
+                before_peers = await before_relm.get_peers(db=db)
+                after_relm = getattr(after_node, rel_name)
+                after_peers = await after_relm.get_peers(db=db)
+
+                assert set(before_peers.keys()) == set(after_peers.keys())
+                for peer_id in before_peers:
+                    before_peer = before_peers[peer_id]
+                    after_peer = after_peers[peer_id]
+                    assert before_peer.db_id == after_peer.db_id
