0277b48c1f24cca0659661dc21e960b81a46c9dd
Guillaume Mazoyer <guillaume@opsmill.com>
2025-11-04 13:06:52 +0100

    Restore if clause

--- backend/infrahub/core/migrations/graph/m044_backfill_hfid_display_label_in_db.py
+++ backend/infrahub/core/migrations/graph/m044_backfill_hfid_display_label_in_db.py
@@ -692,7 +692,7 @@ class Migration044(MigrationRequiringRebase):
                 )
                 await update_display_label_query.execute(db=db)
 
-            if progress and update_task is not None:
+            if progress is not None and update_task is not None:
                 progress.update(update_task, advance=num_updates)
 
             if num_updates == 0:
