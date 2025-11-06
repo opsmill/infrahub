6a3a8c735d057b744b0dba8787da008cfa30ed1b
bilalabbad <bilal@opsmill.com>
2025-11-06 14:29:02 +0100

    cleanup

--- frontend/app/src/entities/resource-manager/api/resource-pool.ts
+++ frontend/app/src/entities/resource-manager/api/resource-pool.ts
@@ -1,19 +1,5 @@
 import { gql } from "@apollo/client";
 
-export const GET_KIND_FOR_RESOURCE_POOL = gql`
-  query GET_KIND_FOR_RESOURCE_POOL($ids: [ID]) {
-    CoreResourcePool(ids: $ids) {
-      edges {
-        node {
-          id
-          display_label
-          __typename
-        }
-      }
-    }
-  }
-`;
-
 export const GET_RESOURCE_POOL_ALLOCATED = gql`
   query GET_RESOURCE_POOL_ALLOCATED(
     $poolId: String!
