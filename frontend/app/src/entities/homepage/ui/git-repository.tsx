bc9c193b3a74d0491644fb1f1819dd2f2002917a
pa-lem <pa-lem@hotmail.fr>
2025-11-06 10:40:21 +0100

    add link

--- frontend/app/src/entities/homepage/ui/git-repository.tsx
+++ frontend/app/src/entities/homepage/ui/git-repository.tsx
@@ -1,11 +1,20 @@
+import { GENERIC_REPOSITORY_KIND } from "@/config/constants";
+
 import type { CoreRepository } from "@/shared/api/graphql/generated/graphql";
+import { constructPath } from "@/shared/api/rest/fetch";
+import { Link } from "@/shared/components/ui/link";
 import { Tooltip } from "@/shared/components/ui/tooltip";
 import { classNames, getTextColor } from "@/shared/utils/common";
 
-export const GitRepositoryItem = ({ display_label, sync_status }: CoreRepository) => {
+export const GitRepositoryItem = ({ id, display_label, sync_status }: CoreRepository) => {
   return (
-    <div className="flex items-center justify-between p-4">
-      <div className="flex items-center gap-1">{display_label}</div>
+    <div className="flex items-center justify-between p-4 text-sm">
+      <Link
+        className="flex items-center gap-1"
+        to={constructPath(`objects/${GENERIC_REPOSITORY_KIND}/${id}`)}
+      >
+        {display_label}
+      </Link>
       <Tooltip enabled={!!sync_status?.description} content={sync_status?.description}>
         <div
           className={classNames("rounded-full px-3 py-1.5")}
