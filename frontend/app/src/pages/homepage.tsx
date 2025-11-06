51551993142aa571dbdb1277959e6577345b4f17
pa-lem <pa-lem@hotmail.fr>
2025-11-06 10:40:28 +0100

    change class

--- frontend/app/src/pages/homepage.tsx
+++ frontend/app/src/pages/homepage.tsx
@@ -11,9 +11,7 @@ const Homepage = () => {
       <div className="grid grid-cols-3">
         <div className="col-span-2 max-h-44" />
 
-        <div className="max-h-44">
-          <GitRepositories />
-        </div>
+        <GitRepositories className="min-h-44" />
       </div>
 
       <GettingStarted />
