a8789e4af3102fbe13493c416fd5ff370ea02440
Paul Leménager <pa-lem@hotmail.fr>
2025-11-07 16:07:12 +0100

    Add git repositories in landing page - IFC-1971 (#7571)
    * start add git repo layout
    
    * remove useless
    
    * add empty screen
    
    * add git repo list
    
    * add git icon
    
    * update types but not proud
    
    * upate changelog
    
    * update icons
    
    * fix loading
    
    * update empty card
    
    * update empty state and props
    
    * add link
    
    * change class
    
    * light query
    
    * update git repo style with listbox item
    
    * use listbox
    
    * update empty state and classes
    
    * use construct path
    
    * update font siwe
    
    * update height
    
    * add textvalue and remove new tab link
    
    * fix loading and imports

--- frontend/app/src/pages/homepage.tsx
+++ frontend/app/src/pages/homepage.tsx
@@ -1,11 +1,18 @@
 import Content from "@/shared/components/layout/content";
 
 import { GettingStarted } from "@/entities/homepage/ui/getting-started";
+import { GitRepositories } from "@/entities/homepage/ui/git-repositories";
 
 const Homepage = () => {
   return (
-    <Content className="p-4">
-      <h1 className="pb-4 font-semibold text-2xl">Welcome to Infrahub!</h1>
+    <Content className="flex flex-col gap-4 p-4">
+      <h1 className="font-semibold text-2xl">Welcome to Infrahub!</h1>
+
+      <div className="grid grid-cols-3">
+        <div className="col-span-2 max-h-44" />
+
+        <GitRepositories className="h-44" />
+      </div>
 
       <GettingStarted />
     </Content>
