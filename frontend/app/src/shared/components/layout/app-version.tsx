0731de1001e52a3d127daf07d1f21b3b0f15cfb4
Ashley Tayles <ashley@tayles.uk>
2025-11-08 11:34:35 +0000

    use predefined function/hook

--- frontend/app/src/shared/components/layout/app-version.tsx
+++ frontend/app/src/shared/components/layout/app-version.tsx
@@ -5,22 +5,20 @@ import { CONFIG } from "@/config/config";
 import { fetchUrl } from "@/shared/api/rest/fetch";
 import type { components } from "@/shared/api/rest/types.generated";
 import { Skeleton } from "@/shared/components/skeleton";
+import { useConfig } from "@/entities/config/ui/config-provider";
+import { capitalizeFirstLetter } from "@/shared/utils/string";
 
 export const AppVersion = () => {
   const [info, setInfo] = useState<components["schemas"]["InfoAPI"] | null>(null);
-  const [config, setConfig] = useState<components["schemas"]["ConfigAPI"] | null>(null);
-  const [error, setError] = useState<string | null>(null);
+  const config = useConfig();
 
   useEffect(() => {
     fetchUrl(CONFIG.INFO_URL)
       .then((result) => setInfo(result))
-      .catch((error) => setError("Failed to load version info: " + error.message));
-    fetchUrl(CONFIG.CONFIG_URL)
-      .then((result) => setConfig(result))
-      .catch((error) => setError("Failed to load edition info: " + error.message));
+      .catch((error) => console.error("Failed to load version info:", error.message));
   }, []);
 
-  const installationType = config ? config.installation_type.charAt(0).toUpperCase() + config.installation_type.slice(1) + " Edition" : <Skeleton className="h-4 w-14" />;
+  const installationType = config ? capitalizeFirstLetter(config.installation_type) + " Edition" : <Skeleton className="h-4 w-14" />;
   const version = info ? info.version : <Skeleton className="h-4 w-14" />;
 
   return (
