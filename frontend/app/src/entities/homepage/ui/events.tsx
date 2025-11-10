f762c483baa63939822e70a076f281cefefa5a50
Paul Leménager <pa-lem@hotmail.fr>
2025-11-10 17:27:09 +0100

    Display recent activities in landing page (#7627)
    * update homepage with new ui
    
    * update links
    
    * changelog
    
    * update description
    
    * fix tag
    
    * update archi
    
    * update component to use card
    
    * adjustment
    
    * start add proposed change list and rename header to filters
    
    * start add git repo layout
    
    * remove useless
    
    * add empty screen
    
    * add git repo list
    
    * add git icon
    
    * update types but not proud
    
    * upate changelog
    
    * update icons
    
    * fix loading
    
    * add branches
    
    * update empty card
    
    * update empty state and props
    
    * add link
    
    * change class
    
    * light query
    
    * update git repo style with listbox item
    
    * use listbox
    
    * update empty state and classes
    
    * use construct path
    
    * update font siwe
    
    * sort branches by created at desc
    
    * update type
    
    * update height
    
    * update height
    
    * update changelog
    
    * update height
    
    * udpate height
    
    * fix scroll
    
    * use listbox and update style
    
    * add view all
    
    * fix url
    
    * add textvalue and remove new tab link
    
    * remove new tab link
    
    * fix loading and imports
    
    * update diff ui
    
    * revert props
    
    * revert props
    
    * lint
    
    * update ui for diff summary
    
    * add light proposed changer ui for landing page
    
    * update ui for landing page
    
    * update changelog
    
    * add header and update ui
    
    * udpate empty state
    
    * update date format
    
    * fix title
    
    * update render
    
    * update render
    
    * fix props and types
    
    * only opened proposed changes
    
    * use const
    
    * update const
    
    * light card for homepage
    
    * add max parameter
    
    * add events in homepage
    
    * update responsive
    
    * clean
    
    * update changelog
    
    * add dedicated component and cleanup
    
    ---------
    
    Co-authored-by: bilalabbad <bilal@opsmill.com>

---
+++ frontend/app/src/entities/homepage/ui/events.tsx
@@ -0,0 +1,32 @@
+import { Icon } from "@iconify-icon/react";
+
+import { constructPath } from "@/shared/api/rest/fetch";
+import { HomeCard } from "@/shared/components/ui/home-card";
+import { ScrollArea } from "@/shared/components/ui/scroll-area";
+import { classNames } from "@/shared/utils/common";
+
+import { HomeEvents } from "@/entities/events/ui/node-details-events-homepage";
+
+interface EventsProps {
+  className?: string;
+}
+
+export const Events = ({ className }: EventsProps) => {
+  return (
+    <HomeCard className={classNames("flex flex-col", className)}>
+      <HomeCard.Title className="flex items-center justify-between">
+        <span className="flex items-center gap-2">
+          <Icon icon={"mdi:file-replace-outline"} /> Recent Activities
+        </span>
+
+        <HomeCard.Link to={constructPath("/activities")}>
+          View all <Icon icon={"mdi:chevron-right"} />
+        </HomeCard.Link>
+      </HomeCard.Title>
+
+      <ScrollArea>
+        <HomeEvents />
+      </ScrollArea>
+    </HomeCard>
+  );
+};
