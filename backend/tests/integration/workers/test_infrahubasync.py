5865a1455f9dc095290b43fec37d73377bbfa04b
Babatunde Olusola <solababatunde12@gmail.com>
2025-11-12 15:25:02 +0100

    add return type

--- backend/tests/integration/workers/test_infrahubasync.py
+++ backend/tests/integration/workers/test_infrahubasync.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, AsyncGenerator
 
 import pytest
 from prefect.client.orchestration import PrefectClient
@@ -121,8 +121,8 @@ class TestWorker(TestWorkerInfrahubAsync):
         return stdout.decode().strip() if proc.returncode == 0 else None
 
     @pytest.fixture
-    async def git_global_user_config(self) -> None:
-        async def set_git_config(key: str, value: str | None):
+    async def git_global_user_config(self) -> AsyncGenerator[None, None, None]:
+        async def set_git_config(key: str, value: str | None) -> None:
             if value is not None:
                 await asyncio.create_subprocess_exec("git", "config", "--global", key, value)
             else:
