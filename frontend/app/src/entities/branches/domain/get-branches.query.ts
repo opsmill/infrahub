e44d58f4edcd071a79e3a9f05172e7985496fa29
pa-lem <pa-lem@hotmail.fr>
2025-11-15 09:29:03 +0100

    disable branch query cache

--- frontend/app/src/entities/branches/domain/get-branches.query.ts
+++ frontend/app/src/entities/branches/domain/get-branches.query.ts
@@ -1,12 +1,10 @@
 import { queryOptions, useQuery } from "@tanstack/react-query";
 
-import { branchesQueryKeys } from "@/entities/branches/domain/branch.query-keys";
-
 import { getBranches } from "./get-branches";
 
 export function getBranchesQueryOptions() {
   return queryOptions({
-    queryKey: branchesQueryKeys.all,
+    queryKey: [],
     queryFn: getBranches,
   });
 }
