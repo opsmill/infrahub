fa91fa796be6d461e43af4bd9ac233340baf9c5d
Fatih Acar <fatih@opsmill.com>
2025-11-17 20:16:20 +0100

    feat: migrate to uv
    Signed-off-by: Fatih Acar <fatih@opsmill.com>

--- .devcontainer/updateContentCommand.sh
+++ .devcontainer/updateContentCommand.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 export WEB_CONCURRENCY=2
-poetry run invoke demo.start
+uv run invoke demo.start
 sleep 120
 docker logs infrahub-server-1
-poetry run invoke demo.load-infra-schema
+uv run invoke demo.load-infra-schema
 docker logs infrahub-server-1
 sleep 90
 docker logs infrahub-server-1
-poetry run invoke demo.load-infra-data
-poetry run invoke demo.stop
+uv run invoke demo.load-infra-data
+uv run invoke demo.stop
