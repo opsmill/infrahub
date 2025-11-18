d0fd890462aa920a33b48a1568b7dd3e5616e886
Guillaume Mazoyer <guillaume@opsmill.com>
2025-11-18 17:52:24 +0100

    Fix test check

--- backend/tests/unit/core/profiles/test_node_applier.py
+++ backend/tests/unit/core/profiles/test_node_applier.py
@@ -523,11 +523,11 @@ async def test_get_many_with_profile_relationships_clear(
         db=db, branch=branch, ids=[child_and_thing_nodes.child_nodes[0].id], include_source=True
     )
     assert len(node_map) == 1
-    updated_child_one = node_map[child_and_thing_nodes.child_nodes[0].id]
+    second_updated_child_one = node_map[child_and_thing_nodes.child_nodes[0].id]
     await _validate_node_profile_relationships(
         db=db,
         schema=child_and_thing_nodes.child_node_schema,
-        original_node=child_and_thing_nodes.child_nodes[0],
-        updated_node=updated_child_one,
+        original_node=updated_child_one,
+        updated_node=second_updated_child_one,
         expected_profile_relationships=[ExpectedProfileRelationship(name="things", peers=[], source_uuid="")],
     )
