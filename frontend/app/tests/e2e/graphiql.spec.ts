196296ca41930bfa09814ed691896918bbd8915c
bilalabbad <bilal@opsmill.com>
2025-11-18 09:26:17 +0100

    more

--- frontend/app/tests/e2e/graphiql.spec.ts
+++ frontend/app/tests/e2e/graphiql.spec.ts
@@ -3,8 +3,9 @@ import { expect, test } from "@playwright/test";
 test("/graphql - GraphiQL", async ({ page }) => {
   await test.step("navigate to GraphiQL and open new tab", async () => {
     await page.goto("/graphql");
-    await page.getByRole("button", { name: "New tab" }).click();
     await expect(page.getByRole("button", { name: "Re-fetch GraphQL schema" })).toBeEnabled(); // required for autocompletion to load
+    await expect(page.getByText("# Welcome to GraphiQL")).toBeVisible();
+    await page.getByRole("button", { name: "New tab" }).click();
   });
 
   if (await page.getByText("# Welcome to GraphiQL").isVisible()) {
