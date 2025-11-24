1bd2c875b13e3fe1370b444966bde513838e9696
pa-lem <pa-lem@hotmail.fr>
2025-11-24 16:22:28 +0100

    add new test for template and profile

--- frontend/app/tests/e2e/object-template/create-object-instance-using-template.spec.ts
+++ frontend/app/tests/e2e/object-template/create-object-instance-using-template.spec.ts
@@ -1,57 +1,74 @@
 import { expect, test } from "@playwright/test";
 
 import { ACCOUNT_STATE_PATH } from "../../constants";
-import { saveScreenshotForDocs } from "../../utils";
 
 test.describe.fixme("object-template", () => {
   test.use({ storageState: ACCOUNT_STATE_PATH.ADMIN });
 
-  test.describe.fixme("object-template diff data", () => {
-    test.describe.configure({ mode: "serial" });
-    test.use({ storageState: ACCOUNT_STATE_PATH.ADMIN });
-    test.slow();
-
-    test.beforeEach(async function ({ page }) {
-      page.on("response", async (response) => {
-        if (response.status() === 500) {
-          await expect(response.url()).toBe("This URL responded with a 500 status");
-        }
-      });
-    });
-  });
-
-  test("create-object-instance-using-template", async ({ page }) => {
-    await test.step("view-existing-template", async () => {
-      await page.goto("/objects/CoreObjectTemplate");
-      await expect(page.getByTestId("create-object-button")).toBeVisible();
-      await saveScreenshotForDocs(page, "/guides/object-template/template_list");
-    });
-
-    await test.step("create-patch-panel-using-template", async () => {
-      await page.goto("/objects/InfraPatchPanel");
-      await expect(page.getByTestId("create-object-button")).toBeVisible();
+  test("should create nodes from a template", async ({ page }) => {
+    await test.step("should create profile first", async () => {
+      await page.goto("/objects/CoreProfile");
+      await expect(page.getByRole("heading", { name: "Profile" })).toBeVisible();
       await page.getByTestId("create-object-button").click();
-      await expect(page.getByRole("button", { name: "Start from template Pick a" })).toBeVisible();
-      await saveScreenshotForDocs(page, "/guides/object-template/template_or_from_scratch");
-      await page.getByRole("button", { name: "Start from template Pick a" }).click();
-      await page.getByRole("option", { name: "Regular_Patch_Panel" }).click();
-      await expect(page.getByRole("button", { name: "Save" })).toBeVisible();
-      await page.getByLabel("Name *").fill("patch-panel-01");
-      await saveScreenshotForDocs(page, "/guides/object-template/form_with_template");
+
+      await page.getByRole("combobox", { name: "Select an object type" }).click();
+      await page.getByRole("option", { name: "Patch Panel Infra" }).click();
+
+      await page.getByRole("textbox", { name: "Profile Name *" }).fill("Profile for patch panel");
+
+      await page.getByRole("spinbutton", { name: "Module Capacity" }).fill("1000");
+
+      await page.getByRole("textbox", { name: "Description" }).fill("Description from profile");
+
       await page.getByRole("button", { name: "Save" }).click();
+      await expect(page.getByText("InfraPatchPanel created")).toBeVisible();
+      await expect(page.getByRole("link", { name: "Profile for patch panel" })).toBeVisible();
     });
 
-    await test.step("view-created-object", async () => {
-      await page.goto("/objects/InfraPatchPanel");
-      await expect(page.getByText("Patch Panel used to connect racks")).toBeVisible();
-      await page.getByTestId("identifier-cell").first().click();
-      await expect(page.getByText("Front Interfaces6")).toBeVisible();
-      await page.getByText("Front Interfaces6").click();
-      await expect(page.getByText("patch-panel-01, C1.P01")).toBeVisible();
-      await saveScreenshotForDocs(
-        page,
-        "/guides/object-template/object_components_created_using_template"
+    await test.step("should create a template with the profile", async () => {
+      await page.goto("/objects/CoreObjectTemplate");
+      await expect(page.getByRole("heading", { name: "Object Templates" })).toBeVisible();
+      await page.getByTestId("create-object-button").click();
+      await page.getByRole("combobox", { name: "Select an object type" }).click();
+      await page.getByRole("option", { name: "Patch Panel Infra" }).click();
+      await page.getByRole("button", { name: "Select profiles optional" }).click();
+      await page.getByRole("option", { name: "Profile for patch panel" }).click();
+      await expect(page.getByTestId("source-profile-badge").first()).toBeVisible();
+      await expect(page.getByRole("textbox", { name: "Description" })).toHaveValue(
+        "Description from profile"
       );
+      await page.getByRole("textbox", { name: "Template Name *" }).fill("Template with profile");
+      // Override module capacity
+      await page.getByRole("spinbutton", { name: "Module Capacity" }).fill("2000");
+      await page.getByRole("button", { name: "Save" }).click();
+      await expect(page.getByText("InfraPatchPanel created")).toBeVisible();
+      await expect(page.getByRole("link", { name: "Template with profile" })).toBeVisible();
+    });
+
+    await test.step("should create a node from the template", async () => {
+      await page.goto("http://localhost:8080/objects/InfraPatchPanel");
+      await expect(page.getByRole("heading", { name: "Patch Panel" })).toBeVisible();
+      await page.getByTestId("create-object-button").click();
+      await page.getByRole("button", { name: "Start from template Pick a" }).click();
+      await page.getByRole("option", { name: "Template with profile" }).click();
+      await expect(page.getByTestId("source-template-badge")).toBeVisible();
+      await expect(page.getByTestId("source-profile-badge")).toBeVisible();
+      await expect(page.getByRole("textbox", { name: "Description" })).toHaveValue(
+        "Description from profile"
+      );
+      await expect(page.getByRole("spinbutton", { name: "Module Capacity" })).toHaveValue("2000");
+      await page.getByRole("textbox", { name: "Name *" }).click();
+      await page.getByRole("textbox", { name: "Name *" }).fill("Test from template and profile");
+      await page.getByRole("button", { name: "Save" }).click();
+      await expect(page.getByText("PatchPanel created")).toBeVisible();
+      await expect(
+        page.getByRole("link", { name: "Test from template and profile" })
+      ).toBeVisible();
+      await expect(
+        page.locator("span").filter({ hasText: "Test from template and profile" })
+      ).toBeVisible();
+      await expect(page.getByText("2000")).toBeVisible();
+      await expect(page.getByText("Description from profile")).toBeVisible();
     });
   });
 });
