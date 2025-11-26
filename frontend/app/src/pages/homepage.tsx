b51a7088a75d5200a8d8fd0cad569276bc1a4146
pa-lem <pa-lem@hotmail.fr>
2025-11-26 11:14:10 +0100

    remove welcome

--- frontend/app/src/pages/homepage.tsx
+++ frontend/app/src/pages/homepage.tsx
@@ -10,8 +10,6 @@ import { TasksWidget } from "@/entities/homepage/ui/tasks-widget";
 const Homepage = () => {
   return (
     <Content className="flex flex-col gap-4 p-4">
-      <h1 className="font-semibold text-2xl">Welcome to Infrahub!</h1>
-
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
         <div className="col-span-2 flex flex-col gap-4">
           <ProposedChangesWidget className="min-h-80" />
