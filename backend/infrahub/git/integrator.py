233c01f5013c2e97502fe384b26ed301a6ea453e
Pete Crocker <pete@petecrocker.com>
2025-11-27 18:13:27 +0000

    Remove unnecessary type ignore comments from integrator.py
    Since get_repository_config() no longer returns None, the type ignore
    comments are no longer needed.

--- backend/infrahub/git/integrator.py
+++ backend/infrahub/git/integrator.py
@@ -189,25 +189,23 @@ class InfrahubRepositoryIntegrator(InfrahubRepositoryBase):
         error: Exception | None = None
 
         try:
-            config_file = await self.get_repository_config(branch_name=infrahub_branch_name, commit=commit)  # type: ignore[misc]
-            await self.import_schema_files(branch_name=infrahub_branch_name, commit=commit, config_file=config_file)  # type: ignore[misc]
+            config_file = await self.get_repository_config(branch_name=infrahub_branch_name, commit=commit)
+            await self.import_schema_files(branch_name=infrahub_branch_name, commit=commit, config_file=config_file)
             await self.import_all_graphql_query(
                 branch_name=infrahub_branch_name, commit=commit, config_file=config_file
-            )  # type: ignore[misc]
+            )
             await self.import_objects(
                 branch_name=infrahub_branch_name,
                 commit=commit,
                 config_file=config_file,
-            )  # type: ignore[misc]
-            await self.import_all_python_files(  # type: ignore[call-overload]
-                branch_name=infrahub_branch_name, commit=commit, config_file=config_file
-            )  # type: ignore[misc]
+            )
+            await self.import_all_python_files(branch_name=infrahub_branch_name, commit=commit, config_file=config_file)
             await self.import_jinja2_transforms(
                 branch_name=infrahub_branch_name, commit=commit, config_file=config_file
-            )  # type: ignore[misc]
+            )
             await self.import_artifact_definitions(
                 branch_name=infrahub_branch_name, commit=commit, config_file=config_file
-            )  # type: ignore[misc]
+            )
 
         except Exception as exc:
             sync_status = RepositorySyncStatus.ERROR_IMPORT
