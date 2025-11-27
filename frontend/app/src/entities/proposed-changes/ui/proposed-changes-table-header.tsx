ecc01a19143031a63661ec36f7aee090409ab76d
pa-lem <pa-lem@hotmail.fr>
2025-11-27 16:42:24 +0100

    fix sticky header

--- frontend/app/src/entities/proposed-changes/ui/proposed-changes-table-header.tsx
+++ frontend/app/src/entities/proposed-changes/ui/proposed-changes-table-header.tsx
@@ -1,6 +1,6 @@
 export const ProposedChangesTableHeader = () => {
   return (
-    <div className="sticky top-0 grid grid-cols-2 bg-gray-50 px-4 py-2 text-center text-sm">
+    <div className="sticky top-0 z-10 grid grid-cols-2 bg-gray-50 px-4 py-2 text-center text-sm">
       <span className="text-left">Name</span>
       <div className="grid grid-cols-7">
         <span>Reviews</span>
