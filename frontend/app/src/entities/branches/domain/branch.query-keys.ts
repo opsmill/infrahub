0892147b41f36b6ec522c88e97af452945560be1
pa-lem <pa-lem@hotmail.fr>
2025-12-10 17:28:54 +0100

    add keys

--- frontend/app/src/entities/branches/domain/branch.query-keys.ts
+++ frontend/app/src/entities/branches/domain/branch.query-keys.ts
@@ -1,4 +1,6 @@
 export const branchesQueryKeys = {
   all: ["branches"] as const,
+  list: (search?: string) => [...branchesQueryKeys.all, "list", { search }] as const,
+  count: (search?: string) => [...branchesQueryKeys.all, "count", { search }] as const,
   details: ({ branchName }: { branchName: string }) => [...branchesQueryKeys.all, branchName],
 } as const;
