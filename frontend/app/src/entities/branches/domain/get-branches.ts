cf1a02021c3926a5863be54f14e98d60b3f5e55d
pa-lem <pa-lem@hotmail.fr>
2025-12-10 15:53:32 +0100

    add status

--- frontend/app/src/entities/branches/domain/get-branches.ts
+++ frontend/app/src/entities/branches/domain/get-branches.ts
@@ -43,6 +43,7 @@ export const getBranches: GetBranches = async () => {
       origin_branch: node.origin_branch?.value,
       branched_from: node.branched_from?.value,
       created_at: node.created_at,
+      status: node.status?.value,
       sync_with_git: node.sync_with_git?.value,
       is_default: node.is_default?.value,
       has_schema_changes: node.has_schema_changes?.value,
