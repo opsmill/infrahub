08ae7d9186945255278b230e29b171851199f6f0
pa-lem <pa-lem@hotmail.fr>
2025-12-10 17:29:05 +0100

    update queries

--- frontend/app/src/entities/branches/api/get-branches-from-api.ts
+++ frontend/app/src/entities/branches/api/get-branches-from-api.ts
@@ -2,8 +2,9 @@ import graphqlClient from "@/shared/api/graphql/graphqlClientApollo";
 
 import { GET_BRANCHES } from "@/entities/branches/api/query/get-branches-query";
 
-export const getBranchesFromApi = async () => {
+export const getBranchesFromApi = async (branchName?: string) => {
   return graphqlClient.query({
     query: GET_BRANCHES,
+    variables: { branchName },
   });
 };
--- frontend/app/src/entities/branches/domain/get-branches.query.ts
+++ frontend/app/src/entities/branches/domain/get-branches.query.ts
@@ -4,13 +4,27 @@ import { branchesQueryKeys } from "@/entities/branches/domain/branch.query-keys"
 
 import { getBranches } from "./get-branches";
 
-export function getBranchesQueryOptions() {
+export function getBranchesQueryOptions(search?: string) {
   return queryOptions({
-    queryKey: branchesQueryKeys.all,
-    queryFn: getBranches,
+    queryKey: branchesQueryKeys.list(search),
+    queryFn: () => getBranches(search),
   });
 }
 
-export function useGetBranches() {
-  return useQuery(getBranchesQueryOptions());
+export function useGetBranches(search?: string) {
+  return useQuery(getBranchesQueryOptions(search));
+}
+
+export function getBranchesCountQueryOptions(search?: string) {
+  return queryOptions({
+    queryKey: branchesQueryKeys.count(search),
+    queryFn: async () => {
+      const branches = await getBranches(search);
+      return branches.length;
+    },
+  });
+}
+
+export function useGetBranchesCount(search?: string) {
+  return useQuery(getBranchesCountQueryOptions(search));
 }
