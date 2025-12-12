f6fe260073a726bbca4abb2557efb21f61f6d7ad
pa-lem <pa-lem@hotmail.fr>
2025-12-12 14:59:39 +0100

    fix import

--- frontend/app/src/entities/branches/domain/create-branch.mutation.ts
+++ frontend/app/src/entities/branches/domain/create-branch.mutation.ts
@@ -2,6 +2,7 @@ import { useMutation } from "@tanstack/react-query";
 
 import { queryClient } from "@/shared/api/rest/client";
 
+import { branchesQueryKeys } from "@/entities/branches/domain/branch.query-keys";
 import { createBranch } from "@/entities/branches/domain/create-branch";
 
 export function useCreateBranchMutation() {
