39695001e2b0c867d6b95c66244b2ff2ef523ec2
pa-lem <pa-lem@hotmail.fr>
2025-12-12 15:12:51 +0100

    revert temporary

--- frontend/app/src/entities/branches/domain/create-branch.mutation.ts
+++ frontend/app/src/entities/branches/domain/create-branch.mutation.ts
@@ -2,7 +2,6 @@ import { useMutation } from "@tanstack/react-query";
 
 import { queryClient } from "@/shared/api/rest/client";
 
-import { branchesQueryKeys } from "@/entities/branches/domain/branch.query-keys";
 import { createBranch } from "@/entities/branches/domain/create-branch";
 
 import { getBranchesQueryOptions } from "./get-branches.query";
@@ -15,9 +14,7 @@ export function useCreateBranchMutation() {
 
       const { queryKey } = getBranchesQueryOptions();
       queryClient.setQueryData(queryKey, (oldBranches) => [...(oldBranches ?? []), branchCreated]);
-
-      // Wait for refetch to complete before allowing navigation to the new branch
-      await queryClient.refetchQueries({ queryKey: branchesQueryKeys.all });
+      queryClient.invalidateQueries({ queryKey });
     },
   });
 }
