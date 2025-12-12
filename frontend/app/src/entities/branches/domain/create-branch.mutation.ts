87b472c58869eb947aed5913bfb1d41fc00318f9
pa-lem <pa-lem@hotmail.fr>
2025-12-12 15:15:03 +0100

    clean up

--- frontend/app/src/entities/branches/domain/create-branch.mutation.ts
+++ frontend/app/src/entities/branches/domain/create-branch.mutation.ts
@@ -5,16 +5,11 @@ import { queryClient } from "@/shared/api/rest/client";
 import { branchesQueryKeys } from "@/entities/branches/domain/branch.query-keys";
 import { createBranch } from "@/entities/branches/domain/create-branch";
 
-import { getBranchesQueryOptions } from "./get-branches.query";
-
 export function useCreateBranchMutation() {
   return useMutation({
     mutationFn: createBranch,
     onSuccess: async (branchCreated) => {
       if (!branchCreated) return;
-
-      const { queryKey } = getBranchesQueryOptions();
-      queryClient.setQueryData(queryKey, (oldBranches) => [...(oldBranches ?? []), branchCreated]);
 
       // Wait for refetch to complete before allowing navigation to the new branch
       await queryClient.refetchQueries({ queryKey: branchesQueryKeys.all });
