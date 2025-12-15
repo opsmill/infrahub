99a2cee4dd23f54409ec15520c4360ae41aa3e20
pa-lem <pa-lem@hotmail.fr>
2025-12-15 18:08:51 +0100

    remove unused

--- frontend/app/src/entities/branches/api/get-branches-count-from-api.ts
+++ frontend/app/src/entities/branches/api/get-branches-count-from-api.ts
@@ -1,13 +1,6 @@
 import graphqlClient from "@/shared/api/graphql/graphqlClientApollo";
-import type { PaginationParams } from "@/shared/api/types";
 
 import { GET_BRANCHES_COUNT } from "@/entities/branches/api/get-branches-count-query";
-
-export const BRANCHES_PER_PAGE = 40;
-
-export interface GetBranchesFromApiParams extends PaginationParams {
-  branchName?: string;
-}
 
 export const getBranchesCountFromApi = async (branchName?: string) => {
   return graphqlClient.query({
