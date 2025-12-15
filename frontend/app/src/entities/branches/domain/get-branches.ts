9e99192c9084a55e380d1f8d5c88ecbdd20c012a
pa-lem <pa-lem@hotmail.fr>
2025-12-15 15:34:35 +0100

    rename

--- frontend/app/src/entities/branches/domain/get-branches.ts
+++ frontend/app/src/entities/branches/domain/get-branches.ts
@@ -8,7 +8,7 @@ import {
 import {
   type InfrahubBranchResponse,
   mapInfrahubBranchNodeToBranch,
-} from "@/entities/branches/domain/branch-response.types";
+} from "@/entities/branches/domain/branch.mappers";
 import { branchesState } from "@/entities/branches/stores";
 
 export type GetBranchesParams = GetBranchesFromApiParams;
