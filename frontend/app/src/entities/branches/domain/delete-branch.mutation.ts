cbca3a362c1ab2d584b1688ad2675e7814493466
pa-lem <pa-lem@hotmail.fr>
2025-12-15 11:29:12 +0100

    fix cache update for delete

--- frontend/app/src/entities/branches/domain/delete-branch.mutation.ts
+++ frontend/app/src/entities/branches/domain/delete-branch.mutation.ts
@@ -3,7 +3,7 @@ import { useMutation } from "@tanstack/react-query";
 import { queryClient } from "@/shared/api/rest/client";
 
 import { deleteBranch } from "@/entities/branches/domain/delete-branch";
-import { getBranchesQueryOptions } from "@/entities/branches/domain/get-branches.query";
+import { getBranchesInfiniteQueryOptions } from "@/entities/branches/domain/get-branches.query";
 
 export function useDeleteBranchMutation() {
   return useMutation({
@@ -11,10 +11,17 @@ export function useDeleteBranchMutation() {
     onSuccess: async (branchDeleted) => {
       if (!branchDeleted) return;
 
-      const { queryKey } = getBranchesQueryOptions();
-      queryClient.setQueryData(queryKey, (oldBranches) =>
-        oldBranches?.filter((branch) => branch.name !== branchDeleted)
-      );
+      const { queryKey } = getBranchesInfiniteQueryOptions();
+      queryClient.setQueryData(queryKey, (oldData) => {
+        if (!oldData) return oldData;
+
+        return {
+          ...oldData,
+          pages: oldData.pages.map((page) =>
+            page.filter((branch) => branch.name !== branchDeleted)
+          ),
+        };
+      });
       queryClient.invalidateQueries({ queryKey });
     },
   });
