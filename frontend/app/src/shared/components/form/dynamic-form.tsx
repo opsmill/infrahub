19c86b8072d11bcd609446abda6bc699a3c05cb1
bilalabbad <bilal@opsmill.com>
2025-12-16 10:28:55 +0100

    Remove unused componenrt

--- frontend/app/src/shared/components/form/dynamic-form.tsx
+++ frontend/app/src/shared/components/form/dynamic-form.tsx
@@ -15,7 +15,6 @@ import PasswordInputField from "@/shared/components/form/fields/password-input.f
 import RelationshipField from "@/shared/components/form/fields/relationships/relationship.field";
 import RelationshipHierarchicalField from "@/shared/components/form/fields/relationships/relationship-hierarchical.field";
 import RelationshipManyField from "@/shared/components/form/fields/relationships/relationship-many.field";
-import RelationshipParentConstraintField from "@/shared/components/form/fields/relationships/relationship-parent-constraint.field";
 import { SelectField } from "@/shared/components/form/fields/select.field";
 import TextareaField from "@/shared/components/form/fields/textarea.field";
 import type { DynamicFieldProps, FormFieldValue } from "@/shared/components/form/type";
@@ -133,10 +132,6 @@ export const DynamicField = (props: DynamicFieldProps) => {
 
       if (props.relationship.cardinality === "many") {
         return <RelationshipManyField {...props} />;
-      }
-
-      if (props.relationship.common_parent) {
-        return <RelationshipParentConstraintField {...props} />;
       }
 
       return <RelationshipField {...props} />;
