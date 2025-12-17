68205818ede03e5ca80d52aa73905037d63e59e6
pa-lem <pa-lem@hotmail.fr>
2025-12-17 13:42:52 +0100

    extract get branch details query

--- frontend/app/src/entities/branches/api/get-branch-details-from-api.ts
+++ frontend/app/src/entities/branches/api/get-branch-details-from-api.ts
@@ -1,17 +1,17 @@
 import type {
-  GetBranchesQuery,
-  GetBranchesQueryVariables,
+  GetBranchDetailsQuery,
+  GetBranchDetailsQueryVariables,
 } from "@/shared/api/graphql/generated/graphql";
 import graphqlClient from "@/shared/api/graphql/graphqlClientApollo";
 import type { BranchContextParams } from "@/shared/api/types";
 
-import { GET_BRANCHES } from "@/entities/branches/api/get-branches-query";
+import { GET_BRANCH_DETAILS } from "@/entities/branches/api/get-branch-details-query";
 
 export interface GetBranchDetailsFromApiParams extends BranchContextParams {}
 
 export function getBranchDetailsFromApi({ branchName }: GetBranchDetailsFromApiParams) {
-  return graphqlClient.query<GetBranchesQuery, GetBranchesQueryVariables>({
-    query: GET_BRANCHES,
+  return graphqlClient.query<GetBranchDetailsQuery, GetBranchDetailsQueryVariables>({
+    query: GET_BRANCH_DETAILS,
     variables: { branchName },
   });
 }
