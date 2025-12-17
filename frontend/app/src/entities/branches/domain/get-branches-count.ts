e80f57d5acedc4c8318b2ee16d9e08e5b0468f2a
pa-lem <pa-lem@hotmail.fr>
2025-12-17 11:30:01 +0100

    remove comment

--- frontend/app/src/entities/branches/domain/get-branches-count.ts
+++ frontend/app/src/entities/branches/domain/get-branches-count.ts
@@ -1,7 +1,6 @@
 import { getBranchesCountFromApi } from "@/entities/branches/api/get-branches-count-from-api";
 import type { InfrahubBranchResponse } from "@/entities/branches/domain/branch.mappers";
 
-// Fetch branches count (for header badge)
 export const getBranchesCount = async (branchName?: string): Promise<number> => {
   const { data, errors } = await getBranchesCountFromApi(branchName);
 
