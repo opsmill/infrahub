6641d4be76dd279d80791b76a17e1d6a5fb1ec8f
pa-lem <pa-lem@hotmail.fr>
2025-12-18 09:17:48 +0100

    update property display

--- frontend/app/src/entities/branches/ui/branch-list-item/branch-list-item.tsx
+++ frontend/app/src/entities/branches/ui/branch-list-item/branch-list-item.tsx
@@ -1,65 +1,85 @@
 import { ListBoxItem, type ListBoxItemProps } from "react-aria-components";
-
-import type { Branch } from "@/shared/api/graphql/generated/graphql";
-import { constructPath } from "@/shared/api/rest/fetch";
-import { Col, Row } from "@/shared/components/container";
-import { focusVisibleStyle } from "@/shared/components/style-rac";
-import { classNames } from "@/shared/utils/common";
-import { formatFullDate } from "@/shared/utils/date";
-
 import { BranchDefaultBadge } from "@/entities/branches/ui/branch-list-item/branch-default-badge";
 import { BranchGitSyncBadge } from "@/entities/branches/ui/branch-list-item/branch-git-sync-badge";
 import { BranchMetadata } from "@/entities/branches/ui/branch-list-item/branch-metadata";
 import { BranchSchemaChangesBadge } from "@/entities/branches/ui/branch-list-item/branch-schema-changes-badge";
 import { BranchStatusBadge } from "@/entities/branches/ui/branch-list-item/branch-status-badge";
+import { getNodeLabel } from "@/entities/nodes/object/utils/get-node-label";
+import { constructPath } from "@/shared/api/rest/fetch";
+import { Col, Row } from "@/shared/components/container";
+import { focusVisibleStyle } from "@/shared/components/style-rac";
+import { classNames } from "@/shared/utils/common";
+import { formatFullDate } from "@/shared/utils/date";
+import type { BranchWithMetadata } from "../../domain/branch.mappers";
 
 interface BranchListItemProps extends ListBoxItemProps {
-  branch: Branch;
+	branch: BranchWithMetadata;
 }
 
-export function BranchListItem({ branch, className, ...props }: BranchListItemProps) {
-  return (
-    <ListBoxItem
-      textValue={branch.name}
-      href={constructPath(`/branches/${branch.name}`)}
-      className={classNames(
-        focusVisibleStyle,
-        "flex flex-wrap items-center gap-6 px-6 py-4",
-        "border border-transparent not-last:border-b-gray-200",
-        "first:rounded-t-lg last:rounded-b-lg",
-        "hover:bg-neutral-100",
-        className
-      )}
-      {...props}
-    >
-      <Col className="min-w-1/3 flex-1 gap-0 text-sm">
-        <Row className="shrink-0">
-          <span className="font-semibold">{branch.name}</span>
+export function BranchListItem({
+	branch,
+	className,
+	...props
+}: BranchListItemProps) {
+	console.log("branch.created_by: ", branch.created_by);
 
-          {branch.is_default ? (
-            <BranchDefaultBadge />
-          ) : (
-            <BranchStatusBadge status={branch.status} />
-          )}
+	return (
+		<ListBoxItem
+			textValue={branch.name}
+			href={constructPath(`/branches/${branch.name}`)}
+			className={classNames(
+				focusVisibleStyle,
+				"flex flex-wrap items-center gap-6 px-6 py-4",
+				"border border-transparent not-last:border-b-gray-200",
+				"first:rounded-t-lg last:rounded-b-lg",
+				"hover:bg-neutral-100",
+				className,
+			)}
+			{...props}
+		>
+			<Col className="min-w-1/3 flex-1 gap-0 text-sm">
+				<Row className="shrink-0">
+					<span className="font-semibold">{branch.name}</span>
 
-          {branch.has_schema_changes && <BranchSchemaChangesBadge />}
-        </Row>
+					{branch.is_default ? (
+						<BranchDefaultBadge />
+					) : (
+						<BranchStatusBadge status={branch.status} />
+					)}
 
-        <p className="truncate text-gray-600 text-xs">{branch.description}</p>
-      </Col>
+					{branch.has_schema_changes && <BranchSchemaChangesBadge />}
+				</Row>
 
-      {branch.sync_with_git && <BranchGitSyncBadge />}
+				<p className="truncate text-gray-600 text-xs">{branch.description}</p>
+			</Col>
 
-      <Row className="ml-auto gap-6">
-        <BranchMetadata
-          label="last rebase"
-          value={branch.branched_from ? formatFullDate(branch.branched_from) : "-"}
-        />
-        <BranchMetadata
-          label="created at"
-          value={branch.created_at ? formatFullDate(branch.created_at) : "-"}
-        />
-      </Row>
-    </ListBoxItem>
-  );
+			{branch.sync_with_git && <BranchGitSyncBadge />}
+
+			<Row className="gap-4">
+				<Col>
+					<BranchMetadata
+						label="last rebase"
+						value={
+							branch.branched_from ? formatFullDate(branch.branched_from) : "-"
+						}
+					/>
+					<BranchMetadata
+						label="last update"
+						value={branch.updated_at && formatFullDate(branch.updated_at)}
+					/>
+				</Col>
+
+				<Col>
+					<BranchMetadata
+						label="created at"
+						value={branch.created_at ? formatFullDate(branch.created_at) : "-"}
+					/>
+					<BranchMetadata
+						label="created by"
+						value={branch.created_by && getNodeLabel(branch.created_by)}
+					/>
+				</Col>
+			</Row>
+		</ListBoxItem>
+	);
 }
