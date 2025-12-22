c19efd66169ae5895f1327b0d39f24cf47ed4003
bilalabbad <bilal@opsmill.com>
2025-12-22 01:15:38 +0100

    fix

--- frontend/app/src/entities/nodes/object/ui/object-details/object-data-display/object-data-row.tsx
+++ frontend/app/src/entities/nodes/object/ui/object-details/object-data-display/object-data-row.tsx
@@ -9,7 +9,7 @@ export function ObjectDataRow({ name, value }: ObjectDataRowProps) {
   return (
     <div className="grid grid-cols-[200px_auto] gap-4 px-4 py-2 text-xs">
       <dt className="flex h-8 items-center font-medium text-gray-500">{name}</dt>
-      <dd className="flex items-center gap-2">{value}</dd>
+      <dd className="flex items-center gap-2 overflow-hidden">{value}</dd>
     </div>
   );
 }
