1e6817cb285873ed550348008b30eed7fda29275
bilalabbad <bilal@opsmill.com>
2025-12-26 17:46:02 +0100

    lint

--- frontend/app/src/entities/user-profile/domain/get-infrahub-account-token.ts
+++ frontend/app/src/entities/user-profile/domain/get-infrahub-account-token.ts
@@ -1,6 +1,4 @@
-
 import { getAccountTokenFromApi } from "@/entities/user-profile/api/get-account-token-from-api";
-
 
 export const getInfrahubAccountToken = async () => {
   const { data } = await getAccountTokenFromApi();
