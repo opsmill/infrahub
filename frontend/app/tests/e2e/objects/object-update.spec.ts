6994fde08ecbf5e1b561162749ddc23c39c62a34
pa-lem <pa-lem@hotmail.fr>
2026-01-05 12:26:47 +0100

    fix test

--- frontend/app/tests/e2e/objects/object-update.spec.ts
+++ frontend/app/tests/e2e/objects/object-update.spec.ts
@@ -56,7 +56,7 @@ test.describe("Object update", () => {
 
       // Verify updates in view
       await expect(page.getByText("Nameatl1-core1-new-name")).toBeVisible();
-      await expect(page.getByTestId("object-header").getByText("New description")).toBeVisible();
+      await expect(page.getByText("New description")).toBeVisible();
       await expect(page.getByRole("link", { name: "AS174 174" })).toBeVisible();
       await expect(page.getByText("StatusMaintenance")).toBeVisible();
       await expect(page.getByText("Edge Router")).toBeVisible();
