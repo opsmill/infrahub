b3fe4d100f633c7cdd13f8b1e36420739a52efb6
Sola Babatunde <solababatunde12@gmail.com>
2026-01-06 18:46:01 +0100

    add return type

--- backend/tests/integration_docker/test_triggered_actions.py
+++ backend/tests/integration_docker/test_triggered_actions.py
@@ -40,7 +40,7 @@ class TestingTag(BuiltinTag): ...
 
 class TestTriggeredActions(TestInfrahubDockerClient, SchemaCarPerson):
     @pytest.fixture(scope="class")
-    def builtin_tag_schema(self):
+    def builtin_tag_schema(self) -> NodeSchema:
         return NodeSchema(
             name="Tag",
             namespace="Builtin",
@@ -59,7 +59,7 @@ class TestTriggeredActions(TestInfrahubDockerClient, SchemaCarPerson):
         )
 
     @pytest.fixture(scope="class")
-    def testing_tag_schema(self):
+    def testing_tag_schema(self) -> NodeSchema:
         return NodeSchema(
             name="Tag",
             namespace="Testing",
