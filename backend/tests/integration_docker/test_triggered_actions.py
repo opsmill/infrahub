662c9e9b3fa478365d4c77532cb9bd1f48cd962b
Sola Babatunde <solababatunde12@gmail.com>
2026-01-07 13:35:56 +0100

    add testing tag

--- backend/tests/integration_docker/test_triggered_actions.py
+++ backend/tests/integration_docker/test_triggered_actions.py
@@ -35,6 +35,9 @@ from tests.helpers.fixtures import get_fixtures_dir
 CURRENT_DIRECTORY = Path(__file__).parent.resolve()
 
 
+class TestingTag(BuiltinTag): ...
+
+
 class TestTriggeredActions(TestInfrahubDockerClient, SchemaCarPerson):
     @pytest.fixture(scope="class")
     def testing_tag_base(self) -> NodeSchema:
@@ -142,7 +145,7 @@ class TestTriggeredActions(TestInfrahubDockerClient, SchemaCarPerson):
         group_action = await client.create(kind=CoreGroupAction, name="add-to-people", group=group_people)
         await group_action.save()
 
-        tags_original = await client.all(kind=BuiltinTag)
+        tags_original = await client.all(kind=TestingTag)
         tag_names_original = [tag.name.value for tag in tags_original]
 
         node_trigger = await client.create(
@@ -212,7 +215,7 @@ class TestTriggeredActions(TestInfrahubDockerClient, SchemaCarPerson):
 
         await group_people.members.fetch()
         for _ in range(30):
-            tags_updated = await client.all(kind=BuiltinTag)
+            tags_updated = await client.all(kind=TestingTag)
             tag_names_updated = [tag.name.value for tag in tags_updated]
             if len(tag_names_updated) > len(tag_names_original):
                 break
