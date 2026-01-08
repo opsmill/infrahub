0aba28b4a712f2cebf26bf6a8b449de94b39d72d
pa-lem <pa-lem@hotmail.fr>
2026-01-08 12:13:54 +0100

    add custom detailed view

--- frontend/app/src/entities/nodes/object/ui/object-details/object-details-body.tsx
+++ frontend/app/src/entities/nodes/object/ui/object-details/object-details-body.tsx
@@ -1,11 +1,14 @@
 import ErrorScreen from "@/shared/components/errors/error-screen";
 import { LoadingIndicator } from "@/shared/components/loading/loading-indicator";
+import { FILE_ATTACHMENT_KIND } from "@/shared/config/constants";
 
 import { useGetObject } from "@/entities/nodes/object/domain/get-object.query";
+import { FileAttachmentDetails } from "@/entities/nodes/object/ui/object-details/file-attachment-details";
 import { ObjectDetails } from "@/entities/nodes/object/ui/object-details/object-details";
 import { ObjectDetailsTabs } from "@/entities/nodes/object/ui/object-details/object-details-tabs";
 import type { Permission } from "@/entities/permission/types";
 import type { ModelSchema } from "@/entities/schema/types";
+import { isOfKind } from "@/entities/schema/utils/is-of-kind";
 
 interface ObjectDetailsBodyProps {
   objectId: string;
@@ -24,10 +27,25 @@ export function ObjectDetailsBody({ objectSchema, objectId, permission }: Object
     return <ErrorScreen message={error.message} />;
   }
 
+  const isFileAttachment = isOfKind(FILE_ATTACHMENT_KIND, objectSchema);
+  console.log("isFileAttachment: ", isFileAttachment);
+
   return (
     <>
       <ObjectDetailsTabs objectSchema={objectSchema} objectData={objectData} />
-      <ObjectDetails objectSchema={objectSchema} objectData={objectData} permission={permission} />
+      {isFileAttachment ? (
+        <FileAttachmentDetails
+          objectSchema={objectSchema}
+          objectData={objectData}
+          permission={permission}
+        />
+      ) : (
+        <ObjectDetails
+          objectSchema={objectSchema}
+          objectData={objectData}
+          permission={permission}
+        />
+      )}
     </>
   );
 }
--- frontend/app/src/shared/config/constants.ts
+++ frontend/app/src/shared/config/constants.ts
@@ -25,7 +25,7 @@ export const GLOBAL_PERMISSION_OBJECT = "CoreGlobalPermission";
 export const OBJECT_PERMISSION_OBJECT = "CoreObjectPermission";
 
 export const FILE_GENERIC_KIND = "CoreFile";
-export const FILE_ATTACHMENT_KIND = "BuiltinFileAttachment";
+export const FILE_ATTACHMENT_KIND = "CustomFileAttachment";
 
 export const GENERIC_REPOSITORY_KIND = "CoreGenericRepository";
 
