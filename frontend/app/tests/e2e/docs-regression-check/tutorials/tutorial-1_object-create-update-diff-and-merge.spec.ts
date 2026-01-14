5b7eca1b9ccdf0bd989bf60d3881ca7168379413
pa-lem <pa-lem@hotmail.fr>
2026-01-14 15:40:27 +0100

    lint

--- frontend/app/tests/e2e/docs-regression-check/tutorials/tutorial-1_object-create-update-diff-and-merge.spec.ts
+++ frontend/app/tests/e2e/docs-regression-check/tutorials/tutorial-1_object-create-update-diff-and-merge.spec.ts
@@ -5,200 +5,167 @@ import { ACCOUNT_STATE_PATH } from "../../../constants";
 import { saveScreenshotForDocs } from "../../../utils";
 
 test.describe("Getting started with Infrahub - Object and branch creation, update, diff and merge", () => {
-	test.describe.configure({ mode: "serial" });
-	test.use({ storageState: ACCOUNT_STATE_PATH.ADMIN });
-	test.slow();
+  test.describe.configure({ mode: "serial" });
+  test.use({ storageState: ACCOUNT_STATE_PATH.ADMIN });
+  test.slow();
 
-	let dateBeforeTest: Date;
+  let dateBeforeTest: Date;
 
-	test("1. Create a new organization", async ({ page }) => {
-		await page.goto("/");
+  test("1. Create a new organization", async ({ page }) => {
+    await page.goto("/");
 
-		await page
-			.getByTestId("sidebar")
-			.getByRole("button", { name: "Organization" })
-			.click();
-		await page.getByRole("menuitem", { name: "Tenant" }).click();
-		await expect(page.getByRole("heading", { name: "Tenant" })).toBeVisible();
-		await expect(page.getByRole("link", { name: "Duff" })).toBeVisible();
+    await page.getByTestId("sidebar").getByRole("button", { name: "Organization" }).click();
+    await page.getByRole("menuitem", { name: "Tenant" }).click();
+    await expect(page.getByRole("heading", { name: "Tenant" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Duff" })).toBeVisible();
 
-		dateBeforeTest = new Date();
+    dateBeforeTest = new Date();
 
-		await test.step("fill and submit form for new organization", async () => {
-			await page.getByTestId("create-object-button").click();
+    await test.step("fill and submit form for new organization", async () => {
+      await page.getByTestId("create-object-button").click();
 
-			await page.getByLabel("Name *").fill("my-first-tenant");
-			await page.getByLabel("Description").fill("Testing Infrahub");
-			await saveScreenshotForDocs(page, "tutorial_1_organization_create");
+      await page.getByLabel("Name *").fill("my-first-tenant");
+      await page.getByLabel("Description").fill("Testing Infrahub");
+      await saveScreenshotForDocs(page, "tutorial_1_organization_create");
 
-			await page.getByRole("button", { name: "Save" }).click();
-		});
+      await page.getByRole("button", { name: "Save" }).click();
+    });
 
-		await test.step("confirm creation and update UI", async () => {
-			await expect(page.locator("#alert-success-Tenant-created")).toContainText(
-				"Tenant created",
-			);
-			await expect(
-				page.getByRole("link", { name: "my-first-tenant" }),
-			).toBeVisible();
-			await expect(page.getByText("Testing Infrahub")).toBeVisible();
-		});
-	});
+    await test.step("confirm creation and update UI", async () => {
+      await expect(page.locator("#alert-success-Tenant-created")).toContainText("Tenant created");
+      await expect(page.getByRole("link", { name: "my-first-tenant" })).toBeVisible();
+      await expect(page.getByText("Testing Infrahub")).toBeVisible();
+    });
+  });
 
-	test("2. Create a new branch", async ({ page }) => {
-		await page.goto("/");
-		await page.getByTestId("branch-selector-trigger").click();
-		await page.getByTestId("create-branch-button").click();
+  test("2. Create a new branch", async ({ page }) => {
+    await page.goto("/");
+    await page.getByTestId("branch-selector-trigger").click();
+    await page.getByTestId("create-branch-button").click();
 
-		await test.step("fill and submit form for new organization", async () => {
-			await expect(page.getByText("Create a new branch")).toBeVisible();
-			await page.getByLabel("New branch name *").fill("cr1234");
-			await saveScreenshotForDocs(page, "tutorial_1_branch_creation");
-			await page.getByRole("button", { name: "Create a new branch" }).click();
-		});
+    await test.step("fill and submit form for new organization", async () => {
+      await expect(page.getByText("Create a new branch")).toBeVisible();
+      await page.getByLabel("New branch name *").fill("cr1234");
+      await saveScreenshotForDocs(page, "tutorial_1_branch_creation");
+      await page.getByRole("button", { name: "Create a new branch" }).click();
+    });
 
-		// After submit
-		await expect(page.getByTestId("branch-selector-trigger")).toContainText(
-			"cr1234",
-		);
-		await expect(page).toHaveURL(/.*?branch=cr1234/);
-	});
+    // After submit
+    await expect(page.getByTestId("branch-selector-trigger")).toContainText("cr1234");
+    await expect(page).toHaveURL(/.*?branch=cr1234/);
+  });
 
-	test("3. Update an organization", async ({ page }) => {
-		await test.step("Go to the newly created organization on branch cr1234", async () => {
-			await page.goto("/?branch=cr1234");
+  test("3. Update an organization", async ({ page }) => {
+    await test.step("Go to the newly created organization on branch cr1234", async () => {
+      await page.goto("/?branch=cr1234");
 
-			await page
-				.getByTestId("sidebar")
-				.getByRole("button", { name: "Organization" })
-				.click();
-			await page.getByRole("menuitem", { name: "Tenant" }).click();
+      await page.getByTestId("sidebar").getByRole("button", { name: "Organization" }).click();
+      await page.getByRole("menuitem", { name: "Tenant" }).click();
 
-			const myFirstOrgLink = page
-				.getByTestId("object-items")
-				.getByRole("link", { name: "my-first-tenant" });
-			await expect(myFirstOrgLink).toBeVisible();
-			await saveScreenshotForDocs(page, "tutorial_1_organizations");
-			await myFirstOrgLink.click();
-		});
+      const myFirstOrgLink = page
+        .getByTestId("object-items")
+        .getByRole("link", { name: "my-first-tenant" });
+      await expect(myFirstOrgLink).toBeVisible();
+      await saveScreenshotForDocs(page, "tutorial_1_organizations");
+      await myFirstOrgLink.click();
+    });
 
-		await test.step("Edit the organization description on branch cr1234", async () => {
-			const editButton = page.getByTestId("edit-button");
-			await expect(editButton).toBeVisible();
-			await saveScreenshotForDocs(page, "tutorial_1_organization_details");
-			await editButton.click();
+    await test.step("Edit the organization description on branch cr1234", async () => {
+      const editButton = page.getByTestId("edit-button");
+      await expect(editButton).toBeVisible();
+      await saveScreenshotForDocs(page, "tutorial_1_organization_details");
+      await editButton.click();
 
-			await page.getByLabel("Description").fill("Changes from branch cr1234");
-			await saveScreenshotForDocs(page, "tutorial_1_organization_edit");
-			await page.getByRole("button", { name: "Save" }).click();
-		});
+      await page.getByLabel("Description").fill("Changes from branch cr1234");
+      await saveScreenshotForDocs(page, "tutorial_1_organization_edit");
+      await page.getByRole("button", { name: "Save" }).click();
+    });
 
-		await test.step("Update confirmation and update UI", async () => {
-			await expect(page.getByText("Tenant updated")).toBeVisible();
-			await expect(page.getByText("Changes from branch cr1234")).toBeVisible();
-		});
+    await test.step("Update confirmation and update UI", async () => {
+      await expect(page.getByText("Tenant updated")).toBeVisible();
+      await expect(page.getByText("Changes from branch cr1234")).toBeVisible();
+    });
 
-		await test.step("See initial value on main branch", async () => {
-			await page.getByTestId("branch-selector-trigger").click();
-			await page.getByRole("option", { name: "main default" }).click();
-			await expect(
-				page.getByTestId("object-details").getByText("Testing Infrahub"),
-			).toBeVisible();
-		});
-	});
+    await test.step("See initial value on main branch", async () => {
+      await page.getByTestId("branch-selector-trigger").click();
+      await page.getByRole("option", { name: "main default" }).click();
+      await expect(page.getByTestId("object-details").getByText("Testing Infrahub")).toBeVisible();
+    });
+  });
 
-	test("4. View the Diff and Merge the branch cr1234 into main", async ({
-		page,
-	}) => {
-		await test.step("Go to branch cr1234 page", async () => {
-			await page.goto("/?branch=cr1234");
-			await page.getByTestId("branch-selector-trigger").click();
-			await page.getByRole("link", { name: "View all branches" }).click();
-			await saveScreenshotForDocs(page, "tutorial_1_branch_list");
-			await page.getByRole("link", { name: "cr1234" }).click();
-			await expect(page.getByRole("heading", { name: "cr1234" })).toBeVisible();
-		});
+  test("4. View the Diff and Merge the branch cr1234 into main", async ({ page }) => {
+    await test.step("Go to branch cr1234 page", async () => {
+      await page.goto("/?branch=cr1234");
+      await page.getByTestId("branch-selector-trigger").click();
+      await page.getByRole("link", { name: "View all branches" }).click();
+      await saveScreenshotForDocs(page, "tutorial_1_branch_list");
+      await page.getByRole("link", { name: "cr1234" }).click();
+      await expect(page.getByRole("heading", { name: "cr1234" })).toBeVisible();
+    });
 
-		await test.step("trigger the diff update", async () => {
-			await page.getByText("Data").click();
-			await expect(page.getByText("We are computing the diff")).toBeVisible();
-			await page.getByRole("button", { name: "Refresh" }).click();
-			await expect(page.getByText("Diff updated!")).toBeVisible();
-		});
+    await test.step("trigger the diff update", async () => {
+      await page.getByText("Data").click();
+      await expect(page.getByText("We are computing the diff")).toBeVisible();
+      await page.getByRole("button", { name: "Refresh" }).click();
+      await expect(page.getByText("Diff updated!")).toBeVisible();
+    });
 
-		await test.step("View branch diff", async () => {
-			await page.getByText("UpdatedTenantmy-first-Tenant").click();
-			await expect(page.getByText("Testing Infrahub")).toBeVisible();
-			await saveScreenshotForDocs(page, "tutorial_1_branch_diff");
-			await expect(page.getByText("Changes from branch cr1234")).toBeVisible();
-		});
+    await test.step("View branch diff", async () => {
+      await page.getByText("UpdatedTenantmy-first-Tenant").click();
+      await expect(page.getByText("Testing Infrahub")).toBeVisible();
+      await saveScreenshotForDocs(page, "tutorial_1_branch_diff");
+      await expect(page.getByText("Changes from branch cr1234")).toBeVisible();
+    });
 
-		await test.step("Merge branch cr1234 into main", async () => {
-			await page.getByText("Details", { exact: true }).click();
-			const mergeButton = page.getByRole("button", { name: "Merge" });
-			await expect(mergeButton).toBeVisible();
-			await saveScreenshotForDocs(page, "tutorial_1_branch_details");
-			await mergeButton.click();
-			await expect(page.locator("#alert-success")).toContainText(
-				"Branch merge requested!",
-			);
-			await page.getByTestId("tasks-accordion").click();
-			await expect(
-				page.getByText("COMPLETEDMerge branch graphQL"),
-			).toBeVisible();
-		});
+    await test.step("Merge branch cr1234 into main", async () => {
+      await page.getByText("Details", { exact: true }).click();
+      const mergeButton = page.getByRole("button", { name: "Merge" });
+      await expect(mergeButton).toBeVisible();
+      await saveScreenshotForDocs(page, "tutorial_1_branch_details");
+      await mergeButton.click();
+      await expect(page.locator("#alert-success")).toContainText("Branch merge requested!");
+      await page.getByTestId("tasks-accordion").click();
+      await expect(page.getByText("COMPLETEDMerge branch graphQL")).toBeVisible();
+    });
 
-		await test.step("Validate merged changes in main", async () => {
-			await page.getByTestId("branch-selector-trigger").click();
-			await page.getByRole("option", { name: "main default" }).click();
-			await expect(page.getByTestId("branch-selector-trigger")).toContainText(
-				"main",
-			);
+    await test.step("Validate merged changes in main", async () => {
+      await page.getByTestId("branch-selector-trigger").click();
+      await page.getByRole("option", { name: "main default" }).click();
+      await expect(page.getByTestId("branch-selector-trigger")).toContainText("main");
 
-			await page
-				.getByTestId("sidebar")
-				.getByRole("button", { name: "Organization" })
-				.click();
-			await page.getByRole("menuitem", { name: "Tenant" }).click();
+      await page.getByTestId("sidebar").getByRole("button", { name: "Organization" }).click();
+      await page.getByRole("menuitem", { name: "Tenant" }).click();
 
-			await expect(page.getByTestId("object-items")).toContainText(
-				"Changes from branch cr1234",
-			);
-		});
-	});
+      await expect(page.getByTestId("object-items")).toContainText("Changes from branch cr1234");
+    });
+  });
 
-	test("5. Browse historical data", async ({ page }) => {
-		await page.goto("/objects/OrganizationTenant");
+  test("5. Browse historical data", async ({ page }) => {
+    await page.goto("/objects/OrganizationTenant");
 
-		await test.step("Row my-first-tenant is visible at current time", async () => {
-			await expect(
-				page.getByRole("link", { name: "my-first-tenant" }),
-			).toBeVisible();
-		});
+    await test.step("Row my-first-tenant is visible at current time", async () => {
+      await expect(page.getByRole("link", { name: "my-first-tenant" })).toBeVisible();
+    });
 
-		await test.step("Row my-first-tenant is not visible when date prior to its creation is selected", async () => {
-			await page.getByTestId("timeframe-selector").click();
-			await saveScreenshotForDocs(page, "tutorial_2_historical");
-			await page
-				.getByRole("option", {
-					name: format(dateBeforeTest, "h:mm aa"),
-					exact: true,
-				})
-				.click();
-			await expect(page.getByRole("link", { name: "Duff" })).toBeVisible();
-			await expect(
-				page.getByRole("link", { name: "Changes from branch cr1234" }),
-			).not.toBeVisible();
-		});
+    await test.step("Row my-first-tenant is not visible when date prior to its creation is selected", async () => {
+      await page.getByTestId("timeframe-selector").click();
+      await saveScreenshotForDocs(page, "tutorial_2_historical");
+      await page
+        .getByRole("option", {
+          name: format(dateBeforeTest, "h:mm aa"),
+          exact: true,
+        })
+        .click();
+      await expect(page.getByRole("link", { name: "Duff" })).toBeVisible();
+      await expect(
+        page.getByRole("link", { name: "Changes from branch cr1234" })
+      ).not.toBeVisible();
+    });
 
-		await test.step("Row my-first-tenant is visible again when we reset date input", async () => {
-			await page.getByTestId("reset-timeframe-selector").click();
-			await expect(page.getByTestId("object-items")).toContainText(
-				"Changes from branch cr1234",
-			);
-			await expect(page.getByTestId("object-items")).not.toContainText(
-				"Testing Infrahub",
-			);
-		});
-	});
+    await test.step("Row my-first-tenant is visible again when we reset date input", async () => {
+      await page.getByTestId("reset-timeframe-selector").click();
+      await expect(page.getByTestId("object-items")).toContainText("Changes from branch cr1234");
+      await expect(page.getByTestId("object-items")).not.toContainText("Testing Infrahub");
+    });
+  });
 });
