36336c519368d81ce77f314b3e2bc84f6d67509d
bilalabbad <bilal@opsmill.com>
2026-01-15 15:45:57 +0100

    more

--- frontend/app/src/entities/nodes/object/ui/object-details/object-details-card.tsx
+++ frontend/app/src/entities/nodes/object/ui/object-details/object-details-card.tsx
@@ -1,3 +1,4 @@
+import ObjectEditSlideOverTrigger from "@/shared/components/form/object-edit-slide-over-trigger";
 import { Card, CardWithBorder } from "@/shared/components/ui/card";
 
 import { ObjectDataDisplay } from "@/entities/nodes/object/ui/object-details/object-data-display/object-data-display";
@@ -20,7 +21,16 @@ export function ObjectDetailsCard({
 }: ObjectDetailsCardProps) {
   return (
     <Card className={className} data-testid="object-details">
-      <CardWithBorder.Title className="border-gray-200 border-b">Details</CardWithBorder.Title>
+      <CardWithBorder.Title className="flex items-center border-gray-200 border-b">
+        Details
+        <ObjectEditSlideOverTrigger
+          data={objectData}
+          schema={objectSchema}
+          permission={permission}
+          variant="ghost"
+          className="ml-auto size-5 text-neutral-400 hover:bg-neutral-200 hover:text-neutral-500"
+        />
+      </CardWithBorder.Title>
 
       <ObjectDataDisplay
         objectSchema={objectSchema}
