0ede8856908e96f545dbb8125fe5d995e636cd73
Sola Babatunde <solababatunde12@gmail.com>
2026-01-16 06:45:36 +0100

    empty __init__ file

--- backend/infrahub/core/validators/schema_branch/__init__.py
+++
@@ -1,7 +0,0 @@
-from .hierarchical_nodes_restricted_words_validator import HierarchicalNodesRestrictedWords
-from .interface import SchemaBranchValidator
-
-__all__ = [
-    "HierarchicalNodesRestrictedWords",
-    "SchemaBranchValidator",
-]
