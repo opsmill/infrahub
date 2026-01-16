60aa3210924d58873098ab6c1583080637e93db6
pa-lem <pa-lem@hotmail.fr>
2026-01-16 14:11:09 +0100

    update test

--- frontend/app/tests/e2e/repository/repository-objects.spec.ts
+++ frontend/app/tests/e2e/repository/repository-objects.spec.ts
@@ -9,6 +9,7 @@ const REPO_NAME = "test repository";
 
 test.describe("Repository - Creation and objects view", () => {
   test.use({ storageState: ACCOUNT_STATE_PATH.ADMIN });
+  test.describe.configure({ mode: "serial" });
 
   const BRANCH_NAME = generateRandomBranchName("repository-branch");
 
@@ -37,5 +38,40 @@ test.describe("Repository - Creation and objects view", () => {
       .filter({ hasText: "Objects" })
       .click();
     await expect(page.getByText("No objects found for this")).toBeVisible();
+  });
+
+  test("test", async ({ page }) => {
+    await test.step("access repository detailed page", async () => {
+      await page.goto("http://localhost:8080/");
+      await page.getByRole("button", { name: "Integrations" }).click();
+      await page.getByRole("menuitem", { name: "Git Repositories" }).click();
+      await expect(page.getByRole("heading", { name: "Git Repository" })).toBeVisible();
+      await page.getByRole("link", { name: "test-read-only-repo" }).click();
+
+      await expect(page.getByRole("heading", { name: "test-read-only-repo" })).toBeVisible();
+    });
+
+    await test.step("trigger connectivity action", async () => {
+      await page.getByTestId("object-details-menu").click();
+      await page.getByRole("menuitem", { name: "Check connectivity" }).click();
+      await expect(
+        page.getByRole("heading", { name: "Check repository connectivity" })
+      ).toBeVisible();
+      await page.getByRole("button", { name: "Check now" }).click();
+      await expect(page.getByText("Successfully accessed")).toBeVisible();
+      await page.getByRole("button", { name: "Done" }).click();
+    });
+
+    await test.step("trigger latest commit action", async () => {
+      await page.getByTestId("object-details-menu").click();
+      await page.getByRole("menuitem", { name: "Import latest from remote" }).click();
+      await expect(page.getByText("Import from remote started.")).toBeVisible();
+    });
+
+    await test.step("trigger current commit from remote action", async () => {
+      await page.getByTestId("object-details-menu").click();
+      await page.getByRole("menuitem", { name: "Import current commit" }).click();
+      await expect(page.getByText("Import of current commit")).toBeVisible();
+    });
   });
 });
