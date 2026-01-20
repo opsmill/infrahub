af64a5389d91e355701b3e1a92c5826fef880f8c
pa-lem <pa-lem@hotmail.fr>
2026-01-20 15:29:12 +0100

    remove tooltip menu item

--- frontend/app/src/shared/components/aria/menu.tsx
+++ frontend/app/src/shared/components/aria/menu.tsx
@@ -15,7 +15,6 @@ import {
 
 import { Popover } from "@/shared/components/aria/popover";
 import { disabledStyle } from "@/shared/components/aria/style-rac";
-import { Tooltip } from "@/shared/components/ui/tooltip";
 import { useCopyToClipboard } from "@/shared/hooks/useCopyToClipboard";
 import { classNames } from "@/shared/utils/common";
 
@@ -100,22 +99,5 @@ export function CopyToClipboardMenuItem({
       <CopyIcon className="size-3" />
       {children}
     </MenuItem>
-  );
-}
-
-export interface MenuItemWithTooltipProps extends MenuItemProps {
-  tooltipContent?: React.ReactNode;
-  tooltipEnabled?: boolean;
-}
-
-export function MenuItemWithTooltip({
-  tooltipContent,
-  tooltipEnabled,
-  ...props
-}: MenuItemWithTooltipProps) {
-  return (
-    <Tooltip enabled={tooltipEnabled} content={tooltipContent}>
-      <MenuItem {...props} />
-    </Tooltip>
   );
 }
