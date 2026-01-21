aebd98c44e005178f7f2c32dcfccaeb22282d7b0
pa-lem <pa-lem@hotmail.fr>
2026-01-21 10:33:29 +0100

    remove actions

--- frontend/app/src/entities/repository/api/actions.ts
+++ frontend/app/src/entities/repository/api/actions.ts
@@ -8,25 +8,3 @@ export const CHECK_REPOSITORY_CONNECTIVITY = graphql(`
     }
   }
 `);
-
-export const REIMPORT_LAST_COMMIT = graphql(`
-  mutation REIMPORT_LAST_COMMIT($repositoryId: String!) {
-    InfrahubReadOnlyRepositoryImportLastCommit(data: { id: $repositoryId }) {
-      ok
-      task {
-        id
-      }
-    }
-  }
-`);
-
-export const IMPORT_READONLY_REPOSITORY_LAST_COMMIT = graphql(`
-  mutation IMPORT_READONLY_REPOSITORY_LAST_COMMIT($repositoryId: String!) {
-    InfrahubRepositoryProcess(data: { id: $repositoryId }) {
-      ok
-      task {
-        id
-      }
-    }
-  }
-`);
