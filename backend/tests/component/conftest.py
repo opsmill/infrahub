8a4cedc02e7601d906e90c61f7dd4132984f9396
Aaron McCarty <ajtmccarty@pm.me>
2026-01-23 09:02:33 -0600

    handle agnostic attributes on aware objects correctly during branch delete (#8212)
    * delete branch-agnostic attrs of a branch-aware node during branch delete
    
    * add changelog
    
    * add migration for possible orphaned Nodes
    
    * small cypher updates
    
    * migration test
    
    * fix comment typo

--- backend/tests/component/conftest.py
+++ backend/tests/component/conftest.py
@@ -36,6 +36,8 @@ from infrahub.core.constants import (
     InfrahubKind,
     PermissionAction,
     PermissionDecision,
+    RelationshipCardinality,
+    RelationshipDirection,
 )
 from infrahub.core.initialization import (
     create_branch,
@@ -50,6 +52,7 @@ from infrahub.core.protocols_base import CoreNode
 from infrahub.core.schema import (
     GenericSchema,
     NodeSchema,
+    RelationshipSchema,
     SchemaRoot,
     core_models,
 )
@@ -2960,3 +2963,67 @@ async def generic_car_person_schema(default_branch: Branch, data_schema) -> None
 
     schema_root = SchemaRoot(**schema)
     registry.schema.register_schema(schema=schema_root, branch=default_branch.name)
+
+
+@pytest.fixture
+async def branch_aware_node_with_agnostic_attrs_schema(default_branch: Branch, data_schema: None) -> SchemaBranch:
+    """Schema with a branch-aware Node that has branch-agnostic attributes and relationships."""
+    schema = SchemaRoot(
+        nodes=[
+            NodeSchema(
+                name="Device",
+                namespace="Test",
+                default_filter="name__value",
+                branch=BranchSupportType.AWARE,
+                uniqueness_constraints=[["name__value"]],
+                attributes=[
+                    AttributeSchema(
+                        name="name",
+                        kind="Text",
+                        unique=True,
+                        branch=BranchSupportType.AWARE,
+                    ),
+                    AttributeSchema(
+                        name="serial_number",
+                        kind="Text",
+                        optional=True,
+                        branch=BranchSupportType.AGNOSTIC,
+                    ),
+                ],
+                relationships=[
+                    RelationshipSchema(
+                        name="site",
+                        peer="TestSite",
+                        optional=True,
+                        cardinality=RelationshipCardinality.ONE,
+                        direction=RelationshipDirection.OUTBOUND,
+                        branch=BranchSupportType.AGNOSTIC,
+                    ),
+                ],
+            ),
+            NodeSchema(
+                name="Site",
+                namespace="Test",
+                default_filter="name__value",
+                branch=BranchSupportType.AWARE,
+                uniqueness_constraints=[["name__value"]],
+                attributes=[
+                    AttributeSchema(
+                        name="name",
+                        kind="Text",
+                        unique=True,
+                    ),
+                ],
+                relationships=[
+                    RelationshipSchema(
+                        name="devices",
+                        peer="TestDevice",
+                        cardinality=RelationshipCardinality.MANY,
+                        direction=RelationshipDirection.INBOUND,
+                        branch=BranchSupportType.AGNOSTIC,
+                    ),
+                ],
+            ),
+        ],
+    )
+    return registry.schema.register_schema(schema=schema, branch=default_branch.name)
