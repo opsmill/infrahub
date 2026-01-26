c374c52746516206ca7ee08e9c73f8eba94f4b75
Noah Robin <120611026+nrnvgh@users.noreply.github.com>
2026-01-26 10:18:30 -0500

    Updated shift-click handling of node lists to be the same for select and deselect (#8230)

--- frontend/app/tests/e2e/objects/list/object-list-select-range.spec.ts
+++ frontend/app/tests/e2e/objects/list/object-list-select-range.spec.ts
@@ -103,4 +103,89 @@ test.describe("/objects/:objectKind - Bulk edit some rows", () => {
     await expect(page.getByTestId("identifier-checkbox-cell").nth(4)).not.toBeChecked();
     await expect(page.getByTestId("identifier-checkbox-cell").nth(5)).not.toBeChecked();
   });
+
+  test("should deselect range forward when shift-clicking a selected row", async ({ page }) => {
+    await page.goto(`/objects/InfraDevice?branch=${BRANCH_NAME}`);
+    await page.getByTestId("identifier-checkbox-cell").nth(1).click();
+    await page
+      .getByTestId("identifier-checkbox-cell")
+      .nth(4)
+      .click({ modifiers: ["Shift"] });
+
+    await page.getByTestId("identifier-checkbox-cell").nth(1).click();
+    await page
+      .getByTestId("identifier-checkbox-cell")
+      .nth(3)
+      .click({ modifiers: ["Shift"] });
+
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(0)).not.toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(1)).not.toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(2)).not.toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(3)).not.toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(4)).toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(5)).not.toBeChecked();
+  });
+
+  test("should deselect range backward when shift-clicking a selected row", async ({ page }) => {
+    await page.goto(`/objects/InfraDevice?branch=${BRANCH_NAME}`);
+    await page.getByTestId("identifier-checkbox-cell").nth(1).click();
+    await page
+      .getByTestId("identifier-checkbox-cell")
+      .nth(4)
+      .click({ modifiers: ["Shift"] });
+
+    await page.getByTestId("identifier-checkbox-cell").nth(3).click();
+    await page
+      .getByTestId("identifier-checkbox-cell")
+      .nth(1)
+      .click({ modifiers: ["Shift"] });
+
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(0)).not.toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(1)).not.toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(2)).not.toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(3)).not.toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(4)).toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(5)).not.toBeChecked();
+  });
+
+  test("should reset anchor after selecting all rows", async ({ page }) => {
+    await page.goto(`/objects/BuiltinTag?branch=${BRANCH_NAME}`);
+    await expect(page.getByTestId("identifier-checkbox-cell")).toHaveCount(3);
+    await page.getByTestId("select-all-rows").click();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(0)).toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(1)).toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(2)).toBeChecked();
+
+    await page
+      .getByTestId("identifier-checkbox-cell")
+      .nth(1)
+      .click({ modifiers: ["Shift"] });
+
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(0)).toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(1)).not.toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(2)).toBeChecked();
+  });
+
+  test("should use last click as shift-click anchor", async ({ page }) => {
+    await page.goto(`/objects/InfraDevice?branch=${BRANCH_NAME}`);
+    await page.getByTestId("identifier-checkbox-cell").nth(6).click();
+    await page
+      .getByTestId("identifier-checkbox-cell")
+      .nth(2)
+      .click({ modifiers: ["Shift"] });
+
+    await page.getByTestId("identifier-checkbox-cell").nth(4).click();
+    await page
+      .getByTestId("identifier-checkbox-cell")
+      .nth(3)
+      .click({ modifiers: ["Shift"] });
+
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(0)).not.toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(1)).not.toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(2)).toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(3)).not.toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(4)).not.toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(5)).toBeChecked();
+    await expect(page.getByTestId("identifier-checkbox-cell").nth(6)).toBeChecked();
+  });
 });
