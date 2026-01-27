56f2fab5fc1c57572e337901f1f25fcc1a9565c3
pa-lem <pa-lem@hotmail.fr>
2026-01-27 16:54:14 +0100

    consistency for date format

--- frontend/app/src/shared/utils/date.ts
+++ frontend/app/src/shared/utils/date.ts
@@ -1,9 +1,9 @@
 import { format, formatDistance, getYear } from "date-fns";
 
-export const DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss (O)";
+export const DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
 export function formatFullDate(date: string | number | Date) {
-  return format(date, "dd/MM/yyyy HH:mm");
+  return format(date, DATE_TIME_FORMAT);
 }
 
 export function formatRelativeTimeFromNow(date: number | Date) {
