f64ad4fd187b14af50a97e0880d3a05a5a1b49ff
bilalabbad <bilal@opsmill.com>
2026-01-29 16:18:23 +0100

    fix

--- frontend/app/src/entities/nodes/object/ui/object-table/object-table.tsx
+++ frontend/app/src/entities/nodes/object/ui/object-table/object-table.tsx
@@ -3,6 +3,7 @@ import { DataTable } from "@/shared/components/table/data-table";
 import { InfiniteScroll } from "@/shared/components/utils/infinite-scroll";
 
 import { useObjects } from "@/entities/nodes/object/domain/get-objects.query";
+import { useObjectsCount } from "@/entities/nodes/object/domain/get-objects-count.query";
 import { useObjectTableContext } from "@/entities/nodes/object/ui/object-table/object-table-context";
 import { ObjectTableEmpty } from "@/entities/nodes/object/ui/object-table/object-table-empty";
 import { getObjectActionsColumn } from "@/entities/nodes/object/ui/object-table/utils/get-object-actions-column";
@@ -10,6 +11,11 @@ import { getObjectTableColumns } from "@/entities/nodes/object/ui/object-table/u
 
 export const ObjectTable = () => {
   const { filters, selectedSchema, permission } = useObjectTableContext();
+
+  const { data: count } = useObjectsCount({
+    objectKind: selectedSchema.kind!,
+    filters,
+  });
 
   const { data, fetchNextPage, error, hasNextPage, isPending, isFetchingNextPage } = useObjects({
     schema: selectedSchema,
