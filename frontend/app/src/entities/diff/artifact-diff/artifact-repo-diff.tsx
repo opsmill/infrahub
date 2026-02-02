8f738027e3db038ef082e9828e25ebb722e720e1
bilalabbad <bilal@opsmill.com>
2026-02-02 13:49:26 +0100

    clean up ArtifactRepoDiff

--- frontend/app/src/entities/diff/artifact-diff/artifact-repo-diff.tsx
+++ frontend/app/src/entities/diff/artifact-diff/artifact-repo-diff.tsx
@@ -1,67 +1,16 @@
-import { gql } from "@apollo/client";
-import { useAtom } from "jotai";
-
-import useQuery from "@/shared/api/graphql/useQuery";
 import Accordion from "@/shared/components/display/accordion";
-import ErrorScreen from "@/shared/components/errors/error-screen";
-import NoDataFound from "@/shared/components/errors/no-data-found";
-import { Badge } from "@/shared/components/ui/badge";
 
-import { getArtifactDetails } from "@/entities/artifacts/api/getArtifacts";
-import { getNodeLabel } from "@/entities/nodes/object/utils/get-node-label";
-import { nodeSchemasAtom } from "@/entities/schema/stores/schema.atom";
-import "react-diff-view/style/index.css";
+import { ArtifactContentDiff } from "@/entities/diff/artifact-diff/artifact-content-diff";
+import type { ArtifactDiff } from "@/entities/diff/domain/get-artifacts-diff";
 
-import { Row } from "@/shared/components/container";
-import { LoadingIndicator } from "@/shared/components/loading/loading-indicator";
+interface ArtifactRepoDiffProps {
+  diff: ArtifactDiff;
+}
 
-import { ArtifactContentDiff } from "./artifact-content-diff";
-
-export const ArtifactRepoDiff = (props: any) => {
-  const { diff } = props;
-
-  const [schemaList] = useAtom(nodeSchemasAtom);
-
-  const schemaData = schemaList.find((s) => s.kind === "Artifact");
-
-  const queryString = schemaData
-    ? getArtifactDetails({
-        id: diff.id,
-      })
-    : // Empty query to make the gql parsing work
-      // TODO: Find another solution for queries while loading schemaData
-      "query { ok }";
-
-  const query = gql`
-    ${queryString}
-  `;
-
-  const { loading, error, data } = useQuery(query, { skip: !schemaData });
-
-  if (loading) {
-    return <LoadingIndicator className="m-4 h-10" />;
-  }
-
-  if (error) {
-    return <ErrorScreen message="Something went wrong when artifact differences." />;
-  }
-
-  if (!diff) {
-    return <NoDataFound message="No artifact diff" />;
-  }
-
-  const artifactNode = data?.CoreArtifact?.edges[0]?.node?.object?.node;
-
-  const title = (
-    <Row>
-      {artifactNode && <Badge className="font-normal">{getNodeLabel(artifactNode)}</Badge>}
-      {diff?.display_label}
-    </Row>
-  );
-
+export function ArtifactRepoDiff({ diff }: ArtifactRepoDiffProps) {
   return (
     <div className="rounded-lg bg-white p-2 text-sm shadow-sm" id={diff.id}>
-      <Accordion title={title}>
+      <Accordion title={diff.display_label}>
         <ArtifactContentDiff
           id={diff.id}
           itemNew={diff.item_new}
@@ -70,4 +19,4 @@ export const ArtifactRepoDiff = (props: any) => {
       </Accordion>
     </div>
   );
-};
+}
