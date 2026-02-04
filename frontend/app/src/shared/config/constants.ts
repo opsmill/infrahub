566a38fc40cb29f09b3eccfe1bac66b7fa14ea8f
pa-lem <pa-lem@hotmail.fr>
2026-02-04 16:37:33 +0100

    lint

--- frontend/app/src/shared/config/constants.ts
+++ frontend/app/src/shared/config/constants.ts
@@ -1,6 +1,7 @@
+import type { CheckType } from "@/shared/api/graphql/generated/graphql";
+
 import type { RelationshipKind } from "@/entities/nodes/types";
 import { PROPOSED_CHANGE_OBJECT } from "@/entities/proposed-changes/constants";
-import type { CheckType } from "@/shared/api/graphql/generated/graphql";
 
 export const DEFAULT_BRANCH_NAME = "main";
 
@@ -62,68 +63,68 @@ export const NUMBER_POOL_OBJECT = "CoreNumberPool";
 export const TASK_OBJECT = "InfrahubTask";
 
 export const MENU_EXCLUDELIST = [
-	PROPOSED_CHANGE_OBJECT,
-	"CoreChangeComment",
-	"CoreChangeThread",
-	"CoreFileThread",
-	"CoreArtifactThread",
-	"CoreObjectThread",
-	"InternalRefreshToken",
-	"CoreThreadComment",
-	"CoreArtifactCheck",
-	"CoreArtifactTarget",
-	"CoreCheck",
-	"CoreComment",
-	"CoreGeneratorCheck",
-	"CoreGeneratorValidator",
-	"CoreNode",
-	"CoreStandardCheck",
-	"CoreTaskTarget",
-	"CoreThread",
-	"CoreDataCheck",
-	"CoreFileCheck",
-	"CoreSchemaCheck",
-	"CoreSchemaValidator",
-	"CoreDataValidator",
-	"CoreRepositoryValidator",
-	"CoreArtifactValidator",
-	"CoreUserValidator",
-	"CoreValidator",
-	"LineageOwner",
-	"LineageSource",
+  PROPOSED_CHANGE_OBJECT,
+  "CoreChangeComment",
+  "CoreChangeThread",
+  "CoreFileThread",
+  "CoreArtifactThread",
+  "CoreObjectThread",
+  "InternalRefreshToken",
+  "CoreThreadComment",
+  "CoreArtifactCheck",
+  "CoreArtifactTarget",
+  "CoreCheck",
+  "CoreComment",
+  "CoreGeneratorCheck",
+  "CoreGeneratorValidator",
+  "CoreNode",
+  "CoreStandardCheck",
+  "CoreTaskTarget",
+  "CoreThread",
+  "CoreDataCheck",
+  "CoreFileCheck",
+  "CoreSchemaCheck",
+  "CoreSchemaValidator",
+  "CoreDataValidator",
+  "CoreRepositoryValidator",
+  "CoreArtifactValidator",
+  "CoreUserValidator",
+  "CoreValidator",
+  "LineageOwner",
+  "LineageSource",
 ];
 
 export const NODE_PATH_EXCLUDELIST = ["property"];
 
 export const VALIDATION_STATES = {
-	QUEUED: "queued",
-	IN_PROGRESS: "in_progress",
-	COMPLETED: "completed",
+  QUEUED: "queued",
+  IN_PROGRESS: "in_progress",
+  COMPLETED: "completed",
 };
 
 export const VALIDATION_CONCLUSIONS = {
-	UNKNOWN: "unknown",
-	FAILURE: "failure",
-	SUCCESS: "success",
+  UNKNOWN: "unknown",
+  FAILURE: "failure",
+  SUCCESS: "success",
 };
 
 export const CHECKS_LABEL = {
-	EMPTY: "Empty",
-	UNKOWN: "Unkown",
-	QUEUED: "Queued",
-	FAILURE: "Failure",
-	SUCCESS: "Success",
-	IN_PROGRESS: "In progress",
+  EMPTY: "Empty",
+  UNKOWN: "Unkown",
+  QUEUED: "Queued",
+  FAILURE: "Failure",
+  SUCCESS: "Success",
+  IN_PROGRESS: "In progress",
 };
 
 export const VALIDATIONS_ENUM_MAP: { [key: string]: CheckType } = {
-	CoreArtifactValidator: "ARTIFACT",
-	CoreDataValidator: "DATA",
-	CoreGeneratorValidator: "GENERATOR",
-	CoreRepositoryValidator: "REPOSITORY",
-	CoreSchemaValidator: "SCHEMA",
-	CoreUserValidator: "USER",
-	all: "ALL",
+  CoreArtifactValidator: "ARTIFACT",
+  CoreDataValidator: "DATA",
+  CoreGeneratorValidator: "GENERATOR",
+  CoreRepositoryValidator: "REPOSITORY",
+  CoreSchemaValidator: "SCHEMA",
+  CoreUserValidator: "USER",
+  all: "ALL",
 };
 
 export const MAX_VALUE_LENGTH_DISPLAY = 40;
@@ -132,29 +133,26 @@ export const MAX_PASSWORD_DOTS_DISPLAY = 20;
 export const attributesKindForDetailsViewExclude = ["HashedPassword"];
 
 export const relationshipsForListView = {
-	one: ["Attribute", "Hierarchy", "Parent"],
-	many: ["Attribute"],
+  one: ["Attribute", "Hierarchy", "Parent"],
+  many: ["Attribute"],
 };
 
 export const relationshipsForDetailsView: {
-	one: RelationshipKind[];
-	many: RelationshipKind[];
+  one: RelationshipKind[];
+  many: RelationshipKind[];
 } = {
-	one: ["Generic", "Attribute", "Component", "Parent", "Hierarchy"],
-	many: ["Attribute", "Parent"],
+  one: ["Generic", "Attribute", "Component", "Parent", "Hierarchy"],
+  many: ["Attribute", "Parent"],
 };
 
 export const RELATIONSHIP_VIEW_BLACKLIST = [
-	"member_of_groups",
-	"subscriber_of_groups",
-	"children",
-	"profiles",
+  "member_of_groups",
+  "subscriber_of_groups",
+  "children",
+  "profiles",
 ];
 
-export const relationshipKindForForm: Array<RelationshipKind> = [
-	"Attribute",
-	"Parent",
-];
+export const relationshipKindForForm: Array<RelationshipKind> = ["Attribute", "Parent"];
 
 export const PROPOSED_CHANGES_EDITABLE_STATE = ["open", "closed"];
 
@@ -167,9 +165,9 @@ export const SEARCH_PARTIAL_MATCH = "partial_match";
 export const SEARCH_FILTERS = [SEARCH_ANY_FILTER, SEARCH_PARTIAL_MATCH];
 
 export const DIFF_TABS = {
-	DATA: "data",
-	FILES: "files",
-	ARTIFACTS: "artifacts",
-	SCHEMA: "schema",
-	CHECKS: "checks",
+  DATA: "data",
+  FILES: "files",
+  ARTIFACTS: "artifacts",
+  SCHEMA: "schema",
+  CHECKS: "checks",
 };
