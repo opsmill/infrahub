2cc2dac8987679f8c69c10864fc71d1687de6d30
pa-lem <pa-lem@hotmail.fr>
2026-02-04 16:05:28 +0100

    fix type after merge

--- frontend/app/src/entities/artifacts/types.ts
+++ frontend/app/src/entities/artifacts/types.ts
@@ -1,32 +1,31 @@
-import type { NodeCore } from "@/entities/nodes/types";
 import type { CoreArtifact } from "@/shared/api/graphql/generated/graphql";
+
+import type { NodeCore } from "@/entities/nodes/types";
 
 export type ArtifactStatus = "Error" | "Pending" | "Processing" | "Ready";
 
 export interface ArtifactObject extends NodeCore {
-	checksum: NonNullable<CoreArtifact["checksum"]> & { value: string };
-	content_type: NonNullable<CoreArtifact["content_type"]> & { value: string };
-	storage_id: NonNullable<CoreArtifact["storage_id"]> & { value: string };
-	status: NonNullable<CoreArtifact["status"]> & { value: ArtifactStatus };
-	definition: NonNullable<CoreArtifact["definition"]> & { node: NodeCore };
-	object: NonNullable<CoreArtifact["object"]> & { node: NodeCore };
+  checksum: NonNullable<CoreArtifact["checksum"]> & { value: string };
+  content_type: NonNullable<CoreArtifact["content_type"]> & { value: string };
+  storage_id: NonNullable<CoreArtifact["storage_id"]> & { value: string };
+  status: NonNullable<CoreArtifact["status"]> & { value: ArtifactStatus };
+  definition: NonNullable<CoreArtifact["definition"]> & { node: NodeCore };
+  object: NonNullable<CoreArtifact["object"]> & { node: NodeCore };
 }
 
-export function assertArtifactObject(
-	data: NodeCore | null | undefined,
-): ArtifactObject | null {
-	const artifact = data as Partial<ArtifactObject> | null | undefined;
+export function assertArtifactObject(data: NodeCore | null | undefined): ArtifactObject | null {
+  const artifact = data as Partial<ArtifactObject> | null | undefined;
 
-	if (
-		!artifact?.content_type?.value ||
-		!artifact.storage_id?.value ||
-		!artifact.status?.value ||
-		!artifact.checksum?.value ||
-		!artifact.definition?.node ||
-		!artifact.object?.node
-	) {
-		return null;
-	}
+  if (
+    !artifact?.content_type?.value ||
+    !artifact.storage_id?.value ||
+    !artifact.status?.value ||
+    !artifact.checksum?.value ||
+    !artifact.definition?.node ||
+    !artifact.object?.node
+  ) {
+    return null;
+  }
 
-	return artifact as ArtifactObject;
+  return artifact as ArtifactObject;
 }
