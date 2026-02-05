61cf6d0c438bd2950d1bfbbd14b7c16e766f4d5f
bilalabbad <bilal@opsmill.com>
2026-02-05 18:47:05 +0100

    update check counter

--- frontend/app/src/entities/proposed-changes/ui/tabs/checks-tab.tsx
+++ frontend/app/src/entities/proposed-changes/ui/tabs/checks-tab.tsx
@@ -1,8 +1,6 @@
-import { gql, useQuery } from "@apollo/client";
+import { DIFF_TABS } from "@/shared/config/constants";
 
-import { DIFF_TABS, PROPOSED_CHANGES_OBJECT } from "@/shared/config/constants";
-
-import { getProposedChangesChecks } from "@/entities/proposed-changes/api/getProposedChangesChecks";
+import { useGetValidatorsQuery } from "@/entities/diff/domain/get-validators.query";
 import { ProposedChangeTab } from "@/entities/proposed-changes/ui/tabs/proposed-change-tab";
 
 export interface ChecksTabProps {
@@ -10,26 +8,14 @@ export interface ChecksTabProps {
 }
 
 export function ChecksTab({ proposedChangeId }: ChecksTabProps) {
-  const queryString = getProposedChangesChecks({
-    id: proposedChangeId,
-    kind: PROPOSED_CHANGES_OBJECT,
-  });
-
-  const query = gql`
-    ${queryString}
-  `;
-
-  const { loading, data } = useQuery(query);
-
-  const result = data ? data[PROPOSED_CHANGES_OBJECT]?.edges[0]?.node : {};
-  const count = result?.validations?.count ?? 0;
+  const { isPending, data: validators } = useGetValidatorsQuery({ proposedChangeId });
 
   return (
     <ProposedChangeTab
       tabId={DIFF_TABS.CHECKS}
       label="Checks"
-      count={count}
-      isCountLoading={loading}
+      count={validators?.length}
+      isCountLoading={isPending}
     />
   );
 }
