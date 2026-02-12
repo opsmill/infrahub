da7768bb79be3eb104844294dde985a9be89d924
pa-lem <pa-lem@hotmail.fr>
2026-02-12 18:07:02 +0100

    lint

--- frontend/app/src/pages/objects/object-convert.tsx
+++ frontend/app/src/pages/objects/object-convert.tsx
@@ -26,7 +26,10 @@ function ObjectConvertPage() {
   }
 
   return (
-    <RequireObjectPermissions objectKind={schema.kind ?? ""} loadingClassName="h-[calc(100vh-10.5rem)]">
+    <RequireObjectPermissions
+      objectKind={schema.kind ?? ""}
+      loadingClassName="h-[calc(100vh-10.5rem)]"
+    >
       {({ permission }) => {
         return <ObjectConvert objectSchema={schema} objectId={objectId} permission={permission} />;
       }}
