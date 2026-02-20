36603cb66d90fa926738e0c89769e3478842161b
Aaron McCarty <ajtmccarty@pm.me>
2026-02-20 09:59:35 -0600

    add migration to delete duplicated schemas on the default branch (#8418)

--- backend/infrahub/core/migrations/graph/__init__.py
+++ backend/infrahub/core/migrations/graph/__init__.py
@@ -58,6 +58,7 @@ from .m053_fix_branch_level_zero import Migration053
 from .m054_cleanup_orphaned_nodes import Migration054
 from .m055_remove_webhook_validate_certificates_default import Migration055
 from .m056_update_schema_node_generic_constraints import Migration056
+from .m057_deduplicate_schema_nodes import Migration057
 
 if TYPE_CHECKING:
     from ..shared import MigrationTypes
@@ -120,6 +121,7 @@ MIGRATIONS: list[type[MigrationTypes]] = [
     Migration054,
     Migration055,
     Migration056,
+    Migration057,
 ]
 
 
