55249abf1f0d683a4a05251903b40a8ed62d1eba
Baptiste <32564248+BaptisteGi@users.noreply.github.com>
2026-02-24 15:04:29 +0100

    Add object file topic (#8433)

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -185,12 +185,13 @@ const sidebars: SidebarsConfig = {
             },
             {
               type: 'category',
-              label: 'Transforms',
+              label: 'Transforms & Artifacts',
               link: {
                 type: 'generated-index',
               },
               items: [
                 'topics/transformation',
+                'topics/artifact',
               ],
             },
             {
@@ -217,12 +218,12 @@ const sidebars: SidebarsConfig = {
             },
             {
               type: 'category',
-              label: 'Artifacts',
+              label: 'File Storage',
               link: {
                 type: 'generated-index',
               },
               items: [
-                'topics/artifact',
+                'topics/file-object',
                 'topics/object-storage',
               ],
             },
