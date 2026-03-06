56bd5a7081645e9eddf13432a4d125ab526c6f11
pa-lem <pa-lem@hotmail.fr>
2026-03-06 17:17:42 +0100

    include proposed change id for diff summary

--- frontend/app/src/entities/diff/domain/get-diff-summary.query.ts
+++ frontend/app/src/entities/diff/domain/get-diff-summary.query.ts
@@ -4,10 +4,10 @@ import type { QueryConfig } from "@/shared/api/types";
 
 import { type GetDiffSummaryParams, getDiffSummary } from "@/entities/diff/domain/get-diff-summary";
 
-export function getDiffSummaryQueryOptions({ branch, filters }: GetDiffSummaryParams) {
+export function getDiffSummaryQueryOptions({ branch, filters, proposedChangeId }: GetDiffSummaryParams) {
   return queryOptions({
-    queryKey: ["diff-summary", branch, filters],
-    queryFn: () => getDiffSummary({ branch, filters }),
+    queryKey: ["diff-summary", branch, filters, proposedChangeId],
+    queryFn: () => getDiffSummary({ branch, filters, proposedChangeId }),
   });
 }
 
