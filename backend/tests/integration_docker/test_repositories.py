c4e8e95df163c1b19795e792ef144afdf290240b
Babatunde Olusola <solababatunde12@gmail.com>
2026-03-10 10:08:13 +0100

    remove unneeded fixtures

--- backend/tests/integration_docker/test_repositories.py
+++ backend/tests/integration_docker/test_repositories.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
+import pytest
 from infrahub_sdk import InfrahubClient
-from infrahub_sdk.protocols import CoreReadOnlyRepository, CoreRepository
+from infrahub_sdk.protocols import CoreGenericRepository, CoreReadOnlyRepository, CoreRepository
 from infrahub_sdk.testing.docker import TestInfrahubDockerClient
 from infrahub_sdk.testing.repository import GitRepo, GitRepoType
 
@@ -10,30 +11,36 @@ from tests.helpers.fixtures import get_fixtures_dir
 
 
 class TestRepositoryOperationalStatus(TestInfrahubDockerClient):
-    async def test_read_only_repo_operational_status(self, client: InfrahubClient, remote_repos_dir: Path) -> None:
+    @pytest.mark.parametrize(
+        "git_repo_type,repo_kind",
+        [
+            pytest.param(
+                GitRepoType.READ_ONLY,
+                CoreReadOnlyRepository,
+                id="read-only-repo",
+            ),
+            pytest.param(
+                GitRepoType.INTEGRATED,
+                CoreRepository,
+                id="core-repo",
+            ),
+        ],
+    )
+    async def test_repo_operational_status(
+        self,
+        client: InfrahubClient,
+        remote_repos_dir: Path,
+        git_repo_type: GitRepoType,
+        repo_kind: CoreGenericRepository,
+    ) -> None:
         fixture_dir = get_fixtures_dir()
-        repo_name = "read-only-repo"
+        repo_name = "test_base"
         repo_dir = fixture_dir / "repos" / repo_name / "initial__main"
-        repo = GitRepo(
-            type=GitRepoType.READ_ONLY, name=repo_name, src_directory=repo_dir, dst_directory=remote_repos_dir
-        )
+        repo = GitRepo(type=git_repo_type, name=repo_name, src_directory=repo_dir, dst_directory=remote_repos_dir)
         await repo.add_to_infrahub(client=client)
         in_sync = await repo.wait_for_sync_to_complete(client=client)
         assert in_sync
 
-        repos = await client.all(kind=CoreReadOnlyRepository)
-        assert len(repos) == 1
-        assert repos[0].operational_status.value == RepositoryOperationalStatus.ONLINE.value
-
-    async def test_core_repo_operational_status(self, client: InfrahubClient, remote_repos_dir: Path) -> None:
-        fixture_dir = get_fixtures_dir()
-        repo_name = "core-repo"
-        repo_dir = fixture_dir / "repos" / repo_name / "initial__main"
-        repo = GitRepo(name=repo_name, src_directory=repo_dir, dst_directory=remote_repos_dir)
-        await repo.add_to_infrahub(client=client)
-        in_sync = await repo.wait_for_sync_to_complete(client=client)
-        assert in_sync
-
-        repos = await client.all(kind=CoreRepository)
+        repos = await client.all(kind=repo_kind)
         assert len(repos) == 1
         assert repos[0].operational_status.value == RepositoryOperationalStatus.ONLINE.value
