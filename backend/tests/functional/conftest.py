389367d2d2dd210cec31f94686ae00901004135c
Sola Babatunde <solababatunde12@gmail.com>
2026-03-19 14:22:14 +0100

    remove unnecessary test

--- backend/tests/functional/conftest.py
+++ backend/tests/functional/conftest.py
@@ -3,8 +3,17 @@ from collections.abc import Generator
 import pytest
 from prefect.testing.utilities import prefect_test_harness
 
+from infrahub import config
+
 
 @pytest.fixture(scope="session", autouse=True)
 def prefect_test_fixture() -> Generator[None]:
     with prefect_test_harness(server_startup_timeout=60):
         yield
+
+
+@pytest.fixture
+def delete_branch_after_merge_reset_config() -> Generator[None]:
+    original = config.SETTINGS.main.delete_branch_after_merge
+    yield
+    config.SETTINGS.main.delete_branch_after_merge = original
