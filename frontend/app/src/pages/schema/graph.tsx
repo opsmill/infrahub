dd9a292c9a33173331de345f437cc21d4463d271
pa-lem <pa-lem@hotmail.fr>
2026-03-23 14:00:56 +0100

    add qsp for hilighted node

--- frontend/app/src/pages/schema/graph.tsx
+++ frontend/app/src/pages/schema/graph.tsx
@@ -1,5 +1,8 @@
 import { SchemaVisualizer, type SchemaVisualizerData } from "infrahub-schema-visualizer";
 import { useAtomValue } from "jotai";
+import { parseAsString, useQueryState } from "nuqs";
+
+import { QSP } from "@/shared/config/qsp";
 
 import {
   genericSchemasAtom,
@@ -13,12 +16,18 @@ function SchemaGraph() {
   const generics = useAtomValue(genericSchemasAtom);
   const profiles = useAtomValue(profileSchemasAtom);
   const templates = useAtomValue(templateSchemasAtom);
+  const [highlightNodeId, setHighlightNodeId] = useQueryState(QSP.HIGHLIGHT, parseAsString);
 
   const schemaData: SchemaVisualizerData = { nodes, generics, profiles, templates };
 
   return (
     <div className="flex min-h-125 flex-1">
-      <SchemaVisualizer data={schemaData} className="flex-1" />
+      <SchemaVisualizer
+        data={schemaData}
+        className="flex-1"
+        highlightNodeId={highlightNodeId}
+        onClearHighlight={() => setHighlightNodeId(null)}
+      />
     </div>
   );
 }
