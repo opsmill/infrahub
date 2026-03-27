54915c47cd9c21f2c9eb1ec8e843d28128018567
pa-lem <pa-lem@hotmail.fr>
2026-03-27 21:47:59 +0100

    update locator

--- frontend/app/tests/e2e/docs-regression-check/tutorials/tutorial-1_object-create-update-diff-and-merge.spec.ts
+++ frontend/app/tests/e2e/docs-regression-check/tutorials/tutorial-1_object-create-update-diff-and-merge.spec.ts
@@ -113,7 +113,7 @@ test.describe("Getting started with Infrahub - Object and branch creation, updat
 
     await test.step("View branch diff", async () => {
       await page.getByText("UpdatedOrganization ›Tenantmy-first-Tenant").click();
-      await expect(page.getByText("Testing Infrahub")).toBeVisible();
+      await expect(page.getByText("Testing Infrahub").first()).toBeVisible();
       await saveScreenshotForDocs(page, "tutorial_1_branch_diff");
       await expect(page.getByText("Changes from branch cr1234")).toBeVisible();
     });
