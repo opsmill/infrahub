022c0516d6648b18bcbcb9bbe398f32f59c2df7f
bilalabbad <bilal@opsmill.com>
2026-03-30 11:26:31 +0200

    more

--- frontend/app/src/shared/components/ui/card.tsx
+++ frontend/app/src/shared/components/ui/card.tsx
@@ -44,6 +44,7 @@ const CardWithBorderTitle = forwardRef<HTMLElement, HTMLAttributes<HTMLElement>>
   ({ className, ...props }, ref) => (
     <header
       ref={ref}
+      role="heading"
       className={classNames("rounded-t bg-neutral-100 px-3 py-2 font-semibold text-sm", className)}
       {...props}
     />
