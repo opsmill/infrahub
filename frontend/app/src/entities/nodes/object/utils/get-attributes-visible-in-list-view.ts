793fadd8ef4fca8053ee7ae3fa6b27c771467d9f
bilalabbad <bilal@opsmill.com>
2026-03-30 11:49:35 +0200

    Exclude fields with display "extra" from object list view table

--- frontend/app/src/entities/nodes/object/utils/get-attributes-visible-in-list-view.ts
+++ frontend/app/src/entities/nodes/object/utils/get-attributes-visible-in-list-view.ts
@@ -3,6 +3,9 @@ import type { AttributeKind, AttributeSchema } from "@/entities/schema/types";
 
 export function getAttributesVisibleInListView(attributes: AttributeSchema[]): AttributeSchema[] {
   return attributes.filter((attribute) => {
-    return ATTRIBUTE_KINDS_FOR_LIST_VIEW.includes(attribute.kind as AttributeKind);
+    return (
+      attribute.display !== "extra" &&
+      ATTRIBUTE_KINDS_FOR_LIST_VIEW.includes(attribute.kind as AttributeKind)
+    );
   });
 }
