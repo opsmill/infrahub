f91382be67db56057c10c34d8e176ef8698ce34b
pa-lem <pa-lem@hotmail.fr>
2026-03-30 14:41:12 +0200

    update locators

--- frontend/app/tests/e2e/proposed-changes/proposed-changes_diff.spec.ts
+++ frontend/app/tests/e2e/proposed-changes/proposed-changes_diff.spec.ts
@@ -28,15 +28,13 @@ test.describe("/proposed-changes diff data", () => {
     });
 
     await test.step("check diff data", async () => {
-      await expect(page.getByText("UpdatedInfra ›Interface L3Ethernet1")).toBeVisible();
+      await expect(page.getByText("Infra ›Interface L3")).toBeVisible();
       await expect(page.getByRole("link", { name: "Ethernet1" })).toBeVisible();
-      await expect(page.getByText("UpdatedInfra ›Deviceden1-edge1")).toBeVisible();
+      await expect(page.getByText("Infra ›Deviceden1-edge1")).toBeVisible();
       await expect(page.getByRole("link", { name: "den1-edge1" })).toBeVisible();
       await saveScreenshotForDocs(page, "topics/proposed_change/pc_tab_data");
-      await page.getByText("UpdatedInfra ›Interface L3Ethernet1").click();
-      await expect(
-        page.getByText("UpdatedInfra ›Interface L3Ethernet1 main den1-maintenance-")
-      ).toBeVisible();
+      await page.getByText("Infra ›Interface L3").click();
+      await expect(page.getByText("Infra ›Interface L3 main den1-maintenance-")).toBeVisible();
       await page.getByLabel("diff tree").getByText("den1-edge1").click();
       await page
         .getByText(
@@ -69,10 +67,10 @@ test.describe("/proposed-changes diff data", () => {
     });
 
     await test.step("comment proposed changes", async () => {
-      await page.locator("span").filter({ hasText: "UpdatedInfra ›Deviceden1-edge1" }).hover();
+      await page.locator("span").filter({ hasText: "Infra ›Deviceden1-edge1" }).hover();
       await page
         .locator("span")
-        .filter({ hasText: "UpdatedInfra ›Deviceden1-edge1" })
+        .filter({ hasText: "Infra ›Deviceden1-edge1" })
         .getByTestId("data-diff-add-comment")
         .click();
       await expect(page.getByText("Add a comment")).toBeVisible();
