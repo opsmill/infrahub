1d3c673782a1ca604c79444322353043d2507d2f
Bilal ABBAD <bilal@opsmill.com>
2026-03-31 11:07:51 +0200

    Clean up redundant get global permission call (#8741)

--- frontend/app/src/pages/role-management/index.tsx
+++ frontend/app/src/pages/role-management/index.tsx
@@ -1,33 +1,10 @@
-import { gql } from "@apollo/client";
 import { Outlet } from "react-router";
 
-import useQuery from "@/shared/api/graphql/useQuery";
-import ErrorScreen from "@/shared/components/errors/error-screen";
-import UnauthorizedScreen from "@/shared/components/errors/unauthorized-screen";
 import Content from "@/shared/components/layout/content";
-import { LoadingIndicator } from "@/shared/components/loading/loading-indicator";
-import { GLOBAL_PERMISSION_OBJECT } from "@/shared/config/constants";
 
-import { getObjectPermissionsQuery } from "@/entities/permission/queries/getObjectPermissions";
 import { RoleManagementNavigation } from "@/entities/role-manager/ui";
 
-function RoleManagement() {
-  const { loading, error } = useQuery(gql(getObjectPermissionsQuery(GLOBAL_PERMISSION_OBJECT)));
-
-  if (loading) {
-    return <LoadingIndicator message="Checking permissions..." className="h-full" />;
-  }
-
-  if (error) {
-    if (error.networkError?.statusCode === 403) {
-      const { message } = error.networkError?.result?.errors?.[0] ?? {};
-
-      return <UnauthorizedScreen message={message} />;
-    }
-
-    return <ErrorScreen message="Something went wrong when fetching the permissions." />;
-  }
-
+export function Component() {
   return (
     <Content.Card>
       <Content.CardTitle
@@ -41,8 +18,4 @@ function RoleManagement() {
       <Outlet />
     </Content.Card>
   );
-}
-
-export function Component() {
-  return <RoleManagement />;
 }
