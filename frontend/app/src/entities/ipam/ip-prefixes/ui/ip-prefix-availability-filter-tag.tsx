d8451845161b77b724fc52ce41276a1ff64db948
pa-lem <pa-lem@hotmail.fr>
2026-04-01 16:45:18 +0200

    start fix ip filter

--- frontend/app/src/entities/ipam/ip-prefixes/ui/ip-prefix-availability-filter-tag.tsx
+++ frontend/app/src/entities/ipam/ip-prefixes/ui/ip-prefix-availability-filter-tag.tsx
@@ -1,5 +1,4 @@
 import type { TagProps } from "react-aria-components";
-import { useParams } from "react-router";
 
 import useFilters from "@/shared/hooks/useFilters";
 
@@ -14,9 +13,7 @@ import { FilterTag } from "@/entities/nodes/object/ui/filters/filter-tag";
 
 export function IpPrefixAvailabilityFilterTag({ ...props }: TagProps) {
   const [filters] = useFilters();
-  const { objectId } = useParams();
 
-  if (!objectId) return null; // to hide it on IPAM homepage
   if (hasIncompatibleFiltersForIpAvailability(filters)) return null;
 
   const currentIpAvailabilityFilter = filters.find(
