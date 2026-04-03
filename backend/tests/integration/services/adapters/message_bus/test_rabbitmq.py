3e9d317814eb117feb4320d4a802a818b1fdd500
Aaron McCarty <aaron@opsmill.com>
2026-04-03 13:58:29 -0700

    polling and longer wait for RMQ test that is flaky in CI

--- backend/tests/integration/services/adapters/message_bus/test_rabbitmq.py
+++ backend/tests/integration/services/adapters/message_bus/test_rabbitmq.py
@@ -438,7 +438,10 @@ async def test_rabbitmq_on_message(rabbitmq_api: RabbitMQManager, fake_log: Fake
 
     with patch("infrahub.message_bus.operations.send.echo.get_logger", return_value=fake_log):
         await bus.send(message=messages.SendEchoRequest(message="Hello there"))
-        await asyncio.sleep(delay=1)
+        for _ in range(50):
+            if fake_log.info_logs:
+                break
+            await asyncio.sleep(delay=0.2)
         await bus.shutdown()
 
     assert fake_log.info_logs == ["Received message: Hello there"]
@@ -457,7 +460,10 @@ async def test_rabbitmq_on_message_invalid_routing_key(rabbitmq_api: RabbitMQMan
         await bus.publish(
             routing_key="request.something.invalid", message=messages.SendEchoRequest(message="Hello there")
         )
-        await asyncio.sleep(delay=1)
+        for _ in range(50):
+            if fake_log.error_logs:
+                break
+            await asyncio.sleep(delay=0.2)
         await bus.shutdown()
 
     assert fake_log.info_logs == []
