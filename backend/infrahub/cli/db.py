ceeff3fc14923cbbe6bf1f69691ca2bf93fbb77b
Guillaume Mazoyer <guillaume@opsmill.com>
2026-04-09 13:44:38 +0200

    Do not suppress succesful core schema update log

--- backend/infrahub/cli/db.py
+++ backend/infrahub/cli/db.py
@@ -767,12 +767,13 @@ async def update_core_schema(db: InfrahubDatabase, initialize: bool = True, debu
         get_migration_console().log(f"{ERROR_BADGE} | Schema update failed: {exc}")
         raise typer.Exit(1) from exc
 
+    message = f"{SUCCESS_BADGE} | Core Schema updated successfully"
+
     branches = await get_branches_needing_rebase(db=db)
     if branches:
         branch_names = ", ".join(b.name for b in branches)
-        get_migration_console().log(
-            f"The Core Schema has been updated, make sure to rebase the following branches: {branch_names}"
-        )
+        message += f". Make sure to rebase the following branches: {branch_names}"
+    get_migration_console().log(message)
     if debug:
         get_migration_console().log(f"New schema hash: {default_branch.active_schema_hash.main}")
 
