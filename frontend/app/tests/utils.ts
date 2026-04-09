d7e57fb2ea5a34262464094ac7b3db86beab4da4
bilalabbad <bilal@opsmill.com>
2026-04-09 19:04:32 +0200

    fix e2e

--- frontend/app/tests/utils.ts
+++ frontend/app/tests/utils.ts
@@ -1,4 +1,4 @@
-import { expect, type Page } from "@playwright/test";
+import type { Page } from "@playwright/test";
 
 export const saveScreenshotForDocs = async (page: Page, filename: string) => {
   if (!process.env.UPDATE_DOCS_SCREENSHOTS) return;
@@ -8,20 +8,6 @@ export const saveScreenshotForDocs = async (page: Page, filename: string) => {
     path: `../../docs/docs/media/${filename}.png`,
     animations: "disabled",
   });
-};
-
-export const createBranch = async (page: Page, branchName: string) => {
-  await page.getByTestId("branch-selector-trigger").click();
-  await page.getByTestId("create-branch-button").click();
-  await page.getByLabel("New branch name *").fill(branchName);
-  await page.getByRole("button", { name: "Create a new branch" }).click();
-  await expect(page.getByTestId("branch-selector-trigger")).toContainText(branchName);
-};
-
-export const deleteBranch = async (page: Page, branchName: string) => {
-  await page.goto("/branches/" + branchName);
-  await page.getByRole("button", { name: "Delete" }).click();
-  await page.getByTestId("modal-delete-confirm").click();
 };
 
 export const generateRandomBranchName = (prefix?: string) => {
