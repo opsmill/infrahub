cb0b436b9371c81eb943850f5830b2c3ff64e06a
pa-lem <pa-lem@hotmail.fr>
2026-04-14 21:31:15 +0200

    update pc merge action

--- frontend/app/src/entities/proposed-changes/ui/action-button/pc-merge-button.tsx
+++ frontend/app/src/entities/proposed-changes/ui/action-button/pc-merge-button.tsx
@@ -1,5 +1,6 @@
 import { Icon } from "@iconify-icon/react";
 import { useAtomValue } from "jotai";
+import { useQueryState } from "nuqs";
 import { toast } from "react-toastify";
 
 import { queryClient } from "@/shared/api/rest/client";
@@ -7,8 +8,8 @@ import { ALERT_TYPES, Alert } from "@/shared/components/ui/alert";
 import { Button } from "@/shared/components/ui/button";
 import { Tooltip } from "@/shared/components/ui/tooltip";
 import { PROPOSED_CHANGES_OBJECT } from "@/shared/config/constants";
+import { QSP } from "@/shared/config/qsp";
 
-import { useNavigateAfterBranchRemoval } from "@/entities/branches/ui/hooks/use-navigate-after-branch-removal";
 import { useConfig } from "@/entities/config/ui/config-provider";
 import { useUpdateObjectMutation } from "@/entities/nodes/object/ui/queries/update-object.mutation";
 import { MERGE_STATE } from "@/entities/proposed-changes/constants";
@@ -22,7 +23,7 @@ export const MergeButton = ({ setOpen }: ProposedChangeActionButtonProps) => {
 
   const proposedChangesDetails = useAtomValue(proposedChangedState);
   const config = useConfig();
-  const navigateAfterBranchRemoval = useNavigateAfterBranchRemoval();
+  const [branchInQueryString, setBranchInQueryString] = useQueryState(QSP.BRANCH);
 
   const { mutate, isPending } = useUpdateObjectMutation({
     onSuccess: async () => {
@@ -39,8 +40,8 @@ export const MergeButton = ({ setOpen }: ProposedChangeActionButtonProps) => {
 
       toast(<Alert type={ALERT_TYPES.SUCCESS} message={message} />);
 
-      if (deleteBranchAfterMerge) {
-        navigateAfterBranchRemoval("/proposed-changes", sourceBranch);
+      if (deleteBranchAfterMerge && sourceBranch && branchInQueryString === sourceBranch) {
+        setBranchInQueryString(null);
       }
     },
     onError: () => {
