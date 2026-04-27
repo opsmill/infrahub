5dcf3c8f23fa47f0098efc3996f5b195f456d1c5
bilalabbad <bilal@opsmill.com>
2026-04-27 12:59:19 +0200

    fix e2e

--- frontend/app/src/entities/nodes/object/ui/object-table/toolbar/actions/groups/group-panel.tsx
+++ frontend/app/src/entities/nodes/object/ui/object-table/toolbar/actions/groups/group-panel.tsx
@@ -1,19 +1,27 @@
-import { Card, CardHeader, type CardHeaderProps, type CardProps } from "@infrahub/ui/card";
+import { Card, type CardProps } from "@infrahub/ui/card";
 import type React from "react";
 
 import { classNames } from "@/shared/utils/common";
 
 export function GroupCard({ className, ...props }: CardProps) {
   return (
-    <Card
-      className={classNames("max-h-[min(47rem,calc(100vh-6rem))] shadow-sm", className)}
-      {...props}
-    />
+    <Card className={classNames("max-h-[min(47rem,calc(100vh-6rem))]", className)} {...props} />
   );
 }
 
-export function GroupPanelHeader({ className, ...props }: CardHeaderProps) {
-  return <CardHeader className={classNames("h-10 text-xs", className)} {...props} />;
+export function GroupPanelHeader({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLHeadingElement>) {
+  return (
+    <h3
+      className={classNames(
+        "flex h-10 shrink-0 items-center border-gray-200 border-b p-2 font-medium text-neutral-600 text-xs",
+        className
+      )}
+      {...props}
+    />
+  );
 }
 
 export function GroupPanelBody({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
