59906cd38c7120a61e6799a81f66f9b259ddbea7
Sola Babatunde <solababatunde12@gmail.com>
2026-04-27 11:22:00 +0100

    fix rff

--- backend/infrahub/core/relationship/model.py
+++ backend/infrahub/core/relationship/model.py
@@ -7,7 +7,6 @@ from dataclasses import dataclass
 from typing import (
     TYPE_CHECKING,
     Any,
-    Generic,
     Iterable,
     Iterator,
     Literal,
@@ -57,7 +56,6 @@ if TYPE_CHECKING:
 
 
 PeerType = TypeVar("PeerType")
-RelationshipManagerPeerType = TypeVar("RelationshipManagerPeerType")
 
 PREFIX_PROPERTY = "_relation__"
 INDEX_DEFAULT_STOP = sys.maxsize
@@ -852,7 +850,7 @@ class RelationshipValidatorList:
         self.validate_max()
 
 
-class RelationshipManager(Generic[RelationshipManagerPeerType]):
+class RelationshipManager[RelationshipManagerPeerType]:
     def __init__(
         self, schema: RelationshipSchema, branch: Branch, at: Timestamp, node: Node, is_from_profile: bool = False
     ) -> None:
