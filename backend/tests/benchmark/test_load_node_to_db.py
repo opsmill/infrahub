22b9f9a9a5abb1f4a462b890fb9f0bd6453edb84
Aaron McCarty <aaron@opsmill.com>
2026-05-13 15:10:47 -0700

    un-rename benchmark test

--- backend/tests/benchmark/test_load_node_to_db.py
+++ backend/tests/benchmark/test_load_node_to_db.py
@@ -13,7 +13,7 @@ from infrahub.core.timestamp import Timestamp
 from infrahub.database import InfrahubDatabase
 
 
-def test_create_node_in_db_node_schema(
+def test_load_node_to_db_node_schema(
     aio_benchmark: Callable[..., Any], db: InfrahubDatabase, default_branch: Branch
 ) -> None:
     registry.schema = SchemaManager()
