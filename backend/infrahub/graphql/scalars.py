21310e0160ee719acf108019a49b33d00790f3b3
Sola Babatunde <solababatunde12@gmail.com>
2026-05-25 08:42:07 +0100

    update tests

--- backend/infrahub/graphql/scalars.py
+++ backend/infrahub/graphql/scalars.py
@@ -23,19 +23,22 @@ class FixedGenericScalar(GenericScalar):
     """
 
     @staticmethod
-    def parse_literal(ast: ValueNode, _variables: dict[str, Any] | None = None) -> Any:  # noqa: PLR0911
+    def parse_literal(ast: ValueNode, _variables: dict[str, Any] | None = None) -> Any:
+        result: Any = None
         if isinstance(ast, (StringValueNode, BooleanValueNode)):
-            return ast.value
-        if isinstance(ast, IntValueNode):
+            result = ast.value
+        elif isinstance(ast, IntValueNode):
             num = int(ast.value)
             if MIN_INT <= num <= MAX_INT:
-                return num
+                result = num
         elif isinstance(ast, FloatValueNode):
-            return float(ast.value)
+            result = float(ast.value)
         elif isinstance(ast, ListValueNode):
-            return [FixedGenericScalar.parse_literal(v, _variables) for v in ast.values]
+            result = [FixedGenericScalar.parse_literal(v, _variables) for v in ast.values]
         elif isinstance(ast, ObjectValueNode):
-            return {field.name.value: FixedGenericScalar.parse_literal(field.value, _variables) for field in ast.fields}
+            result = {
+                field.name.value: FixedGenericScalar.parse_literal(field.value, _variables) for field in ast.fields
+            }
         elif isinstance(ast, VariableNode) and _variables is not None:
-            return _variables.get(ast.name.value)
-        return None
+            result = _variables.get(ast.name.value)
+        return result
