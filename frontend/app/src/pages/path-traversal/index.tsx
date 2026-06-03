eb9cedfe699f6cf98097177ecaf33b3cea012c45
pa-lem <pa-lem@hotmail.fr>
2026-06-03 08:35:57 +0200

    use card

--- frontend/app/src/pages/path-traversal/index.tsx
+++ frontend/app/src/pages/path-traversal/index.tsx
@@ -1,9 +1,11 @@
+import Content from "@/shared/components/layout/content";
+
 import { PathTraversalPage } from "@/entities/path-traversal/ui/path-traversal-page";
 
 export function Component() {
   return (
-    <div className="flex-1 overflow-hidden rounded-lg border border-gray-200 bg-white">
+    <Content.Card className="grow">
       <PathTraversalPage />
-    </div>
+    </Content.Card>
   );
 }
