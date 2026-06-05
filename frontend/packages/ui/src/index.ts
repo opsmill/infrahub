cf73d9a268f0752aa07d5032676b99bf690715d6
bilalabbad <bilal@opsmill.com>
2026-06-05 12:15:45 +0200

    Added CheckboxCard to @infrahub/ui + fix boolean inputs in form

--- frontend/packages/ui/src/index.ts
+++ frontend/packages/ui/src/index.ts
@@ -5,6 +5,8 @@ export {
   LinkButton,
   type LinkButtonProps,
 } from "./components/button/button";
+export { CheckboxCard, type CheckboxCardProps } from "./components/checkbox-card/checkbox-card";
+export { Spinner, type SpinnerProps } from "./components/spinner/spinner";
 export {
   Card,
   CardContent,
