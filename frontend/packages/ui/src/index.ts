33da87f24670b853893c8dbe6882ff2d05d8fee1
Bilal ABBAD <bilal@opsmill.com>
2026-06-05 17:43:54 +0200

    Migrate Tree to @infrahub/ui (#9483)

--- frontend/packages/ui/src/index.ts
+++ frontend/packages/ui/src/index.ts
@@ -1,29 +1,37 @@
 export {
   Button,
+  type ButtonProps,
   buttonVariants,
   LinkButton,
-  type ButtonProps,
   type LinkButtonProps,
 } from "./components/button/button";
-export { Spinner, type SpinnerProps } from "./components/spinner/spinner";
 export {
   Card,
   CardContent,
-  CardHeader,
   type CardContentProps,
+  CardHeader,
   type CardHeaderProps,
   type CardProps,
 } from "./components/card/card";
+export { Meter, type MeterProps } from "./components/meter/meter";
 export {
   Modal,
   ModalOverlay,
   type ModalOverlayProps,
   type ModalProps,
 } from "./components/modal/modal";
-export { Meter, type MeterProps } from "./components/meter/meter";
-export { ScrollArea, type ScrollAreaProps } from "./components/scroll-area/scroll-area";
 export {
-  ResizablePanelGroup,
-  ResizablePanel,
   ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
 } from "./components/resizable/resizable";
+export { ScrollArea, type ScrollAreaProps } from "./components/scroll-area/scroll-area";
+export { Spinner, type SpinnerProps } from "./components/spinner/spinner";
+export {
+  Tree,
+  TreeItem,
+  TreeItemContent,
+  type TreeItemContentProps,
+  TreeItemLoader,
+  type TreeItemProps,
+} from "./components/tree/tree";
