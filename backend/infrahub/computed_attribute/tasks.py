e33a6820f3333a4b3b9ad7ab061c43f379828f8b
fatih-acar <fatih-acar@users.noreply.github.com>
2026-06-05 09:51:51 +0200

    Update backend/infrahub/computed_attribute/tasks.py

--- backend/infrahub/computed_attribute/tasks.py
+++ backend/infrahub/computed_attribute/tasks.py
@@ -205,7 +205,7 @@ async def trigger_update_python_computed_attributes(
     if not object_ids:
         return
 
-    chunk_size = int(os.environ.get("PREFECT_SERVER_EVENTS_MAXIMUM_RELATED_RESOURCES", "500"))
+    chunk_size = int(os.environ.get("PREFECT_SERVER_EVENTS_MAXIMUM_RELATED_RESOURCES", "500")) / 2
     for chunk in _chunk_ids(object_ids, chunk_size):
         await get_workflow().submit_workflow(
             workflow=COMPUTED_ATTRIBUTE_PROCESS_TRANSFORM,
