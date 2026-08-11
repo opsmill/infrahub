4d795b7cd4fbef9d8900d60137454626b75915b6
Baptiste <girard.baptiste@hotmail.fr>
2026-08-11 09:43:35 +0200

    Cleanup - improvements - links

--- docs/src/components/StandoutLink/index.tsx
+++ docs/src/components/StandoutLink/index.tsx
@@ -2,7 +2,7 @@ import Link from "@docusaurus/Link";
 
 export default function StandoutLink({ title, url, openInNewTab }) {
   return (
-    <Link to={url} autoAddBaseUrl {...openInNewTab && { target: "_blank" }} className="button button--primary button--lg margin-bottom--md">
+    <Link to={url} autoAddBaseUrl {...openInNewTab && { target: "_blank" }} className="button button--primary button--lg button--block margin-top--lg margin-bottom--md">
       {title} →
     </Link>
   )
