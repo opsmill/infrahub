29472f8e3186a7ffb13ff23a3a7d4eee5c540663
Baptiste <32564248+BaptisteGi@users.noreply.github.com>
2026-08-18 09:07:18 +0200

    docs: rework IPAM documentation
    docs: rework IPAM documentation

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -161,9 +161,11 @@ const sidebars: SidebarsConfig = {
           label: 'IPAM',
           link: { type: 'doc', id: 'ipam/overview' },
           items: [
-            'ipam/ip-namespaces',
-            'ipam/building-your-schema',
-            'ipam/automate-with-resource-manager',
+            'ipam/build-your-ipam-schema',
+            'ipam/use-namespaces',
+            'ipam/allocate-ips-and-prefixes',
+            'ipam/query-ipam-data',
+            'ipam/plan-changes-on-a-branch',
           ],
         },
         {
