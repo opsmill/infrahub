793f8bc9c7253e4b3fcc8bc2ceb6d0854cf387b6
Aaron McCarty <aaron@opsmill.com>
2026-09-03 11:32:22 -0700

    remove duplicated lines

--- backend/infrahub/core/branch/tasks.py
+++ backend/infrahub/core/branch/tasks.py
@@ -259,10 +259,6 @@ async def rebase_branch(branch: str, context: InfrahubContext, send_events: bool
             # could diverge between registry and database during a failed txn commit.
             registry.branch[user_branch.name] = user_branch
 
-            # Only update registry after txn commit. Otherwise, branch status and branched_from
-            # could diverge between registry and database during a failed txn commit.
-            registry.branch[user_branch.name] = user_branch
-
             if migration_baseline_schema is not None and pre_rebase_schema is not None:
                 # Update the registry and run migrations after the rebase, with rollback on failure.
                 # Schema nodes were already written by the rebase, so load that schema and apply only
